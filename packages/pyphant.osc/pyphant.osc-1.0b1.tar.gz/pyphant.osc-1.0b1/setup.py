07373ef28d52455636ebc7babc47af7d028570d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:10 +0000

    Add package pyphant.osc/1.0b1/pyphant.osc-1.0b1.tar.gz

---
+++ packages/pyphant.osc/pyphant.osc-1.0b1.tar.gz/pyphant.osc-1.0b1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Pyphant Organic Solar Cells toolbox
+Visit http://pyphant.sourceforge.net for more information.
+"""
+
+__author__ = "Andreas W. Liehr, Klaus Zimmermann"
+
+__id__ = '$Id$'
+__revision__ = '$Revision: 25 $'
+
+VERSION = '1.0b1'
+
+import setuptools
+
+setuptools.setup(
+    name="pyphant.osc",
+    version=VERSION,
+    author=__author__,
+    author_email='klaus.zimmermann@fmf.uni-freiburg.de',
+    maintainer='Klaus Zimmermann',
+    maintainer_email='klaus.zimmermann@fmf.uni-freiburg.de',
+    license="BSD",
+    description=__doc__,
+    install_requires=['pyphant>=1.0b1'],
+    packages=['OSC'],
+    entry_points="""
+    [pyphant.workers]
+    myeentry = OSC
+    """,
+    test_suite='OSC.tests'
+    )
