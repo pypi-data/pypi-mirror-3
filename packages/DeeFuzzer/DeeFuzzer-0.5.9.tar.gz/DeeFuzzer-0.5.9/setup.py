3e454d1ac6920ea4d22f73383421a7f31d6948a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:27 +0000

    Add package DeeFuzzer/0.5.9/DeeFuzzer-0.5.9.tar.gz

---
+++ packages/DeeFuzzer/DeeFuzzer-0.5.9.tar.gz/DeeFuzzer-0.5.9/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''The setup and build script for the python-twitter library.'''
+
+import os
+
+__version__ = '0.5.9'
+__author__ = 'yomguy@parisson.com'
+    
+# The base package metadata to be used by both distutils and setuptools
+METADATA = dict(
+  name = "DeeFuzzer",
+  version = __version__,
+  py_modules = ['deefuzzer'],
+  description='an easy and instant media streaming tool',
+  author='Guillaume Pellerin', 
+  author_email='yomguy@parisson.com',
+  license='License.txt',
+  url='http://svn.parisson.org/deefuzzer',
+  keywords='audio video streaming broadcast shout',
+  install_requires = ['setuptools', 'tinyurl', 'python-shout', 'python-twitter', 'mutagen'],
+  packages=['deefuzzer', 'deefuzzer.tools'], 
+  include_package_data = True,
+  scripts=['scripts/deefuzzer'], 
+  classifiers = ['Programming Language :: Python', 'Topic :: Internet :: WWW/HTTP :: Dynamic Content', 'Topic :: Multimedia :: Sound/Audio', 'Topic :: Multimedia :: Sound/Audio :: Players',],
+  long_description=open('README.txt').read(),
+)
+
+def Main():
+  # Use setuptools if available, otherwise fallback and use distutils
+  try:
+    import setuptools
+    setuptools.setup(**METADATA)
+  except ImportError:
+    import distutils.core
+    distutils.core.setup(**METADATA)
+    
+if __name__ == '__main__':
+    Main()
