c5d6cb1d8798ca9ed4dba17f0eaa6de390f1ac60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:47 +0000

    Add package mysolr/0.3/mysolr-0.3.tar.gz

---
+++ packages/mysolr/mysolr-0.3.tar.gz/mysolr-0.3/mysolr/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from mysolr_response import SolrResponse
+from mysolr import Solr
+
+__title__ = 'mysolr'
+__version__ = '0.3.0'
+__build__ = 0x000030
+__author__ = 'Rubén Abad, Miguel Olivares'
