e7e6df6e835d219e6a37396c28b3dce00e5a728c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:59 +0000

    Add package ikazuchi.plugins.pofile/0.1.2/ikazuchi.plugins.pofile-0.1.2.tar.bz2

---
+++ packages/ikazuchi.plugins.pofile/ikazuchi.plugins.pofile-0.1.2.tar.bz2/ikazuchi.plugins.pofile-0.1.2/src/ikazuchi/plugins/pofile.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import polib
+import re
+from ikazuchi.core.handler.base import BaseHandler
+from ikazuchi.core.handler.utils import get_and_check_file_access
+from ikazuchi.ikazuchi import (base_parser, subparsers)
+
+try:
+    from ikazuchi.locale import _
+except ImportError:
+    def _(s): return s
+
+__version__ = "0.1.2"
+
+_NOTRANSLATE_PTRN = re.compile(r"""(
+      ["|']*(%|%(.+?))[d|r|s]["|']*
+)""", re.U | re.X)
+
+# argument parser for pofile
+po_parser = subparsers.add_parser("pofile", parents=[base_parser])
+po_parser.set_defaults(po_file=None)
+po_parser.add_argument(dest="po_file", help="target po file")
+po_parser.add_argument("--version", action="version",
+                        version="%(prog)s {0}".format(__version__))
+
+class Handler(BaseHandler):
+    """
+    Handler class for translating PO file interactively
+    """
+    max_query = 100
+
+    def __init__(self, opts):
+        if not opts.api or opts.api == "microsoft":
+            self.method_name = "translate_array"
+        self.encoding = opts.encoding
+        po_file = get_and_check_file_access(opts.po_file)
+        self.po = polib.pofile(po_file, autodetect_encoding=False,
+                               encoding=self.encoding[1])
+        self.po.metadata["Content-Type"] = "text/plain; charset={0}".format(
+                                                self.encoding[1])
+
+    def _select_translation(self, ref, current, entered):
+        """define which translated string use"""
+        s = entered
+        if entered.lower() == "y":
+            s = ref
+        elif current and entered == "":
+            s = current
+        return s
+
+    def _call_method(self, api_method):
+        """translate msgid in po file"""
+        _prompt = _(u"Input: ").encode(self.encoding[0])
+        for pos in range(0, len(self.po), self.max_query):
+            sliced_po = self.po[pos:pos + self.max_query]
+            items = [self.markup_msgid_notranslate(p.msgid) for p in sliced_po]
+            api, ref = api_method(items)
+            _reference = _(u"reference({0}):").format(api)
+            for num, p in enumerate(sliced_po):
+                _ref = ref[num]
+                print u"{0:25}{1}".format(_(u"msgid:"), p.msgid)
+                if p.msgstr:
+                    print u"{0:25}{1}".format(_(u"current msgstr:"), p.msgstr)
+                print u"{0:25}{1}".format(_reference, _ref)
+                entered = unicode(raw_input(_prompt), self.encoding[0])
+                p.msgstr = self._select_translation(_ref, p.msgstr, entered)
+                self.po.save()
+                print u"{0:25}{1}".format(_(u"updated msgstr:"), p.msgstr)
+                print ""
+
+    @classmethod
+    def markup_msgid_notranslate(self, msgid):
+        msgid = msgid.replace(u"&", u"&amp;")
+        msgid = msgid.replace(u"<", u"&lt;")
+        msgid = msgid.replace(u">", u"&gt;")
+        repl = r"<span class=notranslate>\1</span>"
+        return re.sub(_NOTRANSLATE_PTRN, repl, msgid)
---
+++ packages/ikazuchi.plugins.pofile/ikazuchi.plugins.pofile-0.1.2.tar.bz2/ikazuchi.plugins.pofile-0.1.2/src/ikazuchi/tests/test_pofile_handler.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import tempfile
+from nose.tools import *
+
+# functions for test
+from ikazuchi.plugins.pofile import *
+
+class TestHandler(object):
+    class Option(object):
+        def __init__(self):
+            self.api = "google"
+            self.po_file = None
+            self.encoding = ["utf-8", "utf-8"]
+    option = Option()
+
+    def setup(self):
+        def make_po_file(path):
+            f = tempfile.NamedTemporaryFile(dir=path)
+            f.write('msgid "forest"\nmsgstr ""\nmsgid "book"\nmsgstr "hon"')
+            f.flush()
+            return f
+        self.po_file = make_po_file(tempfile.tempdir)
+        self.option.po_file = self.po_file.name
+
+    def teardown(self):
+        self.po_file.close()
+
+    def test_select_translation(self):
+        data = [
+            (["ref", "", "y"], "ref"),
+            (["ref", "cur", "y"], "ref"),
+            (["ref", "cur", ""], "cur"),
+            (["ref", "", ""], ""),
+            (["ref", "cur", "ent"], "ent"),
+            (["ref", "", "ent"], "ent"),
+        ]
+        h = Handler(self.option)
+        for d in data:
+            assert_equals(d[1], h._select_translation(*d[0]))
+
+def test_markup_msgid_notranslate():
+    from data.po.markup_notranslate import DATA_SET
+    _fmt = u"{0}_{1} ({2} ...)"
+    _func = u"test_markup_msgid_notranslate"
+    for num, (data, expected) in enumerate(DATA_SET):
+        name = _fmt.format(_func, num, str(data)[:10])
+        _assert = lambda e, a: assert_equal(e, a)
+        _assert.description = name
+        yield _assert, expected, Handler.markup_msgid_notranslate(data)
