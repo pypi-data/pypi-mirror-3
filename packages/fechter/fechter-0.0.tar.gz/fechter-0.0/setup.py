22f25cdbb50e180c7a03341688cd26cccd279334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:55 +0000

    Add package fechter/0.0/fechter-0.0.tar.gz

---
+++ packages/fechter/fechter-0.0.tar.gz/fechter-0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(name='fechter',
+      version='0.0',
+      description='a simple high-availability manager',
+      author='Johan Rydberg',
+      author_email='johan.rydberg@gmail.com',
+      url='http://github.com/jrydberg/fechter',
+      packages=find_packages() + ['twisted.plugins']
+)
