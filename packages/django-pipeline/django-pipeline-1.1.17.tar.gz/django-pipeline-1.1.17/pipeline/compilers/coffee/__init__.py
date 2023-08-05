4ec122053506a21dace6d2c987cecc6067cedecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:17 +0000

    Add package django-pipeline/1.1.17/django-pipeline-1.1.17.tar.gz

---
+++ packages/django-pipeline/django-pipeline-1.1.17.tar.gz/django-pipeline-1.1.17/pipeline/compilers/coffee/__init__.py
@@ -0,0 +1,13 @@
+from pipeline.conf import settings
+from pipeline.compilers import SubProcessCompiler
+
+
+class CoffeeScriptCompiler(SubProcessCompiler):
+    output_extension = 'js'
+
+    def match_file(self, path):
+        return path.endswith('.coffee')
+
+    def compile_file(self, content):
+        command = "%s -sc %s" % (settings.PIPELINE_COFFEE_SCRIPT_BINARY, settings.PIPELINE_COFFEE_SCRIPT_ARGUMENTS)
+        return self.execute_command(command, content)
