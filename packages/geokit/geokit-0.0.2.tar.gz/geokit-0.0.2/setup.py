da7380805ea86db864c479c88cd46fe4c5a8134d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:41 +0000

    Add package geokit/0.0.2/geokit-0.0.2.tar.gz

---
+++ packages/geokit/geokit-0.0.2.tar.gz/geokit-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(name='geokit',
+      version='0.0.2',
+      packages=find_packages(),
+      author='Alex Toney',
+      author_email='toneyalex@gmail.com',
+      url='',
+      license='BSD',
+      include_package_data=True,
+      description='Geocoding toolkit',
+      long_description='',
+      platforms=['any'],
+      classifiers='',
+      install_requires=['furl', 'geohash'],
+)
