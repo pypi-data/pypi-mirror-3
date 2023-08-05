a3f159fa4a867c8d24a491c515e318232b600f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:55 +0000

    Add package auslfe.formonline.content/0.4.0/auslfe.formonline.content-0.4.0.tar.gz

---
+++ packages/auslfe.formonline.content/auslfe.formonline.content-0.4.0.tar.gz/auslfe.formonline.content-0.4.0/README.txt
@@ -0,0 +1,34 @@
+Introduction
+============
+
+A simple Page-like content type for Plone, to be used with `auslfe.formonline.pfgadapter`__
+
+__ http://plone.org/products/auslfe.formonline.pfgadapter
+
+The basic installation of this product simply provide proper interface and workflow for use
+the "Form Online Adapter".
+
+An alternative import step ("Form Online: include FormOnline type") will also register a new
+content type that automatically use the proper workflow. This can simplify a lot the site configuration
+for not experienced Plone administrators.
+
+Credits
+=======
+
+Developed with the support of `Azienda USL Ferrara`__; Azienda USL Ferrara supports the
+`PloneGov initiative`__.
+
+.. image:: http://www.ausl.fe.it/logo_ausl.gif
+   :alt: Azienda USL's logo
+
+__ http://www.ausl.fe.it/
+__ http://www.plonegov.it/
+
+Authors
+=======
+
+This product was developed by RedTurtle Technology team.
+
+.. image:: http://www.redturtle.it/redturtle_banner.png
+   :alt: RedTurtle Technology Site
+   :target: http://www.redturtle.it/
---
+++ packages/auslfe.formonline.content/auslfe.formonline.content-0.4.0.tar.gz/auslfe.formonline.content-0.4.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.4.0'
+
+setup(name='auslfe.formonline.content',
+      version=version,
+      description="The Form Online content type for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 3.3",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='plonegov form plone',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.it',
+      url='http://plone.org/products/auslfe.formonline.pfgadapter',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['auslfe', 'auslfe.formonline'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
