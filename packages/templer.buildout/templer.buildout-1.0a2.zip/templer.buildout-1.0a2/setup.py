56d7a3a8784a0c5d2340e4ff7a2925269243db2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:33 +0000

    Add package templer.buildout/1.0a2/templer.buildout-1.0a2.zip

---
+++ packages/templer.buildout/templer.buildout-1.0a2.zip/templer.buildout-1.0a2/setup.py
@@ -0,0 +1,68 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a2'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+tests_require=[
+    'Cheetah', 
+    'PasteScript',
+    'templer.core'],
+
+setup(name='templer.buildout',
+      version=version,
+      description="Templer system extensions for buildouts and buildout recipes",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Framework :: Plone",
+        "Framework :: Buildout",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Code Generators",
+        ],
+      keywords='',
+      author='Cris Ewing',
+      author_email='cris@crisewing.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='MIT',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['templer'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'templer.core',
+      ],
+      tests_require=tests_require,
+      extras_require=dict(test=tests_require),
+      entry_points="""
+      [paste.paster_create_template]
+      recipe = templer.buildout:Recipe
+      basic_buildout = templer.buildout:BasicBuildout
+      
+      [templer.templer_structure]
+      bootstrap = templer.buildout.structures:BootstrapStructure
+      """,
+      )
