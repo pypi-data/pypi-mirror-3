37164088d2b2701140b6261f7446ceb642c64553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:33 +0000

    Add package Shake-SQLAlchemy/0.5/Shake-SQLAlchemy-0.5.tar.gz

---
+++ packages/Shake-SQLAlchemy/Shake-SQLAlchemy-0.5.tar.gz/Shake-SQLAlchemy-0.5/shake_sqlalchemy/types.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from shake.serializers import to_json, from_json
+from sqlalchemy.types import TypeDecorator, Text
+
+
+class JSONEncodedType(TypeDecorator):
+    """Represents an immutable structure as a JSON-encoded string.
+    """
+
+    impl = Text
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return None
+        try:
+            json_value = to_json(value)
+        except ValueError:
+            json_value = {}
+        return json_value
+
+    def process_result_value(self, json_value, dialect):
+        if json_value is None:
+            return {}
+        try:
+            value = from_json(json_value)
+        except ValueError:
+            value = {}
+        return value
+    
+    def copy(self):
+          return self.__class__()
+
