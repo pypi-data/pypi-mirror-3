1276794ae2f0faf290f36cbaa9dfba365fbabc26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:08 +0000

    Add package ccnmtldjango/0.15.0/ccnmtldjango-0.15.0.tar.gz

---
+++ packages/ccnmtldjango/ccnmtldjango-0.15.0.tar.gz/ccnmtldjango-0.15.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='ccnmtldjango',
+      version="0.15.0",
+      author="anders pearson",
+      description="Django template for CCNMTL",
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      install_requires = ["PasteScript"],
+      zip_safe=False,
+      package_data = {'' : ['*.*']},
+      include_package_data=True,
+      entry_points="""
+      [paste.paster_create_template]
+      ccnmtldjango = ccnmtldjango:CcnmtlDjangoTemplate
+      """
+      )
+	
