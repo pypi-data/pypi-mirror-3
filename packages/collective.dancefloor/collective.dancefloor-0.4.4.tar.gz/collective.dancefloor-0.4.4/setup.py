1dd55b8ecfe44ffe51c7fb7641257c7564c35e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package collective.dancefloor/0.4.4/collective.dancefloor-0.4.4.tar.gz

---
+++ packages/collective.dancefloor/collective.dancefloor-0.4.4.tar.gz/collective.dancefloor-0.4.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.4.4'
+
+setup(name='collective.dancefloor',
+      version=version,
+      description="S&D extension to allow local newsletters",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Andrea Cecchi, Ramon Bartl, Stefan Eletzhofer',
+      author_email='andrea.cecchi@redturtle.it, ramon.bartl@inquant.de, stefan.eletzhofer@inquant.de',
+      url='https://svn.plone.org/svn/collective/collective.dancefloor/trunk',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.dancing',
+          'archetypes.schemaextender',
+          'collective.monkeypatcher',
+      ],
+      entry_points="""
+      # -*- entry_points -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
