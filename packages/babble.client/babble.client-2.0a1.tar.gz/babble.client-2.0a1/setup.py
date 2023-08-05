8f0808d553b0c3613d9f76eb06f1e4f8370403c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:59 +0000

    Add package babble.client/2.0a1/babble.client-2.0a1.tar.gz

---
+++ packages/babble.client/babble.client-2.0a1.tar.gz/babble.client-2.0a1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.0a1'
+
+tests_require=[]
+
+setup(
+    name='babble.client',
+    version=version,
+    description="Babble: Instant messaging client for Plone",
+    long_description=open("README.txt").read() + "\n" +
+                    open(os.path.join("docs", "CHANGES.txt")).read() +
+                    open(os.path.join("docs", "CONTRIBUTORS.txt")).read(),
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+    "Framework :: Plone",
+    "Programming Language :: Python",
+    ],
+    keywords='plone syslab.com portlet onlinecontacts',
+    author='Syslab.com, JC Brand',
+    author_email='brand@syslab.com',
+    url='http://plone.org/products/babble.client',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    namespace_packages=['babble'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'simplejson',
+        'babble.server >= 1.0b1dev',
+        'Products.CMFPlone',
+        'plone.app.portlets',
+        'collective.js.blackbird'
+    ],
+    tests_require=tests_require,
+    extras_require=dict(tests=tests_require),
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
---
+++ packages/babble.client/babble.client-2.0a1.tar.gz/babble.client-2.0a1/src/babble/client/config.py
@@ -0,0 +1,10 @@
+I18N_DOMAIN = 'babble.client'
+
+SUCCESS = 0
+AUTH_FAIL = -1
+TIMEOUT = 1
+SERVER_FAULT = 2
+
+from datetime import datetime
+NULL_DATE = datetime.min.isoformat()
+
