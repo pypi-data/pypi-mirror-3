e82ed4fc9d7840dd71fb6569e13366c5ffb47b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:29 +0000

    Add package djangoutils/0.1.1/djangoutils-0.1.1.tar.gz

---
+++ packages/djangoutils/djangoutils-0.1.1.tar.gz/djangoutils-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='djangoutils',
+  version='0.1.1',
+  packages=find_packages(),
+  license='GPL',
+  author="Andrew Magee",
+  author_email="amagee@gmail.com",
+  description="Miscellaneous app-independent django utilities",
+  long_description="Miscellaneous app-independent django utilities",
+  keywords = "django utils",
+  url = "https://bitbucket.org/amagee/djangoutils",
+)
+
