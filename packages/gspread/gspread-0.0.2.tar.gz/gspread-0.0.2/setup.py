64dba6790305c9e7b60e91f975e454ccb1e5c9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:45 +0000

    Add package gspread/0.0.2/gspread-0.0.2.tar.gz

---
+++ packages/gspread/gspread-0.0.2.tar.gz/gspread-0.0.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import gspread
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='gspread',
+    packages=['gspread'],
+    description='Google Spreadsheets Python library',
+    version=gspread.__version__,
+    author='Anton Burnashev',
+    author_email='',
+    url='https://github.com/burnash/gspread',
+    keywords=['spreadsheets', 'google-spreadsheets'],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "Topic :: Office/Business :: Financial :: Spreadsheet",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
+    license='MIT'
+    )
