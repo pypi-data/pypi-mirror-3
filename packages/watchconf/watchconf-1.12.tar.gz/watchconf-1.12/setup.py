224739a4ce3aa5fefcb4df025290225d50162308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:08 +0000

    Add package watchconf/1.12/watchconf-1.12.tar.gz

---
+++ packages/watchconf/watchconf-1.12.tar.gz/watchconf-1.12/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import find_packages, setup
+
+curdir = os.path.dirname(os.path.abspath(__file__))
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+setup(
+    name = 'watchconf',
+    description = 'compare configuration between servers',
+    long_description = read('README.rst'),
+    license = 'http://www.gnu.org/licenses/gpl-2.0.html',
+    version = '1.12',
+    author = 'Òscar Vilaplana',
+    author_email = 'dev@oscarvilaplana.cat',
+    url = 'https://github.com/grimborg/watchconf',
+    install_requires = ['paramiko', 'flask', 'flask-cache', 'opster'],
+    packages=find_packages(curdir),
+    entry_points = {'console_scripts': ['watchconf=watchconf:run.command', ]},
+    )
