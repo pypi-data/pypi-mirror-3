1f10ed47e1f22771b44068c9b634079420aa3ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:49 +0000

    Add package django-pipeline/1.1.19/django-pipeline-1.1.19.tar.gz

---
+++ packages/django-pipeline/django-pipeline-1.1.19.tar.gz/django-pipeline-1.1.19/pipeline/compilers/less/__init__.py
@@ -0,0 +1,30 @@
+import os
+import tempfile
+
+from pipeline.conf import settings
+from pipeline.compilers import SubProcessCompiler
+
+
+class LessCompiler(SubProcessCompiler):
+    output_extension = 'css'
+
+    def match_file(self, filename):
+        return filename.endswith('.less')
+
+    def compile_file(self, content):
+        in_file, in_filename = tempfile.mkstemp()
+        in_file = os.fdopen(in_file, 'w+b')
+        in_file.write(content)
+        in_file.flush()
+
+        command = '%s %s %s' % (
+            settings.PIPELINE_LESS_BINARY,
+            settings.PIPELINE_LESS_ARGUMENTS,
+            in_filename
+        )
+        content = self.execute_command(command, content)
+
+        in_file.close()
+        os.remove(in_filename)
+
+        return content
