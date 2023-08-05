db85176ac594704251e9eed8867e376fd58e02d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:44 +0000

    Add package stringutil/1.1.1/stringutil-1.1.1.zip

---
+++ packages/stringutil/stringutil-1.1.1.zip/stringutil-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name       = 'stringutil',
+    version    = '1.1.1',
+    py_modules = ['stringutil'],
+    author     = 'sidedoor',
+    author_email = 'johnfraboni@gmail.com',
+    url          = 'http://www.johnfraboni.com',
+    description  = 'A collection of basic string utility methods, mostly created for learning python: Fixes in 1.1.1: 1. Adding recursive setting of indentation boolean.',
+    )
