46b9c95ab79b6a0a22e308d7745fedc6596eb6f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:07 +0000

    Add package PonyExpress/0.13stable/PonyExpress-0.13stable.tar.gz

---
+++ packages/PonyExpress/PonyExpress-0.13stable.tar.gz/PonyExpress-0.13stable/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='PonyExpress',
+    version='0.13stable',
+		author = 'Tony Landis',
+		author_email = 'tony.landis@gmail.com',
+    packages=['ponyexpress',],
+		url = 'https://github.com/tony-landis/PonyExpress',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+		install_requires=[
+			'Flask',
+			'Flask-Actions',
+			'gearman',
+			'formencode',
+			'couchdbkit',
+		]
+)
