400f3bb0522cea5d6bdd2731f8f0df225f8b25c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:19 +0000

    Add package PyVCF/0.0.0/PyVCF-0.0.0.tar.gz

---
+++ packages/PyVCF/PyVCF-0.0.0.tar.gz/PyVCF-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='PyVCF',
+    py_modules=['vcf'],
+    scripts=['vcf_melt'],
+    author='James Casbon',
+    author_email='casbon@gmail.com',
+    description='Variant Call Format (VCF) parser for python',
+)
