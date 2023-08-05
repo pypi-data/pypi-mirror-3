f21baba92f10e82143f53954dbf1f3baeb8e1a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:54 +0000

    Add package testjg/1.1.0/testjg-1.1.0.zip

---
+++ packages/testjg/testjg-1.1.0.zip/testjg-1.1.0/setup.py
@@ -0,0 +1,11 @@
+﻿from distutils.core import setup
+
+setup	(
+	name		= 'testjg',
+	version		= '1.1.0',
+	py_modules	= ['testjg'],
+	author		= 'jaegu',
+	author_email	= 'jaegu@jaegu.com',
+	url		= 'http://www.jargu.com',
+	description	= 'just for testing PyPI',
+	)
