356ef70cb19ab535562df00283e4659f28025f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:24 +0000

    Add package opster/3.3.1/opster-3.3.1.tar.gz

---
+++ packages/opster/opster-3.3.1.tar.gz/opster-3.3.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+import opster
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def desc():
+    info = read('README.rst')
+    try:
+        return info + '\n\n' + read('docs/changelog.rst')
+    except IOError:
+        # no docs
+        return info
+
+setup(
+    name = 'opster',
+    description = 'command line parsing speedster',
+    long_description = desc(),
+    license = 'BSD',
+    version = opster.__version__,
+    author = opster.__author__,
+    author_email = opster.__email__,
+    url = 'http://github.com/piranha/opster/',
+    classifiers = [
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        ],
+    py_modules = ['opster'],
+    platforms='any',
+    )
