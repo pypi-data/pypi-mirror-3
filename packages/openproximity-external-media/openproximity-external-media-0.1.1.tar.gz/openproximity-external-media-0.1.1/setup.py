5b057126d2cfd940aeb36f7125e7c1b05ac6e74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:21 +0000

    Add package openproximity-external-media/0.1.1/openproximity-external-media-0.1.1.tar.gz

---
+++ packages/openproximity-external-media/openproximity-external-media-0.1.1.tar.gz/openproximity-external-media-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name = "openproximity-external-media",
+    version = "0.1.1",
+    packages = ['openproximity'],
+    include_package_data = True,
+    author = "Naranjo Manuel Francisco",
+    author_email = "manuel@aircable.net",
+    description = "This is OpenProximity external metadata package",
+    keywords = "openproximity proximity marketing bluetooth static",
+    url = "https://github.com/OpenProximity/OpenProximity-web-3rdparty-static",
+)
