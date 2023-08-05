b61665fc5917e0b75d9596144cb909de7893f8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:06 +0000

    Add package appspace/0.3.3/appspace-0.3.3-py2.7.egg

---
+++ packages/appspace/appspace-0.3.3-py2.7.egg/appspace/test/apps.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from appspace import patterns
+
+apps = patterns(
+    '',
+    ('square', 'math.sqrt'),
+    ('fabulous', 'math.fabs'),
+    ('formit', 're.match'),
+    ('lower', 'string.lowercase'),
+    ('upper', 'string.uppercase'),
+    ('store', 'UserDict.UserDict'),
+)
