8e0bc9b9cc7f5bbc8bd9f623d7d268433575b368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:28 +0000

    Add package Couchapp/1.0.0/Couchapp-1.0.0.tar.gz

---
+++ packages/Couchapp/Couchapp-1.0.0.tar.gz/Couchapp-1.0.0/couchapp/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of couchapp released under the Apache 2 license. 
+# See the NOTICE for more information.
+
+version_info = (1, 0, 0)
+__version__ =  ".".join(map(str, version_info))
