3d3c0e338b330e6f5864259da735aea4dba5c4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:19 +0000

    Add package marturion/0.1.3/marturion-0.1.3.tar.gz

---
+++ packages/marturion/marturion-0.1.3.tar.gz/marturion-0.1.3/marturion/templatetags/marturion_tags.py
@@ -0,0 +1,49 @@
+from django import template
+
+from marturion.models import Testimonial
+
+
+register = template.Library()
+
+
+class TestimonialsNode(template.Node):
+    
+    @classmethod
+    def handle_token(cls, parser, token, order_by, **kwargs):
+        bits = token.split_contents()
+        
+        if len(bits) != 4:
+            raise template.TemplateSyntaxError("%r takes exactly three arguments "
+                "(second argument must be 'as')" % bits[0])
+        if bits[2] != "as":
+            raise template.TemplateSyntaxError("Second argument to %r must be "
+                "'as'" % bits[0])
+        
+        return cls(bits[2], bits[3], order_by)
+    
+    def __init__(self, how_many, context_var, order_by):
+        self.how_many = how_many
+        self.context_var = context_var
+        self.order_by = order_by
+    
+    def render(self, context):
+        how_many = self.how_many.resolve(context)
+        context[self.context_var] = [
+            t
+            for t in Testimonial.objects.filter(
+                active=True
+            ).order_by(
+                self.order_by
+            )[:how_many]
+        ]
+        return u""
+
+
+@register.tag
+def random_testimonials(parser, token):
+    return TestimonialsNode.handle_token(parser, token, order_by="?")
+
+
+@register.tag
+def testimonials(parser, token):
+    return TestimonialsNode.handle_token(parser, token, order_by="-added")
