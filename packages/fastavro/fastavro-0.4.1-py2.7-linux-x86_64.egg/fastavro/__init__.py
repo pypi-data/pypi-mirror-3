4cc13028390278fe363512d0c599408446243630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:46 +0000

    Add package fastavro/0.4.1/fastavro-0.4.1-py2.7-linux-x86_64.egg

---
+++ packages/fastavro/fastavro-0.4.1-py2.7-linux-x86_64.egg/fastavro/__init__.py
@@ -0,0 +1,27 @@
+'''Fast Avro file iteration.
+
+Most of the code here is ripped off the Python avro package. It's missing a lot
+of features in order to get speed.
+
+The only onterface function is iter_avro, example usage::
+
+    import fastavro as avro
+
+    with open('some-file.avro', 'rb') as fo:
+        reader = fastavro.reader(fo)
+        schema = reader.schema
+
+        for record in reader:
+            process_record(record)
+'''
+
+__all__ = [ 'iter_avro' ]
+__version__ = '0.4.1'
+
+try:
+    from . import cfastavro as _avro
+except ImportError:
+    from . import pyfastavro as _avro
+
+reader = iter_avro = _avro.iter_avro
+
