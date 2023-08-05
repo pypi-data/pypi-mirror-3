dca498e05855d6ffbdf046a5cdb792ac67fc6186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:54 +0000

    Add package xerox/0.3.1/xerox-0.3.1.tar.gz

---
+++ packages/xerox/xerox-0.3.1.tar.gz/xerox-0.3.1/xerox/core.py
@@ -0,0 +1,18 @@
+import sys
+
+__title__ = 'xerox'
+__version__ = '0.2.1'
+__author__ = 'Kenneth Reitz'
+__license__ = 'MIT'
+
+if sys.platform == 'darwin':
+    from .darwin import *
+    
+elif sys.platform == 'linux2':
+    from .linux import *
+    
+elif sys.platform == 'win32':
+    from .win import *
+
+elif sys.platform == 'cli':
+    from .cli import *
