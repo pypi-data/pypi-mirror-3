ee98090dd0d794d1efe1506e3c48ce8cf8b78a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:25 +0000

    Add package ageliaco.p10userdata/0.1.2/ageliaco.p10userdata-0.1.2-py2.6.egg

---
+++ packages/ageliaco.p10userdata/ageliaco.p10userdata-0.1.2-py2.6.egg/ageliaco/p10userdata/adapter.py
@@ -0,0 +1,31 @@
+from plone.app.users.browser.personalpreferences import UserDataPanelAdapter
+
+class EnhancedUserDataPanelAdapter(UserDataPanelAdapter):
+    """
+    """
+    def get_firstname(self):
+        return self.context.getProperty('firstname', '')
+    def set_firstname(self, value):
+        return self.context.setMemberProperties({'firstname': value})
+    firstname = property(get_firstname, set_firstname)
+
+    def get_lastname(self):
+        return self.context.getProperty('lastname', '')
+    def set_lastname(self, value):
+        return self.context.setMemberProperties({'lastname': value})
+    lastname = property(get_lastname, set_lastname)
+
+    def get_school(self):
+        return self.context.getProperty('school', '')
+    def set_school(self, value):
+        return self.context.setMemberProperties({'school': value})
+    school = property(get_school, set_school)
+
+    def get_reference(self):
+        return self.context.getProperty('reference', '')
+    def set_reference(self, value):
+        return self.context.setMemberProperties({'reference': value})
+    reference = property(get_reference, set_reference)
+
+
+
