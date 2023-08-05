3b863a8f3f15129f3d43574b9ae08038291786e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:00 +0000

    Add package schachtelier/1.0.0/schachtelier-1.0.0.zip

---
+++ packages/schachtelier/schachtelier-1.0.0.zip/schachtelier-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+        name = 'schachtelier',
+        version ='1.0.0',
+        py_modules = ['schachtelier'],
+        author = 'Thomas Raba, Aligia GmbH',
+        author_email = 'it.service@aligia.de',
+        url = 'http://www.aligia.de',
+        )
