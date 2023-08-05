b8a03359030961074ecc641fdc1cc200f361782f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:45 +0000

    Add package auslfe.formonline.tokenaccess/0.2.0/auslfe.formonline.tokenaccess-0.2.0.tar.gz

---
+++ packages/auslfe.formonline.tokenaccess/auslfe.formonline.tokenaccess-0.2.0.tar.gz/auslfe.formonline.tokenaccess-0.2.0/auslfe/formonline/tokenaccess/sharing_provider.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from zope.interface import implements
+from zope.component import getUtility
+from zope.annotation.interfaces import IAnnotations
+from auslfe.formonline.pfgadapter.interfaces import IFormSharingProvider
+
+from collective.powertoken.core.interfaces import IPowerTokenUtility
+
+class EmailWithTokenSharingProvider(object):
+    implements(IFormSharingProvider)
+    
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+    
+    def share(self, formonline, overseer):
+        """
+        Gives to an unknow overseer (an e-mail address):
+         * a temporary Editor role
+         * a permanent Reader role
+        
+        Using collective.powertoken
+        """
+        utility = getUtility(IPowerTokenUtility)
+        tokenView = utility.enablePowerToken(formonline,
+                                             'view.viewDocument',
+                                             roles=['Owner'],
+                                             oneTime=False,
+                                             view='tinyview')
+        tokenApprove = utility.enablePowerToken(formonline,
+                                                'workflow.doAction',
+                                                roles=['Editor'],
+                                                workflow_action='approval')
+        tokenReject = utility.enablePowerToken(formonline,
+                                               'workflow.doAction',
+                                               roles=['Editor'],
+                                               workflow_action='retract_approval')
+        IAnnotations(formonline)['share-tokens'] = {'email': overseer,
+                                                    'view': tokenView,
+                                                    'approve': tokenApprove,
+                                                    'reject': tokenReject,
+                                                    }
