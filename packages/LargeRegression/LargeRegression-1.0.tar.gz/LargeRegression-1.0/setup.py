12936fbc4bde56b8ba32c60dbaabff046dd33e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:20 +0000

    Add package LargeRegression/1.0/LargeRegression-1.0.tar.gz

---
+++ packages/LargeRegression/LargeRegression-1.0.tar.gz/LargeRegression-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='LargeRegression',
+      version='1.0',
+      description='Gradient descent to minimize the least squares objective function',
+      author='Jeffrey Wong',
+      author_email='jeff.ct.wong@stanford.edu',
+      url='http://www.stanford.edu/~jeffwong',
+      py_modules=['LargeRegression'],
+      )
