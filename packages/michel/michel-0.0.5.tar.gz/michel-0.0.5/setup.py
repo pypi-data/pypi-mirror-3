efe6be297f2f39a5f70e63151833a5e3f97f26a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:31 +0000

    Add package michel/0.0.5/michel-0.0.5.tar.gz

---
+++ packages/michel/michel-0.0.5.tar.gz/michel-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='michel',
+    version='0.0.5',
+    author='Christophe-Marie Duquesne',
+    author_email='chm.duquesne@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/chmduquesne/michel',
+    license=open('LICENSE.txt').read(),
+    description='pushes/pull flat text files to google tasks',
+    long_description=open('README.md').read(),
+    install_requires = ['google-api-python-client'],
+    entry_points=("""
+    [console_scripts]
+    michel = michel.michel:main
+    """)
+)
