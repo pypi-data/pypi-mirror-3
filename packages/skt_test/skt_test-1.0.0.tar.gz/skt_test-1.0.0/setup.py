fe85cc8ddb07c44bfbe147ae97084e5d65b9704c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package skt_test/1.0.0/skt_test-1.0.0.tar.gz

---
+++ packages/skt_test/skt_test-1.0.0.tar.gz/skt_test-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+
+		name 				='skt_test',
+		version 			="1.0.0",
+		py_modules 			=['skt_test'],
+		author			        ='saikatz',
+		description			='another test file',
+
+	)
