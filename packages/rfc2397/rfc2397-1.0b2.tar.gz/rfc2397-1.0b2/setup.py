b56a2c1912fffd30f9401e03788469f5204050dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:30 +0000

    Add package rfc2397/1.0b2/rfc2397-1.0b2.tar.gz

---
+++ packages/rfc2397/rfc2397-1.0b2.tar.gz/rfc2397-1.0b2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+import rfc2397
+
+setup(name=rfc2397.name,
+      py_modules=(rfc2397.name,),
+      version=rfc2397.version,
+      author='Jacob Oscarson',
+      author_email='jacob@plexical.com',
+      description='A Python RFC2397 implementation',
+      long_description=open('README.rst').read(),
+      entry_points = { 'console_scripts': ('rfc2397 = rfc2397:cli') },
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Environment :: Console',
+          'Topic :: Internet :: WWW/HTTP :: Site Management',
+          'Topic :: Multimedia :: Graphics :: Graphics Conversion'
+          ],
+      license='MIT')
