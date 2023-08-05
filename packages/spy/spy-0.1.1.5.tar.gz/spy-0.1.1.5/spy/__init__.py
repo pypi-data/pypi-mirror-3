20589ec9570cd0bf0101f914edd1fa4519160163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:21 +0000

    Add package spy/0.1.1.5/spy-0.1.1.5.tar.gz

---
+++ packages/spy/spy-0.1.1.5.tar.gz/spy-0.1.1.5/spy/__init__.py
@@ -0,0 +1,54 @@
+import errno
+import glob
+import os
+from functools import partial
+from itertools import imap
+
+
+# there must be no other files in this directory or shit gets fucked
+BASE = os.environ['SPY_BASE']
+try:
+    os.makedirs(BASE)
+except Exception, e:
+    if e.errno != errno.EEXIST:
+        raise
+
+def parse_status():
+    def parse_file(file_name):
+        try:
+            return list(p.split(':') for p in open(file_name, 'r').read().strip().split('\n'))
+        except:
+            return None
+    def not_none(elem):
+        return elem is not None
+    return list(filter(not_none, imap(parse_file, glob.glob(os.path.join(BASE, '*')))))
+
+
+def write(fd, name, time, percent_complete):
+    eta = (1.0 * time / percent_complete) - time
+    _format = (
+        ('name', name),
+        ('time', '%d seconds' % time),
+        ('complete', '%0.0f%%' % (100.0 * percent_complete)), 
+        ('eta', '%d seconds' % eta)
+    )
+    def write_line(fd, args):
+        fd.write('%s:%s\n' % args
+    any(imap(partial(write_line, fd), _format))
+
+def serve(host='127.0.0.1', port=5000):
+    server.app.run()
+
+def test():
+    files = ('one', 'two', 'three', 'four', 'five')
+    args = (
+        ('me', 69, 1),
+        ('love', 96, 0.1),
+        ('you', 0, 0.45),
+        ('long', 999, 0.34),
+        ('time', 42, .123123),
+    )
+    write_file = lambda args: write(open(os.path.join(BASE, args[0]), 'w'), *args[1])
+    any(imap(write_file, zip(files, args)))
+    return parse_status()
+
