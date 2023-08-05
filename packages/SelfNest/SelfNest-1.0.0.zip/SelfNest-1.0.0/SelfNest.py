ecaf7d7ffe07f02aa80019707bdb0986629664ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:34 +0000

    Add package SelfNest/1.0.0/SelfNest-1.0.0.zip

---
+++ packages/SelfNest/SelfNest-1.0.0.zip/SelfNest-1.0.0/SelfNest.py
@@ -0,0 +1,10 @@
+""" Following HF Python book """
+""" Recursive function to print list items """
+
+def print_lol(the_list):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item)
+		else:
+			print(each_item)
+
