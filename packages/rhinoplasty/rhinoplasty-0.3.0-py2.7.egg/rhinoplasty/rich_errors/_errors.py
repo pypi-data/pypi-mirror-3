73b110712e5f3b2342a1b4f17c1402bbdc893b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:22 +0000

    Add package rhinoplasty/0.3.0/rhinoplasty-0.3.0-py2.7.egg

---
+++ packages/rhinoplasty/rhinoplasty-0.3.0-py2.7.egg/rhinoplasty/rich_errors/_errors.py
@@ -0,0 +1,87 @@
+"""Additional exceptions for working with automated tests."""
+
+#TODO move exception docstrings to the helper functions? Or think about that some more...
+
+#TODO new skip method for when the functionality is not yet implemented (although the tests are written).
+    # -> just use NotImplementedError in the code, and handle that specifically
+    # Add an alias (or decorator) here to make this clearer? SubjectUnderTestNotImplementedException
+
+
+__all__ = [
+    'BrokenTestException',
+    'ExcludeTestException',
+    'InvalidTestConfigurationException',
+    'IrrelevantTestException',
+]
+
+
+import unittest
+
+
+class BrokenTestException(unittest.SkipTest):
+    """Skip a test because it is known to be broken.
+    
+    This avoids constantly re-running tests that are known to fail or cause an
+    error, yet allows us to still mark them as failures.
+    
+    In a perfect Test Driven Development world, all broken tests would be
+    fixed immediately by the developer that caused them to break.
+    
+    However, test frameworks are not necessarily used in a perfect TDD world
+    (for instance, consider a situation where tests are being written against
+    an existing legacy codebase), so pragmatically we need some process to
+    manage the broken tests.
+    
+    Some example scenarios:
+     * The development team decide that the defect exposed by the test can be
+        tolerated in the short-term, but will allocate resources to fix it in
+        the long-term.
+     * It is not known how to fix the defect exposed by the test.
+     * The defect exposed by the test is caused by a third party product.
+     * The defect exposed by the test cannot be fixed without making
+        incompatible changes to the product's external API (and these changes
+        need to be carefully managed).
+    
+    @see failing()
+    """
+    #TODo update docstrings for broken and broken_inherited_tests
+    pass
+
+
+class ExcludeTestException(unittest.SkipTest):
+    """Skip a test because the user specifically excluded it from the current
+    test run.
+    
+    This differs from the builtin SkipTest exception in that it is specifically
+    intended for exclusions specified at test-run-time, whereas SkipTest has a
+    very general meaning and implies nothing about the reason for skipping the
+    test.
+    
+    Note that there may be other ways to exclude unwanted test (eg. Nose
+    provides several ways to specify which functions should be considered as
+    tests).
+    """
+    #FIXME update docstring for SkipTest to label it as abstract and general
+    pass
+
+
+class InvalidTestConfigurationException(unittest.SkipTest):
+    """Skip a test because the system is not configured correctly.
+    
+    For example, this might be used when a test fixture cannot be setup because
+    important configuration information (such as the database to connect to)
+    has not been supplied by the user. 
+    
+    This situation should be considered a test failure.
+    """
+    #TODO review all uses of this exception to ensure they are appropriate (eg. some might need to be ExcludeTestException, or a different sort of error)
+    pass
+
+
+class IrrelevantTestException(unittest.SkipTest):
+    """Skip a test because it is not relevant for the current system.
+    
+    For example, this might be used to avoid running a Linux-specific test
+    under Microsoft Windows.
+    """
+    pass
