16c8a79686689049e4046fbe71ee5df36b3d07d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:46 +0000

    Add package printr/1.0.1/printr-1.0.1.zip

---
+++ packages/printr/printr-1.0.1.zip/printr-1.0.1/printr.py
@@ -0,0 +1,15 @@
+# Begin
+def print_r(the_list, level):
+	
+	# @arg the_list	The list to iterate over
+	# @arg level	Number of levels for iteration
+	
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_r(each_item, level + 1)
+		else:
+			for tab_stop in range(level):
+				print("\t", end = '')
+				print(each_item)
+
+# End of file printr.py
