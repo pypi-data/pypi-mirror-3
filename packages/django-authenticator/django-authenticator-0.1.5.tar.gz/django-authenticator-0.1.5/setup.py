5077c5abfa693879149a0386fb2a5371c79b3ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:54 +0000

    Add package django-authenticator/0.1.5/django-authenticator-0.1.5.tar.gz

---
+++ packages/django-authenticator/django-authenticator-0.1.5.tar.gz/django-authenticator-0.1.5/setup.py
@@ -0,0 +1,43 @@
+"""setup routine for ``django_authenticator``.
+Uses setuptools.
+"""
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+
+#NOTE: if you want to develop authenticator
+#you might want to install django-debug-toolbar as well
+
+INSTALL_REQUIRES = [
+    'django-extra-form-fields',
+    'django-registration',
+    'import-utils',
+    'multi-registry',
+    'South',
+    'oauth2',
+    'python-openid',
+    'python-wordpress-xmlrpc',
+]
+
+import django_authenticator
+
+setup(
+    name = "django-authenticator",
+    version = django_authenticator.__version__,
+    description = 'authentication client for django',
+    packages = find_packages(),
+    author = 'Evgeny.Fadeev',
+    author_email = 'evgeny.fadeev@gmail.com',
+    license = 'GPLv3',
+    keywords = 'authentication, oauth, openid',
+    url = '',
+    include_package_data = True,
+    install_requires = INSTALL_REQUIRES,
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+    ],
+    long_description = """Note: this module is not yet ready for 
+use in production. Developed for askbot forum - please ask questions
+at `http://askbot.org <http://askbot.org>`_.
+"""
+)
