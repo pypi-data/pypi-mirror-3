5b70e57971dbf208002c28928fc89e9ed21fe71c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:47 +0000

    Add package stripe/1.5.30/stripe-1.5.30.tar.gz

---
+++ packages/stripe/stripe-1.5.30.tar.gz/stripe-1.5.30/setup.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from distutils.core import setup
+
+# Don't import stripe module here, since deps may not be installed
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'stripe'))
+import version
+
+path, script = os.path.split(sys.argv[0])
+os.chdir(os.path.abspath(path))
+
+setup(name='stripe',
+      version=version.VERSION,
+      description='Stripe python bindings',
+      author='Stripe',
+      author_email='support@stripe.com',
+      url='https://stripe.com/',
+      packages=['stripe'],
+      package_data={'stripe' : ['data/ca-certificates.crt', '../VERSION']},
+      install_requires=['simplejson', 'pycurl']
+)
