7d096f86a282363a61304f7e07765bc71f731555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:34 +0000

    Add package kickstart/0.273/kickstart-0.273.tar.gz

---
+++ packages/kickstart/kickstart-0.273.tar.gz/kickstart-0.273/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'kickstart',
+    scripts = ['kickstart.py'],
+    packages = find_packages(),
+    data_files = [('templates', ['kickstart/base/*'])],
+    url = 'http://pypi.python.org/pypi/kickstart',
+    version = '0.273',
+    install_requires = ['PyYAML'],
+    description = 'Framework agnostic setup script',
+    author = 'Richard Layte',
+    author_email = 'rich.layte@gmail.com'
+)
