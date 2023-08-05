fa5d812ae9847995c0a0bcb2c44fb63ecf498af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:23 +0000

    Add package smtproutes/1.2.5/smtproutes-1.2.5.tar.gz

---
+++ packages/smtproutes/smtproutes-1.2.5.tar.gz/smtproutes-1.2.5/test/test_message.py
@@ -0,0 +1,19 @@
+import unittest, email
+from smtproutes.model import Message
+
+class TestMessage(unittest.TestCase):
+    
+    def setUp(self):
+        self.attachment_eml = file('test/fixtures/attachments.eml').read()
+    
+    def test_parsing_message_with_custom_message_class(self):
+        message = email.message_from_string(self.attachment_eml, Message)
+        self.assertEqual(message.get('to'), 'bencoe@gmail.com')
+    
+    def test_attachments_returned_by_messages_attachments_property(self):
+        message = email.message_from_string(self.attachment_eml, Message)
+        self.assertEqual(len(message.attachments), 2)
+    
+    def test_body_returns_text_body_of_message(self):
+        message = email.message_from_string(self.attachment_eml, Message)
+        self.assertEqual(message.body, 'message with attachments.\n')
