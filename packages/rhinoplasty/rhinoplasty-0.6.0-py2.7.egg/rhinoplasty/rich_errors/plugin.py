c12e95152ad0c5b2a56405fee9f4f9eb122f6579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package rhinoplasty/0.6.0/rhinoplasty-0.6.0-py2.7.egg

---
+++ packages/rhinoplasty/rhinoplasty-0.6.0-py2.7.egg/rhinoplasty/rich_errors/plugin.py
@@ -0,0 +1,25 @@
+"""Nose plugin for displaying results from rich errors."""
+
+from nose.plugins.errorclass import ErrorClass, ErrorClassPlugin
+from _errors import *
+
+class RichErrorReportingPlugin(ErrorClassPlugin):
+    """Nose plugin that installs error output handling for a richer error set
+    (as described elsewhere in this package).
+    """
+    # Standard Plugin attributes
+    name = "rich-errors"
+    
+    # Define error classes that we handle
+    #TODO Be able to customise the single letter labels
+    broken = ErrorClass(BrokenTestException, label='BROKEN', isfailure=False) #TODO should this be a failure? If so, we want to stop displaying tracebacks for it.
+    excluded = ErrorClass(ExcludeTestException, label='XCLUDE', isfailure=False) #TODO better label
+    irrelevant = ErrorClass(IrrelevantTestException, label='IRRELEVANT', isfailure=False)
+    misconfigured = ErrorClass(InvalidTestConfigurationException, label='CONFIG_WRONG', isfailure=True) #TODO better label
+    unimplemented = ErrorClass(NotImplementedError, label="UNIMPLEMENTED", isfailure=False) #TODO should this be a failure? If so, we want to stop displaying tracebacks for it.
+    
+    # Use default implementation of options() and configure to enable this plugin
+    
+    def help(self):
+        return "Display richer error types."
+
