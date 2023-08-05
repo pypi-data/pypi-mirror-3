62a0d027c5696894fa5a25b515fee6b2354995a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:39 +0000

    Add package RefBuild/0.1.2/RefBuild-0.1.2.zip

---
+++ packages/RefBuild/RefBuild-0.1.2.zip/RefBuild-0.1.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from distutils.core import setup
+      
+setup(
+    name = "RefBuild",
+    version = "0.1.2",
+    package_dir={'refbuild': 'src'},
+    packages = ['refbuild'],
+    scripts= ['src/buildref'],
+    author = "Thomas S. McTavish",
+    author_email = "Tom.McTavish@gmail.com",
+    description = "A tool to prepare source reference from docstrings.",
+      long_description = """A tool to construct reStructuredText files from docstrings in Python source files and link to Sphinx autodoc functionality to build API/source reference documentation.""",
+    license = "MIT License",
+    keywords = "API reference source documentation generation",
+    url = "http://bitbucket.org/tommctavish/refbuild",
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Documentation'],
+    requires = ['Sphinx (>=1.0)']
+)
+
