8a893718a0d419897f3255e2cb983b78bd10ec1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package parse/1.1.6/parse-1.1.6.tar.gz

---
+++ packages/parse/parse-1.1.6.tar.gz/parse-1.1.6/setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+from parse import __version__, __doc__
+
+with open('README.rst', 'w') as f:
+    f.write(__doc__)
+
+# perform the setup action
+setup(
+    name = "parse",
+    version = __version__,
+    description = "parse() is the opposite of format()",
+    long_description = __doc__.decode('utf8'),
+    author = "Richard Jones",
+    author_email = "rjones@ekit-inc.com",
+    py_modules = ['parse'],
+    url = 'https://github.com/r1chardj0n3s/parse',
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
+
+# vim: set filetype=python ts=4 sw=4 et si
