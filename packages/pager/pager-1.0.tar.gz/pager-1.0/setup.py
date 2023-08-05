4f6f043d6ea9873643720e95a07490b3f237f2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:59 +0000

    Add package pager/1.0/pager-1.0.tar.gz

---
+++ packages/pager/pager-1.0.tar.gz/pager-1.0/setup.py
@@ -0,0 +1,19 @@
+
+from distutils.core import setup
+
+setup(
+    name='pager',
+    version='1.0',
+    description='Terminal/console pager module for Python',
+    long_description=open('README.rst').read(),
+    py_modules=['pager'],
+    license='Public Domain',
+    author='anatoly techtonik',
+    author_email='techtonik@gmail.com',
+    url='http://bitbucket.org/techtonik/python-pager',
+    classifiers=[
+        'Environment :: Console',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+    ],
+)
