717849d9120510ceae4873d97ca46a960396a04d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:51 +0000

    Add package pyScss/1.0.9/pyScss-1.0.9.tar.gz

---
+++ packages/pyScss/pyScss-1.0.9.tar.gz/pyScss-1.0.9/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+from scss.scss_meta import PROJECT, URL, VERSION, AUTHOR, AUTHOR_EMAIL, LICENSE, DOWNLOAD_URL
+
+
+def read(fname):
+    import os
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read().strip()
+    except IOError:
+        return ''
+
+
+extra = {}
+import sys
+if sys.version_info >= (3, 0):
+    extra.update(
+        use_2to3=True,
+    )
+
+
+setup(name=PROJECT,
+    version=VERSION,
+    description=read('DESCRIPTION'),
+    long_description=read('README.rst'),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    packages=['scss'],
+    license=LICENSE,
+    keywords='css oocss xcss sass scss less precompiler',
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Intended Audience :: Developers",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python",
+                 "Programming Language :: Python :: 3",
+                 "Topic :: Software Development :: Code Generators",
+                 "Topic :: Text Processing :: Markup",
+                 "Topic :: Software Development :: Libraries :: Python Modules"
+                 ],
+    entry_points="""
+    [console_scripts]
+    pyscss = scss:main
+    """,
+    **extra
+    )
