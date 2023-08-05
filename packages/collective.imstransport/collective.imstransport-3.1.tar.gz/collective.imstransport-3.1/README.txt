59f5c6b23fce36af8f2c8cadb785b8635bc28eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package collective.imstransport/3.1/collective.imstransport-3.1.tar.gz

---
+++ packages/collective.imstransport/collective.imstransport-3.1.tar.gz/collective.imstransport-3.1/README.txt
@@ -0,0 +1,7 @@
+collective.imstransport Package Readme
+=========================
+
+Overview
+--------
+
+IMS Content Packaging plugin for Plone.
---
+++ packages/collective.imstransport/collective.imstransport-3.1.tar.gz/collective.imstransport-3.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '3.1'
+
+setup(name='collective.imstransport',
+      version=version,
+      description="IMS Content Packaging plugin for Plone.",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme',
+      author='Plone Collective',
+      author_email='product-developers@lists.plone.org',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
