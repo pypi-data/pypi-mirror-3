88070643278cbcb69077001c44877f3aa565d65f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:02 +0000

    Add package plivohelper/0.3.4/plivohelper-0.3.4.tar.gz

---
+++ packages/plivohelper/plivohelper-0.3.4.tar.gz/plivohelper-0.3.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import sys
+
+requires = []
+if sys.version_info < (2, 6):
+    requires.append('simplejson')
+
+setup(
+    name = "plivohelper",
+    py_modules = ['plivohelper'],
+    version = "0.3.4",
+    description = "Plivo API client and RESTXML generator",
+    author = "Plivo Team",
+    author_email = "hello@plivo.org",
+    url = "https://github.com/plivo/plivohelper-python",
+    keywords = ["plivo", "rest"],
+    install_requires = requires,
+    classifiers = [
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Communications :: Telephony"
+        ],
+    long_description = """\
+        Python Plivo Client Helper Library
+         """ )
