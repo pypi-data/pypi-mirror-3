b3b0c708878120eda321b7e39a51b358b3eddf0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:54 +0000

    Add package weasyprint/0.2/WeasyPrint-0.2.tar.gz

---
+++ packages/weasyprint/WeasyPrint-0.2.tar.gz/WeasyPrint-0.2/setup.py
@@ -0,0 +1,57 @@
+"""
+WeasyPrint
+----------
+
+WeasyPrint converts web documents (HTML, CSS, SVG, ...) to PDF.
+
+See the documentation at http://weasyprint.org/
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='WeasyPrint',
+    version='0.2',  # Also change this in weasy/__init__.py
+    url='http://weasyprint.org/',
+    license='GNU Affero General Public License v3',
+    description='WeasyPrint converts web documents to PDF.',
+    long_description=__doc__,
+    author='Simon Sapin',
+    author_email='simon.sapin@kozea.fr',
+    plateforms='Any',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Printing',
+    ],
+    packages=find_packages(),
+    package_data={
+        'weasy.tests': ['resources/*',
+                        # Make sure the directories are created
+                        '*_results/.gitignore'],
+        'weasy.css': ['*.css']},
+    zip_safe=False,
+    install_requires=[
+        # Keep this in sync with the "install" documentation
+        'lxml',
+        'cssutils>=0.9.8a3',
+        'PIL',
+        # Not installable by pip:
+        #  Pango>=1.29.3
+        #  PyGObject
+        #  PyCairo
+    ],
+    test_loader='attest:FancyReporter.test_loader',
+    test_suite='weasy.tests',
+    entry_points={
+        'console_scripts': [
+            'weasyprint = weasy:main',
+        ],
+    },
+    use_2to3=True
+)
---
+++ packages/weasyprint/WeasyPrint-0.2.tar.gz/WeasyPrint-0.2/weasy/utils.py
@@ -0,0 +1,91 @@
+# coding: utf8
+
+#  WeasyPrint converts web documents (HTML, CSS, ...) to PDF.
+#  Copyright (C) 2011  Simon Sapin
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+Various utils.
+
+"""
+
+import urllib
+from urlparse import urljoin, urlparse
+
+from cssutils.helper import path2url
+
+from . import VERSION
+
+
+def get_url_attribute(element, key):
+    """Get the URL corresponding to the ``key`` attribute of ``element``.
+
+    The retrieved URL is absolute, even if the URL in the element is relative.
+
+    """
+    attr_value = element.get(key)
+    if attr_value is None:
+        return None
+    return urljoin(element.base_url, attr_value.strip())
+
+
+def ensure_url(string):
+    """Get a ``scheme://path`` URL from ``string``.
+
+    If ``string`` looks like an URL, return it unchanged. Otherwise assume a
+    filename and convert it to a ``file://`` URL.
+
+    """
+    return string if urlparse(string).scheme else path2url(string)
+
+
+class URLopener(urllib.FancyURLopener):
+    # User-Agent
+    version = 'WeasyPrint/%s http://weasyprint.org/' % VERSION
+
+
+def urlopen(url):
+    """Fetch an URL and return ``(file_like, mime_type, charset)``.
+    """
+    file_like = URLopener().open(url)
+    info = file_like.info()
+    if hasattr(info, 'get_content_type'):
+        # Python 3
+        mime_type = info.get_content_type()
+    else:
+        # Python 2
+        mime_type = info.gettype()
+    if hasattr(info, 'get_param'):
+        # Python 3
+        charset = info.get_param('charset')
+    else:
+        # Python 2
+        charset = info.getparam('charset')
+    return file_like, mime_type, charset
+
+
+def urllib_fetcher(url):
+    """URL fetcher for cssutils.
+
+    This fetcher is based on urllib instead of urllib2, since urllib has
+    support for the "data" URL scheme.
+
+    """
+    file_like, mime_type, charset = urlopen(url)
+    if mime_type != 'text/css':
+        # TODO: add a warning
+        return None
+    return charset, file_like.read()
