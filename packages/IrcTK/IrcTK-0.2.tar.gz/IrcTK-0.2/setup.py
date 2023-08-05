99f777a3e796cf59f77d533b43bc4c04ba5bd5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:27 +0000

    Add package IrcTK/0.2/IrcTK-0.2.tar.gz

---
+++ packages/IrcTK/IrcTK-0.2.tar.gz/IrcTK-0.2/setup.py
@@ -0,0 +1,23 @@
+'''
+IrcTK
+-----
+
+A simple framework for writing IRC applications.
+'''
+
+from setuptools import setup
+
+setup(
+    name='IrcTK',
+    version='0.2',
+    url='https://github.com/maxcountryman/irctk',
+    license='BSD',
+    author='Max Countryman',
+    author_email='maxc@me.com',
+    description='A simple framework for IRC apps',
+    long_description=__doc__,
+    zip_safe=False,
+    platforms='any',
+    test_suite='test_irctk'
+)
+
