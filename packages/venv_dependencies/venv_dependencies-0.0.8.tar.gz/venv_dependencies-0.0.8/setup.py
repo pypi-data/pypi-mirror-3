84f6b16e51e78152a0f31a9c6f1007d8e80d8439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:36 +0000

    Add package venv_dependencies/0.0.8/venv_dependencies-0.0.8.tar.gz

---
+++ packages/venv_dependencies/venv_dependencies-0.0.8.tar.gz/venv_dependencies-0.0.8/setup.py
@@ -0,0 +1,23 @@
+import os
+from distutils.core import setup
+
+setup(
+    name = "venv_dependencies",
+    version = "0.0.8",
+    author = "Felipe Arruda Pontes",
+    author_email = "contato@arruda.blog.br",
+    description = ("Easy to install any dependencies in a virtualenviroment(without making symlinks by hand and etc...)"),
+    license = 'LICENSE',
+    keywords = "virtualenv dependencies",
+    url = "https://github.com/arruda/venv-dependencies",
+    packages=['venv_dependencies'],
+    scripts = ['bin/link_venv.py'],
+    data_files = [],
+    install_requires=['virtualenv'],
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
