ae348d79b04e16407fae32d121672f89a97dd276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:58 +0000

    Add package globchecksum/1.0.1/globchecksum-1.0.1.tar.gz

---
+++ packages/globchecksum/globchecksum-1.0.1.tar.gz/globchecksum-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup( 
+        name         = 'globchecksum', 
+        version      = '1.0.1', 
+        py_modules   = ['globchecksum'],
+        author       = 'AlienOne',
+        author_email = 'Justin@alienonesecurity.com',
+        url          = 'https://github.com/alienone/Python/tree/master/globchecksum',
+        description  = 'MD5 checksum glob of files @ $PATH',
+     )
