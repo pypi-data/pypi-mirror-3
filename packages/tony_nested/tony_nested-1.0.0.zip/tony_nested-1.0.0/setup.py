a12960aab3d12d7ebf2ac94481ff9c1eb64351a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:05 +0000

    Add package tony_nested/1.0.0/tony_nested-1.0.0.zip

---
+++ packages/tony_nested/tony_nested-1.0.0.zip/tony_nested-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name 		= 'tony_nested',
+	version		= '1.0.0',
+	py_modules	= ['tony_nested'],
+	author		= 'tontate',
+	author_email	= 'tontate@hotmail.com',
+	url		= 'http://www.tontate.com',
+	description	= 'A simple printer of nestered lists'
+)
