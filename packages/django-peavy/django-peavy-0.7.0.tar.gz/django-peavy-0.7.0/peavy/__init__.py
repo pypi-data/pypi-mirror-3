90d208a1d889aedc9f5cc689906d471a8860f7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:39 +0000

    Add package django-peavy/0.7.0/django-peavy-0.7.0.tar.gz

---
+++ packages/django-peavy/django-peavy-0.7.0.tar.gz/django-peavy-0.7.0/peavy/__init__.py
@@ -0,0 +1,9 @@
+"""
+django-peavy makes it easy to collect and monitor Django application logging.
+"""
+VERSION = (0, 7, 0)
+
+def get_version():
+    return '.'.join((str(d) for d in VERSION))
+
+
