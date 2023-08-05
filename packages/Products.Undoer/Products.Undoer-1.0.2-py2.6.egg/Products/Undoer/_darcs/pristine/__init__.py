ea71fe89f53881cd480dc6be3040ad17b0f767dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:18 +0000

    Add package Products.Undoer/1.0.2/Products.Undoer-1.0.2-py2.6.egg

---
+++ packages/Products.Undoer/Products.Undoer-1.0.2-py2.6.egg/Products/Undoer/_darcs/pristine/__init__.py
@@ -0,0 +1,46 @@
+from Globals import MessageDialog, DTMLFile, InitializeClass
+from DateTime import DateTime
+import types
+from App.Undo import UndoSupport
+
+def undo_changes_by_date(self, date=None, REQUEST=None):
+    """Undoes changes made to the database after a given date;  if
+    a date is not specified, all changes are undone."""
+    if date is None:
+        date = float(0)
+    elif type(date) == types.StringType:
+        date = float(DateTime(date))
+    else:
+        date = float(date)
+    transactions = self._p_jar.db().undoLog(0, 2**32)
+    undo=self._p_jar.db().undo
+    count = 0
+    for transaction in transactions:
+        if transaction['time'] >= date:
+            undo(transaction['id'])
+            count += 1
+    if REQUEST:
+        return MessageDialog(
+		title='Result of undoing transactions',
+		message="<em>%s</em> transactions were undone" % count,
+		action='./manage_UndoForm'
+	)
+    return count
+
+UndoSupport.undo_changes_by_date = undo_changes_by_date
+UndoSupport.manage_UndoForm = DTMLFile(
+	'dtml/undo',
+	globals(),
+	PrincipiaUndoBatchSize=20,
+	first_transaction=0,
+	last_transaction=20
+)
+UndoSupport.__ac_permissions__ = (
+	('Undo changes', (
+		'manage_undo_transactions',
+		'undoable_transactions',
+		'manage_UndoForm',
+		'undo_changes_by_date',
+	)),
+)
+InitializeClass(UndoSupport)
