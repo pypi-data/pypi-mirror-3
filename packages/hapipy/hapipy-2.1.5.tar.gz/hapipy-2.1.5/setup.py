d126c0ac79a070522ce3f0dfd9c8e4c7ee538fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:22 +0000

    Add package hapipy/2.1.5/hapipy-2.1.5.tar.gz

---
+++ packages/hapipy/hapipy-2.1.5.tar.gz/hapipy-2.1.5/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='hapipy',
+    version='2.1.5',
+    description="A python wrapper around HubSpot's APIs",
+    long_description = open('README.md').read(),
+    author='Adrian Mott',
+    author_email='amott@hubspot.com',
+    url='https://github.com/HubSpot/hapipy',
+    download_url='https://github.com/HubSpot/hapipy/tarball/v2.1.5',
+    license='LICENSE.txt',
+    packages=['hapi'],
+    install_requires=[
+        'nose==1.1.2',
+        'unittest2==0.5.1',
+        'simplejson==2.2.1'
+    ],
+)
