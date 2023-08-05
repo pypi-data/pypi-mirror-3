038bf28729d257df416360c44ece0ef9169261b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:07 +0000

    Add package django-tastypie-with-file-upload-and-model-form-validation/1.0.0-beta/django-tastypie-with-file-upload-and-model-form-validation-1.0.0-beta.tar.gz

---
+++ packages/django-tastypie-with-file-upload-and-model-form-validation/django-tastypie-with-file-upload-and-model-form-validation-1.0.0-beta.tar.gz/django-tastypie-with-file-upload-and-model-form-validation-1.0.0-beta/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    name='django-tastypie-with-file-upload-and-model-form-validation',
+    version='1.0.0-beta',
+    description='A flexible & capable API layer for Django.',
+    author='Daniel Lindsley',
+    author_email='daniel@toastdriven.com',
+    url='http://github.com/claudiobi/django-tastypie/',
+    long_description='Tastypie with File Upload, ModelFormValidation, Error 500 description, Throttle with DummyCache, FormValidationExcluding',
+    packages=[
+        'tastypie',
+        'tastypie.utils',
+        'tastypie.management',
+        'tastypie.management.commands',
+    ],
+    package_data={
+        'tastypie': ['templates/tastypie/*'],
+    },
+    requires=[
+        'mimeparse',
+        'python_dateutil(>=1.5, < 2.0)',
+    ],
+    install_requires=[
+        'mimeparse',
+        'python_dateutil >= 1.5, < 2.0',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
