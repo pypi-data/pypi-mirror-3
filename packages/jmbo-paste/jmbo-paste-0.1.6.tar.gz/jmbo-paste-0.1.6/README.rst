d79395cf2bd34c901e3b46a7685c17cdb80e567e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:14 +0000

    Add package jmbo-paste/0.1.6/jmbo-paste-0.1.6.tar.gz

---
+++ packages/jmbo-paste/jmbo-paste-0.1.6.tar.gz/jmbo-paste-0.1.6/README.rst
@@ -0,0 +1,9 @@
+Jmbo Paste
+==========
+Python Paste templates creating Jmbo Buildout environments.
+
+This packages is part of the larger `Jmbo <http://www.jmbo.org>`_ project .
+
+.. contents:: Contents
+    :depth: 2
+
---
+++ packages/jmbo-paste/jmbo-paste-0.1.6.tar.gz/jmbo-paste-0.1.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmbo-paste',
+    version='0.1.6',
+    description='Python Paste templates creating Jmbo Buildout environments.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='https://github.com/praekelt/jmbo-paste',
+    packages = find_packages(),
+    install_requires = [
+        'Cheetah>=2.4.2.1',
+        'PasteScript',
+        'setuptools',
+    ],
+    include_package_data=True,
+    entry_points = """
+        [paste.paster_create_template]
+        jmbo_project=jmbo_paste.templates:JmboProjectTemplate
+    """,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Paste",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False
+)
