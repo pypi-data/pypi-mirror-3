27f202ed790114d3f67af55cbfaf464d89fca66d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:10 +0000

    Add package lithium/0.4.1/lithium-0.4.1.tar.gz

---
+++ packages/lithium/lithium-0.4.1.tar.gz/lithium-0.4.1/lithium/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.4.1'
+__copyright__ = 'Copyright (c) 2011, Kyle Fuller'
+__licence__ = 'BSD'
+__author__ = 'Kyle Fuller <inbox@kylefuller.co.uk>'
+__URL__ = 'http://github.com/kylef/lithium/'
+
+def templates_path():
+    from os.path import dirname, join
+    return join(dirname(__file__), 'templates')
