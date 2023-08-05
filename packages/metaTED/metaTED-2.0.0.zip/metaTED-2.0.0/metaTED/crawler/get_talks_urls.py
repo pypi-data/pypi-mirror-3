18f852414b50259481f48f29048672e045db9f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:02 +0000

    Add package metaTED/2.0.0/metaTED-2.0.0.zip

---
+++ packages/metaTED/metaTED-2.0.0.zip/metaTED-2.0.0/metaTED/crawler/get_talks_urls.py
@@ -0,0 +1,31 @@
+import logging
+from lxml import html
+from lxml.cssselect import CSSSelector
+from urlparse import urljoin
+from .. import SITE_URL
+
+
+TALKS_LIST_URL = "http://www.ted.com/talks/quick-list"
+_TALKS_URLS_SELECTOR = CSSSelector('table.downloads tr td:nth-child(3) a')
+
+
+TALKS_URLS_BLACKLIST = [
+    # No downloads
+    'http://www.ted.com/talks/rokia_traore_sings_m_bifo.html',
+    'http://www.ted.com/talks/rokia_traore_sings_kounandi.html',
+]
+
+
+def get_talks_urls():
+    logging.debug('Looking for talk urls...')
+    document = html.parse(TALKS_LIST_URL)
+    talks_urls = [
+        urljoin(SITE_URL, a.get('href'))
+        for a in _TALKS_URLS_SELECTOR(document)
+    ]
+    
+    # Remove the well-known problematic talk URLs (i.e. no downloads available)
+    talks_urls = [url for url in talks_urls if url not in TALKS_URLS_BLACKLIST]
+    
+    logging.info("Found %d talk url(s) in total", len(talks_urls))
+    return talks_urls
