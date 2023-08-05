b8e168ee6c064c21bbe2b736db964d9ee7cf3c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package checkit/0.3.2b/checkit-0.3.2b.zip

---
+++ packages/checkit/checkit-0.3.2b.zip/checkit-0.3.2b/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+version = '0.3.2b'
+long_description = '\n\n'.join([
+    open('README.rst').read(),
+    open('CHANGES.txt').read(),
+])
+
+setup(
+    name = "checkit",
+    version = version,
+    description = "Validate your Python software against specifications created with BDD style grammar.",
+    long_description = long_description,
+    author = "Rudy Lattae",
+    author_email = "rudylattae@gmail.com",
+    url = 'https://bitbucket.org/rudylattae/checkit',
+    license = "Simplified BSD",
+    keywords = ['Specification', 'BDD', 'TDD', 'check', 'validate', 'example', 'nose', 'nosetest'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.5',
+    ],
+    py_modules = ['checkit'],
+    zip_safe = False,
+    install_requires = ['nose'],
+    entry_points = {
+        'console_scripts': [
+            'checkit = checkit:main'
+        ]
+    }
+)
