71145830c891a13d58707b0e0cf82d008534dfa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:49 +0000

    Add package tornado_tools/0.2.4/tornado_tools-0.2.4.tar.gz

---
+++ packages/tornado_tools/tornado_tools-0.2.4.tar.gz/tornado_tools-0.2.4/tornado_tools/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Gregory Sitnin <sitnin@gmail.com>"
+__copyright__ = "Gregory Sitnin, 2011"
+
+version = "0.2.4"
+
+
+import decorators
+import tpl_funcs
+
+
+UUID = r"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
