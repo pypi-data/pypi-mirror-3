3a4d3b150186aad0cbf940fd29328a73aafbcfe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:59 +0000

    Add package HalWeb/0.5.5/HalWeb-0.5.5-py2.7.egg

---
+++ packages/HalWeb/HalWeb-0.5.5-py2.7.egg/EGG-INFO/scripts/halicea_manage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import sys
+import logging
+try:
+    from halicea import hal
+except:
+    print "Hal web is not located in your python path"
+    print "accessing halweb thru path {{hal_path}}"
+    sys.path.append("{{hal_path}}")
+    import hal
+if __name__=='__main__':
+    hal.main_safe(sys.argv)
