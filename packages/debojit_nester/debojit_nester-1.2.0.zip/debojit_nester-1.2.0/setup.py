e3aee2e6ce9885196a74e88ac501bcdb9bbab00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:22 +0000

    Add package debojit_nester/1.2.0/debojit_nester-1.2.0.zip

---
+++ packages/debojit_nester/debojit_nester-1.2.0.zip/debojit_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'debojit_nester',
+		version = '1.2.0',
+		py_modules = ['debojitnester'],
+		author = 'debojit',
+		author_email = 'sinha.debojit@gmail.com',
+		url = 'http://www.google.com',
+		description = 'My first Python code'
+	 )
