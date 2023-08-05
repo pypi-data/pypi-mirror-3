cbeb23f2f1f3c55e45f2fcfbc2c8ea1b4f205502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:05 +0000

    Add package seqdiag/0.4.0/seqdiag-0.4.0.tar.gz

---
+++ packages/seqdiag/seqdiag-0.4.0.tar.gz/seqdiag-0.4.0/src/seqdiag/DiagramMetrix.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+#  Copyright 2011 Takeshi KOMIYA
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import blockdiag.DiagramMetrix
+from blockdiag.utils.XY import XY
+
+
+class DiagramMetrix(blockdiag.DiagramMetrix.DiagramMetrix):
+    def __init__(self, diagram, **kwargs):
+        super(DiagramMetrix, self).__init__(diagram, **kwargs)
+
+        self.setdefault('edges', diagram.edges)
+
+        scale_ratio = self.scale_ratio
+        if diagram.edge_height:
+            self.setdefault('edge_height', diagram.edge_height)
+        else:
+            self.setdefault('edge_height', self.nodeHeight / scale_ratio)
+
+        if diagram.edge_length:
+            span_width = diagram.edge_length - self['nodeWidth']
+            if span_width < 0:
+                msg = "WARNING: edge_length is too short: %d\n" % \
+                      diagram.edge_length
+                sys.stderr.write(msg)
+
+                span_width = 0
+
+            self['spanWidth'] = span_width
+
+    def originalMetrix(self):
+        kwargs = {}
+        for key in self:
+            kwargs[key] = self[key]
+        kwargs['scale_ratio'] = 1
+
+        return DiagramMetrix(self, **kwargs)
+
+    def pageSize(self, nodes=None):
+        if nodes:
+            width = max(x.xy.x for x in nodes)
+        else:
+            width = 0
+
+        size = super(DiagramMetrix, self).pageSize(width + 1, 1)
+
+        height = int(sum(e.height for e in self.edges) * self.edge_height)
+        height += self.spanHeight + self.edge_height / 2
+
+        return XY(size.x, size.y + height)
+
+    def groupBox(self, group):
+        box = list(self.cell(group).marginBox())
+        box[3] = self.pageSize().y - self.pageMargin.y - self.pagePadding[2]
+
+        return box
+
+    def lifeline(self, node):
+        y = self.pageSize().y - self.pageMargin.y - self.pagePadding[2]
+
+        pt1 = self.node(node).bottom()
+        pt2 = XY(pt1.x, y)
+
+        return [pt1, pt2]
+
+    def activity_box(self, node, activity):
+        # y coodinates for top of activity box
+        starts = activity['lifetime'][0]
+        edge = self.edges[starts]
+        y1 = self.edge(edge).baseheight
+        if edge.diagonal and edge.node2 == node:
+            y1 += self.edge_height * 3 / 4
+
+        # y coodinates for bottom of activity box
+        ends = activity['lifetime'][-1] + 1
+        if ends < len(self.edges):
+            y2 = self.edge(self.edges[ends]).baseheight
+        else:
+            y2 = self.pageSize().y - self.pageMargin.y - self.edge_height / 2
+
+        index = activity['level']
+        base_x = self.cell(node).bottom().x
+        box = (base_x + (index - 1) * self.cellSize / 2, y1,
+               base_x + (index + 1) * self.cellSize / 2, y2)
+
+        return box
+
+    def activity_shadow(self, node, activity):
+        box = self.activity_box(node, activity)
+
+        return (box[0] + self.shadowOffsetX, box[1] + self.shadowOffsetY,
+                box[2] + self.shadowOffsetX, box[3] + self.shadowOffsetY)
+
+    def edge(self, edge):
+        return EdgeMetrix(edge, self)
+
+
+class EdgeMetrix(object):
+    def __init__(self, edge, metrix):
+        self.metrix = metrix
+        self.edge = edge
+
+    @property
+    def baseheight(self):
+        return self.metrix.node(self.edge.node1).bottom().y + \
+               self.metrix.spanHeight + self.metrix.edge_height / 2 + \
+               int(self.edge.y * self.metrix.edge_height)
+
+    @property
+    def shaft(self):
+        m = self.metrix
+        baseheight = self.baseheight
+
+        if self.edge.direction == 'self':
+            fold_width = m.nodeWidth / 2 + m.cellSize
+            fold_height = m.edge_height / 4
+
+            # adjust textbox to right on activity-lines
+            base_x = self.metrix.node(self.edge.node1).bottom().x
+            x1 = base_x + self.activity_line_width(self.edge.node1)
+
+            line = [XY(x1 + m.cellSize, baseheight),
+                    XY(x1 + fold_width, baseheight),
+                    XY(x1 + fold_width, baseheight + fold_height),
+                    XY(x1 + m.cellSize, baseheight + fold_height)]
+        else:
+            x1 = self.metrix.node(self.edge.left_node).bottom().x + \
+                 self.activity_line_width(self.edge.left_node)
+            x2 = self.metrix.node(self.edge.right_node).bottom().x
+
+            margin = m.cellSize
+            if self.edge.diagonal:
+                line = [XY(x1 + margin, baseheight),
+                        XY(x2 - margin, baseheight + m.edge_height * 3 / 4)]
+            else:
+                line = [XY(x1 + margin, baseheight),
+                        XY(x2 - margin, baseheight)]
+
+        return line
+
+    @property
+    def head(self):
+        cell = self.metrix.cellSize
+
+        head = []
+        if self.edge.direction == 'right':
+            xy = self.shaft[-1]
+            head.append(XY(xy.x - cell, xy.y - cell / 2))
+            head.append(xy)
+            head.append(XY(xy.x - cell, xy.y + cell / 2))
+        else:  # left or self
+            xy = self.shaft[0]
+            head.append(XY(xy.x + cell, xy.y - cell / 2))
+            head.append(xy)
+            head.append(XY(xy.x + cell, xy.y + cell / 2))
+
+        return head
+
+    @property
+    def textbox(self):
+        m = self.metrix
+
+        if self.edge.direction == 'self':
+            x1 = m.node(self.edge.node1).bottom().x
+            x2 = x1 + m.nodeWidth + m.spanWidth
+
+            x = [x1, x2]
+        else:
+            x = [m.node(self.edge.node1).bottom().x,
+                 m.node(self.edge.node2).bottom().x]
+            x.sort()
+
+        x[0] += self.activity_line_width(self.edge.node1)
+
+        baseheight = self.baseheight - self.metrix.edge_height / 2
+        return (x[0], baseheight,
+                x[1], baseheight + int(m.edge_height * 0.45))
+
+    def activity_line_width(self, node):
+        m = self.metrix
+
+        index = self.edge.y
+        activities = [a for a in node.activities if index in a['lifetime']]
+        if activities:
+            level = max(a['level'] for a in activities)
+        else:
+            level = 0
+
+        return m.cellSize / 2 * level
---
+++ packages/seqdiag/seqdiag-0.4.0.tar.gz/seqdiag-0.4.0/src/seqdiag/elements.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+#  Copyright 2011 Takeshi KOMIYA
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import re
+import sys
+import blockdiag.elements
+from blockdiag.elements import *
+from blockdiag.utils.XY import XY
+
+
+class DiagramNode(blockdiag.elements.DiagramNode):
+    def __init__(self, id):
+        super(DiagramNode, self).__init__(id)
+
+        self.activity = []
+        self.activities = []
+
+    def activate(self, height, index):
+        if len(self.activity) <= index:
+            self.activity.insert(index, [])
+
+        if len(self.activity[index]) > 0 and \
+           self.activity[index][-1] != height - 1:
+            self.deactivate(index)
+
+        self.activity[index].append(height)
+
+    def deactivate(self, index=None):
+        if index is None:
+            for i in range(len(self.activity)):
+                self.deactivate(i)
+            return
+
+        if self.activity[index]:
+            attr = {'lifetime': self.activity[index],
+                    'level': index}
+            self.activities.append(attr)
+
+        self.activity[index] = []
+
+
+class Diagram(blockdiag.elements.Diagram):
+    def __init__(self):
+        super(Diagram, self).__init__()
+
+        self.int_attrs.append('edge_height')
+        self.int_attrs.append('edge_length')
+
+        self.draw_activation = True
+        self.edge_height = None
+        self.edge_length = None
+        self.groups = []
+
+    def set_draw_activation(self, value):
+        if value == 'False':
+            self.draw_activation = False
+        else:
+            self.draw_activation = True
+
+
+class DiagramEdge(blockdiag.elements.DiagramEdge):
+    def __init__(self, node1, node2):
+        super(DiagramEdge, self).__init__(node1, node2)
+
+        self.height = 1
+        self.y = 0
+        self.activate = True
+        self.async = False
+        self.diagonal = False
+        self.return_label = ''
+
+    @property
+    def left_node(self):
+        if self.node1.xy.x <= self.node2.xy.x:
+            return self.node1
+        else:
+            return self.node2
+
+    @property
+    def right_node(self):
+        if self.node1.xy.x > self.node2.xy.x:
+            return self.node1
+        else:
+            return self.node2
+
+    @property
+    def direction(self):
+        if self.node1.xy.x == self.node2.xy.x:
+            direction = 'self'
+        elif self.node1.xy.x < self.node2.xy.x:
+            # n1 .. n2
+            if self.dir == 'forward':
+                direction = 'right'
+            else:
+                direction = 'left'
+        else:
+            # n2 .. n1
+            if self.dir == 'forward':
+                direction = 'left'
+            else:
+                direction = 'right'
+
+        return direction
+
+    def set_diagonal(self, value):
+        self.diagonal = True
+        self.height = 1.5
+
+    def set_async(self, value):
+        self.dir = 'forward'
+
+    def set_return(self, value):
+        self.return_label = value
+
+    def set_activate(self, value):
+        self.activate = True
+
+    def set_noactivate(self, value):
+        self.activate = False
+
+    def set_dir(self, value):
+        dir = value.lower()
+        if dir in ('back', 'both', 'forward'):
+            self.dir = dir
+        elif dir == '=>':
+            self.dir = 'both'
+        elif dir in ('->', '->>', '-->', '-->>'):
+            self.dir = 'forward'
+
+            if re.search('--', dir):
+                self.style = 'dashed'
+            else:
+                self.style = None
+
+            if re.search('>>', dir):
+                self.async = True
+            else:
+                self.async = False
+        elif dir in ('<-', '<<-', '<--', '<<--'):
+            self.dir = 'back'
+
+            if re.search('--', dir):
+                self.style = 'dashed'
+            else:
+                self.style = None
+
+            if re.search('<<', dir):
+                self.async = True
+            else:
+                self.async = False
+        else:
+            msg = "WARNING: unknown edge dir: %s\n" % dir
+            sys.stderr.write(msg)
