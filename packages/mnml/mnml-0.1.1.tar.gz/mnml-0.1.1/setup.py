f20c7deaf94a17322f9f2bfd7a639f82743e7ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:45 +0000

    Add package mnml/0.1.1/mnml-0.1.1.tar.gz

---
+++ packages/mnml/mnml-0.1.1.tar.gz/mnml-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="mnml",
+    version="0.1.1",
+    description="Parsing package for the Mnml language",
+    author="Emil Loer",
+    author_email="emil@koffietijd.net",
+    url="http://getmnml.com",
+    packages=["mnml"],
+    license="MIT"
+)
