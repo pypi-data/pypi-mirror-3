4935413fa0b8a19377dc2f14611e5c7237f354d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:55 +0000

    Add package hamlpy/0.3/hamlpy-0.3.tar.gz

---
+++ packages/hamlpy/hamlpy-0.3.tar.gz/hamlpy-0.3/hamlpy/hamlpy_watcher.py
@@ -0,0 +1,81 @@
+# haml-watcher.py
+# Author: Christian Stefanescu (st.chris@gmail.com)
+#
+# Watch a folder for files with the given extensions and call the HamlPy
+# compiler if the modified time has changed since the last check.
+
+import sys
+import codecs
+import os
+import os.path
+import time
+from hamlpy import Compiler
+
+EXTENSIONS = ['.haml']    # watched extensions
+CHECK_INTERVAL = 3          # in seconds
+DEBUG = False               # print file paths when a file is compiled
+
+# dict of compiled files [fullpath : timestamp]
+compiled = dict()
+
+def watched_extension(extension):
+    """Return True if the given extension is one of the watched extensions"""
+    for ext in EXTENSIONS:
+        if extension.endswith(ext):
+            return True
+    return False
+
+def watch_folder():
+    """Main entry point. Expects one or two arguments (the watch folder + optional destination folder)."""
+    argv_len = len(sys.argv)
+    if argv_len in (2, 3):
+        folder = os.path.realpath(sys.argv[1])
+        destination = os.path.realpath(argv_len == 3 and os.path.realpath(sys.argv[2]) or folder)
+        
+        print "Watching %s at refresh interval %s seconds" % (folder,CHECK_INTERVAL)
+        while True:
+            try:
+                _watch_folder(folder, destination)
+                time.sleep(CHECK_INTERVAL)
+            except KeyboardInterrupt:
+                # allow graceful exit (no stacktrace output)
+                sys.exit(0)
+                pass
+    else:
+        print "Usage: haml-watcher.py <watch_folder> [destination_folder]"
+
+def _watch_folder(folder, destination):
+    """Compares "modified" timestamps against the "compiled" dict, calls compiler
+    if necessary."""
+    for dirpath, dirnames, filenames in os.walk(folder):
+        for filename in filenames:
+            if watched_extension(filename):
+                fullpath = os.path.join(dirpath, filename) 
+            mtime = os.stat(fullpath).st_mtime
+            compiled_path = _compiled_path(destination, filename)
+            if (not fullpath in compiled or
+                compiled[fullpath] < mtime or
+                not os.path.isfile(compiled_path)):
+                compile_file(fullpath, compiled_path)
+                compiled[fullpath] = mtime
+
+def _compiled_path(destination, filename):
+    return os.path.join(destination, filename[:filename.rfind('.')] + '.html')
+
+def compile_file(fullpath, outfile_name):
+    """Calls HamlPy compiler."""
+    try:
+        if DEBUG:
+            print "Compiling %s -> %s" % (fullpath, outfile_name)
+        haml_lines = codecs.open(fullpath, 'r', encoding='utf-8').read().splitlines()
+        compiler = Compiler()
+        output = compiler.process_lines(haml_lines)
+        outfile = codecs.open(outfile_name, 'w', encoding='utf-8')
+        outfile.write(output)
+    except Exception, e:
+        # import traceback
+        print "Failed to compile %s -> %s\nReason:\n%s" % (fullpath, outfile_name, e)
+        # print traceback.print_exc()
+
+if __name__ == '__main__':
+    watch_folder()
