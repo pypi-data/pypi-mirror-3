88e5c1e41deb7c99991ccfcc48e064c8e0d64f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:01 +0000

    Add package djangoproject_wizard/1.1a/djangoproject_wizard-1.1a.tar.gz

---
+++ packages/djangoproject_wizard/djangoproject_wizard-1.1a.tar.gz/djangoproject_wizard-1.1a/bin/djangoproject-wizard.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+def main():
+    from djangoproject_wizard.management import start_project
+    start_project()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/djangoproject_wizard/djangoproject_wizard-1.1a.tar.gz/djangoproject_wizard-1.1a/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf8 -*-
+
+from setuptools import setup
+import os
+
+
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+DATA_DIR = os.path.join(ROOT_DIR, 'djangoproject_wizard', 'project_template')
+STARTPROJECT_DATA = []
+for path, dirs, filenames in os.walk(DATA_DIR):
+    # Ignore directories that start with '.'
+    for i, dir in enumerate(dirs):
+        if dir.startswith('.'):
+            del dirs[i]
+    path = path[len(DATA_DIR) + 1:]
+    STARTPROJECT_DATA.append(os.path.join('project_template', path, '*.*'))
+    # Get files starting with '.' too (they are excluded from the *.* glob).
+    STARTPROJECT_DATA.append(os.path.join('project_template', path, '.*'))
+
+
+setup(name='djangoproject_wizard',
+    version='1.1a',
+    author='Modified by Gilson Filho (Source: Lincoln Loop)',
+    author_email='contato@gilsondev.com',
+    description=('Create a Django project layout based on Lincoln Loop '
+                'best practices.'),
+    packages=['djangoproject_wizard'],
+    package_data={'djangoproject_wizard': STARTPROJECT_DATA},
+    scripts=['bin/djangoproject-wizard.py'],
+    classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Framework :: Django",
+    ],)
