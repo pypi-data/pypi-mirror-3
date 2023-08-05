17c07760e41ec0a5ac93c86b84d73d2fdf1e3d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:41 +0000

    Add package zc.zope3recipes/0.15.0/zc.zope3recipes-0.15.0.tar.gz

---
+++ packages/zc.zope3recipes/zc.zope3recipes-0.15.0.tar.gz/zc.zope3recipes-0.15.0/setup.py
@@ -0,0 +1,57 @@
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+name = "zc.zope3recipes"
+setup(
+    name = name,
+    version = '0.15.0',
+    author = "Jim Fulton",
+    author_email = "jim@zope.com",
+    description = "ZC Buildout recipe for defining Zope 3 applications",
+    license = "ZPL 2.1",
+    keywords = "zope3 buildout",
+    url='http://pypi.python.org/pypi/'+name,
+    long_description = (
+        read('README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('zc', 'zope3recipes', 'README.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        ),
+    packages = find_packages('.'),
+    include_package_data = True,
+    namespace_packages = ['zc'],
+    install_requires = [
+        'zc.buildout >=1.2.0',
+        'zope.testing',
+        'setuptools',
+        'zc.recipe.egg >=1.2.0',
+        'ZConfig >=2.4a5'],
+    entry_points = {
+        'zc.buildout': [
+             'application = %s.recipes:Application' % name,
+             'app = %s.recipes:App' % name,
+             'instance = %s.recipes:Instance' % name,
+             'offline = %s.recipes:Offline' % name,
+             'zopeconf = %s.recipes:ZopeConf' % name,
+             ],
+        },
+    extras_require = dict(
+        tests = ['zdaemon', 'zc.recipe.filestorage'],
+        ),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Buildout",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    )
