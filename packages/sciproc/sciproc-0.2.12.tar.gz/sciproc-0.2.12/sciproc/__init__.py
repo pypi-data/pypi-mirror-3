2fb669c2bda38da1105adfdb9ceb53cd92d66f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:43 +0000

    Add package sciproc/0.2.12/sciproc-0.2.12.tar.gz

---
+++ packages/sciproc/sciproc-0.2.12.tar.gz/sciproc-0.2.12/sciproc/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# Filename: __init__.py
+
+from dtrange import *
+
+# selection procedures
+from scisel import *
+
+# apply a function repeatedly on a multidimensional matrix
+from scimulti import *
+
+# statistics calculation
+from scistat import *
