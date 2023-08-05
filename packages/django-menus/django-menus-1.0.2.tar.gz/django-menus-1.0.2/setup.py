7a390ceb51400eede2d64606f4da2e154ae3976f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:44 +0000

    Add package django-menus/1.0.2/django-menus-1.0.2.tar.gz

---
+++ packages/django-menus/django-menus-1.0.2.tar.gz/django-menus-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = "django-menus",
+    version = "1.0.2",
+    description = "Menu helpers for django projects",
+    url = "http://bitbucket.org/schinckel/django-menus/",
+    author = "Matthew Schinckel",
+    author_email = "matt@schinckel.net",
+    packages = [
+        "menus",
+    ],
+    long_description = open("README.rst").read(),
+    classifiers = [
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+    ],
+)
