0d5ce6d59cef51355e34731f8bfc82564c36b42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:56 +0000

    Add package nose-progressive/1.3/nose-progressive-1.3.tar.gz

---
+++ packages/nose-progressive/nose-progressive-1.3.tar.gz/nose-progressive-1.3/setup.py
@@ -0,0 +1,40 @@
+import sys
+
+from setuptools import setup, find_packages
+
+
+extra_setup = {}
+if sys.version_info >= (3,):
+    extra_setup['use_2to3'] = True
+
+setup(
+    name='nose-progressive',
+    version='1.3',
+    description='Nose plugin to show progress bar and tracebacks during tests',
+    long_description=open('README.rst').read(),
+    author='Erik Rose',
+    author_email='erikrose@grinchcentral.com',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires=['Nose>=0.11.0', 'blessings>=1.3,<2.0'],
+    url='',
+    include_package_data=True,
+    entry_points="""
+        [nose.plugins.0.10]
+        noseprogressive = noseprogressive:ProgressivePlugin
+        """,
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Testing'
+        ],
+    **extra_setup
+)
