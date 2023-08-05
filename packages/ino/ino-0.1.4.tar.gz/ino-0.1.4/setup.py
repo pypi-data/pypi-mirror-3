ddd1a8e85eb65c91d2762f4ddd584babffadd74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:17 +0000

    Add package ino/0.1.4/ino-0.1.4.tar.gz

---
+++ packages/ino/ino-0.1.4.tar.gz/ino-0.1.4/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+install_requires = open("requirements.txt").read().split('\n')
+readme_content = open("README.rst").read()
+
+setup(
+    name='ino',
+    version='0.1.4',
+    description='Command line toolkit for working with Arduino hardware',
+    long_description=readme_content,
+    author='Victor Nakoryakov, Amperka Team',
+    author_email='victor@amperka.ru',
+    license='MIT',
+    keywords="arduino build system",
+    url='http://inotool.org',
+    packages=['ino', 'ino.commands'],
+    scripts=['bin/ino'],
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Embedded Systems",
+    ],
+)
