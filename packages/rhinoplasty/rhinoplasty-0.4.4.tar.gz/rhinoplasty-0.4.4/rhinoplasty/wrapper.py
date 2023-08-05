65d763814cbd2cb52874fa330e1c66cf7a4d11c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:36 +0000

    Add package rhinoplasty/0.4.4/rhinoplasty-0.4.4.tar.gz

---
+++ packages/rhinoplasty/rhinoplasty-0.4.4.tar.gz/rhinoplasty-0.4.4/rhinoplasty/wrapper.py
@@ -0,0 +1,87 @@
+"""Helper functions for wrapping one object inside a similar object.
+
+These are often helpful when implementing decorators.
+"""
+
+from nose.tools import make_decorator
+import inspect
+
+
+def wrap_test_function(original):
+    """Decorator to wrap a test function.
+    
+    This will copy any relevant metadata from the original function to the new
+    definition.
+    
+    @param original: The original function that is being wrapped.
+    """
+    if not callable(original):
+        raise ValueError("Original function is not actually a function.")
+    
+    # Just use the standard Nose function wrapper
+    #TODO investigate whether any changes need to be made to the standard function
+    return make_decorator(original)
+
+
+def wrap_test_class(original):
+    """Decorator to wrap a test suite.
+    
+    This will copy any relevant metadata from the original class to the new
+    definition.
+    
+    @param original: The original class that is being wrapped.
+    """
+    if not inspect.isclass(original):
+        raise ValueError("Original class is not actually a class.")
+    
+    def decorate(replacement):
+        if not inspect.isclass(replacement):
+            raise ValueError("Replacement class is not actually a class.")
+        
+        # Copy class name and module
+        replacement.__name__ = original.__name__
+        replacement.__module__ = original.__module__
+        
+        # Update the docstring, if possible
+        # New-style objects do not permit this.
+        try:
+            replacement.__doc__ = original.__doc__
+        except AttributeError:
+            pass
+        
+        # Copy metadata across. We can't necessarily copy attributes directly,
+        # and we need to filter them anyway.
+        for name, value in original.__dict__.items():
+            # Ignore special attributes
+            if name[:2] == "__" and name [-2:] == "__":
+                continue
+            
+            # Do not copy methods (obviously)
+            if callable(value):
+                continue
+            
+            # Don't replace existing attributes
+            if hasattr(replacement, name):
+                continue
+            
+            setattr(replacement, name, value)
+            
+        return replacement
+    
+    return decorate
+
+
+def wrap_test_fixture(original):
+    """Decorator to wrap a test case or test suite.
+    
+    This will copy any relevant metadata from the original test to the new
+    definition.
+    
+    @param original: The original class or function that is being wrapped.
+    """
+    if inspect.isclass(original):
+        return wrap_test_class(original)
+    elif callable(original):
+        return wrap_test_function(original)
+    else:
+        raise ValueError("Decorated object type is not recognised")
