df607e37835777c384cff6c99f133be78f3a7653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:17 +0000

    Add package mcint/0.1dev1/mcint-0.1dev1.zip

---
+++ packages/mcint/mcint-0.1dev1.zip/mcint-0.1dev1/mcint.py
@@ -0,0 +1,10 @@
+﻿# mcint/integrate.py
+
+import itertools
+
+def integrate(integrand, sampler, measure=1.0, n=100):
+    samples = list(itertools.islice(sampler(), n))
+    # Compute the sum of samples
+    ss = sum(samples)
+    # Return answer
+    return measure * ss / n
