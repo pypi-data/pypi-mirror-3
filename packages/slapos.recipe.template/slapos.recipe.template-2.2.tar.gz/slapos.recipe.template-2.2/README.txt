59ab7fa493c44d1688f9e2aba51a2ffc47f7a7b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:50 +0000

    Add package slapos.recipe.template/2.2/slapos.recipe.template-2.2.tar.gz

---
+++ packages/slapos.recipe.template/slapos.recipe.template-2.2.tar.gz/slapos.recipe.template-2.2/README.txt
@@ -0,0 +1,8 @@
+======================
+slapos.recipe.template
+======================
+
+Template recipe which supports remote resource.
+
+Inspired by collective.recipe.template, with minimum set of features, but with
+(hopefully) safer buildout-based templating.
---
+++ packages/slapos.recipe.template/slapos.recipe.template-2.2.tar.gz/slapos.recipe.template-2.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.2'
+name = 'slapos.recipe.template'
+long_description = open("README.txt").read() + "\n" + \
+    open(os.path.join('slapos', 'recipe',
+                      'template', "README.txt")).read() + "\n" + \
+    open("CHANGES.txt").read() + "\n"
+
+# extras_requires are not used because of
+#   https://bugs.launchpad.net/zc.buildout/+bug/85604
+setup(name=name,
+      version=version,
+      description="Templating recipe with remote resource support.",
+      long_description=long_description,
+      classifiers=[
+          "Framework :: Buildout :: Recipe",
+          "Programming Language :: Python",
+        ],
+      keywords='slapos recipe',
+      license='GPLv3',
+      namespace_packages=['slapos', 'slapos.recipe'],
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+        'setuptools', # namespaces
+        'zc.buildout', # plays with buildout
+        ],
+      zip_safe=True,
+      extras_require ={
+        'test': 'zope.testing',
+      },
+      entry_points={
+        'zc.buildout': [
+          'default = slapos.recipe.template:Recipe',
+      ]},
+    )
