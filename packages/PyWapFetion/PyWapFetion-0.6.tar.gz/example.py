a8616c35a620809877eff3bd05a8e2883bdb21aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:01 +0000

    Add package PyWapFetion/0.6/PyWapFetion-0.6.tar.gz

---
+++ packages/PyWapFetion/PyWapFetion-0.6.tar.gz/example.py
@@ -0,0 +1,21 @@
+﻿#coding=utf-8
+from PyWapFetion import *
+myfetion = Fetion('手机号','密码')
+
+myfetion.send2self('发给自己的东西')
+myfetion.findid('输入手机号，返回飞信ID')
+myfetion.send2id('飞信ID','消息')
+myfetion.send('手机号','消息')#发送飞信信息
+myfetion.send('手机号','消息',sm=True)#通过设定sm=True强制发送短信（sm=ShortMessage）
+myfetion.addfriend('手机号','你的昵称（5字以内）')
+myfetion.send(['手机号1','手机号2','这就是传说中的群发'],'消息')
+#成功返回True，失败返回False
+myfetion.send2self('这个是发给自己的定时短信',time='201111201120')#2011年11月20日11时20分发送
+
+myfetion.changeimpresa('改签名')
+myfetion.alive()#保持在线，10分钟以上无操作会被判定为离线
+
+myfetion.logout()
+#----------------------------------------------------------------------
+send2self('手机号','密码','信息')
+send('手机号','密码','接收方手机号','信息')
---
+++ packages/PyWapFetion/PyWapFetion-0.6.tar.gz/setup.py
@@ -0,0 +1,49 @@
+﻿#coding=utf-8
+from setuptools import setup,find_packages
+import PyWapFetion
+
+setup(
+name = PyWapFetion.__name__,
+version = PyWapFetion.__version__,
+
+packages = find_packages(),
+keywords = 'library mobile fetion',
+author = PyWapFetion.__author__,
+author_email = 'whtsky@vip.qq.com',
+
+url = PyWapFetion.__website__,
+description = 'A simple python lib for WapFetion',
+long_description = '''
+Fetion SDK for Python.
+
+PyWapFetion is a useful Fetion Library for Python made by .It's very easy to use.
+
+Go to https://github.com/whtsky/PyWapFetion for more information.
+
+* Cache support
+* Auto keep alive support
+* NO CAPTCHA during using it
+* Supports Python 2.5 - 2.7
+* MIT LICENSE
+
+THIS IS A THRID PARTY SDK,USE AT YOUR RISK.
+''',
+license = PyWapFetion.__license__,
+classifiers = [
+'Development Status :: 4 - Beta',
+'Environment :: Console',
+'Intended Audience :: Developers',
+'License :: OSI Approved :: MIT License',
+'Natural Language :: English',
+'Natural Language :: Chinese (Simplified)',
+'Operating System :: OS Independent',
+'Programming Language :: Python :: 2',
+'Programming Language :: Python :: 2.5',
+'Programming Language :: Python :: 2.6',
+'Programming Language :: Python :: 2.7',
+'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+'Topic :: Utilities',
+'Topic :: Software Development :: Libraries :: Python Modules',
+],
+zip_safe = True,
+)
