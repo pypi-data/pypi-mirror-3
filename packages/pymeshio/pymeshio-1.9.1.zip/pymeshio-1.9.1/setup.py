730ed789132eaae9ac0c32ead04ea12d41c27a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:25 +0000

    Add package pymeshio/1.9.1/pymeshio-1.9.1.zip

---
+++ packages/pymeshio/pymeshio-1.9.1.zip/pymeshio-1.9.1/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup
+import sys
+
+name='pymeshio'
+version='1.9.1'
+short_description='pure python 3d model io library'
+long_description='''\
+`pymeshio` is a package for 3d model io.
+
+Requirements
+------------
+* Python 3
+
+Features
+--------
+* read/write Metasequioa mqo format
+* read/write MikuMikuDance pmd format
+* read/write MikuMikuDance vmd format
+* read/write MikuMikuDance vpd format
+
+Setup
+-----
+::
+
+   $ easy_install pymeshio
+
+History
+-------
+1.9.1 (2011-9-23)
+~~~~~~~~~~~~~~~~~~
+* register pypi
+'''
+
+classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: zlib/libpng License',
+        'Topic :: Multimedia :: Graphics :: 3D Modeling',
+        ]
+
+setup(
+        name=name,
+        version=version,
+        description=short_description,
+        long_description=long_description,
+        classifiers=classifiers,
+        keywords=['mqo', 'pmd', 'vmd', 'vpd', 'mmd', 'blender'],
+        author='ousttrue',
+        author_email='ousttrue@gmail.com',
+        url='http://meshio.sourceforge.jp/',
+        license='zlib',
+        package_dir={
+            'pymeshio': 'blender25-meshio/pymeshio'
+            },
+        packages=['pymeshio'],
+        test_suite='nose.collector',
+        tests_require=['Nose'],
+        zip_safe = (sys.version>="2.5"),   # <2.5 needs unzipped for -m to work
+        )
+
