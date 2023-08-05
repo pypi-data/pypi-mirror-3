ba9f006e027489d0a19160d7d37fb14e63e91237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package OpenProximity/11.11.5/OpenProximity-11.11.5.tar.gz

---
+++ packages/OpenProximity/OpenProximity-11.11.5.tar.gz/OpenProximity-11.11.5/src/openproximity/lib/manage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#    OpenProximity2.0 is a proximity marketing OpenSource system.
+#    Copyright (C) 2010,2009,2008 Naranjo Manuel Francisco <manuel@aircable.net>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import setpaths
+
+try:
+    import settings # Assumed to be in the same directory.
+except ImportError, e:
+    import sys, traceback
+    print "Error: Can't find the file 'settings.py' in the directory",
+    print "containing %r. It appears you've customized things.\nYou'll have" % __file__,
+    print "to run django-admin.py, passing it your settings module.\n(If the",
+    print "file settings.py does indeed exist, it's causing an ImportError",
+    print "somehow.)""" 
+    print traceback.print_exc()
+    sys.exit(1)
+
+from django.core.management import execute_from_command_line, setup_environ
+setup_environ(settings)
+
+from net.aircable.openproximity.pluginsystem import pluginsystem
+pluginsystem.post_environ()
+
+def main():
+    from django.conf import settings as set
+    execute_from_command_line()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/OpenProximity/OpenProximity-11.11.5.tar.gz/OpenProximity-11.11.5/src/openproximity/management/commands/rpc.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import threading, time, traceback, sys, os
+from openproximity.rpc import server
+from rpyc import Service, async
+from rpyc.utils.server import ThreadedServer, ForkingServer
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from optparse import make_option, OptionParser
+from configglue.glue import schemaconfigglue
+
+
+SERVER_OPTIONS = {
+    'host': settings.RPC_HOST,
+    'port': settings.RPC_PORT,
+    'autoregister': settings.RPC_AUTOREGISTER,
+    'threaded': settings.RPC_THREADED,
+    'daemonize': settings.RPC_DAEMONIZE,
+    'pidfile': settings.RPC_PIDFILE,
+    'server_user': settings.RPC_SERVER_USER,
+    'server_group': settings.RPC_SERVER_GROUP,
+}
+
+class Command(BaseCommand):
+    help = "RPyC Server for OpenProximity."
+
+    def handle(self, *args, **options):
+        from django.conf import settings
+        from django.utils import translation
+        from net.aircable import utils
+        utils.log_setup(settings, True)
+        runserver(args)
+
+    def usage(self, subcommand):
+        return self.help
+
+    def create_parser(self, prog_name, subcommand):
+        """
+        Add all our SchemaConfigParser's options so they can be shown
+        in help messages and such.
+        """
+        parser = OptionParser(prog=prog_name,
+            usage=self.usage(subcommand),
+            version=self.get_version(),
+            option_list=self.option_list)
+
+        configglue_parser = settings.__CONFIGGLUE_PARSER__
+        configglue_parser.setmode("rpyc")
+        op, options, args = schemaconfigglue(configglue_parser, op=parser)
+        return op
+
+def change_uid_gid(uid, gid=None):
+    """Try to change UID and GID to the provided values.
+    UID and GID are given as names like 'nobody' not integer.
+
+    Src: http://mail.mems-exchange.org/durusmail/quixote-users/4940/1/
+    """
+    if not os.geteuid() == 0:
+        # Do not try to change the gid/uid if not root.
+        return
+    (uid, gid) = get_uid_gid(uid, gid)
+    os.setgid(gid)
+    os.setuid(uid)
+
+def get_uid_gid(uid, gid=None):
+    """Try to change UID and GID to the provided values.
+    UID and GID are given as names like 'nobody' not integer.
+
+    Src: http://mail.mems-exchange.org/durusmail/quixote-users/4940/1/
+    """
+    import pwd, grp
+    uid, default_grp = pwd.getpwnam(uid)[2:4]
+    if gid is None:
+        gid = default_grp
+    else:
+        try:
+            gid = grp.getgrnam(gid)[2]
+        except KeyError:
+            gid = default_grp
+    return (uid, gid)
+
+def start_server(options):
+    """
+    Start RPyC server
+    """
+
+    if options['daemonize'] and options['server_user'] and \
+            options['server_group']:
+        #ensure the that the daemon runs as specified user
+        change_uid_gid(options['server_user'], options['server_group'])
+
+    if options['threaded']:
+        serv = ThreadedServer(server.OpenProximityService, options['host'],
+            port = options['port'], auto_register = options['autoregister'])
+    else:
+        print "Forking server is not a tested server!"
+        serv = ForkingServer(server.OpenProximityService, options['host'],
+            port = options['port'], auto_register = options['autoregister'])
+
+    try:
+        serv.start()
+    except KeyboardInterrupt, err:
+        serv.stop()
+    
+    
+def runserver(argset=[], **kwargs):
+    # Get the options
+    options = SERVER_OPTIONS.copy()
+    options.update(kwargs)
+
+    from net.aircable.utils import getLogger
+    logger = getLogger(__name__)
+
+    if options['daemonize']:
+        if not options['pidfile']:
+            options['pidfile'] = '/var/run/openproximity_rpc_%s.pid' % \
+                options['port']
+
+        logger.info("storing pid file in %s" % options['pidfile'])
+        from django.utils.daemonize import become_daemon
+        
+        logger.info("daemonizing")
+        become_daemon()
+        logger.info("daemonized")
+
+        fp = open(options['pidfile'], 'w')
+        fp.write("%d\n" % os.getpid())
+        fp.close()
+
+    # Start the rpc server
+    logger.debug('starting server with options %s' % options)
+    start_server(options)
+    
+if __name__ == '__main__':
+    runserver(sys.argv[1:])
