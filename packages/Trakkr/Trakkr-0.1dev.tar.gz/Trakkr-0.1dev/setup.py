31557b9faf1ffd7c4bf0e304e838f30c74682035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:50 +0000

    Add package Trakkr/0.1dev/Trakkr-0.1dev.tar.gz

---
+++ packages/Trakkr/Trakkr-0.1dev.tar.gz/Trakkr-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Trakkr',
+    version='0.1dev',
+    packages=['trakkr',],
+    license='MIT License',
+    long_description=open('README.txt').read(),
+)
