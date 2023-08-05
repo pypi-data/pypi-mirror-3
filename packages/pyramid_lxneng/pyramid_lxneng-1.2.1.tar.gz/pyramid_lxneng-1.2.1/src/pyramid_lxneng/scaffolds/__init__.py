ee553e5e00a11ea8dd7ed1cd47274c33cf0a277f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:56 +0000

    Add package pyramid_lxneng/1.2.1/pyramid_lxneng-1.2.1.tar.gz

---
+++ packages/pyramid_lxneng/pyramid_lxneng-1.2.1.tar.gz/pyramid_lxneng-1.2.1/src/pyramid_lxneng/scaffolds/__init__.py
@@ -0,0 +1,11 @@
+from pyramid.scaffolds import PyramidTemplate
+
+
+class SQLAlchemyProjectTemplate(PyramidTemplate):
+    _template_dir = 'lxneng_sqlalchemy'
+    summary = 'Pyramid + SQLAlchemy + Jinja2 project using url dispatch'
+
+
+class MongoEngineProjectTemplate(PyramidTemplate):
+    _template_dir = 'lxneng_mongoengine'
+    summary = 'Pyramid + MongoEngine + Jinja2 project using url dispatch'
