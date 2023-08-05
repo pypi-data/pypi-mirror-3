9736a571ea5971cb3aae2961db12c3ff67194e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:13 +0000

    Add package django-facebook/3.2.0/django-facebook-3.2.0.zip

---
+++ packages/django-facebook/django-facebook-3.2.0.zip/django-facebook-3.2.0/django_facebook/utils.py
@@ -0,0 +1,166 @@
+
+from django.http import QueryDict
+from django.conf import settings
+from django.db import models
+
+
+
+def get_oauth_url(request, scope, redirect_uri=None):
+    '''
+    Returns the oauth url for the given request and scope
+    Request maybe shouldnt be tied to this function, but for now it seems 
+    rather ocnvenient
+    '''
+    from django_facebook import settings as facebook_settings
+    scope = parse_scope(scope)
+    query_dict = QueryDict('', True)
+    query_dict['scope'] = ','.join(scope)
+    query_dict['client_id'] = facebook_settings.FACEBOOK_APP_ID
+    redirect_uri = redirect_uri or request.build_absolute_uri()
+    
+    #prevent redirect loops
+    print redirect_uri
+    if '?' not in redirect_uri:
+        redirect_uri += '?attempt=1'
+    else:
+        redirect_uri += '&attempt=1'
+    print redirect_uri
+        
+    if ('//localhost' in redirect_uri or '//127.0.0.1' in redirect_uri) and settings.DEBUG:
+        raise ValueError, 'Facebook checks the domain name of your apps. Therefor you cannot run on localhost. Instead you should use something like local.fashiolista.com. Replace Fashiolista with your own domain name.'
+    query_dict['redirect_uri'] = redirect_uri
+    url = 'https://www.facebook.com/dialog/oauth?'
+    url += query_dict.urlencode()
+    return url, redirect_uri
+
+def next_redirect(request, default='/', additional_params=None, next_key='next'):
+    from django.http import HttpResponseRedirect
+    if not isinstance(next_key, (list, tuple)):
+        next_key = [next_key]
+    
+    #get the redirect url
+    redirect_url = None
+    for key in next_key:
+        redirect_url = request.REQUEST.get(key)
+        if redirect_url:
+            break
+    if not redirect_url:
+        redirect_url = default
+        
+    if additional_params:
+        query_params = QueryDict('', True)
+        query_params.update(additional_params)
+        seperator = '&' if '?' in redirect_url else '?'
+        redirect_url += seperator + query_params.urlencode()
+        
+    return HttpResponseRedirect(redirect_url)
+
+
+def get_profile_class():
+    profile_string = settings.AUTH_PROFILE_MODULE
+    app_label, model = profile_string.split('.')
+
+    return models.get_model(app_label, model)
+    
+    
+def mass_get_or_create(model_class, base_queryset, id_field, default_dict, global_defaults):
+    '''
+    Updates the data by inserting all not found records
+    
+    Doesnt delete records if not in the new data
+
+    example usage
+    >>> model_class = ListItem #the class for which you are doing the insert
+    >>> base_query_set = ListItem.objects.filter(user=request.user, list=1) #query for retrieving currently stored items
+    >>> id_field = 'user_id' #the id field on which to check
+    >>> default_dict = {'12': dict(comment='my_new_item'), '13': dict(comment='super')} #list of default values for inserts
+    >>> global_defaults = dict(user=request.user, list_id=1) #global defaults
+    '''
+    current_instances = list(base_queryset)
+    current_ids = [unicode(getattr(c, id_field)) for c in current_instances]
+    given_ids = map(unicode, default_dict.keys())
+    new_ids = [g for g in given_ids if g not in current_ids]
+    inserted_model_instances = []
+    for new_id in new_ids:
+        defaults = default_dict[new_id]
+        defaults[id_field] = new_id
+        defaults.update(global_defaults)
+        model_instance = model_class.objects.create(
+            **defaults
+        )
+        inserted_model_instances.append(model_instance)
+        
+    #returns a list of existing and new items
+    return current_instances, inserted_model_instances
+
+
+def get_registration_backend():
+    '''
+    Ensures compatability with the new and old version of django registration
+    '''
+    backend = None
+    try:        
+        #support for the newer implementation
+        from registration.backends import get_backend
+        try:
+            backend = get_backend(settings.REGISTRATION_BACKEND)
+        except:
+            raise ValueError, 'Cannot get django-registration backend from settings.REGISTRATION_BACKEND'
+    except ImportError, e:
+        backend = None
+    return backend
+
+
+
+def parse_scope(scope):
+    '''
+    Turns
+    'email,user_about_me'
+    or
+    ('email','user_about_me')
+    into a nice consistent
+    ['email','user_about_me']
+    '''
+    assert scope, 'scope is required'
+    if isinstance(scope, basestring):
+        scope_list = scope.split(',')
+    elif isinstance(scope, (list, tuple)):
+        scope_list = list(scope)
+        
+    return scope_list 
+
+
+def to_int(input, default=0, exception=(ValueError, TypeError), regexp=None):
+    '''Convert the given input to an integer or return default
+
+    When trying to convert the exceptions given in the exception parameter
+    are automatically catched and the default will be returned.
+
+    The regexp parameter allows for a regular expression to find the digits
+    in a string.
+    When True it will automatically match any digit in the string.
+    When a (regexp) object (has a search method) is given, that will be used.
+    WHen a string is given, re.compile will be run over it first
+
+    The last group of the regexp will be used as value
+    '''
+    import re
+    if regexp is True:
+        regexp = re.compile('(\d+)')
+    elif isinstance(regexp, basestring):
+        regexp = re.compile(regexp)
+    elif hasattr(regexp, 'search'):
+        pass
+    elif regexp is not None:
+        raise TypeError, 'unknown argument for regexp parameter'
+
+    try:
+        if regexp:
+            match = regexp.search(input)
+            if match:
+                input = match.groups()[-1]
+        return int(input)
+    except exception:
+        return default
+    
+    
---
+++ packages/django-facebook/django-facebook-3.2.0.zip/django-facebook-3.2.0/django_facebook/views.py
@@ -0,0 +1,135 @@
+import logging
+import sys
+
+from django.conf import settings
+from django.contrib import messages
+from django.http import Http404, HttpResponseRedirect, QueryDict
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from django.utils.translation import ugettext as _
+from django.views.decorators.csrf import csrf_exempt
+
+# NOTE: from inside the application, you can directly import the file
+from django_facebook import exceptions as facebook_exceptions, \
+    settings as facebook_settings
+from django_facebook.api import get_facebook_graph, get_persistent_graph,\
+    FacebookUserConverter
+from django_facebook.canvas import generate_oauth_url
+from django_facebook.connect import CONNECT_ACTIONS, connect_user
+from django_facebook.utils import next_redirect
+from django_facebook.decorators import facebook_required
+
+logger = logging.getLogger(__name__)
+
+
+
+@facebook_required(scope='publish_stream')
+def wall_post(request):
+    fb = get_persistent_graph(request)
+
+    message = request.POST.get('message')
+    fb.set('me/feed', message=message)
+
+    messages.info(request, 'Posted the message to your wall')
+
+    return next_redirect(request)
+
+
+@facebook_required(scope='publish_stream,user_photos')
+def image_upload(request):
+    fb = get_persistent_graph(request)
+    pictures = request.POST.getlist('pictures')
+
+    for picture in pictures:
+        fb.set('me/photos', url=picture, message='the writing is one The '
+            'wall image %s' % picture)
+
+    messages.info(request, 'The images have been added to your profile!')
+
+    return next_redirect(request)
+
+@csrf_exempt
+def connect(request):
+    '''
+    Handles the view logic around connect user
+    - (if authenticated) connect the user
+    - login
+    - register
+    '''
+    context = RequestContext(request)
+
+    assert context.get('FACEBOOK_APP_ID'), 'Please specify a facebook app id '\
+        'and ensure the context processor is enabled'
+    facebook_login = bool(int(request.REQUEST.get('facebook_login', 0)))
+
+    if facebook_login:
+        graph = get_facebook_graph(request)
+        if graph:
+            facebook = FacebookUserConverter(graph)
+            if facebook.is_authenticated():
+                facebook_data = facebook.facebook_profile_data()
+                #either, login register or connect the user
+                try:
+                    action, user = connect_user(request)
+                except facebook_exceptions.IncompleteProfileError, e:
+                    warn_message = u'Incomplete profile data encountered '\
+                        'with error %s' % e
+                    logger.warn(warn_message,
+                        exc_info=sys.exc_info(), extra={
+                        'request': request,
+                        'data': {
+                             'username': request.user.username,
+                             'facebook_data': facebook.facebook_profile_data(),
+                             'body': unicode(e),
+                         }
+                    })
+
+                    context['facebook_mode'] = True
+                    context['form'] = e.form
+                    return render_to_response(
+                        'registration/registration_form.html',
+                        context_instance=context,
+                    )
+
+                if action is CONNECT_ACTIONS.CONNECT:
+                    messages.info(request, _("You have connected your account "
+                        "to %s's facebook profile") % facebook_data['name'])
+                elif action is CONNECT_ACTIONS.REGISTER:
+                    return user.get_profile().post_facebook_registration(request)
+        else:
+            return next_redirect(request, next_key=['error_next', 'next'],
+                additional_params=dict(fb_error_or_cancel=1))
+
+        return next_redirect(request)
+
+    if not settings.DEBUG and facebook_settings.FACEBOOK_HIDE_CONNECT_TEST:
+        raise Http404
+
+    return render_to_response('django_facebook/connect.html', context)
+
+
+
+
+
+
+
+
+@csrf_exempt
+def canvas(request):
+    context = RequestContext(request)
+
+    context['auth_url'] = generate_oauth_url()
+    fb = get_persistent_graph(request)
+    if fb.is_authenticated():
+        likes = context['facebook'].get_connections("me", "likes", limit=3)
+        logger.info('found these likes %s', likes)
+
+    return render_to_response('django_facebook/canvas.html', context)
+
+
+@csrf_exempt
+def my_style(request):
+    context = RequestContext(request)
+    context['auth_url'] = generate_oauth_url()
+
+    return render_to_response('django_facebook/my_style.html', context)
