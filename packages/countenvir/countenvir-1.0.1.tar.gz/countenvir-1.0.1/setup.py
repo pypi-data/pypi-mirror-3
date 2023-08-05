86444135ec904578711df5130b108d68b4c146b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:43 +0000

    Add package countenvir/1.0.1/countenvir-1.0.1.tar.gz

---
+++ packages/countenvir/countenvir-1.0.1.tar.gz/countenvir-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'countenvir',
+	version		= '1.0.1',
+	py_modules	= ['countenvir'],
+	author		= 'Jochen Demmer',
+	author_email	= 'info@csdemmer.de',
+	url		= 'http://www.csdemmer.de',
+	license		= 'GPLv3',
+	description	= 'A simple environment to count the occurrances of lation letters')
