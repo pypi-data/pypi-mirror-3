880c36e47f3dab43bc77f9ecdc2414b5e0ccb8da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:27 +0000

    Add package fabutil/0.1.1/fabutil-0.1.1.tar.gz

---
+++ packages/fabutil/fabutil-0.1.1.tar.gz/fabutil-0.1.1/fabutil/nested_task.py
@@ -0,0 +1,28 @@
+from fabric.colors import green
+import fabric.tasks as tasks
+from types import MethodType
+
+from indenter import indenter
+
+def task(*args, **kwargs):
+    func, args = args[0], ()
+
+    def myrun(self, *args, **kwargs):
+        print green(">>> entering ") + green("%s" % func.__name__, True)
+        indenter.push()
+        result = self.oldrun(*args, **kwargs)
+        indenter.pop()
+        print green(">>> leaving ") + green("%s" % func.__name__, True)
+        return result
+
+    def wrapper(func):
+        t = tasks.WrappedCallableTask(func, *args, **kwargs)
+        t.oldrun = t.run
+        t.run = MethodType(myrun, t, tasks.WrappedCallableTask)
+        return t
+
+    return wrapper(func)
+
+tasks.task = task
+
+__all__ = ["task"]
