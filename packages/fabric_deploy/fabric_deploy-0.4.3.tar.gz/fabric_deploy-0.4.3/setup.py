a990d7c917447589924c669ee94af5925d86f38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:04 +0000

    Add package fabric_deploy/0.4.3/fabric_deploy-0.4.3.tar.gz

---
+++ packages/fabric_deploy/fabric_deploy-0.4.3.tar.gz/fabric_deploy-0.4.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from __future__ import with_statement
+try:
+  from setuptools import setup, find_packages
+except ImportError:
+  from ez_setup import use_setuptools
+  use_setuptools()
+  from setuptools import setup, find_packages
+
+import contextlib
+version = None
+with contextlib.closing(open('VERSION')) as fp:
+  version = fp.read().strip()
+
+setup(
+  name='fabric_deploy',
+  version=version,
+  description='capistrano like deploy recipe for fabric',
+  author='Yamashita Yuu',
+  author_email='yamashita@geishatokyo.com',
+  url='https://github.com/yyuu/fabric_deploy',
+  install_requires=[
+    'Fabric>=1.2',
+  ],
+  packages=find_packages(),
+)
