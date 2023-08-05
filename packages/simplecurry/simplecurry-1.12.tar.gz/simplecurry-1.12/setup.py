d0feaa3bfd9225c49fac9296c7985fe92f0d283b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:08 +0000

    Add package simplecurry/1.12/simplecurry-1.12.tar.gz

---
+++ packages/simplecurry/simplecurry-1.12.tar.gz/simplecurry-1.12/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup
+
+curdir = os.path.dirname(os.path.abspath(__file__))
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+setup(
+    name = 'simplecurry',
+    description = 'Curried decorator',
+    long_description = read('README.rst'),
+    license = 'http://www.gnu.org/licenses/gpl-2.0.html',
+    version = '1.12',
+    author = 'Òscar Vilaplana',
+    author_email = 'dev@oscarvilaplana.cat',
+    url = 'https://github.com/grimborg/simplecurry',
+    py_modules=['simplecurry'],
+    )
