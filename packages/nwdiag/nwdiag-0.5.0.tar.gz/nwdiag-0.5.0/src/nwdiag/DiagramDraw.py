2fc7755c8add09e93fc43379ed53c799918ec049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:23 +0000

    Add package nwdiag/0.5.0/nwdiag-0.5.0.tar.gz

---
+++ packages/nwdiag/nwdiag-0.5.0.tar.gz/nwdiag-0.5.0/src/nwdiag/DiagramDraw.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+#  Copyright 2011 Takeshi KOMIYA
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import sys
+import math
+import blockdiag.DiagramDraw
+from blockdiag.utils.XY import XY
+from blockdiag import noderenderer
+from blockdiag.DiagramMetrix import DiagramMetrix
+
+
+class DiagramDraw(blockdiag.DiagramDraw.DiagramDraw):
+    def __init__(self, format, diagram, filename=None, **kwargs):
+        super(DiagramDraw, self).__init__(format, diagram, filename, **kwargs)
+        self.drawer.forward = 'vertical'
+        self.drawer.jump_radius = self.metrix.jump_radius
+        self.drawer.jump_shift = self.metrix.jump_shift
+
+    @property
+    def groups(self):
+        return self.diagram.groups
+
+    def pagesize(self, scaled=False):
+        # FIXME: force int'ize
+        xy = super(DiagramDraw, self).pagesize(scaled)
+        return XY(int(xy.x), int(xy.y))
+
+    def _draw_background(self):
+        super(DiagramDraw, self)._draw_background()
+        self._draw_trunklines_shadow()
+
+    def _draw_trunklines_shadow(self):
+        xdiff = self.metrix.shadowOffsetX
+        ydiff = self.metrix.shadowOffsetY
+
+        metrix = self.metrix.originalMetrix()
+        for network in self.diagram.networks:
+            if network.hidden == False:
+                self.trunkline(network, shadow=True)
+
+    def _draw_trunklines(self):
+        metrix = self.metrix
+        for network in self.diagram.networks:
+            if network.hidden == False:
+                self.trunkline(network)
+
+                # FIXME: first network links to global network
+                if network == self.diagram.networks[0]:
+                    r = metrix.trunk_diameter / 2
+
+                    pt = metrix.network(network).top()
+                    pt0 = XY(pt.x, pt.y - metrix.spanHeight * 2 / 3)
+                    pt1 = XY(pt.x, pt.y - r)
+
+                    self.drawer.line([pt0, pt1], fill=network.linecolor)
+
+    def trunkline(self, network, shadow=False):
+        if shadow:
+            metrix = self.metrix.originalMetrix()
+        else:
+            metrix = self.metrix
+
+        m = metrix.network(network)
+        r = metrix.trunk_diameter / 2
+
+        pt1, pt2 = m.trunkline
+        box = (pt1.x, pt1.y - r, pt2.x, pt2.y + r)
+
+        if shadow:
+            xdiff = self.metrix.shadowOffsetX
+            ydiff = self.metrix.shadowOffsetY / 2
+
+            box = (pt1.x + xdiff, pt1.y - r + ydiff,
+                   pt2.x + xdiff, pt2.y + r + ydiff)
+
+        if self.format == 'SVG':
+            from blockdiag.imagedraw.simplesvg import pathdata
+
+            path = pathdata(box[0], box[1])
+            path.line(box[2], box[1])
+            path.ellarc(r / 2, r, 0, 0, 1, box[2], box[3])
+            path.line(box[0], box[3])
+            path.ellarc(r / 2, r, 0, 0, 1, box[0], box[1])
+
+            if shadow:
+                self.drawer.path(path, fill=self.shadow, filter='blur')
+            else:
+                self.drawer.path(path, fill=network.color,
+                                 outline=network.linecolor)
+
+                path = pathdata(box[2], box[3])
+                path.ellarc(r / 2, r, 0, 0, 1, box[2], box[1])
+                self.drawer.path(path, fill='none', outline=network.linecolor)
+
+                # for edge jumping
+                line = (XY(box[0], box[1]), XY(box[2], box[1]))
+                self.drawer.line(line, fill='none', jump=True)
+        else:
+            lsection = (box[0] - r / 2, box[1], box[0] + r / 2, box[3])
+            rsection = (box[2] - r / 2, box[1], box[2] + r / 2, box[3])
+
+            if shadow:
+                color = self.shadow
+            else:
+                color = network.color
+
+            # fill background
+            self.drawer.rectangle(box, outline=color, fill=color)
+            self.drawer.ellipse(lsection, outline=color, fill=color)
+            self.drawer.ellipse(rsection, outline=color, fill=color)
+
+            if not shadow:
+                upper = (XY(box[0], box[1]), XY(box[2], box[1]))
+                self.drawer.line(upper,
+                                 fill=network.linecolor, jump=True)
+
+                bottom = (XY(box[0], box[3]), XY(box[2], box[3]))
+                self.drawer.line(bottom,
+                                 fill=network.linecolor, jump=True)
+
+                self.drawer.arc(lsection, 90, 270, fill=network.linecolor)
+                self.drawer.ellipse(rsection, outline=network.linecolor,
+                                    fill=network.color)
+
+    def _draw_elements(self):
+        self._draw_trunklines()
+        self._draw_trunkline_labels()
+        super(DiagramDraw, self)._draw_elements()
+
+    def _draw_trunkline_labels(self):
+        for network in self.diagram.networks:
+            if network.display_label:
+                m = self.metrix.network(network)
+                self.drawer.textarea(m.textbox, network.display_label,
+                                     fill=self.diagram.textcolor,
+                                     halign="right", font=self.font,
+                                     fontsize=self.metrix.fontSize)
+
+    def node(self, node, **kwargs):
+        m = self.metrix
+
+        for connector in m.node(node).connectors:
+            self.draw_connector(connector)
+
+            if connector.network in node.address:
+                label = node.address[connector.network]
+                self.drawer.textarea(connector.textbox, label,
+                                     fill=node.textcolor,
+                                     halign="left", font=self.font,
+                                     fontsize=self.metrix.fontSize)
+
+        super(DiagramDraw, self).node(node, **kwargs)
+
+    def draw_connector(self, connector):
+        m = self.metrix
+        self.drawer.line(connector.line,
+                         fill=connector.network.linecolor, jump=True)
+
+    def group_label(self, group):
+        if group.label:
+            m = self.metrix.cell(group)
+            self.drawer.textarea(m.groupLabelBox(), group.label, valign='top',
+                                 fill=group.textcolor, font=self.font,
+                                 fontsize=self.metrix.fontSize)
+
+
+from DiagramMetrix import DiagramMetrix
+DiagramDraw.set_metrix_class(DiagramMetrix)
---
+++ packages/nwdiag/nwdiag-0.5.0.tar.gz/nwdiag-0.5.0/src/nwdiag/elements.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+#  Copyright 2011 Takeshi KOMIYA
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import re
+import sys
+import blockdiag.elements
+from blockdiag.elements import *
+from blockdiag.utils.XY import XY
+from blockdiag.utils import images
+
+
+class DiagramNode(blockdiag.elements.DiagramNode):
+    def __init__(self, id):
+        super(DiagramNode, self).__init__(id)
+
+        self.address = {}
+        self.networks = []
+        self.layouted = False
+
+    def set_attributes(self, network, attrs):
+        for attr in attrs:
+            if attr.name == 'address':
+                address = re.sub('\s*,\s*', '\n', unquote(attr.value))
+                self.address[network] = address
+            else:
+                self.set_attribute(attr)
+
+
+class Network(blockdiag.elements.NodeGroup):
+    basecolor = (185, 203, 228)
+    linecolor = (0, 0, 0)
+
+    @classmethod
+    def set_default_line_color(cls, color):
+        cls.linecolor = images.color_to_rgb(color)
+
+    @classmethod
+    def clear(cls):
+        super(Network, cls).clear()
+        cls.basecolor = (185, 203, 228)
+        cls.linecolor = (0, 0, 0)
+
+    def __init__(self, id):
+        super(Network, self).__init__(id)
+
+        self.address = None
+        self.hidden = False
+        self.width = 0
+        self.height = 0
+
+    @classmethod
+    def create_anonymous(klass, nodes, attrs=[]):
+        if len(set(nodes)) != len(nodes):
+            msg = "Do not connect same node to peer network: %s"
+            raise RuntimeError(msg % nodes[0].id)
+
+        # search networks including same nodes
+        is_same = lambda nw: set(nodes) & set(nw.nodes) == set(nodes)
+        same = [nw for nw in nodes[0].networks if nw.hidden and is_same(nw)]
+        if [nw for nw in nodes[0].networks if nw.hidden and is_same(nw)]:
+            return None
+
+        network = klass(None)
+        network.hidden = True
+        for node in nodes:
+            node.networks.append(network)
+            network.nodes.append(node)
+
+        if attrs:
+            node.set_attributes(network, attrs)
+
+        return network
+
+    @property
+    def display_label(self):
+        if self.label:
+            if self.address:
+                label = "%s\n%s" % (self.label, self.address)
+            else:
+                label = self.label
+        else:
+            label = self.address
+
+        return label
+
+
+class Diagram(blockdiag.elements.Diagram):
+    _DiagramNode = DiagramNode
+    _Network = Network
+
+    def set_default_line_color(self, color):
+        super(Diagram, self).set_default_line_color(color)
+        self._Network.set_default_line_color(self.linecolor)
+
+    def set_default_network_color(self, color):
+        color = images.color_to_rgb(color)
+        self._Network.set_default_color(color)
+
+    def __init__(self):
+        super(Diagram, self).__init__()
+
+        self.orientation = 'portrait'
+        self.node_width = 104
+        self.span_width = 48
+        self.span_height = 104
+        self.groups = []
+        self.networks = []
+
+    def fixiate(self):
+        if len(self.nodes) + len(self.networks) > 0:
+            nodes = self.nodes + self.networks
+            self.width = max(x.xy.x + x.width for x in nodes)
+            self.height = max(x.xy.y + x.height for x in nodes)
