3a4aba9e964edcdbd97687d1c489e052f4f7f978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:09 +0000

    Add package django-imagekit/0.4.1/django-imagekit-0.4.1.tar.gz

---
+++ packages/django-imagekit/django-imagekit-0.4.1.tar.gz/django-imagekit-0.4.1/imagekit/__init__.py
@@ -0,0 +1,13 @@
+"""
+Django ImageKit
+
+:author: Justin Driscoll <justin.driscoll@gmail.com>
+:maintainer: Bryan Veloso <bryan@revyver.com>
+:license: BSD
+
+"""
+__title__ = 'django-imagekit'
+__version__ = '0.4.1'
+__build__ = 0x000400
+__author__ = 'Justin Driscoll, Bryan Veloso, Greg Newman, Chris Drackett'
+__license__ = 'BSD'
