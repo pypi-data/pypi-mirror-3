70ddd68f38db42f721a3045884e6bd991b5f273a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:20 +0000

    Add package python-apply-linkedin/0.1.1/python-apply-linkedin-0.1.1.zip

---
+++ packages/python-apply-linkedin/python-apply-linkedin-0.1.1.zip/python-apply-linkedin-0.1.1/apply_linkedin/utils.py
@@ -0,0 +1,40 @@
+from datetime import date
+
+
+def parse_meta(meta):
+    dict_meta = {}
+    if meta == 'Apply with LinkedIn':
+        return dict_meta
+    list_meta = meta.split(',')
+    for item in list_meta:
+        key, value = item.split(':')
+        dict_meta[key] = value
+    return dict_meta
+
+
+def parse_date(value):
+    try:
+        day = int(value.get('day', 1))
+        month = int(value.get('month', 1))
+        year = int(value['year'])
+    except:
+        return None
+    else:
+        return date(year, month, day)
+    
+    
+def _import_simplejson():
+    try:
+        import simplejson as json
+    except ImportError:
+        try:
+            import json  # Python 2.6+
+        except ImportError:
+            try:
+                from django.utils import simplejson as json  # Google App Engine
+            except ImportError:
+                raise ImportError, "Can't load a json library"
+
+    return json
+
+simplejson = _import_simplejson()
---
+++ packages/python-apply-linkedin/python-apply-linkedin-0.1.1.zip/python-apply-linkedin-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-apply-linkedin',
+    version='0.1.1',
+    description='library that made easy implement Apply with Linkedin functionality',
+    author='Reiner Marquez',
+    author_email='rmaceissoft@gmail.com',
+    url='https://github.com/rmaceissoft/python-apply-linkedin',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'docs']),
+    classifiers=["Environment :: Web Environment",
+                 'Intended Audience :: Developers',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Utilities'],
+)      
