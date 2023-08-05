c47c55334fa90b2f077c70ef8abae5f19f26f647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:38 +0000

    Add package Anika/1.0.0/Anika-1.0.0.tar.gz

---
+++ packages/Anika/Anika-1.0.0.tar.gz/Anika-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+		name 		= 'Anika',
+		version 	= '1.0.0',
+		py_modules 	= ['Anika'],
+		author 		= 'saikat',
+		author_email 	= 'saikatz@gmail.com',
+		
+
+        )
+    
