108d066d05e0fc0f761bffcd6e407a6136bcf2c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:00 +0000

    Add package proxiedssl/0.1.1/proxiedssl-0.1.1-py2.6.egg

---
+++ packages/proxiedssl/proxiedssl-0.1.1-py2.6.egg/proxiedssl/middleware.py
@@ -0,0 +1,23 @@
+from logging import getLogger
+
+
+
+log = getLogger('proxiedssl')
+
+class ProxiedSslWsgiMiddleware(object):
+    def process_request(self, request):
+        if 'HTTP_X_URL_SCHEME' in request.META:
+            log.debug('setting wsgi.url_scheme to %s' % 
+                      (request.META['HTTP_X_URL_SCHEME'].lower(),))
+            request.environ['wsgi.url_scheme'] = \
+                request.META['HTTP_X_URL_SCHEME'].lower()
+        elif 'HTTP_X_FORWARDED_PROTOCOL' in request.META:
+            log.debug('setting wsgi.url_scheme to %s' % 
+                      (request.META['HTTP_X_FORWARDED_PROTOCOL'].lower(),))
+            request.environ['wsgi.url_scheme'] = \
+                request.META['HTTP_X_FORWARDED_PROTOCOL'].lower()
+        else:
+            log.debug('not setting wsgi.url_scheme: request.META=%s' % 
+                      (unicode(request.META),))
+            return
+        log.debug('request.is_secure = %s' % (request.is_secure(),))
