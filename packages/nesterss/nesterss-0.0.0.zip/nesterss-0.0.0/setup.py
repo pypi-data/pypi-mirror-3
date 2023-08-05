dcc995025b7f6178a5589162b7515a3e73ff6b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:22 +0000

    Add package nesterss/0.0.0/nesterss-0.0.0.zip

---
+++ packages/nesterss/nesterss-0.0.0.zip/nesterss-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name ='nesterss',
+        verision ='1.0.0',
+        py_modules =['nesterss'],
+        author = 'dokelung',
+        author_email = 'dokelung@gmail.com',
+        description = 'A simple function'
+     )
