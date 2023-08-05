ce4ae30f8f25024485e9c1a44fea19e84b0aff24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:17 +0000

    Add package collective.wtforms/1.0a2/collective.wtforms-1.0a2.zip

---
+++ packages/collective.wtforms/collective.wtforms-1.0a2.zip/collective.wtforms-1.0a2/collective/wtforms/views.py
@@ -0,0 +1,124 @@
+from zope.component import getMultiAdapter
+from AccessControl import Unauthorized
+from Products.Five import BrowserView
+from plone.memoize.view import memoize
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class PostData(dict):
+    """
+    The form wants the getlist method - no problem.
+    """
+    def getlist(self, key):
+        v = self[key]
+        if not isinstance(v, (list, tuple)):
+            v = [v]
+        return v
+
+
+class WTFormView(BrowserView):
+    formClass = None
+    label = 'Form Title'
+    description = 'Form Description'
+    prefix = 'wtform'
+    buttonPrefix = 'form.actions.'
+    wrapWithFieldset = True
+    csrfProtect = True
+    buttons = (
+        'Save',
+        'Cancel'
+    )
+
+    fieldsets = ()
+
+    index = ViewPageTemplateFile('basicform.pt')
+    fieldTemplate = ViewPageTemplateFile('field.pt')
+    formTemplate = ViewPageTemplateFile('form.pt')
+
+    def viewname(self):
+        return self.__name__
+
+    def fixName(self, name):
+        return name.replace(' ', '').replace(',', '')
+
+    def mungeForm(self, form):
+        pass
+
+    def getButtonName(self, button):
+        return '%s%s' % (self.buttonPrefix, self.fixName(button))
+
+    @property
+    def submitted(self):
+        return self.request.get('REQUEST_METHOD') == 'POST' and \
+            self.request.get('form.submitted', 'false') == 'true'
+
+    @property
+    def data(self):
+        return {}
+
+    @property
+    @memoize
+    def form(self):
+        if not self.submitted:
+            data = self.data
+            for key, value in data.items():
+                data[self.prefix + '-' + key] = value
+                del data[key]
+        else:
+            data = {}
+        data.update(self.request.form)
+        form = self.formClass(PostData(data), prefix=self.prefix)
+        self.mungeForm(form)
+        return form
+
+    def renderField(self, field):
+        return self.fieldTemplate(field=field)
+
+    def renderForm(self):
+        return self.formTemplate()
+
+    def validate(self):
+        if self.csrfProtect:
+            authenticator = getMultiAdapter((self.context, self.request),
+                                            name=u"authenticator")
+            if not authenticator.verify():
+                raise Unauthorized
+        return self.form.validate()
+
+    def submit(self, button=None):
+        if button == 'Save':
+            if self.validate():
+                self.request.response.redirect(self.context.absolute_url())
+                return 1
+        elif button == 'Cancel':
+            self.request.response.redirect(self.context.absolute_url())
+            return 1
+
+    def __call__(self):
+        if self.submitted:
+            for button in self.buttons:
+                if button == self.request.get(self.getButtonName(button)):
+                    result = self.submit(button)
+                    if result:
+                        return result
+                    else:
+                        break
+        return self.index()
+
+    def iterfieldsets(self):
+        form = self.form
+        if self.fieldsets:
+            for fieldset, fieldnames in self.fieldsets:
+                fields = []
+                for name in fieldnames:
+                    field = getattr(form, name, None)
+                    if field:
+                        fields.append(field)
+                yield fieldset, fields
+        else:
+            yield self.label, [f for f in form]
+
+
+class WTFormControlPanelView(WTFormView):
+    index = ViewPageTemplateFile('controlpanelform.pt')
+    status = None
