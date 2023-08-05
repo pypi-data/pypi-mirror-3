355d67f5f0583f256db79988dcdf5e3af522c6d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:31 +0000

    Add package quick_orm/0.2.1/quick_orm-0.2.1.tar.gz

---
+++ packages/quick_orm/quick_orm-0.2.1.tar.gz/quick_orm-0.2.1/quick_orm/examples/__init__.py
@@ -0,0 +1,18 @@
+examples_list = [
+    'hello_world',
+    'foreign_key',
+    'many_to_many',
+    'raw_sql',
+    'multiple_databases',
+    'table_inheritance',
+    'write_less_do_more',
+]
+examples_dict = {
+    'hello_world': 'Hello World',
+    'foreign_key': 'Foreign Key',
+    'many_to_many': 'Many-to-many relationship',
+    'raw_sql': 'Performing raw sql query',
+    'multiple_databases': 'Multiple databases',
+    'table_inheritance': 'Table inheritance',
+    'write_less_do_more': 'Write less, do more',    
+}
---
+++ packages/quick_orm/quick_orm-0.2.1.tar.gz/quick_orm-0.2.1/quick_orm/examples/write_less_do_more.py
@@ -0,0 +1,25 @@
+from quick_orm.core import Database
+from sqlalchemy import Column, String, DateTime, func
+
+class DefaultModel(object):
+    name = Column(String(70))
+    created = Column(DateTime, default = func.now(), nullable = False)
+
+metaclass = Database.MetaBuilder(DefaultModel)
+
+class User(object):
+    __metaclass__ = metaclass
+
+class Group(object):
+    __metaclass__ = metaclass
+
+if __name__ == '__main__':
+    database = Database('sqlite://')
+    database.create_tables()
+    user = User(name = 'tylerlong')
+    database.session.add(user)
+    group = Group(name = 'python')
+    database.session.add_then_commit(group)
+
+    print user.name, user.created
+    print group.name, group.created
