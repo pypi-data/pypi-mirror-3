961aa451a8faca59c77ce5f66afa2f66a9d51b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:45 +0000

    Add package makesite/0.7.34/makesite-0.7.34.tar.gz

---
+++ packages/makesite/makesite-0.7.34.tar.gz/makesite-0.7.34/makesite/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+version_info = (0, 7, 34)
+
+__version__ = version = '.'.join(map(str, version_info))
+__project__ = PROJECT = __name__
+__author__ = AUTHOR = "Kirill Klenov <horneds@gmail.com>"
+__license__ = LICENSE = "GNU LGPL"
+
+INI_FILENAME = "makesite.ini"
+TEMPLATES_FILE = '.makesite'
