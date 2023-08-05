3decd594383d15274a4d19f92a50fbd8276450d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:00 +0000

    Add package puke/1.3.7/puke-1.3.7.tar.gz

---
+++ packages/puke/puke-1.3.7.tar.gz/puke-1.3.7/puke/Yak.py
@@ -0,0 +1,11 @@
+class Yak(dict):
+
+	class __metaclass__(type):
+		def __iter__(self):
+			for attr in dir(Yak):
+				if not attr.startswith("__"):
+					yield attr
+	@staticmethod
+	def set(key, value):
+		setattr(Yak, key, value)
+
