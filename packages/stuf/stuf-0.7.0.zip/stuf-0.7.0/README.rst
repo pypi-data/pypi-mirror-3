f1ed8e645c6c057dc7ee64d2ba8ef8adccb26e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:03 +0000

    Add package stuf/0.7.0/stuf-0.7.0.zip

---
+++ packages/stuf/stuf-0.7.0.zip/stuf-0.7.0/README.rst
@@ -0,0 +1,2 @@
+A collection of Python dictionary that support attribute-style access. Includes  
+*defaultdict*,  *OrderedDict*, restricted, and frozen implementations.
---
+++ packages/stuf/stuf-0.7.0.zip/stuf-0.7.0/stuf/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from .core import defaultstuf, fixedstuf, frozenstuf, orderedstuf, stuf
+
+idefaultstuf = defaultstuf
+ifixedstuf = fixedstuf
+ifrozenstuf = frozenstuf
+iorderedstuf = orderedstuf
+istuf = stuf
