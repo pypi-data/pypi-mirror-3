0a9dc912625b092ea0aa17ba16901a19ca9871ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:38 +0000

    Add package pytest-xdist/1.7/pytest-xdist-1.7.zip

---
+++ packages/pytest-xdist/pytest-xdist-1.7.zip/pytest-xdist-1.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name="pytest-xdist",
+    version='1.7',
+    description='py.test xdist plugin for distributed testing and loop-on-failing modes',
+    long_description=open('README.txt').read(),
+    license='GPLv2 or later',
+    author='holger krekel and contributors',
+    author_email='py-dev@codespeak.net,holger@merlinux.eu',
+    url='http://bitbucket.org/hpk42/pytest-xdist',
+    platforms=['linux', 'osx', 'win32'],
+    packages = ['xdist'],
+    entry_points = {'pytest11': ['xdist = xdist.plugin'],},
+    zip_safe=False,
+    install_requires = ['execnet>=1.0.8', 'pytest>=2.2.0'],
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: POSIX',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: MacOS :: MacOS X',
+    'Topic :: Software Development :: Testing',
+    'Topic :: Software Development :: Quality Assurance',
+    'Topic :: Utilities',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    ],
+)
