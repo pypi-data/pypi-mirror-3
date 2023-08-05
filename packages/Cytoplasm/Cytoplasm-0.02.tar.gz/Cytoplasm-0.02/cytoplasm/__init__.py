961c76d1d8e388910d4adfb0d004bdaa92317278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:49 +0000

    Add package Cytoplasm/0.02/Cytoplasm-0.02.tar.gz

---
+++ packages/Cytoplasm/Cytoplasm-0.02.tar.gz/Cytoplasm-0.02/cytoplasm/__init__.py
@@ -0,0 +1,2 @@
+from .build import build, copy_over
+from . import errors, interpreters, configuration, defaults
---
+++ packages/Cytoplasm/Cytoplasm-0.02.tar.gz/Cytoplasm-0.02/cytoplasm/controllers.py
@@ -0,0 +1,25 @@
+import imp, os
+
+def controllerclass(name):
+    "Given the name of a controller's module, return that controller's class."
+    fp, pathname, description = imp.find_module(name, ["_controllers/"])
+    try:
+        controller = imp.load_module(name, fp, pathname, description)
+        return controller.info["class"]
+    finally:
+        if fp != None: fp.close
+
+class Controller(object):
+    "Controllers take data from some files, do stuff with it, and write it to the build directory."
+    def __init__(self, data, destination, templates="_templates"):
+        # take three arguments: the source directory, the destination directory, and, optionally,
+        # a directory wherein the templates reside.
+        self.data_directory = data
+        self.destination_directory = destination
+        self.templates_directory = templates
+        # create the destination directory, if it doesn't exist
+        if not os.path.exists(self.destination_directory): os.mkdir(self.destination_directory)
+    
+    def __call__(self):
+        # do whatever needs to be done here...
+        pass
