5c934aed00759e24fe020577a44bc04e018ed5fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:03 +0000

    Add package pycket/0.1.3/pycket-0.1.3.tar.gz

---
+++ packages/pycket/pycket-0.1.3.tar.gz/pycket-0.1.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.3'
+
+setup(name='pycket',
+      version=version,
+      description="Redis sessions for Tornado",
+      long_description="Redis sessions for Tornado (see GitHub page for more info)",
+      classifiers=[
+          'Topic :: Internet :: WWW/HTTP :: Session',
+          'Topic :: Database',
+          ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='pycket redis tornado session python',
+      author='Diogo Baeder',
+      author_email='desenvolvedor@diogobaeder.com.br',
+      url='https://github.com/diogobaeder/pycket',
+      license='BSD 2-Clause',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'redis',
+          'tornado',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      test_suite="nose.collector",
+      tests_require="nose",
+)
