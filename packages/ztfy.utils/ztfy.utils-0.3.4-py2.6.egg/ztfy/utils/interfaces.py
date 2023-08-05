d761a6194fa4756decb83936909709e206b5d837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:33 +0000

    Add package ztfy.utils/0.3.4/ztfy.utils-0.3.4-py2.6.egg

---
+++ packages/ztfy.utils/ztfy.utils-0.3.4-py2.6.egg/ztfy/utils/interfaces.py
@@ -0,0 +1,31 @@
+### -*- coding: utf-8 -*- ####################################################
+##############################################################################
+#
+# Copyright (c) 2008-2010 Thierry Florac <tflorac AT ulthar.net>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+__docformat__ = "restructuredtext"
+
+# import standard packages
+
+# import Zope3 interfaces
+from zope.component.interfaces import IObjectEvent
+
+# import local interfaces
+
+# import Zope3 packages
+
+# import local packages
+
+
+class INewSiteManagerEvent(IObjectEvent):
+    """Event interface for new site manager event"""
