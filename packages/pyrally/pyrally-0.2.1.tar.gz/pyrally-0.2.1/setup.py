8f94a456483370e300f720d3942443ee4137102d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:59 +0000

    Add package pyrally/0.2.1/pyrally-0.2.1.tar.gz

---
+++ packages/pyrally/pyrally-0.2.1.tar.gz/pyrally-0.2.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# # coding: utf-8
+
+from setuptools import setup
+long_description = open('README').read()
+
+
+setup(
+    name='pyrally',
+    description='Rally API Client',
+    long_description=long_description,
+    version='0.2.1',
+    author='Alex Couper',
+    author_email='amcouper@gmail.com',
+    url='https://github.com/alexcouper/pyrally',
+    packages=['pyrally'],
+    install_requires=['simplejson'],
+    scripts=[
+    ],
+    package_data={
+        '': ['*.txt', '*.rst'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
