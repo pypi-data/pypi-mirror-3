1288b0c5939f97d233702b66a86390aa1072c933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:04 +0000

    Add package nashvegas/0.7/nashvegas-0.7.tar.gz

---
+++ packages/nashvegas/nashvegas-0.7.tar.gz/nashvegas-0.7/nashvegas/tests.py
@@ -0,0 +1,8 @@
+"""
+Things to test:
+
+For Postgresql and MySQL:
+
+1. Given a set of new models generate SQL as expected
+2. Given a set of migration files (sql and py) execute them in order (no need to actually execute them during test)
+"""
