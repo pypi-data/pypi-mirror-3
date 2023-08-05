9dee18876525a35ad5fcc4a5e59c3b17253562f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:40 +0000

    Add package pymogile/2.0.2/pymogile-2.0.2.tar.gz

---
+++ packages/pymogile/pymogile-2.0.2.tar.gz/pymogile-2.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pymogile',
+    version='2.0.2',
+    description="pymogile",
+    long_description="""Python MogileFS Client""",
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    author='AloneRoad',
+    author_email='aloneroad@gmail.com',
+    maintainer='stefanfoulis',
+    maintainer_email='stefan.foulis@gmail.com',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False
+)
