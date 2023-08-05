379a4e39eca4165dcf36834efe4c6639261ec207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:00 +0000

    Add package zest.emailhider/2.6/zest.emailhider-2.6.tar.gz

---
+++ packages/zest.emailhider/zest.emailhider-2.6.tar.gz/zest.emailhider-2.6/README.txt
@@ -0,0 +1,1 @@
+See zest/emailhider/README.txt
---
+++ packages/zest.emailhider/zest.emailhider-2.6.tar.gz/zest.emailhider-2.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = open(os.path.join(
+        'zest', 'emailhider', "version.txt")).read().strip()
+
+setup(name='zest.emailhider',
+      version=version,
+      description=("A simple jQuery component for hiding email addresses from "
+                   "spammers."),
+      long_description=(
+        open(os.path.join('zest', 'emailhider', "README.txt")).read()
+        + "\n" +
+        open("CHANGES.txt").read()),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='zestsoftware email spamprotection javascript',
+      author='Zest Software',
+      author_email='s.hildebrandt@zestsoftware.nl',
+      url='http://zestsoftware.nl',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['zest'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'jquery.pyproxy>=0.3.1'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
