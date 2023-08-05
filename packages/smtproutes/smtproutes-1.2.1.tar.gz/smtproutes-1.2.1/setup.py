c04f69ef1c6363ad15f49a304dedef63696a1746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:40 +0000

    Add package smtproutes/1.2.1/smtproutes-1.2.1.tar.gz

---
+++ packages/smtproutes/smtproutes-1.2.1.tar.gz/smtproutes-1.2.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name="smtproutes",
+    version="1.2.1",
+    description="A simple, Sinatra inspired, SMTP routing server.",
+    author="Benjamin Coe",
+    author_email="bencoe@gmail.com",
+    url="https://github.com/bcoe/smtproutes",
+    packages = find_packages(),
+    install_requires = [
+        'secure-smtpd>=1.1.3',
+        'pydkim',
+        'dnspython',
+        'pyspf',
+        'pydns'
+    ],
+    tests_require=[
+        'nose'
+    ]
+)
