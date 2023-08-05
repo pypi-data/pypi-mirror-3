d30e1fa2c1023502ae0c2aefe68f2b447b74dfd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:53 +0000

    Add package ngblog/0.1/ngblog-0.1.tar.gz

---
+++ packages/ngblog/ngblog-0.1.tar.gz/ngblog-0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import ngblog
+
+setup(
+    name='ngblog',
+    version=ngblog.__version__,
+    license='BSD',
+    description='A simple and easy to use plain-text-like blogging-engine.',
+    long_description=open('README').read(),
+    author='Nerd Gordo',
+    author_email='nerdgordo@nerdgordo.com',
+    url='http://nerdgordo.com/git/?p=ngblog.git',
+    py_modules=['ngblog'],
+    install_requires=['argparse', 'Werkzeug'],
+    entry_points={'console_scripts': ['ngblog = ngblog:main']},
+)
