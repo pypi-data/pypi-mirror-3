b9a7f3550f5a5f8b69ded79697b8c8a38ddca57b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:59 +0000

    Add package pybloomfiltermmap/0.3.1/pybloomfiltermmap-0.3.1.tar.gz

---
+++ packages/pybloomfiltermmap/pybloomfiltermmap-0.3.1.tar.gz/pybloomfiltermmap-0.3.1/setup.py
@@ -0,0 +1,43 @@
+from distutils.core import setup
+from distutils.extension import Extension
+
+ext_files = ["src/mmapbitarray.c",
+             "src/bloomfilter.c",
+             "src/md5.c",
+             "src/primetester.c",
+             ]
+
+kwargs = {}
+
+try:
+    from Cython.Distutils import build_ext
+    print "Building from Cython"
+    ext_files.append("src/pybloomfilter.pyx")
+    kwargs['cmdclass'] = {'build_ext': build_ext}
+except ImportError:
+    ext_files.append("src/pybloomfilter.c")
+    print "Building from C"
+
+ext_modules = [Extension("pybloomfilter",
+                         ext_files)]
+
+setup(
+  name = 'pybloomfiltermmap',
+  version = "0.3.1",
+  author = "Michael Axiak, Rob Stacey",
+  author_email = "mike@axiak.net",
+  url = "http://github.com/axiak/pybloomfiltermmap/",
+  description = "A Bloom filter (bloomfilter) for Python built on mmap",
+  license = "MIT License",
+  ext_modules = ext_modules,
+  classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: C',
+        'Programming Language :: Cython',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+  **kwargs
+)
+ 
