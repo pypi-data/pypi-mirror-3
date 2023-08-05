77b50f1c21b5503f5e02929996a53d3b84485486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:21 +0000

    Add package cl/0.0.3/cl-0.0.3.tar.gz

---
+++ packages/cl/cl-0.0.3.tar.gz/cl-0.0.3/examples/hello.py
@@ -0,0 +1,31 @@
+from cl import Actor, Agent
+
+from kombu import Connection
+
+connection = Connection()
+
+
+class GreetingActor(Actor):
+    default_routing_key = "GreetingActor"
+
+    class state:
+
+        def greet(self, who="world"):
+            return "Hello %s" % who
+greeting = GreetingActor(connection)
+
+
+class GreetingAgent(Agent):
+    actors = [greeting]
+
+
+if __name__ == "__main__":
+    GreetingAgent(connection).run_from_commandline()
+
+
+# Run this script from the command line and try this
+# in another console:
+#
+#   >>> from hello import greeting
+#   >>> greeting.call("greet")
+#   "Hello world"
