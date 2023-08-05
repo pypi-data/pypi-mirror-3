918991c45d8e55b69101cfc113cfa56e8579bc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:31 +0000

    Add package neo4django/0.1.5/neo4django-0.1.5.tar.gz

---
+++ packages/neo4django/neo4django-0.1.5.tar.gz/neo4django-0.1.5/neo4django/constants.py
@@ -0,0 +1,10 @@
+INTERNAL_ATTR='_neo4django'
+
+#node-related constants
+TYPE_ATTR=INTERNAL_ATTR + '_type'
+
+#relationship-related constants
+ORDER_ATTR=INTERNAL_ATTR + '_order'
+
+#auto-property index suffix
+AUTO_PROP_INDEX_VALUE='LAST_AUTO_VALUE'
---
+++ packages/neo4django/neo4django-0.1.5.tar.gz/neo4django-0.1.5/neo4django/tests/test_settings.py
@@ -0,0 +1,13 @@
+NEO4J_DATABASES = {
+    'default' : {
+        'HOST':'localhost',
+        'PORT':7474,
+        'ENDPOINT':'/db/data'
+    }
+}
+
+DEBUG = True
+
+NEO4DJANGO_PROFILE_REQUESTS = False
+
+NEO4J_DELETE_KEY = 'supersecretdebugkey!'
