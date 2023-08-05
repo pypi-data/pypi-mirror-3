1b44964e11ce154b6c0cf83dbbedb80819631ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:14 +0000

    Add package djc.recipe/0.8/djc.recipe-0.8.tar.gz

---
+++ packages/djc.recipe/djc.recipe-0.8.tar.gz/djc.recipe-0.8/djc/recipe/manage.py
@@ -0,0 +1,24 @@
+import imp
+from django.core.management import setup_environ, ManagementUtility
+
+def main(settings_file):
+    import sys
+    try:
+        imp.acquire_lock()
+        mod = imp.load_source('_django_settings', settings_file)
+    except Exception, e:
+        imp.release_lock()
+        sys.stderr.write("Error loading the settings module '%s': %s"
+                            % (settings_file, e))
+
+        import traceback
+        traceback.print_exc()
+
+        return sys.exit(1)
+    
+    imp.release_lock()
+
+    setup_environ(mod, '_django_settings')
+    utility = ManagementUtility(sys.argv)
+    utility.execute()
+    
