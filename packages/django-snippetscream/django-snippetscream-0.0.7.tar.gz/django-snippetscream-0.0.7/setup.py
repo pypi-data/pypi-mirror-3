93e93e945c96d64bfa15fdaa93e3c038c7ff6af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:35 +0000

    Add package django-snippetscream/0.0.7/django-snippetscream-0.0.7.tar.gz

---
+++ packages/django-snippetscream/django-snippetscream-0.0.7.tar.gz/django-snippetscream-0.0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-snippetscream',
+    version='0.0.7',
+    description='Django app packaging the best snippets found on http://djangosnippets.org',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Shaun Sephton',
+    author_email='shaunsephton@gmail.com',
+    url='http://github.com/shaunsephton/django-snippetscream',
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite="setuptest.SetupTestSuite",
+    tests_require=[
+        'django-setuptest>=0.0.6',
+    ],
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
