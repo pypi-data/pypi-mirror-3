1c5a9b9ad95c562983b2ec4506a284371791275f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:44 +0000

    Add package tweeql/0.1.8/tweeql-0.1.8.tar.gz

---
+++ packages/tweeql/tweeql-0.1.8.tar.gz/tweeql-0.1.8/tweeql/bin/tweeql-command-line.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from tweeql.exceptions import TweeQLException
+from tweeql.query_runner import QueryRunner
+
+import settings
+import traceback
+
+def main():
+    runner = QueryRunner()
+    try:
+        while True:
+            cmd = raw_input("tweeql> ");
+            process_command(runner, cmd)
+    except KeyboardInterrupt:
+        print '\nGoodbye!'
+
+def process_command(runner, cmd):
+    try:
+        runner.run_query(cmd, False)
+    except KeyboardInterrupt:
+        runner.stop_query()
+    except TweeQLException, e:
+        runner.stop_query()
+        if settings.DEBUG:
+            traceback.print_exc()
+        else:
+            print e
+
+if __name__ == '__main__':
+    main()
