cf2c57affd9ed515934da3fc19ce808a71beae7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:37 +0000

    Add package glglue/0.1.1/glglue-0.1.1.zip

---
+++ packages/glglue/glglue-0.1.1.zip/glglue-0.1.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+import sys
+
+setup(
+        name='glglue',
+        version='0.1.1',
+        description='The glue code which mediates between OpenGL and some GUI',
+        long_description=open('README.rst').read(),
+        classifiers=[
+            'Programming Language :: Python :: 2',
+            'License :: OSI Approved :: zlib/libpng License',
+            'Topic :: Multimedia :: Graphics :: 3D Modeling',
+            ],
+        keywords=['opengl'],
+        author='ousttrue',
+        author_email='ousttrue@gmail.com',
+        license='zlib',
+        packages=find_packages(),
+        test_suite='nose.collector',
+        tests_require=['Nose'],
+        )
+
