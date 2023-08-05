3cc3157272d2d378d9cfa40d19b232daa976952d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:13 +0000

    Add package evasion-agency/1.1.3/evasion_agency-1.1.3-py2.7.egg

---
+++ packages/evasion-agency/evasion_agency-1.1.3-py2.7.egg/evasion/agency/__init__.py
@@ -0,0 +1,46 @@
+"""
+:mod:`agency` 
+==============
+
+.. module:: 'agency'
+.. moduleauthor:: Oisin Mulvihill <oisin.mulvihill@gmail.com>
+
+This `agency.agency` module provides the single manager instance used to store the Agent instances. 
+This module also provides the AGENT_CATEGORIES. These categories are used to group agents of common 
+functionality together. There is no rigid enforcement of which agent belongs where. They are only 
+used as a convention. These categories are the strings that are used in the class field in the 
+configuration.
+
+.. data:: agency.agency.AGENT_CATEGORIES
+
+.. autoclass:: agency.agency.Nodes
+   :members:
+   :undoc-members:
+   
+.. autofunction::  agency.agency.shutdown()   
+
+.. automodule:: agency.agency
+
+.. automodule:: agency.agent
+
+.. automodule:: agency.agents
+
+.. automodule:: agency.config
+
+.. automodule:: agency.manager
+
+"""
+import logging
+
+import agent
+import agency
+import config
+import agents
+import manager
+from agency import *
+from config import ConfigError
+from manager import ManagerError
+
+
+def get_log():
+    return logging.getLogger('agency')
