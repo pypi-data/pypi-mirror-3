e906a8a39487205d68269fccb3bcca8bd4af0974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:17 +0000

    Add package pyramid-fanstatic/0.2/pyramid_fanstatic-0.2.tar.gz

---
+++ packages/pyramid-fanstatic/pyramid_fanstatic-0.2.tar.gz/pyramid_fanstatic-0.2/pyramid_fanstatic/__init__.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from fanstatic.config import convert_config
+from fanstatic.publisher import Publisher
+import fanstatic
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def fanstatic_config(config, prefix='fanstatic.'):
+    cfg = {'publisher_signature': fanstatic.DEFAULT_SIGNATURE}
+    for k, v in config.items():
+        if k.startswith(prefix):
+            cfg[k[len(prefix):]] = v
+    return convert_config(cfg)
+
+
+class Tween(object):
+    def __init__(self, handler, config):
+        self.config = fanstatic_config(config)
+        self.handler = handler
+        self.publisher = Publisher(fanstatic.get_library_registry())
+        self.publisher_signature = self.config.get('publisher_signature')
+        self.trigger = '/%s/' % self.publisher_signature
+
+    def __call__(self, request):
+
+        # publisher
+        if len(request.path_info.split(self.trigger)) > 1:
+            path_info = request.path_info
+            ignored = request.path_info_pop()
+            while ignored != self.publisher_signature:
+                ignored = request.path_info_pop()
+            response = request.get_response(self.publisher)
+            # forward to handler if the resource could not be found
+            if response.status_int == 404:
+                request.path_info = path_info
+                return self.handler(request)
+            return response
+
+        # injector
+        needed = fanstatic.init_needed(**self.config)
+        request.environ[fanstatic.NEEDED] = needed
+
+        response = self.handler(request)
+
+        if not (response.content_type and
+                response.content_type.lower() in ['text/html',
+                                                  'text/xml']):
+            fanstatic.del_needed()
+            return response
+
+        if needed.has_resources():
+            result = needed.render_topbottom_into_html(response.body)
+            response.body = ''
+            response.write(result)
+        fanstatic.del_needed()
+        return response
+
+
+def tween_factory(handler, registry):
+    return Tween(handler, registry.settings.copy())
+
+
+def includeme(config):
+    config.add_tween('pyramid_fanstatic.tween_factory')
