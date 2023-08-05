ae41740039c167cd4faa1b6eb3532b6b05a5fb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:15 +0000

    Add package redsolutioncms.django-simple-feedback/0.2.0/redsolutioncms.django-simple-feedback-0.2.0.tar.gz

---
+++ packages/redsolutioncms.django-simple-feedback/redsolutioncms.django-simple-feedback-0.2.0.tar.gz/redsolutioncms.django-simple-feedback-0.2.0/feedback/settings.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+
+DEFAULT_FORM = 'feedback.forms.FeedbackForm'
+
+FEEDBACK_FORM = getattr(settings, 'FEEDBACK_FORM', DEFAULT_FORM)
+
+DIRECT_TO_TEMPLATE = getattr(settings, 'DIRECT_TO_TEMPLATE', True)
+
+# new in 0.1.1
+FEEDBACK_FORMS = getattr(settings, 'FEEDBACK_FORMS', {
+    'default': FEEDBACK_FORM,
+})
