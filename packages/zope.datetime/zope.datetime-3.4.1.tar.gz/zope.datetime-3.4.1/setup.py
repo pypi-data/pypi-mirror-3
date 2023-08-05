6be29c45f784c84e83d4f061967647e34cbcf58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:22 +0000

    Add package zope.datetime/3.4.1/zope.datetime-3.4.1.tar.gz

---
+++ packages/zope.datetime/zope.datetime-3.4.1.tar.gz/zope.datetime-3.4.1/setup.py
@@ -0,0 +1,49 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.datetime package
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.datetime',
+      version='3.4.1',
+      url='http://pypi.python.org/pypi/zope.datetime',
+      license='ZPL 2.1',
+      description='Zope datetime',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=(
+          "Commonly used date and time related utility functions.\n"
+          + '\n' +
+          read('CHANGES.txt')),
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      install_requires=['setuptools'],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
