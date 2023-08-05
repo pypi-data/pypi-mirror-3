f6860f5a24c026df37e242cc8aaec9eb9585ffbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:24 +0000

    Add package nonNewtonianIcoFlux/2.0/nonNewtonianIcoFlux-2.0.tar.gz

---
+++ packages/nonNewtonianIcoFlux/nonNewtonianIcoFlux-2.0.tar.gz/nonNewtonianIcoFlux-2.0/nonNewtonianIcoFlux/__init__.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+#--------------------------------------------------------------------------------------
+## pythonFlu - Python wrapping for OpenFOAM C++ API
+## Copyright (C) 2010- Alexey Petrov
+## Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+## 
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+## 
+## See http://sourceforge.net/projects/pythonflu
+##
+## Author : Alexey PETROV
+##
+
+
+#--------------------------------------------------------------------------------------
+from Foam import FOAM_VERSION, FOAM_REF_VERSION, FOAM_BRANCH_VERSION
+if FOAM_REF_VERSION( ">=", "010600" ):
+    from nonNewtonianIcoFlux.r1_6 import *
+    pass
+   
+   
+#--------------------------------------------------------------------------------------
+if FOAM_BRANCH_VERSION( "dev", ">=", "010600" ):
+    from nonNewtonianIcoFlux.r1_6_dev import *
+    pass
+  
+
+#--------------------------------------------------------------------------------------
+if FOAM_VERSION( ">=", "020000" ):
+    from nonNewtonianIcoFlux.r2_0_0 import *
+    pass
+  
+
+#--------------------------------------------------------------------------------------
+
+def entry_point():
+    try:
+       import sys; argv = sys.argv
+       return main_standalone( len( argv ), argv )
+    except NameError:
+       print
+       print "There is no implementation of the current OpenFOAM version"
+       print
+       pass
+
+
+#--------------------------------------------------------------------------------------
+if __name__ == "__main__" :
+    entry_point()
+    pass
+    
+    
+#--------------------------------------------------------------------------------------
