42de5872a5029e314ad127741845840c51eac52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:47 +0000

    Add package monet.calendar.criteria/0.1.0/monet.calendar.criteria-0.1.0.tar.gz

---
+++ packages/monet.calendar.criteria/monet.calendar.criteria-0.1.0.tar.gz/monet.calendar.criteria-0.1.0/README.txt
@@ -0,0 +1,38 @@
+Introduction
+============
+
+This is an extension part of the `Monet Calendar`__ suite. For a complete overview of the
+project, see the suite page.
+
+__ http://plone.org/products/monet.calendar.star
+
+This product give to Plone two new types of collection criteria, explicitly using features
+given by `monet.calendar.event`__. You will be able to perform Collection search that
+take care of event's filters: weekdays, days to ignore and days to include.
+
+__ http://pypi.python.org/pypi/monet.calendar.event/
+
+Credits
+=======
+  
+Developed with the support of:
+
+* `Rete Civica Mo-Net - Comune di Modena`__
+
+  .. image:: http://www.comune.modena.it/grafica/logoComune/logoComunexweb.jpg 
+     :alt: Comune di Modena - logo
+
+* `Provincia di Ferrara`__
+
+  .. image:: http://people.provincia.fe.it/people/img/logo_provincia.png
+     :alt: Provincia di Ferrara - logo
+
+* `Regione Emilia Romagna`__
+
+All of them supports the `PloneGov initiative`__.
+
+__ http://www.comune.modena.it/
+__ http://www.provincia.fe.it/
+__ http://www.regione.emilia-romagna.it/
+__ http://www.plonegov.it/
+
---
+++ packages/monet.calendar.criteria/monet.calendar.criteria-0.1.0.tar.gz/monet.calendar.criteria-0.1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name='monet.calendar.criteria',
+      version=version,
+      description="Additional Plone collection criteria for Monet Calendar suite",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 3.3",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.it',
+      url='http://plone.org/products/monet.calendar.star',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['monet', 'monet.calendar'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'monet.calendar.event'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
