7a5e7c3d3e56e9f89981a3ad89c75b32948d1bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:03 +0000

    Add package blargs/0.1b/blargs-0.1b.tar.gz

---
+++ packages/blargs/blargs-0.1b.tar.gz/blargs-0.1b/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='blargs',
+      version='0.1b',
+      py_modules=['blargs'],
+      description='Blargs command line parser',
+      author='Karl Gyllstrom',
+      author_email='karl.gyllstrom+blargs@gmail.com',
+      url='https://bitbucket.org/gyllstromk/blargs',
+     )
