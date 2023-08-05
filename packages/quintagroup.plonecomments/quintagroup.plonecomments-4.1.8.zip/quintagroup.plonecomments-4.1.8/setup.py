ce60157e9e147f85181c996260b9c28d93316060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:22 +0000

    Add package quintagroup.plonecomments/4.1.8/quintagroup.plonecomments-4.1.8.zip

---
+++ packages/quintagroup.plonecomments/quintagroup.plonecomments-4.1.8.zip/quintagroup.plonecomments-4.1.8/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '4.1.8'
+
+setup(name='quintagroup.plonecomments',
+      version=version,
+      description="Plone Comments",
+      long_description=open("README.txt").read() + "\n\n" +
+          open(os.path.join("docs", "INSTALL.txt")).read() + "\n\n" +
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme',
+      author='Quintagroup',
+      author_email='support@quintagroup.com',
+      url='http://quintagroup.com/services/plone-development'
+          '/products/plone-comments',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['quintagroup'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.browserlayer',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      """,
+      )
