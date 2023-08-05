986a526f6842b1e9123c1ba918cc41bdeb754f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:29 +0000

    Add package django-test/0.1.1/django-test-0.1.1.tar.gz

---
+++ packages/django-test/django-test-0.1.1.tar.gz/django-test-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='django-test',
+  version='0.1.1',
+  packages=find_packages(),
+  license='GPL',
+  author="Andrew Magee",
+  author_email="amagee@gmail.com",
+  long_description="Miscellaneous app-independent django tools",
+  keywords = "django utils",
+  url = "https://bitbucket.org/amagee/djangoutils",
+)
+
