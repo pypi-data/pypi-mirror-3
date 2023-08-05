0d64aa2d5dddf35c8e355d90f352455df18c2eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:26 +0000

    Add package cilantro/0.9b3/cilantro-0.9b3.tar.gz

---
+++ packages/cilantro/cilantro-0.9b3.tar.gz/cilantro-0.9b3/cilantro/static/js/src/common/models/polling.js
@@ -0,0 +1,53 @@
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+  for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+  function ctor() { this.constructor = child; }
+  ctor.prototype = parent.prototype;
+  child.prototype = new ctor;
+  child.__super__ = parent.prototype;
+  return child;
+};
+define(['common/utils'], function(utils) {
+  var Mixin, PollingCollection, PollingModel;
+  Mixin = {
+    pollInterval: 1000 * 10,
+    initialize: function() {
+      return this.startPolling();
+    },
+    startPolling: function() {
+      return this._pollInterval = setInterval((__bind(function() {
+        return this.poll();
+      }, this)), this.pollInterval);
+    },
+    stopPolling: function() {
+      return clearTimeout(this._pollInterval);
+    }
+  };
+  PollingModel = (function() {
+    __extends(PollingModel, Backbone.Model);
+    function PollingModel() {
+      PollingModel.__super__.constructor.apply(this, arguments);
+    }
+    PollingModel.prototype.poll = function() {
+      return this.fetch();
+    };
+    return PollingModel;
+  })();
+  PollingCollection = (function() {
+    __extends(PollingCollection, Backbone.Collection);
+    function PollingCollection() {
+      PollingCollection.__super__.constructor.apply(this, arguments);
+    }
+    PollingCollection.prototype.poll = function() {
+      return this.fetch({
+        update: true
+      });
+    };
+    return PollingCollection;
+  })();
+  utils.include(PollingModel, Mixin);
+  utils.include(PollingCollection, Mixin);
+  return {
+    Model: PollingModel,
+    Collection: PollingCollection
+  };
+});
