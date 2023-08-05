45b2751f570826c9d55be58a5bea361f3f0826e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:38 +0000

    Add package armstrong.apps.related_content/1.0.1/armstrong.apps.related_content-1.0.1.tar.gz

---
+++ packages/armstrong.apps.related_content/armstrong.apps.related_content-1.0.1.tar.gz/armstrong.apps.related_content-1.0.1/armstrong/apps/related_content/fields.py
@@ -0,0 +1,19 @@
+from django.contrib.contenttypes.generic import GenericRelation
+
+from .models import RelatedContent
+
+
+class RelatedContentField(GenericRelation):
+    def __init__(self, **kwargs):
+        defaults = {
+            "object_id_field": "source_id",
+            "content_type_field": "source_type",
+        }
+        defaults.update(kwargs)
+        super(RelatedContentField, self).__init__(RelatedContent, **defaults)
+
+try:
+    from south.modelsinspector import add_ignored_fields
+    add_ignored_fields(["^armstrong\.apps\.related_content\.fields\.RelatedContentField"])
+except ImportError:
+    pass
