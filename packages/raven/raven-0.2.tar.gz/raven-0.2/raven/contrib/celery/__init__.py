02feab8dde13b5d008e1c8723a61c880694cfa70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package raven/0.2/raven-0.2.tar.gz

---
+++ packages/raven/raven-0.2.tar.gz/raven-0.2/raven/contrib/celery/__init__.py
@@ -0,0 +1,9 @@
+"""
+raven.contrib.celery
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+from raven.contrib.celery.client import CeleryClient
