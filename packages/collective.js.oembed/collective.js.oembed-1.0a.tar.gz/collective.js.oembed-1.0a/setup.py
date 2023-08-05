42d9fea771eb6af1f8c0e601cb99f0ecd1e9d246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:18 +0000

    Add package collective.js.oembed/1.0a/collective.js.oembed-1.0a.tar.gz

---
+++ packages/collective.js.oembed/collective.js.oembed-1.0a.tar.gz/collective.js.oembed-1.0a/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a'
+
+setup(name='collective.js.oembed',
+      version=version,
+      description="JQuery oembed plistred in the Plone resource registry",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        ],
+      keywords='',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='toutpt@gmail.com',
+      url='https://github.com/collective/collective.js.oembed',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
