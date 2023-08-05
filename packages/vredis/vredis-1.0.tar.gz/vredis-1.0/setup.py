19bd7da342f5d317fb1c345fdee6cee61b57b72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:04 +0000

    Add package vredis/1.0/vredis-1.0.tar.gz

---
+++ packages/vredis/vredis-1.0.tar.gz/vredis-1.0/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(name='vredis',
+      version='1.0',
+      description='v-bucket aware redis client',
+      url='http://pypi.python.org/pypi/vredis',
+      author='50onred',
+      author_email='idyedov@50onred.com',
+      py_modules=['vredis'],
+      )
