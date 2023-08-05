ff3f1eec42b997116066b1fe679e1f67805eaefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:20 +0000

    Add package meta/0.2.3/meta-0.2.3.tar.gz

---
+++ packages/meta/meta-0.2.3.tar.gz/meta-0.2.3/setup.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2008-2011 by Enthought, Inc.
+# All rights reserved.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='meta',
+    version='0.2.3',
+    author='Sean Ross-Ross, Enthought Inc.',
+    author_email='srossross@enthought.com',
+    maintainer='Sean Ross-Ross',
+    maintainer_email='enthought-dev@enthought.com',
+    url='http://srossross.github.com/Meta',
+    
+    classifiers=[c.strip() for c in """\
+        Development Status :: 5 - Production/Stable
+        Intended Audience :: Developers
+        License :: OSI Approved :: BSD License
+        Operating System :: MacOS
+        Operating System :: Microsoft :: Windows
+        Operating System :: OS Independent
+        Operating System :: POSIX
+        Operating System :: Unix
+        Programming Language :: Python
+        Topic :: Scientific/Engineering
+        Topic :: Software Development
+        Topic :: Software Development :: Libraries
+        """.splitlines() if len(c.strip()) > 0],
+    description='Byte-code and ast programming tools',
+    include_package_data=True,
+    license='BSD',
+    packages=find_packages(),
+    platforms=["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
+    entry_points = {
+                    'console_scripts': [
+                                        'depyc = meta.scripts.depyc:main',
+                                        ],
+                    }
+
+)
