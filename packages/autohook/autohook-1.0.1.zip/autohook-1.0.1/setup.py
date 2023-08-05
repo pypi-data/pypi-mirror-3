c64fcc2a4e851e435c4676b88cd3109272407fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:49 +0000

    Add package autohook/1.0.1/autohook-1.0.1.zip

---
+++ packages/autohook/autohook-1.0.1.zip/autohook-1.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*- 
+from setuptools import setup
+
+
+setup(
+    name = "autohook",
+    version = "1.0.1",
+    packages = ['autohook'],
+    install_requires = ["hgapi>=1.0.0"],
+    # metadata
+    author = u"Fredrik Håård",
+    author_email = "fredrik@haard.se",
+    description = "Automatic hook configuration for multiple Mercurial repositories",
+    license = "Do whatever you want, don't blame me",
+    keywords = "mercurial hook api",
+    url = "https://bitbucket.org/haard/autohook",
+    long_description = """
+Can be configured in any hgrc (system, user, repo) and trigger for 
+a push (pretxnchangegroup) into any repository that uses that hgrc. 
+Can load external (Python) hooks that needs to accept parameters 
+repo, user, start revison, end revision
+
+Contains two built-in hooks, adds_branch and has_multiple_heads, 
+and users that can bypass hooks can be defined. Depends on hgapi (https://bitbucket.org/haard/hgapi).
+"""
+)
