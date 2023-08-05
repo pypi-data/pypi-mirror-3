b9c3f0a0f7a699388f48743ef0ff14f9ed2b3ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:51 +0000

    Add package goldenarch/0.0.4/goldenarch-0.0.4.tar.gz

---
+++ packages/goldenarch/goldenarch-0.0.4.tar.gz/goldenarch-0.0.4/goldenarch/core.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+goldenarch.py
+~~~~~~~~~~~~~
+
+Serves crap. Fast.
+"""
+
+import os
+import sys
+
+import static
+
+PORT = os.environ.get('PORT', 8000)
+STATIC_DIR =  os.path.abspath(sys.argv[1])
+
+
+app = static.Cling(STATIC_DIR)
+
+
+def cli():
+    print 'Serving crap. Fast.'
+
+    cmd = (
+        'gunicorn goldenarch:app '
+        '-b "0.0.0.0:{port}" '
+        '-w 16 -k gevent -t 2 '
+        '--name goldenarch'
+    ).format(port=PORT)
+
+    os.system(cmd)
+
+
+if __name__ == '__main__':
+    cli()
