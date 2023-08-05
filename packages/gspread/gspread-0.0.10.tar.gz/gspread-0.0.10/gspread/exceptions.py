0f1cf20b49f2eb064dece357e123a5331910dbea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:31 +0000

    Add package gspread/0.0.10/gspread-0.0.10.tar.gz

---
+++ packages/gspread/gspread-0.0.10.tar.gz/gspread-0.0.10/gspread/exceptions.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+gspread.exceptions
+~~~~~~~~~~~~~~~~~~
+
+Exceptions used in gspread.
+
+"""
+
+class GSpreadException(Exception):
+    """A base class for gspread's exceptions"""
+
+class AuthenticationError(GSpreadException):
+    """An error during authentication process"""
+
+class SpreadsheetNotFound(GSpreadException):
+    """Trying to open non-existent or inaccessible spreadsheet"""
+
+class NoValidUrlKeyFound(GSpreadException):
+    """No valid key found in URL"""
+
+class UnsupportedFeedTypeError(GSpreadException):
+    pass
+
+class UrlParameterMissing(GSpreadException):
+    pass
+
+class IncorrectCellLabel(GSpreadException):
+    """The cell label is incorrect"""
