a643ffde30e1ad06d1515aed3056c754bc00cbe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package collective.js.ceebox/1.0.1/collective.js.ceebox-1.0.1.zip

---
+++ packages/collective.js.ceebox/collective.js.ceebox-1.0.1.zip/collective.js.ceebox-1.0.1/collective/js/ceebox/interfaces.py
@@ -0,0 +1,12 @@
+'''
+Created on 27/11/2009
+
+@author: jpg
+'''
+from zope.interface import Interface
+
+class IBrowserLayer(Interface):
+    """
+    A layer specific to this product.
+    Is registered using browserlayer.xml
+    """
---
+++ packages/collective.js.ceebox/collective.js.ceebox-1.0.1.zip/collective.js.ceebox-1.0.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.1'
+
+setup(name='collective.js.ceebox',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Rcom',
+      author_email='info@rcom.com.ar',
+      url='http://svn.plone.org/svn/plone/plone.app.example',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- entry_points -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
