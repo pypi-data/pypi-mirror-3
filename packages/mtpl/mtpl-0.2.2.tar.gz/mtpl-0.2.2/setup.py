7516ae22a7f6afcd102f1f6ce74c8822e1fb0b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:45 +0000

    Add package mtpl/0.2.2/mtpl-0.2.2.tar.gz

---
+++ packages/mtpl/mtpl-0.2.2.tar.gz/mtpl-0.2.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='mtpl',
+      version='0.2.2',
+      description='scripts templates manager that use git',
+      author='Laurent Peuch',
+      long_description=open("README").read(),
+      author_email='cortex@worlddomination.be',
+      url='http://worlddomination.be/projects/mtpl.html',
+      license= 'GPLv3+',
+      # list of scripts supplied by your application
+      scripts=['mtpl'],
+      keywords='templates script cli git',
+     )
+
+# vim:set shiftwidth=4 tabstop=4 expandtab:
