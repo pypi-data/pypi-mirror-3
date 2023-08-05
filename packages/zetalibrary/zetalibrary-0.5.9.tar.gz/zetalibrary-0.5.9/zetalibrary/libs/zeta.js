1621168b8a54e74322fce65cd0407c6df83e03b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:26 +0000

    Add package zetalibrary/0.5.9/zetalibrary-0.5.9.tar.gz

---
+++ packages/zetalibrary/zetalibrary-0.5.9.tar.gz/zetalibrary-0.5.9/zetalibrary/libs/zeta.js
@@ -0,0 +1,59 @@
+/* Zeta is a static framework.
+Version 0.1
+https://github.com/klen/zeta-libs */
+
+require("jquery.js");
+
+
+(function($){
+    
+    // Zeta
+    zeta = window.zeta = {
+
+        blocks: {},
+
+        history: [],
+
+        el: {},
+
+        start: function(context, selector) {
+
+            context = context || document;
+
+            selector = selector || '.zeta';
+
+            $(context).find(selector).each(function(){
+
+                var block = $(this),
+
+                    params = this.onclick ? this.onclick() : {},
+
+                    name = params.name || this.className.split(' ')[0] || '',
+
+                    init = zeta.blocks[name];
+
+                if (init && !block.data(name)) {
+
+                    block
+                        .data(name, true)
+                        .addClass(name + '_js');
+
+                    zeta.el[ name ] = zeta.el[ name ] || [];
+                    zeta.el[ name ].push(block);
+                    init.call(block, params);
+                }
+
+            });
+        }
+    };
+
+    zeta.log = function(){
+        zeta.history.push(arguments);
+        if(window.console) { window.console.log(Array.prototype.slice.call(arguments)); }
+    };
+
+    document.documentElement.className += ' js';
+
+    $(document).ready(function(){ zeta.start(); });
+
+})(jQuery);
