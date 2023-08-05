72bf019c8fcb9f19c79e84d3973ce422054fe2a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:48 +0000

    Add package checkit/0.3.1b/checkit-0.3.1b.zip

---
+++ packages/checkit/checkit-0.3.1b.zip/checkit-0.3.1b/checkit.py
@@ -0,0 +1,29 @@
+"""Checkit is a tool to validate executable specifications
+created with BDD style grammar.
+
+The checkit module is simply a wrapper around nose to discover
+and execute specifications using flexible matching rules.
+"""
+import os
+import sys
+
+
+# these are the options that are passed to nose
+NOSE_OPTIONS = [
+    '-i "([Ss]pec[s]?)(.py)?$"',
+    '-i "^([Dd]escribe)"',
+    '-i "^(it_|should_)"',
+    '--with-doctest'
+]
+
+
+def main():
+    """A simple wrapper that calls nosetests
+    with a regex of keywords to use in discovering specs
+    """
+    command = 'nosetests %s %s' % (' '.join(NOSE_OPTIONS), ' '.join(sys.argv[1:]))
+    return os.system(command)
+
+
+if __name__ == '__main__':
+    main()
