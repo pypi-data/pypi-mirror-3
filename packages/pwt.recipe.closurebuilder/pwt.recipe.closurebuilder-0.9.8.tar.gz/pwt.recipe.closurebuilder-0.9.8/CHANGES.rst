013968243fd181fd06530883f198ed7f2045ed3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:44 +0000

    Add package pwt.recipe.closurebuilder/0.9.8/pwt.recipe.closurebuilder-0.9.8.tar.gz

---
+++ packages/pwt.recipe.closurebuilder/pwt.recipe.closurebuilder-0.9.8.tar.gz/pwt.recipe.closurebuilder-0.9.8/CHANGES.rst
@@ -0,0 +1,49 @@
+=======
+CHANGES
+=======
+
+0.9.8
+-----
+
+- Make the Deps recipe compatible with the Compile recipe by adding in the
+  `filename` option.
+
+0.9.7
+-----
+
+- Update recipe to generate all contents on import so that we can generate
+  configuration details.
+
+0.9.6
+-----
+
+-
+
+0.9.5
+-----
+
+- Update to latest closure compiler.
+
+0.9.4
+-----
+
+- Package the contents correctly.
+
+0.9.3
+-----
+- Update closurebuilder and the closure compiler.
+
+0.9.2
+-----
+
+- Add filename to the Compile recipe options directory. So it can be referenced.
+
+0.9.1
+-----
+
+- Add missing files into the distribution.
+
+0.9
+---
+
+- Initial release.
---
+++ packages/pwt.recipe.closurebuilder/pwt.recipe.closurebuilder-0.9.8.tar.gz/pwt.recipe.closurebuilder-0.9.8/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "pwt.recipe.closurebuilder",
+    version = "0.9.8",
+    author = "Michael Kerrin",
+    author_email = "michael.kerrin@gmail.com",
+
+    description = "zc.buildout recipe for closurebuilder",
+    long_description = open("src/pwt/recipe/closurebuilder/README.txt").read(),
+    url = "https://github.com/mkerrin/pwt.recipe.closurebuilder",
+
+    packages = find_packages("src"),
+    package_dir = {"": "src"},
+    namespace_packages = ["pwt"],
+
+    install_requires = ["setuptools",
+                        "zc.buildout",
+                        ],
+    extras_require = {
+        "test": ["zope.testing"],
+        },
+
+    entry_points = """
+[zc.buildout]
+dependency = pwt.recipe.closurebuilder:Deps
+compile = pwt.recipe.closurebuilder:Compile
+""",
+
+    include_package_data = True,
+    zip_safe = False,
+
+    tests_require = ["zope.testing"],
+    test_suite = "pwt.recipe.closurebuilder.tests.suite",
+    )
