200e1c540a13c8d8cad95e64abdd4edba1add869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:06 +0000

    Add package fastavro/0.1.0/fastavro-0.1.0.tar.gz

---
+++ packages/fastavro/fastavro-0.1.0.tar.gz/fastavro-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fastavro',
+    version='0.1.0',
+    description='Fast iteration of AVRO files',
+    author='Miki Tebeka',
+    author_email='miki.tebeka@gmail.com',
+    license='MIT',
+    url='https://bitbucket.org/tebeka/fastavro',
+    py_modules = ['fastavro'],
+)
