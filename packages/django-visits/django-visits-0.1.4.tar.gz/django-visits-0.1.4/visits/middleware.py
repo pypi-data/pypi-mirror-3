89c23680482b55673915633fdb5b1116b152b8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:56 +0000

    Add package django-visits/0.1.4/django-visits-0.1.4.tar.gz

---
+++ packages/django-visits/django-visits-0.1.4.tar.gz/django-visits-0.1.4/visits/middleware.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from visits import settings
+from visits.models import Visit
+from visits.utils import is_ignored
+
+class CounterMiddleware(object):
+    """ Middleware for count uri visits. """
+
+    def process_request(self, request):
+        if settings.URI_WITH_GET_PARAMS:
+            Visit.objects.add_uri_visit(request, request.get_full_path())
+        else:
+            Visit.objects.add_uri_visit(request, request.path_info)
+
+class BotVisitorMiddleware(object):
+    """ Middleware for count uri visits for bots. """
+
+    def process_request(self, request):
+        user_agent = request.META.get("HTTP_USER_AGENT", None)
+        if user_agent in settings.BOTS_USER_AGENTS:
+            request.META.setdefault("IS_BOT", True)
+        else:
+            request.META.setdefault("IS_BOT", False)
