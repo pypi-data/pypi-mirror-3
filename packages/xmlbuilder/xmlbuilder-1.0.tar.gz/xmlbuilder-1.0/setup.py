2fba2ffe90bce6dbe035ab2be8c08137bdbe44c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:58 +0000

    Add package xmlbuilder/1.0/xmlbuilder-1.0.tar.gz

---
+++ packages/xmlbuilder/xmlbuilder-1.0.tar.gz/xmlbuilder-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xmlbuilder',
+    version='1.0',
+    description="pythonic way to crate xml/(x)html files",
+    author='Kostiantyn Danylov aka koder',
+    author_email='koder.mail@gmail.com',
+    packages=find_packages(),
+    license='LGPL v3',
+    long_description=open('README.txt').read(),
+)
