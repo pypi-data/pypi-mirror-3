eab0f3130e888924922bb4341dd975b8ecc9ccfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:42 +0000

    Add package zope.app.testing/3.10.0/zope.app.testing-3.10.0.tar.gz

---
+++ packages/zope.app.testing/zope.app.testing-3.10.0.tar.gz/zope.app.testing-3.10.0/setup.py
@@ -0,0 +1,89 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.app.testing package
+
+$Id: setup.py 124042 2012-01-13 23:19:15Z jim $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.app.testing',
+      version = '3.10.0',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Zope Application Testing Support',
+      long_description=(
+          read('README.txt')
+          + '\n\n.. contents::\n\n' +
+          read('src', 'zope', 'app', 'testing', 'dochttp.txt')
+          + '\n\n' +
+          read('src', 'zope', 'app', 'testing', 'doctest.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 test testing setup functional",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.app.testing',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require=dict(test=[
+          'ZODB3',
+          'zope.app.zcmlfiles',
+          'zope.login',
+          'zope.publisher >= 3.12',
+          'zope.securitypolicy',
+          ]),
+      install_requires=['setuptools',
+                        'zope.annotation',
+                        'zope.app.appsetup >= 3.11',
+                        'zope.processlifetime',
+                        'zope.app.debug',
+                        'zope.app.dependable',
+                        'zope.app.publication',
+                        # We need zope.component with the hooks module.
+                        'zope.component >= 3.8',
+                        'zope.container',
+                        'zope.i18n',
+                        'zope.interface',
+                        'zope.password',
+                        'zope.publisher',
+                        'zope.schema',
+                        'zope.security',
+                        'zope.site',
+                        'zope.testing',
+                        'zope.testbrowser >= 4',
+                        'zope.traversing',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
---
+++ packages/zope.app.testing/zope.app.testing-3.10.0.tar.gz/zope.app.testing-3.10.0/src/zope/app/testing/xmlrpc.py
@@ -0,0 +1,86 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""XMLRPC testing helpers for Zope 3.
+
+$Id: xmlrpc.py 116141 2010-09-02 15:05:04Z patricks $
+"""
+
+import httplib
+import StringIO
+import xmlrpclib
+
+from zope.app.testing.functional import HTTPCaller
+
+
+class FakeSocket(object):
+
+    def __init__(self, data):
+        self.data = data
+
+    def makefile(self, mode, bufsize=None):
+        return StringIO.StringIO(self.data)
+
+
+class ZopeTestTransport(xmlrpclib.Transport):
+    """xmlrpclib transport that delegates to
+    zope.app.testing.functional.HTTPCaller.
+
+    It can be used like a normal transport, including support for basic
+    authentication.
+    """
+
+    verbose = False
+    handleErrors = True
+
+    def request(self, host, handler, request_body, verbose=0):
+        request = "POST %s HTTP/1.0\n" % (handler,)
+        request += "Content-Length: %i\n" % len(request_body)
+        request += "Content-Type: text/xml\n"
+
+        host, extra_headers, x509 = self.get_host_info(host)
+        if extra_headers:
+            request += "Authorization: %s\n" % (
+                dict(extra_headers)["Authorization"],)
+
+        request += "\n" + request_body
+        response = HTTPCaller()(request, handle_errors=self.handleErrors)
+
+        errcode = response.getStatus()
+        errmsg = response.getStatusString()
+        # This is not the same way that the normal transport deals with the
+        # headers.
+        headers = response.getHeaders()
+
+        if errcode != 200:
+            raise xmlrpclib.ProtocolError(
+                host + handler,
+                errcode, errmsg,
+                headers
+                )
+        res = httplib.HTTPResponse(FakeSocket(response.getBody()))
+        res.begin()
+        return self.parse_response(res)
+
+
+def ServerProxy(uri, transport=None, encoding=None,
+                verbose=0, allow_none=0, handleErrors=True):
+    """A factory that creates a server proxy using the ZopeTestTransport
+    by default.
+
+    """
+    if transport is None:
+        transport = ZopeTestTransport()
+    if isinstance(transport, ZopeTestTransport):
+        transport.handleErrors = handleErrors
+    return xmlrpclib.ServerProxy(uri, transport, encoding, verbose, allow_none)
