7e6773bff17f06f3f35f8208122864831f0da398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:17 +0000

    Add package git-goggles/0.2.12/git-goggles-0.2.12.tar.gz

---
+++ packages/git-goggles/git-goggles-0.2.12.tar.gz/git-goggles-0.2.12/gitgoggles/progress.py
@@ -0,0 +1,44 @@
+import atexit
+import logging
+import StringIO
+import sys
+from gitgoggles.utils import console, force_unicode, force_str
+
+class ProgressStreamHandler(logging.StreamHandler):
+    def __init__(self, *args, **kwargs):
+        self._stdout = sys.stdout
+        self._capture_stdout = StringIO.StringIO()
+        self.spinner = '-\\|/'
+        self.msg = ''
+        self.max_length = 0
+        logging.StreamHandler.__init__(self, *args, **kwargs)
+
+    def capture_stdout(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._capture_stdout
+
+    def uncapture_stdout(self):
+        sys.__stdout__.write(''.ljust(self.max_length))
+        sys.__stdout__.write('\r')
+        sys.stdout = self._stdout
+        console(force_unicode(self._capture_stdout.getvalue()))
+        self._capture_stdout = StringIO.StringIO()
+
+    def emit(self, record):
+        if self.msg != record.msg:
+            self.msg = record.msg
+            msg = ' %s   %s' % (self.spinner[0], self.msg)
+            self.max_length = max(len(msg), self.max_length)
+            self.spinner = self.spinner[1:] + self.spinner[:1]
+            sys.__stdout__.write(force_str(msg.ljust(self.max_length)))
+            sys.__stdout__.write('\r')
+
+handler = ProgressStreamHandler()
+
+def enable_progress():
+    log.addHandler(handler)
+    log.setLevel(logging.INFO)
+    handler.capture_stdout()
+    atexit.register(handler.uncapture_stdout)
+
+log = logging.getLogger('progress')
