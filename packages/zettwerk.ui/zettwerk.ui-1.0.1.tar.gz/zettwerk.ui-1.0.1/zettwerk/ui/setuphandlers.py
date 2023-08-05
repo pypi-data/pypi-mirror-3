a9e47f8f40bb55789160dbba4cd1d93c2cc598df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:31 +0000

    Add package zettwerk.ui/1.0.1/zettwerk.ui-1.0.1.tar.gz

---
+++ packages/zettwerk.ui/zettwerk.ui-1.0.1.tar.gz/zettwerk.ui-1.0.1/zettwerk/ui/setuphandlers.py
@@ -0,0 +1,20 @@
+from Products.CMFCore.utils import getToolByName
+
+
+def disable_sunburst_patch(context):
+    """ we must disable the patch of collective.js.jqueryui to sunburst
+    to make our font colors work.
+    """
+    if context.readDataFile('zettwerk.ui-disable_sunburst_patch.txt') is None:
+        return
+
+    site = context.getSite()
+    portal_css = getToolByName(site, 'portal_css')
+    portal_css.getResourcesDict() \
+        .get('++resource++jquery-ui-themes/sunburst-patch.css') \
+        .setEnabled(False)
+    ## thats always disabled. if it is needed, we link
+    ## it directly (see tool.tool.UITool.css)
+    portal_css.getResourcesDict() \
+        .get('++resource++jquery-ui-themes/sunburst/jqueryui.css') \
+        .setEnabled(False)
