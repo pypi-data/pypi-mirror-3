cacec0533caf6477460c8785f607121894e9f67c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:31 +0000

    Add package asv_meta/dev-20120118-01/asv_meta-dev-20120118-01.tar.gz

---
+++ packages/asv_meta/asv_meta-dev-20120118-01.tar.gz/asv_meta-dev-20120118-01/asv_meta/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__name__ = 'asv_meta'
+__version__ = 'dev-20120118-01'
+__keywords__ = 'django asv'
+__description__ = '''
+This is a metapackage for quick installing other asv_* packages.
+'''
+
---
+++ packages/asv_meta/asv_meta-dev-20120118-01.tar.gz/asv_meta-dev-20120118-01/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from asv_meta import __name__, __version__, __keywords__, __description__
+##
+## To deploy use
+## $ python ./setup.py sdist upload
+##
+## Your need increase __version__ before it !!!
+##
+                  
+packages  = find_packages()
+setup(
+    name = __name__,
+    version = __version__,
+    packages = packages,
+    include_package_data = True,
+
+    install_requires = [
+        'pytils>=0.2.3',
+        'asv-imgs>=dev-20111110-01',
+        'asv-seo>=dev-20110720-01',
+        'asv-txt>=dev-20110709-01',
+        'asv-utils>=dev-20120115-02',
+    ],
+    setup_requires = [
+        'distribute>=0.6',
+    ],
+
+    author       = 'Sergey Vasilenko',
+    author_email = 'sv@makeworld.ru',
+    keywords  = __keywords__,
+    description  = __description__,
+    long_description = open('README.txt').read(),
+    license   = 'GPL',
+    platforms = 'All',
+    url  = 'http://bitbucket.org/xenolog/{}/wiki/Home'.format(__name__),
+
+    classifiers = [
+        'Environment :: Other Environment',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Natural Language :: Russian',
+        'Development Status :: 4 - Beta',
+        'Topic :: Software Development :: Libraries',
+    ],
+    zip_safe = False,
+)
+
+
