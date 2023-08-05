d83b56e13d407949824f2197aa3a4077b44a13ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:42 +0000

    Add package django-sekizai/0.5/django-sekizai-0.5.tar.gz

---
+++ packages/django-sekizai/django-sekizai-0.5.tar.gz/django-sekizai-0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+version = __import__('sekizai').__version__
+
+setup(
+    name = 'django-sekizai',
+    version = version,
+    description = 'Django Sekizai',
+    author = 'Jonas Obrist',
+    author_email = 'jonas.obrist@divio.ch',
+    url = 'http://github.com/ojii/django-sekizai',
+    packages = find_packages(),
+    zip_safe=False,
+    install_requires = [
+        'django-classy-tags>=0.3.1',
+    ],
+)
