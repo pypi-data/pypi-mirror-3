59a5c937e15d3ca76cee3407707d8768016b275b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package pydas/0.1/pydas-0.1.tar.gz

---
+++ packages/pydas/pydas-0.1.tar.gz/pydas-0.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='pydas',
+      version='0.1',
+      packages=[''],
+      author='Patrick Reynolds',
+      author_email='patrick.reynolds@kitware.com',
+      url='http://github.com/cpatrick/pydas',
+      )
