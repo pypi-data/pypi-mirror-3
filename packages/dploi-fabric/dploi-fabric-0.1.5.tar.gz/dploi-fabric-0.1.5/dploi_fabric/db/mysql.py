a3bd7e40345dcf7326bb31cd38fb8a8439c5c436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:54 +0000

    Add package dploi-fabric/0.1.5/dploi-fabric-0.1.5.tar.gz

---
+++ packages/dploi-fabric/dploi-fabric-0.1.5.tar.gz/dploi-fabric-0.1.5/dploi_fabric/db/mysql.py
@@ -0,0 +1,21 @@
+from fabric.tasks import Task
+from dploi_fabric.db.base import DumpDatabaseTask, DownloadDatabase
+
+class MysqlDumpDatabaseTask(DumpDatabaseTask, Task):
+    """
+    Dump the database (MySQL)
+    """
+    name = 'dump'
+    database_type = 'mysql'
+
+    def run(self, reason='unknown', compress=False, nolock=False):
+        self.nolock = nolock
+        return super(MysqlDumpDatabaseTask, self).run(reason, compress)
+
+    def get_command(self, env, file_name):
+        nolock = ' --lock-tables=false' if self.nolock else ''
+        host = ' --host %(db_host)s' % env if hasattr(env, 'db_host') else ''
+        return ('mysqldump' + nolock + host + ' --user="%(db_username)s" --password="%(db_password)s" "%(db_name)s" > ' % env) + file_name
+
+dump = MysqlDumpDatabaseTask()
+download = DownloadDatabase(dump_task=dump)
---
+++ packages/dploi-fabric/dploi-fabric-0.1.5.tar.gz/dploi-fabric-0.1.5/dploi_fabric/db/pg.py
@@ -0,0 +1,16 @@
+from fabric.tasks import Task
+from dploi_fabric.db.base import DumpDatabaseTask, DownloadDatabase
+
+class PostgreDumpDatabaseTask(DumpDatabaseTask, Task):
+    """
+    Dump the database (PostgreSQL)
+    """
+
+    name = 'dump'
+    database_type = 'pg'
+
+    def get_command(self, env, file_name):
+        return ('pg_dump --username="%(db_username)s" "%(db_name)s" > ' % env) + file_name
+
+dump = PostgreDumpDatabaseTask()
+download = DownloadDatabase(dump_task=dump)
