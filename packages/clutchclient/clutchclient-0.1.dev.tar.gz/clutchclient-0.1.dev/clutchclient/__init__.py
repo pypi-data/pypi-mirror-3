5b240395ca6be28e2122169db24a2ef91d6fa520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:27 +0000

    Add package clutchclient/0.1.dev/clutchclient-0.1.dev.tar.gz

---
+++ packages/clutchclient/clutchclient-0.1.dev.tar.gz/clutchclient-0.1.dev/clutchclient/__init__.py
@@ -0,0 +1,2 @@
+# http://packages.python.org/versiontools/#how-version-tuple-affects-version-string
+__version__ = (0, 1, 0, 'dev', 0)
---
+++ packages/clutchclient/clutchclient-0.1.dev.tar.gz/clutchclient-0.1.dev/clutchclient/commands/__init__.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+
+CLUTCH_CONF = '~/.clutchio'
+
+PARSER = argparse.ArgumentParser(
+    description='Command-line interface for clutch.io')
+PARSER.add_argument('command')
+PARSER.add_argument('-c', '--config', dest='config', action='store', nargs='?', default=CLUTCH_CONF)
+
+APP_PARSER = argparse.ArgumentParser(
+    description='Command-line interface for clutch.io')
+APP_PARSER.add_argument('command')
+APP_PARSER.add_argument('-a', '--app', dest='app', action='store', nargs='?', required=True)
+APP_PARSER.add_argument('-c', '--config', dest='config', action='store', nargs='?', default=CLUTCH_CONF)
+APP_PARSER.add_argument('-d', '--directory', dest='directory', action='store', nargs='?', default=os.getcwd())
+
+from clutchclient.commands.dev import handle as dev
+from clutchclient.commands.upload import handle as upload
+from clutchclient.commands.startapp import handle as startapp
+from clutchclient.commands.startscreen import handle as startscreen
+
+# Placate PyFlakes
+def __exported_functionality__():
+    return [dev, upload, startapp, startscreen]
