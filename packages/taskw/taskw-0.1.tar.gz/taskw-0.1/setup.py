569245363b3101dc27ca54bcb8ab979192dcc4e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:05 +0000

    Add package taskw/0.1/taskw-0.1.tar.gz

---
+++ packages/taskw/taskw-0.1.tar.gz/taskw-0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+f = open('README.rst')
+long_description = f.read().strip()
+long_description = long_description.split('split here', 1)[1]
+f.close()
+
+setup(name='taskw',
+      version=version,
+      description="Python bindings for your taskwarrior database",
+      long_description=long_description,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='taskwarrior task',
+      author='Ralph Bean',
+      author_email='ralph.bean@gmail.com',
+      url='http://threebean.org',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
