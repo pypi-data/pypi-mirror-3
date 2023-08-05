1e32fccf963b385f83ecd1709f3461f5a9eaf1ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:19 +0000

    Add package nosmsd/0.1/nosmsd-0.1.tar.gz

---
+++ packages/nosmsd/nosmsd-0.1.tar.gz/nosmsd-0.1/nosmsd/nosmsd_inject.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# encoding=utf-8
+# maintainer: rgaudin
+
+""" Injects an arbitrary message to Gammu Inbox Database and process it. """
+
+import sys
+import logging
+
+from nosmsd.settings import settings as nosettings
+from nosmsd.database import Inbox
+from nosmsd.nosmsd_incoming import handle as nohandle
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+
+
+def main():
+
+    # args format: (sender, text)
+    if len(sys.argv) != 3:
+        logger.error(u"Incorrect input.\nUsage: %s FROM TEXT" % sys.argv[0])
+        sys.exit(1)
+
+    # create message object in DB
+    try:
+        sender, text = sys.argv[1:]
+        sender = sender.strip()
+        text = text.strip()
+        msg = Inbox.add(sender, text)
+
+        logger.info("Added message as ID #%d" % msg.id)
+
+        # launch message handler
+        nohandle(msg.id)
+
+    except Exception as e:
+        logger.error(u"Unable to record message:\n%r" % e)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
