64e4c95847fa4ccdbeea77c221ee21d7e8f4abfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:15 +0000

    Add package gocept.exttest/1.0/gocept.exttest-1.0.tar.gz

---
+++ packages/gocept.exttest/gocept.exttest-1.0.tar.gz/gocept.exttest-1.0/setup.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='gocept.exttest',
+    version='1.0',
+    author='Wolfgang Schnerring <ws at gocept dot com>, Michael Howitz <mh at gocept dot com>',
+    author_email='mail@gocept.com',
+    url='http://code.gocept.com/hg/public/gocept.exttest',
+    description="Helper to integrate external tests with python unittests.",
+    long_description=(
+        open('README.txt').read()
+        + '\n\n'
+        + open('CHANGES.txt').read()),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='ZPL',
+    namespace_packages=['gocept'],
+    install_requires=[
+        'setuptools',
+        'zc.recipe.testrunner',
+    ],
+    extras_require=dict(test=[
+        'mock',
+    ]),
+)
---
+++ packages/gocept.exttest/gocept.exttest-1.0.tar.gz/gocept.exttest-1.0/src/gocept/exttest/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from gocept.exttest.case import makeSuite
