7e95e190eafd48be5086986ccea4bcc0a69f60f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:16 +0000

    Add package nydus/0.2.0/nydus-0.2.0.tar.gz

---
+++ packages/nydus/nydus-0.2.0.tar.gz/nydus-0.2.0/nydus/db/backends/__init__.py
@@ -0,0 +1,41 @@
+"""
+nydus.db.backends
+~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2011 DISQUS.
+:license: Apache License 2.0, see LICENSE for more details.
+"""
+
+class BaseConnection(object):
+    """
+    Base connection class.
+
+    Child classes should implement at least
+    connect() and disconnect() methods.
+    """
+
+    retryable_exceptions = ()
+
+    def __init__(self, num, **options):
+        self._connection = None
+        self.num = num
+
+    def __repr__(self):
+        return '<%s: num=%s>' % (self.__class__.__name__, self.num)
+
+    @property
+    def connection(self):
+        if self._connection is None:
+            self._connection = self.connect()
+        return self._connection
+
+    def close(self):
+        if self._connection:
+            self.disconnect()
+        self._connection = None
+
+    def connect(self):
+        raise NotImplementedError
+
+    def disconnect(self):
+        raise NotImplementedError
---
+++ packages/nydus/nydus-0.2.0.tar.gz/nydus-0.2.0/nydus/db/backends/redis.py
@@ -0,0 +1,39 @@
+"""
+nydus.db.backends.redis
+~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2011 DISQUS.
+:license: Apache License 2.0, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import
+
+from redis import Redis as RedisClient
+from redis import exceptions as client_exceptions
+
+from nydus.db.backends import BaseConnection
+
+class Redis(BaseConnection):
+
+    # Exceptions that can be retried by this backend
+    retryable_exceptions = client_exceptions
+
+    def __init__(self, host='localhost', port=6379, db=0, timeout=None, **options):
+        self.host = host
+        self.port = port
+        self.db = db
+        self.timeout = timeout
+        super(Redis, self).__init__(**options)
+
+    def connect(self):
+        return RedisClient(host=self.host, port=self.port, db=self.db, socket_timeout=self.timeout)
+
+    def disconnect(self):
+        self.connection.disconnect()
+
+    def __getattr__(self, attr):
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        else:
+            # Send to router
+            return getattr(self.connection, attr)
