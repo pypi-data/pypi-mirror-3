0692f5aa8c043977bb9029c9b14820cf9cd61b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:21 +0000

    Add package turrentine/0.0.2/turrentine-0.0.2.tar.gz

---
+++ packages/turrentine/turrentine-0.0.2.tar.gz/turrentine-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup( name='turrentine',
+    version = '0.0.2',
+    description = 'A very simple CMS for Django',
+    url = 'https://github/af/turrentine',
+
+    author = 'Aaron Franks',
+    author_email = 'aaron.franks+turrentine@gmail.com',
+
+    keywords = ['django', 'cms',],
+    packages = ['turrentine',],
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
