9a4b9b9987f9c49b6cbf06c4666a5b37053009a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package kickstart/0.296/kickstart-0.296.tar.gz

---
+++ packages/kickstart/kickstart-0.296.tar.gz/kickstart-0.296/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'kickstart',
+    description = 'Framework agnostic setup script',
+    version = '0.296',
+    include_package_data = True,
+    packages = ['kickstarter'],
+    scripts = ['kickstart.py'],
+    install_requires = ['PyYAML'],
+    url = 'https://github.com/rlayte/kickstart',
+    author = 'Richard Layte',
+    author_email = 'rich.layte@gmail.com'
+)
