be93e3ac64bf4b91e8b1ee5403a6fc0abb3348e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:21 +0000

    Add package scalymongo/0.1.2/scalymongo-0.1.2.tar.gz

---
+++ packages/scalymongo/scalymongo-0.1.2.tar.gz/scalymongo-0.1.2/scalymongo/errors.py
@@ -0,0 +1,18 @@
+class UnsafeBehaviorError(Exception):
+    pass
+
+
+class GlobalQueryException(Exception):
+    pass
+
+
+class SchemaError(ValueError):
+    pass
+
+
+class ValidationError(Exception):
+    pass
+
+
+class ModifyFailedError(Exception):
+    pass
