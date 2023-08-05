f72a20e9e81624369b7b83adaa620108c40bd984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:05 +0000

    Add package greenlet/0.3.3/greenlet-0.3.3.zip

---
+++ packages/greenlet/greenlet-0.3.3.zip/greenlet-0.3.3/platform/switch_arm32_gcc.h
@@ -0,0 +1,53 @@
+/*
+ * this is the internal transfer function.
+ *
+ * HISTORY
+ * 14-Aug-06 File creation. Ported from Arm Thumb. Sylvain Baro
+ *  3-Sep-06 Commented out saving of r1-r3 (r4 already commented out) as I
+ *           read that these do not need to be saved.  Also added notes and
+ *           errors related to the frame pointer. Richard Tew.
+ *
+ * NOTES
+ *
+ *   It is not possible to detect if fp is used or not, so the supplied
+ *   switch function needs to support it, so that you can remove it if
+ *   it does not apply to you.
+ *
+ * POSSIBLE ERRORS
+ *
+ *   "fp cannot be used in asm here"
+ *
+ *   - Try commenting out "fp" in REGS_TO_SAVE.
+ *
+ */
+
+#define STACK_REFPLUS 1
+
+#ifdef SLP_EVAL
+#define STACK_MAGIC 0
+#define REGS_TO_SAVE "r4", "r5", "r6", "r7", "r8", "r9", "lr"
+
+static int
+slp_switch(void)
+{
+        void *fp;
+        register int *stackref, stsizediff;
+        __asm__ volatile ("" : : : REGS_TO_SAVE);
+        __asm__ volatile ("str fp,%0" : "=m" (fp));
+        __asm__ ("mov %0,sp" : "=g" (stackref));
+        {
+                SLP_SAVE_STATE(stackref, stsizediff);
+                __asm__ volatile (
+                    "add sp,sp,%0\n"
+                    "add fp,fp,%0\n"
+                    :
+                    : "r" (stsizediff)
+                    );
+                SLP_RESTORE_STATE();
+        }
+        __asm__ volatile ("ldr fp,%0" : : "m" (fp));
+        __asm__ volatile ("" : : : REGS_TO_SAVE);
+        return 0;
+}
+
+#endif
---
+++ packages/greenlet/greenlet-0.3.3.zip/greenlet-0.3.3/run-tests.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python
+
+import sys, os, getopt, unittest
+from distutils.spawn import spawn
+
+build = True
+verbosity = 2
+here = os.path.dirname(os.path.abspath(__file__))
+os.chdir(here)
+
+def bits():
+    """determine if running on a 32 bit or 64 bit platform
+    just looking at sys.maxint isn't enough
+    x64 windows has sys.maxint == (1<<31) - 1
+    """
+
+    if sys.version_info[:2] < (2, 5):
+        if sys.maxint == 2147483647:  # (1 << 31) - 1
+            return 32
+        else:
+            return 64
+
+    class X(object):
+        def __len__(self):
+            return 1 << 31
+    try:
+        len(X())
+        return 64
+    except OverflowError:
+        return 32
+
+# -- parse options
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "nq")
+    if args:
+        raise getopt.GetoptError("too many arguments")
+except getopt.GetoptError:
+    sys.exit("run-tests.py: error: %s" % sys.exc_info()[1])
+
+for o, a in opts:
+    if o == "-q":
+        verbosity = 0
+    elif o == "-n":
+        build = False
+
+# -- build greenlet
+if build:
+    if verbosity == 0:
+        cmd = [sys.executable, "setup.py", "-q", "build_ext", "-q"]
+    else:
+        cmd = [sys.executable, "setup.py", "build_ext"]
+
+    spawn(cmd, search_path=0)
+
+
+# -- find greenlet but skip the one in "."
+if not build:
+    oldpath = sys.path[:]
+    sys.path.remove(here)
+
+import greenlet
+
+if not build:
+    sys.path[:] = oldpath
+
+sys.stdout.write("python %s (%s bit) using greenlet %s from %s\n" %
+                 (sys.version.split()[0], bits(), greenlet.__version__, greenlet.__file__))
+
+
+# -- run tests
+from tests import test_collector
+suite = test_collector()
+unittest.TextTestRunner(verbosity=verbosity).run(suite)
