190e77d7888a134cc11ccd814790170127ac174a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:15 +0000

    Add package fhurl/0.1.2/fhurl-0.1.2.zip

---
+++ packages/fhurl/fhurl-0.1.2.zip/fhurl-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "fhurl",
+    version = "0.1.2",
+    url = 'http://packages.python.org/fhurl/',
+    license = 'BSD',
+    description = "Django generic form handler view",
+    author = 'Amit Upadhyay',
+    author_email = "upadhyay@gmail.com",
+    py_modules = ["fhurl"],
+)
