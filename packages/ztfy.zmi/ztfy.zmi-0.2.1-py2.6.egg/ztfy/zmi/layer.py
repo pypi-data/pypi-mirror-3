42e0ea566ea923f8e2a8865fcf468f8249493cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:37 +0000

    Add package ztfy.zmi/0.2.1/ztfy.zmi-0.2.1-py2.6.egg

---
+++ packages/ztfy.zmi/ztfy.zmi-0.2.1-py2.6.egg/ztfy/zmi/layer.py
@@ -0,0 +1,36 @@
+### -*- coding: utf-8 -*- ####################################################
+##############################################################################
+#
+# Copyright (c) 2008-2010 Thierry Florac <tflorac AT ulthar.net>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+__docformat__ = "restructuredtext"
+
+# import standard packages
+
+# import Zope3 interfaces
+from zope.app.basicskin import IDefaultBrowserLayer
+
+# import local interfaces
+from ztfy.blog.layer import IZTFYBlogBackLayer, IZTFYBlogBackSkin
+
+# import Zope3 packages
+
+# import local packages
+
+
+class IZMILayer(IZTFYBlogBackLayer, IDefaultBrowserLayer):
+    """ZMI layer interface"""
+
+
+class IZMISkin(IZTFYBlogBackSkin, IZMILayer):
+    """ZMI skin interface"""
---
+++ packages/ztfy.zmi/ztfy.zmi-0.2.1-py2.6.egg/ztfy/zmi/login.py
@@ -0,0 +1,72 @@
+### -*- coding: utf-8 -*- ####################################################
+##############################################################################
+#
+# Copyright (c) 2008-2011 Thierry Florac <tflorac AT ulthar.net>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+__docformat__ = "restructuredtext"
+
+# import standard packages
+
+# import Zope3 interfaces
+from zope.authentication.interfaces import IAuthentication
+
+# import local interfaces
+
+# import Zope3 packages
+from z3c.form import field, button
+from zope.component import getUtility
+from zope.interface import Interface
+from zope.schema import TextLine, Password
+from zope.traversing.browser import absoluteURL
+
+# import local packages
+from ztfy.skin.form import AddForm
+
+from ztfy.zmi import _
+
+
+class ILoginFormFields(Interface):
+    """Login form fields interface"""
+
+    username = TextLine(title=_("login-field", "Login"),
+                        description=_("Principal ID"),
+                        required=True)
+
+    password = Password(title=_("password-field", "Password"),
+                        description=_("User password"),
+                        required=True)
+
+
+class LoginForm(AddForm):
+    """ZMI login form"""
+
+    title = _("Login form")
+    legend = _("Please enter valid credentials to login")
+
+    fields = field.Fields(ILoginFormFields)
+
+    @button.buttonAndHandler(_("login-button", "Login"))
+    def handleLogin(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        self.request.form['login'] = data['username']
+        self.request.form['password'] = data['password']
+        auth = getUtility(IAuthentication)
+        principal = auth.authenticate(self.request)
+        if principal is not None:
+            context, _layer, _permission = self.context.args
+            self.request.response.redirect(absoluteURL(context, self.request))
+        else:
+            auth.unauthorized(None, self.request)
