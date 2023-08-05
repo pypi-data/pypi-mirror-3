fe4e537585e2e9396762781f126b2b6027b84303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:43 +0000

    Add package django-fsfield/0.5/django-fsfield-0.5.tar.gz

---
+++ packages/django-fsfield/django-fsfield-0.5.tar.gz/django-fsfield-0.5/src/fsfield/tests/models.py
@@ -0,0 +1,16 @@
+import os.path as op
+from django.core.files.storage import FileSystemStorage
+from django.db import models
+from django.utils import simplejson as json
+from fsfield import FileStorageField
+
+
+tmp_dir = op.join(op.dirname(__file__), "tmp")
+storage = FileSystemStorage(tmp_dir)
+
+class StorageModel(models.Model):
+
+    normal_field = models.CharField(max_length=255)
+    simple_file_field = FileStorageField()
+    json_file_field = FileStorageField(storage, load=json.load, dump=json.dump)
+    custom_name_file_field = FileStorageField(filename="custom")
