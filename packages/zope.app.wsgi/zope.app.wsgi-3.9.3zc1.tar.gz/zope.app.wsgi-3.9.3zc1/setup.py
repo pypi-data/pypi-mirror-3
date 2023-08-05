7240660e00b9eeff507ca0524a721f749582ba1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:49 +0000

    Add package zope.app.wsgi/3.9.3zc1/zope.app.wsgi-3.9.3zc1.tar.gz

---
+++ packages/zope.app.wsgi/zope.app.wsgi-3.9.3zc1.tar.gz/zope.app.wsgi-3.9.3zc1/setup.py
@@ -0,0 +1,88 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.app.wsgi package
+
+$Id: setup.py 124045 2012-01-14 18:24:48Z jim $
+"""
+from setuptools import setup, find_packages
+
+setup(name='zope.app.wsgi',
+      version='3.9.3zc1',
+      url='http://pypi.python.org/pypi/zope.app.wsgi',
+      license='ZPL 2.1',
+      description='WSGI application for the zope.publisher',
+      long_description=\
+          open('README.txt').read() + \
+          '\n\n' + \
+          open('CHANGES.txt').read(),
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      classifiers=['Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Zope Public License',
+                   'Programming Language :: Python',
+                   'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+                   'Framework :: Zope3',
+                   ],
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require = dict(test=[
+          'zope.annotation',
+          'zope.authentication',
+          'zope.browserpage',
+          'zope.componentvocabulary',
+          'zope.location',
+          'zope.login',
+          'zope.password',
+          'zope.principalregistry',
+          'zope.securitypolicy',
+          ]),
+      install_requires=[
+          'setuptools',
+          'ZConfig',
+          'wsgi_intercept',
+          'ZODB3',
+          'zope.app.appsetup >= 3.14.0',
+          'zope.processlifetime',
+          'zope.app.publication',
+          'zope.event',
+          'zope.interface',
+          'zope.publisher',
+          'zope.security',
+          'zope.component',
+          'zope.configuration',
+          'zope.container',
+          'zope.error',
+          'zope.lifecycleevent',
+          'zope.session',
+          'zope.site',
+          'zope.testbrowser',
+          'zope.testing',
+          'zope.traversing',
+          ],
+      entry_points={
+          'paste.app_factory': [
+              'main = zope.app.wsgi.paste:ZopeApplication'
+          ]
+      },
+      include_package_data = True,
+      zip_safe = False,
+      )
---
+++ packages/zope.app.wsgi/zope.app.wsgi-3.9.3zc1.tar.gz/zope.app.wsgi-3.9.3zc1/src/zope/app/wsgi/tests.py
@@ -0,0 +1,130 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""WSGI tests
+
+$Id: tests.py 110773 2010-04-13 11:49:56Z thefunny42 $
+"""
+import tempfile
+import unittest
+import re
+import zope.component
+
+from zope import component, interface
+from zope.component.testlayer import ZCMLFileLayer
+from zope.testing import doctest
+from zope.testing import renormalizing
+
+import zope.event
+import zope.app.wsgi
+import zope.publisher.interfaces.browser
+from zope.app.publication.requestpublicationregistry import factoryRegistry
+from zope.app.publication.requestpublicationfactories import BrowserFactory
+from zope.app.wsgi.testing import AppWSGILayer
+from zope.authentication.interfaces import IAuthentication
+from zope.securitypolicy.tests import principalRegistry
+
+
+def cleanEvents(s):
+    zope.event.subscribers.pop()
+
+
+class FileView:
+
+    interface.implements(zope.publisher.interfaces.browser.IBrowserPublisher)
+    component.adapts(interface.Interface,
+                     zope.publisher.interfaces.browser.IBrowserRequest)
+
+    def __init__(self, _, request):
+        self.request = request
+
+    def browserDefault(self, *_):
+        return self, ()
+
+    def __call__(self):
+        self.request.response.setHeader('content-type', 'text/plain')
+        f = tempfile.TemporaryFile()
+        f.write("Hello\nWorld!\n")
+        return f
+
+
+def test_file_returns():
+    """We want to make sure that file returns work
+
+Let's register a view that returns a temporary file and make sure that
+nothing bad happens. :)
+
+    >>> component.provideAdapter(FileView, name='test-file-view.html')
+    >>> from zope.security import checker
+    >>> checker.defineChecker(
+    ...     FileView,
+    ...     checker.NamesChecker(['browserDefault', '__call__']),
+    ...     )
+
+    >>> from zope.app.wsgi.testlayer import Browser
+    >>> browser = Browser()
+    >>> browser.handleErrors = False
+    >>> browser.open('http://localhost/@@test-file-view.html')
+    >>> browser.headers['content-type']
+    'text/plain'
+
+    >>> browser.headers['content-length']
+    '13'
+
+    >>> print browser.contents
+    Hello
+    World!
+    <BLANKLINE>
+
+Clean up:
+
+    >>> checker.undefineChecker(FileView)
+    >>> component.provideAdapter(
+    ...     None,
+    ...     (interface.Interface,
+    ...      zope.publisher.interfaces.browser.IBrowserRequest),
+    ...     zope.publisher.interfaces.browser.IBrowserPublisher,
+    ...     'test-file-view.html',
+    ...     )
+
+
+"""
+
+def test_suite():
+
+    checker = renormalizing.RENormalizing([
+        (re.compile(r"&lt;class 'zope.component.interfaces.ComponentLookupError'&gt;"),
+                    r'ComponentLookupError'),
+    ])
+    functional_suite = doctest.DocTestSuite()
+    functional_suite.layer = AppWSGILayer
+
+    readme_test = doctest.DocFileSuite(
+            'README.txt',
+            checker=checker, tearDown=cleanEvents,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
+
+    doctest_suite = doctest.DocFileSuite(
+            'fileresult.txt', 'paste.txt',
+            checker=checker,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
+
+    readme_test.layer = ZCMLFileLayer(zope.app.wsgi)
+    doctest_suite.layer = ZCMLFileLayer(zope.app.wsgi)
+
+
+    return unittest.TestSuite((
+        functional_suite, readme_test, doctest_suite))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
