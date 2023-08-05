97e31dca567bc4165745099c7a83cf597d57e6f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:45 +0000

    Add package plumi.content/4.3rc2/plumi.content-4.3rc2.zip

---
+++ packages/plumi.content/plumi.content-4.3rc2.zip/plumi.content-4.3rc2/plumi/content/browser/reTranscode.py
@@ -0,0 +1,13 @@
+from Products.Five.browser import BrowserView
+from zope.component import getUtility
+from collective.transcode.star.interfaces import ITranscodeTool
+
+
+class reTranscode(BrowserView):
+
+    def __init__(self, context, request):
+        super(reTranscode, self).__init__(context, request)
+
+    def __call__(self):
+        tt = getUtility(ITranscodeTool)
+        return tt.add(self.context, force=True)
