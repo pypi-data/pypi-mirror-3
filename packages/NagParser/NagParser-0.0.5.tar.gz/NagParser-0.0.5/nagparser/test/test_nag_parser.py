9115b919893f620bacc1faeddd38a9959adc501d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:09 +0000

    Add package NagParser/0.0.5/NagParser-0.0.5.tar.gz

---
+++ packages/NagParser/NagParser-0.0.5.tar.gz/NagParser-0.0.5/nagparser/test/test_nag_parser.py
@@ -0,0 +1,24 @@
+# Some basic Nose tests....
+from nose.tools import *
+import nagparser
+import os
+
+
+class test_nagparser():
+    def setup(self):
+        testdir, filename = os.path.split(os.path.abspath(__file__))
+        basedir = testdir + '/data/'
+        importantservicegroups = None
+        files = [basedir + 'test_objects.cache', basedir + 'test_status.dat']
+        self.config = {'importantservicegroups': importantservicegroups, 'files': files}    
+        self.lastupdated = '2011-10-31 12:08:22'
+        
+    def test_nag_objcreation(self):
+        '''Just create the nag object, fail on any exception'''
+        nag = nagparser.parse(self.config)
+        
+    
+    def test_lastupdated_matches_testdata(self):
+        '''Test to ensure the timestamp matches expected given test data'''
+        nag = nagparser.parse(self.config)
+        assert (str(nag.lastupdated) == str(self.lastupdated))
