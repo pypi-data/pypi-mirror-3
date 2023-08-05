2ceffaaa871e614071a8f3497e0d1d7c8e39237a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:00 +0000

    Add package BusScraper/0.1.2/BusScraper-0.1.2.tar.gz

---
+++ packages/BusScraper/BusScraper-0.1.2.tar.gz/BusScraper-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='BusScraper',
+    version='0.1.2',
+    author='Matthew Owen',
+    author_email='matthew.owen95@gmail.com',
+    packages=['busscraper', 'busscraper.test'],
+    url='http://pypi.python.org/pypi/BusScraper/',
+    license='LICENSE.txt',
+    description='Scraper for ACIS based bus time sites',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "httplib2",
+        "BeautifulSoup"
+    ],
+)
