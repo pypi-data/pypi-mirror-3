c30cc7cae6c258112be31178b122a432f7aeb5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:31 +0000

    Add package grc/1.0b3/grc-1.0b3.tar.gz

---
+++ packages/grc/grc-1.0b3.tar.gz/grc-1.0b3/setup.py
@@ -0,0 +1,30 @@
+import os
+from os.path import join
+
+from setuptools import setup, find_packages
+
+PACKAGE = "grc"
+NAME = "grc"
+DESCRIPTION = "Generic Colorizer"
+AUTHOR = "Michel Albert"
+AUTHOR_EMAIL = "michel@albert.lu"
+VERSION = __import__(PACKAGE).__version__
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=open("docs/README.rst").read(),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="GPL",
+    install_requires = [],
+    data_files=[
+        ("configs", [join('configs', _) for _ in
+            os.listdir('configs') if _[-1] != '~'])
+        ],
+    scripts = ['grc/scripts/grc'],
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    zip_safe=False,
+)
+
