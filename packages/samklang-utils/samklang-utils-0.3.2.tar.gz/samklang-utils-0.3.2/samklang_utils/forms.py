8af9c86eff51bfc546eb2b1ef0567458f7a7b249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:57 +0000

    Add package samklang-utils/0.3.2/samklang-utils-0.3.2.tar.gz

---
+++ packages/samklang-utils/samklang-utils-0.3.2.tar.gz/samklang-utils-0.3.2/samklang_utils/forms.py
@@ -0,0 +1,25 @@
+import floppyforms as forms
+
+class MarkdownTextarea(forms.Textarea):
+    template_name = 'samklang_utils/markdown_textarea.html'
+
+    class Media:
+        js = ('js/wmd.js', 'js/showdown.js', 'js/flext.js')
+        css = {
+                'all': ('css/wmd.css',)
+                }
+
+    def get_context_data(self):
+        self.attrs['class'] += " flext growme maxheight-500"
+        return super(MarkdownTextarea, self).get_context_data()
+
+class AutoupdateTextInput(forms.TextInput):
+    template_name = 'samklang_utils/autoupdate_text_input.html'
+
+class SharedToGroup(forms.MultiWidget):
+    #template_name = 'samklang_utils/shared_to_group.html'
+
+    def __init__(self, attrs=None):
+        widgets = (forms.Checkbox, forms.Modelselect)
+        super(SharedToGroup, self).__init__(widgets, attrs)
+
---
+++ packages/samklang-utils/samklang-utils-0.3.2.tar.gz/samklang-utils-0.3.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+        name='samklang-utils',
+        version="0.3.2",
+        author='Sigurd Gartmann',
+        author_email='sigurdga-samklang@sigurdga.no',
+        url='http://github.com/sigurdga/samklang-utils',
+        description='Common utilities for Samklang',
+        long_description=open('README.txt').read(),
+        license="AGPL",
+        packages = ['samklang_utils', 'samklang_utils.middleware'],
+        package_data = {'samklang_utils': ['static/js/*.js', 'static/css/*.css', 'locale/*/LC_MESSAGES/django.*o'] },
+        py_modules=['samklang_utils.widgets'],
+        install_requires=['django-floppyforms>=0.4.7'],
+        classifiers=[
+                "Development Status :: 3 - Alpha",
+                "License :: OSI Approved :: GNU Affero General Public License v3",
+                "Intended Audience :: Developers",
+                "Framework :: Django",
+                "Environment :: Web Environment",
+                "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+                ]
+        )
+
