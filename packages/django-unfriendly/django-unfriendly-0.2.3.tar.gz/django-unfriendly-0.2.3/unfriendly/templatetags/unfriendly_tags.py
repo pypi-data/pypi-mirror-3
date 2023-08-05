ea41b2edaf47cf8c0c90a98547c2ff2e725cd8d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:14 +0000

    Add package django-unfriendly/0.2.3/django-unfriendly-0.2.3.tar.gz

---
+++ packages/django-unfriendly/django-unfriendly-0.2.3.tar.gz/django-unfriendly-0.2.3/unfriendly/templatetags/unfriendly_tags.py
@@ -0,0 +1,33 @@
+from django import template
+from django.core.urlresolvers import reverse
+from django.template.defaultfilters import slugify
+
+from unfriendly import settings
+from unfriendly.utils import encrypt
+
+
+register = template.Library()
+
+
+@register.filter
+def obfuscate(value, juice=None):
+    """
+    Template filter that obfuscates whatever text it is applied to. The text is
+    supposed to be a URL, but it will obfuscate anything.
+
+    Usage:
+        Extremely unfriendly URL:
+        {{ "/my-secret-path/"|obfuscate }}
+
+        Include some SEO juice:
+        {{ "/my-secret-path/"|obfuscate:"some SEO friendly text" }}
+    """
+    kwargs = {
+        'key': encrypt(value,
+                       settings.UNFRIENDLY_SECRET,
+                       checksum=settings.UNFRIENDLY_ENFORCE_CHECKSUM),
+    }
+    if juice:
+        kwargs['juice'] = slugify(juice)
+
+    return reverse('unfriendly-deobfuscate', kwargs=kwargs)
