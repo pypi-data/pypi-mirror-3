bc88b54e76a1992b6b5c597cb83d79a6cd6d51a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:07 +0000

    Add package zope.app.rotterdam/3.5.3/zope.app.rotterdam-3.5.3.tar.gz

---
+++ packages/zope.app.rotterdam/zope.app.rotterdam-3.5.3.tar.gz/zope.app.rotterdam-3.5.3/setup.py
@@ -0,0 +1,80 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.app.rotterdam package
+
+$Id: setup.py 124142 2012-01-23 16:49:45Z menesis $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.app.rotterdam',
+      version='3.5.3',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Rotterdam -- A Zope 3 ZMI Skin',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 zmi skin rotterdam",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.app.rotterdam',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require = dict(test=[
+          'zope.app.component',
+          'zope.app.testing',
+          'zope.app.zcmlfiles',
+          'zope.login',
+          'zope.password',
+          'zope.securitypolicy',
+          'zope.site',
+          ]),
+      install_requires=['setuptools',
+                        'zope.app.basicskin',
+                        'zope.app.form',
+                        'zope.app.pagetemplate',
+                        'zope.component',
+                        'zope.container',
+                        'zope.i18n',
+                        'zope.i18nmessageid',
+                        'zope.interface',
+                        'zope.proxy',
+                        'zope.publisher >= 3.12',
+                        'zope.security',
+                        'zope.traversing',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
