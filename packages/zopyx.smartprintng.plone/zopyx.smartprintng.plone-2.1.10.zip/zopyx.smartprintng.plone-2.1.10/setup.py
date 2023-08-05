4c578f4bf3ca54be5d0027b878bef8d505c697e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:19 +0000

    Add package zopyx.smartprintng.plone/2.1.10/zopyx.smartprintng.plone-2.1.10.zip

---
+++ packages/zopyx.smartprintng.plone/zopyx.smartprintng.plone-2.1.10.zip/zopyx.smartprintng.plone-2.1.10/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.1.10'
+
+setup(name='zopyx.smartprintng.plone',
+      version=version,
+      description="Produce & Publisher server integration with Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "source", "HISTORY.rst")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='PDF Plone Python EBook EPUB',
+      author='Andreas Jung',
+      author_email='info@zopyx.com',
+      url='http://pypi.python.org/pypi/zopyx.smartprintng.plone',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['zopyx', 'zopyx.smartprintng'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'BeautifulSoup',
+          'zopyx.smartprintng.client',
+          'zopyx.convert2',
+          'lxml',
+          'uuid',
+          'Pillow',
+          'archetypes.schemaextender',
+          'unittest2'
+          # -*- Extra requirements: -*-
+      ],
+      tests_require=['zope.testing'],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
