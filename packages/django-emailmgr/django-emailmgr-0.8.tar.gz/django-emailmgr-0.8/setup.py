db13b594f95cb546f6bb3df0fbe5d04c5ee91c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:14 +0000

    Add package django-emailmgr/0.8/django-emailmgr-0.8.tar.gz

---
+++ packages/django-emailmgr/django-emailmgr-0.8.tar.gz/django-emailmgr-0.8/setup.py
@@ -0,0 +1,24 @@
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='django-emailmgr',
+    version='0.8',
+    description = "An email manager for Django user",
+    long_description = read('README'),
+    author='Val L33',
+    author_email='val@neekware.com',
+    url='https://github.com/un33k/django-emailmgr',
+    packages=['emailmgr'],
+    #install_requires = [''],
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Web Environment',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Utilities'],
+    )
+
