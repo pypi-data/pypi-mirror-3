b3a45b217dacf02904831bc423a5b357536567d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:40 +0000

    Add package gocept.exttest/0.1.3/gocept.exttest-0.1.3.tar.gz

---
+++ packages/gocept.exttest/gocept.exttest-0.1.3.tar.gz/gocept.exttest-0.1.3/setup.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='gocept.exttest',
+    version='0.1.3',
+    author='gocept',
+    author_email='mail@gocept.com',
+    url='http://code.gocept.com/hg/public/gocept.exttest',
+    description="Helper to run external commands as tests with zc.testrunner.",
+    long_description=(
+        open('README.txt').read()
+        + '\n\n'
+        + open('CHANGES.txt').read()),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='ZPL',
+    namespace_packages=['gocept'],
+    install_requires=[
+        'setuptools',
+        'zc.recipe.testrunner',
+    ],
+    extras_require=dict(test=[
+        'mock',
+    ]),
+)
