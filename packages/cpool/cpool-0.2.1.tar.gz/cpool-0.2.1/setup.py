1a36ce287f01af1a4aa04bc510086c9906aae59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:15 +0000

    Add package cpool/0.2.1/cpool-0.2.1.tar.gz

---
+++ packages/cpool/cpool-0.2.1.tar.gz/cpool-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='cpool',
+    version='0.2.1',
+    author='Chris Petersen',
+    author_email='geek@ex-nerd.com',
+    py_modules=['cpool'],
+    url='https://github.com/ex-nerd/cpool',
+    license='LICENSE.txt',
+    description='Simple Connection Pool handler',
+    long_description=open('README.rst').read(),
+    install_requires=[],
+)
