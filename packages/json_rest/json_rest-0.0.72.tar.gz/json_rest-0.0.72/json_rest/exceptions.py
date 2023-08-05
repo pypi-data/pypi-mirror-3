628a2e9ba75350a0898c7c8dc69af6d33d3b4b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:29 +0000

    Add package json_rest/0.0.72/json_rest-0.0.72.tar.gz

---
+++ packages/json_rest/json_rest-0.0.72.tar.gz/json_rest-0.0.72/json_rest/exceptions.py
@@ -0,0 +1,39 @@
+import httplib
+
+_EXCEPTION_REPR_TEMPLATE = """HTTP {e.code} ({e.code_string}):
+  METHOD: {e.method}
+  URL: {e.url}
+  SENT DATA: {e.sent_data}
+  GOT DATA: {e.received_data}
+"""
+
+class JSONRestRequestException(Exception):
+    def __init__(self, method, url, code, sent_headers, sent_data, received_headers, received_data, msg=None):
+        super(JSONRestRequestException, self).__init__()
+        self.method = method
+        self.url = url
+        self.code = code
+        self.code_string = httplib.responses.get(code, "?")
+        self.sent_data = sent_data
+        self.sent_headers = sent_headers
+        self.received_data = received_data
+        self.received_headers = received_headers
+        self.msg = msg
+    @classmethod
+    def from_request_and_response(cls, request, response, sent_data, received_data, msg=None):
+        return cls(
+            method=request.get_method(),
+            url=request.get_full_url(),
+            code=response.code,
+            sent_headers=request.headers,
+            sent_data=sent_data,
+            received_headers=response.headers,
+            received_data=received_data,
+            msg=msg)
+    def __repr__(self):
+        return _EXCEPTION_REPR_TEMPLATE.format(e=self)
+    def __str__(self):
+        return repr(self)
+
+class JSONRestDecodeException(JSONRestRequestException):
+    pass
