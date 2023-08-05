b6915f008e7cd740c14cebcbafb450f9a86b7d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:25 +0000

    Add package dynect/0.0.5/dynect-0.0.5.tar.gz

---
+++ packages/dynect/dynect-0.0.5.tar.gz/dynect-0.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from os.path import join, abspath, dirname
+here = lambda *x: join(abspath(dirname(__file__)), *x)
+
+
+setup(
+    name='dynect',
+    version='0.0.5',
+    description='Wrapper library to Dynect API.',
+    long_description=here('README.rst'),
+    packages=['dynect'],
+    author='Jorge Eduardo Cardona',
+    author_email='jorgeecardona@gmail.com',
+    install_requires=['distribute', 'requests', 'decorator'],
+    setup_requires=['unittest2', 'mock', 'requests'],
+    test_suite='tests')
