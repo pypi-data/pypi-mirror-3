0655a0a61e3dab84053446ca9496c38199b8daba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:49 +0000

    Add package provy/0.4.3a/provy-0.4.3a.tar.gz

---
+++ packages/provy/provy-0.4.3a.tar.gz/provy-0.4.3a/provy/more/debian/programming/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Roles in this namespace are meant to configure programming languages in Debian distributions.
+'''
+from provy.more.debian.programming.nodejs import NodeJsRole
+from provy.more.debian.programming.ruby import RubyRole
