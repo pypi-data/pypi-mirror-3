db00ac38a9e1ea240d9a4a2adb74f4141875f378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:46 +0000

    Add package FinDates/0.1/FinDates-0.1.zip

---
+++ packages/FinDates/FinDates-0.1.zip/FinDates-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='FinDates',
+    version='0.1',
+    description = 'Dealing with dates in finance',
+    author='Artem Frolov (Amelanche Inc.)',
+    author_email='findates@artemfrolov.fastmail.fm',
+    packages=['findates',],
+    license='OSI Approved',
+    long_description=open('README.txt').read(),
+)
+
