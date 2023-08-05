958b717c6a664017bd0cf6763e76dbf5b08d87f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:33 +0000

    Add package poi.receivemail/1.7/poi.receivemail-1.7.tar.gz

---
+++ packages/poi.receivemail/poi.receivemail-1.7.tar.gz/poi.receivemail-1.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '1.7'
+
+setup(name='poi.receivemail',
+      version=version,
+      description="Receive email in the Poi issue tracker",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Maurits van Rees',
+      author_email='maurits@vanrees.org',
+      url='http://svn.plone.org/svn/collective/poi.receivemail',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['poi'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
