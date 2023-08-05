4831d4d6fb088d55b9db807106abd1e6a8b27297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:08 +0000

    Add package HalWeb/0.6.0/HalWeb-0.6.0.tar.gz

---
+++ packages/HalWeb/HalWeb-0.6.0.tar.gz/HalWeb-0.6.0/src/halicea/baseProject/lib/halicea/baseRepositories.py
@@ -0,0 +1,6 @@
+from google.appengine.ext import db
+class GaeRepo(object):
+    t=None
+    save = lambda *args: db.put(*args)
+    delete = lambda *args:db.delete(*args)
+    all = lambda t:db.all(t)
