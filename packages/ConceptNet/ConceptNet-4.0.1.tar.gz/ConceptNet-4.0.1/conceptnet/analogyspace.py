360e2bd6e9be687db31107635df5192006d8363e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:38 +0000

    Add package ConceptNet/4.0.1/ConceptNet-4.0.1.tar.gz

---
+++ packages/ConceptNet/ConceptNet-4.0.1.tar.gz/ConceptNet-4.0.1/conceptnet/analogyspace.py
@@ -0,0 +1,1 @@
+raise ImportError("conceptnet.analogyspace is deprecated. See http://csc.media.mit.edu/docs/divisi2/tutorial_aspace.html for how to use Divisi2 to run AnalogySpace.")
---
+++ packages/ConceptNet/ConceptNet-4.0.1.tar.gz/ConceptNet-4.0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+packages = find_packages(exclude=['maint'])
+version_str = '4.0.1'
+
+setup(  name='ConceptNet',
+        version=version_str,
+        description='A Python API to a Semantic Network Representation of the Open Mind Common Sense Project',
+        author='Catherine Havasi, Robert Speer, Jason Alonso, and Kenneth Arnold',
+        author_email='conceptnet@media.mit.edu',
+        url='http://conceptnet.media.mit.edu/',
+        packages=packages,
+        include_package_data=False,
+        install_requires=['csc-utils >= 0.6', 'django', 'simplenlp'],
+        # Metadata
+        license = "GPL v3",
+        )
