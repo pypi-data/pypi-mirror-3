63d6f6fb993fbfdda9e0d4a5b114d14c75d8dbd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:38 +0000

    Add package Poundpay/0.2.5/Poundpay-0.2.5.tar.gz

---
+++ packages/Poundpay/Poundpay-0.2.5.tar.gz/Poundpay-0.2.5/poundpay/payments.py
@@ -0,0 +1,100 @@
+from .resource import Resource
+
+
+class PaymentError(Exception):
+    pass
+
+
+class PaymentEscrowError(PaymentError):
+    """Thrown when an error occurs while attempting to ESCROW a payment
+
+    """
+    pass
+
+
+class PaymentReleaseError(PaymentError):
+    """Thrown when an error occurs while attempting to RELEASE a payment
+
+    """
+    pass
+
+
+class PaymentCancelError(PaymentError):
+    """Thrown when an error occurs while attempting to CANCEL a payment
+
+    """
+    pass
+
+
+_error_str_fmt = 'Payment status is {}. Only {} payments may be {}'
+
+
+class Payment(Resource):
+    """The Payment resource, represented by a RESTful resource located at
+    ``/payments``.
+
+    """
+    _name = 'payments'
+
+    def versions(self):
+        """Retrieves the list of past versions of the payment.
+
+        Each item in the list is itself a Payment object.
+        """
+        cls = self.__class__
+
+        versions = cls.client.get(cls._get_path(self.sid) + '/history')
+        versions_json = versions.json['payment_history']
+
+        return [cls(**version) for version in versions_json]
+
+    def escrow(self):
+        """Escrows an ``AUTHORIZED`` payment by charging the authorized
+        payment method associated with the ``AUTHORIZED`` payment
+
+        .. note::
+
+           a :exc:`~poundpay.payments.PaymentEscrowError` is thrown if the
+           Payment's status is not ``AUTHORIZED``
+
+        """
+        if self.status != 'AUTHORIZED':
+            msg = _error_str_fmt.format(self.status, 'AUTHORIZED', 'ESCROWED')
+            raise PaymentEscrowError(msg)
+
+        self.status = 'ESCROWED'
+        self.save()
+
+    def release(self):
+        """Releases an ``ESCROWED`` payment by paying out the funds to
+        a PoundPay account.
+
+        .. note::
+
+           a :exc:`~poundpay.payments.PaymentReleaseError` is thrown
+           if the Payment's status is not ``ESCROWED``
+
+        """
+        if self.status != 'ESCROWED':
+            msg = _error_str_fmt.format(self.status, 'ESCROWED', 'RELEASED')
+            raise PaymentReleaseError(msg)
+
+        self.status = 'RELEASED'
+        self.save()
+
+    def cancel(self):
+        """Cancels an ``ESCROWED`` payment by refunding the payer. All
+        PoundPay fees are refunded back to the developer, as well.
+
+        .. note::
+
+           a :exc:`~poundpay.payments.PaymentCancelError` is thrown
+           if the Payment's status is not ``ESCROWED``
+
+        """
+        if self.status != 'ESCROWED':
+            msg = _error_str_fmt.format(self.status, 'ESCROWED', 'CANCELED')
+            raise PaymentCancelError(msg)
+
+        self.status = 'CANCELED'
+        self.save()
