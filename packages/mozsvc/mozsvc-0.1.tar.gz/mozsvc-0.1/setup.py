8bd9747f6b8853b02bef98179d86ab0f62c73936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:00 +0000

    Add package mozsvc/0.1/mozsvc-0.1.tar.gz

---
+++ packages/mozsvc/mozsvc-0.1.tar.gz/mozsvc-0.1/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = ['pyramid', 'pyramid_debugtoolbar', 'simplejson']
+
+
+setup(name='mozsvc',
+      version='0.1',
+      description='Various utilities for Mozilla apps',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Mozilla Services',
+      author_email='services-dev@mozilla.org',
+      url='https://github.com/mozilla-services/mozservices',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="mozsvc",
+      paster_plugins=['pyramid'])
