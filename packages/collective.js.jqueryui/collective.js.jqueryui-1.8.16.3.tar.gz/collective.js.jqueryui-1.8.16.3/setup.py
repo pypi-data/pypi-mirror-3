402eea7a6b73b32d781d3ae2678415fb76a4dc52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:21 +0000

    Add package collective.js.jqueryui/1.8.16.3/collective.js.jqueryui-1.8.16.3.tar.gz

---
+++ packages/collective.js.jqueryui/collective.js.jqueryui-1.8.16.3.tar.gz/collective.js.jqueryui-1.8.16.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.8.16.3'
+
+setup(name='collective.js.jqueryui',
+      version=version,
+      description="JQueryUI ready to be included in portal_javascript for Plone 4 by Makina Corpus",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "UPGRADE.txt")).read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='toutpt@gmail.com',
+      url='https://github.com/collective/collective.js.jqueryui',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
