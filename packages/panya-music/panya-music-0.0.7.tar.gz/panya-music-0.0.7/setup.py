ec873ae7191ba4c164ef9825bb1aad11096f5b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:35 +0000

    Add package panya-music/0.0.7/panya-music-0.0.7.tar.gz

---
+++ packages/panya-music/panya-music-0.0.7.tar.gz/panya-music-0.0.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='panya-music',
+    version='0.0.7',
+    description='Panya music app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/panya-music',
+    packages = find_packages(),
+    install_requires = [
+        'django-ckeditor',
+        'django-preferences',
+        'panya',
+        'pylast',
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
