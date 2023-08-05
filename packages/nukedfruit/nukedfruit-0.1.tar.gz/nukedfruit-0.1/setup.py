dba4a5ca3e02fcfe33ba36fa118720e1626041c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:33 +0000

    Add package nukedfruit/0.1/nukedfruit-0.1.tar.gz

---
+++ packages/nukedfruit/nukedfruit-0.1.tar.gz/nukedfruit-0.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup (
+    name='nukedfruit',
+    version='0.1',
+    packages=['nukedfruit',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike licence',
+    long_description=open('README').read())
