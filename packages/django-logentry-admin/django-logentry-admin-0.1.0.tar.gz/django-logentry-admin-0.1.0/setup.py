12eb719e208c6e61d05bec2ec4565d313161a15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:13 +0000

    Add package django-logentry-admin/0.1.0/django-logentry-admin-0.1.0.tar.gz

---
+++ packages/django-logentry-admin/django-logentry-admin-0.1.0.tar.gz/django-logentry-admin-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='django-logentry-admin',
+    author='Yuri Prezument',
+    author_email='y@yprez.com',
+    version='0.1.0',
+    packages=['logentry_admin'],
+    license='LICENSE.txt',
+    url='http://github.com/yprez/django-logentry-admin',
+    description='Add Django LogEntries the the Django admin site',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'Django >= 1.3',
+    ],
+)
