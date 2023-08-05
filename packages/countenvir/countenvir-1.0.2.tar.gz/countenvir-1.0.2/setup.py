5a729ab3b22d3d8073cae42db39e58a0e50ed9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:43 +0000

    Add package countenvir/1.0.2/countenvir-1.0.2.tar.gz

---
+++ packages/countenvir/countenvir-1.0.2.tar.gz/countenvir-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'countenvir',
+	version		= '1.0.2',
+	py_modules	= ['countenvir'],
+	author		= 'Jochen Demmer',
+	author_email	= 'info@csdemmer.de',
+	url		= 'http://www.csdemmer.de',
+	license		= 'GPLv3',
+	description	= 'A simple environment to count the occurrences of the 26 latin letters')
