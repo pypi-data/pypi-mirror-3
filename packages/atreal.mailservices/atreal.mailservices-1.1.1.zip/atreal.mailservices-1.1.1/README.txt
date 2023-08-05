d7f181f35c136ee3ab798b0915884f1e9537ea33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:13 +0000

    Add package atreal.mailservices/1.1.1/atreal.mailservices-1.1.1.zip

---
+++ packages/atreal.mailservices/atreal.mailservices-1.1.1.zip/atreal.mailservices-1.1.1/README.txt
@@ -0,0 +1,89 @@
+.. contents::
+
+Overview
+========
+
+MailServices allows to send emails to portal's users and groups.
+
+
+Description
+===========
+
+* This very lightweight plone product let's you mailing people from your
+  portal, by groups or by selecting users.
+
+* It equally allows you to mail people that are'nt members of the portals,
+  but WITHOUT ANY VERIFICATION! (for the moment).
+
+* Otherwise, it adds a document action, which works in the
+  same way that classic plone 'send_to' action, but let's you choosing your
+  target by scanning the portal groups/users.
+
+* It also adds an user action, which works in the same way as described above.
+
+* Manager can choose in ControlPanel :
+  - default subject,
+  - default body,
+  - if he will receive a copy of each mail sent,
+  - if users are able to send mails to non-portal's users.
+
+* Mails are sent from user email address. For security purposes, this can't be
+  modified.
+ 
+ 
+Important
+=========
+
+Permission is set for 'Manager and 'Member'
+
+
+Note
+====
+
+Bcc support is currently implemented in a branche. However, due to MailHost behaviour,
+all recipients can see the bcc list.
+We are working on this issue, feel free to send your proposals in case you have some.
+
+
+Authors
+=======
+
+|atreal|_
+
+* `atReal Team`_
+
+  - Matthias Broquet [tiazma]
+  - Florent Michon [f10w]
+
+.. |atreal| image:: http://www.atreal.fr/medias/atreal-logo-48.png
+.. _atreal: http://www.atreal.fr/
+.. _atReal Team: mailto:contact@atreal.fr
+
+
+Contributors
+============
+
+* `Thanks to`
+
+    - Thomas DESVENAIN [thomasdesvenain]
+    - Wouter VANDEN HOVE [WouterVH]
+
+* `atReal Team`_
+
+  - Romain BEYLERIAN [rbeylerian]
+
+  .. _atReal Team: mailto:contact@atreal.fr
+
+
+Credits
+=======
+
+* Sponsorised by ML-COM - www.ml-com.com (and some international research labs)
+
+
+TODO
+====
+
+* Make BCC mailing work.
+* Modify template mechanism to adopt the same granularity that's provided in 
+  collective.contentrule.mail.
---
+++ packages/atreal.mailservices/atreal.mailservices-1.1.1.zip/atreal.mailservices-1.1.1/atreal/mailservices/browser/controlpanel.py
@@ -0,0 +1,77 @@
+from zope.interface import Interface
+from zope.component import adapts
+from zope.interface import implements
+from zope.schema import SourceText, TextLine, Bool
+from zope.formlib import form
+
+from Products.CMFDefault.formlib.schema import ProxyFieldProperty
+from Products.CMFDefault.formlib.schema import SchemaAdapterBase
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+
+from atreal.mailservices import MailServicesMessageFactory as _
+from plone.app.controlpanel.form import ControlPanelForm
+
+
+class IMailServicesSchema(Interface):
+
+    # XXX : Support BCC
+    #mailservices_privacy_mode = Bool(
+    #    title=_(u'label_privacy_mode',
+    #            default=u"Privacy Mode ?"),
+    #    description=_(u"help_privacy_mode",
+    #                  default=u"This option allows to the user to choose cc to send a mail."),
+    #    default=True,
+    #    required=True)
+
+    mailservices_admin_bcc = Bool(
+        title=_(u'label_admin_bcc',
+            default=u"Would you like the Administrator of this Plone Site receive all mails?"),
+        description=_(u"help_admin_bcc",
+            default=u"This will mail each mail sended with MailServices to Portal Administrator."),
+        default=True,
+        required=True)
+
+    mailservices_additionals = Bool(
+        title=_(u'label_mailservices_additionals',
+            default=u"Would you like Users can send mail to additionals recipients?"),
+        description=_(u"help_mailservices_additionals",
+            default=u"This will add additionals fields to MailServices form to add mail address manually."),
+        default=False,
+        required=True)
+
+    mailservices_subject = TextLine(
+        title=_(u'label_mailservices_subject',
+            default=u"Default Subject"),
+        description=_(u"help_mailservices_subject",
+            default=u"Default subject. You can use ${portal_title}, ${object_title}, ${object_url} to replace with values."),
+        required=False)
+
+    mailservices_body = SourceText(
+        title=_(u'label_mailservices_body',
+            default=u"Message template"),
+        description=_(u"help_mailservices_body",
+            default=u"Default body message. You can use ${portal_title}, ${object_title}, ${object_url} to replace with values."),
+        required=False)
+
+
+class MailServicesControlPanelAdapter(SchemaAdapterBase):
+
+    adapts(IPloneSiteRoot)
+    implements(IMailServicesSchema)
+
+    def __init__(self, context):
+        super(MailServicesControlPanelAdapter, self).__init__(context)
+
+    # XXX : Support BCC
+    #mailservices_privacy_mode = ProxyFieldProperty(IMailServicesSchema['mailservices_privacy_mode'])
+    mailservices_admin_bcc = ProxyFieldProperty(IMailServicesSchema['mailservices_admin_bcc'])
+    mailservices_additionals = ProxyFieldProperty(IMailServicesSchema['mailservices_additionals'])
+    mailservices_subject = ProxyFieldProperty(IMailServicesSchema['mailservices_subject'])
+    mailservices_body = ProxyFieldProperty(IMailServicesSchema['mailservices_body'])
+
+
+class MailServicesControlPanel(ControlPanelForm):
+    form_fields = form.FormFields(IMailServicesSchema)
+    label = _("MailServices settings")
+    description = _("MailServices settings for this site.")
+    form_name = _("MailServices settings")
