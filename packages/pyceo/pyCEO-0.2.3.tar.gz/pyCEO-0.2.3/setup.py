e6030536ade023baf8ef15c490520d48afc42167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:36 +0000

    Add package pyceo/0.2.3/pyCEO-0.2.3.tar.gz

---
+++ packages/pyceo/pyCEO-0.2.3.tar.gz/pyCEO-0.2.3/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+
+ROOTDIR = os.path.dirname(__file__)
+README = os.path.join(ROOTDIR, 'README.md')
+
+
+def run_tests():
+    import sys, subprocess
+    errno = subprocess.call([sys.executable, 'run_tests.py'])
+    raise SystemExit(errno)
+
+
+setup(
+    name='pyCEO',
+    version='0.2.3',
+    author='Juan-Pablo Scaletti',
+    author_email='juanpablo@lucumalabs.com',
+    packages=['pyceo'],
+    package_data={'pyceo': [
+            '*.py',
+            '*.md',
+        ]},
+    zip_safe=False,
+    url='http://github.com/lucuma/pyCEO',
+    license='MIT license (http://www.opensource.org/licenses/mit-license.php)',
+    description='Create management scripts for your applications so you can do things like `python manage.py runserver`.',
+    long_description=open(README).read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite='__main__.run_tests'
+)
