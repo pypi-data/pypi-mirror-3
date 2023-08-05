86df4c2643d033e6e9f88df63f904b66dcb1212c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:39 +0000

    Add package fluidinfo.py/1.1.3/fluidinfo.py-1.1.3.zip

---
+++ packages/fluidinfo.py/fluidinfo.py-1.1.3.zip/fluidinfo.py-1.1.3/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fluidinfo.py',
+      version='1.1.3',
+      description="A thin wrapper for Fluidinfo's RESTful API (formerly"\
+        " fluiddb.py)",
+      author='Nicholas Tollervey (based upon work by Sanghyeon Seo)',
+      author_email='ntoll@ntoll.org',
+      url='http://fluidinfo.com',
+      py_modules=['fluidinfo',],
+      license='MIT',
+      requires=['httplib2',],
+      long_description=open('README.rst').read(),
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Database',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'Topic :: Software Development :: Libraries']
+)
