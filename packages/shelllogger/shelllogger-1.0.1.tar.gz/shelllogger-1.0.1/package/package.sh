205869357656aa7ba99e0f2c97226620fd307182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:37 +0000

    Add package shelllogger/1.0.1/shelllogger-1.0.1.tar.gz

---
+++ packages/shelllogger/shelllogger-1.0.1.tar.gz/shelllogger-1.0.1/package/package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+mkdir shelllogger
+cp ../readme.txt ../shelllogger ../setup.py ../validate.py ../view.py shelllogger/
+rst2html.py shelllogger/readme.txt > shelllogger/readme.html
+svnversion > shelllogger/VERSION
+zip shelllogger-r`svnversion` shelllogger/*
+rm -rf shelllogger/
---
+++ packages/shelllogger/shelllogger-1.0.1.tar.gz/shelllogger-1.0.1/setup.py
@@ -0,0 +1,15 @@
+'''
+Setup file for installing shelllogger
+'''
+from setuptools import setup
+
+setup(
+ name = "shelllogger",
+ version = "1.0.1",
+ author = "Lorin Hochstein, Prakashkumar Thiagarajan",
+ author_email = "lorinh@gmail.com, tprak@seas.upenn.edu",
+ url = "http://code.google.com/p/shelllogger/",
+ packages = ["sl"],
+ scripts = ['shelllogger','sl-validate','sl-view'],
+ description = "Logs shell commands, similar to Unix script program",
+)
