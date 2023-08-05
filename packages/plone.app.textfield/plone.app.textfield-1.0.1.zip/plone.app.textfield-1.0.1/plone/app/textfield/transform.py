f067e75c3ab4a7ec3c37a934a50a8872ac19c7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:54 +0000

    Add package plone.app.textfield/1.0.1/plone.app.textfield-1.0.1.zip

---
+++ packages/plone.app.textfield/plone.app.textfield-1.0.1.zip/plone.app.textfield-1.0.1/plone/app/textfield/transform.py
@@ -0,0 +1,43 @@
+from zope.interface import implements
+
+from zope.app.component.hooks import getSite
+from ZODB.POSException import ConflictError
+
+from plone.app.textfield.interfaces import ITransformer, TransformError
+
+from Products.CMFCore.utils import getToolByName
+
+class PortalTransformsTransformer(object):
+    """Invoke portal_transforms to perform a conversion
+    """
+    
+    implements(ITransformer)
+    
+    def __init__(self, context):
+        self.context = context
+    
+    def __call__(self, value, mimeType):
+        
+        # shortcut if we already have the right value
+        if mimeType is value.mimeType:
+            return value.output
+        
+        site = getSite()
+        
+        transforms = getToolByName(site, 'portal_transforms', None)
+        if transforms is None:
+            raise TransformError("Cannot find portal_transforms tool")
+        
+        try:
+            data = transforms.convertTo(mimeType,
+                                        value.raw_encoded,
+                                        mimetype=value.mimeType,
+                                        context=self.context,
+                                        object=value._raw_holder, # portal_transforms caches on this object
+                                        encoding=value.encoding)
+            output = data.getData()
+            return output.decode(value.encoding)
+        except ConflictError:
+            raise
+        except Exception, e:
+            raise TransformError("Error during transformation", e)
