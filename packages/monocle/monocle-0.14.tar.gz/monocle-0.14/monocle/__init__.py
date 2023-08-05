d541c99a733acbc337d02116530ad22a42e7b350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:02 +0000

    Add package monocle/0.14/monocle-0.14.tar.gz

---
+++ packages/monocle/monocle-0.14.tar.gz/monocle-0.14/monocle/__init__.py
@@ -0,0 +1,11 @@
+import sys
+
+import core
+from core import _o, o, launch, Return, InvalidYieldException
+
+VERSION = '0.14'
+
+_stack_name = None
+def init(stack_name):
+    global _stack_name
+    _stack_name = stack_name
