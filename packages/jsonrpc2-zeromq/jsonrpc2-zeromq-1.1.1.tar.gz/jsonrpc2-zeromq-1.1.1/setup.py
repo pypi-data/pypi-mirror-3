99d4a59285005d7e30886eca04a29167db33e737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:32 +0000

    Add package jsonrpc2-zeromq/1.1.1/jsonrpc2-zeromq-1.1.1.tar.gz

---
+++ packages/jsonrpc2-zeromq/jsonrpc2-zeromq-1.1.1.tar.gz/jsonrpc2-zeromq-1.1.1/setup.py
@@ -0,0 +1,41 @@
+# Part of the jsonrpc2-zeromq-python project.
+# (c) 2012 Wireless Innovation Ltd, All Rights Reserved.
+# Please see the LICENSE file in the root of this project for license
+# information.
+
+from setuptools import setup
+
+description = "Library for JSON-RPC 2.0 over ZeroMQ"
+
+import os
+try:
+    long_description = open(os.path.join(os.path.dirname(__file__),
+                                         'README.rst')).read()
+except IOError:
+    long_description = description
+
+version = "1.1.1"
+
+setup(
+    name = "jsonrpc2-zeromq",
+    version = version,
+    description = description,
+    long_description = long_description,
+    url = "https://github.com/wiltd/python-jsonrpc2-zeromq",
+    download_url = "https://github.com/wiltd/"
+                       "jsonrpc2-zeromq-python/tarball/v%s" % (version,),
+    classifiers = [
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        ],
+    author = "Dan Brown",
+    author_email = "dan@danwb.net",
+    packages = ["jsonrpc2_zeromq"],
+    install_requires=[
+        "distribute",
+        "pyzmq<2.2",
+        ],
+    )
+
