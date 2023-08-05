5228c5d53b63fdf820436ac7df324fe70b9ec430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:35 +0000

    Add package python-gettext/1.2/python-gettext-1.2.zip

---
+++ packages/python-gettext/python-gettext-1.2.zip/python-gettext-1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+version = '1.2'
+
+
+setup(name='python-gettext',
+      version=version,
+      description="Python Gettext po to mo file compiler.",
+      long_description=open("README.rst").read() +  "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Software Development :: Localization',
+      ],
+      keywords='Python Gettext Msgctxt',
+      author='Hanno Schlichting',
+      author_email='hanno@hannosch.eu',
+      url='http://pypi.python.org/pypi/python-gettext',
+      license='BSD',
+      packages=['pythongettext', 'pythongettext.tests'],
+      install_requires=['unittest2'],
+      include_package_data = True,
+      zip_safe = False,
+      )
