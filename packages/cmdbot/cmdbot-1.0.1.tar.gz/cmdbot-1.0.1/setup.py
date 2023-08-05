264bae398ba8b55f4c059b73d30c84424e1f34c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:27 +0000

    Add package cmdbot/1.0.1/cmdbot-1.0.1.tar.gz

---
+++ packages/cmdbot/cmdbot-1.0.1.tar.gz/cmdbot-1.0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README.rst') as fd:
+    long_description = fd.read()
+
+setup(
+    name='cmdbot',
+    version='1.0.1',
+    packages=['cmdbot'],
+    url='https://github.com/brunobord/cmdbot/',
+    author="Bruno Bord",
+    author_email='bruno@jehaisleprintemps.net',
+    license="Public Domain (WTFPL)",
+    platforms='any',
+    description="An IRC Bot with a `cmd` attitude",
+    long_description=long_description,
+)
