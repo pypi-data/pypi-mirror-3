1fffa81eb9422b66dc59f1a37b3169187bad5429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package dosca/0.0.2/dosca-0.0.2.tar.gz

---
+++ packages/dosca/dosca-0.0.2.tar.gz/dosca-0.0.2/dosca/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from .dosca import parse, ParseError
+
+
+__all__ = ('parse', 'ParseError')
+
+
+__version__ = '0.0.2'
+__author__ = 'Roma Sokolov'
+__license__ = 'MIT'
