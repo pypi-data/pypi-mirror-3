d6d79483cc85c8080fb3799a986416a28736e74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:19 +0000

    Add package publicsuffix/1.0.1/publicsuffix-1.0.1.tar.gz

---
+++ packages/publicsuffix/publicsuffix-1.0.1.tar.gz/publicsuffix-1.0.1/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+from distutils.core import Command, setup
+from distutils.command.install import INSTALL_SCHEMES
+import unittest
+
+UNITTESTS = [
+		"tests", 
+	]
+
+class TestCommand(Command):
+	user_options = [ ]
+
+	def initialize_options(self):
+		pass
+
+	def finalize_options(self):
+		pass
+
+	def run(self):
+		suite = unittest.TestSuite()
+
+		suite.addTests( 
+			unittest.defaultTestLoader.loadTestsFromNames( 
+								UNITTESTS ) )
+
+		result = unittest.TextTestRunner(verbosity=2).run(suite)
+
+# Install data file into the same path as the module
+for scheme in INSTALL_SCHEMES.values():
+	scheme['data'] = scheme['purelib']
+
+setup(name='publicsuffix',
+	version='1.0.1',
+	description='Get a public suffix for a domain name using the Public Suffix List.',
+	license='MIT',
+	long_description=open("README").read(),
+	author=u'Toma\N{latin small letter z with caron} \N{latin capital letter s with caron}olc',
+	author_email='tomaz.solc@tablix.org',
+
+	py_modules = ['publicsuffix'],
+	data_files = [('', ['publicsuffix.txt'])],
+	provides = [ 'publicsuffix' ],
+
+	cmdclass = { 'test': TestCommand }
+)
