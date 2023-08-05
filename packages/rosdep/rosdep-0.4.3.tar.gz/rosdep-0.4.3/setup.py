d792b305a7a63c19413f45c43d16d14ca88b1071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:10 +0000

    Add package rosdep/0.4.3/rosdep-0.4.3.tar.gz

---
+++ packages/rosdep/rosdep-0.4.3.tar.gz/rosdep-0.4.3/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(name='rosdep',
+      version= '0.4.3',
+      packages=['rosdep2', 'rosdep2.platforms'],
+      package_dir = {'':'src'},
+#      data_files=[('man/man1', ['doc/man/rosdep.1'])],
+      scripts = ['scripts/rosdep', 'scripts/rosdep-source'],
+      author = "Tully Foote, Ken Conley", 
+      author_email = "foote@willowgarage.com, kwc@willowgarage.com",
+      url = "http://www.ros.org/wiki/rosdep",
+      download_url = "http://pr.willowgarage.com/downloads/rosdep/", 
+      keywords = ["ROS"],
+      classifiers = [
+        "Programming Language :: Python", 
+        "License :: OSI Approved :: BSD License" ],
+      description = "rosdep system dependency installation tool", 
+      long_description = """\
+Command-line tool for installing system dependencies on a variety of platforms.
+""",
+      license = "BSD"
+      )
