d43b84f7581ed7880b876e9c920bec7241c3c64b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:13 +0000

    Add package StorageAlchemy/0.3/StorageAlchemy-0.3.tar.gz

---
+++ packages/StorageAlchemy/StorageAlchemy-0.3.tar.gz/StorageAlchemy-0.3/setup.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='StorageAlchemy',
+    version='0.3',
+    description='Use sqlalchemy to commit/rollback changes to files on the filesystem or any other external storage.',
+    url='http://github.com/epegzz/StorageAlchemy',
+
+    author='Daniel Schäfer',
+    author_email='epegzz@gmail.com',
+    license='BSD-3',
+
+    packages=['storagealchemy'],
+
+    install_requires=\
+        [ 'sqlalchemy'
+        , 'sqlahelper'
+        ],
+
+    classifiers=[
+       'Development Status :: 5 - Production/Stable',
+       'Intended Audience :: Developers',
+       'License :: OSI Approved :: BSD License',
+       'Natural Language :: English',
+       'Operating System :: OS Independent',
+       'Programming Language :: Python :: 2.5',
+       'Programming Language :: Python :: 2.6',
+       'Programming Language :: Python :: 2.7',
+       'Topic :: Software Development :: Libraries :: Python Modules',
+   ],
+)
