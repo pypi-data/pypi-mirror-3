63d86662c57d45290b46eec1c2c4f2ff4a103177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:06 +0000

    Add package factorial/1.0.0/factorial-1.0.0.zip

---
+++ packages/factorial/factorial-1.0.0.zip/factorial-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name         =  'factorial',
+    version      =  '1.0.0',
+    py_modules   =  ['factorial007'],
+    author       =  'bharath',
+    author_email =  'bharath_ves@hotmail.com',
+    url          =  'http://3novices.blogspot.com',
+    description  =  'A simple printer of factorial of a number',
+)
