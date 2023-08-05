c64da5a1ad05a94c398595754d38bcf97eadab94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:40 +0000

    Add package pyRserve/0.5/pyRserve-0.5.tar.gz

---
+++ packages/pyRserve/pyRserve-0.5.tar.gz/pyRserve-0.5/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+# NOTE: Other files to be included are specified in MANIFEST.in
+
+## Get long_description from intro.txt:
+here = os.path.dirname(os.path.abspath(__file__))
+f = open(os.path.join(here, 'doc', 'intro.rst'))
+long_description = f.read()
+f.close()
+
+setup(name='pyRserve',
+      version='0.5',     # ALWAYS UPDATE __version__  in __init__.py and conf.py for sphinx!!!
+      description='A Python client to remotely access R statistic package via Rserve',
+      long_description=long_description,
+      author='Ralph Heinkel',
+      author_email='rh [at] ralph-heinkel.com',
+      url='http://pypi.python.org/pypi/pyRserve/',
+      packages=['pyRserve'],
+      install_requires=['numpy'],
+      license='MIT license',
+      platforms=['unix', 'linux', 'cygwin', 'win32'],
+      classifiers=[  'Development Status :: 4 - Beta',
+                     'Environment :: Console',
+                     'License :: OSI Approved :: MIT License',
+                     'Operating System :: POSIX',
+                     'Operating System :: Microsoft :: Windows',
+                     'Programming Language :: Python',
+                     'Intended Audience :: Developers',
+                     'Topic :: Scientific/Engineering :: Information Analysis',
+                     'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+                     'Topic :: Scientific/Engineering :: Mathematics',
+                     ],
+     )
