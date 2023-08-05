a5d6e090d250a9e60e8640686f202d9a64252c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:26 +0000

    Add package scalarTransportFlux/2.0/scalarTransportFlux-2.0.tar.gz

---
+++ packages/scalarTransportFlux/scalarTransportFlux-2.0.tar.gz/scalarTransportFlux-2.0/scalarTransportFlux/r1_6_dev/__init__.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+#--------------------------------------------------------------------------------------
+## pythonFlu - Python wrapping for OpenFOAM C++ API
+## Copyright (C) 2010- Alexey Petrov
+## Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+## 
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+## 
+## See http://sourceforge.net/projects/pythonflu
+##
+## Author : Alexey PETROV
+##
+
+
+#----------------------------------------------------------------------------
+def _createFields( runTime, mesh ):
+    from Foam.OpenFOAM import ext_Info, nl
+    from Foam.OpenFOAM import IOdictionary, IOobject, word, fileName
+    from Foam.finiteVolume import volScalarField
+    ext_Info() << "Reading field T\n" << nl
+    T = volScalarField( IOobject( word( "T" ),
+                                  fileName( runTime.timeName() ),
+                                  mesh,
+                                  IOobject.MUST_READ,
+                                  IOobject.AUTO_WRITE ),
+                        mesh )
+
+    ext_Info() << "Reading field U\n" << nl
+    from Foam.finiteVolume import volVectorField
+    U = volVectorField( IOobject( word( "U" ),
+                                  fileName( runTime.timeName() ),
+                                  mesh,
+                                  IOobject.MUST_READ,
+                                  IOobject.AUTO_WRITE ),
+                        mesh )
+
+
+    ext_Info() << "Reading transportProperties\n" << nl
+    transportProperties = IOdictionary( IOobject( word( "transportProperties" ),
+                                                  fileName( runTime.constant() ),
+                                                  mesh,
+                                                  IOobject.MUST_READ,
+                                                  IOobject.NO_WRITE ) )
+
+
+    ext_Info() << "Reading diffusivity D\n" << nl
+    from Foam.OpenFOAM import dimensionedScalar
+    DT = dimensionedScalar( transportProperties.lookup( word( "DT" ) ) )
+ 
+    from Foam.finiteVolume.cfdTools.incompressible import createPhi
+    phi = createPhi( runTime, mesh, U )
+           
+    return T, U, transportProperties, DT, phi 
+
+
+#--------------------------------------------------------------------------------------
+def main_standalone( argc, argv ):
+
+    from Foam.OpenFOAM.include import setRootCase
+    args = setRootCase( argc, argv )
+
+    from Foam.OpenFOAM.include import createTime
+    runTime = createTime( args )
+
+    from Foam.OpenFOAM.include import createMesh
+    mesh = createMesh( runTime )
+    
+    T, U, transportProperties, DT, phi = _createFields( runTime, mesh )
+
+    from Foam.OpenFOAM import ext_Info, nl
+    ext_Info() << "\nCalculating scalar transport\n" << nl
+        
+    from Foam.finiteVolume.cfdTools.incompressible import CourantNo
+    CoNum, meanCoNum, velMag = CourantNo( mesh, phi, runTime )
+    
+    while runTime.loop():
+        ext_Info() << "Time = " << runTime.timeName() << nl << nl
+        
+        from Foam.finiteVolume.cfdTools.general.include import readSIMPLEControls
+        simple, nNonOrthCorr, momentumPredictor, fluxGradp, transonic = readSIMPLEControls( mesh )
+        
+        from Foam.finiteVolume import solve
+        from Foam import fvm
+        for nonOrth in range ( nNonOrthCorr + 1 ):
+            solve( fvm.ddt( T ) + fvm.div( phi, T ) - fvm.laplacian( DT, T ) )
+            pass
+
+        runTime.write()
+        pass
+        
+    ext_Info() << "End\n" << nl 
+
+    import os
+    return os.EX_OK
+
+
+#--------------------------------------------------------------------------------------
+import sys, os
+from Foam import FOAM_VERSION, FOAM_BRANCH_VERSION
+if FOAM_BRANCH_VERSION( "dev", "==", "010600" ):
+   if __name__ == "__main__" :
+      argv = sys.argv
+      os._exit( main_standalone( len( argv ), argv ) )
+      pass
+   pass
+else:
+   from Foam.OpenFOAM import ext_Info
+   ext_Info()<< "\nTo use this solver, It is necessary to SWIG OpenFoam1.6 \n "
+   pass
+	
+	
+#--------------------------------------------------------------------------------------
+
