868b92e62dff17f03616308a9568e6325bf75c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:58 +0000

    Add package gecode-python/0.14/gecode-python-0.14.tar.gz

---
+++ packages/gecode-python/gecode-python-0.14.tar.gz/gecode-python-0.14/_gecode.cc
@@ -0,0 +1,2 @@
+#include "gecode/support/config.hpp"
+#include GECODE_VERSION
---
+++ packages/gecode-python/gecode-python-0.14.tar.gz/gecode-python-0.14/setup.py
@@ -0,0 +1,50 @@
+## -*- python -*-
+##=============================================================================
+## Copyright (C) 2011 by Denys Duchier
+##
+## This program is free software: you can redistribute it and/or modify it
+## under the terms of the GNU Lesser General Public License as published by the
+## Free Software Foundation, either version 3 of the License, or (at your
+## option) any later version.
+## 
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+## more details.
+## 
+## You should have received a copy of the GNU Lesser General Public License
+## along with this program.  If not, see <http:##www.gnu.org/licenses/>.
+##=============================================================================
+
+from distutils.core import setup
+from distutils.extension import Extension
+
+setup(
+    name='gecode-python',
+    description="bindings for the Gecode constraint-programming library",
+    author="Denys Duchier",
+    author_email="denys.duchier@univ-orleans.fr",
+    version="0.14",
+    url="https://launchpad.net/gecode-python",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Cython",
+        "Programming Language :: C++",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
+    package_dir={ "gecode" : "" },
+    py_modules=["gecode.__init__", "gecode.boundvar", "gecode.matrix"],
+    ext_modules=[ 
+        Extension("gecode._gecode",
+                  sources=["_gecode.cc"],
+                  define_macros=[("DISJUNCTOR",None)],
+                  libraries=["stdc++", "gecodeint", "gecodeset", "gecodesearch", "gecodekernel", "gecodesupport"],
+                  language="c++"
+                  ),
+        ]
+    )
