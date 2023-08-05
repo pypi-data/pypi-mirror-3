8a571af4a2cb8ee3588005067b07a374f017151b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package Cartridge/0.3/Cartridge-0.3.tar.gz

---
+++ packages/Cartridge/Cartridge-0.3.tar.gz/Cartridge-0.3/cartridge/shop/admin.py
@@ -0,0 +1,185 @@
+
+from copy import deepcopy
+
+from django.contrib import admin
+from django.db.models import ImageField
+from django.utils.translation import ugettext_lazy as _
+
+from mezzanine.core.admin import DisplayableAdmin, TabularDynamicInlineAdmin
+from mezzanine.pages.admin import PageAdmin
+
+from cartridge.shop.fields import MoneyField
+from cartridge.shop.forms import ProductAdminForm, ProductVariationAdminForm
+from cartridge.shop.forms import ProductVariationAdminFormset
+from cartridge.shop.forms import DiscountAdminForm, ImageWidget, MoneyWidget
+from cartridge.shop.models import Category, Product, ProductImage
+from cartridge.shop.models import ProductVariation, ProductOption, Order
+from cartridge.shop.models import OrderItem, Sale, DiscountCode
+
+
+# Lists of field names.
+option_fields = [f.name for f in ProductVariation.option_fields()]
+billing_fields = [f.name for f in Order._meta.fields
+    if f.name.startswith("billing_detail")]
+shipping_fields = [f.name for f in Order._meta.fields
+    if f.name.startswith("shipping_detail")]
+
+category_fieldsets = deepcopy(PageAdmin.fieldsets)
+category_fieldsets[0][1]["fields"][3:3] = ["content"]  # , "products"]
+category_fieldsets += ((_("Product filters"), {
+    "fields": ("options", "sale", ("price_min", "price_max"), "combined"),
+    "classes": ("collapse-closed",)},),)
+
+
+class CategoryAdmin(PageAdmin):
+    fieldsets = category_fieldsets
+    formfield_overrides = {ImageField: {"widget": ImageWidget}}
+    filter_horizontal = ("options",)  # "products", )
+
+
+class ProductVariationAdmin(admin.TabularInline):
+    verbose_name_plural = _("Current variations")
+    model = ProductVariation
+    fields = ("sku", "default", "num_in_stock", "unit_price", "sale_price",
+        "sale_from", "sale_to", "image")
+    extra = 0
+    formfield_overrides = {MoneyField: {"widget": MoneyWidget}}
+    form = ProductVariationAdminForm
+    formset = ProductVariationAdminFormset
+
+
+class ProductImageAdmin(TabularDynamicInlineAdmin):
+    model = ProductImage
+    formfield_overrides = {ImageField: {"widget": ImageWidget}}
+
+
+product_fieldsets = deepcopy(DisplayableAdmin.fieldsets)
+product_fieldsets[0][1]["fields"].extend(["available", "categories",
+                                          "content"])
+product_fieldsets = list(product_fieldsets)
+product_fieldsets.append((_("Other products"),
+    {"classes": ("collapse-closed",), "fields": ("related_products",
+                                                 "upsell_products")}))
+product_fieldsets.insert(1, (_("Create new variations"),
+    {"classes": ("create-variations",), "fields": option_fields}))
+
+
+class ProductAdmin(DisplayableAdmin):
+
+    list_display = ("admin_thumb", "title", "status", "available",
+                    "admin_link")
+    list_display_links = ("admin_thumb", "title")
+    list_editable = ("status", "available")
+    list_filter = ("status", "available", "categories")
+    filter_horizontal = ("categories", "related_products", "upsell_products")
+    search_fields = ("title", "content", "categories__title",
+                     "variations__sku")
+    inlines = (ProductImageAdmin, ProductVariationAdmin)
+    form = ProductAdminForm
+    fieldsets = product_fieldsets
+
+    def save_model(self, request, obj, form, change):
+        """
+        Store the product object for creating variations in save_formset.
+        """
+        super(ProductAdmin, self).save_model(request, obj, form, change)
+        self._product = obj
+
+    def save_formset(self, request, form, formset, change):
+        """
+        Create variations for selected options if they don't exist, manage the
+        default empty variation creating it if no variations exist or removing
+        it if multiple variations exist, and copy the pricing and image fields
+        from the default variation to the product.
+        """
+        super(ProductAdmin, self).save_formset(request, form, formset, change)
+        if isinstance(formset, ProductVariationAdminFormset):
+            options = dict([(f, request.POST.getlist(f)) for f in option_fields
+                if request.POST.getlist(f)])
+            self._product.variations.create_from_options(options)
+            self._product.variations.manage_empty()
+            self._product.copy_default_variation()
+
+
+class ProductOptionAdmin(admin.ModelAdmin):
+    ordering = ("type", "name")
+    list_display = ("type", "name")
+    list_display_links = ("type",)
+    list_editable = ("name",)
+    list_filter = ("type",)
+    search_fields = ("type", "name")
+    radio_fields = {"type": admin.HORIZONTAL}
+
+
+class OrderItemInline(admin.TabularInline):
+    verbose_name_plural = _("Items")
+    model = OrderItem
+    extra = 0
+    formfield_overrides = {MoneyField: {"widget": MoneyWidget}}
+
+
+class OrderAdmin(admin.ModelAdmin):
+    ordering = ("status", "-id")
+    list_display = ("id", "billing_name", "total", "time", "status", "invoice")
+    list_editable = ("status",)
+    list_filter = ("status", "time")
+    list_display_links = ("id", "billing_name",)
+    search_fields = ["id", "status"] + billing_fields + shipping_fields
+    date_hierarchy = "time"
+    radio_fields = {"status": admin.HORIZONTAL}
+    inlines = (OrderItemInline,)
+    formfield_overrides = {MoneyField: {"widget": MoneyWidget}}
+    fieldsets = (
+        (_("Billing details"), {"fields": (tuple(billing_fields),)}),
+        (_("Shipping details"), {"fields": (tuple(shipping_fields),)}),
+        (None, {"fields": ("additional_instructions", ("shipping_total",
+            "shipping_type"), ("discount_total", "discount_code"),
+            "item_total", ("total", "status"))}),
+    )
+
+
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ("title", "active", "discount_deduct", "discount_percent",
+        "discount_exact", "valid_from", "valid_to")
+    list_editable = ("active", "discount_deduct", "discount_percent",
+        "discount_exact", "valid_from", "valid_to")
+    filter_horizontal = ("categories", "products")
+    formfield_overrides = {MoneyField: {"widget": MoneyWidget}}
+    form = DiscountAdminForm
+    fieldsets = (
+        (None, {"fields": ("title", "active")}),
+        (_("Apply to product and/or products in categories"),
+            {"fields": ("products", "categories")}),
+        (_("Reduce unit price by"),
+            {"fields": (("discount_deduct", "discount_percent",
+            "discount_exact"),)}),
+        (_("Sale period"), {"fields": (("valid_from", "valid_to"),)}),
+    )
+
+
+class DiscountCodeAdmin(admin.ModelAdmin):
+    list_display = ("title", "active", "code", "discount_deduct",
+        "discount_percent", "min_purchase", "free_shipping", "valid_from",
+        "valid_to")
+    list_editable = ("active", "code", "discount_deduct", "discount_percent",
+        "min_purchase", "free_shipping", "valid_from", "valid_to")
+    filter_horizontal = ("categories", "products")
+    formfield_overrides = {MoneyField: {"widget": MoneyWidget}}
+    form = DiscountAdminForm
+    fieldsets = (
+        (None, {"fields": ("title", "active", "code")}),
+        (_("Apply to product and/or products in categories"),
+            {"fields": ("products", "categories")}),
+        (_("Reduce unit price by"),
+            {"fields": (("discount_deduct", "discount_percent"),)}),
+        (None, {"fields": (("min_purchase", "free_shipping"),)}),
+        (_("Valid for"), {"fields": (("valid_from", "valid_to"),)}),
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductOption, ProductOptionAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Sale, SaleAdmin)
+admin.site.register(DiscountCode, DiscountCodeAdmin)
---
+++ packages/Cartridge/Cartridge-0.3.tar.gz/Cartridge-0.3/cartridge/shop/templatetags/shop_tags.py
@@ -0,0 +1,116 @@
+
+import locale
+from urllib import quote
+
+from django import template
+from django.template.defaultfilters import slugify
+
+from mezzanine.conf import settings
+
+from cartridge.shop.utils import set_locale
+
+
+register = template.Library()
+
+
+@register.filter
+def currency(value):
+    """
+    Format a value as currency according to locale.
+    """
+    set_locale()
+    if not value:
+        value = 0
+    if hasattr(locale, "currency"):
+        value = locale.currency(value)
+    else:
+        # based on locale.currency() in python >= 2.5
+        conv = locale.localeconv()
+        value = [conv["currency_symbol"], conv["p_sep_by_space"] and " " or "",
+            (("%%.%sf" % conv["frac_digits"]) % value).replace(".",
+            conv["mon_decimal_point"])]
+        if not conv["p_cs_precedes"]:
+            value.reverse()
+        value = "".join(value)
+    return value
+
+
+def _order_totals(context):
+    """
+    Add ``item_total``, ``shipping_total``, ``discount_total`` and
+    ``order_total`` to the template context. Use the order object for
+    email receipts, or the cart object for checkout.
+    """
+    if "order" in context:
+        for f in ("item_total", "shipping_total", "discount_total"):
+            context[f] = getattr(context["order"], f)
+    else:
+        context["item_total"] = context["request"].cart.total_price()
+        if context["item_total"] == 0:
+            # Ignore session if cart has no items, as cart may have
+            # expired sooner than the session.
+            context["discount_total"] = context["shipping_total"] = 0
+        else:
+            for f in ("shipping_type", "shipping_total", "discount_total"):
+                context[f] = context["request"].session.get(f, None)
+    context["order_total"] = context.get("item_total", None)
+    if context.get("shipping_total", None) is not None:
+        context["order_total"] += context["shipping_total"]
+    if context.get("discount_total", None) is not None:
+        context["order_total"] -= context["discount_total"]
+    return context
+
+
+@register.inclusion_tag("shop/order_totals.html", takes_context=True)
+def order_totals(context):
+    """
+    HTML version of order_totals.
+    """
+    return _order_totals(context)
+
+
+@register.inclusion_tag("shop/order_totals.txt", takes_context=True)
+def order_totals_text(context):
+    """
+    Text version of order_totals.
+    """
+    return _order_totals(context)
+
+
+@register.inclusion_tag("shop/product_sorting.html", takes_context=True)
+def product_sorting(context, products):
+    """
+    Renders the links for each product sort option.
+    """
+    sort_options = [(option[0], slugify(option[0])) for option in
+                                        settings.SHOP_PRODUCT_SORT_OPTIONS]
+    querystring = context["request"].REQUEST.get("query", "")
+    if querystring:
+        querystring = "&query=" + quote(querystring)
+    else:
+        del sort_options[0]
+    context.update({"selected_option": getattr(products, "sort"),
+                    "sort_options": sort_options, "querystring": querystring})
+    return context
+
+
+@register.inclusion_tag("shop/product_paging.html", takes_context=True)
+def product_paging(context, products):
+    """
+    Renders the links for each page number in a paginated list of products.
+    """
+    settings = context["settings"]
+    querystring = ""
+    for name in ("query", "sort"):
+        value = context["request"].REQUEST.get(name)
+        if value is not None:
+            querystring += "&%s=%s" % (name, quote(value))
+    page_range = products.paginator.page_range
+    page_links = settings.SHOP_MAX_PAGING_LINKS
+    if len(page_range) > page_links:
+        start = min(products.paginator.num_pages - page_links,
+            max(0, products.number - (page_links / 2) - 1))
+        page_range = page_range[start:start + page_links]
+    context.update({"products": products, "querystring": querystring,
+                    "page_range": page_range})
+    return context
