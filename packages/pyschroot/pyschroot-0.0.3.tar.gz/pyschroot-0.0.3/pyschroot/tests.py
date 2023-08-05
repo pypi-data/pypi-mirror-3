fa2faabd273d9010902ede73fc87f861d2f2eaaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package pyschroot/0.0.3/pyschroot-0.0.3.tar.gz

---
+++ packages/pyschroot/pyschroot-0.0.3.tar.gz/pyschroot-0.0.3/pyschroot/tests.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from command import Schroot
+
+
+class TestFunctions(unittest.TestCase):
+
+    def test_simple_command(self):
+        c = Schroot('wheezy')
+        c.run_cmd('ls', '-lah')
+
+
+if __name__ == '__main__':
+    unittest.main()
