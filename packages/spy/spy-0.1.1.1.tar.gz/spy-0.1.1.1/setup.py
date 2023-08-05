3e6212b90291c72dbbe430044168007958c6c206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:25 +0000

    Add package spy/0.1.1.1/spy-0.1.1.1.tar.gz

---
+++ packages/spy/spy-0.1.1.1.tar.gz/spy-0.1.1.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+templates = 'something'
+
+setup(
+    name='spy',
+    description='Super simple monitoring for long running jobs',
+    author='George Courtsunis',
+    author_email='gjcourt@gmail.com',
+    url='http://github.com/gjcourt/spy',
+    version='0.1.1.1',
+    packages=['spy', 'spy.server', 'spy.server.templates'],
+    data_files=[
+        ('spy/server/templates', ['spy/server/templates/monitor.html']),
+    ],
+    requires=['Flask (==0.8)']
+    )
+
