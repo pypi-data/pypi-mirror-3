9096ee12ad5221559a3e4a18f6eb3e9c615c6990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:09 +0000

    Add package pyduck/0.3/pyduck-0.3.tar.gz

---
+++ packages/pyduck/pyduck-0.3.tar.gz/pyduck-0.3/pyduck/__init__.py
@@ -0,0 +1,9 @@
+'''
+pyduck
+
+@summary: Python implementation of Go-like interfaces for more robust duck typing
+@author: Karol Kuczmarski "Xion" 
+@license: MIT
+'''
+from interface import InterfaceMeta, Interface, implements, contains
+from decorators import Any, expects, returns
