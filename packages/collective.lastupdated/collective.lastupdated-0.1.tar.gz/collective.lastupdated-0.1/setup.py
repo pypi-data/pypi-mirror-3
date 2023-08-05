41ba4d979f1bd1acf89c8bc9af0dfbe480566312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package collective.lastupdated/0.1/collective.lastupdated-0.1.tar.gz

---
+++ packages/collective.lastupdated/collective.lastupdated-0.1.tar.gz/collective.lastupdated-0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='collective.lastupdated',
+      version=version,
+      description="Provides a viewlet which indicates when content has last been modified.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Dylan Jay',
+      author_email='software@pretaweb.com',
+      url='https://github.com/collective/collective.lastupdated',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
