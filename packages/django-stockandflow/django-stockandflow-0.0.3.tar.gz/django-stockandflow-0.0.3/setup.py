47d61b180b752f6ee3ccf66bd5ff52aef09d4bdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:08 +0000

    Add package django-stockandflow/0.0.3/django-stockandflow-0.0.3.tar.gz

---
+++ packages/django-stockandflow/django-stockandflow-0.0.3.tar.gz/django-stockandflow-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+long_description = (open('README.rst').read() + 
+                    open('CHANGES.rst').read() +
+                    open('TODO.rst').read())
+
+setup(
+    name='django-stockandflow',
+    version='0.0.3',
+    description='Django stock and flow tracking for business intelligence metrics',
+    long_description = long_description,
+    author='Jesse Heitler',
+    author_email='jesseh@i-iterate.com',
+    url='http://github.com/jesseh/django-stockandflow/',
+    packages=['stockandflow',],
+    license='LICENSE.txt',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+)
+
