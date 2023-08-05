5a99a8d72f58da99a706ba3daad4a7b4c211c638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:01 +0000

    Add package django-netauth/0.1.93/django-netauth-0.1.93.tar.gz

---
+++ packages/django-netauth/django-netauth-0.1.93.tar.gz/django-netauth-0.1.93/netauth/auth.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+
+from netauth.models import NetID
+
+
+class NetBackend(object):
+    """
+    Add this Authentication Backend to
+    AUTHENTICATION_BACKENDS tuple in your settings.py
+    """
+    supports_object_permissions = False
+    supports_anonymous_user = True
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
+    def authenticate(self, identity=None, provider=None):
+        """Authenticate user by net identity."""
+        user = None
+        if identity:
+            try:
+                user = NetID.objects.get(identity=identity, provider=provider).user
+            except NetID.DoesNotExist:
+                pass
+        return user
