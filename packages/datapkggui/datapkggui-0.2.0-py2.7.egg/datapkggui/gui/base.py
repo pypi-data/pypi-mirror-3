38139e354e69b84a32163b9549905b024874af44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package datapkggui/0.2.0/datapkggui-0.2.0-py2.7.egg

---
+++ packages/datapkggui/datapkggui-0.2.0-py2.7.egg/datapkggui/gui/base.py
@@ -0,0 +1,86 @@
+__author__ = 'dgraziotin'
+"""
+General GUI module. it contains the base class for datapkggui GUIs, that should not be instantiated.
+"""
+import wx
+import wx.xrc
+import datapkggui
+import os
+
+class GUI(object):
+    """
+    Generic GUI class, holds the common shared properties and methods.
+    Don't instantiate it, please.
+    """
+
+    def __init__(self, xml, frame_name, panel_name="panel"):
+        """
+        Constructor of our "abstract" class.
+        Datapkggui windows have one frame and one panel, so they must be passed as arguments.
+        The xml XRC file must be passed, too.
+        Shared objects such as the status bar and the menu bar are created here.
+        """
+        self.m_xml = xml
+
+        self.m_frame = xml.LoadFrame(None, frame_name)
+
+        self.m_panel = self.GetWidget(panel_name)
+        self.m_status_bar = self.m_frame.CreateStatusBar()
+
+        self.m_menubar = self.m_frame.GetMenuBar()
+
+        self.Bind(wx.EVT_MENU, self.OnMenuClickExit, 'menu_exit')
+        self.Bind(wx.EVT_MENU, self.OnMenuClickAbout, 'menu_about')
+
+        self.m_frame.SetSize(wx.Size(600, 625))
+        self.m_frame.Centre()
+        self.m_frame.Show()
+
+    def SetStatusBarMessage(self, message):
+        self.m_status_bar.SetStatusText(message)
+
+    def OnMenuClickAbout(self, event):
+        """
+        Creates the About window.
+        """
+        about_frame = self.m_xml.LoadFrame(None, "AboutFrame")
+        about_text = self.GetWidget('about_text', about_frame)
+        label = "datapkggui version %s.\n%s\nWebsite: %s\nLicense:\n%s" % (
+            datapkggui.__version__, datapkggui.__description__, "http://task3.cc/projects/datapkggui",
+            datapkggui.__license_full__)
+
+        about_text.AppendText(label)
+        about_frame.SetSize(wx.Size(694, 447))
+        about_frame.Centre()
+        about_frame.Show()
+
+    def Show(self, show):
+        self.m_frame.Show(show)
+
+    def OnMenuClickExit(self, event):
+        self.m_frame.Close()
+
+    def DownloadDirDialog(self):
+        """
+        Create a DirDialog for choosing the directory in which we save the Package
+        """
+        dialog = wx.DirDialog(self.m_frame, "Choose a Download Directory", os.getcwd())
+        if dialog.ShowModal() == wx.ID_OK:
+            download_dir = dialog.GetPath()
+            return download_dir
+        else:
+            return None
+
+    def GetWidget(self, name, window=None):
+        """
+        Wrapper around wx.xrc.XRCCTRL with window = self.m_frame set as default.
+        """
+        if not window:
+            window = self.m_frame
+        return wx.xrc.XRCCTRL(window, name)
+
+    def Bind(self, event, method, name):
+        """
+        Wrapper around Bind method.
+        """
+        self.m_frame.Bind(event, method, id=wx.xrc.XRCID(name))
---
+++ packages/datapkggui/datapkggui-0.2.0-py2.7.egg/datapkggui/operations.py
@@ -0,0 +1,149 @@
+__author__ = 'dgraziotin'
+"""
+Module holding all the operations of Datapkg. Operations are extended Python Threads that communicate to the GUI
+through special objects called Operation Messages.
+Many of the steps of this module were possible thanks to this post:
+http://www.blog.pythonlibrary.org/2010/05/22/wxpython-and-threads/
+"""
+import os
+import threading
+import wx
+import lib
+import inspect
+import ctypes
+import shutil
+
+# Define notification event for thread completion
+OPERATION_MESSAGE_ID = wx.NewId()
+
+# Possible status of an Operation
+OPERATION_STATUS_ID = {
+    "started": wx.NewId(),
+    "finished": wx.NewId(),
+    "error": wx.NewId(),
+    "info": wx.NewId(),
+    }
+
+def OPERATION_MESSAGE_HANDLER(frame, method):
+    frame.Connect(-1, -1, OPERATION_MESSAGE_ID, method)
+
+
+class KillOperationException(Exception):
+    """
+    An exception used to Kill an Operation
+    """
+
+    def __init__(self, message):
+        Exception.__init__(self, message)
+
+
+class OperationMessage(wx.PyEvent):
+    """
+    An Event that is used as data format for communication from threads to the GUI.
+    An OperationMessage has a type (e.g. SearchOperation), a status (e.g. "finished") and some data (e.g. Packages)
+    """
+
+    def __init__(self, type, status, data=None):
+        wx.PyEvent.__init__(self)
+        self.SetEventType(OPERATION_MESSAGE_ID)
+        self.type = type
+        self.status = status
+        self.data = data
+
+
+class Operation(threading.Thread):
+    """
+    Base class defining an operation. The constructor is only responsible for linking a wx object (e.g. a Frame)
+    that will be signaled.
+    """
+
+    def __init__(self, linked_wxobject):
+        threading.Thread.__init__(self)
+        self.m_wxobject = linked_wxobject
+        self.setDaemon(True)
+
+    def run(self):
+        raise NotImplementedError
+
+    def RaiseException(self, exception_type):
+        # thank you very much, Bluebird75
+        # http://stackoverflow.com/questions/323972/is-there-any-way-to-kill-a-thread-in-python/325528#325528
+        """Raises the given exception type in the context of this thread.
+
+        If the thread is busy in a system call (time.sleep(),
+        socket.accept(), ...), the exception is simply ignored.
+
+        If you are sure that your exception should terminate the thread,
+        one way to ensure that it works is:
+
+            t = ThreadWithExc( ... )
+            ...
+            t.raiseExc( SomeException )
+            while t.isAlive():
+                time.sleep( 0.1 )
+                t.raiseExc( SomeException )
+
+        If the exception is to be caught by the thread, you need a way to
+        check that your thread has caught it.
+
+        CAREFUL : this function is executed in the context of the
+        caller thread, to raise an excpetion in the context of the
+        thread represented by this instance.
+        """
+        '''Raises an exception in the threads with id tid'''
+        if not inspect.isclass(exception_type):
+            raise TypeError("Only types can be raised (not instances)")
+
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(self.ident,
+                                                         ctypes.py_object(exception_type))
+        if res > 1:
+            # "if it returns a number greater than one, you're in trouble,
+            # and you should call it again with exc=NULL to revert the effect"
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(self.ident, 0)
+            raise SystemError("PyThreadState_SetAsyncExc failed")
+
+
+class DownloadOperation(Operation):
+    """
+    It calls lib.download in a separate thread for retrieving the package and its resources.
+    """
+
+    def __init__(self, linked_wxobject, package, download_dir):
+        Operation.__init__(self, linked_wxobject)
+        self.package = package
+        self.download_dir = download_dir
+        self.start()
+
+    def run(self):
+        wx.PostEvent(self.m_wxobject, OperationMessage(self.__class__, OPERATION_STATUS_ID["started"]))
+        try:
+            result = lib.download("ckan://" + self.package.name, self.download_dir)
+        except Exception, e:
+            shutil.rmtree(self.download_dir + os.sep + self.package.name, ignore_errors=True)
+            wx.PostEvent(self.m_wxobject, OperationMessage(self.__class__, OPERATION_STATUS_ID["error"], str(e)))
+            return
+        wx.PostEvent(self.m_wxobject, OperationMessage(self.__class__, OPERATION_STATUS_ID["finished"]))
+
+
+class SearchOperation(Operation):
+    """
+    Given a query, it searches in Ckan using the query. If there are no exceptions,
+    it communicates to the main GUI that the the search was finished, returning the retrieved Packages
+    in an OperationMessage object.
+    """
+
+    def __init__(self, linked_wxobject, query):
+        self.query = query
+        Operation.__init__(self, linked_wxobject)
+        self.start()
+
+    def run(self):
+        wx.PostEvent(self.m_wxobject,
+                     OperationMessage(self.__class__, OPERATION_STATUS_ID["started"]))
+        try:
+            results = lib.search("ckan://", self.query)
+        except Exception, e:
+            wx.PostEvent(self.m_wxobject, OperationMessage(self.__class__, OPERATION_STATUS_ID["error"], str(e)))
+            return
+        wx.PostEvent(self.m_wxobject, OperationMessage(self.__class__, OPERATION_STATUS_ID["finished"], results))
+        
