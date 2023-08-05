96990b6ad601ac603d64aaa48cd7081f4916fc1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package Products.ECAutoAssessmentBox/1.4.2/Products.ECAutoAssessmentBox-1.4.2.tar.gz

---
+++ packages/Products.ECAutoAssessmentBox/Products.ECAutoAssessmentBox-1.4.2.tar.gz/Products.ECAutoAssessmentBox-1.4.2/Products/ECAutoAssessmentBox/browser/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# $Id: __init__.py 1592 2011-08-25 17:58:29Z amelung $
+#
+# Copyright (c) 2006-2011 Otto-von-Guericke-UniversitŠt Magdeburg
+#
+# This file is part of ECAutoAssessmentBox.
+#
+__author__ = """Mario Amelung <mario.amelung@gmx.de>"""
+__docformat__ = 'plaintext'
+
+# make me a python module
---
+++ packages/Products.ECAutoAssessmentBox/Products.ECAutoAssessmentBox-1.4.2.tar.gz/Products.ECAutoAssessmentBox-1.4.2/Products/ECAutoAssessmentBox/tool/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# $Id: __init__.py 1592 2011-08-25 17:58:29Z amelung $
+#
+# Copyright (c) 2006-2011 Otto-von-Guericke-UniversitŠt Magdeburg
+#
+# This file is part of ECAutoAssessmentBox.
+#
+import ECSpoolerTool
+
