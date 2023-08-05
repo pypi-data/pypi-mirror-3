23a65a0c5becf5e9d4a2b4ec6a5d63358c99ed84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:25 +0000

    Add package celery-haystack/0.4/celery-haystack-0.4.tar.gz

---
+++ packages/celery-haystack/celery-haystack-0.4.tar.gz/celery-haystack-0.4/tests/search_indexes.py
@@ -0,0 +1,22 @@
+from haystack import indexes, __version__ as haystack_version
+from .models import Note
+
+from celery_haystack.indexes import CelerySearchIndex
+
+if haystack_version[:2] < (2, 0):
+    from haystack import site
+    class Indexable(object):
+        pass
+    indexes.Indexable = Indexable
+else:
+    site = None
+
+# Simplest possible subclass that could work.
+class NoteIndex(CelerySearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, model_attr='content')
+
+    def get_model(self):
+        return Note
+
+if site:
+    site.register(Note, NoteIndex)
