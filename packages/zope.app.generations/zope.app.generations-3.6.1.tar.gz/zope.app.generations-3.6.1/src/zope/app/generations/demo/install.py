a530887441542d43d072dbbb0b59e2f1ac38d6e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:08 +0000

    Add package zope.app.generations/3.6.1/zope.app.generations-3.6.1.tar.gz

---
+++ packages/zope.app.generations/zope.app.generations-3.6.1.tar.gz/zope.app.generations-3.6.1/src/zope/app/generations/demo/install.py
@@ -0,0 +1,25 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Silly demo evolution module
+
+$Id: install.py 124134 2012-01-23 15:20:44Z menesis $
+"""
+__docformat__ = 'restructuredtext'
+
+generation = 3
+
+import zope.app.generations.demo
+
+def evolve(context):
+    zope.app.generations.demo.evolve(context, 'installed')
