579935007b16062b87636aab63da084d65b7baf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:00 +0000

    Add package veh/0.93.0/veh-0.93.0.tar.gz

---
+++ packages/veh/veh-0.93.0.tar.gz/veh-0.93.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+from veh import __version__
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Utilities',
+]
+
+setup(
+    name = "veh",
+    version = __version__,
+    description = "virtualenv for hg",
+    long_description = """Tie virtualenvs to individual mercurial repositorys.""",
+    license = "GNU GPL v3",
+    author = "Nic Ferrier",
+    author_email = "nic@ferrier.me.uk",
+    url = "http://github.com/nicferrier/veh",
+    download_url="http://github.com/nicferrier/veh/downloads",
+    platforms = ["any"],
+    packages=['veh'],
+    install_requires=['pip', 'virtualenv'],
+    entry_points = {
+       'console_scripts': [
+            'veh = veh:main'
+        ],
+    },
+    classifiers =  classifiers,
+    )
