11cf13bf52542cb6053ef74159fb802b7cd14600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:37 +0000

    Add package collective.contentleadimage/1.3.4/collective.contentleadimage-1.3.4.zip

---
+++ packages/collective.contentleadimage/collective.contentleadimage-1.3.4.zip/collective.contentleadimage-1.3.4/src/collective/contentleadimage/tests/test_base.py
@@ -0,0 +1,51 @@
+import unittest
+
+from zope.testing import doctestunit
+from zope.component import testing
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+ptc.setupPloneSite()
+
+import collective.contentleadimage
+
+class TestCase(ptc.FunctionalTestCase):
+    class layer(PloneSite):
+        @classmethod
+        def setUp(cls):
+            fiveconfigure.debug_mode = True
+            zcml.load_config('configure.zcml',
+                             collective.contentleadimage)
+            fiveconfigure.debug_mode = False
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+
+    suite = [
+        # Integration tests that use PloneTestCase
+        ztc.ZopeDocFileSuite(
+            'README.txt', package='collective.contentleadimage',
+            test_class=TestCase),
+        ]
+
+    try:
+        from Products.CacheSetup.interfaces import IPurgeUrls
+        suite.append(
+            ztc.ZopeDocFileSuite(
+                'tests/cachesetup.txt', package='collective.contentleadimage',
+                test_class=TestCase)
+        )
+    except ImportError:
+        pass
+
+    return unittest.TestSuite(suite)
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
