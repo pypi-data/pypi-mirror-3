ceca7cdc4da7972cb88f7108c8efc788d9027f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:36 +0000

    Add package kaa-imlib2/0.3.0dev-r4564-20111003/kaa_imlib2-0.3.0dev_r4564_20111003-py2.7-linux-x86_64.egg

---
+++ packages/kaa-imlib2/kaa_imlib2-0.3.0dev_r4564_20111003-py2.7-linux-x86_64.egg/kaa/imlib2/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# Imlib2.py - Imlib2 wrapper for Python
+# -----------------------------------------------------------------------------
+# $Id: __init__.py 4381 2010-09-05 19:45:54Z tack $
+# -----------------------------------------------------------------------------
+# kaa.imlib2 - An imlib2 wrapper for Python
+# Copyright (C) 2004-2006 Dirk Meyer, Jason Tackaberry
+#
+# First Edition: Jason Tackaberry <tack@urandom.ca>
+# Maintainer:    Jason Tackaberry <tack@urandom.ca>
+#
+# Please see the file AUTHORS for a complete list of authors.
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version
+# 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+#
+# -----------------------------------------------------------------------------
+
+from version import VERSION
+from image import *
+from font import *
