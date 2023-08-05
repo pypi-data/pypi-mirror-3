b0b5ba4c73e2ed17dddfa3a582ae38f71cafcdf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:05 +0000

    Add package blogodev/0.1/blogodev-0.1.tar.gz

---
+++ packages/blogodev/blogodev-0.1.tar.gz/blogodev-0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import sys
+import os
+import re
+
+extra = {}
+if sys.version_info >= (3, 0):
+    extra.update(
+        use_2to3=True,
+    )
+
+
+setup(name='blogodev',
+      version="0.1",
+      description="An interim front end for blogofile development",
+      classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: Implementation :: CPython',
+      'Programming Language :: Python :: Implementation :: PyPy',
+      ],
+      keywords='Blogofile',
+      author='Mike Bayer',
+      author_email='mike@zzzcomputing.com',
+      license='MIT',
+      packages=['blogodev'],
+      tests_require = ['nose >= 0.11'],
+      test_suite = "nose.collector",
+      zip_safe=False,
+      install_requires=[
+            'Blogofile==0.7.1'
+      ],
+      entry_points = {
+        'console_scripts': [ 'blogodev = blogodev:main' ],
+      },
+      **extra
+)
