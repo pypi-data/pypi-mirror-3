e4d04c68dc304060cd12da20d7a08a3fd0344d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package quick_orm/0.2.2/quick_orm-0.2.2.tar.gz

---
+++ packages/quick_orm/quick_orm-0.2.2.tar.gz/quick_orm-0.2.2/quick_orm/examples/__init__.py
@@ -0,0 +1,27 @@
+examples_list = [
+    'hello_world',
+    'foreign_key',
+    'foreign_key_options',
+    'many_to_many',
+    'many_to_many_options',
+    'one_to_one',
+    'multiple_foreign_keys',
+    'raw_sql',
+    'multiple_databases',
+    'table_inheritance',
+    'write_less_do_more',
+]
+
+examples_dict = {
+    'hello_world': 'Hello World',
+    'foreign_key': 'Foreign key',
+    'foreign_key_options': 'Foreign key options',
+    'many_to_many': 'Many-to-many relationship',
+    'many_to_many_options': 'Many-to-many relationship options',
+    'one_to_one': 'One-to-one relationship',
+    'multiple_foreign_keys': 'Multiple foreign keys',
+    'raw_sql': 'Performing raw sql query',
+    'multiple_databases': 'Multiple databases',
+    'table_inheritance': 'Table inheritance',
+    'write_less_do_more': 'Write less, do more',    
+}
