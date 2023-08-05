27e95ed02d8c4b20b691aceb4c785fd6bf86cdd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:48 +0000

    Add package voldemort/0.6.0/voldemort-0.6.0-py2.7.egg

---
+++ packages/voldemort/voldemort-0.6.0-py2.7.egg/voldemort/util.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+#
+# Common utility functions
+#
+# @author: Sreejith K
+# Created On 19th Sep 2011
+
+
+import logging
+import logging.handlers
+
+
+def setup_logging(path, level):
+    """Setup application logging.
+    """
+    log_handler = logging.handlers.RotatingFileHandler(path,
+                                                      maxBytes=1024*1024,
+                                                      backupCount=2)
+    root_logger = logging.getLogger('')
+    root_logger.setLevel(level)
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    formatter = logging.Formatter(format)
+    log_handler.setFormatter(formatter)
+    root_logger.addHandler(log_handler)
+    # add console logger
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('--> %(message)s')
+    ch.setFormatter(formatter)
+    root_logger.addHandler(ch)
+    
+    
