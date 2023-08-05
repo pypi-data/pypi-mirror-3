f79b3697712aa019e5988a60c9015209e465ac3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:00 +0000

    Add package SelfNest/1.3.0/SelfNest-1.3.0.zip

---
+++ packages/SelfNest/SelfNest-1.3.0.zip/SelfNest-1.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'SelfNest',
+    version = '1.3.0',
+    py_modules = ['SelfNest'],
+    author = 'jsh',
+    author_email = 'jhotchk1@fairview.org',
+    url = '',
+    description = 'Revised First py function',
+    )
+    
