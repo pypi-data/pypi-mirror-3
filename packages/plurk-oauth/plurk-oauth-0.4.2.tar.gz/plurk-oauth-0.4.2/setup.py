0e1268b22a2287e0b6bdb7ca1151eb766e563bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package plurk-oauth/0.4.2/plurk-oauth-0.4.2.tar.gz

---
+++ packages/plurk-oauth/plurk-oauth-0.4.2.tar.gz/plurk-oauth-0.4.2/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Plurk OAuth API',
+    'author': 'Cheng-Lung Sung',
+    'url': 'https://github.com/clsung/plurk-oauth',
+    'download_url': 'http://pypi.python.org/pypi/plurk-oauth',
+    'author_email': 'clsung@gmail.com',
+    'version': '0.4.2',
+    'install_requires': ['nose', 'oauth2'],
+    'packages': ['plurk_oauth'],
+    'scripts': ['bin/get_own_profile.py', 'bin/post_to_plurk.py'],
+    'name': 'plurk-oauth'
+}
+
+setup(**config)
