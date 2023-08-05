e2405a88f6757db40b0efd9499e8c25a55ab7523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:35 +0000

    Add package apiclient/1.0/apiclient-1.0.tar.gz

---
+++ packages/apiclient/apiclient-1.0.tar.gz/apiclient-1.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+try:
+    import setuptools
+except ImportError, _:
+    pass # No 'develop' command, oh well.
+
+
+version = '1.0'
+long_description = open('README.rst').read()
+
+requirements = [
+    'urllib3',
+]
+tests_requirements = requirements + [
+    'nose',
+]
+
+setup(name='apiclient',
+      version=version,
+      description="Framework for making good API client libraries using urllib3.",
+      long_description=long_description,
+      classifiers=[
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Software Development :: Libraries',
+      ],
+      keywords='api client urllib3 keepalive threadsafe http rest',
+      author='Andrey Petrov',
+      author_email='andrey.petrov@shazow.net',
+      url='https://github.com/shazow/apiclient',
+      license='MIT',
+      packages=['apiclient'],
+      requires=requirements,
+      tests_require=tests_requirements,
+      )
