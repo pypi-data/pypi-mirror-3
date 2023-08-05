4000b457a2bd7a4ca9be760e109a4e9518f79e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:59 +0000

    Add package py.saunter/0.34/py.saunter-0.34-py2.7.egg

---
+++ packages/py.saunter/py.saunter-0.34-py2.7.egg/saunter/testcase/base.py
@@ -0,0 +1,46 @@
+# Copyright 2011 Element 34
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest2 as unittest
+
+class BaseTestCase(unittest.TestCase):
+    def verify_equal(self, want, got, message = ""):
+        try:
+            self.assertEqual(want, got)
+        except AssertionError, e:
+            self.verificationErrors.append(str(e))
+
+    def verify_text_present(self, text, message = ""):
+        try:
+            self.assertTrue(self.selenium.is_text_present(text))
+        except AssertionError, e:
+            self.verificationErrors.append(str(e))
+
+    def verify_element_present(self, locator, message = ""):
+        try:
+            self.assertTrue(self.selenium.is_element_present(locator))
+        except AssertionError, e:
+            self.verificationErrors.append(str(e))
+
+    def verify_visible(self, locator, message = ""):
+        try:
+            self.assertTrue(self.selenium.is_visible(locator))
+        except AssertionError, e:
+            self.verificationErrors.append(str(e))
+
+    def verify_true(self, condition, message = ""):
+        try:
+            self.assertTrue(condition)
+        except AssertionError, e:
+            self.verificationErrors.append(str(e))
