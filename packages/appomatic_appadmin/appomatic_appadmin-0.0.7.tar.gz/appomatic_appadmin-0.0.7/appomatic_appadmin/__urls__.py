49b37e1af488b4bfc89811fc8500b6d294f776f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:53 +0000

    Add package appomatic_appadmin/0.0.7/appomatic_appadmin-0.0.7.tar.gz

---
+++ packages/appomatic_appadmin/appomatic_appadmin-0.0.7.tar.gz/appomatic_appadmin-0.0.7/appomatic_appadmin/__urls__.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+urlpatterns = patterns('',
+    (r'^admin/appomatic_appadmin/application/?$', 'appomatic_appadmin.views.index'),
+    (r'^admin/appomatic_appadmin/application/add/?$', 'appomatic_appadmin.views.add'),
+    (r'^admin/appomatic_appadmin/application/action/?$', 'appomatic_appadmin.views.action'),
+    (r'^admin/appomatic_appadmin/application/progress/(?P<pid>.*)/?$', 'appomatic_appadmin.views.progress'),
+)
