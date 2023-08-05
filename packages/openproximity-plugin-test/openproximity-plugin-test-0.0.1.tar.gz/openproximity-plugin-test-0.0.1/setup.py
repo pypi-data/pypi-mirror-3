518aafd1c105206cf5c8b9b4ef94a3a4451c5481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:55 +0000

    Add package openproximity-plugin-test/0.0.1/openproximity-plugin-test-0.0.1.tar.gz

---
+++ packages/openproximity-plugin-test/openproximity-plugin-test-0.0.1.tar.gz/openproximity-plugin-test-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from test import __version__
+
+setup(
+    name = "openproximity-plugin-test",
+    version = __version__,
+    packages = ['test',],
+    summary = "A Test Plugin",
+    description = 
+    """A very simple example on what an OpenProximity plugins looks like""",
+    include_package_data = True,
+    author = "Naranjo Manuel Francisco",
+    author_email = "manuel@aircable.net",
+    license = "GPL2",
+    url = "https://github.com/OpenProximity/plugins-test", 
+)
