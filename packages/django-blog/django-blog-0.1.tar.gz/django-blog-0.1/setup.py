c0b43cdf1bd4fc1ad29a6fee52c33f895c8e5076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:16 +0000

    Add package django-blog/0.1/django-blog-0.1.tar.gz

---
+++ packages/django-blog/django-blog-0.1.tar.gz/django-blog-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-blog',
+    version='0.1',
+    author='Michael Samoylov',
+    author_email='mike@djangoware.com',
+    url='http://github.com/djangoware/django-blog',
+    description = 'A simple blog app',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
