6478ecc17818e6c03aa51971d107dc8fd3bbcd84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:34 +0000

    Add package collective.setuphelpers/0.5/collective.setuphelpers-0.5.zip

---
+++ packages/collective.setuphelpers/collective.setuphelpers-0.5.zip/collective.setuphelpers-0.5/collective/setuphelpers/__init__.py
@@ -0,0 +1,23 @@
+# The import below imports not yet initialized packages - the tests fail
+#from structure import setupStructure, clearUpSite
+from various import (
+    setupCatalog,
+    registerDisplayViews,
+    unregisterDisplayViews,
+    hideActions,
+    registerActions,
+    setupTinyMCE,
+    setupCTAvailability,
+    setupHTMLFiltering,
+    registerTransform,
+    unregisterTransform,
+    setHomePage,
+    setupNavigation,
+    hidePortletTypes,
+)
+
+from usersandgroups import (
+    createGroups,
+    enableSelfRegistration,
+    enableUserPwdChoice
+)
