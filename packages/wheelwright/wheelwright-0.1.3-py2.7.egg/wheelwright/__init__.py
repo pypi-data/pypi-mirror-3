7261bd642fb7e11673642a950d752b2cd2043eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:11 +0000

    Add package wheelwright/0.1.3/wheelwright-0.1.3-py2.7.egg

---
+++ packages/wheelwright/wheelwright-0.1.3-py2.7.egg/wheelwright/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Kris Andersen"
+__email__ = "kris@biciworks.com"
+__copyright__ = u"Copyright © 2012 Kris Andersen"
+__license__ = "GPLv3"
+
+from wheelwright import __version__, __doc__, main
