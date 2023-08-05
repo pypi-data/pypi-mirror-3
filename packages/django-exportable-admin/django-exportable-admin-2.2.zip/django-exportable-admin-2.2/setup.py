cc5e62435e4d1b3dfcc3ee3a04f8ce46d6dcebbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:38 +0000

    Add package django-exportable-admin/2.2/django-exportable-admin-2.2.zip

---
+++ packages/django-exportable-admin/django-exportable-admin-2.2.zip/django-exportable-admin-2.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+    name = "django-exportable-admin",
+    version = "2.2",
+
+    packages = ['django_exportable_admin'],
+
+    author = "Jay Wineinger",
+    author_email = "jay.wineinger@gmail.com",
+
+    description = "An app to allow Django admin changelists to be exported to CSV",
+    long_description=open('README.rst').read(),
+
+    url = "https://github.com/jwineinger/django-exportable-admin",
+    download_url = "https://github.com/jwineinger/django-exportable-admin/downloads",
+
+    requires = [
+        "Django (>=1.4.0a0)"
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
