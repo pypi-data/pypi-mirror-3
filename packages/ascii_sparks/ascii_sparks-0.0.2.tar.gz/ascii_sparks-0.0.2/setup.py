47135d0a49383a37f9831ec396d070422d464e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:58 +0000

    Add package ascii_sparks/0.0.2/ascii_sparks-0.0.2.tar.gz

---
+++ packages/ascii_sparks/ascii_sparks-0.0.2.tar.gz/ascii_sparks-0.0.2/setup.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="ascii_sparks",
+      version="0.0.2",
+      author="Rory McCann",
+      author_email="rory@technomancy.org",
+      py_modules=['ascii_sparks'],
+      url="http://www.technomancy.org/",
+)
