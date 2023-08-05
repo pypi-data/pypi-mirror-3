8c183cb22c2492c4b53b525049aea52ca65cd4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:50 +0000

    Add package django-dirbrowser/0.0.2/django-dirbrowser-0.0.2.tar.gz

---
+++ packages/django-dirbrowser/django-dirbrowser-0.0.2.tar.gz/django-dirbrowser-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-dirbrowser',
+    version='0.0.2',
+    description='Django app For browsing a directory tree.',
+    long_description=open('README.rst', 'r').read(),
+    author='Ferran Pegueroles',
+    author_email='ferran@pegueroles.com',
+    license='GPL',
+    url='http://bitbucket.org/ferranp/django-dirbrowser',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    data_files=['README.rst', 
+                'dirbrowser/templates/404.html',
+                'dirbrowser/templates/dirbrowser/direcotry_index.html',
+                ],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Environment :: Web Environment",
+    ],
+    keywords='browser,django',
+)
