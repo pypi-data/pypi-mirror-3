df3fa2355e95b796f39cd73a16d60cac81cea92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:46 +0000

    Add package jaraco.net/2.0/jaraco.net-2.0.zip

---
+++ packages/jaraco.net/jaraco.net-2.0.zip/jaraco.net-2.0/tests/test_notification.py
@@ -0,0 +1,11 @@
+from jaraco.net import notification
+
+class TestMailbox(object):
+	def test_dest_addrs(self):
+		mbx = notification.SMTPMailbox(
+			to_addrs = "a@example.com,b@example.com",
+			cc_addrs = "c@example.com,d@example.com",
+			bcc_addrs = "e@example.com,f@example.com",
+		)
+		assert len(list(mbx.dest_addrs)) == 6
+		
