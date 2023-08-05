c73e3ad5bf985cf8e586f1417e1bdc963035ba4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:45 +0000

    Add package mcint/0.1dev3/mcint-0.1dev3.zip

---
+++ packages/mcint/mcint-0.1dev3.zip/mcint-0.1dev3/setup.py
@@ -0,0 +1,15 @@
+﻿from distutils.core import setup
+
+setup(
+    name='mcint',
+    author='Tristan Snowsill',
+    description='''
+    A simple tool to perform numerical integration using Monte Carlo techniques.
+    ''',
+    author_email='tristan.snowsill@googlemail.com',
+    version='0.1dev3',
+    py_modules=['mcint'],
+    url='http://pypi.python.org/pypi/mcint/',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
