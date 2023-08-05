d0b338a8c0104557f4cb41a94c042407776d6383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:17 +0000

    Add package collective.readunread/1.0b6/collective.readunread-1.0b6.zip

---
+++ packages/collective.readunread/collective.readunread-1.0b6.zip/collective.readunread-1.0b6/README.rst
@@ -0,0 +1,9 @@
+Introduction
+============
+
+This package provides read/unread status for Plone objects.
+
+It adds a "readBy" field and a proper index to any object marked with IReadByProvider interface.
+
+
+See collective/readunread/README.rst for further info.
---
+++ packages/collective.readunread/collective.readunread-1.0b6.zip/collective.readunread-1.0b6/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b6'
+
+def get_long_desc():
+    contents = [
+        open(os.path.join("collective","readunread","README.rst")).read(),"",
+        open(os.path.join("docs", "HISTORY.txt")).read(),"",
+        open(os.path.join("docs", "CREDITS.txt")).read(),"",
+    ]
+    return "\n".join(contents)
+
+setup(name='collective.readunread',
+      version=version,
+      description="",
+      long_description=get_long_desc(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='Plone',
+      author='Simone Orsi',
+      author_email='simahawk@gmail.com',
+      url='https://github.com/collective/collective.readunread',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'plone.validatehook',
+          'archetypes.schemaextender',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
