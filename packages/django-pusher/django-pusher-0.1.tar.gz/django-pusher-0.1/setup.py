be527c4dab5634e213eafbfebeb4091ae8997982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:13 +0000

    Add package django-pusher/0.1/django-pusher-0.1.tar.gz

---
+++ packages/django-pusher/django-pusher-0.1.tar.gz/django-pusher-0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+
+VERSION = __import__("django_pusher").__version__
+
+
+setup(
+    name="django-pusher",
+    version=VERSION,
+    author="Eldarion",
+    author_email="development@eldarion.com",
+    description="a reusable app that wraps the api for pusher.com to allow registration of namespaces and auth callbacks",
+    long_description=open("README.rst").read(),
+    license="BSD",
+    url="http://github.com/eldarion/django-pusher/",
+    packages=[
+        "django_pusher",
+    ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
