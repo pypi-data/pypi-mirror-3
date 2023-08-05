3e5e58ae6e07c28f60ad0d0088c6f0b8b5ca9398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:58 +0000

    Add package appomatic/0.0.3/appomatic-0.0.3.tar.gz

---
+++ packages/appomatic/appomatic-0.0.3.tar.gz/appomatic-0.0.3/setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+from setuptools import setup, find_packages
+setup(
+    name = "appomatic",
+    version = "0.0.3",
+
+    author = "RedHog (Egil Moeller)",
+    author_email = "egil.moller@freecode.no",
+    description = "Appomatic is a userfriendly Django environment with automatic plugin (app) management based on pip.",
+    license = "GPL",
+    keywords = "appomatic django apps pip",
+    url = "http://github.com/redhog/appomatic",
+    # long_description =
+    # download_url =
+    # classifiers =
+
+    install_requires = ['appomaticcore', 'appomatic_appadmin'],
+
+    packages = [],
+    scripts = [],
+    package_data = {'': ['*.txt']},
+)
