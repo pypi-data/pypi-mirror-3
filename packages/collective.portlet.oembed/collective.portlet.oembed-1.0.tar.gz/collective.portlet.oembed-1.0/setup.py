1a13c218683b147fda5b3e9af0a9352f075f1ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:33 +0000

    Add package collective.portlet.oembed/1.0/collective.portlet.oembed-1.0.tar.gz

---
+++ packages/collective.portlet.oembed/collective.portlet.oembed-1.0.tar.gz/collective.portlet.oembed-1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.portlet.oembed',
+      version=version,
+      description="Portlet waiting for an external link displayed using oembed/embedly",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='toutpt@gmail.com',
+      url='https://github.com/collective/collective.portlet.oembed',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.oembed',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
