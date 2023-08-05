ca5f45fce6526c020b1e1cb1ab14ad0ef95ddd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:09 +0000

    Add package newsuper/0.3b/newsuper-0.3b.zip

---
+++ packages/newsuper/newsuper-0.3b.zip/newsuper-0.3b/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="newsuper",
+    version="0.3b",
+    description="Python 3.x super() in 2.x",
+    long_description=open('readme.txt').read(),
+    author="invlpg",
+    author_email="invlpg@gmail.com",
+    url="http://pypi.python.org/pypi/newsuper/",
+    license="MIT",
+    packages=find_packages(),
+)
