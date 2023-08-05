d5b78ee82cf9053b1878717825f64e3102a8403a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:34 +0000

    Add package rhinoplasty/0.4.1/rhinoplasty-0.4.1.tar.gz

---
+++ packages/rhinoplasty/rhinoplasty-0.4.1.tar.gz/rhinoplasty-0.4.1/rhinoplasty/rich_errors/plugin.py
@@ -0,0 +1,20 @@
+"""Nose plugin for displaying results from rich errors."""
+
+from nose.plugins.errorclass import ErrorClass, ErrorClassPlugin
+from _errors import *
+
+class RichErrorReportingPlugin(ErrorClassPlugin):
+    """Plugin that installs error class handling for a richer set of errors."""
+    # Standard Plugin attributes
+    enabled = False
+    name = "rich-errors"
+    score = 1001 # Handle errors before the standard Skip plugin #TODO check this works
+    
+    # Define error classes that we handle
+    broken = ErrorClass(BrokenTestException, label='BROKEN', isfailure=True)
+    excluded = ErrorClass(ExcludeTestException, label='XCLUDE', isfailure=False) #TODO better label
+    misconfigured = ErrorClass(InvalidTestConfigurationException, label='CONFIG_WRONG', isfailure=False) #TODO better label
+    irrelevant = ErrorClass(IrrelevantTestException, label='IRRELEVANT', isfailure=False)
+    
+    # Use default implementation of options() and configure to enable this plugin
+
---
+++ packages/rhinoplasty/rhinoplasty-0.4.1.tar.gz/rhinoplasty-0.4.1/setup.py
@@ -0,0 +1,45 @@
+"""Packaging."""
+
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+
+## Helper Functions ##
+def read(fname):
+    """Import another file into the setup function.
+    
+    Taken from the example PyPI project. See
+        http://packages.python.org/an_example_pypi_project/setuptools.html
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+## Run Setup ##
+setup(
+    name = "rhinoplasty",
+    version = "0.4.1",
+    author = "Gary Donovan",
+    author_email = "garyd@crucialfruit.com.au",
+    url = "N/A", #FIXME get a URL
+    license = "LGPL",
+    keywords = "Nose test unittest",
+    description = ("Experimental extensions to Nose."),
+    long_description = read('README.txt'),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Topic :: Software Development :: Testing",
+    ],
+    packages = find_packages("."),
+    entry_points = {
+        'nose.plugins.0.10': [
+            "rich-errors = rhinoplasty.rich_errors.plugin:RichErrorReportingPlugin",
+        ],
+    },
+)
