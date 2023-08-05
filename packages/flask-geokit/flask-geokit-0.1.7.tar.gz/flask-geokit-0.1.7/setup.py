944ed261c31e1c2eaa0cfa35c3e8c6d18cfdf8d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:43 +0000

    Add package flask-geokit/0.1.7/flask-geokit-0.1.7.tar.gz

---
+++ packages/flask-geokit/flask-geokit-0.1.7.tar.gz/flask-geokit-0.1.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(name='flask-geokit',
+      version='0.1.7',
+      packages=['flaskext'],
+      namespace_packages=['flaskext'],
+      author='Alex Toney',
+      author_email='toneyalex@gmail.com',
+      url='https://github.com/bluemoon/flask-geokit',
+      license='BSD',
+      description='Geocoding toolkit',
+      zip_safe=False,
+      long_description='',
+      platforms=['any'],
+      install_requires=[
+          'furl', 
+          'geohash', 
+          'Flask'
+          ],
+      classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
