cf22b8c9925cd2cbdd9cadf02926abd336199b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:25 +0000

    Add package urlfetch/0.1.5/urlfetch-0.1.5.tar.bz2

---
+++ packages/urlfetch/urlfetch-0.1.5.tar.bz2/urlfetch-0.1.5/urlfetch/__init__.py
@@ -0,0 +1,17 @@
+#coding: utf8
+#
+#    urlfetch 
+#    ~~~~~~~~
+#
+#    An easy to use HTTP client based on httplib.
+#
+#    :copyright: (c) 2011 by Elyes Du.
+#    :license: BSD, see LICENSE for more details.
+#
+
+from urlfetch import *
+from uas import randua
+
+__version__ = '0.1.5'
+__author__ = 'Elyes Du <lyxint@gmail.com>'
+__url__ = 'https://github.com/lyxint/urlfetch'
