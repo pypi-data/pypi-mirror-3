3ea1760ae23d8252144e36ef30fedd46bf3e4d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:53 +0000

    Add package nester_koppie/1.2.0/nester_koppie-1.2.0.tar.gz

---
+++ packages/nester_koppie/nester_koppie-1.2.0.tar.gz/nester_koppie-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name          =   'nester_koppie',
+      version       =   '1.2.0',
+      py_modules    =   ['nester_koppie'],
+      author        =   'koppie',
+      author_email  =   'jordan@tenukidesign.com',
+      url           =   'http://tenukidesign.com',
+      description   =   "Demo module with hat tip to O'Reilly Head First",
+      )
