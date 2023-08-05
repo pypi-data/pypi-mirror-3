edef9e9cdc80f6ab3552fd9715afe345490549dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:49 +0000

    Add package tornado_tools/0.2.3a/tornado_tools-0.2.3a.tar.gz

---
+++ packages/tornado_tools/tornado_tools-0.2.3a.tar.gz/tornado_tools-0.2.3a/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import distutils.core
+import sys
+
+try:
+    import setuptools
+except ImportError:
+    pass
+
+version = "0.2.3a"
+
+distutils.core.setup(
+    name="tornado_tools",
+    version=version,
+    packages = ["tornado_tools"],
+    author="Gregory Sitnin",
+    author_email="sitnin@gmail.com",
+    url="http://github.com/sitnin",
+    download_url="https://github.com/sitnin/tornado_tools/tarball/%s"%version,
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    description="Classes and functions for extending Tornado",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
---
+++ packages/tornado_tools/tornado_tools-0.2.3a.tar.gz/tornado_tools-0.2.3a/tornado_tools/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Gregory Sitnin <g.sitnin@wwpass.com>"
+__copyright__ = "Gregory Sitnin, 2011"
+
+import decorators
+import tpl_funcs
+
+UUID = r"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
