ee0633a4174810cc9961d7b7c78fb9c3411f2728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:45 +0000

    Add package aspen/0.11.13/aspen-0.11.13.tar.bz2

---
+++ packages/aspen/aspen-0.11.13.tar.bz2/aspen-0.11.13/aspen/resources/json_resource.py
@@ -0,0 +1,35 @@
+import datetime
+
+from aspen import json
+from aspen.resources.dynamic_resource import DynamicResource
+
+
+class JSONResource(DynamicResource):
+
+    min_pages = 2
+    max_pages = 2
+
+    def compile_third(self, one, two, three, padding):
+        """Given None, return None. JSON resources have no third page.
+        """
+        assert three is None # sanity check
+        return three 
+
+    def process_raised_response(self, response):
+        """Given a response, return a response.
+        """
+        return self._process(response)
+  
+    def get_response(self, namespace):
+        """Given a namespace dict, return a response object.
+        """
+        response = namespace['response']
+        return self._process(response)
+
+    def _process(self, response):
+        """Given a response object, process it for JSON.
+        """
+        if not isinstance(response.body, basestring):
+            response.body = json.dumps(response.body)
+        response.headers.set('Content-Type', self.website.json_content_type)
+        return response
