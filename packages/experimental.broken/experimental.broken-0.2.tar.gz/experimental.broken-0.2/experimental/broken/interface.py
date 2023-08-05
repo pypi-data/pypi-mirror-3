1930ac8b358aa7f5643a096d8baa81551d8c4579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:02 +0000

    Add package experimental.broken/0.2/experimental.broken-0.2.tar.gz

---
+++ packages/experimental.broken/experimental.broken-0.2.tar.gz/experimental.broken-0.2/experimental/broken/interface.py
@@ -0,0 +1,38 @@
+"""Broken Interfaces Handling"""
+
+from ZODB.interfaces import IBroken
+from ZODB.broken import Broken
+from ZODB.broken import broken_cache
+from ZODB.broken import find_global
+
+from zope.interface.interface import InterfaceClass
+from zope.interface import declarations
+
+orig_init = declarations.ProvidesClass.__init__
+
+
+class BrokenInterfaceClass(InterfaceClass):
+    """An interface whose module is no longer available."""
+
+    def __reduce__(self):
+        """Use ZODB.broken's missing tolerant global handling."""
+        return (find_global, (self.__module__, self.__name__))
+
+
+def __init__(self, cls, *interfaces):
+    return orig_init(
+        self, cls, *rebuildBrokenInterfaces(*interfaces))
+
+
+def rebuildBrokenInterfaces(*interfaces):
+    for iface in interfaces:
+        cls = iface.__class__
+        if (cls is type and
+            len(iface.__bases__) == 1 and
+            iface.__bases__[0] is Broken):
+            broken_cache.pop((iface.__module__, iface.__name__,))
+            yield find_global(
+                iface.__module__, iface.__name__,
+                Broken=IBroken, type=BrokenInterfaceClass)
+        else:
+            yield iface
