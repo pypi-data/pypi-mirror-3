9b5c89191d4ddad30a553ebfdcc43aafba731832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:31 +0000

    Add package django-shortcuts/1.0/django-shortcuts-1.0.tar.gz

---
+++ packages/django-shortcuts/django-shortcuts-1.0.tar.gz/django-shortcuts-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name = 'django-shortcuts',
+  version = '1.0',
+  description = "You spend way too much time typing 'python manage.py'",
+  author = "Johannes Gorset",
+  author_email = "jgorset@gmail.com",
+  url = "http://github.com/jgorset/django-shortcuts",
+  scripts = ['scripts/django']
+)
