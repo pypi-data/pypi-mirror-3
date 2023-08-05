eaebde00f975d20b22d187fa2669759a5e12cef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:17 +0000

    Add package mcint/0.1dev2/mcint-0.1dev2.zip

---
+++ packages/mcint/mcint-0.1dev2.zip/mcint-0.1dev2/mcint.py
@@ -0,0 +1,12 @@
+﻿# mcint/integrate.py
+
+import itertools
+
+def integrate(integrand, sampler, measure=1.0, n=100):
+    scale = float(measure/n)
+    result = 0.0
+    for i in range(n):
+        x = sampler().next()
+        result += scale * integrand(x)
+    # Return answer
+    return result
