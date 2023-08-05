d94ef81e2f5f6581617d80978cae525d235738c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:11 +0000

    Add package isotoma.zope.groupreadableblobstorage/0.0.5/isotoma.zope.groupreadableblobstorage-0.0.5.zip

---
+++ packages/isotoma.zope.groupreadableblobstorage/isotoma.zope.groupreadableblobstorage-0.0.5.zip/isotoma.zope.groupreadableblobstorage-0.0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '0.0.5'
+
+setup(
+    name = 'isotoma.zope.groupreadableblobstorage',
+    version = version,
+    url = "http://github.com/isotoma/isotoma.zope.groupreadableblobstorage",
+    maintainer = "John Carr",
+    maintainer_email = "john.carr@isotoma.com",
+    description = "Make blob storage group readable",
+    long_description = open('README.rst').read(),
+    package_data = {
+        '': ['README.rst', ],
+    },
+    packages = find_packages(exclude=['ez_setup']),
+    namespace_packages = ['isotoma', 'isotoma.zope'],
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = [
+        'setuptools',
+    ],
+    entry_points="""
+        # -*- Entry points: -*-
+       [z3c.autoinclude.plugin]
+       target = plone
+       """,
+)
