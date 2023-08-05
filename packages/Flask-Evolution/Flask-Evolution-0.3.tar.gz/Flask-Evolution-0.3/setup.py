1c4bd2d2dff985fe64ab7f6afbae98e62e0924bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:00 +0000

    Add package Flask-Evolution/0.3/Flask-Evolution-0.3.tar.gz

---
+++ packages/Flask-Evolution/Flask-Evolution-0.3.tar.gz/Flask-Evolution-0.3/setup.py
@@ -0,0 +1,37 @@
+"""
+Flask-Evolution
+-------------
+
+Simple migrations for Flask/SQLAlchemy projects
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Evolution',
+    version='0.3',
+    url='http://github.com/adamrt/flask-evolution/',
+    license='BSD',
+    author='Adam Patterson',
+    author_email='fakeempire@gmail.com',
+    description='Simple migrations for Flask/SQLAlchemy projects',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'Flask-SQLAlchemy',
+        'Flask-Script',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
