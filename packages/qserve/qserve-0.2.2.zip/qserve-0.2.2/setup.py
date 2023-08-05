1f67c198eca0805870872f12acf175f9f6249569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:33 +0000

    Add package qserve/0.2.2/qserve-0.2.2.zip

---
+++ packages/qserve/qserve-0.2.2.zip/qserve-0.2.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+def get_version():
+    d = {}
+    execfile("qs/__init__.py", d, d)
+    return d["__version__"]
+
+if __name__ == "__main__":
+    from distutils.core import setup
+    setup(name='qserve',
+          version=get_version(),
+          url="https://github.com/pediapress/qserve",
+          description="job queue server",
+          license="BSD License",
+          maintainer="pediapress.com",
+          maintainer_email="info@pediapress.com",
+          packages=["qs"])
