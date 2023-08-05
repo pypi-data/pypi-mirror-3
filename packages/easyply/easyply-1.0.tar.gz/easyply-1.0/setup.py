b2f38d60d48111a3950b0ce985879ae1fd1e45de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:13 +0000

    Add package easyply/1.0/easyply-1.0.tar.gz

---
+++ packages/easyply/easyply-1.0.tar.gz/easyply-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='easyply',
+      version='1.0',
+      description='Helper functions for PLY (Python Lex & Yacc)',
+      author='Mike Hordecki',
+      author_email='mike@hordecki.com',
+      packages=['easyply'],
+      long_description = open('README.rst').read(),
+      install_requires=[
+        "ply",
+        ]
+      )
