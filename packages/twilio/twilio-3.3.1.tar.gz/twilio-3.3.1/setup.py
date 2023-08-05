798465a70ec078d597d1eff1da69630526baf13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:32 +0000

    Add package twilio/3.3.1/twilio-3.3.1.tar.gz

---
+++ packages/twilio/twilio-3.3.1.tar.gz/twilio-3.3.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+setup(
+    name = "twilio",
+    version = "3.3.1",
+    description = "Twilio API client and TwiML generator",
+    author = "Twilio",
+    author_email = "help@twilio.com",
+    url = "http://github.com/twilio/twilio-python/",
+    keywords = ["twilio","twiml"],
+    install_requires = ["httplib2", "pyjwt"],
+    packages = find_packages(),
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Communications :: Telephony",
+        ],
+    long_description = """\
+    Python Twilio Helper Library
+    ----------------------------
+
+    DESCRIPTION
+    The Twilio REST SDK simplifies the process of makes calls to the Twilio REST.
+    The Twilio REST API lets to you initiate outgoing calls, list previous calls,
+    and much more.  See http://www.github.com/twilio/twilio-python for more information.
+
+     LICENSE The Twilio Python Helper Library is distributed under the MIT
+    License """ )
