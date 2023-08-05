842d286841eb7b49c5718e1952a488ffa0f67c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:38 +0000

    Add package makesite/0.7.26/makesite-0.7.26.tar.gz

---
+++ packages/makesite/makesite-0.7.26.tar.gz/makesite-0.7.26/makesite/templates/celeryd/service/celeryd_remove.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Import BSFL
+PROJECT_SERVICEDIR={{ project_servicedir }}
+source $PROJECT_SERVICEDIR/.bsfl
+
+PROGRAMM_NAME={{ project }}.{{ branch }}.celeryd
+SUPERVISOR_CELERY_CONFPATH={{ supervisor_celery_confpath }}
+
+# Check supervisor
+check_program supervisord
+check_program supervisorctl
+
+if [ -f $SUPERVISOR_CELERY_CONFPATH ]; then
+    msg_info "Remove link to celeryd supervisor conf: $SUPERVISOR_CELERY_CONFPATH"
+    cmd_or_die "sudo rm -rf $SUPERVISOR_CELERY_CONFPATH"
+fi
+
+msg_info "Update supervisord for celeryd"
+cmd_or_die "sudo supervisorctl reread"
+cmd_or_die "sudo supervisorctl reload"
