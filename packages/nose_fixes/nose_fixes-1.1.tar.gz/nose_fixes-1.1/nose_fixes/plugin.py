cf757a58b28b3096f075e7bd4f65c59f7a97f4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:15 +0000

    Add package nose_fixes/1.1/nose_fixes-1.1.tar.gz

---
+++ packages/nose_fixes/nose_fixes-1.1.tar.gz/nose_fixes-1.1/nose_fixes/plugin.py
@@ -0,0 +1,43 @@
+from nose.plugins import Plugin as NosePlugin
+from nose.loader import TestLoader
+
+class BetterLoader(TestLoader):
+
+    def __init__(self, test_suite_func, config):
+        super(BetterLoader, self).__init__(config)
+        self.test_suite_func = test_suite_func
+        
+    def loadTestsFromModule(self, module, path=None, discovered=False):
+        suite_func = getattr(module, self.test_suite_func, None)
+        if suite_func is not None:
+            return suite_func()
+        return super(BetterLoader, self).loadTestsFromModule(
+            module, path, discovered
+            )
+    
+class Plugin(NosePlugin):
+
+    name = 'nose_fixes'
+
+    enabled = True
+    
+    def options(self, parser, env):
+        parser.add_option("--test-suite-func", action="store",
+                          dest="test_suite_func",
+                          default='test_suite',
+                          help="A function in modules that will return a TestSuite. "
+                               "Defaults to 'test_suite'.")
+        parser.add_option('--show-docstrings', dest='show_docstrings',
+                          action='store_true', default=False,
+                          help='Allow docstrings to be displayed as test '
+                          'descriptions.')
+    
+    def configure(self, options, config):
+        self.test_suite_func = options.test_suite_func
+        self.show_docstrings = options.show_docstrings
+        
+    def prepareTestLoader(self, loader):
+        return BetterLoader(self.test_suite_func, loader.config)
+
+    def prepareTestResult(self, result):
+        result.descriptions = self.show_docstrings
---
+++ packages/nose_fixes/nose_fixes-1.1.tar.gz/nose_fixes-1.1/nose_fixes/tests/test_collection.py
@@ -0,0 +1,44 @@
+from imp import new_module
+from nose.case import Test
+from nose.loader import TestLoader
+from unittest import TestCase
+
+class Tests(TestCase):
+
+    def setUp(self):
+        from nose_fixes.plugin import Plugin
+        plugin = Plugin()
+        plugin.test_suite_func = 'test_suite'
+        self.original_loader = TestLoader()
+        self.l = plugin.prepareTestLoader(self.original_loader)
+        
+    def test_suite(self):
+
+        suite = object()
+        def test_suite():
+            # return a suite
+            return suite
+        
+        mod = new_module('with_test_suite')
+        mod.test_suite = test_suite
+
+        self.assertTrue(self.l.loadTestsFromModule(mod) is suite)
+
+    def test_no_suite(self):
+        def test_sweet():
+            # a tester of sweets ;-)
+            assert True!=False
+        
+        mod = new_module('without_test_suite')
+        mod.test_sweet = test_sweet
+
+        suite = self.l.loadTestsFromModule(mod)
+
+        tests = [t for t in suite]
+        self.assertEqual(len(tests), 1)
+        test = tests[0]
+        self.assertTrue(isinstance(test, Test))
+        self.assertEqual(repr(test), 'Test(without_test_suite.test_sweet)')
+
+    def test_config_kept(self):
+        self.assertTrue(self.l.config is self.original_loader.config)
