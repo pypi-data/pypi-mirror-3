c6853825b476dcfdea536ba4cd7807b788130f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:48 +0000

    Add package Shake-Files/0.3/Shake-Files-0.3.tar.gz

---
+++ packages/Shake-Files/Shake-Files-0.3.tar.gz/Shake-Files-0.3/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+    :Copyright © 2010-2011 by Lúcuma labs (http://lucumalabs.com).
+    :MIT License. (http://www.opensource.org/licenses/mit-license.php)
+
+"""
+import os
+from setuptools import setup
+
+
+ROOTDIR = os.path.dirname(__file__)
+README = os.path.join(ROOTDIR, 'README.md')
+
+
+def run_tests():
+    import sys, subprocess
+    errno = subprocess.call([sys.executable, 'run_tests.py'])
+    raise SystemExit(errno)
+
+
+setup(
+    name='Shake-Files',
+    version='0.3',
+    author='Juan-Pablo Scaletti',
+    author_email='juanpablo@lucumalabs.com',
+    packages=['shake_files'],
+    zip_safe=False,
+    url='http://github.com/lucuma/Shake-Files',
+    license='MIT license (http://www.opensource.org/licenses/mit-license.php)',
+    description='Allows your Shake application to flexibly and efficiently handle file uploads',
+    long_description=open(README).read(),
+    include_package_data=True,
+    install_requires=['Shake'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite='__main__.run_tests',
+)
