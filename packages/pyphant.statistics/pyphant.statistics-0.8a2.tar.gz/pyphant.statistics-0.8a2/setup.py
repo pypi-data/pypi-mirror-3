5f3d56d928f6a422e4c54579778767692e727a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:12 +0000

    Add package pyphant.statistics/0.8a2/pyphant.statistics-0.8a2.tar.gz

---
+++ packages/pyphant.statistics/pyphant.statistics-0.8a2.tar.gz/pyphant.statistics-0.8a2/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Pyphant Statistics toolbox
+This is the Statistics toolbox, that provides workers
+for the Pyphant framework. In order to use it you must have
+the Pyphant framework installed first. Visit
+http://pyphant.sourceforge.net for more information.
+"""
+
+__author__ = "Alexander Held, Andreas W. Liehr, Klaus Zimmermann"
+
+__id__ = '$Id$'
+__revision__ = '$Revision: 25 $'
+
+VERSION = '0.8a2'
+
+import setuptools
+
+setuptools.setup(
+    name="pyphant.statistics",
+    version=VERSION,
+    author=__author__,
+    author_email='alexander.held@fmf.uni-freiburg.de',
+    maintainer='Alexander Held',
+    maintainer_email='alexander.held@fmf.uni-freiburg.de',
+    license="BSD",
+    description=__doc__,
+    install_requires=['pyphant>=0.8a2'],
+    packages=['Statistics'],
+    entry_points="""
+    [pyphant.workers]
+    myeentry = Statistics
+    """,
+    test_suite='Statistics.tests'
+    )
