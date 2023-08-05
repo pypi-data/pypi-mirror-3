beb761ba0f97228d8931d29694420592b586577f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:56 +0000

    Add package plonetheme.ewb_case/0.1.0/plonetheme.ewb_case-0.1.0.tar.gz

---
+++ packages/plonetheme.ewb_case/plonetheme.ewb_case-0.1.0.tar.gz/plonetheme.ewb_case-0.1.0/plonetheme/ewb_case/__init__.py
@@ -0,0 +1,10 @@
+from Products.CMFCore.DirectoryView import registerFileExtension
+from Products.CMFCore.FSFile import FSFile
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
+    
+    # Register SVG File Extensions for CMF
+     
+    registerFileExtension('svg', FSFile)
