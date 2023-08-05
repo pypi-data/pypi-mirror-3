70c632723e6b06a16432cb096c1e8e81bc71de68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:08 +0000

    Add package HalWeb/0.6.0/HalWeb-0.6.0-py2.7.egg

---
+++ packages/HalWeb/HalWeb-0.6.0-py2.7.egg/halicea/baseProject/lib/halicea/dummyControllerMethods.py
@@ -0,0 +1,14 @@
+def edit(request, *args, **kwargs):
+    return "I'm in edit"
+
+def delete(request, *args, **kwargs):
+    return "I'm in delete"
+
+def save(request, *args, **kwargs):
+    return "I'm in save"
+
+def index(request, *args, **kwargs):
+    return "I'm in index"
+
+def view(request, *args, **kwargs):
+    return "I'm in index"
