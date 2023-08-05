9b4f2df64a2f6fef019dabc4f9f4ff071bed9580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:10 +0000

    Add package facepy/0.3/facepy-0.3.tar.gz

---
+++ packages/facepy/facepy-0.3.tar.gz/facepy-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name = 'facepy',
+  version = '0.3',
+  description = "Facepy is an API client for Facebook's Graph API that doesn't suck.",
+  author = "Johannes Gorset",
+  author_email = "jgorset@gmail.com",
+  url = "http://github.com/jgorset/facepy",
+  packages = ['facepy'],
+  install_requires = ['requests']
+)
