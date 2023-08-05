21b4238ecb9f2d2b4fb19b1abc946efdf461c6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:18 +0000

    Add package Products.ChinaPak/1.1/Products.ChinaPak-1.1.tar.gz

---
+++ packages/Products.ChinaPak/Products.ChinaPak-1.1.tar.gz/Products.ChinaPak-1.1/README.txt
@@ -0,0 +1,36 @@
+Introduction
+============
+
+A Chinese Simplified pak for plone.
+
+Tested only on plone 4.
+
+Contain all .po files from ploe.app.locales 4.0.0, fully translated.
+
+Also a chinese id normalizer. :)
+
+covert all chinese characters to **-** splitted pinyin string.
+
+like:
+
+ - u"你好" => "ni-hao"
+ - u"你好 Python" => "ni-hao-python"
+
+Why
+---
+
+Current Plone Chinese Translation is buggy and haven't updated for some days.
+
+How
+---
+
+Inspired By http://www.zope.org/Members/panjunyong/ZopeChinaPak .
+
+Also http://plone-regional-forums.221720.n2.nabble.com/plone4-how-to-override-translations-in-plone-app-locales-tt5456430.html .
+
+Contact
+-------
+
+andelf
+
+ - e-mail: andelf@gmail.com
---
+++ packages/Products.ChinaPak/Products.ChinaPak-1.1.tar.gz/Products.ChinaPak-1.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+reload(sys).setdefaultencoding("UTF-8")
+from setuptools import setup, find_packages
+import os
+import codecs
+
+version = '1.1'
+
+setup(name='Products.ChinaPak',
+      version=version,
+      description="Chinese new translation and a normalizer for Plone 4",
+      long_description=open("README.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        ],
+      keywords='plone i18n chinese normalizer translation chinapak',
+      author='andelf',
+      author_email='andelf@gmail.com',
+      url='http://github.com/andelf/Products.ChinaPak',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
