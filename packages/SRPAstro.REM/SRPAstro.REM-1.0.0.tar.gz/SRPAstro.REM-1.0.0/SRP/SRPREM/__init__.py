c893c1bbe63fe3590d8d71c0d78eb27c75128548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:30 +0000

    Add package SRPAstro.REM/1.0.0/SRPAstro.REM-1.0.0.tar.gz

---
+++ packages/SRPAstro.REM/SRPAstro.REM-1.0.0.tar.gz/SRPAstro.REM-1.0.0/SRP/SRPREM/__init__.py
@@ -0,0 +1,20 @@
+""" 
+
+Context : SRP
+Module  : REM
+Version : 1.0.0
+Author  : Stefano Covino
+Date    : 16/12/2011
+E-mail  : stefano.covino@brera.inaf.it
+URL     : http://www.me.oa-brera.inaf.it/utenti/covino
+
+
+Usage   : to be imported
+
+Remarks :
+
+History : (16/12/2011) First version.
+"""
+
+__version__ = '1.0.0'
+
