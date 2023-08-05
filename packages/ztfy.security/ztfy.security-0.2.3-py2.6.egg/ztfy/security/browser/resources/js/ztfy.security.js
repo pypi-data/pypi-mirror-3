0a815d626468472a05bb6f343f735cda1e43bd3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:35 +0000

    Add package ztfy.security/0.2.3/ztfy.security-0.2.3-py2.6.egg

---
+++ packages/ztfy.security/ztfy.security-0.2.3-py2.6.egg/ztfy/security/browser/resources/js/ztfy.security.js
@@ -0,0 +1,56 @@
+(function($) {
+
+	if (typeof($.ZTFY) == 'undefined') {
+		$.ZTFY = {}
+	}
+
+	$.ZTFY.security = {
+
+		json: {
+
+			getAddr: function() {
+				var href = window.location.href;
+				return href.substr(0, href.lastIndexOf("/")+1);
+			},
+
+			post: function(method, params, onsuccess, onerror, base) {
+				var addr = $.ZTFY.security.json.getAddr();
+				if (base) {
+					addr += '/' + base;
+				}
+				var options = {
+					url: addr,
+					type: 'POST',
+					method: method,
+					params: params,
+					success: onsuccess,
+					error: onerror
+				};
+				$.jsonRpc(options);
+			}
+
+		},  /** $.ZTFY.security.json */
+
+		findPrincipals: function(query) {
+			var result;
+			var options = {
+				url: $.ZTFY.security.json.getAddr(),
+				type: 'POST',
+				method: 'findPrincipals',
+				async: false,
+				params: {
+					query: query
+				},
+				success: function(data, status) {
+					result = data.result;
+				},
+				error: function(request, status, error) {
+					jAlert(request.responseText, "Error !", window.location.reload);
+				}
+			}
+			$.jsonRpc(options);
+			return result;
+		}
+	}
+
+})(jQuery);
