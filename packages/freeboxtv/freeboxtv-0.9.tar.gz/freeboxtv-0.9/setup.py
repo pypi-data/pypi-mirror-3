3f76eac7978e81d4ce555fe8f3cc96d7ae874b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:25 +0000

    Add package freeboxtv/0.9/freeboxtv-0.9.tar.gz

---
+++ packages/freeboxtv/freeboxtv-0.9.tar.gz/freeboxtv-0.9/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.9'
+
+long_description = ''
+if os.path.isfile('README.txt'):
+    long_description=open('README.txt').read() + open('CHANGES.txt').read()
+
+setup(name='freeboxtv',
+      version=version,
+      description="VLC launcher for Freebox TV",
+      long_description=long_description,
+      classifiers=[
+          'Environment :: Console',
+          'License :: OSI Approved',
+          'Operating System :: POSIX',
+          'Topic :: Home Automation',
+      ],
+      keywords='freebox',
+      author='Gael Pasgrimaud',
+      author_email='gael@gawel.org',
+      url='http://www.gawel.org/docs/dist',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      fbxtv = freeboxtv:main
+      """,
+      )
