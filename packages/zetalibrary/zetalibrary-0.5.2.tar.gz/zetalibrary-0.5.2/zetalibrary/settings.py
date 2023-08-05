0b274ffd5c06ba831276ddbd8af464b9cdd0c65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:54 +0000

    Add package zetalibrary/0.5.2/zetalibrary-0.5.2.tar.gz

---
+++ packages/zetalibrary/zetalibrary-0.5.2.tar.gz/zetalibrary-0.5.2/zetalibrary/settings.py
@@ -0,0 +1,15 @@
+from os import path as op, environ, getcwd
+
+
+VERSION = '0.5.2'
+BASEDIR = op.abspath(op.dirname(__file__))
+LIBDIR  = op.join(BASEDIR, 'libs')
+CUSTOMDIR = environ.get('ZETA_LIBDIR', None)
+FORMATS = ['css', 'scss', 'js']
+CURRENT_CONFIG = op.join(getcwd(), "zeta.ini")
+COLORS = dict(
+    okgreen = '\033[92m',
+    warning = '\033[93m',
+    fail = '\033[91m',
+    endc = '\033[0m',
+)
