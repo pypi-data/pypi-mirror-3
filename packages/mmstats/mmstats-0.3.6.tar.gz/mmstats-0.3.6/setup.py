95b03de6a5f117ca2378733f79e092165b1d4a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:58 +0000

    Add package mmstats/0.3.6/mmstats-0.3.6.tar.gz

---
+++ packages/mmstats/mmstats-0.3.6.tar.gz/mmstats-0.3.6/setup.py
@@ -0,0 +1,37 @@
+import sys
+
+from setuptools import setup
+from setuptools.extension import Extension
+
+#XXX gettid only works on Linux, don't bother else
+if 'linux' in sys.platform:
+    exts = [Extension('_libgettid', sources=['_libgettid.c'])]
+else:
+    exts = []
+
+setup(
+    name='mmstats',
+    url='https://github.com/schmichael/mmstats',
+    version='0.3.6',
+    license='BSD',
+    author='Michael Schurter',
+    author_email='m@schmichael.com',
+    description='Stat publishing and consuming tools',
+    long_description=open('README.rst').read(),
+    py_modules=['libgettid',
+                'mmstats',
+                'slurpstats',
+                'mmash',
+                'mmash_settings'
+            ],
+    entry_points={
+        'console_scripts': [
+            'mmash=mmash:main',
+            'slurpstats=slurpstats:main',
+        ],
+    },
+    ext_modules=exts,
+    install_requires=['Flask'],
+    classifiers=['License :: OSI Approved :: BSD License'],
+    zip_safe=False,
+)
