81ce1e539cd27e6313f8fe1e49f15cb39fd3208c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:15 +0000

    Add package pydrone/0.2/pydrone-0.2.tar.gz

---
+++ packages/pydrone/pydrone-0.2.tar.gz/pydrone-0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup, Extension
+
+module = Extension('pydrone',
+                    libraries = ['mozjs'],
+                    include_dirs = ['/usr/local/include/mozjs', '/usr/include/mozjs'],
+                    sources = ['pydrone.c'])
+
+setup (name = 'pydrone',
+       author = 'Unbit',
+       version = '0.2',
+       description = 'pydrone',
+       ext_modules = [module])
