b30b8ca3d6a093804df13ee3ccc81b58cf395a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package Products.Archetypes/1.5.18/Products.Archetypes-1.5.18.tar.gz

---
+++ packages/Products.Archetypes/Products.Archetypes-1.5.18.tar.gz/Products.Archetypes-1.5.18/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+version = '1.5.18'
+
+setup(name='Products.Archetypes',
+      version=version,
+      description="Archetypes is a developers framework for rapidly "
+                  "developing and deploying rich, full featured content "
+                  "types within the context of Zope/CMF and Plone.",
+      long_description=open("README.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        ],
+      keywords='Archetypes Plone CMF Zope',
+      author='Benjamin Saller and others',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://plone.org/',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.Marshall',
+          'Products.MimetypesRegistry',
+          'Products.PortalTransforms',
+          'Products.validation>=1.6.4',
+      ],
+      )
