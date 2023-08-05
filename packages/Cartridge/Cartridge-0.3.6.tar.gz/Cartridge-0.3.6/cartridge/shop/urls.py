f069167233f706d7451bce4218586d6279f5fa65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:14 +0000

    Add package Cartridge/0.3.6/Cartridge-0.3.6.tar.gz

---
+++ packages/Cartridge/Cartridge-0.3.6.tar.gz/Cartridge-0.3.6/cartridge/shop/urls.py
@@ -0,0 +1,21 @@
+
+from django.conf.urls.defaults import *
+
+from mezzanine.conf import settings
+
+
+urlpatterns = patterns("cartridge.shop.views",
+    url("^product/(?P<slug>.*)/$", "product", name="shop_product"),
+    url("^search/$", "search", name="shop_search"),
+    url("^wishlist/$", "wishlist", name="shop_wishlist"),
+    url("^cart/$", "cart", name="shop_cart"),
+    url("^checkout/$", "checkout_steps", name="shop_checkout"),
+    url("^checkout/complete/$", "complete", name="shop_complete"),
+    url("^invoice/(?P<order_id>\d+)/$", "invoice", name="shop_invoice"),
+)
+
+if getattr(settings, "SHOP_CHECKOUT_ACCOUNT_ENABLED", False):
+    urlpatterns += patterns("cartridge.shop.views",
+        url("^account/$", "account", name="shop_account"),
+        url("^logout/$", "logout", name="shop_logout"),
+    )
