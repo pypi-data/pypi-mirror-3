fb7148b35c2fb8545e5b0da92890612b6534efbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:56 +0000

    Add package surveilr/0.1a1/surveilr-0.1a1.tar.gz

---
+++ packages/surveilr/surveilr-0.1a1.tar.gz/surveilr-0.1a1/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+#
+#    Surveilr - Log aggregation, analysis and visualisation
+#
+#    Copyright (C) 2011  Linux2Go
+#
+#    This program is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU Affero General Public License
+#    as published by the Free Software Foundation, either version 3 of
+#    the License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public
+#    License along with this program.  If not, see
+#    <http://www.gnu.org/licenses/>.
+#
+from setuptools import setup
+
+def get_install_requires():
+    install_requires = []
+    with open('tools/pip-requirements.txt', 'r') as fp:
+        for l in fp:
+            l = l.strip()
+            if l.startswith('#'):
+                continue
+            elif l.startswith('-e'):
+                install_requires.append(l[l.index('#egg=')+5:])
+            else:
+                install_requires.append(l)
+    return install_requires
+
+setup(
+    name='surveilr',
+    version='0.1a1',
+    description='Monitoring System evolved',
+    author='Soren Hansen',
+    license='AGPL',
+    author_email='soren@linux2go.dk',
+    url='http://surveilr.org/',
+    packages=['surveilr'],
+    install_requires=get_install_requires(),
+    test_suite='nose.collector',
+    install_package_data=True,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Internet :: Log Analysis',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Networking :: Monitoring',
+    ]
+)
---
+++ packages/surveilr/surveilr-0.1a1.tar.gz/surveilr-0.1a1/surveilr/utils.py
@@ -0,0 +1,60 @@
+"""
+    Surveilr - Log aggregation, analysis and visualisation
+
+    Copyright (C) 2011  Linux2Go
+
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU Affero General Public License
+    as published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public
+    License along with this program.  If not, see
+    <http://www.gnu.org/licenses/>.
+
+    Utility functions
+"""
+
+import httplib2
+import json
+
+
+def truncate(number, rounding_factor):
+    """Truncate to nearest arbitrary multiple
+
+    >>> truncate(1000, 100)
+    1000
+    >>> truncate(1099, 100)
+    1000
+    >>> truncate(1099, 50)
+    1050
+    >>> truncate(1099, 50)
+    1050
+    """
+    return (int(number) / int(rounding_factor)) * int(rounding_factor)
+
+
+def enhance_data_point(data_point):
+    http = httplib2.Http(timeout=10)
+
+    service = data_point.service[0]
+    user = service.user[0]
+
+    for plugin in service.plugins:
+        saved_state = plugin.get('saved_state', None)
+        url = plugin.get('url')
+        body = json.dumps({'timestamp': data_point.timestamp,
+                           'metrics': data_point.metrics,
+                           'service_id': service.key,
+                           'user_id': user.key,
+                           'saved_state': saved_state})
+
+        response, content = http.request(url, method="POST", body=body)
+        data = json.loads(content)
+        plugin['saved_state'] = data['state']
+    service.save()
