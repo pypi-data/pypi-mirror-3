aedad1bb6e96d7eccc35ba75a6164b0d31e443d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package pyjslint/0.3.2/pyjslint-0.3.2.tar.gz

---
+++ packages/pyjslint/pyjslint-0.3.2.tar.gz/pyjslint-0.3.2/CHANGES.rst
@@ -0,0 +1,20 @@
+Changes
+=======
+
+0.3.3dev (unreleased)
+---------------------
+- ...
+
+0.3.2 2011-11-17
+----------------
+- Fix bug in setup.py, wrong directive and wrong entry point
+
+0.3.1 2011-11-16
+----------------
+- Rename the module to avoid collisions with the jslint python wrapper project
+
+0.3 2011-11-16
+--------------
+- Simplify the module by moving the main file into the root directory
+- Remove the reformat function
+
---
+++ packages/pyjslint/pyjslint-0.3.2.tar.gz/pyjslint-0.3.2/setup.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2011  Alejandro Blanco <ablanco@yaco.es>
+
+import os
+from setuptools import setup
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(
+    name="pyjslint",
+    version='0.3.2',
+    author="Alejandro Blanco",
+    author_email="ablanco@yaco.es",
+    description="JSLint wrapper",
+    long_description=read('README.rst'),
+    license="BSD-3",
+    classifiers = ["Development Status :: 4 - Beta",
+                   "Environment :: Console",
+                   "Intended Audience :: Developers",
+                   "Intended Audience :: System Administrators",
+                   "License :: OSI Approved :: BSD License",
+                   "Natural Language :: English",
+                   "Programming Language :: JavaScript",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Quality Assurance"],
+    keywords="jslint javascript lint hook qa",
+    py_modules=['pyjslint'],
+    url='https://github.com/Yaco-Sistemas/pyjslint/',
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'pyjslint = pyjslint:main',
+            ]},
+)
