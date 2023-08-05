61ffd5122419a71bc17d79294de19cbf98a04bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:54 +0000

    Add package collective.sendaspdf/2.4.1/collective.sendaspdf-2.4.1.zip

---
+++ packages/collective.sendaspdf/collective.sendaspdf-2.4.1.zip/collective.sendaspdf-2.4.1/collective/sendaspdf/tests/test_unit.py
@@ -0,0 +1,11 @@
+import unittest
+import doctest
+
+from collective.sendaspdf.tests.base import OPTIONFLAGS
+
+def test_suite():
+    return unittest.TestSuite([
+        doctest.DocFileSuite('utils.py',
+                              optionflags=OPTIONFLAGS,
+                              package='collective.sendaspdf'),
+        ])
