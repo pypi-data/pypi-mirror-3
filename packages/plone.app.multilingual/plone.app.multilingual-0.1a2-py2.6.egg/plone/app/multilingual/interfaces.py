2af51b48455cfa57797aac05853d63f4cca09d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:17 +0000

    Add package plone.app.multilingual/0.1a2/plone.app.multilingual-0.1a2-py2.6.egg

---
+++ packages/plone.app.multilingual/plone.app.multilingual-0.1a2-py2.6.egg/plone/app/multilingual/interfaces.py
@@ -0,0 +1,9 @@
+from zope.interface import Interface
+from zope import schema
+from plone.app.multilingual import _
+from plone.registry import field
+
+
+class IPloneAppMultilingualInstalled(Interface):
+    """ layer """
+
