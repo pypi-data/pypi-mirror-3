3a628205db31c426c70d2f26d8cbfad9403645af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:26 +0000

    Add package xi/1.0/xi-1.0.tar.gz

---
+++ packages/xi/xi-1.0.tar.gz/xi-1.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+
+setup(
+    name = "xi",
+    version = "1.0",
+    author = "startling",
+    author_email = "tdixon51793@gmail.com",
+    description = "A little script for copying and pasting through unix pipes.",
+    scripts = ['scripts/xi'],
+    install_requires = ['clint', 'xerox'],
+    keywords = ['paste', 'copy', 'copypaste', 'pipes'],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Environment :: Console"
+    ]
+)
