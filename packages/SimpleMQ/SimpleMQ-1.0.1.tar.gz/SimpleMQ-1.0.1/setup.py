4f8c11839328e12696d0df6956e81e41f62fc01a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:03 +0000

    Add package SimpleMQ/1.0.1/SimpleMQ-1.0.1.tar.gz

---
+++ packages/SimpleMQ/SimpleMQ-1.0.1.tar.gz/SimpleMQ-1.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SimpleMQ',
+      version='1.0.1',
+      author='huangz',
+      author_email='huangz1990@gmail.com',
+      url='https://github.com/huangz1990/SimpleMQ',
+      description='An MQ(message queue) library use Redis as backend.',
+      long_description='',
+      download_url='',
+      packages=['simplemq'],
+      classifiers = [
+        'Topic :: Database',
+        ],
+     )
