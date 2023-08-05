ed831b259787aebf43dcf92093ee6f277650981e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:20 +0000

    Add package are-you-a-human/1.0.3/are-you-a-human-1.0.3.tar.gz

---
+++ packages/are-you-a-human/are-you-a-human-1.0.3.tar.gz/are-you-a-human-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'are-you-a-human',
+    version = '1.0.3',
+    author = 'Are You a Human',
+    author_email = 'support@areyouahuman.com',
+    maintainer = 'Are You a Human',
+    maintainer_email = 'plugins@areyouahuman.com',
+    url = 'http://portal.areyouahuman.com',
+    license = 'MIT (See LICENSE.txt)',
+    description = 'Are You a Human (AYAH) Python integration library and CAPTCHA alternative',
+    keywords = 'are you a human ayah captcha alternative',
+    packages = ['ayah'],
+)
