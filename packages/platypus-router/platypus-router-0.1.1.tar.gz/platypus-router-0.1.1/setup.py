605fbd1991bf1a929f7b99321132e7bab117c362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:56 +0000

    Add package platypus-router/0.1.1/platypus-router-0.1.1.tar.gz

---
+++ packages/platypus-router/platypus-router-0.1.1.tar.gz/platypus-router-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README') as f:
+    long_description = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name = 'platypus-router',
+    version = '0.1.1',
+    description = 'Django like WSGI router',
+    long_description=long_description,
+    author = 'Vincent Jauneau',
+    author_email = 'vincent.jauneau@platypus-creation.com',
+    url = 'https://github.com/platypus-creation/platypus-router/',
+    license=license,
+    packages = find_packages(),
+    install_requires = ['WebOb',],
+    test_suite='router.tests.TestRouter',
+    keywords='platypus, router, routing, wsgi, django',
+)
