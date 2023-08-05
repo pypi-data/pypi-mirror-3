43f654c8cff6e0039dfc2427533ce9b1df05b3c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package PyUbootImage/0.1e/PyUbootImage-0.1e.tar.gz

---
+++ packages/PyUbootImage/PyUbootImage-0.1e.tar.gz/PyUbootImage-0.1e/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup,find_packages
+
+setup(
+	name='PyUbootImage',
+	version='0.1e',
+	description='Uboot image parser',
+	long_description='This package provides a way to read u-boot images header and to retrieve encapsulated binaries. It also handles multi-image files.',
+	author='Matteo Mattei; Nicola Ponzeveroni',
+	author_email='info@matteomattei.com; nicola.ponzeveroni@gilbarco.com',
+	url='https://github.com/matteomattei/PyUbootImage',
+	packages=find_packages(),
+	keywords = ["filesystem", "parser", "u-boot"],
+	classifiers = [
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3",
+		"Development Status :: 4 - Beta",
+		"Environment :: Other Environment",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+		"Operating System :: OS Independent",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		],
+)
+
