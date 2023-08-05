3ab7cefab2bd1f67c078670454b76bd4459d6dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:27 +0000

    Add package toolkit_library/0.2.2/toolkit_library-0.2.2.tar.gz

---
+++ packages/toolkit_library/toolkit_library-0.2.2.tar.gz/toolkit_library-0.2.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'toolkit_library',
+    version = '0.2.2',
+    url = 'http://stackoverflow.com/users/862862/tyler-long',
+    license = 'BSD',
+    author = 'Tyler Long',
+    author_email = 'tyler4long@gmail.com',
+    description = 'Toolkit Library, full of useful toolkits',
+    long_description = open('README').read(),
+    packages = ['toolkit_library', ],
+    platforms='any',
+)
