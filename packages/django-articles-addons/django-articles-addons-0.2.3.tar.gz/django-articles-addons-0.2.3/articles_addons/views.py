cc2bf22d23638a6aa91830ca3b23cafb7d25d67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:18 +0000

    Add package django-articles-addons/0.2.3/django-articles-addons-0.2.3.tar.gz

---
+++ packages/django-articles-addons/django-articles-addons-0.2.3.tar.gz/django-articles-addons-0.2.3/articles_addons/views.py
@@ -0,0 +1,26 @@
+# Views for article CRUD
+from articles.models import Article
+from articles_addons.forms import ArticleForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+class ArticleCreateView(CreateView):
+    form_class=ArticleForm
+    template_name='articles/post_create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleCreateView, self).get_context_data(**kwargs)
+        return context
+
+class ArticleUpdateView(UpdateView):
+    form_class=ArticleForm
+    template_name='articles/post_update.html'
+    queryset=Article.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleUpdateView, self).get_context_data(**kwargs)
+        return context
+
+class ArticleDeleteView(DeleteView):
+    model=Article
+    template_name='articles/post_delete.html'
+    success_url='/articles/' 
