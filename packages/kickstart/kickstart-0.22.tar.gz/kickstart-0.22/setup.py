aba3f65fa5fb4c85f254a17bcd76510ea88c859e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:33 +0000

    Add package kickstart/0.22/kickstart-0.22.tar.gz

---
+++ packages/kickstart/kickstart-0.22.tar.gz/kickstart-0.22/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'kickstart',
+    scripts = ['kickstart.py'],
+    packages = ['kickstart', 'kickstart/base'],
+    url = 'http://pypi.python.org/pypi/kickstart',
+    version = '0.22',
+    install_requires = ['PyYAML'],
+    description = 'Framework agnostic setup script',
+    author = 'Richard Layte',
+    author_email = 'rich.layte@gmail.com'
+)
