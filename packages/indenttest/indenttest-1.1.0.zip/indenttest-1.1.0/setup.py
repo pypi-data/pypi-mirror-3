eb43143230b23814a2726232ea900302a25756a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:16 +0000

    Add package indenttest/1.1.0/indenttest-1.1.0.zip

---
+++ packages/indenttest/indenttest-1.1.0.zip/indenttest-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'indenttest',
+    version = '1.1.0',
+    py_modules = ['indent'],
+    author = 'name',
+    url = 'ygang.tisotry.com',
+    description = 'indent',
+    )
