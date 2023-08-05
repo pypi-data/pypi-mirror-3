a1f110002e7e694900a021dd71c32d4c0cd12b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:10 +0000

    Add package abouttag/1.0/abouttag-1.0.tar.gz

---
+++ packages/abouttag/abouttag-1.0.tar.gz/abouttag-1.0/abouttag/test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+    abouttag.test
+
+    Run tests for the whole abouttag module.
+
+    Copyright 2010 AUTHORS (see AUTHORS file)
+    License: MIT, see LICENSE for more information
+"""
+
+import unittest
+from about import *
+from abouttag.fluiddb import *
+from abouttag.uri import *
+from abouttag.database import *
+from abouttag.objects import *
+from abouttag.location import *
+from abouttag.books import *
+from abouttag.music import *
+from abouttag.film import *
+
+unittest.main()
+
+
+
