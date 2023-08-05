1c763dbe02f6173f39285075c2477ee112c60055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:44 +0000

    Add package ageliaco.tracker/0.1.5/ageliaco.tracker-0.1.5.tar.gz

---
+++ packages/ageliaco.tracker/ageliaco.tracker-0.1.5.tar.gz/ageliaco.tracker-0.1.5/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import os
+from setuptools.command import sdist
+del sdist.finders[:]
+
+version = '0.1.5'
+
+setup(name='ageliaco.tracker',
+      version=version,
+      description="Issue tracker",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Serge Renfer',
+      author_email='serge.renfer@gmail.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ageliaco'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.dexterity',
+          'collective.autopermission',
+          # -*- Extra requirements: -*-
+          'Plone',
+          'plone.principalsource',
+          'collective.wtf',
+          'Products.AdvancedQuery',
+          'collective.gtags',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
