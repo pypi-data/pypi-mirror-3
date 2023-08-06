be83ac03c18971a59b779d22886241611dc5a32d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:45 +0000

    Add package naster/1.0.1/naster-1.0.1.tar.gz

---
+++ packages/naster/naster-1.0.1.tar.gz/naster-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name			=	'naster',
+	version			=	'1.0.1',
+	py_modules		=	['naster'],
+	author			=	'nigelbhall',
+	author_email	=	'nigel@hallnet.com',
+	url				=	'http://hallnet.com',
+	description		=	'A simple nested list from the Head First book',
+	)
