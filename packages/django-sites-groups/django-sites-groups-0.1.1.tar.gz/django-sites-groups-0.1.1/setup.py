1ff3385b58d7d2dce49056f9e24091507db73aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:42 +0000

    Add package django-sites-groups/0.1.1/django-sites-groups-0.1.1.tar.gz

---
+++ packages/django-sites-groups/django-sites-groups-0.1.1.tar.gz/django-sites-groups-0.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-sites-groups',
+    version='0.1.1',
+    description='Organize sites from the Django sites framework into groups.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/django-sites-groups',
+    packages = find_packages(),
+    dependency_links = [
+    ],
+    install_requires = [
+        'django'
+    ],
+    include_package_data=True,
+    tests_require=[
+        'django-setuptest>=0.0.6',
+    ],
+    test_suite="setuptest.SetupTestSuite",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
