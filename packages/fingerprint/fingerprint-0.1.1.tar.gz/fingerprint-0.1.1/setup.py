164bceebb5c23e239dad7a406dc1fec3f9b3f814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:37 +0000

    Add package fingerprint/0.1.1/fingerprint-0.1.1.tar.gz

---
+++ packages/fingerprint/fingerprint-0.1.1.tar.gz/fingerprint-0.1.1/setup.py
@@ -0,0 +1,24 @@
+# set up details
+from distutils.core import setup
+
+setup(
+    name='fingerprint',
+    version='0.1.1',
+    url='http://github.com/kailashbuki/fingerprint',
+    license='LICENSE.txt',
+    author='Kailash Budhathoki',
+    author_email='kailash.buki@gmail.com',
+    description='Document fingerprint generator',
+    long_description=open('README').read(),
+    packages=['fingerprint'],
+    zip_safe=False,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
