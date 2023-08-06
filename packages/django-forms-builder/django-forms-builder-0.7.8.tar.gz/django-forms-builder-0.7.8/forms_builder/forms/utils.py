668507e17e100c3e7ed8727e0af869cc93b9b913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:34 +0000

    Add package django-forms-builder/0.7.8/django-forms-builder-0.7.8.tar.gz

---
+++ packages/django-forms-builder/django-forms-builder-0.7.8.tar.gz/django-forms-builder-0.7.8/forms_builder/forms/utils.py
@@ -0,0 +1,19 @@
+
+from django.template.defaultfilters import slugify as django_slugify
+from unidecode import unidecode
+
+
+# Timezone support with fallback.
+try:
+    from django.utils.timezone import now
+except ImportError:
+    from datetime import datetime
+    now = datetime.now
+
+
+def slugify(ss):
+    """
+    Translates unicode into closest possible ascii chars before
+    slugifying.
+    """
+    return django_slugify(unidecode(ss))
