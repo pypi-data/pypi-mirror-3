17a443721c2592d7ee66326c3289a297dd93cf41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:47 +0000

    Add package CodeShow/0.2.2/CodeShow-0.2.2.tar.gz

---
+++ packages/CodeShow/CodeShow-0.2.2.tar.gz/CodeShow-0.2.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+	name = 'CodeShow',
+	version = '0.2.2',
+	url = 'http://codeshow.site44.com',
+	license = open('LICENSE').read(),
+	author = 'Steve Marx',
+	author_email = 'smarx@smarx.com',
+	description = 'CodeShow turns a directory of code into a static website suitable for use in presentations.',
+	long_description = open('README.md').read(),
+	packages = ['codeshow'],
+	entry_points = { 'console_scripts': ['codeshow = codeshow.cmdline:main']},
+	install_requires = map(str.rstrip, open('requirements.txt').readlines()),
+	include_package_data = True
+)
