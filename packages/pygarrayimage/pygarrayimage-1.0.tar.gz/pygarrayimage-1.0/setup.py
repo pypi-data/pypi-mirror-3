2bdd933161d47a2350898b52b90946d7ec89876c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:17 +0000

    Add package pygarrayimage/1.0/pygarrayimage-1.0.tar.gz

---
+++ packages/pygarrayimage/pygarrayimage-1.0.tar.gz/pygarrayimage-1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+
+kws = {}
+
+setup(name='pygarrayimage',
+      description="Allow numpy arrays as source of texture data for pyglet.",
+      long_description = \
+"""pygarrayimage allows display of Python objects supporting the array
+interface as OpenGL textures without a copy. The OpenGL texture
+handling is done using pyglet.
+
+In other words, this allows fast transfer of data from numpy arrays
+(or any other data source supporting the array interface) to the video
+card.
+
+TODO: support the buffer interface added with Python 2.6/3.0 rather
+than the numpy-defined array interface.
+
+""",
+      version='1.0', # keep in sync with pygarrayimage/arrayimage.py
+      author='Andrew Straw',
+      author_email='strawman@astraw.com',
+      url='http://code.astraw.com/projects/motmot/wiki/pygarrayimage',
+      license='BSD',
+      packages=['pygarrayimage'],
+      **kws)
