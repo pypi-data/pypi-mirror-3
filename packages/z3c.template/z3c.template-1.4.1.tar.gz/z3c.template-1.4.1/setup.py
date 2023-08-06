eb4a1f7a1cf999eb2a235b74b2cd95b82d6a154f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:28 +0000

    Add package z3c.template/1.4.1/z3c.template-1.4.1.tar.gz

---
+++ packages/z3c.template/z3c.template-1.4.1.tar.gz/z3c.template-1.4.1/setup.py
@@ -0,0 +1,78 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+
+$Id: setup.py 124412 2012-02-15 18:22:34Z menesis $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup (
+    name='z3c.template',
+    version='1.4.1',
+    author = "Roger Ineichen and the Zope Community",
+    author_email = "zope-dev@zope.org",
+    description = "A package implementing advanced Page Template patterns.",
+    long_description=(
+        read('README.txt')
+        + '\n\n.. contents::\n\n'+
+        read('src', 'z3c', 'template', 'README.txt')
+        + '\n' +
+        read('src', 'z3c', 'template', 'zcml.txt')
+        + '\n' +
+        read('CHANGES.txt')
+        ),
+    license = "ZPL 2.1",
+    keywords = "zope3 template layout zpt pagetemplate",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://pypi.python.org/pypi/z3c.template',
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c'],
+    extras_require = dict(
+        test = [
+            'zope.app.testing',
+            'zope.app.pagetemplate',
+            'zope.testing',
+            'z3c.ptcompat>=1.0',
+            ],
+        chameleon = [
+            'z3c.pt >= 2.1',
+            'z3c.ptcompat',
+            ],
+        ),
+    install_requires = [
+        'setuptools',
+        'zope.component',
+        'zope.configuration',
+        'zope.interface',
+        'zope.pagetemplate',
+        'zope.publisher',
+        'zope.schema',
+        ],
+    include_package_data = True,
+    zip_safe = False,
+    )
---
+++ packages/z3c.template/z3c.template-1.4.1.tar.gz/z3c.template-1.4.1/src/z3c/template/tests.py
@@ -0,0 +1,62 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id: tests.py 124401 2012-02-15 17:50:33Z menesis $
+"""
+__docformat__ = "reStructuredText"
+
+from zope.app.testing import setup
+from zope.configuration import xmlconfig
+import doctest
+import itertools
+import unittest
+
+import z3c.template.template
+
+
+def setUp(test):
+    root = setup.placefulSetUp(site=True)
+    test.globs['root'] = root
+
+def tearDown(test):
+    setup.placefulTearDown()
+
+def setUpZPT(suite):
+    setUp(suite)
+
+def setUpZ3CPT(suite):
+    setUp(suite)
+    import z3c.pt
+    import z3c.ptcompat
+    xmlconfig.XMLConfig('configure.zcml', z3c.pt)()
+    xmlconfig.XMLConfig('configure.zcml', z3c.ptcompat)()
+
+    # We have to cook this template explicitly, because it's a module
+    # global.
+    from z3c.template.template import Macro
+    Macro.wrapper._cook()
+
+
+def test_suite():
+    tests = ((
+        doctest.DocFileSuite('README.txt',
+            setUp=setUp, tearDown=tearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            ),
+        doctest.DocFileSuite('zcml.txt', setUp=setUp, tearDown=tearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            ),
+        ) for setUp in (setUpZPT, setUpZ3CPT,))
+
+    return unittest.TestSuite(itertools.chain(*tests))
