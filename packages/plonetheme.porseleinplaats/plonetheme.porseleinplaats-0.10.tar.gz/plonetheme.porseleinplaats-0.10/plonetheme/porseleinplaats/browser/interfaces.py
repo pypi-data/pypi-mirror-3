11f56bdb62732cd50964dbd3475c920b4305b408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:50 +0000

    Add package plonetheme.porseleinplaats/0.10/plonetheme.porseleinplaats-0.10.tar.gz

---
+++ packages/plonetheme.porseleinplaats/plonetheme.porseleinplaats-0.10.tar.gz/plonetheme.porseleinplaats-0.10/plonetheme/porseleinplaats/browser/interfaces.py
@@ -0,0 +1,12 @@
+from plone.theme.interfaces import IDefaultPloneLayer
+from plone.portlets.interfaces import IPortletManager
+
+from plonetheme.intkBase.browser.interfaces import IIntkBaseLayer
+from zope.interface import implements, Interface
+
+class IThemeSpecific(IIntkBaseLayer):
+    """Marker interface that defines a Zope 3 browser layer.
+    """
+    
+class ISidebarManager(IPortletManager):
+ """Portlets on the permanent sidebar"""
