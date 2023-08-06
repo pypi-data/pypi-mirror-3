9676d1dd8d624fd46dc958f82682ec9a533322fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:02 +0000

    Add package modilabs-python-utils/0.1.2/modilabs-python-utils-0.1.2.tar.gz

---
+++ packages/modilabs-python-utils/modilabs-python-utils-0.1.2.tar.gz/modilabs-python-utils-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+     name='modilabs-python-utils',
+     version='0.1.2',
+     author='modilabs',
+     author_email='info@modilabs.org',
+     packages=['modilabs', 'modilabs.utils'],
+     scripts=[],
+     url='http://pypi.python.org/pypi/modilabs-python-utils/',
+     license='LICENSE.txt',
+     description='Collection of common Python utilities by Modi Group',
+     long_description=open('README.txt').read(),
+)
