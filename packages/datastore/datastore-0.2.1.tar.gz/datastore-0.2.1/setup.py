8055cbea95f4d78dc6d7ce124f77e945ee7595b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:56 +0000

    Add package datastore/0.2.1/datastore-0.2.1.tar.gz

---
+++ packages/datastore/datastore-0.2.1.tar.gz/datastore-0.2.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+__version__ = '0.2.1'
+# don't forget to update datastore/__init__.py
+
+packages = filter(lambda p: p.startswith('datastore'), find_packages())
+
+setup(
+  name="datastore",
+  version=__version__,
+  description="simple, unified API for multiple data stores",
+  author="Juan Batiz-Benet",
+  author_email="juan@benet.ai",
+  url="http://github.com/jbenet/datastore",
+  keywords=["datastore", "unified api", "memcached", "redis", "git", "mongo"],
+  packages=packages,
+  install_requires=["smhasher"],
+  extras_requires=["bson", "nanotime"],
+  license="MIT License",
+  classifiers=[
+    "Topic :: Database :: Front-Ends"
+  ]
+)
