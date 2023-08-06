fd28bfebc46b02d15c500281a230c91745835858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:10 +0000

    Add package SRPAstro.REM/1.2.0/SRPAstro.REM-1.2.0.tar.gz

---
+++ packages/SRPAstro.REM/SRPAstro.REM-1.2.0.tar.gz/SRPAstro.REM-1.2.0/SRP/SRPREM/__init__.py
@@ -0,0 +1,40 @@
+""" 
+
+Context : SRP
+Module  : REM
+Version : 1.0.5
+Author  : Stefano Covino
+Date    : 18/04/2012
+E-mail  : stefano.covino@brera.inaf.it
+URL     : http://www.me.oa-brera.inaf.it/utenti/covino
+
+
+Usage   : to be imported
+
+Remarks :
+
+History : (16/12/2011) First version.
+        : (12/01/2012) V. 1.1.0b1.
+        : (06/02/2012) V. 1.1.0.
+        : (01/04/2012) V. 1.1.1b1.
+        : (04/04/2012) V. 1.2.0b1 and new constant.
+        : (18/04/2012) V. 1.2.0.
+"""
+
+__version__ = '1.2.0'
+
+
+
+# REM pixel size
+REMIRPixSize    = 1.22
+ROSSPixSize     = 0.54
+
+
+# Constants
+REMIR   =   'REMIR'
+ROSS    =   'REM-ROSS'
+CAMERA  =   'INSTRUME'
+
+# External commands
+astro   =   'SRPAstrometry'
+
