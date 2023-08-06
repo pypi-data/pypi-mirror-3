dd4c93644414cb08fc8276410a52706e620acad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:24 +0000

    Add package robot-detection/0.2.6/robot-detection-0.2.6.tar.gz

---
+++ packages/robot-detection/robot-detection-0.2.6.tar.gz/robot-detection-0.2.6/setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="robot-detection",
+      version="0.2.6",
+      author="Rory McCann",
+      author_email="rory@technomancy.org",
+      py_modules=['robot_detection'],
+      summary="Library for detecting if a HTTP User Agent header is likely to be a bot",
+      url="http://www.celtic-knot-creator.com/robot-detection/",
+)
