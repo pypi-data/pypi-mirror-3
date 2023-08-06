7c8779dd50b802003b146b55f02374c69aeec2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:27 +0000

    Add package Products.ifQuotes/1.0.0/Products.ifQuotes-1.0.0.zip

---
+++ packages/Products.ifQuotes/Products.ifQuotes-1.0.0.zip/Products.ifQuotes-1.0.0/README.txt
@@ -0,0 +1,4 @@
+Products.ifQuotes
+=================
+
+A "quote" content type for Plone, by ifPeople.net. Development sponsored by Land Trust Alliance (lta.org).
---
+++ packages/Products.ifQuotes/Products.ifQuotes-1.0.0.zip/Products.ifQuotes-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.0"
+
+
+setup(
+    description="A 'quote' content type for Plone",
+    long_description=open("README.txt").read() + '\n' +
+        open("HISTORY.txt").read(),
+    maintainer="Alex Clark",
+    maintainer_email="aclark@aclark.net",
+    name='Products.ifQuotes',
+    packages=find_packages(),
+    namespace_packages=[
+        'Products'
+    ],
+    install_requires=[
+        'setuptools',
+    ],
+    url="https://github.com/collective/Products.ifQuotes",
+    version=VERSION,
+)
