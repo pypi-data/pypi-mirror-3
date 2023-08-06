20cfaa465f1133c097fa2bee90dc2682edbcd055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:37 +0000

    Add package hanjq/1.0.0/hanjq-1.0.0.zip

---
+++ packages/hanjq/hanjq-1.0.0.zip/hanjq-1.0.0/nester.py
@@ -0,0 +1,11 @@
+'''
+Created on 2012-7-30
+
+@author: hanjinqi
+'''
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
