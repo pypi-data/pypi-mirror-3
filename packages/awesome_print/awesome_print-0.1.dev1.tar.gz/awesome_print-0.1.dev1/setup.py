ef266ca7a5ce0bc9b6ae77e8af2a0e1d6df19b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:40 +0000

    Add package awesome_print/0.1.dev1/awesome_print-0.1.dev1.tar.gz

---
+++ packages/awesome_print/awesome_print-0.1.dev1.tar.gz/awesome_print-0.1.dev1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='awesome_print',
+    version='0.1.dev1',
+    author='Stan Mazhara',
+    author_email='akmegran@gmail.com',
+    packages=['awesome_print', 'awesome_print.test'],
+    license='LICENSE',
+    description='Awesome print.',
+    long_description=open('README.txt').read(),
+    url='https://github.com/smazhara/awesome_print'
+)
