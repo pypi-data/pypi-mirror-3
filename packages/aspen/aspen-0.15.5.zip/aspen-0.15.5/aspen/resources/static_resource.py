fec1f8986168944d08dd3e693ce00bd51ecd89bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:55 +0000

    Add package aspen/0.15.5/aspen-0.15.5.zip

---
+++ packages/aspen/aspen-0.15.5.zip/aspen-0.15.5/aspen/resources/static_resource.py
@@ -0,0 +1,20 @@
+from aspen import Response
+from aspen.resources.resource import Resource
+
+
+class StaticResource(Resource):
+
+    def __init__(self, *a, **kw):
+        super(StaticResource, self).__init__(*a, **kw)
+        if self.mimetype == 'application/json':
+            self.mimetype = self.website.json_content_type
+
+    def respond(self, request, response=None):
+        """Given a Request and maybe a Response, return or raise a Response.
+        """
+        response = response or Response()
+        # XXX Perform HTTP caching here.
+        response.body = self.raw
+        response.headers['Content-Type'] = self.mimetype
+        # XXX How do we know what charset to send? Let the browser decide ...
+        return response
