5f081973ff8bdfccfa93a6a24937f343a81e53bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:59 +0000

    Add package django-crowd/0.41dev/django-crowd-0.41dev.tar.gz

---
+++ packages/django-crowd/django-crowd-0.41dev.tar.gz/django-crowd-0.41dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='django-crowd',
+    version='0.41dev',
+    packages=['crowd',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
