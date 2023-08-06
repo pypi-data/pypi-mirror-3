afaaa61ee21de41921c148d161250df7214adecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:23 +0000

    Add package extended_range/1.0.0/extended_range-1.0.0.tar.gz

---
+++ packages/extended_range/extended_range-1.0.0.tar.gz/extended_range-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+		name         = 'extended_range',
+		version      = '1.0.0',
+		py_modules   = ['extended_range'],
+		author       = 'Bolt',
+		author_email = 'bolt.khan@gmail.com',
+		description  = 'An extended version of the range() BIF that can repeat elements of the list as well',
+)
