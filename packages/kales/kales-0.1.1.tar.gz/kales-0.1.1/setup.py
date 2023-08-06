db4547b49b29f10dcc751ff48602e749e0195d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:40 +0000

    Add package kales/0.1.1/kales-0.1.1.tar.gz

---
+++ packages/kales/kales-0.1.1.tar.gz/kales-0.1.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='kales',
+    version='0.1.1',
+    author='Timothée Peignier',
+    author_email='timothee.peignier@tryphon.org',
+    description='Python client to the OpenCalais API',
+    url='http://github.com/cyberdelia/kales',
+    license=license,
+    keywords="opencalais calais",
+    py_modules=['kales'],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        "requests>=0.11"
+    ]
+)
