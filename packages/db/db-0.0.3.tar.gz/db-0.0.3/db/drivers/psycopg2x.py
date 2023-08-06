e817b3c3782c947bbb9ed7d28e43d820360c7fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:18 +0000

    Add package db/0.0.3/db-0.0.3.tar.gz

---
+++ packages/db/db-0.0.3.tar.gz/db-0.0.3/db/drivers/psycopg2x.py
@@ -0,0 +1,35 @@
+from db.drivers import Driver
+
+try:
+    import psycopg2
+    import psycopg2.extras
+    import psycopg2.extensions
+
+    def connect(*args, **kwargs):
+        kwargs["connection_factory"] = psycopg2.extras.NamedTupleConnection
+        import logging
+        logging.info("Creating connection to: %s %s",  args, kwargs)
+        conn = psycopg2.connect(*args, **kwargs)
+        return conn
+
+    class PostgresDriver(Driver):
+
+        PARAM_STYLE = "pyformat"
+
+        def connect(self):
+            return connect(*self.conn_args, **self.conn_kwargs)
+
+        def ignore_exception(self, ex):
+            return "no results to fetch" in str(ex)
+
+        def setup_cursor(self, cursor):
+            psycopg2.extensions.register_type(psycopg2.extensions.UNICODE,
+                                              cursor)
+
+    register = PostgresDriver.register
+
+except ImportError:
+    from missing import connect
+    from missing import register
+
+all = [connect.__name__, register.__name__]
