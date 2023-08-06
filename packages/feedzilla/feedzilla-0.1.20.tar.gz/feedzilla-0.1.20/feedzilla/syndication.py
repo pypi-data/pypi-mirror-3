27bdff39e33a67e94a15c1e5bd8c055b24163cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:32 +0000

    Add package feedzilla/0.1.20/feedzilla-0.1.20.tar.gz

---
+++ packages/feedzilla/feedzilla-0.1.20.tar.gz/feedzilla-0.1.20/feedzilla/syndication.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright: 2011, Grigoriy Petukhov
+# Author: Grigoriy Petukhov (http://lorien.name)
+# License: BSD
+import os
+from datetime import datetime
+
+from django.contrib.syndication.feeds import FeedDoesNotExist
+from django.contrib.syndication.feeds import Feed
+from django.utils.feedgenerator import Atom1Feed
+from django.conf import settings
+
+from feedzilla.models import Post
+
+
+class PostFeed(Feed):
+    feed_type = Atom1Feed
+    link = '/'
+    title = settings.FEEDZILLA_SITE_TITLE
+    subtitle = settings.FEEDZILLA_SITE_DESCRIPTION
+
+    #def item_link(self, obj):
+        #return obj.get_imdb_link()
+
+    def item_guid(self, obj):
+        return str(obj.guid)
+
+    def items(self, obj):
+        return Post.active_objects.all()\
+                   .order_by('-created')[:settings.FEEDZILLA_PAGE_SIZE]
+
+    def item_pubdate(self, obj):
+        return obj.created
