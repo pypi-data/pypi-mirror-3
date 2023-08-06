cb304cf1cd4798b3187815ae03b68b8eda152402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:24 +0000

    Add package print_lol_hotrazor/1.0.0/print_lol_hotrazor-1.0.0.tar.gz

---
+++ packages/print_lol_hotrazor/print_lol_hotrazor-1.0.0.tar.gz/print_lol_hotrazor-1.0.0/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+	name = 'print_lol_hotrazor',
+	version ='1.0.0',
+	py_module = ['print_lol_hotrazor'],
+	author = 'hotrazor',
+	author_email = 'hotrazor@gmail.com',
+	url = 'none',
+	description = 'this is example'
+)
+
