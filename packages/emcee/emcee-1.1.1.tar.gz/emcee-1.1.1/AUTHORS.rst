e74977033f37b21587e41949f6b1d8a7afdd1f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:40 +0000

    Add package emcee/1.1.1/emcee-1.1.1.tar.gz

---
+++ packages/emcee/emcee-1.1.1.tar.gz/emcee-1.1.1/AUTHORS.rst
@@ -0,0 +1,18 @@
+Author:
+
+- `Dan Foreman-Mackey (NYU) <https://github.com/dfm>`_
+
+Direct contributions to the code base:
+
+- `Dustin Lang (Princeton/CMU) <https://github.com/dstndstn>`_
+- `David W. Hogg (NYU) <https://github.com/davidwhogg>`_
+- `Alex Conley (U Colorado, Boulder) <https://github.com/aconley314>`_
+- `Jeremy Sanders (Cambridge) <https://github.com/jeremysanders>`_
+- `Phil Marshall (Oxford) <https://github.com/drphilmarshall>`_
+
+Comments, corrections & suggestions:
+
+- Jonathan Goodman (NYU)
+- Jo Bovy (IAS)
+- Eric Agol (UWash)
+- John Gizis (Delaware)
---
+++ packages/emcee/emcee-1.1.1.tar.gz/emcee-1.1.1/emcee/__init__.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+from .sampler import *
+from .mh import *
+from .ensemble import *
+from . import utils
+
+
+__version__ = "1.1.1"
+
+
+def test():
+    from .tests import Tests
+
+    test_names = [
+                  ("Parallel Sampler",    "test_parallel"),
+                  ("Ensemble Sampler",    "test_ensemble"),
+                  ("Metropolis-Hastings", "test_mh"),
+                 ]
+
+    print("Starting tests...")
+
+    failures = 0
+    tests = Tests()
+    for t in test_names:
+        tests.setUp()
+        try:
+            getattr(tests, t[1])()
+        except Exception as e:
+            print("Test: {0} failed with error:".format(t[0]))
+            print("\t{0}: {1}".format(e.__class__.__name__, e))
+            failures += 1
+        else:
+            print("Test: {0} passed.".format(t[0]))
+
+    print("{0} tests passed".format(len(test_names) - failures))
+    print("{0} tests failed".format(failures))
