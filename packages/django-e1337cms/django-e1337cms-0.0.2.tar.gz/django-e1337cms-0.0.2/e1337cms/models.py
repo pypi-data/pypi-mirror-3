e6291626f6b546eacd71eb9bfbdd703792c2fb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:24 +0000

    Add package django-e1337cms/0.0.2/django-e1337cms-0.0.2.tar.gz

---
+++ packages/django-e1337cms/django-e1337cms-0.0.2.tar.gz/django-e1337cms-0.0.2/e1337cms/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
+
+
+class Page(models.Model):
+    title = models.CharField(_('title'), max_length=250)
+    slug = models.SlugField(_('slug'), max_length=200)
+    content = models.TextField(_('content'))
+    published = models.BooleanField(_("is published"), blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True,
+                                related_name='children', db_index=True,
+                                verbose_name=_('parent'))
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+    changed_date = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse("page", kwargs={"slug": self.slug})
+
+    def __unicode__(self):
+        return unicode(self.title)
+
+    class Meta:
+        ordering = ['creation_date']
+        verbose_name = _('page')
+        verbose_name_plural = _('pages')
---
+++ packages/django-e1337cms/django-e1337cms-0.0.2.tar.gz/django-e1337cms-0.0.2/e1337cms/tests.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from e1337cms.models import Page
+
+
+class ViewTest(TestCase):
+
+    def setUp(self):
+        self.page = Page.objects.create(title='First page title',
+            slug='first-page-slug',
+            content='first page content')
+
+    def test_page_dont_exists(self):
+        r = self.client.get(reverse("page", kwargs={'slug': 'dummy-slug'}))
+        self.assertEquals(r.status_code, 404)
+
+    def test_simple_page_render(self):
+        r = self.client.get(reverse("page", kwargs={'slug': 'first-page-slug'}))
+        self.assertContains(r, "first page content")
+        self.assertNotContains(r, "First page title")
+        self.page.content = '<strong>{{ page.title }}</strong>\nfirst page content'
+        self.page.save()
+        r = self.client.get(reverse("page", kwargs={'slug': 'first-page-slug'}))
+        self.assertContains(r, "first page content")
+        self.assertContains(r, "<strong>First page title</strong>")
+
+    def test_complex_page_render(self):
+        self.page.content = '''
+            {% extends "base.html" %}
+    {% load e1337tags %}
+    {% block title %}e1337::{{ page.title }}{% endblock title %}
+
+    {% block body %}
+    {% rst %}
+My h1 row
+=========
+    {% endrst %}
+    another content, bla, <strong>blu</strong>, bli...
+    <p>html content</p>
+
+    {% endblock body %}
+        '''
+        self.page.save()
+        r = self.client.get(reverse("page", kwargs={'slug': 'first-page-slug'}))
+        self.assertContains(r, "<title>e1337::First page title</title>")
+        self.assertContains(r, "<h1 class=\"title\">My h1 row</h1>")
+
+    def test_rst_block(self):
+        # title
+        self.page.content = '{% load e1337tags %}{% rst %}Page title\n=================={% endrst %}'
+        self.page.save()
+        r = self.client.get(reverse("page", kwargs={'slug': 'first-page-slug'}))
+        self.assertContains(r, "<h1 class=\"title\">Page title</h1>")
+        # bold text
+        self.page.content = '{% load e1337tags %}{% rst %}**bold text**{% endrst %}'
+        self.page.save()
+        r = self.client.get(reverse("page", kwargs={'slug': 'first-page-slug'}))
+        self.assertContains(r, "<div class=\"document\">\n<p><strong>bold text</strong></p>\n</div>\n")
+        # italic text
+        self.page.content = '{% load e1337tags %}{% rst %}*italic text*{% endrst %}'
+        self.page.save()
+        r = self.client.get(reverse("page", kwargs={'slug': 'first-page-slug'}))
+        self.assertContains(r, "<div class=\"document\">\n<p><em>italic text</em></p>\n</div>\n")
+
+    def test_index_page_slug(self):
+        r = self.client.get(reverse("index_page"))
+        self.assertEquals(r.status_code, 404)
+        self.page.slug = 'home'
+        self.page.save()
+        r = self.client.get(reverse("index_page"))
+        self.assertContains(r, "first page content")
