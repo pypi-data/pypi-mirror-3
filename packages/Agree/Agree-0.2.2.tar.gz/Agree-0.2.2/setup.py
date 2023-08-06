b7cbbb1dbf43f702955c3237d139a323ed4ec81d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:50 +0000

    Add package Agree/0.2.2/Agree-0.2.2.tar.gz

---
+++ packages/Agree/Agree-0.2.2.tar.gz/Agree-0.2.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Agree',
+    version='0.2.2',
+    author='Conner Bryan',
+    author_email='conner@nodala.com',
+    packages=['agree'],
+    url='http://pypi.python.org/pypi/Agree/',
+    license='LICENSE.txt',
+    description='Simple argument resolution.',
+    long_description=open('README.txt').read(),
+
+)
