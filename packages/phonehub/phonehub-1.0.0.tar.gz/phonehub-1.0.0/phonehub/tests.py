8fcae770804a3c4b306493344ff420edd35e5079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:55 +0000

    Add package phonehub/1.0.0/phonehub-1.0.0.tar.gz

---
+++ packages/phonehub/phonehub-1.0.0.tar.gz/phonehub-1.0.0/phonehub/tests.py
@@ -0,0 +1,30 @@
+'''
+Created on 04/03/2012
+
+@author: ogonbat
+'''
+import unittest
+from phonehub.aastra.api import AastraIPPhoneTextMenu, MenuItem 
+from phonehub.aastra.items import SoftKeyItem
+class PhoneHubTestCase(unittest.TestCase):
+    def setUp(self):
+        self.textmenu = AastraIPPhoneTextMenu()
+        self.textmenu.addTitle("this is a test")
+        self.textmenu.setDestroyOnExit("yes")
+        self.textmenu.setAllowConf('yes')
+        #add menu item
+        menuItem = MenuItem()
+        menuItem.addItem("Prompt First Item","http://www.google.com")
+        menuItem.addItem("Prompt Second Item","http://www.yahoo.com")
+        self.textmenu.addMenuItems(menuItem)
+        
+        #add SoftKey
+        softkeyitem = SoftKeyItem()
+        softkeyitem.addItem("Test Softkey One", "www.aastra.com")
+        softkeyitem.addItem("Test Softkey Two", "www.aastra.com")
+        self.textmenu.addSoftKeysItems(softkeyitem)
+        
+        
+    def test_aastra_text_menu(self):
+        #print self.textmenu.getXMLRender()
+        self.assertEqual(self.textmenu.getXMLRender(),'<AastraIPPhoneTextMenu destroyOnExit="yes" allowConf="yes"><Title wrap="no">this is a test</Title><MenuItem><Prompt>Prompt First Item</Prompt><URI>http://www.google.com</URI></MenuItem><MenuItem><Prompt>Prompt Second Item</Prompt><URI>http://www.yahoo.com</URI></MenuItem><SoftKey><Label>Test Softkey One</Label><URI>www.aastra.com</URI></SoftKey><SoftKey><Label>Test Softkey Two</Label><URI>www.aastra.com</URI></SoftKey></AastraIPPhoneTextMenu>')
---
+++ packages/phonehub/phonehub-1.0.0.tar.gz/phonehub-1.0.0/setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+
+setup(
+    name = "phonehub",
+    packages = ["phonehub"],
+    version = "1.0.0",
+    description = "django-phonehub is a django app for simple and fast Aastra phones xml creation",
+    author = "Andrea Mucci",
+    author_email = "cingusoft@gmail.com",
+    url = "https://github.com/ogonbat/django-phonehub",
+    keywords = ["aastra", "sip", "xml", "django"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Framework :: Django",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    long_description = """\
+Django app for automatic generation of Aastra XML SIP Phones
+-------------------------------------
+
+Work with
+ - All Aastra SIP Phones of 67xxi serie ( 6730i, 6731i, 6735i, 6737i, 6753i, 6755i, 6757i, 6739i )
+Support
+ - All XML Root commands ( latest firmware release 3.2.2 )
+ - Offer specific HTTP Response that format the code and the HTTP Header for correct phone push
+ - Support ISO-8859-1 and UTF-8
+ 
+This version requires Python 2 or later;
+"""
+)
