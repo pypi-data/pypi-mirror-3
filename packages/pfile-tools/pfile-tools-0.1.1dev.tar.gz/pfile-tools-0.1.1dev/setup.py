358f5735ff122d42e5e1be7012e89258d81bf489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:30 +0000

    Add package pfile-tools/0.1.1dev/pfile-tools-0.1.1dev.tar.gz

---
+++ packages/pfile-tools/pfile-tools-0.1.1dev.tar.gz/pfile-tools-0.1.1dev/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from distribute_setup import use_setuptools
+use_setuptools()
+import setuptools
+from setuptools import setup
+import os
+
+import pfile_tools
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='pfile-tools',
+    version=pfile_tools.VERSION,
+    packages=setuptools.find_packages(),
+    license='BSD License',
+    long_description=read('README.textile'),
+    entry_points = {
+        'console_scripts': [
+            'dump_pfile_header = pfile_tools.scripts.dump_pfile_header:main',
+            'anonymize_pfile = pfile_tools.scripts.anonymize_pfile:main'
+        ]
+    }
+)
