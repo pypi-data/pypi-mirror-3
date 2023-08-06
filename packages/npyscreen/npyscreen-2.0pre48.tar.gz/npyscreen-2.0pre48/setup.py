b7429e53999e3c02a265c934dc97043aed20b8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:44 +0000

    Add package npyscreen/2.0pre48/npyscreen-2.0pre48.tar.gz

---
+++ packages/npyscreen/npyscreen-2.0pre48.tar.gz/npyscreen-2.0pre48/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+	name="npyscreen",
+	version="2.0pre48",
+	description="Writing user interfaces without all that ugly mucking about in hyperspace",
+	author="Nicholas Cole",
+	author_email="n@npcole.com",
+	url="http://www.npcole.com/npyscreen/",
+	packages=['npyscreen'],
+	license='New BSD License',
+	classifiers= [
+	    'Environment :: Console',
+	    'Operating System :: POSIX',
+	    'Environment :: Console :: Curses',
+	    'Intended Audience :: Developers',
+	    'License :: OSI Approved :: BSD License',
+	    
+	    ]   
+)
