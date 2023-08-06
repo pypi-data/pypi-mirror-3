44e941c49640f3424fe1bb398e39fcb9cb068357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:23 +0000

    Add package Killbill/0.1dev/Killbill-0.1dev.tar.gz

---
+++ packages/Killbill/Killbill-0.1dev.tar.gz/Killbill-0.1dev/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'Killbill',
+    version = '0.1dev',
+    packages = ['killbill',],
+    author = 'Killbilling team',
+    author_email = 'killbilling-dev@googlegroups.com',
+    url = 'https://github.com/killbilling/killbill-client-python',
+    license = 'Apache License 2.0',
+    long_description = open('README.txt').read(),
+)
