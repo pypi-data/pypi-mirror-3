c4c79605b46aae61860db22effa5f896f40e3039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:50 +0000

    Add package locales-test/1.4.0/locales-test-1.4.0.tar.gz

---
+++ packages/locales-test/locales-test-1.4.0.tar.gz/locales-test-1.4.0/setup.py
@@ -0,0 +1,17 @@
+# wt-classes, utility classes used by Wt applications
+# Copyright (C) 2011 Boris Nagaev
+#
+# See the LICENSE file for terms of use.
+
+from distutils.core import setup
+
+setup(name='locales-test',
+    version='1.4.0',
+    description='Test of xml-based localizations used by Wt apps',
+    author='Boris Nagaev',
+    author_email='bnagaev@gmail.com',
+    url='https://bitbucket.org/starius/wt-classes/',
+    license='GNU GPL v2',
+    scripts=['locales-test'],
+)
+
