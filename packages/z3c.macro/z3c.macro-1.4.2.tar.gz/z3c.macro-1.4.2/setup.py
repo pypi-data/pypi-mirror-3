05c0e388eb982f46eaea43832ae0033e3bf51343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:28 +0000

    Add package z3c.macro/1.4.2/z3c.macro-1.4.2.tar.gz

---
+++ packages/z3c.macro/z3c.macro-1.4.2.tar.gz/z3c.macro-1.4.2/setup.py
@@ -0,0 +1,84 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+
+$Id: setup.py 124407 2012-02-15 18:16:13Z menesis $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name='z3c.macro',
+    version='1.4.2',
+    author = "Roger Ineichen and the Zope Community",
+    author_email = "zope-dev@zope.org",
+    description = "Simpler definition of ZPT macros.",
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        'Detailed Documentation\n'
+        '======================\n'
+        + '\n\n' +
+        read('src', 'z3c', 'macro', 'README.txt')
+        + '\n\n' +
+        read('src', 'z3c', 'macro', 'zcml.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license = "ZPL 2.1",
+    keywords = "zope3 macro pagetemplate zpt",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://pypi.python.org/pypi/z3c.macro',
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c'],
+    extras_require = dict(
+        test = [
+            'z3c.pt >= 2.1',
+            'z3c.ptcompat>=1.0',
+            'z3c.template',
+            'zope.app.testing',
+            'zope.browserpage>=3.12',
+            'zope.testing',
+            ],
+        chameleon = [
+            'z3c.pt >= 2.1',
+            'z3c.ptcompat>=1.0',
+            ],
+        ),
+    install_requires = [
+        'setuptools',
+        'zope.component',
+        'zope.configuration',
+        'zope.interface',
+        'zope.pagetemplate >= 3.6.2',
+        'zope.publisher',
+        'zope.schema',
+        'zope.tales',
+        ],
+    include_package_data = True,
+    zip_safe = False,
+    )
---
+++ packages/z3c.macro/z3c.macro-1.4.2.tar.gz/z3c.macro-1.4.2/src/z3c/macro/tests.py
@@ -0,0 +1,67 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Viewlet tests
+
+$Id: tests.py 124402 2012-02-15 18:00:33Z menesis $
+"""
+__docformat__ = 'restructuredtext'
+
+from zope.app.testing import setup
+from zope.configuration import xmlconfig
+import doctest
+import itertools
+import unittest
+
+import z3c.macro.tales
+import z3c.macro.zcml
+
+
+def setUp(test):
+    root = setup.placefulSetUp(site=True)
+    test.globs['root'] = root
+
+
+def setUpZPT(test):
+    setUp(test)
+    from zope.browserpage import metaconfigure
+    metaconfigure.registerType('macro', z3c.macro.tales.MacroExpression)
+
+
+def setUpZ3CPT(suite):
+    setUp(suite)
+    import z3c.pt
+    import z3c.ptcompat
+    xmlconfig.XMLConfig('configure.zcml', z3c.pt)()
+    xmlconfig.XMLConfig('configure.zcml', z3c.ptcompat)()
+
+
+def tearDown(test):
+    setup.placefulTearDown()
+
+
+def test_suite():
+    tests = ((
+        doctest.DocFileSuite('README.txt',
+            setUp=setUp, tearDown=tearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            ),
+        doctest.DocFileSuite('zcml.txt', setUp=setUp, tearDown=tearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,),
+        ) for setUp in (setUpZ3CPT, setUpZPT))
+
+    return unittest.TestSuite(itertools.chain(*tests))
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
