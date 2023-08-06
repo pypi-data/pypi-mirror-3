dbde33e41c8d893f1b0cca4ebef8228e39954214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:29 +0000

    Add package cilantro/0.9.1b15/cilantro-0.9.1b15.tar.gz

---
+++ packages/cilantro/cilantro-0.9.1b15.tar.gz/cilantro-0.9.1b15/cilantro/static/cilantro/scripts/javascript/src/types/report/main.js
@@ -0,0 +1,8 @@
+
+define(['cilantro/types/report/models', 'cilantro/types/report/views', 'cilantro/types/report/messages'], function(Models, Views, Messages) {
+  return {
+    Models: Models,
+    Views: Views,
+    Messages: Messages
+  };
+});
