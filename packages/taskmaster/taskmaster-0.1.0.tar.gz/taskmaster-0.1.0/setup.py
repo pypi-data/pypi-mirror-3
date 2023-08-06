650e498ae827976aefe6030e9f4c2e4c1f2fc775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:36 +0000

    Add package taskmaster/0.1.0/taskmaster-0.1.0.tar.gz

---
+++ packages/taskmaster/taskmaster-0.1.0.tar.gz/taskmaster-0.1.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="taskmaster",
+    license='Apache License 2.0',
+    version="0.1.0",
+    description="",
+    author="David Cramer",
+    author_email="dcramer@gmail.com",
+    url="https://github.com/dcramer/taskmaster",
+    packages=find_packages("src"),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'tm-master = taskmaster.cli.master:main',
+            'tm-slave = taskmaster.cli.slave:main',
+        ],
+    },
+    install_requires=[
+        'progressbar',
+    ],
+    tests_require=[
+        'unittest2',
+        'Nose>=1.0',
+    ],
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ])
