29d9d96250e6f323f80b8c24680a61fdfebac11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:37 +0000

    Add package errorcats/1.0.1/errorcats-1.0.1.tar.gz

---
+++ packages/errorcats/errorcats-1.0.1.tar.gz/errorcats-1.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='errorcats',
+    version='1.0.1',
+    description='An amusing replacement error handler',
+    author='Nathaniel Case',
+    author_email='Qalthos@gmail.com',
+    url='https://github.com/Qalthos/errorcats',
+    packages=find_packages(),
+)
