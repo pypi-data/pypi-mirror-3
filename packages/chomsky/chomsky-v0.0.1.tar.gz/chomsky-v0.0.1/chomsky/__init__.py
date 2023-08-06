6519d038bc2d0fb925efd63c55628e891fa6deb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:05 +0000

    Add package chomsky/v0.0.1/chomsky-v0.0.1.tar.gz

---
+++ packages/chomsky/chomsky-v0.0.1.tar.gz/chomsky-v0.0.1/chomsky/__init__.py
@@ -0,0 +1,20 @@
+"""
+Welcome.  Welcome to chomsky.
+"""
+from .buffer import *
+from .exceptions import *
+from .matchers import *
+from .grammar import *
+from .result import *
+
+
+# aliases
+A = Letter
+L = Literal
+W = Word
+S = Whitespace
+R = Regex
+
+
+# hide some Matchers
+del AutoSequence
