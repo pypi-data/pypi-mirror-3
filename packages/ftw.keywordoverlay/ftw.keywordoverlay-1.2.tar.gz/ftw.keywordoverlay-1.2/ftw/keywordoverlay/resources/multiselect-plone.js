7de7a755df5f170e98544e39a807c6c38d0b3c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:36 +0000

    Add package ftw.keywordoverlay/1.2/ftw.keywordoverlay-1.2.tar.gz

---
+++ packages/ftw.keywordoverlay/ftw.keywordoverlay-1.2.tar.gz/ftw.keywordoverlay-1.2/ftw/keywordoverlay/resources/multiselect-plone.js
@@ -0,0 +1,69 @@
+//plone integration of ui.multiselectwidget
+jq(function(){
+    //get all keywordwidgets
+    var $targets = jq('.field.ArchetypesKeywordWidget select');
+    //bind click event on every fieldset that contains an keywordwidget
+    $targets.each(function(i,o){
+        $target = jq(o);
+        //get id and cut off <fieldset->
+        var loadonID = $target.closest('fieldset').attr('id').substr(9);
+        $loadon = jq('ul.formTabs li.formTab [href$='+loadonID+']');
+        //load widget while clicking on formTab
+        $loadon.bind('click', function(e){
+            $target.css('width','90%');
+
+            //load widget
+            $target.multiselect({sortable: false});
+        });
+
+        //hide plones default add textarea
+        var $addarea = $target.parent().prev();
+        $addarea.hide();
+        //hide plone 4 add area
+        $targets.parents('div.field').find('#newTags').hide();
+
+        //area for new items
+        var content  = '<div><ul class="new_item_space"></ul>';
+        content += '<form><input class="item" name="new_item" type="text" /><input class="allowMultiSubmit" type="submit" value="+"></form></div>';
+        var $selected = $target.closest('div');
+        $selected.after(content);
+        //add eventhandler to add and delete items
+        $field = $selected.parent();
+        $field.find('.allowMultiSubmit').bind('click', function(e){
+            e.preventDefault();
+            var item = jq(this).prev();
+            var itemval = item.val()
+            item.val('');
+            if (itemval){
+                var delbutton = '<a class="delbutton" href="#"><img src="'+portal_url+'/delete_icon.gif" /></a>';
+                var toadd = '<li class="ui-state-default ui-element"><span>'+itemval+'</span> '+delbutton+'</li>'
+                //add to the visible part
+                jq(this).parent().prev().append(toadd);
+                //add item to the hidden area (for plone)
+                $addarea.contents('textarea').append(itemval+'\n');   
+            }
+        });
+
+        //delete function
+        jq('.delbutton').live('click',function(e){
+            e.preventDefault();
+            var $this = jq(this)
+            var $parent = $this.parent();
+            var itemval = $this.prev().html();
+            $addarea = jq($this.closest('.ArchetypesKeywordWidget').find('[id$=_keywords]').eq(0));
+            var value = $addarea.html();
+            value = value.replace(itemval+'\n','');
+            $addarea.html(value);
+            $parent.remove();
+        });
+
+
+
+        //load if allready visible
+        if ($target.is(':visible')){
+            $loadon.trigger('click');
+        }
+        
+    });
+
+});
---
+++ packages/ftw.keywordoverlay/ftw.keywordoverlay-1.2.tar.gz/ftw.keywordoverlay-1.2/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.2'
+maintainer = 'Mathias Leimgruber'
+
+setup(name='ftw.keywordoverlay',
+      version=version,
+      description="Puts a nice overlay over Archetypes' default " + \
+          "keywordwidget for plone.",
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+
+      keywords='plone widget archetypes keyword jquery ftw keywordoverlay',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.keywordoverlay',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        'collective.js.jqueryui',
+        'collective.js.ui.multiselect',
+        ],
+
+      entry_points='''
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      ''',
+      )
