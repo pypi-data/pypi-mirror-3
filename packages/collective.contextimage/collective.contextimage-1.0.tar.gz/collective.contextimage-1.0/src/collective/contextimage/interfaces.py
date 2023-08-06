287f6460c7733ac63d40fd890d8ab00dbc0dd6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:54 +0000

    Add package collective.contextimage/1.0/collective.contextimage-1.0.tar.gz

---
+++ packages/collective.contextimage/collective.contextimage-1.0.tar.gz/collective.contextimage-1.0/src/collective/contextimage/interfaces.py
@@ -0,0 +1,18 @@
+from zope.interface import Interface
+
+
+class IContextImageExtensionLayer(Interface):
+    """Browser layer for collective.contextimage
+    """
+
+
+class IPageImageExtensionLayer(IContextImageExtensionLayer): pass
+
+
+class IHeaderImageExtensionLayer(IContextImageExtensionLayer): pass
+
+
+class IViewletImageExtensionLayer(IContextImageExtensionLayer): pass
+
+
+class ILogoImageExtensionLayer(IContextImageExtensionLayer): pass
