b50dcc5731b96c48cc00ecfac3050863643120ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:30 +0000

    Add package Intellect/1.4.7/Intellect-1.4.7.tar.gz

---
+++ packages/Intellect/Intellect-1.4.7.tar.gz/Intellect-1.4.7/setup.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+'''
+Copyright (c) 2011, Michael Joseph Walsh
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. All advertising materials mentioning features or use of this software
+   must display the following acknowledgement:
+   This product includes software developed by the author.
+4. Neither the name of the author nor the
+   names of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+
+import os
+from setuptools import setup, find_packages
+
+DESCRIPTION = 'A Domain-specific language and Rules Engine for Python'
+
+if os.path.exists("README.rst"):
+  long_description = open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')).read()
+else:
+  long_description = "See http://pypi.python.org/pypi/Intellect"
+
+def get_version(version_tuple):
+    version = '%s.%s' % (version_tuple[0], version_tuple[1])
+    if version_tuple[2]:
+        version = '%s.%s' % (version, version_tuple[2])
+    return version
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+init = os.path.join(os.path.dirname(__file__), 'intellect', '__init__.py')
+version_line = filter(lambda l: l.startswith('VERSION'), open(init))[0]
+VERSION = get_version(eval(version_line.split('=')[-1]))
+print VERSION
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    "Topic :: Software Development :: Libraries :: Python Modules"
+]
+
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+
+intellect_dir = 'intellect'
+
+for dirpath, dirnames, filenames in os.walk(intellect_dir):
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+setup(name="Intellect",
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=long_description,
+      author='Michael Joseph Walsh',
+      author_email='github.com{nospam}nemonik.com',
+      url='http://github.com/nemonik/Intellect',
+      classifiers=CLASSIFIERS,
+      keywords='intellect rules engine dsl policy',
+      license='BSD, 4-clause license',
+      packages = packages,
+      package_data = {'': ['*.g', '*.tokens']},
+      include_package_data=True,
+      install_requires=['antlr_python_runtime>=3.1.3'])
