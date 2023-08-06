c2287a443b8e44fd473cb7475a2791861abcb775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:44 +0000

    Add package gisdata/0.3.8/gisdata-0.3.8.tar.gz

---
+++ packages/gisdata/gisdata-0.3.8.tar.gz/gisdata-0.3.8/setup.py
@@ -0,0 +1,26 @@
+import os
+from distutils.core import setup
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="gisdata",
+    version="0.3.8",
+    author="ingenieroariel",
+    author_email="ingenieroariel@gmail.com",
+    description="Sample data for GIS packages",
+    long_description=(read('README')),
+    # Full list of classifiers can be found at:
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License'
+    ],
+    license="BSD",
+    keywords="gis",
+    url='https://github.com/ingenieroariel/gisdata',
+    packages=['gisdata',],
+    package_data={'gisdata': ['data/good/vector/*', 'data/good/raster/*', 'data/bad/*']},
+    zip_safe=False,
+)
