9f88814805a58f6c6bd5d80e7a097e013a71dc28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:25 +0000

    Add package pytest-pep8/0.9.1/pytest-pep8-0.9.1.zip

---
+++ packages/pytest-pep8/pytest-pep8-0.9.1.zip/pytest-pep8-0.9.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name='pytest-pep8',
+    description='pytest plugin to check source code against PEP8 requirements',
+    long_description=open("README.txt").read(),
+    version='0.9.1',
+    author='Holger Krekel and Ronny Pfannschmidt',
+    author_email='holger.krekel@gmail.com',
+    url='http://bitbucket.org/hpk42/pytest-pep8/',
+    py_modules=['pytest_pep8'],
+    entry_points={'pytest11': ['pep8 = pytest_pep8']},
+    install_requires=['pytest>=2.0', 'pep8>=1.3', ],
+)
