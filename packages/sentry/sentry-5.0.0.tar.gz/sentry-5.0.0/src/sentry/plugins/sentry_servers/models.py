0168114a3265c0c2dec58e2d91577ec22eeb1bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:29 +0000

    Add package sentry/5.0.0/sentry-5.0.0.tar.gz

---
+++ packages/sentry/sentry-5.0.0.tar.gz/sentry-5.0.0/src/sentry/plugins/sentry_servers/models.py
@@ -0,0 +1,36 @@
+"""
+sentry.plugins.sentry_servers.models
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2012 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+import sentry
+
+from django.utils.translation import ugettext_lazy as _
+
+from sentry.plugins import register
+from sentry.plugins.bases.tag import TagPlugin
+
+
+class ServersPlugin(TagPlugin):
+    """
+    Adds additional support for showing information about servers including:
+
+    * A panel which shows all servers a message was seen on.
+    * A sidebar module which shows the servers most actively seen on.
+    """
+    slug = 'servers'
+    title = _('Servers')
+    version = sentry.VERSION
+    author = "Sentry Team"
+    author_url = "https://github.com/getsentry/sentry"
+    tag = 'server_name'
+    tag_label = _('Server Name')
+
+    def get_tag_values(self, event):
+        if not event.server_name:
+            return []
+        return [event.server_name]
+
+register(ServersPlugin)
