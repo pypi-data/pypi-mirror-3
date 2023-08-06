b909a268076cb196547a401fdfbe23e1ffef4f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:19 +0000

    Add package django-facebook/3.6.2/django-facebook-3.6.2.zip

---
+++ packages/django-facebook/django-facebook-3.6.2.zip/django-facebook-3.6.2/django_facebook/model_managers.py
@@ -0,0 +1,52 @@
+from django.db.models.query_utils import Q
+from django.core.cache import cache
+from django.db import models
+import operator
+import random
+
+
+
+
+class FacebookUserManager(models.Manager):
+    def find_users(self, queries, base_queryset=None):
+        '''
+        Queries, a list of search queries
+        Base Queryset, the base queryset in which we are searching
+        '''
+        if base_queryset is None:
+            base_queryset = self.all()
+        filters = []
+        for query in queries:
+            match = Q(name__istartswith=query) | Q(name__icontains=' %s' % query)
+            filters.append(match)
+                
+        users = base_queryset.filter(reduce(operator.and_, filters))
+
+        return users
+    
+    def random_facebook_friends(self, user, gender=None, limit=3):
+        '''
+        Returns a random sample of your FB friends
+        
+        Limit = Number of friends
+        Gender = None, M or F 
+        '''
+        assert gender in (None, 'M', 'F'), 'Gender %s wasnt recognized' % gender
+        
+        from django_facebook.utils import get_profile_class
+        facebook_cache_key = 'fusers_%s' % user.id
+        new_facebook_users = cache.get(facebook_cache_key)
+        profile_class = get_profile_class()
+        if not new_facebook_users:
+            facebook_users = list(self.filter(user_id=user.id, gender=gender)[:50])
+            facebook_ids = [u.facebook_id for u in facebook_users]
+            new_facebook_users = list(profile_class.objects.filter(facebook_id__in=facebook_ids).select_related('user'))
+            cache.set(facebook_cache_key, new_facebook_users, 60*60)
+            
+        random_limit = min(len(new_facebook_users), 3)
+        random_facebook_users = []
+        if random_limit:
+            random_facebook_users = random.sample(new_facebook_users, limit)
+            
+        return random_facebook_users
+        
