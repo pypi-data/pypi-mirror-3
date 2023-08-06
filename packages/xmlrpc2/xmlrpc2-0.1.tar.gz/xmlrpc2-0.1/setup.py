a4c2ba64b557902159a068cd61c226fc90c54a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:38 +0000

    Add package xmlrpc2/0.1/xmlrpc2-0.1.tar.gz

---
+++ packages/xmlrpc2/xmlrpc2-0.1.tar.gz/xmlrpc2-0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+install_requires = [
+    "lxml",
+    "requests",
+]
+
+setup(
+    name="xmlrpc2",
+    version="0.1",
+
+    description="",
+    long_description=open("README.rst").read(),
+    url="https://github.com/dstufft/xmlrpc2",
+    license=open("LICENSE").read(),
+
+    author="Donald Stufft",
+    author_email="donald.stufft@gmail.com",
+
+    install_requires=install_requires,
+
+    extras_require={
+        "tests": ["pytest"],
+    },
+
+    packages=find_packages(exclude=["tests"]),
+    zip_safe=False,
+)
