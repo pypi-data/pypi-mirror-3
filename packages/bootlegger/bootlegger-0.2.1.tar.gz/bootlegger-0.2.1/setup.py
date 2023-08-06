aa5739a7b60df1fb75e2ca9a61006930c2016409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:05 +0000

    Add package bootlegger/0.2.1/bootlegger-0.2.1.tar.gz

---
+++ packages/bootlegger/bootlegger-0.2.1.tar.gz/bootlegger-0.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='bootlegger',
+    version='0.2.1',
+    description='Command Line Interface to Speakeasy',
+    author='Zhehao Mao',
+    author_email='zhehao.mao@gmail.com',
+    packages=['bootlegger'],
+    entry_points = {
+        "console_scripts" : [
+            "bootlegger = bootlegger.cli:main",
+            "bl = bootlegger.cli:main"
+        ]
+    },
+    install_requires=['pycrypto', 'requests']
+)
+
