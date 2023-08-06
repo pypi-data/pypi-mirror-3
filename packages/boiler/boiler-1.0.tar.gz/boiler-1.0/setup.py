fb9eda9cd47526e3489b051e4f1b7e50e0d7ec28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:24 +0000

    Add package boiler/1.0/boiler-1.0.tar.gz

---
+++ packages/boiler/boiler-1.0.tar.gz/boiler-1.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(name='boiler',
+      version='1.0',
+      description='A command line tool to quick manage boilerplate structures called **plates**, it is extensible',
+      long_description=long_description,
+      author='Niko Usai / mogui',
+      author_email='usai.niko@gmail.com',
+      url='http://mogui.it',
+      scripts=['./boiler']
+     )
