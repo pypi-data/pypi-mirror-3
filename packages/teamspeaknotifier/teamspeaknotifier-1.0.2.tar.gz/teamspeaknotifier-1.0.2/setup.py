603db43f81b93aeef93a1b5dbb26bcec96a2cf99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:34 +0000

    Add package teamspeaknotifier/1.0.2/teamspeaknotifier-1.0.2.tar.gz

---
+++ packages/teamspeaknotifier/teamspeaknotifier-1.0.2.tar.gz/teamspeaknotifier-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+from teamspeaknotifier import get_version
+
+setup(
+    name='teamspeaknotifier',
+    version=get_version(),
+    url='https://bitbucket.org/latestrevision/teamspeak-notification/',
+    description='Show unobtrusive notifications when certain things happen in teamspeak.',
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Utilities',
+    ],
+    packages=['teamspeaknotifier',],
+    entry_points={'console_scripts': [
+        'teamspeak-notifier = teamspeaknotifier:run_from_cmdline']},
+    install_requires = [
+            'PyGTK',
+            'teamspeak3>=1.3',
+        ]
+)
