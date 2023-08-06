75cb9a538b3bbe0e24ea02d69b4652cd85207aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:37 +0000

    Add package jmbo-banner/0.0.6/jmbo-banner-0.0.6.tar.gz

---
+++ packages/jmbo-banner/jmbo-banner-0.0.6.tar.gz/jmbo-banner-0.0.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmbo-banner',
+    version='0.0.6',
+    description='Jmbo banner app.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/jmbo-banner',
+    packages = find_packages(),
+    install_requires = [
+        'jmbo',
+    ],
+    tests_require=[
+        'django-setuptest>=0.0.6',
+    ],
+    test_suite="setuptest.SetupTestSuite",
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
