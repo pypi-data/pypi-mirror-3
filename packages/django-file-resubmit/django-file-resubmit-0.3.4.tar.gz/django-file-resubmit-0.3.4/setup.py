1eba269e855b9c66735a244a8f7573187d1100be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:55 +0000

    Add package django-file-resubmit/0.3.4/django-file-resubmit-0.3.4.tar.gz

---
+++ packages/django-file-resubmit/django-file-resubmit-0.3.4.tar.gz/django-file-resubmit-0.3.4/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2011 Ilya Shalyapin
+#
+#  django-file-resubmit is free software under terms of the MIT License.
+#
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name     = 'django-file-resubmit',
+    version  = '0.3.4',
+    packages = find_packages(),
+
+    requires = ['python (>= 2.5)', 'django (>= 1.3)', 'sorl.thumbnail (>=11.0)'],
+
+    description  = 'Keeps submited files when validation errors occure.',
+    long_description = open('README.markdown').read(),
+    author       = 'Ilya Shalyapin',
+    author_email = 'ishalyapin@gmail.com',
+    url          = 'https://github.com/un1t/django-file-resubmit',
+    download_url = 'https://github.com/un1t/django-file-resubmit/tarball/master',
+    license      = 'MIT License',
+    keywords     = 'django form filefield resubmit',
+    classifiers  = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+    ],
+)
