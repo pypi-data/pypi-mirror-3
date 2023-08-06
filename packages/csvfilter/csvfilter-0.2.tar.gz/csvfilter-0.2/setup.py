85c577fbbdfa941c1eb6a11eb1feaf0fe4405a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:54 +0000

    Add package csvfilter/0.2/csvfilter-0.2.tar.gz

---
+++ packages/csvfilter/csvfilter-0.2.tar.gz/csvfilter-0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from csvfilter import VERSION
+
+setup(name='csvfilter',
+      version=VERSION,
+      url='https://github.com/codeinthehole/csvfilter',
+      author="David Winterbottom",
+      author_email="david.winterbottom@tangentlabs.co.uk",
+      description="A command-line tool and Python API for manipulating CSV data, eg plucking columns and reordering them",
+      long_description=open('README.rst').read(),
+      keywords="CSV",
+      platforms=['linux'],
+      packages=find_packages(),
+      scripts = ['bin/csvfilter'])
