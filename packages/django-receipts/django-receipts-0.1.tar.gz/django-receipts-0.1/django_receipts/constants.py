2020be882975b997f57348e74bea622e0624ad1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:47 +0000

    Add package django-receipts/0.1/django-receipts-0.1.tar.gz

---
+++ packages/django-receipts/django-receipts-0.1.tar.gz/django-receipts-0.1/django_receipts/constants.py
@@ -0,0 +1,18 @@
+UNCHECKED = 0
+OK = 1
+REFUNDED = 2
+EXPIRED = 3
+PENDING = 4
+ERROR = 5
+
+VALID_LOOKUP = {
+    'unchecked': UNCHECKED,
+    'ok': OK,
+    'refunded':  REFUNDED,
+    'pending': PENDING,
+    'expired': EXPIRED,
+    'error': ERROR
+}
+
+VALID_INVERTED = dict([(v, k) for k, v in VALID_LOOKUP.iteritems()])
+VALID = [(k, v) for k, v in VALID_LOOKUP.iteritems()]
