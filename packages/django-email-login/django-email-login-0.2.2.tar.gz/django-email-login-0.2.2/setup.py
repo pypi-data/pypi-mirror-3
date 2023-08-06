33d63e4e89b8e4d17a339e91bf1aab33188f8790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:10 +0000

    Add package django-email-login/0.2.2/django-email-login-0.2.2.tar.gz

---
+++ packages/django-email-login/django-email-login-0.2.2.tar.gz/django-email-login-0.2.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+import email_login
+
+setup(
+    name='django-email-login',
+    version=email_login.__version__,
+    description='A Django application that enables you to have your users use their email address instead of their username',
+    long_description=open('README.txt').read(),
+    licences='MIT License, see LICENSE.txt',
+    author='Tino de Bruijn',
+    author_email='tinodb@gmail.com',
+    url='http://bitbucket.org/tino/django-email-login',
+    packages=['email_login'],
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
