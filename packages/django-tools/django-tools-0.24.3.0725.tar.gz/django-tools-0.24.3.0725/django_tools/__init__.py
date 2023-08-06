ab26b0d683998a2bbd82d859396653910493e72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:50 +0000

    Add package django-tools/0.24.3.0725/django-tools-0.24.3.0725.tar.gz

---
+++ packages/django-tools/django-tools-0.24.3.0725.tar.gz/django-tools-0.24.3.0725/django_tools/__init__.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+"""
+    we add the committer date to the version number by a .gitattributes filter
+    
+    more info about this here:
+    https://github.com/jedie/python-code-snippets/tree/master/CodeSnippets/git/#readme
+"""
+
+
+import re
+
+
+__version__ = (0, 24, 3)
+
+
+COMMITTER_DATE = "$date:0725$" # set by .gitattributes filter
+DATE_REGEX = re.compile(r"\$date:(.*?)\$")
+
+__version__ += (DATE_REGEX.findall(COMMITTER_DATE)[0],)
+VERSION_STRING = '.'.join(str(part) for part in __version__)
+
+
+if __name__ == "__main__":
+    print "django-tools v%s" % VERSION_STRING
