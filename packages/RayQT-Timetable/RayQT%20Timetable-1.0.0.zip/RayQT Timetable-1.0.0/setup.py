3f4abcfcae0e54281cfbb48d26a08150359c84f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:33 +0000

    Add package RayQT-Timetable/1.0.0/RayQT%20Timetable-1.0.0.zip

---
+++ packages/RayQT-Timetable/RayQT%20Timetable-1.0.0.zip/RayQT Timetable-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name            = 'RayQT Timetable',
+    version         = '1.0.0',
+    py_modules      = ['RayQT Timetable'],
+    author          = 'Raymond',
+    author_email    = 'raymond.pacpaco@gmail.com',
+    description     = 'A simple printer of my timetable',
+)
