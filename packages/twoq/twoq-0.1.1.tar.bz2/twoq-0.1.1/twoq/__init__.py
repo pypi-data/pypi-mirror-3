5e41a3974df4d69750fe5fb4dfb38f29e1b2a250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:53 +0000

    Add package twoq/0.1.1/twoq-0.1.1.tar.bz2

---
+++ packages/twoq/twoq-0.1.1.tar.bz2/twoq-0.1.1/twoq/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+'''twoq iterator utilities'''
+
+from twoq.compat import port
+from twoq.active import twoq, iterexcept
+
+__all__ = ('twoq', 'port', 'iterexcept')
+
+__version__ = (0, 1, 1)
