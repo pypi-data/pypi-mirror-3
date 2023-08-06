727b383a530ea6d3ca07c2d2db11a9b4bd418025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:00 +0000

    Add package Metaphone/0.4/Metaphone-0.4.tar.gz

---
+++ packages/Metaphone/Metaphone-0.4.tar.gz/Metaphone-0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+from metaphone import meta
+
+
+setup(
+    name=meta.display_name,
+    version=meta.version,
+    description=meta.description,
+    author=meta.author,
+    author_email=meta.author_email,
+    url=meta.url,
+    license=meta.license,
+    packages=find_packages(),
+    long_description=open("README.rst").read(),
+    )
