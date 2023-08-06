99a961b3c65902c777fce4ae4d139827fccae3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:24 +0000

    Add package Rot13/1.0/Rot13-1.0.zip

---
+++ packages/Rot13/Rot13-1.0.zip/Rot13-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name='Rot13',
+       version='1.0',
+       py_modules=['Rot13'],
+       author='puneet',
+       author_email='psharma.spuneet@gmail.com',
+       url='http://www.mydomain@gmail.com',
+       description='converts string to rot13',
+       )
