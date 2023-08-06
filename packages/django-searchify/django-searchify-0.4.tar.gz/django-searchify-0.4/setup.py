9c1bbd79b1fa26f14e8afa893b40c2d93929d176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:42 +0000

    Add package django-searchify/0.4/django-searchify-0.4.tar.gz

---
+++ packages/django-searchify/django-searchify-0.4.tar.gz/django-searchify-0.4/setup.py
@@ -0,0 +1,29 @@
+# Use setuptools if we can
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+PACKAGE = 'django-searchify'
+VERSION = '0.4'
+
+setup(
+    name=PACKAGE, version=VERSION,
+    description="Search integration for Django with a focus on indexing",
+    packages=[ 'searchify', 'searchify.clients',
+        'searchify.management.commands',
+        'searchify.management'],
+    license='MIT',
+    author='James Aylett',
+    author_email='james@tartarus.org',
+    install_requires=[
+        'Django>=1.3',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+    ],
+)
