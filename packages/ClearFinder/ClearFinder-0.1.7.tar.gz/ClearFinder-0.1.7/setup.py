482f545998caf9112e4e9679ddebaa2742edc73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:01 +0000

    Add package ClearFinder/0.1.7/ClearFinder-0.1.7.tar.gz

---
+++ packages/ClearFinder/ClearFinder-0.1.7.tar.gz/ClearFinder-0.1.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ClearFinder',
+      version='0.1.7',
+      author='Philip Schleihauf',
+      author_email='uniphil@gmail.com',
+      license='GPLv3',
+      description='Identify clear measurements from solar irradiation data',
+      long_description=open('README.txt').read(),
+      py_modules=['clearfinder'],
+      scripts=['clearfinder.py'])
