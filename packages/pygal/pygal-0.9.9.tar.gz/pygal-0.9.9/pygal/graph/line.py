06e11148ab4bbc94972667b22e20a3b26b4f59be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:13 +0000

    Add package pygal/0.9.9/pygal-0.9.9.tar.gz

---
+++ packages/pygal/pygal-0.9.9.tar.gz/pygal-0.9.9/pygal/graph/line.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+# This file is part of pygal
+#
+# A python svg graph plotting library
+# Copyright © 2012 Kozea
+#
+# This library is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with pygal. If not, see <http://www.gnu.org/licenses/>.
+from pygal.graph.graph import Graph
+from pygal.util import cached_property
+from pygal.interpolate import interpolation
+
+
+class Line(Graph):
+    """Line graph"""
+
+    def _get_value(self, values, i):
+        return str(values[i][1])
+
+    @cached_property
+    def _values(self):
+        if self.interpolate:
+            return [val[1] for serie in self.series
+                    for val in serie.interpolated]
+        else:
+            return  [val[1] for serie in self.series
+                    for val in serie.points]
+
+    def _fill(self, values):
+        zero = self.view.y(min(max(self.zero, self._box.ymin), self._box.ymax))
+        return ([(values[0][0], zero)] +
+                values +
+                [(values[-1][0], zero)])
+
+    def line(self, serie_node, serie):
+        view_values = map(self.view, serie.points)
+
+        if self.show_dots:
+            dots = self.svg.node(serie_node['overlay'], class_="dots")
+            for i, (x, y) in enumerate(view_values):
+                dot = self.svg.node(dots, class_='dot')
+                tag = '%d_%d' % (serie.index, i)
+                self.svg.node(dot, 'circle', cx=x, cy=y, r=2.5,
+                              id="active-%s" % tag,
+                              class_='reactive')
+                self.svg.node(dot, 'text', x=x, y=y,
+                              id="reactive-%s" % tag,
+                              class_='reactive-text'
+                ).text = self._get_value(serie.points, i)
+
+        if self.stroke:
+            if self.interpolate:
+                view_values = map(self.view, serie.interpolated)
+            if self.fill:
+                view_values = self._fill(view_values)
+            self.svg.line(
+                serie_node['plot'], view_values,
+                class_='line reactive' + (' nofill' if not self.fill else ''))
+
+    def _compute(self):
+        self._x_pos = [x / float(self._len - 1) for x in range(self._len)
+        ] if self._len != 1 else [.5]  # Center if only one value
+        for serie in self.series:
+            if not hasattr(serie, 'points'):
+                serie.points = [
+                    (self._x_pos[i], v)
+                    for i, v in enumerate(serie.values)]
+                if self.interpolate:
+                    interpolate = interpolation(
+                        self._x_pos, serie.values, kind=self.interpolate)
+                    p = float(self.interpolation_precision)
+                    serie.interpolated = [(x / p, float(interpolate(x / p)))
+                                          for x in range(int(p + 1))]
+        if self.include_x_axis:
+            self._box.ymin = min(min(self._values), 0)
+            self._box.ymax = max(max(self._values), 0)
+        else:
+            self._box.ymin = min(self._values)
+            self._box.ymax = max(self._values)
+
+        self._y_pos = self._pos(self._box.ymin, self._box.ymax, self.y_scale
+        ) if not self.y_labels else map(int, self.y_labels)
+
+        self._x_labels = self.x_labels and zip(self.x_labels, self._x_pos)
+        self._y_labels = zip(map(str, self._y_pos), self._y_pos)
+
+    def _plot(self):
+        for serie in self.series:
+            self.line(self._serie(serie.index), serie)
---
+++ packages/pygal/pygal-0.9.9.tar.gz/pygal-0.9.9/pygal/graph/radar.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+# This file is part of pygal
+#
+# A python svg graph plotting library
+# Copyright © 2012 Kozea
+#
+# This library is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with pygal. If not, see <http://www.gnu.org/licenses/>.
+from pygal.graph.line import Line
+from pygal.view import PolarView
+from pygal.util import deg, cached_property
+from pygal.interpolate import interpolation
+from math import cos, pi
+
+
+class Radar(Line):
+    """Kiviat graph"""
+
+    def _fill(self, values):
+        return values
+
+    def _get_value(self, values, i):
+        return str(values[i][0])
+
+    @cached_property
+    def _values(self):
+        if self.interpolate:
+            return [val[0] for serie in self.series
+                    for val in serie.interpolated]
+        else:
+            return super(Line, self)._values
+
+    def _set_view(self):
+        self.view = PolarView(
+            self.width - self.margin.x,
+            self.height - self.margin.y,
+            self._box)
+
+    def _x_axis(self):
+        if not self._x_labels:
+            return
+
+        axis = self.svg.node(self.plot, class_="axis x web")
+        format = lambda x: '%f %f' % x
+        center = self.view((0, 0))
+        r = self._rmax
+        for label, theta in self._x_labels:
+            guides = self.svg.node(axis, class_='guides')
+            end = self.view((r, theta))
+            self.svg.node(guides, 'path',
+                      d='M%s L%s' % (format(center), format(end)),
+                      class_='line')
+            r_txt = (1 - self._box.__class__._margin) * self._box.ymax
+            pos_text = self.view((r_txt, theta))
+            text = self.svg.node(guides, 'text',
+                      x=pos_text[0],
+                      y=pos_text[1]
+            )
+            text.text = label
+            angle = - theta + pi / 2.
+            if cos(angle) < 0:
+                angle -= pi
+            text.attrib['transform'] = 'rotate(%f %s)' % (
+                deg(angle), format(pos_text))
+
+    def _y_axis(self):
+        if not self._y_labels:
+            return
+
+        axis = self.svg.node(self.plot, class_="axis y web")
+
+        for label, r in reversed(self._y_labels):
+            guides = self.svg.node(axis, class_='guides')
+            self.svg.line(
+                guides, [self.view((r, theta)) for theta in self._x_pos],
+                close=True,
+                class_='guide line')
+            x, y = self.view((r, self._x_pos[0]))
+            self.svg.node(guides, 'text',
+                             x=x - 5,
+                             y=y
+            ).text = label
+
+    def _compute(self):
+        delta = 2 * pi / float(self._len)
+        self._x_pos = [.5 * pi + i * delta for i in range(self._len + 1)]
+        for serie in self.series:
+            vals = list(serie.values)
+            vals.append(vals[0])
+            serie.points = [
+                (v, self._x_pos[i])
+                for i, v in enumerate(vals)]
+            if self.interpolate:
+                extend = 2
+                extended_x_pos = (
+                    [.5 * pi + i * delta for i in range(-extend, 0)] +
+                    self._x_pos +
+                    [.5 * pi + i * delta for i in range(
+                        self._len + 1, self._len + 1 + extend)])
+                extended_vals = vals[-extend:] + vals + vals[:extend]
+                interpolate = interpolation(
+                    extended_x_pos, extended_vals, kind=self.interpolate)
+                serie.interpolated = []
+                p = float(self.interpolation_precision)
+                for s in range(int(p + 1)):
+                    x = .5 * pi + 2 * pi * (s / p)
+                    serie.interpolated.append((float(interpolate(x)), x))
+
+        self._box._margin *= 2
+        self._box.xmin = self._box.ymin = 0
+        self._box.xmax = self._box.ymax = self._rmax = max(self._values)
+
+        self._y_pos = self._pos(
+            self._box.ymin, self._box.ymax, self.y_scale, max_scale=8
+        ) if not self.y_labels else map(int, self.y_labels)
+        self._x_labels = self.x_labels and zip(self.x_labels, self._x_pos)
+        self._y_labels = zip(map(str, self._y_pos), self._y_pos)
+        self._box.xmin = self._box.ymin = - self._box.ymax
