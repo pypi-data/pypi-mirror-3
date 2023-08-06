5341e63e03b314ea1ec64a551cad4ed19adeb49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:08 +0000

    Add package secobj/0.1adev/secobj-0.1adev-py2.7.egg

---
+++ packages/secobj/secobj-0.1adev-py2.7.egg/secobj/logger.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+import logging
+import logging.config
+
+from secobj.localization import _
+
+LOG      = None
+LOG_BASE = None
+
+# If logging is not initialized, then logging is impossible
+def getlogger(*args):
+    if LOG is None:
+        raise ValueError, _("Logger is not initialized")
+    if len(args) > 0:
+        names = [LOG_BASE]
+        names.extend(args)
+        return logging.getLogger(".".join(names))
+    return LOG
+
+# Bootstrap phase, no logging possible
+def initlogger(base, configfile=None):
+    global LOG, LOG_BASE
+    if LOG is None:
+        if configfile is not None:
+            logging.config.fileConfig(configfile)
+        LOG_BASE = base
+        LOG      = logging.getLogger(LOG_BASE)
+        if len(LOG.handlers) == 0:
+            LOG.addHandler(logging.NullHandler())
+
