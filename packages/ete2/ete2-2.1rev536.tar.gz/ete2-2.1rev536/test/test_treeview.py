a9826efa26cda7dee0b5b4944a009cf493c62bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:55 +0000

    Add package ete2/2.1rev536/ete2-2.1rev536.tar.gz

---
+++ packages/ete2/ete2-2.1rev536.tar.gz/ete2-2.1rev536/test/test_treeview.py
@@ -0,0 +1,51 @@
+import random
+import sys
+sys.path.insert(0, "../")
+from ete2 import Tree, TreeStyle, NodeStyle
+from ete2.treeview.faces import *
+from ete2.treeview.main import random_color, _NODE_TYPE_CHECKER, FACE_POSITIONS
+
+sys.path.insert(0, "../examples/treeview")
+import face_grid, bubble_map, item_faces, node_style, node_background, face_positions
+
+main_tree = Tree()
+main_tree.dist = 0
+
+t, ts = face_grid.get_example_tree()
+t_grid = TreeFace(t, ts)
+n = main_tree.add_child()
+n.add_face(t_grid, 0, "aligned")
+
+t, ts = bubble_map.get_example_tree()
+t_bubble = TreeFace(t, ts)
+n = main_tree.add_child()
+n.add_face(t_bubble, 0, "aligned")
+
+t, ts = item_faces.get_example_tree()
+t_items = TreeFace(t, ts)
+n = main_tree.add_child()
+n.add_face(t_items, 0, "aligned")
+
+t, ts = node_style.get_example_tree()
+t_nodest = TreeFace(t, ts)
+n = main_tree.add_child()
+n.add_face(t_nodest, 0, "aligned")
+
+t, ts = node_background.get_example_tree()
+t_bg = TreeFace(t, ts)
+n = main_tree.add_child()
+n.add_face(t_bg, 0, "aligned")
+
+t, ts = face_positions.get_example_tree()
+t_fpos = TreeFace(t, ts)
+n = main_tree.add_child()
+n.add_face(t_fpos, 0, "aligned")
+
+
+ms = TreeStyle()
+ms.mode = "r"
+ms.show_leaf_name = False
+main_tree.show(tree_style=ms)
+ 
+
+        
