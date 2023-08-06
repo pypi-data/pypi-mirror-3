f3dfc81b811bbde41ae5f436a329f5fb0aefebc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:19 +0000

    Add package veer/1.2.0/veer-1.2.0.zip

---
+++ packages/veer/veer-1.2.0.zip/veer-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'veer',
+        version = '1.2.0',
+        py_modules = ['veer'],
+        author = 'kk',
+        author_ph = 9700036181,
+
+    )
+        
