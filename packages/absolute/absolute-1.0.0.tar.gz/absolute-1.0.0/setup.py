3ae63166c35bf7c36c00af9789a97fe13b18cf4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:12 +0000

    Add package absolute/1.0.0/absolute-1.0.0.tar.gz

---
+++ packages/absolute/absolute-1.0.0.tar.gz/absolute-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='absolute',
+	version='1.0.0',
+	py_modules=['absolute'],
+	author='ysb.4',
+	author_email='saibhargav@iitb.ac.in',
+	url='http://home.iitb.ac.in/~saibhargav',
+	description='A simple function which computes the absolute value of a number',
+	)
