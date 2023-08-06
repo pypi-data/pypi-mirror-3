93dbd59d49e07a2cd02b996b788953b9151393c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:20 +0000

    Add package pyassert/0.2.3/pyassert-0.2.3.tar.gz

---
+++ packages/pyassert/pyassert-0.2.3.tar.gz/pyassert-0.2.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+          name = 'pyassert',
+          version = '0.2.3',
+          description = 'Rich assertions library for Python',
+          long_description = '''
+pyassert is an assertion library for the Python programming language. pyassert aims to provide assertions with provide
+
+* rich functionality: common assertions should be expressed easily
+* good readability: assertions should be easy to read and easy to understand to enhance the overall understandability of the test
+* independent of the test framework: pyassert assertions work with every Python test environment.
+''',
+          author = "Alexander Metzner",
+          author_email = "halimath.wilanthaou@gmail.com",
+          license = 'Apache Software License',
+          url = 'https://github.com/halimath/pyassert',
+          scripts = [],
+          packages = ['pyassert'],
+          classifiers = ['Development Status :: 4 - Beta', 'Environment :: Other Environment', 'Intended Audience :: Developers', 'License :: OSI Approved :: Apache Software License', 'Programming Language :: Python', 'Topic :: Software Development :: Quality Assurance', 'Topic :: Software Development :: Testing'],
+          
+          
+          
+          tests_requires = [ "coverage" ],
+          zip_safe=True
+    )
