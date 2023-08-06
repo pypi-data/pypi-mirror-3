a38e690eca0f5efbcd20c11249fe50e5c3db2ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:37 +0000

    Add package django-adminplus/0.1.7/django-adminplus-0.1.7.tar.gz

---
+++ packages/django-adminplus/django-adminplus-0.1.7.tar.gz/django-adminplus-0.1.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+setup(
+    name='django-adminplus',
+    version='0.1.7',
+    description='Add new pages to the Django admin.',
+    long_description=open('README.rst').read(),
+    author='James Socol',
+    author_email='james@mozilla.com',
+    url='http://github.com/jsocol/django-adminplus',
+    license='BSD',
+    packages=['adminplus'],
+    include_package_data=True,
+    package_data = {'': ['README.rst', 'templates/adminplus/*.html']},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Environment :: Web Environment :: Mozilla',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
