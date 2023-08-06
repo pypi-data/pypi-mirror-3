0ec2f40e673f2bea7d4a0b234215da5a79a0d5ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:06 +0000

    Add package jenkviz/0.3.0/jenkviz-0.3.0.tar.gz

---
+++ packages/jenkviz/jenkviz-0.3.0.tar.gz/jenkviz-0.3.0/CHANGES.rst
@@ -0,0 +1,88 @@
+=================
+jenkviz CHANGES
+=================
+
+.. contents:: Table of Contents
+
+
+jenkviz git master
+--------------------
+
+:git: https://github.com/bdelbosc/jenkviz
+
+:Target: 0.3.1
+
+
+
+jenkviz 0.2.0
+------------------
+
+:Package: http://pypi.python.org/packages/source/j/jenkviz/jenkviz-0.3.0.tar.gz
+
+:github: https://github.com/bdelbosc/jenkviz/tree/0.3.0
+
+:Released date: 2012-01-31
+
+Upgrade notes
+~~~~~~~~~~~~~~
+
+* You should use the ``--update`` options or remove your existing
+  database ``~/jenkviz.db``.
+
+
+New Features
+~~~~~~~~~~~~~
+
+* Use the Jenkins REST API to discover downstream builds, this
+  make it work with free style job.
+ 
+
+Bug Fixes
+~~~~~~~~~~
+
+* Fix duration parsing for hour.
+
+* Fix duration parsing for second in float format.
+
+
+jenkviz 0.2.0
+------------------
+
+:Package: http://pypi.python.org/packages/source/j/jenkviz/jenkviz-0.2.0.tar.gz
+
+:github: https://github.com/bdelbosc/jenkviz/tree/0.2.0
+
+:Released date: 2012-01-26
+
+
+New Features
+~~~~~~~~~~~~~~
+
+* Adding ``--explore`` options
+
+* Adding ``--reverse`` to crawl backward
+
+Bug Fixes
+~~~~~~~~~~
+
+* Handles multiple upstream build.
+
+* Handles 404 on deleted build.
+
+* Fix color for Failed and Aborted build.
+
+
+jenkviz 0.1.1
+------------------
+
+:Package: http://pypi.python.org/packages/source/j/jenkviz/jenkviz-0.1.1.tar.gz
+
+:github: https://github.com/bdelbosc/jenkviz/tree/0.1.1
+
+:Released date: 2012-01-25
+
+First release.
+
+
+
+
---
+++ packages/jenkviz/jenkviz-0.3.0.tar.gz/jenkviz-0.3.0/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+__author__ = "Benoit Delbosc"
+__copyright__ = "Copyright (C) 2012 Nuxeo SA <http://nuxeo.com/>"
+__version__ = '0.3.0'
+"""jenkviz package setup"""
+from setuptools import setup, find_packages
+
+
+setup(
+    name="jenkviz",
+    version=__version__,
+    description="Crawl a jenkins build and report stats and graphs about the build flow.",
+    long_description=''.join(open('README.txt').readlines()),
+    author="Benoit Delbosc",
+    author_email="bdelbosc@nuxeo.com",
+    url="http://pypi.python.org/pypi/jenkviz",
+    download_url="http://pypi.python.org/packages/source/t/jenkviz/jenkviz-%s.tar.gz" % __version__,
+    packages=find_packages(),
+    license='GPL',
+    keywords='jenkins hudson CI chart',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+    # setuptools specific keywords
+    install_requires=['requests', 'sqlalchemy'],
+    zip_safe=True,
+    test_suite='nose.collector',
+    entry_points={
+        'console_scripts': [
+            'jenkviz = jenkviz.main:main'],
+    },
+)
