286c23b41a331a33f4a75f0345114860227b0919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:33 +0000

    Add package pymatgen/1.9.0/pymatgen-1.9.0-py2.7.egg

---
+++ packages/pymatgen/pymatgen-1.9.0-py2.7.egg/pymatgen/phasediagram/__init__.py
@@ -0,0 +1,8 @@
+"""
+The phasediagram package implements the analysis tools to performe phase
+stability analyses, including the constructing of phase diagrams, determination
+of decomposition products, etc.
+"""
+
+__author__ = "Shyue"
+__date__ = "$Nov 30, 2010 9:25:43 AM$"
