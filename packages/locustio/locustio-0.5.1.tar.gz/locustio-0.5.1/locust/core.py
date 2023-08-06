b00b03389a904e6c7b61ad5b943346728d9cd3f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:23 +0000

    Add package locustio/0.5.1/locustio-0.5.1.tar.gz

---
+++ packages/locustio/locustio-0.5.1.tar.gz/locustio-0.5.1/locust/core.py
@@ -0,0 +1,295 @@
+import gevent
+from gevent import monkey, GreenletExit
+
+monkey.patch_all(thread=False)
+
+from time import time
+import sys
+import random
+import warnings
+import traceback
+import logging
+
+from clients import HttpBrowser
+import events
+
+from exception import LocustError, InterruptLocust, RescheduleTaskImmediately
+
+logger = logging.getLogger(__name__)
+
+def require_once(required_func):
+    """
+    @require_once decorator is used on a locust task in order to make sure another locust 
+    task (the argument to require_once) is run once (per client) before the decorated 
+    task.
+    
+    The require_once decorator respects the wait time of the Locust class, by inserting the
+    locust tasks at the beginning of the task execution queue.
+    
+    Example::
+    
+        def login(l):
+            l.client.post("/login", {"username":"joe_hill", "password":"organize"})
+        
+        @require_once(login)
+        def inbox(l):
+            l.client.get("/inbox")
+    """
+    def decorator_func(func):
+        def wrapper(l):
+            if not "_required_once" in l.__dict__:
+                l.__dict__["_required_once"] = {}
+            
+            if not str(required_func) in l._required_once:
+                # when the required task has not been run in the current client, we schedule it to
+                # be the next task in queue, and we also reschedule the original task to be run 
+                # immediately after the required task
+                l._required_once[str(required_func)] = True
+                l.schedule_task(func, first=True)
+                required_func(l)
+                return
+                
+            return func(l)
+        return wrapper
+    return decorator_func
+
+def task(weight_or_func=1):
+    def decorator_func(func):
+        func.locust_task_weight = weight_or_func
+        return func
+    
+    """
+    Check if task was used without parentheses (not called), like this::
+    
+        @task
+        def my_task()
+            pass
+    """
+    if callable(weight_or_func):
+        func = weight_or_func
+        weight_or_func = 1
+        return decorator_func(func)
+    else:
+        return decorator_func
+
+class LocustMeta(type):
+    """
+    Meta class for the main Locust class. It's used to allow Locust classes to specify task execution 
+    ratio using an {task:int} dict, or a [(task0,int), ..., (taskN,int)] list.
+    """
+    
+    def __new__(meta, classname, bases, classDict):
+        new_tasks = []
+        for base in bases:
+            if hasattr(base, "tasks") and base.tasks:
+                new_tasks += base.tasks
+        
+        if "tasks" in classDict and classDict["tasks"] is not None:
+            tasks = classDict["tasks"]
+            if isinstance(tasks, dict):
+                tasks = list(tasks.iteritems())
+            
+            for task in tasks:
+                if isinstance(task, tuple):
+                    task, count = task
+                    for i in xrange(0, count):
+                        new_tasks.append(task)
+                else:
+                    new_tasks.append(task)
+        
+        for item in classDict.itervalues():
+            if hasattr(item, "locust_task_weight"):
+                for i in xrange(0, item.locust_task_weight):
+                    new_tasks.append(item)
+        
+        classDict["tasks"] = new_tasks
+        
+        return type.__new__(meta, classname, bases, classDict)
+
+class LocustBase(object):
+    """
+    Locust base class defining a locust user/client.
+    """
+    
+    tasks = []
+    """
+    List with python callables that represents a locust user task.
+
+    If tasks is a list, the task to be performed will be picked randomly.
+
+    If tasks is a *(callable,int)* list of two-tuples, the task to be performed will be picked randomly, but each task will be
+    weighted according to it's corresponding int value. So in the following case *task1* will be three times more
+    likely to be picked than *task2*::
+
+        class User(Locust):
+            tasks = [(task1, 3), (task2, 1)]
+    """
+    
+    host = None
+    """Base hostname to swarm. i.e: http://127.0.0.1:1234"""
+
+    min_wait = 1000
+    """Minimum waiting time between two execution of locust tasks"""
+    
+    max_wait = 1000
+    """Maximum waiting time between the execution of locust tasks"""
+
+    avg_wait = None
+    """Average waiting time wanted between the execution of locust tasks"""
+    
+    stop_timeout = None
+    """Number of seconds after which the Locust will die. If None it won't timeout."""
+
+    weight = 10
+    """Probability of locust beeing choosen. The higher the weight, the greater is the chance of it beeing chosen."""
+    
+    __metaclass__ = LocustMeta
+    
+    def __init__(self):
+        self._avg_wait = 0
+        self._avg_wait_ctr = 0
+        self._task_queue = []
+        self._time_start = time()
+
+    def __call__(self):
+        if hasattr(self, "on_start"):
+            self.on_start()
+        while (True):
+            try:
+                if self.stop_timeout is not None and time() - self._time_start > self.stop_timeout:
+                    return
+        
+                if not self._task_queue:
+                    self.schedule_task(self.get_next_task())
+                
+                try:
+                    self.execute_next_task()
+                except RescheduleTaskImmediately:
+                    pass
+                else:
+                    self.wait()
+            except InterruptLocust, e:
+                if e.reschedule:
+                    raise RescheduleTaskImmediately()
+                return
+            except GreenletExit:
+                raise
+            except Exception, e:
+                events.locust_error.fire(self, e)
+                sys.stderr.write("\n" + traceback.format_exc())
+    
+    def execute_next_task(self):
+        task = self._task_queue.pop(0)
+        self.execute_task(task["callable"], *task["args"])
+    
+    def execute_task(self, task, *args):
+        # check if the function is a method bound to the current locust, and if so, don't pass self as first argument
+        if hasattr(task, "im_self") and task.im_self == self:
+            task(*args)
+        else:
+            task(self, *args)
+    
+    def schedule_task(self, task_callable, *args, **kwargs):
+        """
+        Add a task to the Locust's task execution queue.
+        
+        *Arguments*:
+        
+        * task_callable: Locust task to schedule
+        * first: Optional keyword argument. If True, the task will be put first in the queue.
+        * All other non keyword arguments will be passed to the task callable.
+        """
+        task = {"callable":task_callable, "args":args}
+        if "first" in kwargs:
+            self._task_queue.insert(0, task)
+        else:
+            self._task_queue.append(task)
+    
+    def get_next_task(self):
+        return random.choice(self.tasks)
+    
+    def wait(self):
+        if self.avg_wait:
+            # Handle (strive for) average wait time
+            if self._avg_wait:
+                if self._avg_wait >= self.avg_wait:
+                    # Want shorter wait
+                    millis = random.randint(self.min_wait, self.avg_wait)
+                else:
+                    # Want longer wait
+                    millis = random.randint(self.avg_wait, self.max_wait)
+                self._avg_wait = ((self._avg_wait * self._avg_wait_ctr) + millis) / (self._avg_wait_ctr + 1.0)
+            else:
+                # Average specified but is first run
+                radius = min(self.avg_wait - self.min_wait, self.max_wait - self.avg_wait)
+                millis = random.randint(self.avg_wait - radius, self.avg_wait + radius)
+                self._avg_wait = millis
+            self._avg_wait_ctr += 1
+        else:
+            # Ignore average wait
+            millis = random.randint(self.min_wait, self.max_wait)
+
+        seconds = millis / 1000.0
+        self._sleep(seconds)
+
+    def _sleep(self, seconds):
+        gevent.sleep(seconds)
+
+
+class Locust(LocustBase):
+    """
+    Locust class that inherits from LocustBase and creates a *client* attribute on instantiation. 
+    
+    The *client* attribute is a simple HTTP client with support for keeping a user session between requests.
+    """
+    
+    client = None
+    """
+    Instance of HttpBrowser that is created upon instantiation of Locust. 
+    The client support cookies, and therefore keeps the session between HTTP requests.
+    """
+    
+    gzip = False
+    """
+    If set to True the HTTP client will set headers for accepting gzip, and decode gzip data
+    that is sent back from the server. This attribute is set from the command line.
+    """
+    
+    def __init__(self):
+        super(Locust, self).__init__()
+        if self.host is None:
+            raise LocustError("You must specify the base host. Either in the host attribute in the Locust class, or on the command line using the --host option.")
+
+        self.client = HttpBrowser(self.host, self.gzip)
+
+class WebLocust(Locust):
+    def __init__(self, *args, **kwargs):
+        warnings.warn("WebLocust class has been, deprecated. Use Locust class instead.")
+        super(WebLocust, self).__init__(*args, **kwargs)
+
+class SubLocust(LocustBase):
+    """
+    Class for making a sub Locust that can be included as a task inside of a normal Locust/WebLocus,
+    as well as inside another sub locust. 
+    
+    When the parent locust enters the sub locust, it will not
+    continue executing it's tasks until a task in the sub locust has called the interrupt() function.
+    """
+    
+    def __init__(self, parent):
+        super(SubLocust, self).__init__()
+        
+        self.parent = parent
+        if isinstance(parent, Locust):
+            self.client = parent.client
+        
+        self()
+    
+    def interrupt(self, reschedule=True):
+        """
+        Interrupt the SubLocust and hand over execution control back to the parent Locust.
+        
+        If *reschedule* is True (default), the parent Locust will immediately re-schedule,
+        and execute, a new task
+        """
+        raise InterruptLocust(reschedule)
---
+++ packages/locustio/locustio-0.5.1.tar.gz/locustio-0.5.1/locust/test/test_stats.py
@@ -0,0 +1,125 @@
+import unittest
+import time
+
+from testcases import WebserverTestCase
+from locust.stats import RequestStats
+from locust.core import Locust, SubLocust, task
+from locust.inspectlocust import get_task_ratio_dict
+
+class TestRequestStats(unittest.TestCase):
+    def setUp(self):
+        RequestStats.global_start_time = time.time()
+        self.s = RequestStats("GET", "test_entry")
+        self.s.log(45, 0)
+        self.s.log(135, 0)
+        self.s.log(44, 0)
+        self.s.log_error(Exception("dummy fail"))
+        self.s.log_error(Exception("dummy fail"))
+        self.s.log(375, 0)
+        self.s.log(601, 0)
+        self.s.log(35, 0)
+        self.s.log(79, 0)
+        self.s.log_error(Exception("dummy fail"))
+
+    def test_median(self):
+        self.assertEqual(self.s.median_response_time, 79)
+
+    def test_total_rps(self):
+        self.assertEqual(self.s.total_rps, 7)
+
+    def test_current_rps(self):
+        self.s.global_last_request_timestamp = int(time.time()) + 4
+        self.assertEqual(self.s.current_rps, 3.5)
+
+        self.s.global_last_request_timestamp = int(time.time()) + 25
+        self.assertEqual(self.s.current_rps, 0)
+
+    def test_num_reqs_fails(self):
+        self.assertEqual(self.s.num_reqs, 7)
+        self.assertEqual(self.s.num_failures, 3)
+
+    def test_avg(self):
+        self.assertEqual(self.s.avg_response_time, 187.71428571428571428571428571429)
+
+    def test_reset(self):
+        self.s.reset()
+        self.s.log(756, 0)
+        self.s.log_error(Exception("dummy fail after reset"))
+        self.s.log(85, 0)
+
+        self.assertEqual(self.s.total_rps, 2)
+        self.assertEqual(self.s.num_reqs, 2)
+        self.assertEqual(self.s.num_failures, 1)
+        self.assertEqual(self.s.avg_response_time, 420.5)
+        self.assertEqual(self.s.median_response_time, 85)
+
+    def test_aggregation(self):
+        s1 = RequestStats("GET", "aggregate me!")
+        s1.log(12, 0)
+        s1.log(12, 0)
+        s1.log(38, 0)
+        s1.log_error("Dummy exzeption")
+
+        s2 = RequestStats("GET", "aggregate me!")
+        s2.log_error("Dummy exzeption")
+        s2.log_error("Dummy exzeption")
+        s2.log(12, 0)
+        s2.log(99, 0)
+        s2.log(14, 0)
+        s2.log(55, 0)
+        s2.log(38, 0)
+        s2.log(55, 0)
+        s2.log(97, 0)
+
+        s = RequestStats("GET", "")
+        s.iadd_stats(s1, full_request_history=True)
+        s.iadd_stats(s2, full_request_history=True)
+
+        self.assertEqual(s.num_reqs, 10)
+        self.assertEqual(s.num_failures, 3)
+        self.assertEqual(s.median_response_time, 38)
+        self.assertEqual(s.avg_response_time, 43.2)
+
+class TestRequestStatsWithWebserver(WebserverTestCase):
+    def test_request_stats_content_length(self):
+        class MyLocust(Locust):
+            host = "http://127.0.0.1:%i" % self.port
+    
+        locust = MyLocust()
+        locust.client.get("/ultra_fast")
+        self.assertEqual(RequestStats.get("GET", "/ultra_fast").avg_content_length, len("This is an ultra fast response"))
+        locust.client.get("/ultra_fast")
+        self.assertEqual(RequestStats.get("GET", "/ultra_fast").avg_content_length, len("This is an ultra fast response"))
+
+
+class MyLocust(Locust):
+    @task(75)
+    def root_task(self):
+        pass
+    
+    @task(25)
+    class MySubLocust(SubLocust):
+        @task
+        def task1(self):
+            pass
+        @task
+        def task2(self):
+            pass
+    
+class TestInspectLocust(unittest.TestCase):
+    def test_get_task_ratio_dict_relative(self):
+        ratio = get_task_ratio_dict([MyLocust])
+        self.assertEqual(1.0, ratio["MyLocust"]["ratio"])
+        self.assertEqual(0.75, ratio["MyLocust"]["tasks"]["root_task"]["ratio"])
+        self.assertEqual(0.25, ratio["MyLocust"]["tasks"]["MySubLocust"]["ratio"])
+        self.assertEqual(0.5, ratio["MyLocust"]["tasks"]["MySubLocust"]["tasks"]["task1"]["ratio"])
+        self.assertEqual(0.5, ratio["MyLocust"]["tasks"]["MySubLocust"]["tasks"]["task2"]["ratio"])
+    
+    def test_get_task_ratio_dict_total(self):
+        ratio = get_task_ratio_dict([MyLocust], total=True)
+        self.assertEqual(1.0, ratio["MyLocust"]["ratio"])
+        self.assertEqual(0.75, ratio["MyLocust"]["tasks"]["root_task"]["ratio"])
+        self.assertEqual(0.25, ratio["MyLocust"]["tasks"]["MySubLocust"]["ratio"])
+        self.assertEqual(0.125, ratio["MyLocust"]["tasks"]["MySubLocust"]["tasks"]["task1"]["ratio"])
+        self.assertEqual(0.125, ratio["MyLocust"]["tasks"]["MySubLocust"]["tasks"]["task2"]["ratio"])
+        
