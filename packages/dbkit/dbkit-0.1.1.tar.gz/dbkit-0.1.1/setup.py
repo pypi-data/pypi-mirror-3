f932fa50f9e9c9b56f1e0849fc9932f1b2bb2e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:44 +0000

    Add package dbkit/0.1.1/dbkit-0.1.1.tar.gz

---
+++ packages/dbkit/dbkit-0.1.1.tar.gz/dbkit-0.1.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import dbkit
+
+setup(
+    name='dbkit',
+    version=dbkit.__version__,
+    description='DB-API made easier',
+    long_description=open('README').read(),
+    url='https://github.com/kgaughan/dbkit/',
+    license='MIT',
+    py_modules=['dbkit'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database',
+    ],
+
+    author=dbkit.__author__,
+    author_email=dbkit.__email__
+)
