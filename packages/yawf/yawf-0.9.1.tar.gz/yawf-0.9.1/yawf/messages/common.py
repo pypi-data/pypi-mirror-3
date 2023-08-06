346aa1577d20e4d8f4780f30357cc48af13d05f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:51 +0000

    Add package yawf/0.9.1/yawf-0.9.1.tar.gz

---
+++ packages/yawf/yawf-0.9.1.tar.gz/yawf-0.9.1/yawf/messages/common.py
@@ -0,0 +1,41 @@
+from django.utils.translation import ugettext_lazy as _
+
+from yawf.messages.spec import MessageSpec
+
+
+def message_spec_fabric(id, verb=None, rank=0, base_spec=MessageSpec, **attrs):
+    # NOTE: reflect old behaviour when MessageSpec was registered as class
+    # and this function was a class fabric.
+    attrs.update({
+        'rank': rank,
+        'verb': verb,
+        'id': id})
+    return base_spec(**attrs)
+
+
+class BasicCancelMessage(MessageSpec):
+    id = 'cancel'
+    verb = _('cancel')
+    rank = 1000
+
+
+class BasicDeleteMessage(MessageSpec):
+    id = 'delete'
+    verb = _('delete')
+    rank = 1001
+
+
+class BasicEditMessage(MessageSpec):
+
+    id = 'edit'
+    verb = _('change')
+    rank = 900
+
+    @staticmethod
+    def params_wrapper(params):
+        return {'edit_fields': params}
+
+
+class BasicStartMessage(MessageSpec):
+    id = 'start_workflow'
+    verb = _('create')
---
+++ packages/yawf/yawf-0.9.1.tar.gz/yawf-0.9.1/yawf/tests/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+from .permissions import *
+from .message_specs import *
