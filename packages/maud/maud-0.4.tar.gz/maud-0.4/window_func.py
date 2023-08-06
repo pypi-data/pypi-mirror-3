77d686003a73354bac009a23af3a5c59b7f7a660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:10 +0000

    Add package maud/0.4/maud-0.4.tar.gz

---
+++ packages/maud/maud-0.4.tar.gz/maud-0.4/window_func.py
@@ -0,0 +1,85 @@
+""" Weight functions"""
+
+import numpy as np
+from numpy import ma
+from numpy import pi
+
+from fluid.common.distance import distance
+
+# defining some weight functions
+
+# blackman
+def _weight_blackman(r,l):
+    """ 
+    using values from wikipedia 'exact blackman'
+    
+    """
+    w = 0.42 +  0.5*np.cos(2*pi*r/l) + 0.08*np.cos(4*pi*r/l) # fase lag-> sign change
+    w[np.absolute(r)>l/2.]=0
+    return w
+
+# rectangular
+
+# triangular
+def _weight_triangular(r,l):
+    """
+    """
+    w = ma.masked_all(r.shape)
+    ind = np.abs(r)<l/2.
+    w[ind] = 1-np.abs(2*r[ind]/l)
+    return w
+
+def _weight_hamming(r,l):
+    """
+    """
+    w=0.54+0.46*np.cos(pi*r/l)
+    w[np.absolute(r)>l]=0
+    return w
+
+# hamming 2D
+def _weight_hamming_2D(x, y, l):
+    """
+    """
+    r = (x**2+y**2)**0.5
+    w = 0.54 + 0.46*np.cos(2*pi*r/l)
+    w[r>l] = 0
+    return w
+
+# hann
+def _weight_hann(r,l):
+    """
+    """
+    w=0.5*(1+np.cos(pi*r/l))
+    w[np.absolute(r)>l]=0
+    return w
+
+# hann 2D
+def _weight_hann_2D(x,y,l):
+    """
+    """
+    r=(x**2+y**2)**0.5
+    w=0.5*(1+np.cos(pi*r/l))
+    w[r>l]=0
+    return w
+
+# lanczos 2D
+def _weight_lanczos_2D(x,y,l,cutoff):
+    """ Working on
+    """
+    #c=cutoff
+    r=(x**2+y**2)**0.5
+    w=np.sinc(r/cutoff)*np.sinc(r/cutoff/l)
+    w[r>3*l]=0
+
+# ==== funcoes antigas para eliminar ======================
+
+def _weight_triangular_2D(x,y,l):
+    """
+    """
+    r=(x**2+y**2)**0.5
+    w=(l-r)/l
+    w[r>l]=0
+    return w
+
+# hamming
+
