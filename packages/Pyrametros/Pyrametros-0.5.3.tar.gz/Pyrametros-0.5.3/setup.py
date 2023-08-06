4f23de95e79f6cefbaa19339b34c1fd6542a0b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:41 +0000

    Add package Pyrametros/0.5.3/Pyrametros-0.5.3.tar.gz

---
+++ packages/Pyrametros/Pyrametros-0.5.3.tar.gz/Pyrametros-0.5.3/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "Pyrametros",
+    version = "0.5.3",
+    author = "Chris Perivolaropoulos",
+    author_email = "darksaga2006@gmail.com",
+    description = ("A way to use ascii tables to automatically generate code."),
+    license = "GPL",
+    keywords = "paramtetric source code table",
+    url = "http://packages.python.org/Pyrmetros",
+    packages=['pyrametros', 'pyrametros.examples'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+	"License :: OSI Approved :: GNU General Public License v2 (GPLv2)"
+    ],
+)
