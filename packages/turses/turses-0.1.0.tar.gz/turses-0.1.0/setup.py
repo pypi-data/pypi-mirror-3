d33315616fc9a299f9bc628acdf5c996563af630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:26 +0000

    Add package turses/0.1.0/turses-0.1.0.tar.gz

---
+++ packages/turses/turses-0.1.0.tar.gz/turses-0.1.0/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+import turses
+
+
+name = "turses"
+
+requirements = [
+    "oauth2",
+    "urwid",
+    "tweepy",
+]
+test_requirements = list(requirements)
+test_requirements.extend(["mock", "nose", "coverage", "nose-progressive"])
+
+try:
+    long_description = open("README.rst").read() + "\n\n" + open("HISTORY.rst").read()
+except IOError:
+    long_description = ""
+
+setup(name="turses",
+      version=turses.version,
+      author="Alejandro Gómez",
+      author_email="alejandroogomez@gmail.com",
+      url="http://github.com/alejandrogomez/turses",
+      description="A Twitter client with a curses interface.",
+      long_description=long_description,
+      keywords="twitter client curses",
+      packages=[
+          "turses", 
+          "turses.api",
+      ],
+      package_data={'': ['LICENSE']},
+      include_package_data=True,
+      package_dir={
+          "turses":  "turses"
+      },
+      scripts=["bin/turses"],
+      platforms=["linux"],
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Environment :: Console :: Curses",
+          "Intended Audience :: End Users/Desktop",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Natural Language :: English",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Communications"
+      ],
+      install_requires=requirements,
+      tests_require=test_requirements,)
