6d3781085977964f0acac431caa9ad82d9d6c35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:34 +0000

    Add package django-doccloud/0.4.2/django-doccloud-0.4.2.tar.gz

---
+++ packages/django-doccloud/django-doccloud-0.4.2.tar.gz/django-doccloud-0.4.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='django-doccloud',
+    version='0.4.2',
+    description='Provides a reusable document app which interfaces with DocumentCloud',
+    author='Bay Citizen',
+    author_email='shifflett.shane@gmail.com',
+    url='http://github.com/BayCitizen/django-doccloud/',
+    packages=[
+        'doccloud',
+    ],
+    package_data={'doccloud': ['templates/*.html']},
+    install_requires=[
+        'python-documentcloud',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
