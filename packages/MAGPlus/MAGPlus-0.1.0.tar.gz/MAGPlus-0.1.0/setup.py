440969509f5c141c0bd6c04a381b7a9ae7727eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:04 +0000

    Add package MAGPlus/0.1.0/MAGPlus-0.1.0.tar.gz

---
+++ packages/MAGPlus/MAGPlus-0.1.0.tar.gz/MAGPlus-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='MAGPlus',
+    version='0.1.0',
+    author='Josh Gachnang',
+    author_email='Josh@ServerCobra.com',
+    packages=['magplus',],
+    #scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/MAGPlus/',
+    license='GPLv2',
+    description='Retrieving latest Minecraft versions.',
+    long_description=open('README.rst').read(),
+    #install_requires=[
+
+    #],
+)
