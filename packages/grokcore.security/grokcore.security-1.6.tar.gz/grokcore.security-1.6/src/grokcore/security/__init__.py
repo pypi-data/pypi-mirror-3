2191bd8ded9bfbed9bcd1f24c02bdfe8cb28c775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:46 +0000

    Add package grokcore.security/1.6/grokcore.security-1.6.tar.gz

---
+++ packages/grokcore.security/grokcore.security-1.6.tar.gz/grokcore.security-1.6/src/grokcore/security/__init__.py
@@ -0,0 +1,28 @@
+##############################################################################
+#
+# Copyright (c) 2006-2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Grok
+"""
+from grokcore.component import *
+
+from grokcore.security.components import Permission, Role
+from grokcore.security.components import Public
+from grokcore.security.directive import require, permissions
+
+# Import this module so that it's available as soon as you import the
+# 'grokcore.security' package.  Useful for tests and interpreter examples.
+import grokcore.security.testing
+
+# Only export public API
+from grokcore.security.interfaces import IGrokcoreSecurityAPI
+__all__ = list(IGrokcoreSecurityAPI)
