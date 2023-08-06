29326b21690691a85fd6036199e23a0121ef05b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:13 +0000

    Add package engineer/0.3.0/engineer-0.3.0.zip

---
+++ packages/engineer/engineer-0.3.0.zip/engineer-0.3.0/engineer/__init__.py
@@ -0,0 +1,2 @@
+# coding=utf-8
+__author__ = 'Tyler Butler <tyler@tylerbutler.com>'
---
+++ packages/engineer/engineer-0.3.0.zip/engineer-0.3.0/engineer/version.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+from datetime import date
+
+__author__ = 'Tyler Butler <tyler@tylerbutler.com>'
+
+__version__ = '0.3.0'
+__date__ = date(2012, 7, 22)
