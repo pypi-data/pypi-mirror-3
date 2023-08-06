f292fdd0158039fde639d20627820be486d3b0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:40 +0000

    Add package StringFormat/0.4/StringFormat-0.4.tar.gz

---
+++ packages/StringFormat/StringFormat-0.4.tar.gz/StringFormat-0.4/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+StringFormat
+============
+
+StringFormat is an independent port of the Python 3 advanced string
+formatting, compatible with Python 2.4 through 2.7.
+This implementation is pure Python.
+
+Usage:
+
+    import stringformat
+    stringformat.init()
+
+
+The advanced string formatting is officially included in the language
+since Python 2.6.
+
+http://docs.python.org/whatsnew/2.6.html#pep-3101-advanced-string-formatting
+
+"""
+
+from setuptools import setup
+
+
+setup(
+    name='StringFormat',
+    version='0.4',
+    license='BSD',
+    url='http://github.com/florentx/stringformat',
+    author='Florent Xicluna',
+    author_email='florent.xicluna@gmail.com',
+    description='Advanced String Formatting for Python >= 2.4',
+    long_description=__doc__,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Filters',
+    ],
+    zip_safe=False,
+    platforms='any',
+    py_modules=['stringformat'],
+    test_suite='tests',
+)
