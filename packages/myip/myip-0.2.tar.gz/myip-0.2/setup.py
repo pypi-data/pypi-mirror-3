c7a8036beb0f3d7eb8d09ef34d44f47f99dc9cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:37 +0000

    Add package myip/0.2/myip-0.2.tar.gz

---
+++ packages/myip/myip-0.2.tar.gz/myip-0.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(name = 'myip',
+      version = '0.2',
+      description = "Simply print the IP of the current host",
+      author = 'Oren Held',
+      author_email = 'oren@held.org.il',
+      url = 'http://www.github.com/orenhe/myip',
+      packages = find_packages(),
+      license = 'MIT',
+      entry_points = {
+          'console_scripts': ['myip = myip.myip_cmd:main'],
+      },
+      install_requires = ["argparse"],
+     )
