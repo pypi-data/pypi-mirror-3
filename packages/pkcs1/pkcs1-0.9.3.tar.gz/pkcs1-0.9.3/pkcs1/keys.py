98d29541576ddf4af0a22b4dcefdf908356795e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:15 +0000

    Add package pkcs1/0.9.3/pkcs1-0.9.3.tar.gz

---
+++ packages/pkcs1/pkcs1-0.9.3.tar.gz/pkcs1-0.9.3/pkcs1/keys.py
@@ -0,0 +1,111 @@
+import primitives
+import exceptions
+import random
+
+class RsaPublicKey(object):
+    __slots__ = ('n', 'e', 'bit_size', 'byte_size')
+
+    def __init__(self, n, e):
+        self.n = n
+        self.e = e
+        self.bit_size = primitives.integer_bit_size(n)
+        self.byte_size = primitives.integer_byte_size(n)
+
+
+    def __repr__(self):
+        return '<RsaPublicKey n: %d e: %d bit_size: %d>' % (self.n, self.e, self.bit_size)
+
+    def rsavp1(self, s):
+        if not (0 <= s <= self.n-1):
+            raise exceptions.SignatureRepresentativeOutOfRange
+        return self.rsaep(s)
+
+    def rsaep(self, m):
+        if not (0 <= m <= self.n-1):
+            raise exceptions.MessageRepresentativeOutOfRange
+        return primitives._pow(m, self.e, self.n)
+
+class RsaPrivateKey(object):
+    __slots__ = ('n', 'd', 'bit_size', 'byte_size')
+
+    def __init__(self, n, d):
+        self.n = n
+        self.d = d
+        self.bit_size = primitives.integer_bit_size(n)
+        self.byte_size = primitives.integer_byte_size(n)
+
+    def __repr__(self):
+        return '<RsaPrivateKey n: %d d: %d bit_size: %d>' % (self.n, self.d, self.bit_size)
+
+    def rsadp(self, c):
+        if not (0 <= c <= self.n-1):
+            raise exceptions.CiphertextRepresentativeOutOfRange
+        return primitives._pow(c, self.d, self.n)
+
+    def rsasp1(self, m):
+        if not (0 <= m <= self.n-1):
+            raise exceptions.MessageRepresentativeOutOfRange
+        return self.rsadp(m)
+
+class MultiPrimeRsaPrivateKey(object):
+    __slots__ = ('primes', 'blind', 'blind_inv', 'n', 'e', 'exponents', 'crts', 'bit_size', 'byte_size')
+
+    def __init__(self, primes, e, blind=True, rnd=random.SystemRandom):
+        self.primes = primes
+        self.n = primitives.product(*primes)
+        self.e = e
+        self.bit_size = primitives.integer_bit_size(self.n)
+        self.byte_size = primitives.integer_byte_size(self.n)
+        self.exponents = []
+        for prime in primes:
+            exponent, a, b = primitives.bezout(e, prime-1)
+            assert b == 1
+            if exponent < 0:
+                exponent += prime-1
+            self.exponents.append(exponent)
+        self.crts = [1]
+        R = primes[0]
+        for prime in primes[1:]:
+            crt, a, b = primitives.bezout(R, prime)
+            assert b == 1
+            R *= prime
+            self.crts.append(crt)
+        public = RsaPublicKey(self.n, self.e)
+        if blind:
+            while True:
+                blind_factor = rnd().getrandbits(self.bit_size-1)
+                self.blind = public.rsaep(blind_factor)
+                u, v, gcd = primitives.bezout(blind_factor, self.n)
+                if gcd == 1:
+                    self.blind_inv = u if u > 0 else u + self.n
+                    assert (blind_factor * self.blind_inv) % self.n == 1
+                    break
+        else:
+            self.blind = None
+            self.blind_inv = None
+
+
+    def __repr__(self):
+        return '<RsaPrivateKey n: %d primes: %s bit_size: %d>' % (self.n, self.primes, self.bit_size)
+
+
+    def rsadp(self, c):
+        if not (0 <= c <= self.n-1):
+            raise exceptions.CiphertextRepresentativeOutOfRange
+        R = 1
+        m = 0
+        if self.blind:
+            c = (c * self.blind) % self.n
+        for prime, exponent, crt in zip(self.primes, self.exponents, self.crts):
+            m_i = primitives._pow(c, exponent, prime)
+            h = ((m_i - m) * crt) % prime
+            m += R * h
+            R *= prime
+        if self.blind_inv:
+            m = (m * self.blind_inv) % self.n
+        return m
+
+    def rsasp1(self, m):
+        if not (0 <= m <= self.n-1):
+            raise exceptions.MessageRepresentativeOutOfRange
+        return self.rsadp(m)
