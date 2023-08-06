26541e18e6566484509f12485af4b6f6a9a2770b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:04 +0000

    Add package django-plugshop/0.1.5/django_plugshop-0.1.5-py2.7.egg

---
+++ packages/django-plugshop/django_plugshop-0.1.5-py2.7.egg/plugshop/models/order_products.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils.translation import ugettext as _
+from plugshop.utils import is_default_model
+
+class OrderProductsAbstract(models.Model):
+    class Meta:
+        abstract = True
+        verbose_name = _('order product')
+        verbose_name_plural = _('order product')
+
+    quantity = models.PositiveIntegerField(_('quantity'), 
+                                            blank=False, 
+                                            null=False, 
+                                            default=1)
+
+if is_default_model('ORDER_PRODUCTS'):
+    class OrderProducts(OrderProductsAbstract):
+        class Meta:
+            app_label = 'plugshop'
+            verbose_name = _('order product')
+            verbose_name_plural = _('order product')
