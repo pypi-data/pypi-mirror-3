f001daf0adf2dc078c48abefe490a3c7acd35611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:41 +0000

    Add package hihimann/1.2.0/hihimann-1.2.0.zip

---
+++ packages/hihimann/hihimann-1.2.0.zip/hihimann-1.2.0/hihimann.py
@@ -0,0 +1,25 @@
+# hihimann.py
+"""This is the nester.py" module and it provides one function called print_lol()
+   which prints lists that may or may not include nested lists."""
+   
+def print_lol(the_list, level=0):
+	"""This function takes the_list, which is any python list (of nested lists).
+	   Each item is printed on the screen on its own line. A second argument called
+	   level is used to insert tab-stops when a nested list is encountered."""
+	for each_item in the_list:
+		if isinstance( each_item, list ):
+			print_lol( each_item, level+1 )
+		else:
+			for tab_stop in range( level ):
+				print("\t", end='')
+			print( each_item )
+
+			
+movies = [ "The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+["Graham Chapman",
+["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+print_lol( movies, 0 )
+print_lol( movies )
+print_lol( movies, 2 )
+
---
+++ packages/hihimann/hihimann-1.2.0.zip/hihimann-1.2.0/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+# Setup for hihimann.py and its greetings function
+
+from distutils.core import setup
+
+setup(
+	name		= 'hihimann',
+	version		= '1.2.0',
+	py_modules	= ['hihimann'],
+	author		= 'hihimann',
+	author_email= 'hihimann@hotmail.com',
+	url			= 'http://www.oracle.com',
+	description = 'a simple greeting to check the process',
+	)
+	
+	
