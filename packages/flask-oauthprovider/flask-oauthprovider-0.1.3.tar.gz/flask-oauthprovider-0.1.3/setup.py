27475690ec6e57cabc52108d70f63c6919f8d014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:18 +0000

    Add package flask-oauthprovider/0.1.3/flask-oauthprovider-0.1.3.tar.gz

---
+++ packages/flask-oauthprovider/flask-oauthprovider-0.1.3.tar.gz/flask-oauthprovider-0.1.3/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from os.path import dirname, join
+from setuptools import setup, find_packages
+
+
+def fread(fn):
+    with open(join(dirname(__file__), fn), 'r') as f:
+        return f.read()
+
+tests_require = ['nose', 'unittest2']
+
+requires = ['oauthlib>=0.2.0']
+
+setup(
+    name='flask-oauthprovider',
+    version='0.1.3',
+    description='A full featured and secure OAuth provider base',
+    long_description=fread('README.rst'),
+    author='Ib Lundgren',
+    author_email='ib.lundgren@gmail.com',
+    url='https://github.com/ib-lundgren/flask-oauthprovider',
+    license=fread('LICENSE'),
+    py_modules=['flask_oauthprovider'],
+    test_suite='nose.collector',
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    install_requires=requires,
+    zip_safe=False,
+    include_package_data=True,
+)
