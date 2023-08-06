4941de7eed4b8eece1ee7e5d7923903364667895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:17 +0000

    Add package fedmsg/0.0.3/fedmsg-0.0.3.tar.gz

---
+++ packages/fedmsg/fedmsg-0.0.3.tar.gz/fedmsg-0.0.3/fedmsg/schema.py
@@ -0,0 +1,15 @@
+USER = 'user'       # ?
+TAG = 'tag'         # For fedora-tagger
+LOG = 'log'         # For fedmsg-logger
+UPDATE = 'update'   # For bodhi
+
+# Used only for testing and developing.
+TEST = 'test'
+
+keys = [
+    USER,
+    TAG,
+    LOG,
+
+    TEST,
+]
