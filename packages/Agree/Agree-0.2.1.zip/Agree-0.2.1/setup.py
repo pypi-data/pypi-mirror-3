9af4899e6131ded952d99bbccb204499f3e3725f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:01 +0000

    Add package Agree/0.2.1/Agree-0.2.1.zip

---
+++ packages/Agree/Agree-0.2.1.zip/Agree-0.2.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='Agree',
+    version='0.2.1',
+    author='Conner Bryan',
+    author_email='conner@nodala.com',
+    packages=['agree'],
+    #scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/Agree/',
+    license='LICENSE.txt',
+    description='Simple argument resolution.',
+    long_description=open('README.txt').read(),
+
+)
