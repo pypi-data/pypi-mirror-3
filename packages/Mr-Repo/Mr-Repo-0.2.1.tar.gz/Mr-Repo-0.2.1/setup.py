0b1430c1053002f5b7560f38d2d8881e69933a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:26 +0000

    Add package Mr-Repo/0.2.1/Mr-Repo-0.2.1.tar.gz

---
+++ packages/Mr-Repo/Mr-Repo-0.2.1.tar.gz/Mr-Repo-0.2.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from mr_repo import version
+
+with open('README.rst') as file:
+        long_description = file.read()
+
+setup(name='Mr-Repo',
+      version=version,
+      author='Ryan McGowan',
+      author_email='ryan@ryanmcg.com',
+      description='A very simple repo manager of repos.',
+      long_description=long_description,
+      url='http://pypi.python.org/pypi/Mr-Repo/' + version,
+      py_modules=['mr_repo'],
+      scripts=['mr_repo'],
+      requires=['PyYAML'],
+      classifiers=['Development Status :: 4 - Beta',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2']
+      )
