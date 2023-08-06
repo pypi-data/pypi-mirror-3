9d5f551a90b8e23b2605b628310f6d53db9527aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:29 +0000

    Add package mallory/0.0.3/mallory-0.0.3.tar.gz

---
+++ packages/mallory/mallory-0.0.3.tar.gz/mallory-0.0.3/mallory/heartbeat_handler.py
@@ -0,0 +1,19 @@
+import tornado.web
+import mallory
+
+class HeartbeatHandler(tornado.web.RequestHandler):
+    def initialize(self, circuit_breaker):
+        print "initializing"
+        self.circuit_breaker = circuit_breaker
+
+    @tornado.web.asynchronous
+    @tornado.gen.engine
+    def get(self):
+        if self.circuit_breaker.is_tripped():
+            self.set_status(503)
+        else:
+            self.set_status(200)
+
+        self.write("Mallory " + mallory.Version + "\n")
+        self.write("OK")
+        self.finish()
