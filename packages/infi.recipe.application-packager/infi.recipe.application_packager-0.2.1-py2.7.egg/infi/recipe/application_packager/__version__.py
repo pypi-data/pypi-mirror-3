9047315db705b45512e9e5db0731c5df149fa390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:47 +0000

    Add package infi.recipe.application-packager/0.2.1/infi.recipe.application_packager-0.2.1-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.2.1-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.1"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '442183896671010cfa5c383783adc0e8540702a9'
+__git_head_subject__ = 'Finished release v0.2.1.'
+__git_head_message__ = '* release/v0.2.1:\n  TRIVIAL fix get_os_version_string\n  empty commit after version v0.2'
+__git_dirty_diff__ = ''
