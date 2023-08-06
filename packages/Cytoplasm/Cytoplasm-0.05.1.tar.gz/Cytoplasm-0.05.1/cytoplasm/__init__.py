f3b2da629e024606eb938fd3fb307e8bac6efd93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:03 +0000

    Add package Cytoplasm/0.05.1/Cytoplasm-0.05.1.tar.gz

---
+++ packages/Cytoplasm/Cytoplasm-0.05.1.tar.gz/Cytoplasm-0.05.1/cytoplasm/__init__.py
@@ -0,0 +1,7 @@
+''' Cytoplasm is a static site compiler meant to be used mostly as a blogging
+engine.
+
+See http://cytoplasm.somethingsido.com.
+'''
+
+from cytoplasm.build import build, copy_over
