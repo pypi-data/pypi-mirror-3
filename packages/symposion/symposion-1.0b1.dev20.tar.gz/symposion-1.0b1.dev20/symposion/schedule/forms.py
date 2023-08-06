5f0c07f4c7719771a1a0beec6efefc795a84b817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:48 +0000

    Add package symposion/1.0b1.dev20/symposion-1.0b1.dev20.tar.gz

---
+++ packages/symposion/symposion-1.0b1.dev20.tar.gz/symposion-1.0b1.dev20/symposion/schedule/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from symposion.schedule.models import Presentation
+
+
+class SlotEditForm(forms.Form):
+    
+    presentation = forms.ModelChoiceField(
+        queryset=Presentation.objects.filter(slot__isnull=True),
+        required=True,
+    )
