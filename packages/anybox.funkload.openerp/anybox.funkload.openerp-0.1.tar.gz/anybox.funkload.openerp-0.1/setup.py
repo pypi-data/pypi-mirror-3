7b13f7a2c3ec461d68034372d5e33d2cbaefa0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:06 +0000

    Add package anybox.funkload.openerp/0.1/anybox.funkload.openerp-0.1.tar.gz

---
+++ packages/anybox.funkload.openerp/anybox.funkload.openerp-0.1.tar.gz/anybox.funkload.openerp-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+version = 0.1
+
+setup(
+    name = "anybox.funkload.openerp",
+    version = version,
+    author="Georges Racinet",
+    author_email="gracinet@anybox.fr",
+    description="Base classes for functional and load testing of OpenERP "
+		"with Funkload",
+    license="GPLv3",
+    long_description=open('README.txt').read() + open('CHANGES.txt').read(),
+    url="",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    namespace_packages=['anybox', 'anybox.funkload'],
+    install_requires=['setuptools', 'funkload'],
+    classifiers=[
+	"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+        "Topic :: System :: Benchmark",
+      ],
+    entry_points = {},
+    )
+
+
