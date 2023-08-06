05a2ad06014c82f715286d16012ece03df2000a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:38 +0000

    Add package salt/0.10.2/salt-0.10.2.tar.gz

---
+++ packages/salt/salt-0.10.2.tar.gz/salt-0.10.2/doc/ref/states/all/index.rst
@@ -0,0 +1,37 @@
+.. _all-salt.states:
+
+===========================
+Full list of builtin states
+===========================
+
+.. currentmodule:: salt.states
+
+.. autosummary::
+    :toctree:
+    :template: autosummary.rst.tmpl
+
+    alias
+    cmd
+    cron
+    file
+    gem
+    git
+    group
+    host
+    kmod
+    mount
+    mysql_database
+    mysql_grants
+    mysql_user
+    network
+    pip
+    pkg
+    postgres_database
+    rvm
+    selinux
+    service
+    ssh_auth
+    ssh_known_hosts
+    sysctl
+    user
+    virtualenv
---
+++ packages/salt/salt-0.10.2.tar.gz/salt-0.10.2/salt/returners/mongo_return.py
@@ -0,0 +1,61 @@
+'''
+Return data to a mongodb server
+
+Required python modules: pymongo
+'''
+
+import logging
+
+try:
+    import pymongo
+    has_pymongo = True
+except ImportError:
+    has_pymongo = False
+
+
+log = logging.getLogger(__name__)
+
+__opts__ = {'mongo.db': 'salt',
+            'mongo.host': 'salt',
+            'mongo.password': '',
+            'mongo.port': 27017,
+            'mongo.user': ''}
+
+
+def __virtual__():
+    if not has_pymongo:
+        return False
+    return 'mongo_return'
+
+def _remove_dots(d):
+    output = {}
+    for k, v in d.iteritems():
+        if isinstance(v, dict):
+            v = _remove_dots(v)
+        output[k.replace('.', '-')] = v
+    return output
+
+def returner(ret):
+    '''
+    Return data to a mongodb server
+    '''
+    conn = pymongo.Connection(__opts__['mongo.host'],
+                              __opts__['mongo.port'])
+    db = conn[__opts__['mongo.db']]
+
+    user = __opts__.get('mongo.user')
+    password = __opts__.get('mongo.password')
+
+    if user and password:
+        db.authenticate(user, password)
+
+    col = db[ret['id']]
+    back = {}
+
+    if isinstance(ret['return'], dict):
+        back = _remove_dots(ret['return'])
+    else:
+        back = ret['return']
+
+    log.debug(back)
+    col.insert({ret['jid']: back})
