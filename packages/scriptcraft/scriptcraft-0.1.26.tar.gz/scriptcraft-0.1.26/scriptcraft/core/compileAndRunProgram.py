f84281157e35698622678574b6efe3573da87ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:36 +0000

    Add package scriptcraft/0.1.26/scriptcraft-0.1.26.tar.gz

---
+++ packages/scriptcraft/scriptcraft-0.1.26.tar.gz/scriptcraft-0.1.26/scriptcraft/core/compileAndRunProgram.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import os
+
+from scriptcraft.core.CompilationStatus import CompilationStatus
+from scriptcraft.core.Environment import Environment
+from scriptcraft.core.Program import Program
+from scriptcraft.core.RunStatus import RunStatus
+from scriptcraft.utils import *
+
+
+
+class CompileAndRunProgram(object):
+    def __init__(self, program, input, folder):
+        self.program = program
+        self.input = input
+        self.folder = folder
+        self.env = Environment(folder)
+
+        self.maybe_compilation_status = self._compile()
+        self.maybe_running_status = self._run()
+
+    @ on_error_return((OSError, IOError), None)
+    def _compile(self):
+        if not self._is_compilation_necessary():
+            return None
+        self._clear_environment()
+        self._create_source_file()
+        compilation_status = self._execute_compilation_command()
+        self._copy_binary_if_exists()
+        return compilation_status
+
+    def _is_compilation_necessary(self):
+        binary = ('cache', self.program.sha)
+        return not self.env.exists_file(binary)
+
+    def _create_source_file(self):
+        self.env.create_file(('env', self.program.language.source_file_name),
+                             self.program.code)
+
+    def _execute_compilation_command(self):
+        input = ''
+        folder = 'env'
+        output, error_output, exit_status = \
+            self.env.execute_bash_command(self.program.language.compilation_command,
+                                          input, folder)
+        return CompilationStatus(output, error_output)
+
+    def _copy_binary_if_exists(self):
+        source = ('env', self.program.language.binary_file_name)
+        destination = ('cache', self.program.sha)
+        if self.env.exists_file(source):
+            self.env.copy_file(source, destination)
+
+    def _clear_environment(self):
+        self.env.remove_folder_recursively('env')
+
+    @ on_error_return((OSError, IOError), None)
+    def _run(self):
+        if not self._is_compilation_successful():
+            return None
+        self._clear_environment()
+        self._copy_binary()
+        self._create_source_file()
+        running_status = self._execute_run_command()
+        return running_status
+
+    def _is_compilation_successful(self):
+        return self.env.exists_file(('cache', self.program.sha))
+
+    def _copy_binary(self):
+        source = ('cache', self.program.sha)
+        destination = ('env', self.program.language.binary_file_name)
+        self.env.copy_file(source, destination)
+
+    def _execute_run_command(self):
+        input = self.input
+        folder = 'env'
+        output, error_output, exit_code = \
+            self.env.execute_bash_command(self.program.language.running_command,
+                                          input, folder)
+        return RunStatus(input, output, error_output)
---
+++ packages/scriptcraft/scriptcraft-0.1.26.tar.gz/scriptcraft-0.1.26/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+from scriptcraft.utils import datafile_path
+import os
+
+# that ugly trick solve problem with encoding of README file
+import sys
+reload(sys).setdefaultencoding("UTF-8")
+
+PROJECT_NAME = 'scriptcraft'
+
+def _fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return _fullsplit(head, [tail] + result)
+
+def _find_packages_and_data_files():
+    packages = []
+    data_files = []
+    for dirpath, dirnames, filenames in os.walk(PROJECT_NAME):
+        for i, dirname in enumerate(dirnames):
+            if dirname.startswith('.'): del dirnames[i]
+        if '__init__.py' in filenames:
+            packages.append('.'.join(_fullsplit(dirpath)))
+        elif filenames:
+            data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+    return packages, data_files
+
+if __name__ == "__main__":
+    packages, _ = _find_packages_and_data_files()
+    setup(
+        name=PROJECT_NAME,
+        version='0.1.26',
+        author = "Krzysztof Medrela",
+        author_email = "krzysiumed@gmail.com",
+        description = "Scriptcraft programming game - program your units to fight against other players.",
+        long_description = open(datafile_path('../README.rst'), 'r').read(),
+        license = "GPLv3",
+        keywords = [PROJECT_NAME, 'programming game', 'game'],
+        url = "http://github.com/krzysiumed/scriptcraft",
+        classifiers = [
+            "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+            "Natural Language :: Polish",
+            "Operating System :: Unix",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 2 :: Only",
+            "Topic :: Games/Entertainment",
+        ],
+
+        install_requires = [
+            'PIL==1.1.7',
+        ],
+        packages = packages,
+        include_package_data = True,
+        entry_points = {
+            'gui_scripts': [
+                'scriptcraft = scriptcraft.client:run',
+            ],
+        },
+    )
