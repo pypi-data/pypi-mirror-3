7612c74948645367dc0964fc97b09aa7f656757b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:42 +0000

    Add package python-intercom/0.2.0/python-intercom-0.2.0.tar.gz

---
+++ packages/python-intercom/python-intercom-0.2.0.tar.gz/python-intercom-0.2.0/intercom/__init__.py
@@ -0,0 +1,35 @@
+#
+# Copyright 2012 keyes.ie
+#
+# License: http://jkeyes.mit-license.org/
+#
+""" Intercom API wrapper. """
+
+import functools
+import time
+
+from datetime import datetime
+
+
+def from_timestamp_property(func_to_decorate):
+    @functools.wraps(func_to_decorate)
+    def wrapper(instance):
+        value = func_to_decorate(instance)
+        return datetime.fromtimestamp(value)
+    return wrapper
+
+def to_timestamp_property(func_to_decorate):
+    @functools.wraps(func_to_decorate)
+    def wrapper(instance, value):
+        value = time.mktime(value.timetuple())
+        func_to_decorate(instance, value)
+    return wrapper
+
+
+from .intercom import AuthenticationError
+from .intercom import ResourceNotFound
+from .intercom import ServerError
+from .intercom import Intercom
+from .user import User
+from .message_thread import MessageThread
+from .impression import Impression
