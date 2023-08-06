c109c4e99e8da2c682ac0793c49edddd29d8ae80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:06 +0000

    Add package pw/0.1.5/pw-0.1.5.tar.gz

---
+++ packages/pw/pw-0.1.5.tar.gz/pw-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from os.path import join, dirname, abspath
+
+setup(
+  name = 'pw',
+  version = '0.1.5',
+  description = 'Grep GPG-encrypted YAML password safe.',
+  author = 'Michael Walter',
+  author_email = 'michael.walter@gmail.com',
+  url = 'https://github.com/catch22/pw',
+  scripts = ['scripts/pw'],
+  classifiers = [
+    'Programming Language :: Python',
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'License :: OSI Approved :: MIT License',
+  ],
+  long_description = open(join(dirname(abspath(__file__)), 'README.txt')).read(),
+  install_requires=['PyYAML', 'xerox', 'termcolor'],
+)
