9de197090dd9ee78e489e738668d353e0641d774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:50 +0000

    Add package configtreeview/0.1.32/configtreeview-0.1.32.tar.gz

---
+++ packages/configtreeview/configtreeview-0.1.32.tar.gz/configtreeview-0.1.32/configtreeview/__init__.py
@@ -0,0 +1,6 @@
+'''
+This module contains the ConfigTreeView and the tools necessary to initialize
+a gtk.TreeView, create a data model, and map the data model to rows
+that can be appended to a gtk.TreeModel
+'''
+from configtreeview import ConfigTreeView
---
+++ packages/configtreeview/configtreeview-0.1.32.tar.gz/configtreeview-0.1.32/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+required = ['simplejson']
+try:
+	import pygtk
+	pygtk.require('2.0')
+except (ImportError,AssertionError) as e:
+	print "ImportError!: %s" % e
+	print "Adding 'pygtk >= 2.0.0' to install_requires list"
+	required.append( 'pygtk >= 2.0.0' )
+
+setup(
+	name='configtreeview',
+	version='0.1.32',
+	author='Wesley Hansen',
+	author_email='wes@ridersdiscount.com',
+	packages=find_packages(),
+	url='http://github.com/wrhansen/ConfigTreeView',
+	license='GPLv3',
+	description='An easily configurable GtkTreeView implementation for pygtk.',
+	long_description = open('README.rst').read(),
+	platforms=["POSIX", "Windows"],
+	install_requires=required
+)
