847b01d47d253d87c86b9e42ff2e0e9d9797c428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:46 +0000

    Add package trytond_health_lab/1.4.4/trytond_health_lab-1.4.4.tar.gz

---
+++ packages/trytond_health_lab/trytond_health_lab-1.4.4.tar.gz/trytond_health_lab-1.4.4/__tryton__.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'GNU Health: Laboratory',
+    'version': '1.4.4',
+    'author': 'GNU Solidario',
+    'email': 'health@gnusolidario.org',
+    'website': 'http://health.gnu.org',
+    'depends': [
+        'health',
+    ],
+
+    'name_es_ES': 'GNU Health : Laboratorio',
+    'translation': ['locale/es_ES.po', 'locale/fr_FR.po', 'locale/el.po',
+        'locale/fa.po','locale/it.po'],
+
+    'description': '''
+
+This modules includes lab tests: Values, reports and PoS.
+
+
+''',
+
+    'description_es_ES': '''
+
+Módulo con la funcionalidad de laboratorio.
+
+''',
+
+    'xml': [
+        'health_lab_view.xml',
+        'health_lab_report.xml',
+        'data/health_lab_sequences.xml',
+        'data/lab_test_data.xml',
+        'wizard/create_lab_test.xml',
+        'security/access_rights.xml',
+    ],
+}
