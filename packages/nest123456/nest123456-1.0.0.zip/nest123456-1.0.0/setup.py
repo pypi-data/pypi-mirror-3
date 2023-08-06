65e2adc55c42bae3e9b9d503acc9233332adf52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:40 +0000

    Add package nest123456/1.0.0/nest123456-1.0.0.zip

---
+++ packages/nest123456/nest123456-1.0.0.zip/nest123456-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name='nest123456',
+    version='1.0.0',
+    py_modules=['nest123456'],
+    author='ambitiousank',
+    author_email='gankur14@gmail.com',
+    url='https://facebook.com/ambitiousank',
+    description='A simple printer of nested list',
+    )
