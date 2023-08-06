39d58ebf934628d3855c6d445832eb31ff5ec382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:18 +0000

    Add package Mars24/0.4.1/Mars24-0.4.1.tar.gz

---
+++ packages/Mars24/Mars24-0.4.1.tar.gz/Mars24-0.4.1/README.rst
@@ -0,0 +1,8 @@
+
+===========
+Mars24 Python library
+===========
+
+The marstime package implements the Mars24 algorithm based on the Allison and McEwan(2000) paper. Details can be found at http://www.giss.nasa.gov/tools/mars24/ . This package is provided to aid transition from the old name (Mars24) to the new name (marstime). The package simply installs and imports marstime.
+
+Christopher Lee, 2012
---
+++ packages/Mars24/Mars24-0.4.1.tar.gz/Mars24-0.4.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.4.1'
+
+install_requires = [
+    'marstime'
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='Mars24',
+    version=version,
+    description="Mars24 wrapper to use marstime",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='',
+    author='Christopher Lee',
+    author_email='lee@foldmountain.com',
+    url='',
+    license='BSD',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+)
