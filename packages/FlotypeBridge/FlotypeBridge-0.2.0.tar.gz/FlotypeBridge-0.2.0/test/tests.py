494e23c45909bcf78596f28301b04ec1802770e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:14 +0000

    Add package FlotypeBridge/0.2.0/FlotypeBridge-0.2.0.tar.gz

---
+++ packages/FlotypeBridge/FlotypeBridge-0.2.0.tar.gz/FlotypeBridge-0.2.0/test/tests.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import logging
+from flotype.bridge import Bridge
+
+bridge = Bridge(host='localhost', port=8090, log_level=logging.DEBUG,
+        api_key='abcdefgh', reconnect=True)
+
+
+class Handler(object):
+    def someFn(self, name, message):
+        print(name + ': ' + message)
+
+
+def callback(message):
+    self.lobby.msg(self.name, message)
+
+
+def start_client():
+    handler = Handler()
+
+    someService = bridge.get_service('someService')
+    someService.someFn(1, 1.0, 'foo', True, None, ['foo', 'bar'], {'foo': 'bar'})
+    someService.someFn(handler, callback);
+
+    someChannel = bridge.get_channel('someChannel')
+    someChannel.someFn(1, 1.0, 'foo', True, None, ['foo', 'bar'], {'foo': 'bar'})
+
+    bridge.join_channel('myChannel', handler, callback)
+    bridge.join_channel('myChannel', handler)
+
+    bridge.publish_service('myService', handler, callback)
+    bridge.publish_service('myService', handler)
+
+    bridge.leave_channel('myChannel', handler)
+    bridge.leave_channel('myChannel', handler, callback)
+
+bridge.ready(start_client)
+bridge.connect()
