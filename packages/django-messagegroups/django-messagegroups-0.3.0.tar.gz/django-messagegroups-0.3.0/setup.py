ab97c087c20d9796be7aa2527008e241b80633a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:28 +0000

    Add package django-messagegroups/0.3.0/django-messagegroups-0.3.0.tar.gz

---
+++ packages/django-messagegroups/django-messagegroups-0.3.0.tar.gz/django-messagegroups-0.3.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-messagegroups',
+      version='0.3.0',
+      description='Render grouped messages with the Django messaging framework',
+      author='Factor AG',
+      author_email='webmaster@factor.ch',
+      maintainer='Danilo Bargen',
+      maintainer_email='gezuru@gmail.com',
+      url='https://github.com/FactorAG/django-messagegroups',
+      license='LGPLv3',
+      packages=['messagegroups', 'messagegroups.templatetags'],
+      package_dir={'messagegroups': 'messagegroups'},
+      package_data={'messagegroups': ['templates/*']},
+      platforms=['any'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
+    )
