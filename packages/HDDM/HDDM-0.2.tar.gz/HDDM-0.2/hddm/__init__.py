465c62ad08962253d5d0a7a8100ff392f28fcd68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:10 +0000

    Add package HDDM/0.2/HDDM-0.2.tar.gz

---
+++ packages/HDDM/HDDM-0.2.tar.gz/HDDM-0.2/hddm/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+__docformat__ = 'restructuredtext'
+
+import likelihoods
+import generate
+import utils
+import sandbox
+
+from model import *
+
+import wfpt
+try:
+    import wfpt_switch
+except:
+    pass
+
+from kabuki.utils import load_csv, save_csv
+
+try:
+    from IPython.core.debugger import Tracer; debug_here = Tracer()
+except:
+    def debug_here(): pass
