cc3e87421301f7191e01f5d8161e05e77ad62bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:58 +0000

    Add package django-photoprocessor/0.1.8/django-photoprocessor-0.1.8.tar.gz

---
+++ packages/django-photoprocessor/django-photoprocessor-0.1.8.tar.gz/django-photoprocessor-0.1.8/setup.py
@@ -0,0 +1,37 @@
+#/usr/bin/env python
+import photoprocessor
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+setup(
+    name='django-photoprocessor',
+    version=photoprocessor.__version__,
+    description='Automated image processing for Django.',
+    author='Jason Kraus',
+    author_email='jasonk@cukerinteractive.com',
+    license='BSD',
+    url='http://github.com/cuker/django-photoprocessor/',
+    packages=find_packages(exclude=['tests']),
+    test_suite='tests.setuptest.SetupTestSuite',
+    tests_require=(
+        'pep8==1.3.1',
+        'coverage',
+        'django',
+        'Mock',
+    ),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ]
+)
