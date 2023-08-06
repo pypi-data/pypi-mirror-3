224dc059f4f0629bf6e9d76aa8d611ceced257ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:52 +0000

    Add package ipnext.site.editablefooter/0.1.7/ipnext.site.editablefooter-0.1.7.tar.gz

---
+++ packages/ipnext.site.editablefooter/ipnext.site.editablefooter-0.1.7.tar.gz/ipnext.site.editablefooter-0.1.7/ipnext/site/editablefooter/browser/interfaces.py
@@ -0,0 +1,30 @@
+from zope.interface import Interface
+from zope import schema
+
+from plone.directives import form
+
+from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
+
+from ipnext.site.editablefooter.config import TMPL_YEAR
+from ipnext.site.editablefooter import _
+
+
+class IFooterSettings(form.Schema):
+    """Describes registry records
+    """
+    form.widget(footer=WysiwygFieldWidget)
+    footer = schema.Text(
+        title=_(u"Footer text"),
+        description=_(
+            u'Your footer text. You may also use the template variable %s' % TMPL_YEAR
+        ),
+        required=False,
+    )
+
+class IEditableFooterLayer(Interface):
+    """A layer specific for this add-on product.
+
+    This interface is referred in browserlayers.xml.
+    """
+
+
