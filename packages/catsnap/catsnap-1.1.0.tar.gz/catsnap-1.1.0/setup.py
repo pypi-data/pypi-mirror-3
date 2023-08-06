0211127c2ae9672be47b97080f50e586a4ea5d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:32 +0000

    Add package catsnap/1.1.0/catsnap-1.1.0.tar.gz

---
+++ packages/catsnap/catsnap-1.1.0.tar.gz/catsnap-1.1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name="catsnap",
+      version="1.1.0",
+      description="catalog and store funny pictures",
+      author="Andrew Lorente",
+      author_email="andrew.lorente@gmail.com",
+      url="github.com/andrewlorente/catsnap",
+      packages=['catsnap', 'catsnap.document'],
+      scripts=['scripts/catsnap'],
+      install_requires=[
+          "boto==2.5.2",
+          "requests==0.13.2",
+
+          "mock==0.8",
+          "nose==1.1.2",
+          ],
+      )
