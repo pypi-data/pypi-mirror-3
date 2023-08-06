892d3abe9c0fe24104a0045f0403d19b30f9c9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:39 +0000

    Add package django-missing/0.1.4/django-missing-0.1.4.tar.gz

---
+++ packages/django-missing/django-missing-0.1.4.tar.gz/django-missing-0.1.4/missing/templatetags/lang_tags.py
@@ -0,0 +1,24 @@
+from django import template
+from django.conf import settings
+from django.utils import translation
+
+register = template.Library()
+
+@register.simple_tag
+def translate(string, lang_code):
+    """
+    Translates given string to the specified language.
+
+    This is useful for text you need in some other language than the current language. For example,
+    for links inviting users to switch to their language.
+
+    Sample usage::
+
+        {% translate "Do you understand this?" "de" %}
+    """
+
+    old_lang = translation.get_language()
+    translation.activate(lang_code)
+    translated = unicode(translation.ugettext(string))
+    translation.activate(old_lang)
+    return translated
---
+++ packages/django-missing/django-missing-0.1.4.tar.gz/django-missing-0.1.4/setup.py
@@ -0,0 +1,37 @@
+import os
+
+from setuptools import setup, find_packages
+
+VERSION = '0.1.4'
+
+if __name__ == '__main__':
+    setup(
+        name = 'django-missing',
+        version = VERSION,
+        description = 'Some missing features in Django. Are not missing anymore.',
+        long_description = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read(),
+        author = 'Mitar',
+        author_email = 'mitar.django@tnode.com',
+        url = 'https://bitbucket.org/mitar/django-missing',
+        license = "AGPLv3",
+        packages = find_packages(),
+        package_data = {
+            'missing' : [
+                'static/missing/*',
+            ],
+        },
+        classifiers = [
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU Affero General Public License v3',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Framework :: Django',
+        ],
+        include_package_data = True,
+        zip_safe = False,
+        install_requires = [
+            'django-staticfiles',
+        ],
+    )
