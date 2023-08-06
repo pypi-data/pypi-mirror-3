f255f815d42b3ec1d174719dd4368b3107ff1ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:45 +0000

    Add package plone.folder/1.0.2/plone.folder-1.0.2.zip

---
+++ packages/plone.folder/plone.folder-1.0.2.zip/plone.folder-1.0.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+version = '1.0.2'
+
+setup(name='plone.folder',
+      version=version,
+      description='BTree-based folder implementation with order support',
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "Intended Audience :: Developers",
+          "Intended Audience :: System Administrators",
+          "Intended Audience :: Other Audience",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+        ],
+      keywords='folder btree order',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.folder',
+      license='GPL version 2',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['plone'],
+      include_package_data=True,
+      platforms='Any',
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.memoize',
+      ],
+      extras_require={'test': [
+          'profilehooks',
+          'Products.CMFCore',
+      ]},
+)
