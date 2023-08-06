d27c0de7ccc8c20ff9d018332f49100fd9ef3c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:26 +0000

    Add package colorific/0.2.0/colorific-0.2.0.tar.gz

---
+++ packages/colorific/colorific-0.2.0.tar.gz/colorific-0.2.0/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+#  setup.py
+#  colorific
+#
+
+"""
+Package information for colorific.
+"""
+
+import sys
+
+# check for the supported Python version
+version = tuple(sys.version_info[:2])
+if version != (2, 7):
+    sys.stderr.write('colorific requires Python 2.7 (you have %d.%d)\n' %\
+            version)
+    sys.stderr.flush()
+    sys.exit(1)
+
+import os
+from setuptools import setup
+
+readme = os.path.join(os.path.dirname(__file__), 'README.md')
+
+setup(
+    name='colorific',
+    version='0.2.0',
+    description='Automatic color palette detection',
+    long_description=open(readme).read(),
+    author='Lars Yencken',
+    author_email='lars@yencken.org',
+    url='http://github.com/99designs/colorific',
+    py_modules=['colorific'],
+    install_requires=[
+            'PIL>=1.1.6',
+            'colormath>=1.0.8',
+            'numpy>=1.6.1',
+        ],
+    license='ISC',
+    entry_points={
+        'console_scripts': [
+                'colorific = colorific:main',
+            ],
+        },
+)
