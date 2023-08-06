71854b0a52d0efc20695778c779c116ced227f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:14 +0000

    Add package ftw.colorbox/1.1/ftw.colorbox-1.1.tar.gz

---
+++ packages/ftw.colorbox/ftw.colorbox-1.1.tar.gz/ftw.colorbox-1.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+maintainer = 'Julian Infanger'
+
+tests_require = [
+    'plone.app.testing',
+]
+
+setup(name='ftw.colorbox',
+      version=version,
+      description="An image gallery for Plone using ColorBox",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
+      url='https://github.com/4teamwork/ftw.colorbox',
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
