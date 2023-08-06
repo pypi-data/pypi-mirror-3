ac3be68e11566724a6d3e7faccba88d7112de59c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:00 +0000

    Add package SigmaCommon/0.1.3/SigmaCommon-0.1.3.zip

---
+++ packages/SigmaCommon/SigmaCommon-0.1.3.zip/SigmaCommon-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='SigmaCommon',
+    version='0.1.3',
+    author='J. Stephen Sichina',
+    author_email='Stephen.Sichina@gmail.com',
+    packages=['sigmacommon', 'sigmacommon.test'],
+    scripts=['bin/binLaden.py'],
+    url='http://pypi.python.org/pypi/SigmaCommon/',
+    license=open('README.txt').read(),
+    description='Hey man whasup?',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.1.1",
+        "caldav == 0.1.4",
+    ],
+)
