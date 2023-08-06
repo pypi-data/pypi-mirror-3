de31a1a0dae2dda53ef77f2efd245a162e82f2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:04 +0000

    Add package fragments/1.1.0/fragments-1.1.0-py2.7.egg

---
+++ packages/fragments/fragments-1.1.0-py2.7.egg/fragments/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+__version__ = (1,1,0)
+
+class FragmentsError(Exception): pass
+
+def _iterate_over_files(args, config):
+    if args:
+        seen = set()
+        for a in args:
+            if a not in seen:
+                yield os.path.realpath(a)
+                seen.add(a)
+    else:
+        for f in sorted(config['files']):
+            yield os.path.join(config.root, f)
---
+++ packages/fragments/fragments-1.1.0-py2.7.egg/fragments/__main__.py
@@ -0,0 +1,5 @@
+from fragments.commands import _main
+
+
+if __name__ == '__main__': # pragma: no cover
+    _main()
