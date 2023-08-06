1cd6c5d7da947099d12603cd7ee6ba34cb93a819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:07 +0000

    Add package datastore/0.2.3/datastore-0.2.3.tar.gz

---
+++ packages/datastore/datastore-0.2.3.tar.gz/datastore-0.2.3/datastore/test/test_basic.py
@@ -0,0 +1,318 @@
+
+import unittest
+
+import datastore
+from datastore import Key
+from datastore import Query
+
+
+class TestDatastore(unittest.TestCase):
+
+  def subtest_simple(self, stores, numelems=1000):
+
+    def checkLength(len):
+      try:
+        for sn in stores:
+          self.assertEqual(len(sn), numelems)
+      except TypeError, e:
+        pass
+
+    self.assertTrue(len(stores) > 0)
+
+    pkey = Key('/dfadasfdsafdas/')
+
+    checkLength(0)
+
+    # ensure removing non-existent keys is ok.
+    for value in range(0, numelems):
+      key = pkey.child(value)
+      for sn in stores:
+        self.assertFalse(sn.contains(key))
+        sn.delete(key)
+        self.assertFalse(sn.contains(key))
+
+    checkLength(0)
+
+    # insert numelems elems
+    for value in range(0, numelems):
+      key = pkey.child(value)
+      for sn in stores:
+        self.assertFalse(sn.contains(key))
+        sn.put(key, value)
+        self.assertTrue(sn.contains(key))
+        self.assertEqual(sn.get(key), value)
+
+    # reassure they're all there.
+    checkLength(numelems)
+
+    for value in range(0, numelems):
+      key = pkey.child(value)
+      for sn in stores:
+        self.assertTrue(sn.contains(key))
+        self.assertEqual(sn.get(key), value)
+
+    checkLength(numelems)
+
+    def test_query(query, expected):
+      for sn in stores:
+        try:
+          result = list(sn.query(query))
+          self.assertTrue(len(result) == len(expected))
+          self.assertTrue(all([val in result for val in expected]))
+          #TODO: should order be preserved?
+          # self.assertEqual(result, expected)
+
+        except NotImplementedError:
+          print 'WARNING: %s does not implement query.' % sn
+
+    for sn in stores:
+      try:
+        dsresults = list(stores[0].query(Query(pkey)))
+      except NotImplementedError:
+        dsresults = []
+
+
+    k = pkey
+    n = int(numelems)
+    test_query(Query(k), list(range(0, n))) # make sure everything is there.
+    test_query(Query(k), dsresults[:n])
+    test_query(Query(k, limit=n), dsresults[:n])
+    test_query(Query(k, limit=n/2), dsresults[:n/2])
+    test_query(Query(k, offset=n/2), dsresults[n/2:])
+    test_query(Query(k, offset=n/3, limit=n/3), dsresults[n/3: 2*(n/3)])
+    del k
+    del n
+
+    # change numelems elems
+    for value in range(0, numelems):
+      key = pkey.child(value)
+      for sn in stores:
+        self.assertTrue(sn.contains(key))
+        sn.put(key, value + 1)
+        self.assertTrue(sn.contains(key))
+        self.assertNotEqual(value, sn.get(key))
+        self.assertEqual(value + 1, sn.get(key))
+
+    checkLength(numelems)
+
+    # remove numelems elems
+    for value in range(0, numelems):
+      key = pkey.child(value)
+      for sn in stores:
+        self.assertTrue(sn.contains(key))
+        sn.delete(key)
+        self.assertFalse(sn.contains(key))
+
+    checkLength(0)
+
+
+class TestDictionaryDatastore(TestDatastore):
+
+  def test_dictionary(self):
+
+    s1 = datastore.DictDatastore()
+    s2 = datastore.DictDatastore()
+    s3 = datastore.DictDatastore()
+    stores = [s1, s2, s3]
+
+    self.subtest_simple(stores)
+
+
+class TestLowercaseKeyDatastore(TestDatastore):
+
+  def test_dictionary(self):
+
+    s1 = datastore.LowercaseKeyDatastore(datastore.DictDatastore())
+    s2 = datastore.LowercaseKeyDatastore(datastore.DictDatastore())
+    s3 = datastore.LowercaseKeyDatastore(datastore.DictDatastore())
+    stores = [s1, s2, s3]
+
+    self.subtest_simple(stores)
+
+
+
+class TestDatastoreCollection(TestDatastore):
+
+  def test_tiered(self):
+
+    s1 = datastore.DictDatastore()
+    s2 = datastore.DictDatastore()
+    s3 = datastore.DictDatastore()
+    ts = datastore.TieredDatastore([s1, s2, s3])
+
+    k1 = Key('1')
+    k2 = Key('2')
+    k3 = Key('3')
+
+    s1.put(k1, '1')
+    s2.put(k2, '2')
+    s3.put(k3, '3')
+
+    self.assertTrue(s1.contains(k1))
+    self.assertFalse(s2.contains(k1))
+    self.assertFalse(s3.contains(k1))
+    self.assertTrue(ts.contains(k1))
+
+    self.assertEqual(ts.get(k1), '1')
+    self.assertEqual(s1.get(k1), '1')
+    self.assertFalse(s2.contains(k1))
+    self.assertFalse(s3.contains(k1))
+
+    self.assertFalse(s1.contains(k2))
+    self.assertTrue(s2.contains(k2))
+    self.assertFalse(s3.contains(k2))
+    self.assertTrue(ts.contains(k2))
+
+    self.assertEqual(s2.get(k2), '2')
+    self.assertFalse(s1.contains(k2))
+    self.assertFalse(s3.contains(k2))
+
+    self.assertEqual(ts.get(k2), '2')
+    self.assertEqual(s1.get(k2), '2')
+    self.assertEqual(s2.get(k2), '2')
+    self.assertFalse(s3.contains(k2))
+
+    self.assertFalse(s1.contains(k3))
+    self.assertFalse(s2.contains(k3))
+    self.assertTrue(s3.contains(k3))
+    self.assertTrue(ts.contains(k3))
+
+    self.assertEqual(s3.get(k3), '3')
+    self.assertFalse(s1.contains(k3))
+    self.assertFalse(s2.contains(k3))
+
+    self.assertEqual(ts.get(k3), '3')
+    self.assertEqual(s1.get(k3), '3')
+    self.assertEqual(s2.get(k3), '3')
+    self.assertEqual(s3.get(k3), '3')
+
+    ts.delete(k1)
+    ts.delete(k2)
+    ts.delete(k3)
+
+    self.assertFalse(ts.contains(k1))
+    self.assertFalse(ts.contains(k2))
+    self.assertFalse(ts.contains(k3))
+
+    self.subtest_simple([ts])
+
+  def test_sharded(self, numelems=1000):
+
+    s1 = datastore.DictDatastore()
+    s2 = datastore.DictDatastore()
+    s3 = datastore.DictDatastore()
+    s4 = datastore.DictDatastore()
+    s5 = datastore.DictDatastore()
+    stores = [s1, s2, s3, s4, s5]
+    hash = lambda key: int(key.name) * len(stores) / numelems
+    sharded = datastore.ShardedDatastore(stores, shardingfn=hash)
+    sumlens = lambda stores: sum(map(lambda s: len(s), stores))
+
+    def checkFor(key, value, sharded, shard=None):
+      correct_shard = sharded._stores[hash(key) % len(sharded._stores)]
+
+      for s in sharded._stores:
+        if shard and s == shard:
+          self.assertTrue(s.contains(key))
+          self.assertEqual(s.get(key), value)
+        else:
+          self.assertFalse(s.contains(key))
+
+      if correct_shard == shard:
+        self.assertTrue(sharded.contains(key))
+        self.assertEqual(sharded.get(key), value)
+      else:
+        self.assertFalse(sharded.contains(key))
+
+    self.assertEqual(sumlens(stores), 0)
+    # test all correct.
+    for value in range(0, numelems):
+      key = Key('/fdasfdfdsafdsafdsa/%d' % value)
+      shard = stores[hash(key) % len(stores)]
+      checkFor(key, value, sharded)
+      shard.put(key, value)
+      checkFor(key, value, sharded, shard)
+    self.assertEqual(sumlens(stores), numelems)
+
+    # ensure its in the same spots.
+    for i in range(0, numelems):
+      key = Key('/fdasfdfdsafdsafdsa/%d' % value)
+      shard = stores[hash(key) % len(stores)]
+      checkFor(key, value, sharded, shard)
+      shard.put(key, value)
+      checkFor(key, value, sharded, shard)
+    self.assertEqual(sumlens(stores), numelems)
+
+    # ensure its in the same spots.
+    for value in range(0, numelems):
+      key = Key('/fdasfdfdsafdsafdsa/%d' % value)
+      shard = stores[hash(key) % len(stores)]
+      checkFor(key, value, sharded, shard)
+      sharded.put(key, value)
+      checkFor(key, value, sharded, shard)
+    self.assertEqual(sumlens(stores), numelems)
+
+    # ensure its in the same spots.
+    for value in range(0, numelems):
+      key = Key('/fdasfdfdsafdsafdsa/%d' % value)
+      shard = stores[hash(key) % len(stores)]
+      checkFor(key, value, sharded, shard)
+      if value % 2 == 0:
+        shard.delete(key)
+      else:
+        sharded.delete(key)
+      checkFor(key, value, sharded)
+    self.assertEqual(sumlens(stores), 0)
+
+    # try out adding it to the wrong shards.
+    for value in range(0, numelems):
+      key = Key('/fdasfdfdsafdsafdsa/%d' % value)
+      incorrect_shard = stores[(hash(key) + 1) % len(stores)]
+      checkFor(key, value, sharded)
+      incorrect_shard.put(key, value)
+      checkFor(key, value, sharded, incorrect_shard)
+    self.assertEqual(sumlens(stores), numelems)
+
+    # ensure its in the same spots.
+    for value in range(0, numelems):
+      key = Key('/fdasfdfdsafdsafdsa/%d' % value)
+      incorrect_shard = stores[(hash(key) + 1) % len(stores)]
+      checkFor(key, value, sharded, incorrect_shard)
+      incorrect_shard.put(key, value)
+      checkFor(key, value, sharded, incorrect_shard)
+    self.assertEqual(sumlens(stores), numelems)
+
+    # this wont do anything
+    for value in range(0, numelems):
+      key = Key('/fdasfdfdsafdsafdsa/%d' % value)
+      incorrect_shard = stores[(hash(key) + 1) % len(stores)]
+      checkFor(key, value, sharded, incorrect_shard)
+      sharded.delete(key)
+      checkFor(key, value, sharded, incorrect_shard)
+    self.assertEqual(sumlens(stores), numelems)
+
+    # this will place it correctly.
+    for value in range(0, numelems):
+      key = Key('/fdasfdfdsafdsafdsa/%d' % value)
+      incorrect_shard = stores[(hash(key) + 1) % len(stores)]
+      correct_shard = stores[(hash(key)) % len(stores)]
+      checkFor(key, value, sharded, incorrect_shard)
+      sharded.put(key, value)
+      incorrect_shard.delete(key)
+      checkFor(key, value, sharded, correct_shard)
+    self.assertEqual(sumlens(stores), numelems)
+
+    # this will place it correctly.
+    for value in range(0, numelems):
+      key = Key('/fdasfdfdsafdsafdsa/%d' % value)
+      correct_shard = stores[(hash(key)) % len(stores)]
+      checkFor(key, value, sharded, correct_shard)
+      sharded.delete(key)
+      checkFor(key, value, sharded)
+    self.assertEqual(sumlens(stores), 0)
+
+    self.subtest_simple([sharded])
+
+
+if __name__ == '__main__':
+  unittest.main()
