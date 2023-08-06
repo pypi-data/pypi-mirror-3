445bdbe038f37f8cbac2aef79fcb52fd24e8df75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:03 +0000

    Add package MobileWorks/0.3.04/MobileWorks-0.3.04.tar.gz

---
+++ packages/MobileWorks/MobileWorks-0.3.04.tar.gz/MobileWorks-0.3.04/setup.py
@@ -0,0 +1,48 @@
+from distutils.core import setup
+
+def get_credentials():
+    '''
+    This will read username/password from the user and check if they are correct or not.
+    '''
+    while True:
+        username = raw_input( 'Username: ' )
+        if username:
+            password = raw_input( 'Password: ' )
+            try:
+                check_credentials( username, password )
+                return username, password
+            except Exception, e:
+                print e
+        else:
+            return None, None
+
+def check_credentials( username, password ):
+    mw.username = username
+    mw.password = password
+    mw._authenticate()
+
+import sys
+
+cmd = sys.argv[1]
+
+if cmd == 'install':
+    import mobileworks as mw
+    if not mw._credentials:
+        import sys
+        old_stdout = sys.stdout
+        sys.stdout = sys.__stdout__
+        sys.__stdout__.write( 'Would you please enter your username and password (leave the username empty to skip this step)' )
+        username, password = get_credentials()
+        if username and password:
+            mw.username = username
+            mw.password = password
+            mw._store_credentials()
+        sys.stdout = old_stdout
+
+setup(
+    name = 'MobileWorks',
+    version = '0.3.04',
+    py_modules = ['mobileworks'],
+    url = 'http://www.mobileworks.com/',
+    description = 'A library for the MobileWorks API.',
+)
