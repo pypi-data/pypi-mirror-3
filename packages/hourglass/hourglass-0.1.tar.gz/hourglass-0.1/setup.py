cad42c6fb175401f8bd9987b681b446de03a7ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:35 +0000

    Add package hourglass/0.1/hourglass-0.1.tar.gz

---
+++ packages/hourglass/hourglass-0.1.tar.gz/hourglass-0.1/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='hourglass',
+    version='0.1',
+    description='Scheduling for rq.',
+    long_description=readme,
+    author='Pitchfork Media, Inc.',
+    author_email='dev@pitchfork.com',
+    url='http://github.com/pitchfork/hourglass/',
+    packages=find_packages(),
+    scripts=['bin/hourglass'],
+    install_requires=['rq'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ],
+)
