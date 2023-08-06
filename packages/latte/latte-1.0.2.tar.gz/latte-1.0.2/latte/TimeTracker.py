98923237626493d7ddc8c92011d60d81ebd039df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:02 +0000

    Add package latte/1.0.2/latte-1.0.2.tar.gz

---
+++ packages/latte/latte-1.0.2.tar.gz/latte-1.0.2/latte/TimeTracker.py
@@ -0,0 +1,117 @@
+"""
+
+Time tracking class
+Handles window time logging and log information storage
+
+"""
+import time
+import json
+import os
+
+class TimeTracker(object):
+    """
+
+    Tracks window time and stores window information
+
+    """
+
+    logs = {}
+
+    def __init__(self, configs, categorizer, projectizer):
+        self._configs = configs
+        self._categorizer = categorizer
+        self._projectizer = projectizer
+        # Load category and project configs
+        self._categorizer.load_groups()
+        self._projectizer.load_groups()
+
+    def get_sleep_time(self):
+        """
+
+        Returns sleepTime config
+
+        """
+        return self._configs['sleepTime']
+
+    def get_window_time(self, window):
+        """
+
+        Returns window time information from logs
+
+        """
+        if window in self.logs.keys():
+            return self.logs[window]['time']
+        else:
+            return None
+
+    def get_window_stats(self, window):
+        """
+
+        Returns a dict of window statistics.
+        That includes window time, category and project.
+
+        """
+        if window in self.logs.keys():
+            return self.logs[window]
+        else:
+            return None
+
+    def log(self, window):
+        """
+
+        Logs window time, handles assigning to projects and categories.
+
+        """
+        if not window in self.logs.keys():
+            self.logs[window] = {
+                'time' : 0,
+                'categories' : [],
+                'project' : ''
+            }
+            categories = self._categorizer.assign(True, window)
+            if categories:
+                self.logs[window]['categories'] = [c.get_title() for c in categories]
+            project = self._projectizer.assign(False, window, categories)
+            if project:
+                self.logs[window]['project'] = project.get_title()
+        self.logs[window]['time'] += self.get_sleep_time()
+
+    def get_logs(self):
+        """
+
+        Returns whole logs dict
+
+        """
+
+        return self.logs
+
+    def clear_logs(self):
+        """
+
+        Removes logs, stored in this object
+
+        """
+        self.logs = {}
+
+    def dump_logs(self):
+        """
+
+        Dumps logs to filesystem
+
+        """
+        # Join stats system path and create it if it doesn't exist
+        target_path = os.path.join(self._configs['appPath'],
+                                   self._configs['statsPath'])
+        if not os.path.exists(target_path):
+            os.makedirs(target_path)
+        # Filename for current timestamp
+        filename = str(int(time.time()))
+        file_path = os.path.join(target_path, filename)
+
+        # Write logs in json format to the log file
+        log_file = open(file_path, 'w')
+        log_file.write(json.dumps(self.get_logs(), indent=4))
+        log_file.close()
+
+        self.clear_logs()
+        return filename
---
+++ packages/latte/latte-1.0.2.tar.gz/latte-1.0.2/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Automatic rule based Linux Time Tracker',
+    'author': 'Tautvidas Sipavicius',
+    'author_email': 'flakas@tautvidas.com',
+    'url': 'https://github.com/flakas/Latte',
+    'install_requires': [],
+    'packages': ['latte', 'latte.Categories', 'latte.Projects'],
+    'scripts': ['bin/latte'],
+    'version': '1.0.2',
+    'name': 'latte'
+}
+
+setup(**config)
