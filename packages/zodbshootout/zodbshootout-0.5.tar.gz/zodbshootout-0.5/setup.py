0ec2b3a6b5a469f4f4deb4461390bfdb20c64a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:28 +0000

    Add package zodbshootout/0.5/zodbshootout-0.5.tar.gz

---
+++ packages/zodbshootout/zodbshootout-0.5.tar.gz/zodbshootout-0.5/setup.py
@@ -0,0 +1,54 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""A ZODB performance test"""
+
+version = "0.5"
+
+from setuptools import setup, find_packages
+import os
+import sys
+
+install_requires=[
+    'setuptools',
+    'ZODB3',
+]
+
+if sys.version_info < (2, 6):
+    install_requires.append('multiprocessing')
+
+def read_file(*path):
+    base_dir = os.path.dirname(__file__)
+    return open(os.path.join(base_dir, *tuple(path))).read()
+
+setup(
+    name='zodbshootout',
+    version=version,
+    description=__doc__,
+    long_description=read_file("README.txt"),
+    url='https://github.com/zodb/zodbshootout',
+    keywords='ZODB ZEO RelStorage',
+    author='Shane Hathaway',
+    author_email='shane@hathawaymix.org',
+    license='ZPL',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=[],
+    include_package_data=True,
+    platforms='Any',
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={'console_scripts': [
+        'zodbshootout = zodbshootout.main:main',
+        ]},
+)
