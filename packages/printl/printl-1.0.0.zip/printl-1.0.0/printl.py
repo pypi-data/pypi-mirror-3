6c1b68f535bcaea87698dbc2e8da5f4ae376b675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:09 +0000

    Add package printl/1.0.0/printl-1.0.0.zip

---
+++ packages/printl/printl-1.0.0.zip/printl-1.0.0/printl.py
@@ -0,0 +1,9 @@
+''' print nested list module '''
+
+def print_lol(the_list):
+    ''' print nested list function '''
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
