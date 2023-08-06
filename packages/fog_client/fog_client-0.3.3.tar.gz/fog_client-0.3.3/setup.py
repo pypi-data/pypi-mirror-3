875d4d82013553c2cd8d4ef73a78e826933884cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:23 +0000

    Add package fog_client/0.3.3/fog_client-0.3.3.tar.gz

---
+++ packages/fog_client/fog_client-0.3.3.tar.gz/fog_client-0.3.3/setup.py
@@ -0,0 +1,24 @@
+#from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+setup(
+    name='fog_client',
+    version='0.3.3',
+    author='Carles Gonzalez',
+    packages=find_packages(),
+    py_modules=['fog_lib', 'fog_client'],
+    install_requires=['cuisine', 'requests>=0.13'],
+    dependency_links=[
+        'https://github.com/sebastien/cuisine/zipball/master#egg=cuisine'
+    ],
+    scripts=['fog_client.py'],
+    data_files=[('/etc/init', ['aux/fog_client.conf']),
+                ('/etc', ['aux/fog_client.ini'])],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: System :: Systems Administration'])
