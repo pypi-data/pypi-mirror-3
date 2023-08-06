0a220ae30764e6718df3fc9da81ea1d61239b990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:19 +0000

    Add package MonthDelta/1.0a/MonthDelta-1.0a.tar.gz

---
+++ packages/MonthDelta/MonthDelta-1.0a.tar.gz/MonthDelta-1.0a/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='MonthDelta',
+      version='1.0a',
+      description='date computations with months',
+      long_description=open('README.rst').read(),
+      author='Jess Austin',
+      author_email='jess.austin@gmail.com',
+      url='http://pypi.python.org/pypi/MonthDelta',
+      license='MIT',
+      py_modules=['monthdelta'],
+      provides=['monthdelta'],
+      test_suite='nose.collector',
+      tests_require=['Nose'],
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3'])
