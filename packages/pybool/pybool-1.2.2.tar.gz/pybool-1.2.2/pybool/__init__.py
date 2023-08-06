421a599ed0a13c44cce5465c1306a9b9c9f6e701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:36 +0000

    Add package pybool/1.2.2/pybool-1.2.2.tar.gz

---
+++ packages/pybool/pybool-1.2.2.tar.gz/pybool-1.2.2/pybool/__init__.py
@@ -0,0 +1,25 @@
+#
+# Copyright John Reid 2010, 2011, 2012
+#
+
+
+"""
+pybool
+======
+
+A package to infer Boolean regulatory relationships.
+"""
+
+
+
+__version_major__  = 1
+__version_minor__  = 2
+__version_change__ = 2
+__version_tuple__  = (__version_major__, __version_minor__, __version_change__)
+__version__ = '%d.%d' % (__version_major__, __version_minor__)
+__release__ = '%d.%d.%d' % __version_tuple__
+__svn_revision__ = '$Revision: 3110 $' # auto-magically updated by subversion using keywords property
+
+def version_string():
+    return '%s %s' % (__release__, __svn_revision__)
+
