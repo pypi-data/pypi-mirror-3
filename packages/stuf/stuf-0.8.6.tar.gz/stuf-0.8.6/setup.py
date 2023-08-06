aecdfcbe0e9504531c2972943b6cc46fe7347f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:05 +0000

    Add package stuf/0.8.6/stuf-0.8.6.tar.gz

---
+++ packages/stuf/stuf-0.8.6.tar.gz/stuf-0.8.6/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+'''setup stuf'''
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+install_requires = []
+if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+    install_requires.extend(['ordereddict', 'unittest2'])
+
+setup(
+    name='stuf',
+    version='0.8.6',
+    description='''stuf has attributes''',
+    long_description=open(os.path.join(os.getcwd(), 'README.rst'), 'r').read(),
+    author='L. C. Rees',
+    url='https://bitbucket.org/lcrees/stuf/',
+    author_email='lcrees@gmail.com',
+    license='MIT',
+    packages=['stuf'],
+    test_suite='stuf.tests',
+    zip_safe=False,
+    keywords='dict attribute collection mapping dot notation access bunch',
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+    ],
+)
