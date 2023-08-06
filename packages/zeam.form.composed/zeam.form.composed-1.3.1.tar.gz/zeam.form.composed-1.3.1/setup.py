25a056c52160a71c4ac1f426cc815d650958c37e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:47 +0000

    Add package zeam.form.composed/1.3.1/zeam.form.composed-1.3.1.tar.gz

---
+++ packages/zeam.form.composed/zeam.form.composed-1.3.1.tar.gz/zeam.form.composed-1.3.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.3.1'
+
+tests_require = [
+    'zope.app.wsgi',
+    'zope.testing',
+    'zeam.form.base [test]',
+    ]
+
+setup(name='zeam.form.composed',
+      version=version,
+      description="Composed form support for zeam.form",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='zeam form composed',
+      author='Sylvain Viollon',
+      author_email='thefunny@gmail.com',
+      url='http://pypi.python.org/pypi/zeam.form.composed',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['zeam', 'zeam.form'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'zeam.form.base >= 1.2',
+        'martian',
+        'grokcore.component',
+        'grokcore.view',
+        'megrok.pagetemplate',
+        'grokcore.chameleon',
+        'zope.component',
+        'zope.interface',
+        'zope.publisher',
+        ],
+      tests_require = tests_require,
+      extras_require = {'test': tests_require},
+      )
