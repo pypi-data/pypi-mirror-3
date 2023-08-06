b460e2ea5790d66b4270b0e58a046957614fc81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:48 +0000

    Add package django-dbsettings/0.3/django-dbsettings-0.3.tar.gz

---
+++ packages/django-dbsettings/django-dbsettings-0.3.tar.gz/django-dbsettings-0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+# Dynamically calculate the version based on dbsettings.VERSION
+version_tuple = (0, 3, None)
+if version_tuple[2] is not None:
+    version = "%d.%d_%s" % version_tuple
+else:
+    version = "%d.%d" % version_tuple[:2]
+
+setup(
+    name = 'django-dbsettings',
+    version = version,
+    description = 'Application settings whose values can be updated while a project is up and running.',
+    author = 'Samuel Cormier-Iijima',
+    author_email = 'sciyoshi@gmail.com',
+    maintainer = 'Jacek Tomaszewski',
+    maintainer_email = 'jacek.tomek@gmail.com',
+    url = 'http://github.com/zlorf/django-dbsettings',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD',
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+)
