a7fed6f524e2b2211ea7281ee9f3166f3488e2b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:12 +0000

    Add package django-audited-models/0.2.alpha/django-audited-models-0.2.alpha.tar.gz

---
+++ packages/django-audited-models/django-audited-models-0.2.alpha.tar.gz/django-audited-models-0.2.alpha/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+    name="django-audited-models",
+    version="0.2.alpha",
+    author="Chris Chambers",
+    author_email="magma.chambers@gmail.com",
+    description=(
+        "An abstract base class providing automatic creator/editor/"
+        "datetime_created/datetime_modified fields."
+    ),
+    long_description=open("README.rst").read(),
+    license="BSD",
+    url="https://github.com/chrischambers/django-audited-models",
+    keywords=['django', 'auditing', 'automatic', 'abstract'],
+    packages=[
+        'audited_models',
+        'test_project',
+        'test_project.test_app',
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
