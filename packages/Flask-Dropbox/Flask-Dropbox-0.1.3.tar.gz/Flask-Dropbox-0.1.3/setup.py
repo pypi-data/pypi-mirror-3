4f446a052bd333a4a43c16f5877b4dd6c7cf871e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:11 +0000

    Add package Flask-Dropbox/0.1.3/Flask-Dropbox-0.1.3.tar.gz

---
+++ packages/Flask-Dropbox/Flask-Dropbox-0.1.3.tar.gz/Flask-Dropbox-0.1.3/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import os
+
+from distutils.core import setup
+
+
+DIRNAME = os.path.dirname(__file__)
+
+readme = open(os.path.join(DIRNAME, 'README.rst'), 'r')
+README = readme.read()
+readme.close()
+
+
+setup(
+    name='Flask-Dropbox',
+    version='0.1.3',
+    description='Add support of Dropbox API to the Flask applications.',
+    long_description=README,
+    author='Igor Davydenko',
+    author_email='playpauseandstop@gmail.com',
+    url='https://github.com/playpauseandstop/Flask-Dropbox',
+    install_requires=[
+        'Flask',
+    ],
+    package_data={
+        'flask_dropbox': [
+            'templates/dropbox/*.html'
+        ],
+    },
+    packages=[
+        'flask_dropbox',
+    ],
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+    ],
+    keywords='flask dropbox',
+    license='BSD License',
+)
