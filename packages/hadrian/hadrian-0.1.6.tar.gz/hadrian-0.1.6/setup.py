dbb315a6a862008da5b66ad62999671ad0f5ee95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:57 +0000

    Add package hadrian/0.1.6/hadrian-0.1.6.tar.gz

---
+++ packages/hadrian/hadrian-0.1.6.tar.gz/hadrian-0.1.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+version = '0.1.6'
+
+setup(name='hadrian',
+      version=version,
+      description="A Collection of Django packages and scripts",
+      long_description=open("README.md", "r").read(),
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Environment :: Web Environment",
+          "Intended Audience :: End Users/Desktop",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          ],
+      keywords='',
+      author='Derek Stegelman',
+      author_email='dstegelman@gmail.com',
+      url='http://github.com/dstegelman/hadrian',
+      license='MIT',
+      packages=find_packages(),
+      install_requires = ['fabric',],
+      include_package_data=True,
+      zip_safe=False,
+    )
