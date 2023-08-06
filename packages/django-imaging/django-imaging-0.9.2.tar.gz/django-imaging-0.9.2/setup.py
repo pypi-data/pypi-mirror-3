2dbcdaf02fe85ff9be3cd5cc77ff6032ab517821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:15 +0000

    Add package django-imaging/0.9.2/django-imaging-0.9.2.tar.gz

---
+++ packages/django-imaging/django-imaging-0.9.2.tar.gz/django-imaging-0.9.2/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+        name = "django-imaging",
+        version = "0.9.2",
+        author = "Jakub Nawalaniec",
+        author_email = "pielgrzym@prymityw.pl",
+        description = ("AJAX driven gallery field for django"),
+        license = "GNU GPL v2",
+        keywords = "django",
+        url = "https://github.com/pielgrzym/django-imaging",
+        packages = ['imaging'],
+        long_description = read("OLD_README.txt"),
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Framework :: Django",
+            "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+            ],
+        )
