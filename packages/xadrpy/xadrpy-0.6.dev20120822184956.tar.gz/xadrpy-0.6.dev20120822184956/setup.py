f930b99d02d0b5b5ae227e6486491cb9bdcd57f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:35 +0000

    Add package xadrpy/0.6.dev20120822184956/xadrpy-0.6.dev20120822184956.tar.gz

---
+++ packages/xadrpy/xadrpy-0.6.dev20120822184956.tar.gz/xadrpy-0.6.dev20120822184956/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+import sys
+import codecs
+
+SRC_PATH = os.path.realpath(os.path.join(os.path.dirname(__file__),"src"))
+sys.path.append(SRC_PATH) 
+
+import xadrpy
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='xadrpy',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    include_package_data=True,    
+    version=xadrpy.get_version(),
+    description="Django tool",
+    long_description='''Django and python tool with many useful packages, modules.''',
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords=('django'),
+    author='Csaba Palankai',
+    author_email='csaba.palankai@gmail.com',
+    maintainer = 'Csaba Palankai',
+    maintainer_email = 'csaba.palankai@gmail.com',
+    url='https://github.com/pacsee/xadrpy',
+    license='GNU LGPL',
+    zip_safe=False,
+    scripts=['src/xadrpy/management/xcmd.py'],
+    install_requires=[
+    ],)
