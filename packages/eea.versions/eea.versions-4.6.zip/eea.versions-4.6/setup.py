bb23675a5976e224f00bf6c95f18d132e17181bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:34 +0000

    Add package eea.versions/4.6/eea.versions-4.6.zip

---
+++ packages/eea.versions/eea.versions-4.6.zip/eea.versions-4.6/setup.py
@@ -0,0 +1,39 @@
+"""setup
+"""
+import os
+from os.path import join
+from setuptools import setup, find_packages
+
+name = 'eea.versions'
+path = name.split('.') + ['version.txt']
+version = open(join(*path)).read().strip()
+
+setup(name=name,
+      version=version,
+      description="EEA versions",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='eea, dataservice, data, service',
+      author='Alec Ghica (Eaudeweb), Tiberiu Ichim (Eaudeweb), '
+             'Antonio De Marinis (EEA), European Environment Agency',
+      author_email='webadmin@eea.europa.eu',
+      url='http://svn.eionet.europa.eu/projects/Zope',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['eea'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          #'p4a.common',
+          #'collective.indexing',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
