53ca1e850c25e78db5f58a9ab824d979500f7f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:27 +0000

    Add package mrvaldez/0.1.1/mrvaldez-0.1.1.tar.gz

---
+++ packages/mrvaldez/mrvaldez-0.1.1.tar.gz/mrvaldez-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+  name='mrvaldez',
+  version='0.1.1',
+  py_modules=['mrvaldez'],
+  author='Mark Steve Samson',
+  author_email='contact@marksteve.me',
+  description='Mr. Valdez has no first name',
+  install_requires=[
+    # What requirements?
+    # Mr. Valdez requires nothing
+  ],
+)
+
+
+# You actually need more details if you're going to submit
+# an actual usable package but mine is simple so these would do
+
