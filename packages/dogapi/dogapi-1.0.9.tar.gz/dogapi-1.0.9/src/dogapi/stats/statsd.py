f249282ff86990ac378a77f88a7ec4a305ef4d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:18 +0000

    Add package dogapi/1.0.9/dogapi-1.0.9.tar.gz

---
+++ packages/dogapi/dogapi-1.0.9.tar.gz/dogapi-1.0.9/src/dogapi/stats/statsd.py
@@ -0,0 +1,24 @@
+
+
+import socket
+from random import random
+
+
+class StatsdAggregator(object):
+
+
+    def __init__(self, host='localhost', port=9966):
+        self.host = host
+        self.port = int(port)
+        self.address = (self.host, self.port)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket_sendto = self.socket.sendto
+
+    def add_point(self, metric, tags, timestamp, value, metric_class, sample_rate=1):
+        if sample_rate == 1 or random() < sample_rate:
+            payload = '%s:%s|%s' % (metric, value, metric_class.stats_tag)
+            if sample_rate != 1:
+                payload += '|@%s' % sample_rate
+            if tags:
+                payload += '|#' + ','.join(tags)
+            self.socket_sendto(payload, self.address)
