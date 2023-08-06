609f83c71f5c923c47617645e7abacfc03ec1e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:29 +0000

    Add package bluestocking/0.1.0/bluestocking-0.1.0.tar.gz

---
+++ packages/bluestocking/bluestocking-0.1.0.tar.gz/bluestocking-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bluestocking',
+    version='0.1.0',
+    author_email='bluestocking-dev@googlegroups.com',
+    packages=['bluestocking'],
+    license='GPL',
+    description="An information extraction toolkit built on top of NLTK.",
+    long_description=open('README.md').read(),
+    install_requires=['nltk']
+)
