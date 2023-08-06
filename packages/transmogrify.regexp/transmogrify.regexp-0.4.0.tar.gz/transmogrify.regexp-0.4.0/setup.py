74f6122f5010992103b0b24158abe55298909ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:47 +0000

    Add package transmogrify.regexp/0.4.0/transmogrify.regexp-0.4.0.tar.gz

---
+++ packages/transmogrify.regexp/transmogrify.regexp-0.4.0.tar.gz/transmogrify.regexp-0.4.0/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    description='Modify key values by applying a regular expression',
+    entry_points={
+        'z3c.autoinclude.plugin': 'target = transmogrify',
+    },
+    long_description=open('README.rst').read(),
+    name='transmogrify.regexp',
+    namespace_packages=['transmogrify'],
+    packages=find_packages(),
+    url='https://github.com/aclark4life/transmogrify.regexp',
+    version='0.4.0',
+)
