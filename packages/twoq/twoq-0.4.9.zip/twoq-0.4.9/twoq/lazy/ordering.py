b638e771a617c7283b7b6ffe54e4b107dee8f61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:59 +0000

    Add package twoq/0.4.9/twoq-0.4.9.zip

---
+++ packages/twoq/twoq-0.4.9.zip/twoq-0.4.9/twoq/lazy/ordering.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+'''twoq lazy ordering queues'''
+
+from twoq.ordering import RandomMixin, OrderingMixin, CombineMixin
+
+from twoq.lazy.mixins import AutoResultMixin, ManResultMixin
+
+###############################################################################
+## lazy random queues #######################################################
+###############################################################################
+
+
+class arandomq(AutoResultMixin, RandomMixin):
+
+    '''auto-balanced random queue'''
+
+randomq = arandomq
+
+
+class mrandomq(ManResultMixin, RandomMixin):
+
+    '''manually balanced random queue'''
+
+###############################################################################
+## lazy order queues #####EEE################################################
+###############################################################################
+
+
+class aorderq(AutoResultMixin, OrderingMixin):
+
+    '''auto-balanced order queue'''
+
+orderq = aorderq
+
+
+class morderq(ManResultMixin, OrderingMixin):
+
+    '''manually balanced order queue'''
+
+
+###############################################################################
+## active combination queues #####EEE##########################################
+###############################################################################
+
+
+class acombineq(AutoResultMixin, CombineMixin):
+
+    '''auto-balanced combination queue'''
+
+combineq = acombineq
+
+
+class mcombineq(ManResultMixin, CombineMixin):
+
+    '''manually balanced combination queue'''
