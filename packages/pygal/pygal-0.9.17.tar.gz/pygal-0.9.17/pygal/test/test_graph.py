7a7ba06eb1f94528400fdfbd916354bfe41250c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:45 +0000

    Add package pygal/0.9.17/pygal-0.9.17.tar.gz

---
+++ packages/pygal/pygal-0.9.17.tar.gz/pygal-0.9.17/pygal/test/test_graph.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# This file is part of pygal
+#
+# A python svg graph plotting library
+# Copyright © 2012 Kozea
+#
+# This library is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with pygal. If not, see <http://www.gnu.org/licenses/>.
+import os
+from pygal import Line
+
+
+def test_render_to_file():
+    file_name = '/tmp/test_graph.svg'
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+    line = Line()
+    line.add('Serie 1', [1])
+    line.render_to_file(file_name)
+    with open(file_name) as f:
+        assert 'pygal' in f.read()
+    os.remove(file_name)
