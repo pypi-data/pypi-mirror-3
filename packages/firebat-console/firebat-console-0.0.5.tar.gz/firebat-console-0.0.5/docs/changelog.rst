e789c029eed8fe49084c9d2e4650d7d0f7372812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:33 +0000

    Add package firebat-console/0.0.5/firebat-console-0.0.5.tar.gz

---
+++ packages/firebat-console/firebat-console-0.0.5.tar.gz/firebat-console-0.0.5/docs/changelog.rst
@@ -0,0 +1,15 @@
+=========
+Changelog
+=========
+
+0.5
+===
+
+* daemonized Phantom launcher added
+* Web based report added 
+
+0.1
+===
+
+* Unit tets and TravisCI integration added
+
---
+++ packages/firebat-console/firebat-console-0.0.5.tar.gz/firebat-console-0.0.5/setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+if not hasattr(sys, 'version_info') or sys.version_info < (2, 7, 0, 'final'):
+    raise SystemExit("Firebat requires Python 2.7 or later.")
+
+try:
+        from setuptools import setup
+except ImportError:
+        from distutils.core import setup
+
+from firebat import __version__
+
+install_requirements = [
+    'PyYAML',
+    'jinja2',
+    'progressbar',
+    'simplejson',
+]
+
+with open("README.rst") as f:
+    README = f.read()
+
+with open("docs/changelog.rst") as f:
+    CHANGES = f.read()
+
+setup(
+    name='firebat-console',
+    version=__version__,
+    author='Gregory Komissarov',
+    author_email='gregory.komissarov@gmail.com',
+    description='Console helpers for Phantom load tool',
+    long_description=README + '\n' + CHANGES,
+    license='BSD',
+    url='https://github.com/greggyNapalm/firebat_console',
+    keywords=['phantom', 'firebat'],
+    scripts=[
+        "bin/fire",
+        "bin/daemon_fire",
+        "bin/fire-chart",
+    ],
+    packages=[
+        'firebat',
+        'firebat.console',
+    ],
+    package_data={
+        "firebat": [
+            "result_markdown/js/firebat/*.js",
+            "result_markdown/js/*.js",
+            "result_markdown/img/*.png",
+            "result_markdown/img/*.ico",
+            "result_markdown/css/*.css",
+            "result_markdown/less/*.less",
+            "result_markdown/less/*.less",
+            "result_markdown/*.jinja",
+            ],
+        },
+    zip_safe=False,
+    install_requires=install_requirements,
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        "Topic :: Software Development :: Testing :: Traffic Generation",
+    ],
+)
