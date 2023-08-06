e414144d1fe8d8b34008a1878afe2acf07f30a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:29 +0000

    Add package brzipcode/0.1.2/brzipcode-0.1.2.tar.gz

---
+++ packages/brzipcode/brzipcode-0.1.2.tar.gz/brzipcode-0.1.2/brzipcode/main.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import requests
+import json
+
+SERVICE_URL = 'http://brzipcode.com/api/v1/zipcode.json?q=%(zip_code)s&token=%(token)s'
+
+
+class BRZipCode(object):
+    @classmethod
+    def get(cls, zip_code, token, plain_text=False):
+        req = requests.get(SERVICE_URL % {
+            'zip_code': zip_code,
+            'token': token,
+        })
+
+        if plain_text:
+            return req.content
+        return json.loads(req.content)
