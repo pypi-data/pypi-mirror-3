2600ffdd7b1e2f604895ebd6c6256ac0e78422be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:11 +0000

    Add package aipy/1.1.0/aipy-1.1.0.tar.gz

---
+++ packages/aipy/aipy-1.1.0.tar.gz/aipy-1.1.0/src/__gitlog__.py
@@ -0,0 +1,6 @@
+__gitlog__ = """9b26950
+Changed plot_uv to make chan/time selection apply before delay/fringe transforms.  Can now select plotting range with xlim and ylim parameters instead.  Significant cleanup of code to use modern idioms, etc.  Bugfix to plotting fringe rates for individual channels in setting x axis scale/units.  Also added plot_each parameter to control when waterfall vs xy plots are used, and for which axes.
+--
+Aaron Parsons
+aparsons@astron.berkeley.edu
+2012-03-29 14:46:22 -0700"""
