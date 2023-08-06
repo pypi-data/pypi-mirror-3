99e0a2fc4c65595ddca5b2c30121325de5503064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:05 +0000

    Add package Marrja_Mail/0.2.2/Marrja_Mail-0.2.2.tar.gz

---
+++ packages/Marrja_Mail/Marrja_Mail-0.2.2.tar.gz/Marrja_Mail-0.2.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Marrja_Mail',
+    author='Will Charles',
+    author_email='will@will.yt',
+    version='0.2.2',
+    packages=['marrja_mail',],
+    license='BSD',
+    url='https://github.com/Dotnetwill/marrja_mail/',
+    description='Small wrapper for marrow.mailer to use Jinja2 templates and automatically send html and plain text emails.',
+    long_description= open('README.txt').read(),
+    install_requires=[
+        "Jinja2>=2.6",
+        "marrow.mailer>=4.0.0",
+        ]
+    )
