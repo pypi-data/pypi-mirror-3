32a084f64edc914ebbda66cd469d01c1dabcdd9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:24 +0000

    Add package stuf/0.9.3/stuf-0.9.3.tar.bz2

---
+++ packages/stuf/stuf-0.9.3.tar.bz2/stuf-0.9.3/stuf/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+'''dictionaries with attribute-style access.'''
+
+from stuf.core import defaultstuf, fixedstuf, frozenstuf, orderedstuf, stuf
+
+__version__ = (0, 9, 3)
+
+__all__ = 'defaultstuf fixedstuf frozenstuf orderedstuf stuf'.split()
