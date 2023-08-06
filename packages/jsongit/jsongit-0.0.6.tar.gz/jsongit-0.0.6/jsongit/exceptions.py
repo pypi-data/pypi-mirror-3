41002ef9bb58ba73698408993585342338b464e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:49 +0000

    Add package jsongit/0.0.6/jsongit-0.0.6.tar.gz

---
+++ packages/jsongit/jsongit-0.0.6.tar.gz/jsongit-0.0.6/jsongit/exceptions.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""
+jsongit.exceptions
+"""
+
+class NotJsonError(ValueError):
+    """
+    Raised when an object that cannot be run through :func:`json.dumps` is
+    committed.  Subclasses :exc:`ValueError`.
+    """
+    pass
+
+
+class InvalidKeyError(TypeError):
+    """
+    Raised when a non-string or invalid string is used as a key in a
+    repository.  Subclasses :exc:`TypeError`.
+    """
+    pass
+
+
+class DifferentRepoError(ValueError):
+    """This is raised if a merge is attempted on a :class:`Object` in a
+    different repo.  Subclasses :exc:`ValueError`.
+    """
+    pass
+
+
+class NoGlobalSettingError(RuntimeError):
+    """Raised when the requested global setting does not exist. Subclasses
+    :exc:`RuntimeError`.
+    """
+
+    def __init__(self, name):
+        super(NoGlobalSettingError, self).__init__(
+            "Git on this system has no global setting '%s'" % name)
+
+class StagedDataError(RuntimeError):
+    """Raised when an attempt is made to remove a key that has data staged in
+    the index. Subclasses :exc: RUntimeError
+    """
+    pass
---
+++ packages/jsongit/jsongit-0.0.6.tar.gz/jsongit-0.0.6/jsongit/utils.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+"""
+jsongit.author
+"""
+
+from time import altzone, daylight, timezone
+from time import time as curtime
+from pygit2 import Signature
+import subprocess
+
+from .exceptions import NoGlobalSettingError
+
+def global_config(name):
+    """Find a git --global setting
+
+    :param name: the name of the setting
+    :type name: string
+    :return: the value of the setting
+    :rtype: string
+    :raises: NoGlobalSettingError
+    """
+    # TODO libgit2 provides an interface for this, but pygit2 does not.  Should
+    # patch pygit2 to provide it.  In the interim, we must use subprocess.
+    popen = subprocess.Popen(['git', 'config', '--global', name],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+
+    out, err = popen.communicate()
+    if popen.returncode == 0:
+        return out.rstrip()
+    else:
+        raise NoGlobalSettingError(name)
+
+def signature(name, email, time=None, offset=None):
+    """Convenience method to generate pygit2 signatures.
+
+    :param name: The name in the signature.
+    :type name: string
+    :param email: The email in the signature.
+    :type email: string
+    :param time: (optional) the time for the signature, in UTC seconds.
+        Defaults to current time.
+    :type time: int
+    :param offset:
+        (optional) the time offset for the signature, in minutes.
+        Defaults to the system offset.
+    :type offset: int
+
+    :returns: a signature
+    :rtype: pygit2.Signature
+    """
+    offset = offset or (altzone / 60 if daylight else timezone / 60)
+    time = time or int(curtime())
+    return Signature(name, email, time, offset)
+
+def import_json():
+    try:
+        import simplejson
+        return simplejson
+    except ImportError:
+        import json
+        return json
+
