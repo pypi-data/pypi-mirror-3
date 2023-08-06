500543d188a5843f1b2728a7f0c548e6babcb62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:13 +0000

    Add package PlayingCards/0.1.0/PlayingCards-0.1.0.tar.gz

---
+++ packages/PlayingCards/PlayingCards-0.1.0.tar.gz/PlayingCards-0.1.0/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='PlayingCards',
+    version='0.1.0',
+    author='Paritosh Mathur',
+    author_email='admin@parrymathur.com',
+    packages=['playingcards','playingcards.test'],
+    scripts=['bin/interact.py'],
+    url='',
+    license='LICENSE.txt',
+    description='A module to create card games and playing card simulations.',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+    )
