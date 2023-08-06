891b4e902257c07ab89de06c9823d85396f38d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:03 +0000

    Add package pypstats/1.2/pypstats-1.2.tar.gz

---
+++ packages/pypstats/pypstats-1.2.tar.gz/pypstats-1.2/setup.py
@@ -0,0 +1,34 @@
+import sys
+from distutils.core import setup
+
+with open('README.rst') as inp:
+    long_description = inp.read()
+    
+__version__ = ''
+with open('pypstats.py') as inp:
+    for line in inp:
+        if (line.startswith('__version__')):
+            exec(line.strip())
+            break
+
+setup(
+    name='pypstats',
+    version=__version__,
+    author='Ahmet Bakan',
+    author_email='ahmetbakan at msn dot com',
+    description='Retrieve monthly package download statistics from PyPI',
+    long_description=long_description,
+    url='https://github.com/abakan/pypstats',
+    py_modules=['pypstats'],
+    license='GPLv3',
+    keywords=('monthly, package, download, statistics'),
+    scripts=['pyps'],
+    classifiers=[
+                 'License :: OSI Approved :: GNU General Public License (GPL)',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                ],
+    )
