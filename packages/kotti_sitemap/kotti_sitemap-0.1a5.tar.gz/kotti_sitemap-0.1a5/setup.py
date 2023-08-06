0ad774dcacaa82d8c7c46499e66b7797ed053e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:11 +0000

    Add package kotti_sitemap/0.1a5/kotti_sitemap-0.1a5.tar.gz

---
+++ packages/kotti_sitemap/kotti_sitemap-0.1a5.tar.gz/kotti_sitemap-0.1a5/setup.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+VERSION = '0.1a5'
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+except IOError:
+    README = ''
+try:
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    CHANGES = ''
+
+
+if sys.argv[-1] == 'publish':  # upload to pypi
+    os.system("python setup.py register sdist upload")
+    print "You probably want to also tag the version now:"
+    print "  git tag -a %s -m 'version %s'" % (VERSION, VERSION)
+    print "  git push --tags"
+    sys.exit()
+
+setup(
+    name='kotti_sitemap',
+    version=VERSION,
+    license='http://koansys.com/license.txt',
+
+    install_requires=[
+        'kotti',
+    ],
+
+    description='Kotti Sitemap - an addon to easily create a sitemap for your Kotti project.',
+    long_description='\n\n'.join([README, CHANGES]),
+    keywords='kotti cms pyramid sitemap sqlalchemy',
+
+    author='Koansys, LLC',
+    author_email='info@koansys.com',
+
+    url='https://github.com/koansys/kotti_sitemap',
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    zip_safe=False,
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+    ]
+)
