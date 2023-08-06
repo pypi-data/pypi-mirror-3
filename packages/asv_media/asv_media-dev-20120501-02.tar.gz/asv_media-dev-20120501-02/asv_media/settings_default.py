949ecc3c31e9cfd0f22dda8ef18b2bc8cce9e9d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:28 +0000

    Add package asv_media/dev-20120501-02/asv_media-dev-20120501-02.tar.gz

---
+++ packages/asv_media/asv_media-dev-20120501-02.tar.gz/asv_media-dev-20120501-02/asv_media/settings_default.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.conf import settings as dj_settings
+
+ASV_MEDIA__STATIC_ROOT = dj_settings.STATIC_URL.rstrip('/')
+#---------------------------------------------------------------
+#---------------------------------------------------------------
+ASV_MEDIA__HTML_RENDER_METHOD = 'html'
+ASV_MEDIA__HTML_DOCTYPE  = 'html5'
+ASV_MEDIA__HTML_ENCODING = 'utf-8'
+ASV_MEDIA__HTML_DOCTYPES = {
+    'html5': '<!DOCTYPE HTML>',
+    'html4': '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">',
+    'xhtml': '<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
+}
+ASV_MEDIA__HTML_DOCTYPES['html'] = ASV_MEDIA__HTML_DOCTYPES['html4']
+ASV_MEDIA__HTML_DOCTYPES['xml']  = ASV_MEDIA__HTML_DOCTYPES['xhtml']
+#---------------------------------------------------------------
+#---------------------------------------------------------------
+ASV_MEDIA__JQUERY_LOCATION = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'
+ASV_MEDIA__JQUERY_LOCATION_LOCAL = '{}/lib/jquery-1.7.2.min.js'.format(ASV_MEDIA__STATIC_ROOT)
+ASV_MEDIA__JQUERYUI_LOCATION = 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js'
+ASV_MEDIA__JQUERYUI_LOCATION_LOCAL = '{}/lib/jquery-ui/jquery-ui-1.8.18.custom.min.js'.format(ASV_MEDIA__STATIC_ROOT)
+ASV_MEDIA__JQUERYUI_CSS_LOCATION = 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/themes/redmond/jquery-ui.css'
+ASV_MEDIA__JQUERYUI_CSS_LOCATION_LOCAL = '{}/lib/jquery-ui/css/smoothness/jquery-ui-1.8.18.custom.css'.format(ASV_MEDIA__STATIC_ROOT)
+
+ASV_MEDIA__JQUERY_COOKIE_LOCATION = '{}/lib/jquery.cookie.min.js'.format(ASV_MEDIA__STATIC_ROOT)
+ASV_MEDIA__JQUERY_JSON_LOCATION = '{}/lib/jquery.json.min.js'.format(ASV_MEDIA__STATIC_ROOT)
+ASV_MEDIA__JQUERY_COLORBOX_LOCATION = '{}/lib/colorbox/jquery.colorbox-min.js'.format(ASV_MEDIA__STATIC_ROOT)
+ASV_MEDIA__JQUERY_COLORBOX_CSS_LOCATION = '{}/lib/colorbox/colorbox.css'.format(ASV_MEDIA__STATIC_ROOT)
+ASV_MEDIA__PLUPLOAD_ROOT = '{}/lib/plupload'.format(ASV_MEDIA__STATIC_ROOT)
+#---------------------------------------------------------------
+#---------------------------------------------------------------
+ASV_MEDIA__DEFAULT_MEDIAFILES = {
+        'text/css': (
+            ASV_MEDIA__JQUERYUI_CSS_LOCATION,
+        ),
+        'text/javascript': (
+            ASV_MEDIA__JQUERY_LOCATION,
+            ASV_MEDIA__JQUERYUI_LOCATION,
+            ASV_MEDIA__JQUERY_COOKIE_LOCATION,
+            ASV_MEDIA__JQUERY_JSON_LOCATION,
+        ),
+}
+#---------------------------------------------------------------
+#---------------------------------------------------------------
