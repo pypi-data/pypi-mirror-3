09ee52397ccf4ef6eedc2a787db12b7f5ab0f066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:30 +0000

    Add package triangle.py/1.0.0/triangle.py-1.0.0.zip

---
+++ packages/triangle.py/triangle.py-1.0.0.zip/triangle.py-1.0.0/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+from distutils.core import setup
+
+setup(
+       name         = 'triangle.py',
+       version      = '1.0.0',
+       py_modules   = ['triangle'],
+       author       = 'hfpython',
+       author_email =  'sjs0270@daum.net',
+       url          = 'http://www.solutus.co.kr',
+       description  = '직각삼각형 그리기',
+      )
