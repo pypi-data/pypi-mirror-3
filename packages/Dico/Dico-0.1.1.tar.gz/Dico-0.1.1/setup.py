191b4a038f069dcdcb2f9fb947861fa1cadfa819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:28 +0000

    Add package Dico/0.1.1/Dico-0.1.1.tar.gz

---
+++ packages/Dico/Dico-0.1.1.tar.gz/Dico-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Dico',
+    version='0.1.1',
+    packages=['dico',],
+    license='BSD',
+    long_description="A \"database-agnostic modeling system\".",
+    author='Fabrice Aneche',
+    author_email='akh@nobugware.com',
+    url='https://github.com/akhenakh/dico',
+)
