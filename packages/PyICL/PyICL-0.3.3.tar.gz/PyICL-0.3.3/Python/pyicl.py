cae5480cfc5f015f447d401732f9008cb77f8bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:16 +0000

    Add package PyICL/0.3.3/PyICL-0.3.3.tar.gz

---
+++ packages/PyICL/PyICL-0.3.3.tar.gz/PyICL-0.3.3/Python/pyicl.py
@@ -0,0 +1,22 @@
+#
+# Copyright John Reid 2011, 2012
+#
+
+"""
+Python interface to the Boost Interval Container Library.
+"""
+
+#
+# Load everything in extension module.
+#
+from _pyicl import *
+
+
+
+__major_version__ = 0
+__minor_version__ = 3
+__release_version__ = 3
+__svn_revision__ = '$Revision: 3088 $' # auto-magically updated by subversion using keywords property
+
+__version__ = '%d.%d' % (__major_version__, __minor_version__)
+__release__ = '%d.%d.%d' % (__major_version__, __minor_version__, __release_version__)
