504512a4728b35f01f7b0a5267cb59f1c6cf1fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:41 +0000

    Add package django-subdomains/1.2.0/django-subdomains-1.2.0.tar.gz

---
+++ packages/django-subdomains/django-subdomains-1.2.0.tar.gz/django-subdomains-1.2.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='django-subdomains',
+    version='1.2.0',
+    url='http://github.com/tkaemming/django-subdomains/',
+    author='ted kaemming',
+    author_email='ted@kaemming.com',
+    description="Subdomain tools for the Django framework, including "
+        "subdomain-based URL routing.",
+    packages=('subdomains',),
+    include_package_data=True,
+    install_requires=['setuptools'],
+    zip_safe=False
+)
