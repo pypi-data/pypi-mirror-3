14f196b533a0f67a3237706598108f63b086bbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:16 +0000

    Add package icolor/1.0/icolor-1.0.tar.gz

---
+++ packages/icolor/icolor-1.0.tar.gz/icolor-1.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='icolor',
+      version='1.0',
+      description='Python string interpolation for ANSI color printing',
+      author='Brian M Hunt',
+      author_email='brianmhunt@gmail.com',
+      url='https://github.com/brianmhunt/icolor',
+      py_modules=['icolor'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Terminals'
+          ],
+      )
