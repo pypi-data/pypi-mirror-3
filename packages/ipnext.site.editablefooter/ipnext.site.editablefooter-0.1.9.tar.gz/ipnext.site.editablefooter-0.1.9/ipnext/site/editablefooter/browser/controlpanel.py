6725ca52d6f8d48e5b30042b1a0a122c19ebcad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:58 +0000

    Add package ipnext.site.editablefooter/0.1.9/ipnext.site.editablefooter-0.1.9.tar.gz

---
+++ packages/ipnext.site.editablefooter/ipnext.site.editablefooter-0.1.9.tar.gz/ipnext.site.editablefooter-0.1.9/ipnext/site/editablefooter/browser/controlpanel.py
@@ -0,0 +1,27 @@
+from z3c.form import field
+
+from five import grok
+
+from plone.z3cform import layout
+
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+
+from ipnext.site.editablefooter.browser.interfaces import IFooterSettings
+from ipnext.site.editablefooter import _
+
+
+class FooterControlPanelForm(RegistryEditForm):
+    grok.context(IFooterSettings)
+
+    schema = IFooterSettings
+    label = _(u"Footer control panel")
+
+    #fields = field.Fields(IFooterSettings)
+    def update(self):
+        super(RegistryEditForm, self).update()
+
+
+FooterControlPanelView = layout.wrap_form(
+    FooterControlPanelForm, ControlPanelFormWrapper
+)
