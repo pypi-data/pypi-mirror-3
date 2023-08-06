0e45db6cbeb6d0cb906f67175df7bd2c78f49688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:37 +0000

    Add package gmvault/1.0-beta/gmvault-1.0-beta.tar.gz

---
+++ packages/gmvault/gmvault-1.0-beta.tar.gz/gmvault-1.0-beta/setup.py
@@ -0,0 +1,47 @@
+'''
+    Gmvault: a tool to backup and restore your gmail account.
+    Copyright (C) <2011-2012>  <guillaume Aubert (guillaume dot aubert at gmail do com)>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import os
+from setuptools import setup, find_packages
+version = '1.0-beta'
+README = os.path.join(os.path.dirname(__file__), './README.md')
+if os.path.exists(README):
+	long_description = open(README).read() + 'nn'
+else:
+   long_description = "Gmvault"
+
+setup(name='gmvault',
+      version=version,
+      description=("Tool to backup and restore your Gmail emails at will. http://www.gmvault.org for more info"),
+      long_description=long_description,
+      classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Communications :: Email",
+        "Topic :: Communications :: Email :: Post-Office :: IMAP",
+        ],
+      keywords='gmail, email, backup, gmail backup, imap',
+      author='Guillaume Aubert',
+      author_email='guillaume.aubert@gmail.com',
+      url='http://www.gmvault.org',
+      license='GPLv3',
+      packages=['gmv'],
+      package_dir = {'gmv':'./src/gmv'},
+      scripts=['./etc/scripts/gmvault'],
+      install_requires=['Logbook==0.3', 'IMAPClient==0.8.1','gdata==2.0.17']
+      )
