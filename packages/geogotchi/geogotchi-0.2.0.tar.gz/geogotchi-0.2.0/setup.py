99a0a08a878b30922da3a099bf5325f58a12f681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:45 +0000

    Add package geogotchi/0.2.0/geogotchi-0.2.0.tar.gz

---
+++ packages/geogotchi/geogotchi-0.2.0.tar.gz/geogotchi-0.2.0/setup.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+from setuptools import setup
+
+setup(name="geogotchi",
+      version="0.2.0",
+      description="Library for working with geonames.org services",
+      author="Simon Pantzare",
+      author_email="simon+geogotchi@pewpewlabs.com",
+      url="https://github.com/Memoto/geogotchi/",
+      packages=["geogotchi"],
+      package_dir={
+          "geogotchi": "geogotchi",
+          },
+      license="MIT",
+      platforms="Posix; MacOS X; Windows",
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Topic :: Internet",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          ],
+      install_requires=[
+          'requests>=0.12.1',
+          ])
+
