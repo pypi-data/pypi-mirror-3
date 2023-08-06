3da9c1511e94922c60edce7b6dd25d1841846cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:09 +0000

    Add package corejet.testrunner/1.0.1/corejet.testrunner-1.0.1.zip

---
+++ packages/corejet.testrunner/corejet.testrunner-1.0.1.zip/corejet.testrunner-1.0.1/corejet/testrunner/filesource.py
@@ -0,0 +1,12 @@
+from __future__ import with_statement 
+
+from corejet.core.model import RequirementsCatalogue
+
+def fileSource(path):
+    """Read a file containing a CoreJet XML document
+    """
+    
+    catalogue = RequirementsCatalogue()
+    with open(path) as stream:
+        catalogue.populate(stream)
+    return catalogue
---
+++ packages/corejet.testrunner/corejet.testrunner-1.0.1.zip/corejet.testrunner-1.0.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+version = '1.0.1'
+
+requires = [
+    'setuptools',
+    'lxml',
+    'zope.testrunner',
+    'corejet.core',
+    'corejet.visualization',
+    'zc.recipe.egg',
+    'zope.dottedname',
+]
+
+setup(name='corejet.testrunner',
+      version=version,
+      description="A test runner which can output an XML report compatible "
+                  "with JUnit and Hudson/Jenkins as well as XML and HTML "
+                  "reports compatible with the CoreJet BDD standard",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='hudson jenkins junit xml corejet zope.testing',
+      author='Martin Aspeli',
+      author_email='optilude@gmail.com',
+      url='http://corejet.org',
+      license='ZPL 2.1',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['corejet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      entry_points="""
+      [zc.buildout]
+      default = corejet.testrunner.recipe:TestRunner
+
+      [corejet.repositorysource]
+      file = corejet.testrunner.filesource:fileSource
+      """,
+      )
