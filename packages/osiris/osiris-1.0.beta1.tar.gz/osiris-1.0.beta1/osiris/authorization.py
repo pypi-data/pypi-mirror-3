0923387e072f465911b2740f27e407c161df1088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:32 +0000

    Add package osiris/1.0.beta1/osiris-1.0.beta1.tar.gz

---
+++ packages/osiris/osiris-1.0.beta1.tar.gz/osiris-1.0.beta1/osiris/authorization.py
@@ -0,0 +1,33 @@
+from pyramid.interfaces import IAuthenticationPolicy
+from pyramid.httpexceptions import HTTPInternalServerError
+from osiris.errorhandling import OAuth2ErrorHandler
+from osiris.generator import generate_token
+
+
+def password_authorization(request, username, password, scope, expires_in):
+
+    policy = request.registry.queryUtility(IAuthenticationPolicy)
+    authapi = policy._getAPI(request)
+    credentials = {'login': username, 'password': password}
+
+    identity, headers = authapi.login(credentials)
+
+    if not identity:
+        return OAuth2ErrorHandler.error_unauthorized_client()
+    else:
+        # Create and store token
+        storage = request.registry.osiris_store
+        token = generate_token()
+        stored = storage.store(token, username, scope, expires_in)
+
+        # Issue token
+        if stored:
+            return dict(
+                        access_token=token,
+                        token_type='bearer',
+                        scope=scope,
+                        expires_in=expires_in
+                        )
+        else:
+            # If operation error, return a generic server error
+            return HTTPInternalServerError()
