fb534151c9907d2bc2e2a9796bbacedaff96d4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:04 +0000

    Add package django-easy-pjax/1.0/django-easy-pjax-1.0.zip

---
+++ packages/django-easy-pjax/django-easy-pjax-1.0.zip/django-easy-pjax-1.0/setup.py
@@ -0,0 +1,42 @@
+#-*- coding: utf-8 -*-
+
+from setuptools import setup
+
+version = "1.0"
+
+setup(
+    name = "django-easy-pjax",
+    version = version,
+    description = "Easy PJAX for Django.",
+    license = "BSD",
+
+    author = "Filip Wasilewski",
+    author_email = "en@ig.ma",
+
+    url = "https://github.com/nigma/django-easy-pjax",
+    download_url='https://github.com/nigma/django-easy-pjax/zipball/master',
+
+    long_description = open("README.rst").read(),
+
+    packages = ["easy_pjax"],
+    include_package_data=True,
+
+    tests_require=[
+        "django>=1.3,<1.5",
+    ],
+
+    classifiers = (
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ),
+    zip_safe = False
+)
