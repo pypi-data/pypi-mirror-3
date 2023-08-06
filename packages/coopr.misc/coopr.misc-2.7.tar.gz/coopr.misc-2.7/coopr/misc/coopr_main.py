22238e84fa88c94057fb7850b300c14f63a16c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:44 +0000

    Add package coopr.misc/2.7/coopr.misc-2.7.tar.gz

---
+++ packages/coopr.misc/coopr.misc-2.7.tar.gz/coopr.misc-2.7/coopr/misc/coopr_main.py
@@ -0,0 +1,26 @@
+
+try:
+    import pkg_resources
+    #
+    # Load modules associated with Plugins that are defined in
+    # EGG files.
+    #
+    for entrypoint in pkg_resources.iter_entry_points('coopr.command'):
+        plugin_class = entrypoint.load()
+except Exception, err:
+    from sys import stderr as SE
+    SE.write( "Error loading coopr.command entry points: %s  entrypoint='%s'\n" % (err, entrypoint) )
+
+
+import coopr_parser
+
+
+def main(args=None):
+    parser = coopr_parser.get_parser()
+
+    if args is None:
+        ret = parser.parse_args()
+    else:
+        ret = parser.parse_args(args)
+    ret.func(ret)
+
