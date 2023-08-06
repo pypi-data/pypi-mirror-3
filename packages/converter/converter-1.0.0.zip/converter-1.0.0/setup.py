ff698e58e9addaf91d4ea7b4f7f9dfae6d8ceda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:14 +0000

    Add package converter/1.0.0/converter-1.0.0.zip

---
+++ packages/converter/converter-1.0.0.zip/converter-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+
+    name = 'converter',
+
+    version = '1.0.0',
+
+    py_modules = ['converter'],
+
+    author_email =  'neylonj24@hotmail.com',
+
+    description = ' A Simple Metric Converter', 
+)
