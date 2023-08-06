84d5fe690c5fc9fb044d30ed652f72dff5262b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:40 +0000

    Add package django-ulogin/0.1.5/django-ulogin-0.1.5.tar.gz

---
+++ packages/django-ulogin/django-ulogin-0.1.5.tar.gz/django-ulogin-0.1.5/django_ulogin/models.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+from django_ulogin import settings as s
+
+
+class ULoginUser(models.Model):
+    user = models.ForeignKey(User,
+        related_name='ulogin_users',
+        verbose_name=_('user'))
+    network = models.CharField(_('network'),
+        db_index=True,
+        max_length=255,
+        choices=s.ALLOWED_PROVIDERS)
+    identity = models.URLField(_('identity'),
+        db_index=True,
+        max_length=255)
+    uid = models.CharField(_('uid'),
+        db_index=True,
+        max_length=255)
+    date_created = models.DateTimeField(_('date created'),
+        editable=False,
+        auto_now=True)
+
+    def __unicode__(self):
+        return unicode(self.user)
+
+    @models.permalink
+    def get_delete_url(self):
+        return 'ulogin_identities_delete', [self.pk]
+
+    class Meta:
+        verbose_name = _('ulogin user')
+        verbose_name_plural = _('ulogin users')
+        unique_together = [('network', 'uid')]
