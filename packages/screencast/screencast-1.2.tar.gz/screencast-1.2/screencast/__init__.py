dca47302e46b6ce246d919bd4508f7be75aea9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:33 +0000

    Add package screencast/1.2/screencast-1.2.tar.gz

---
+++ packages/screencast/screencast-1.2.tar.gz/screencast-1.2/screencast/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+This file should open up your web browser, create a new tab, and load
+the website.
+"""
+
+import webbrowser
+
+webbrowser.open_new_tab('http://importscreencast.com')
