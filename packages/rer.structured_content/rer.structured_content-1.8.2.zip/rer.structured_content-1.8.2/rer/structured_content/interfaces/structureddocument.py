22e512dd0b6c33cd3410074d2cbca9d63e8e743a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:29 +0000

    Add package rer.structured_content/1.8.2/rer.structured_content-1.8.2.zip

---
+++ packages/rer.structured_content/rer.structured_content-1.8.2.zip/rer.structured_content-1.8.2/rer/structured_content/interfaces/structureddocument.py
@@ -0,0 +1,6 @@
+from rer.structured_content.interfaces import IStructuredInterface
+
+class IStructuredDocument(IStructuredInterface):
+    """Description of the Example Type"""
+    
+    # -*- schema definition goes here -*-
