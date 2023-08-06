31b0be914dc7b6ba5e4f82f5b0c3d5959149de0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:45 +0000

    Add package jubatus/0.0.1/jubatus-0.0.1.tar.gz

---
+++ packages/jubatus/jubatus-0.0.1.tar.gz/jubatus-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='jubatus',
+    packages=['jubatus'],
+    version='0.0.1',
+    description='online and parallel machine learning framework and library',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Programming Language :: Python :: 2.7",
+        #"Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering"
+        ],
+    keywords=[
+        "machine learning"
+        ],
+    author="Preferred Infrastructure and NTT",
+    author_email="uenishi.kota@lab.ntt.co.jp",
+    url="http://jubat.us",
+    license='LGPL'
+    )
