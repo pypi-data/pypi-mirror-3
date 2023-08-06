c12a87dcca10303526b19058cf5bbc0931638079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:31 +0000

    Add package django-image-helper/0.1.1/django-image-helper-0.1.1.tar.gz

---
+++ packages/django-image-helper/django-image-helper-0.1.1.tar.gz/django-image-helper-0.1.1/example/sample/admin.py
@@ -0,0 +1,21 @@
+
+from django.contrib import admin
+
+from image_helper.fields import SizedImageField
+from image_helper.widgets import AdminImagePreviewWidget
+
+from sample.models import TestModel
+
+class TestModelAdmin(admin.ModelAdmin):
+    list_display = ['preview_thumbnail', 'name']
+
+    def preview_thumbnail(self, obj):
+        return '<img src="{}" />'.format(obj.image.thumbnail.url())
+    preview_thumbnail.allow_tags = True
+
+
+    formfield_overrides = {
+        SizedImageField: {'widget': AdminImagePreviewWidget},
+    }
+
+admin.site.register(TestModel, TestModelAdmin)
