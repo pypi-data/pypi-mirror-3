f4a6aa51820b9acf2bcbe0bf1c41e709bb25c225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:15 +0000

    Add package django-skwissh/0.0.6/django-skwissh-0.0.6.zip

---
+++ packages/django-skwissh/django-skwissh-0.0.6.zip/django-skwissh-0.0.6/skwissh/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from skwissh.settings import patch_settings
+
+###############################################################################
+# Skwissh version
+###############################################################################
+VERSION = (0, 0, 6)  # PEP 386
+__version__ = ".".join([str(x) for x in VERSION])
+
+try:
+    patch_settings()
+except:
+    pass
