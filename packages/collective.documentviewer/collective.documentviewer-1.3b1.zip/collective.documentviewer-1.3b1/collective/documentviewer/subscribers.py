8a6c4a2877602e1135f4c8194823cbaf1a7bcead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:13 +0000

    Add package collective.documentviewer/1.3b1/collective.documentviewer-1.3b1.zip

---
+++ packages/collective.documentviewer/collective.documentviewer-1.3b1.zip/collective.documentviewer-1.3b1/collective/documentviewer/subscribers.py
@@ -0,0 +1,27 @@
+from zope.app.component.hooks import getSite
+from settings import GlobalSettings
+from Products.CMFCore.utils import getToolByName
+from logging import getLogger
+from collective.documentviewer.utils import allowedDocumentType
+from collective.documentviewer.async import queueJob
+
+logger = getLogger('collective.documentviewer')
+
+
+def handle_file_creation(object, event):
+    qi = getToolByName(object, 'portal_quickinstaller')
+    if not qi.isProductInstalled('collective.documentviewer'):
+        return
+
+    site = getSite()
+    gsettings = GlobalSettings(site)
+
+    if not allowedDocumentType(object, gsettings.auto_layout_file_types):
+        return
+
+    auto_layout = gsettings.auto_select_layout
+    if auto_layout and object.getLayout() != 'documentviewer':
+        object.setLayout('documentviewer')
+
+    if object.getLayout() == 'documentviewer' and gsettings.auto_convert:
+        queueJob(object)
---
+++ packages/collective.documentviewer/collective.documentviewer-1.3b1.zip/collective.documentviewer-1.3b1/collective/documentviewer/upgrades.py
@@ -0,0 +1,67 @@
+from zope.app.component.hooks import getSite
+from logging import getLogger
+from Products.CMFCore.utils import getToolByName
+from collective.documentviewer.config import GROUP_VIEW_DISPLAY_TYPES
+from Products.ATContentTypes.interface.file import IFileContent
+from collective.documentviewer.settings import GlobalSettings
+from collective.documentviewer.settings import Settings
+from collective.documentviewer.utils import allowedDocumentType
+from DateTime import DateTime
+from collective.documentviewer.async import queueJob
+
+default_profile = 'profile-collective.documentviewer:default'
+logger = getLogger('collective.documentviewer')
+
+
+def convert_all(context):
+    catalog = getToolByName(context, 'portal_catalog')
+    portal = getSite()
+    gsettings = GlobalSettings(portal)
+    for brain in catalog(object_provides=IFileContent.__identifier__):
+        file = brain.getObject()
+
+        if not allowedDocumentType(file,
+                gsettings.auto_layout_file_types):
+            continue
+
+        # let's not switch to the document viewer view
+        # until the document is converted. The conversion
+        # process will check if the layout is set correctly.
+        if file.getLayout() != 'documentviewer':
+            settings = Settings(file)
+            settings.last_updated = DateTime('1999/01/01').ISO8601()
+            queueJob(file)
+
+
+def upgrade_to_1_1(context):
+    context.runImportStepFromProfile(default_profile, 'controlpanel')
+
+
+def upgrade_to_1_2(context):
+    # run 1.1 upgrade again since we change the control panel again
+    upgrade_to_1_1(context)
+
+    types = getToolByName(context, 'portal_types')
+    old_display = 'dvpdf-album-view'
+
+    logger.info('fixing group view name')
+    for portal_type in GROUP_VIEW_DISPLAY_TYPES:
+        if portal_type in types.objectIds():
+            _type = types[portal_type]
+            methods = list(_type.view_methods)
+            if old_display in methods:
+                methods.remove(old_display)
+            methods.append('dvpdf-group-view')
+            _type.view_methods = tuple(set(methods))
+
+    logger.info('looking for any existing containers with view to fix')
+    catalog = getToolByName(context, 'portal_catalog')
+    for brain in catalog(portal_type=GROUP_VIEW_DISPLAY_TYPES):
+        obj = brain.getObject()
+        if obj.getLayout() == old_display:
+            obj.setLayout('dvpdf-group-view')
+
+    # could be assigned to site root also
+    site = getSite()
+    if site.getLayout() == old_display:
+        site.setLayout('dvpdf-group-view')
