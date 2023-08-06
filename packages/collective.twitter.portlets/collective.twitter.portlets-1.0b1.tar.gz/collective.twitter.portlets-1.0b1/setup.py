92c45b04dac3f05c98879e8dacb3319759f606ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:16 +0000

    Add package collective.twitter.portlets/1.0b1/collective.twitter.portlets-1.0b1.tar.gz

---
+++ packages/collective.twitter.portlets/collective.twitter.portlets-1.0b1.tar.gz/collective.twitter.portlets-1.0b1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b1'
+long_description = open("README.rst").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
+
+setup(name='collective.twitter.portlets',
+      version=version,
+      description="",
+      long_description=long_description,
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone twitter',
+      author='Franco Pellegrini',
+      author_email='frapell@gmail.com',
+      url='https://github.com/collective/collective.twitter.portlets',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['collective', 'collective.twitter'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'collective.prettydate',
+        'collective.twitter.accounts>=1.0.3',
+        'five.grok>=1.2',
+        ],
+      extras_require={
+        'test': ['plone.app.testing'],
+        },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
