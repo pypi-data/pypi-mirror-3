6f2b49caebccb3e2e1685c9a545ccda2100c73a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:27 +0000

    Add package braintree/2.12.3/braintree-2.12.3.tar.gz

---
+++ packages/braintree/braintree-2.12.3.tar.gz/braintree-2.12.3/braintree/add_on.py
@@ -0,0 +1,8 @@
+from braintree.configuration import Configuration
+from braintree.modification import Modification
+
+class AddOn(Modification):
+
+    @staticmethod
+    def all():
+        return Configuration.gateway().add_on.all()
