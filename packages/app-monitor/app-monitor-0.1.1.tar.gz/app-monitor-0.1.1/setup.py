d70271b4e297bf129a13c3a477c82b2761c027e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:55 +0000

    Add package app-monitor/0.1.1/app-monitor-0.1.1.tar.gz

---
+++ packages/app-monitor/app-monitor-0.1.1.tar.gz/app-monitor-0.1.1/setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='app-monitor',
+      version='0.1.1',
+      description='Check script scheduler',
+      long_description =read('README.txt'),
+      author='Anthony Tresontani',
+      author_email='dev.tresontani@gmail.com',
+      packages=['monitor'],
+      include_package_data=True,
+      scripts = ['monitor/run-check.py'],
+      install_requires = ['SQLAlchemy==0.7.6','clint==0.3.1', 'plac==0.9.1']
+     )
