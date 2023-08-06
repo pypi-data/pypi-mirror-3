7ebe1f35380a6d9d44006c70ff814ac8c60e4b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:38 +0000

    Add package ParamUnittest/0.1/ParamUnittest-0.1.zip

---
+++ packages/ParamUnittest/ParamUnittest-0.1.zip/ParamUnittest-0.1/paramunittest.py
@@ -0,0 +1,56 @@
+import unittest
+import collections
+import importlib
+
+__all__ = [
+    'parametrized',
+    'ParametrizedTestCase',
+]
+
+def _process_parameters(parameters_seq):
+    processed_parameters_seq = []
+    for parameters in parameters_seq:
+        if isinstance(parameters, collections.Mapping):
+            processed_parameters_seq.append((tuple(),
+                                             dict(parameters)))
+        elif (len(parameters) == 2
+              and isinstance(parameters[0], collections.Sequence)
+              and isinstance(parameters[1], collections.Mapping)):
+            processed_parameters_seq.append((tuple(parameters[0]),
+                                             dict(parameters[1])))
+        else:
+            processed_parameters_seq.append((tuple(parameters),
+                                             dict()))
+    return processed_parameters_seq
+
+
+def _build_name(name, index):
+    return '%s_%d' % (name, index)
+
+
+class ParametrizedTestCase(unittest.TestCase):
+    def setParameters(self, *args, **kwargs):
+        raise NotImplementedError(
+            ('setParameters must be implemented '
+             'because it receives the parameters.'))
+
+
+def parametrized(*parameters_seq):
+    parameters_seq = _process_parameters(parameters_seq)
+    def magic_module_set_test_case(cls):
+        if not hasattr(cls, 'setParameters'):
+            raise TypeError('%s does not have a setParameters method.' % (
+                cls.__name__, ))
+        module = importlib.import_module(cls.__module__)
+        for index, parameters in enumerate(parameters_seq):
+            name = _build_name(cls.__name__, index)
+            def closing_over(parameters=parameters):
+                def setUp(self):
+                    self.setParameters(*parameters[0], **parameters[1])
+                    cls.setUp(self)
+                return setUp
+            set_up = closing_over()
+            new_class = type(name, (cls, ), {'setUp': set_up})
+            setattr(module, name, new_class)
+        return None # this is explicit!
+    return magic_module_set_test_case
