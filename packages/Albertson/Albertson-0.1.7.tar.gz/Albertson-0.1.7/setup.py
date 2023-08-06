a0d974ebbda2ea8e70fd6905b783c4831beaab21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:05 +0000

    Add package Albertson/0.1.7/Albertson-0.1.7.tar.gz

---
+++ packages/Albertson/Albertson-0.1.7.tar.gz/Albertson-0.1.7/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError, err:
+    from distutils.core import setup
+
+from albertson import VERSION
+
+setup(
+    name='Albertson',
+    version=".".join(map(str, VERSION)),
+    description="Easy to use, scalable, counters powered by Amazon's DynamoDB",
+    author="Sean O'Connor",
+    author_email="sean@focuslab.io",
+    url="https://github.com/FocusLab/Albertson",
+    packages=['albertson', 'albertson.dynamodb_utils'],
+    license="BSD",
+    long_description=open('README.md').read(),
+    install_requires=['boto>=2.2.2'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    test_suite='nose.collector',
+)
