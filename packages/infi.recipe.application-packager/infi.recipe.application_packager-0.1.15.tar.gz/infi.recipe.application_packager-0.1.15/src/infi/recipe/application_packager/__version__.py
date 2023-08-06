5b0f98d71c859ee38218660c5cd6502555738064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:24 +0000

    Add package infi.recipe.application-packager/0.1.15/infi.recipe.application_packager-0.1.15.tar.gz

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.1.15.tar.gz/infi.recipe.application_packager-0.1.15/src/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.15"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'fedf03db971a9279e33ad09957c0820da8652293'
+__git_head_subject__ = 'Finished release v0.1.15.'
+__git_head_message__ = '* release/v0.1.15:\n  TRIVIAL Installer:.create_package, add a missing import\n  TRIVIAL removing unused import\n  empty commit after version v0.1.14'
+__git_dirty_diff__ = ''
