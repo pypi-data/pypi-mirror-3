76e351b7f57d3044e18e103d000898ecc037a833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:18 +0000

    Add package appomaticcore/0.0.13/appomaticcore-0.0.13.tar.gz

---
+++ packages/appomaticcore/appomaticcore-0.0.13.tar.gz/appomaticcore-0.0.13/appomatic/manage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from django.core.management import execute_manager
+import os.path
+import sys
+
+if sys.argv[1] != 'makemessages':
+    os.chdir(os.path.dirname(__file__))
+
+import settings
+
+def main():
+    execute_manager(settings)
+
+if __name__ == "__main__":
+    main()
+
