ae48fc8958576de52882d9e07bfcf81899a3d686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:53 +0000

    Add package slapos.recipe.build/0.10/slapos.recipe.build-0.10.tar.gz

---
+++ packages/slapos.recipe.build/slapos.recipe.build-0.10.tar.gz/slapos.recipe.build-0.10/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = '0.10'
+name = 'slapos.recipe.build'
+long_description = open("README.txt").read() + "\n" + \
+    open("CHANGES.txt").read() + "\n"
+
+# extras_requires are not used because of
+#   https://bugs.launchpad.net/zc.buildout/+bug/85604
+setup(name=name,
+      version=version,
+      description="Flexible software building recipe.",
+      long_description=long_description,
+      classifiers=[
+          "Development Status :: 2 - Pre-Alpha",
+          "Framework :: Buildout :: Recipe",
+          "Programming Language :: Python",
+        ],
+      keywords='slapos recipe',
+      license='GPLv3',
+      namespace_packages=['slapos', 'slapos.recipe'],
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+        'setuptools', # namespaces
+        'zc.buildout', # plays with buildout
+        ],
+      zip_safe=True,
+      entry_points={
+        'zc.buildout': [
+          'default = slapos.recipe.build:Script',
+          'cmmi = slapos.recipe.build:Cmmi',
+          'cpan = slapos.recipe.cpan:Cpan',
+          'download = slapos.recipe.download:Recipe',
+          'download-unpacked = slapos.recipe.downloadunpacked:Recipe',
+          'npm = slapos.recipe.npm:Npm',
+      ]},
+    )
