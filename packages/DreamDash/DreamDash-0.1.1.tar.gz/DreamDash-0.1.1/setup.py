e3d416aba47ca6da5a3e2e49082b2e0138e4f19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:16 +0000

    Add package DreamDash/0.1.1/DreamDash-0.1.1.tar.gz

---
+++ packages/DreamDash/DreamDash-0.1.1.tar.gz/DreamDash-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='DreamDash',
+    version='0.1.1',
+    author='Rajendran',
+    author_email='rajendran@railsfactory.org',
+    scripts=['python_easy_installer.py'],
+    
+)
