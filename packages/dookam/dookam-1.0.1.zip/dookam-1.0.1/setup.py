7d28b33be552423461858177ad540f30984418fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:20 +0000

    Add package dookam/1.0.1/dookam-1.0.1.zip

---
+++ packages/dookam/dookam-1.0.1.zip/dookam-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'dookam',
+        version         = '1.0.1',
+        py_modules      = ['dookam'],
+        author          = 'dookam',
+        author_email    = 'dookyungahn@gmail.com',
+        url             = 'http://www.headfirstlabs.com',
+        description     = 'My first module',
+    )
