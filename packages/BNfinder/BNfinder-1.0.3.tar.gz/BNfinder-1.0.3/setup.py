882ce39da8f6577fb662c9f5868265c191c3a242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:07 +0000

    Add package BNfinder/1.0.3/BNfinder-1.0.3.tar.gz

---
+++ packages/BNfinder/BNfinder-1.0.3.tar.gz/BNfinder-1.0.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name='BNfinder',
+      version='1.0.3',
+      author='Bartek Wilczynski and Norbert Dojer',
+      author_email='bartek@mimuw.edu.pl',
+      url='http://pypi.python.org/pypi/BNfinder/',
+      packages=['BNfinder'],
+      scripts=['bnf','bnc','bnf-cv'],
+      license='GNU GPL v.2',
+      description='Bayes Net Finder: an efficient and exact method for learning bayesian networks',
+      install_requires=[
+        "fpconst",
+        "scipy.stats",
+   ]
+     )
+
+
