030878c5ffcbdd798a89b889e56340ff1371b77c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:27 +0000

    Add package speedyxml/0.3.2/speedyxml-0.3.2.tar.gz

---
+++ packages/speedyxml/speedyxml-0.3.2.tar.gz/speedyxml-0.3.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+from distutils.core import Extension
+
+
+#from distutils.core import setup, Extension
+
+setup(
+	name				=	'speedyxml',
+	version				=	'0.3.2',
+	description			=	'Speedy XML parser for Python',
+	author				=	'kilroy',
+	author_email		=	'kilroy@uni-koblenz.de',
+	license				=	'LGPL',
+	py_modules			=	[],
+	ext_modules			=	[
+		Extension('speedyxml', ['src/speedyxml.c'])
+	],
+	test_suite			=	'tests.test.suite',
+	classifiers			=	[
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: POSIX',
+		'Programming Language :: Python',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: Text Processing :: Markup :: XML',
+		'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+	],
+)
