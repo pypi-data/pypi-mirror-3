8c7e37d335cfa2ee1a8c8cfaae0930a797cc79dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:05 +0000

    Add package plone.app.blocks/1.0/plone.app.blocks-1.0.tar.gz

---
+++ packages/plone.app.blocks/plone.app.blocks-1.0.tar.gz/plone.app.blocks-1.0/setup.py
@@ -0,0 +1,64 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+tests_require = [
+    'plone.testing',
+    'plone.app.testing',
+    'zope.configuration',
+    'transaction',
+    'Products.BTreeFolder2',
+    'zExceptions',
+    ]
+
+setup(
+    name='plone.app.blocks',
+    version=version,
+    description="Implements the in-Plone blocks rendering process",
+    long_description=open("README.rst").read() + "\n" +
+        open(os.path.join("plone", "app", "blocks",
+                "tests", "rendering.txt")).read() + "\n" +
+        open(os.path.join("plone", "app", "blocks",
+                "tests", "esi.txt")).read() + "\n" +
+        open("CHANGELOG.rst").read(),
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    keywords='plone blocks deco',
+    author='Martin Aspeli, Laurence Rowe',
+    author_email='optilude@gmail.com',
+    url='https://github.com/plone/plone.app.blocks',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'lxml',
+        'zope.interface',
+        'zope.component',
+        'zope.publisher',
+        'zope.schema',
+        'zope.site',
+        'zope.i18nmessageid',
+        'repoze.xmliter',
+        'plone.tiles',
+        'plone.subrequest',
+        'plone.resource',
+        'plone.memoize',
+        'plone.registry',
+        'plone.transformchain',
+        'Acquisition',
+        'Products.CMFCore',
+        'Zope2',
+    ],
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    entry_points="""
+        [z3c.autoinclude.plugin]
+        target = plone
+        """,
+    )
