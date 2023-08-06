40e1021558a3b7680ec7f3b884d6b0dd0cd66170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:12 +0000

    Add package collective.portlet.geo/1.0.1/collective.portlet.geo-1.0.1.tar.gz

---
+++ packages/collective.portlet.geo/collective.portlet.geo-1.0.1.tar.gz/collective.portlet.geo-1.0.1/CHANGES.rst
@@ -0,0 +1,16 @@
+Changelog
+=========
+
+1.0.1 (2012-06-06)
+------------------
+
+- Fixed a compatibility issue with Python 2.6.
+
+- Fixed an issue where the geolocation database location would not get
+  a default value.
+
+1.0 (2012-06-06)
+----------------
+
+- Initial public release.
+
---
+++ packages/collective.portlet.geo/collective.portlet.geo-1.0.1.tar.gz/collective.portlet.geo-1.0.1/collective/portlet/geo/setuphandlers.py
@@ -0,0 +1,29 @@
+import random
+
+from zope.component import getUtility
+from zope.component import queryAdapter
+
+from plone.registry.record import Record
+from plone.registry.interfaces import IRegistry
+from plone.registry.interfaces import IPersistentField
+
+from .interfaces import ISettings
+
+
+def create_update_token(context):
+    registry = getUtility(IRegistry)
+
+    for name, value in (
+        ('update_token', u"%016x" % random.getrandbits(128)),
+        ('url', u"http://software77.net/geo-ip?DL=1"),
+        ):
+        field = ISettings[name]
+        persistent_field = queryAdapter(field, IPersistentField)
+        key = ISettings.__identifier__ + "." + field.__name__
+
+        record = registry.records.get(key)
+        if record is None:
+            registry.records[key] = Record(persistent_field, value)
+        else:
+            if not record.value:
+                record.value = value
