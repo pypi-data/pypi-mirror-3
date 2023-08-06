4bdf89f294e871b0815b8561a0012c1642069080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:25 +0000

    Add package redomino.social/1.2/redomino.social-1.2.tar.gz

---
+++ packages/redomino.social/redomino.social-1.2.tar.gz/redomino.social-1.2/setup.py
@@ -0,0 +1,54 @@
+# Authors: Andrea D'Este <andrea.deste@redomino.com>
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+from setuptools import setup, find_packages
+import os
+
+version = '1.2'
+
+setup(name='redomino.social',
+      version=version,
+      description="Enhances sc.social.like with a portlet and others social plugins.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author="Andrea D'Este",
+      author_email="andrea.deste@redomino.com",
+      url='https://github.com/redomino/redomino.social',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['redomino'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'sc.social.like',
+          'plone.browserlayer',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+#      setup_requires=["PasteScript"],
+#      paster_plugins=["ZopeSkel"],
+      )
