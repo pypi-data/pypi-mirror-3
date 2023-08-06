136eaf4792603e1ea00cb4893d45816e38481716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:22 +0000

    Add package saimmutable/0.1/saimmutable-0.1.tar.gz

---
+++ packages/saimmutable/saimmutable-0.1.tar.gz/saimmutable-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = "0.1"
+
+setup(
+    name="saimmutable",
+    version=version,
+    description="SA instrumentation machinery for immutable data models",
+    author="Andrey Popp",
+    author_email="8mayday@gmail.com",
+    license="BSD",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="tests",
+    install_requires=["SQLAlchemy >= 0.7.5"])
