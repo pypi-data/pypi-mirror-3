b8fcc1a341b5ccd7e69c9e5d28ed8e53d9d919a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:43 +0000

    Add package jmbo-competition/0.0.5/jmbo_competition-0.0.5-py2.6.egg

---
+++ packages/jmbo-competition/jmbo_competition-0.0.5-py2.6.egg/competition/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from jmbo.admin import ModelBaseAdmin
+from competition.models import Competition, CompetitionEntry, \
+        CompetitionPreferences
+
+admin.site.register(Competition, ModelBaseAdmin)
+admin.site.register(CompetitionEntry)
+admin.site.register(CompetitionPreferences)
