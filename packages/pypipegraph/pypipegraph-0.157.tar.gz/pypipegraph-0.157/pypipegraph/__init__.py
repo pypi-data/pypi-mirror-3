71e24050eb542fa94a626f0c65a7faf6458d4d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:07 +0000

    Add package pypipegraph/0.157/pypipegraph-0.157.tar.gz

---
+++ packages/pypipegraph/pypipegraph-0.157.tar.gz/pypipegraph-0.157/pypipegraph/__init__.py
@@ -0,0 +1,59 @@
+from .graph import (
+        run_pipegraph,
+        new_pipegraph,
+        forget_job_status,
+        destroy_global_pipegraph
+        )
+from .ppg_exceptions import (
+        RuntimeError,
+        RuntimeException,
+        CycleError,
+        JobContractError,
+        PyPipeGraphError,
+        JobDiedException
+        )
+try:
+    from . import twisted_fork
+    twisted_available = True
+except ImportError:
+    twisted_available = False
+from . import util
+
+from .job import (
+    Job, JobList,
+    FileGeneratingJob, MultiFileGeneratingJob,
+    DataLoadingJob, AttributeLoadingJob,
+    TempFileGeneratingJob,
+    CachedAttributeLoadingJob, CachedDataLoadingJob,
+    PlotJob, CombinedPlotJob,
+        FunctionInvariant, ParameterInvariant,
+        FileTimeInvariant, FileChecksumInvariant,
+    JobGeneratingJob, DependencyInjectionJob,
+    FinalJob, 
+    MemMappedDataLoadingJob,
+        )
+
+assert_uniqueness_of_object = util.assert_uniqueness_of_object
+
+all = [
+        run_pipegraph, new_pipegraph, forget_job_status,
+        destroy_global_pipegraph,
+
+        RuntimeError, RuntimeException, CycleError, JobContractError,
+        PyPipeGraphError, JobDiedException,
+
+        Job, JobList,
+        FileGeneratingJob, MultiFileGeneratingJob,
+        DataLoadingJob, AttributeLoadingJob,
+        TempFileGeneratingJob,
+        CachedAttributeLoadingJob, CachedDataLoadingJob,
+        PlotJob, CombinedPlotJob,
+        FunctionInvariant, ParameterInvariant,
+        FileTimeInvariant, FileChecksumInvariant,
+        JobGeneratingJob, DependencyInjectionJob,
+        FinalJob,
+        MemMappedDataLoadingJob,
+        util
+        ]
+if twisted_available:
+    all.append(twisted_fork)
