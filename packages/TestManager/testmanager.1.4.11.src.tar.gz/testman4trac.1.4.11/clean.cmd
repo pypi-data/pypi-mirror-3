a5846c9d33151087ae0d6130eca6319641c1ca79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:51 +0000

    Add package TestManager/1.4.11/testmanager.1.4.11.src.tar.gz

---
+++ packages/TestManager/testmanager.1.4.11.src.tar.gz/testman4trac.1.4.11/clean.cmd
@@ -0,0 +1,23 @@
+rmdir /s /q bin
+
+cd tracgenericclass\trunk
+rmdir /s /q build 
+rmdir /s /q dist 
+rmdir /s /q TracGenericClass.egg-info
+
+cd ..\..\tracgenericworkflow/trunk
+rmdir /s /q build 
+rmdir /s /q dist 
+rmdir /s /q TracGenericWorkflow.egg-info
+
+cd ..\..\sqlexecutor/trunk
+rmdir /s /q build 
+rmdir /s /q dist 
+rmdir /s /q SQLExecutor.egg-info
+
+cd ..\..\testman4trac/trunk
+rmdir /s /q build 
+rmdir /s /q dist 
+rmdir /s /q TestManager.egg-info
+
+cd ..\..
