844606706df618c6c40c913fd635db6e7922966a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:55 +0000

    Add package couch-named-python/0.1.4dev/couch-named-python-0.1.4dev.tar.gz

---
+++ packages/couch-named-python/couch-named-python-0.1.4dev.tar.gz/couch-named-python-0.1.4dev/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name="couch-named-python",
+    version="0.1.4dev",
+    author="Daniel Richman",
+    author_email="main@danielrichman.co.uk",
+    url="https://github.com/danielrichman/couch-named-python",
+    description="CouchDB view server that executes functions "
+                "on the python path by name",
+    packages=["couch_named_python"],
+    requires=["yaml", "couchdbkit"],
+    license="GNU General Public License Version 3",
+    scripts=["bin/couch-named-python", "bin/cnp-upload"]
+)
