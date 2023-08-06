954a72122ad34d53aa2f40be4bde4974bf64dbd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:40 +0000

    Add package zweb/0.251/zweb-0.251.tar.gz

---
+++ packages/zweb/zweb-0.251.tar.gz/zweb-0.251/zweb/template/project/misc/config/_user/_example.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+import _env
+
+
+
+def pre_config(o):
+#    o.HOST = 'z32e1.tk'
+    pass
+
+def post_config(o):
+    pass
+
---
+++ packages/zweb/zweb-0.251.tar.gz/zweb-0.251/zweb/template/project/misc/once/install.sh
@@ -0,0 +1,6 @@
+PREFIX=$(cd "$(dirname "$0")"; pwd)
+cd $PREFIX/install
+sudo mkdir -p /var/log/supervisor
+python nginx.py
+python supervisord.py 
+sudo supervisorctl reload
