6b9e74f5d9390cfdc90f4597a11625a1ff8fc66b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:59 +0000

    Add package pyserve/0.1.0/pyserve-0.1.0.tar.gz

---
+++ packages/pyserve/pyserve-0.1.0.tar.gz/pyserve-0.1.0/setup.py
@@ -0,0 +1,40 @@
+from os.path import join, dirname
+
+from setuptools import setup, find_packages
+
+
+CURDIR = dirname(__file__)
+NAME = 'pyserve'
+MODULE = __import__(NAME)
+README = join(CURDIR, 'README.rst')
+REQUIREMENTS = open(join(CURDIR, 'requirements.txt')).readlines()
+COMMANDS = ["%s = %s" % item for item in dict(
+        serve='pyserve.main:main'
+).items()]
+
+
+setup(
+    name=NAME,
+    version=MODULE.__version__,
+    license=MODULE.__license__,
+    author=MODULE.__author__,
+    author_email=MODULE.__email__,
+    # url=MODULE.__url__,
+    long_description=open(README).read(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    entry_points={'console_scripts': COMMANDS},
+    # classifiers=[
+        # 'Development Status :: 4 - Beta',
+        # 'Intended Audience :: Developers',
+        # 'Intended Audience :: System Administrators',
+        # 'Natural Language :: Russian',
+        # 'Natural Language :: English',
+        # 'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        # 'Programming Language :: Python',
+        # 'Environment :: Console',
+        # 'Topic :: Software Development :: Code Generators',
+    # ],
+    # test_suite='tests'
+)
