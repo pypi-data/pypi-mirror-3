05a3fbb1069ab29c8186ca81698d10816b319499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:39 +0000

    Add package cjhello/1.0.0/cjhello-1.0.0.tar.gz

---
+++ packages/cjhello/cjhello-1.0.0.tar.gz/cjhello-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'cjhello',
+    version = '1.0.0',
+    py_modules = ['cjhello'],
+    author = 'chenji',
+    author_email = 'cnchenji@gmail.com',
+    url = 'http://pythoncrazy.appspot.com',
+    description = ' a hello test procedure',
+    )
