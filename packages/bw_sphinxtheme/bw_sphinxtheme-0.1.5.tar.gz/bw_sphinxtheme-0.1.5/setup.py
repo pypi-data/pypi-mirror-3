031622342cee4b27b5f22759a3140f3aff965b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:49 +0000

    Add package bw_sphinxtheme/0.1.5/bw_sphinxtheme-0.1.5.tar.gz

---
+++ packages/bw_sphinxtheme/bw_sphinxtheme-0.1.5.tar.gz/bw_sphinxtheme-0.1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+version = "0.1.5"
+
+setup(name="bw_sphinxtheme",
+      version=version,
+      description="Minimalistic sphinx theme in black and white colours",
+      long_description=open("README", "r").read(),
+      author="Andrey Popp",
+      author_email="8mayday@gmail.com",
+      url="http://pypi.python.org/pypi/bw_sphinxtheme",
+      license="BSD3",
+      packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+      include_package_data=True,
+      zip_safe=False)
