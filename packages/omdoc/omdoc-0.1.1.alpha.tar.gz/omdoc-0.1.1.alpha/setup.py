89d16e0c1a6cfe11bf2210bf52ff9d043d629ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:36 +0000

    Add package omdoc/0.1.1.alpha/omdoc-0.1.1.alpha.tar.gz

---
+++ packages/omdoc/omdoc-0.1.1.alpha.tar.gz/omdoc-0.1.1.alpha/setup.py
@@ -0,0 +1,33 @@
+import os
+from distutils.core import setup
+from omdoc import get_version
+
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+
+setup(
+    name='omdoc',
+    version=get_version(),
+    license = 'BSD 3 Clause',
+    description='A lightweight documentation builder.',
+    long_description=open('README.md').read(),
+    author='Obscure Metaphor',
+    url='https://github.com/obscuremetaphor/omdoc',
+    author_email='hello@obscuremetaphor.co.uk',
+    packages=[
+        'omdoc',
+    ],
+    classifiers=[
+        'Topic :: Documentation',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Documentation',
+    ],
+    scripts=[
+        'bin/omdoc'
+    ],
+    install_requires = [
+        'jinja2',
+        'beautifulsoup4',
+        'markdown']
+)
