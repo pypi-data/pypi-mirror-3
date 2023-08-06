388012aeb1da015d19b42807d7db606b99373461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:38 +0000

    Add package tetst1/1.2.0/tetst1-1.2.0.zip

---
+++ packages/tetst1/tetst1-1.2.0.zip/tetst1-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'tetst1',
+    version = '1.2.0',
+    py_modules = ['test1'],
+    author = 'kclee',
+    author_email = 'kclee76@bridgetec.co.kr',
+    url = 'http://www.bridgetec.co.kr',
+    description = 'first my module..',
+    )
---
+++ packages/tetst1/tetst1-1.2.0.zip/tetst1-1.2.0/test1.py
@@ -0,0 +1,13 @@
+""" 주석달기"""
+"""주석달기 2
+    ""..석 달기 3""
+	"""
+# 주석달기4
+def print_lol(the_list, level=0):
+	for a in the_list:
+		if isinstance(a,list):
+			print_lol(a,level+1)
+		else:
+			for tabs in range(level):
+				print("\t",end='')
+			print(a)
