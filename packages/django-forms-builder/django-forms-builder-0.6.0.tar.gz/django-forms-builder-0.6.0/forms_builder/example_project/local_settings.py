e6951299c862c0267979e1220d818236116e3776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:03 +0000

    Add package django-forms-builder/0.6.0/django-forms-builder-0.6.0.tar.gz

---
+++ packages/django-forms-builder/django-forms-builder-0.6.0.tar.gz/django-forms-builder-0.6.0/forms_builder/example_project/local_settings.py
@@ -0,0 +1,9 @@
+SECRET_KEY = "asrgw45ergW%GW#%GASDGSDFGdsfgsdf"
+DEBUG = True
+SERVER_EMAIL = 'donotreply@sheerethic.com'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = SERVER_EMAIL
+EMAIL_HOST_PASSWORD = 'sdf$^df2xssd'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
