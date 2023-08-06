c0530daa462d85971757081fbabb004367e93e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:23 +0000

    Add package verlib/0.1/verlib-0.1.tar.gz

---
+++ packages/verlib/verlib-0.1.tar.gz/verlib-0.1/README.txt
@@ -0,0 +1,8 @@
+======
+verlib
+======
+
+This library is the implementation of PEP 386
+
+See : http://www.python.org/dev/peps/pep-0386/
+
---
+++ packages/verlib/verlib-0.1.tar.gz/verlib-0.1/setup.py
@@ -0,0 +1,24 @@
+#-*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name='verlib',
+    version='0.1',
+    py_modules=['verlib'],
+    description='A new version comparison tool for distutils.',
+    author='Tarek Ziadé',
+    author_email='tarek@ziade.org',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development :: Libraries',
+    ],
+    url='https://bitbucket.org/tarek/distutilsversion'
+)
