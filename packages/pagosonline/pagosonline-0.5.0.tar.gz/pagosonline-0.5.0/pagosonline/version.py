d24790f22da604fcd0c00afa79ec6e5a18fe7b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:32 +0000

    Add package pagosonline/0.5.0/pagosonline-0.5.0.tar.gz

---
+++ packages/pagosonline/pagosonline-0.5.0.tar.gz/pagosonline-0.5.0/pagosonline/version.py
@@ -0,0 +1,9 @@
+# THIS FILE IS GENERATED FROM setup.py in pagosonline
+short_version = '0.5.0'
+version = '0.5.0'
+full_version = '0.5.0'
+git_revision = 'add505a163d7a3f125a512697222bdc714e1b810'
+release = True
+
+if not release:
+    version = full_version
