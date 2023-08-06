129ba1da959576c902e3d339e34a66c8ff6e805f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:43 +0000

    Add package grabbag/0.12.1/grabbag-0.12.1.tar.gz

---
+++ packages/grabbag/grabbag-0.12.1.tar.gz/grabbag-0.12.1/grabbag/cast.py
@@ -0,0 +1,12 @@
+def nint(val):
+    if val is None: return None
+    return int(val)
+
+def nstr(val):
+    if val is None: return None
+    return str(val)
+
+def nbool(val):
+    if val is None: return None
+    return bool(val)
+
