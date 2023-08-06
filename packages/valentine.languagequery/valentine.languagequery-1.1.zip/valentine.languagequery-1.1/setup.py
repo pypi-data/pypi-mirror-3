3e26324b79b206ad14d4e7a3c5811fe35738fda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:18 +0000

    Add package valentine.languagequery/1.1/valentine.languagequery-1.1.zip

---
+++ packages/valentine.languagequery/valentine.languagequery-1.1.zip/valentine.languagequery-1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+
+setup(name='valentine.languagequery',
+      version=version,
+      description="Allows you to query for all languages if you are logged into Plone CMS.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://github.com/collective/valentine.languagequery',
+      license='[GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['valentine'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
---
+++ packages/valentine.languagequery/valentine.languagequery-1.1.zip/valentine.languagequery-1.1/valentine/languagequery/patches.py
@@ -0,0 +1,42 @@
+from Products.LinguaPlone import patches
+
+_enabled = []
+
+
+def AlreadyApplied(patch):
+    if patch in _enabled:
+        return True
+    _enabled.append(patch)
+    return False
+
+
+def QueryAllLanguagesForAuthenticatedCatalog():
+    # Patches the catalog tool to filter languages
+    if AlreadyApplied('QueryAllLanguagesForAuthenticatedCatalog'):
+        return
+
+    from Products.CMFPlone.CatalogTool import CatalogTool
+    
+    def searchResults(self, REQUEST=None, **kw):
+        """ This version returns the results for all languages if
+            the user is authenticated.
+        """
+
+        if hasattr(self, 'REQUEST'):
+            langtool = self.REQUEST.get('LANGUAGE_TOOL', None)
+            if langtool is not None:
+                if not langtool.tool.isAnonymousUser():
+                    if kw and not kw.has_key('Language'):
+                        kw['Language'] = 'all'
+                    elif REQUEST is not None and not REQUEST.has_key('Language') and not kw:
+                        REQUEST['Language'] = 'all'
+
+                        
+
+        return self.__valentine_old_searchResults(REQUEST, **kw)
+
+    CatalogTool.__valentine_old_searchResults = CatalogTool.searchResults
+    CatalogTool.searchResults = searchResults
+    CatalogTool.__call__ = searchResults
+
+QueryAllLanguagesForAuthenticatedCatalog()
