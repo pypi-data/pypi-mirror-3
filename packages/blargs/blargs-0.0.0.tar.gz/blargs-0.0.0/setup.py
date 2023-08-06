17a0dbd4fe9885347a25507b37f19e5f402ed13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:47 +0000

    Add package blargs/0.0.0/blargs-0.0.0.tar.gz

---
+++ packages/blargs/blargs-0.0.0.tar.gz/blargs-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from version import getversion
+
+from distutils.core import setup
+
+setup(name='blargs',
+      version=getversion(True),
+      py_modules=['blargs'],
+      description='Blargs command line parser',
+      author='Karl Gyllstrom',
+      author_email='karl.gyllstrom+blargs@gmail.com',
+      url='https://bitbucket.org/gyllstromk/blargs',
+     )
+
+# deploy
+# export LC_ALL='en.UTF-8'
+# python setup.py sdist upload
