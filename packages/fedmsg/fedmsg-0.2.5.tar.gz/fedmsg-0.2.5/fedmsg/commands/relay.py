20e2c6ae35d49a8e80b873042e8fd6c21892e74c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:12 +0000

    Add package fedmsg/0.2.5/fedmsg-0.2.5.tar.gz

---
+++ packages/fedmsg/fedmsg-0.2.5.tar.gz/fedmsg-0.2.5/fedmsg/commands/relay.py
@@ -0,0 +1,34 @@
+""" A SUB.bind()->PUB.bind() relay.
+
+This works by flipping a special boolean in the config that enables
+fedmsg.consumers.relay:RelayConsumer to run before starting up an
+instance of the fedmsg-hub.
+
+"""
+
+import fedmsg
+from fedmsg.commands import command
+from fedmsg.consumers.relay import RelayConsumer
+from fedmsg.producers.heartbeat import HeartbeatProducer
+
+extra_args = []
+
+
+@command(name="fedmsg-relay", extra_args=extra_args, daemonizable=True)
+def relay(**kw):
+    """ Relay connections from active loggers to the bus. """
+
+    # Do just like in fedmsg.commands.hub and mangle fedmsg-config.py to work
+    # with moksha's expected configuration.
+    moksha_options = dict(
+        zmq_publish_endpoints=",".join(kw['endpoints']["relay_outbound"]),
+        zmq_subscribe_endpoints=kw['relay_inbound'],
+        zmq_subscribe_method="bind",
+    )
+    kw.update(moksha_options)
+
+    # Flip the special bit that allows the RelayConsumer to run
+    kw['fedmsg.consumers.relay.enabled'] = True
+
+    from moksha.hub import main
+    main(options=kw, consumers=[RelayConsumer], producers=[HeartbeatProducer])
