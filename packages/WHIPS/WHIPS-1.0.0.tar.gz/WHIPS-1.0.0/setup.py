38a24d27636ff61cd6208dfdafa2dca78a9e1e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:38 +0000

    Add package WHIPS/1.0.0/WHIPS-1.0.0.tar.gz

---
+++ packages/WHIPS/WHIPS-1.0.0.tar.gz/WHIPS-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+REQUIRES = ['numpy', 'shapely', 'pyproj', 'tables', 'netCDF4', 'pyhdf']
+
+setup(name='WHIPS',
+      version='1.0.0',
+      install_requires = REQUIRES,
+      description='Scripts for customized regridding of Level-2 data to Level-3 data',
+      long_description=open('reST.txt').read(),
+      license = 'LICENSE.txt',
+      author='Jacob Oberman, Keith Maki',
+      author_email='oberman@wisc.edu',
+      packages=['process_sat'],
+      scripts=['process_sat/whips.py'],
+      url = 'http://github.com/Joberman/process_sat',
+      download_url='http://github.com/Joberman/process_sat/downloads',
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Natural Language :: English',
+            'Programming Language :: Python'
+      ]
+)
