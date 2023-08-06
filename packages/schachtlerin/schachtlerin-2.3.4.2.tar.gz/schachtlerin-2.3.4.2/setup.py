19ee56ae6b4e2c252e49c5eac42dee9188bf40ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:04 +0000

    Add package schachtlerin/2.3.4.2/schachtlerin-2.3.4.2.tar.gz

---
+++ packages/schachtlerin/schachtlerin-2.3.4.2.tar.gz/schachtlerin-2.3.4.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(
+		name		=	'schachtlerin',
+		version		=	'2.3.4.2',
+		py_modules	=	['schachtlerin'],
+		author		=	'Michael Tietz',
+		author_email	=	'tietz@b1-systems.de',
+		url		=	'www.b1-systems.de',
+		description	=	'Einfache Ausgabe verschachtelter Listen',
+		)
+
+
