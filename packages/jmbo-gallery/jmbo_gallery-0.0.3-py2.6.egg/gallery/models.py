09041576fb89bb3da7f7e4cd3fa978a51d638a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:09 +0000

    Add package jmbo-gallery/0.0.3/jmbo_gallery-0.0.3-py2.6.egg

---
+++ packages/jmbo-gallery/jmbo_gallery-0.0.3-py2.6.egg/gallery/models.py
@@ -0,0 +1,43 @@
+from django.core.urlresolvers import reverse
+from django.db import models
+
+from jmbo.models import ModelBase
+
+class Gallery(ModelBase):
+    class Meta():
+        verbose_name = "Gallery"
+        verbose_name_plural = "Galleries"
+    
+    def item_count(self):
+        return GalleryItem.permitted.filter(gallery=self).count()
+    
+    def get_absolute_url(self):
+        return reverse('gallery_object_detail', kwargs={'slug': self.slug})
+
+    def __unicode__(self):
+        return self.title
+
+    def get_items(self):
+        return GalleryItem.permitted.filter(gallery=self).order_by('created')
+    
+class GalleryItem(ModelBase):
+    gallery = models.ForeignKey(
+        'gallery.Gallery',
+    )
+
+class GalleryImage(GalleryItem):
+    class Meta():
+        verbose_name = "Gallery image"
+        verbose_name_plural = "Gallery images"
+
+class VideoEmbed(GalleryItem):
+    embed = models.TextField()
+    class Meta():
+        verbose_name = "Video embed"
+        verbose_name_plural = "Video embeds"
+
+class VideoFile(GalleryItem):
+    file = models.FileField(upload_to='content/videofile')
+    class Meta():
+        verbose_name = "Video file"
+        verbose_name_plural = "Video files"
