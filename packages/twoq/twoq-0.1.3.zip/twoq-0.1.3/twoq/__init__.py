fbf94e4d3d938d99c4f9e59da4ce48c7d45fc770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:40 +0000

    Add package twoq/0.1.3/twoq-0.1.3.zip

---
+++ packages/twoq/twoq-0.1.3.zip/twoq-0.1.3/twoq/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+'''twoq iterator utilities'''
+
+from twoq.support import port, iterexcept
+from twoq.active.queuing import twoq, manq, autoq
+
+__all__ = ('twoq', 'manq', 'autoq', 'port', 'iterexcept')
+
+__version__ = (0, 1, 1)
