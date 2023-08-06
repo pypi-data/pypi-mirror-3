32a4f1b08b42452e932af8fdeeee752676befe1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:57 +0000

    Add package gitpass/0.1/gitpass-0.1.tar.gz

---
+++ packages/gitpass/gitpass-0.1.tar.gz/gitpass-0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='gitpass',
+      version='0.1',
+      description='git extension of getpass',
+      author='Dustin Smith',
+      author_email='dustin@media.mit.edu',
+      download_url='https://github.com/dasmith/gitpass/tarball/master',
+      url='https://github.com/dasmith/gitpass',
+      packages=['gitpass'])
+
+
