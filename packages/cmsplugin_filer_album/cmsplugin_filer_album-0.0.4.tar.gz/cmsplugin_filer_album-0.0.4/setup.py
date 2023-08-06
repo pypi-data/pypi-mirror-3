3408c47d57cc72886e13863a76b97e6a527f26f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:51 +0000

    Add package cmsplugin_filer_album/0.0.4/cmsplugin_filer_album-0.0.4.tar.gz

---
+++ packages/cmsplugin_filer_album/cmsplugin_filer_album-0.0.4.tar.gz/cmsplugin_filer_album-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = 'cmsplugin_filer_album',
+        version = '0.0.4',
+        packages = find_packages(),
+        install_requires=[
+            "django >= 1.3",
+            "django-cms >= 2.2",
+            "django-sekizai >= 0.4.2",
+            "easy_thumbnails >= 1.0",
+            # "django-filer >= 0.9a1.dev1"  # requires
+        ],
+        author = 'Eugene Slizevich',
+        author_email = 'eugene@slizevich.net',
+        description = 'DjangoCMS filer plugin for show albums with template selection',
+        long_description = open('README').read(),
+        license = 'GPL',
+        keywords = 'django-cms filer plugin album template',
+        include_package_data = True,
+        url = 'http://bitbucket.org/ffaxl/cmsplugin-imagealbum'
+     )
