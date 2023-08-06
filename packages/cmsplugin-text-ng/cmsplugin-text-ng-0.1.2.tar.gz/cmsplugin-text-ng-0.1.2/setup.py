6950ce05c185673e3d4502690d7bbe353f7a39a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:31 +0000

    Add package cmsplugin-text-ng/0.1.2/cmsplugin-text-ng-0.1.2.tar.gz

---
+++ packages/cmsplugin-text-ng/cmsplugin-text-ng-0.1.2.tar.gz/cmsplugin-text-ng-0.1.2/setup.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+import os
+
+from setuptools import setup, find_packages
+from cmsplugin_text_ng import __version__ as version
+
+setup(
+    name = 'cmsplugin-text-ng',
+    version = version,
+    description = "django-cms improved text plugin",
+    author = 'Kristian Øllegaard',
+    author_email = 'kristian@oellegaard.com',
+    url = 'https://github.com/KristianOellegaard/cmsplugin-text-ng',
+    packages = find_packages(),
+    zip_safe=False,
+    include_package_data = True,
+    install_requires=[
+        'Django>=1.2',
+        'django-cms>=2.0',
+        ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        ]
+)
