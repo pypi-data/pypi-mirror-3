666bf8ba84393767803a892ebeb79a53b0706980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:25 +0000

    Add package pymc/2.2/pymc-2.2-py2.7-win32.egg

---
+++ packages/pymc/pymc-2.2-py2.7-win32.egg/pymc/tests/test_interactive.py
@@ -0,0 +1,17 @@
+"""
+Test interactive sampler
+"""
+
+# TODO: Make real test case.
+
+from pymc import MCMC
+from pymc.examples import disaster_model
+import os, nose
+
+def test_interactive():
+    S = MCMC(disaster_model)
+    S.isample(200,100,2,  out=open('testresults/interactive.log', 'w'), progress_bar=0)
+
+
+if __name__ == '__main__':
+    nose.runmodule()
