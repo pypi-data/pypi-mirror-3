9db3a399abe59777eb00afc711717a83afed8a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:43 +0000

    Add package jmbo-event/0.0.4/jmbo-event-0.0.4.tar.gz

---
+++ packages/jmbo-event/jmbo-event-0.0.4.tar.gz/jmbo-event-0.0.4/event/views.py
@@ -0,0 +1,37 @@
+from django.core.urlresolvers import reverse
+
+from cal.models import EntryItem
+from event.models import Event
+from jmbo.generic.views import GenericObjectList, GenericObjectDetail
+from jmbo.models import ModelBase
+from jmbo.view_modifiers import DateFieldIntervalViewModifier
+
+class ObjectList(GenericObjectList):
+    def get_extra_context(self, *args, **kwargs):
+        return {'title': 'Events'}
+    
+    def get_view_modifier(self, request, *args, **kwargs):
+        return DateFieldIntervalViewModifier(request=request, field_name='start')
+
+    def get_paginate_by(self, *args, **kwargs):
+        return 7
+    
+    def get_queryset(self, *args, **kwargs):
+        return EntryItem.permitted.by_model(Event).order_by('start')
+    
+    def get_template_name(self, *args, **kwargs):
+        return 'event/event_entryitem_list.html'
+
+object_list = ObjectList()
+
+class ObjectDetail(GenericObjectDetail):
+    def get_extra_context(self, *args, **kwargs):
+        return {'title': 'Events'}
+    
+    def get_view_modifier(self, request, *args, **kwargs):
+        return DateFieldIntervalViewModifier(request=request, field_name='start', base_url=reverse("event_entryitem_list"), ignore_defaults=True)
+
+    def get_queryset(self, *args, **kwargs):
+        return Event.permitted.all()
+        
+object_detail = ObjectDetail()
---
+++ packages/jmbo-event/jmbo-event-0.0.4.tar.gz/jmbo-event-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmbo-event',
+    version='0.0.4',
+    description='Jmbo event app.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/jmbo-event',
+    packages = find_packages(),
+    install_requires = [
+        'jmbo',
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
