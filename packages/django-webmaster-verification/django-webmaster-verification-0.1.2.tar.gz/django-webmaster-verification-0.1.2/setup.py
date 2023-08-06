4ed5870c11b187381cb5ddae5a5d86630332b576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:02 +0000

    Add package django-webmaster-verification/0.1.2/django-webmaster-verification-0.1.2.tar.gz

---
+++ packages/django-webmaster-verification/django-webmaster-verification-0.1.2.tar.gz/django-webmaster-verification-0.1.2/setup.py
@@ -0,0 +1,33 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name = "django-webmaster-verification",
+    version = "0.1.2",
+    author = "Nicolas Kuttler",
+    author_email = "pypi@nicolaskuttler.com",
+    description = "Webmaster tools verification for Django",
+    long_description = open("README.rst").read(),
+    license = "BSD",
+    url = "http://github.com/nkuttler/django-webmaster-verification",
+    packages = ['webmaster_verification'],
+    package_data = {
+        'webmaster_verification': [
+            'templates/webmaster_verification/*',
+            'README.rst',
+            'LICENSE',
+        ],
+    },
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
+    zip_safe = True,
+)
