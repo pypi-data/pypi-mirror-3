38f8bff53c71e8ccac984e2de83a124b4d543641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:45 +0000

    Add package doublecheck/0.1pre2012-05-08T10-01-13/doublecheck-0.1pre2012-05-08T10-01-13.tar.gz

---
+++ packages/doublecheck/doublecheck-0.1pre2012-05-08T10-01-13.tar.gz/doublecheck-0.1pre2012-05-08T10-01-13/setup.py
@@ -0,0 +1,20 @@
+import setuptools 
+import os.path
+import datetime
+
+setuptools.setup(   
+    name = "doublecheck",
+    version = "0.1pre" + datetime.datetime.utcnow().replace(microsecond=0).isoformat(),
+    url = "https://github.com/kennknowles/python-doublecheck",
+    maintainer = "Kenn Knowles",
+    maintainer_email = "kenn.knowles@gmail.com",
+    license = 'Apache 2.0',
+    packages = setuptools.find_packages(),
+    description = "Pythonic library for QuickCheck-style randomized testing and SmallCheck-style exhaustive testing of the same test suite.",
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Testing",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
