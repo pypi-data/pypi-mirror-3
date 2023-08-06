6a9a5c9d6ab85fad3d07289d0ac5db3801435ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:15 +0000

    Add package utilspy/0.9.0/utilspy-0.9.0.tar.gz

---
+++ packages/utilspy/utilspy-0.9.0.tar.gz/utilspy-0.9.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+VERSION = '0.9.0'
+
+setup(
+    name='utilspy',
+    version=VERSION,
+    description='Random useful python utilities that don\'t seem to exist elsewhere',
+    long_description = open('README.rst').read(),
+    author='Michael Prior',
+    author_email='prior@cracklabs.com',
+    url='https://github.com/prior/utilspy',
+    download_url='https://github.com/prior/utilspy/tarball/v%s'%VERSION,
+    license='LICENSE.txt',
+    packages=['utils','utils.test'],
+    install_requires=[ 'nose==1.1.2' ]
+)
