11c6ec532d4a7be98a767d99ebeddb6aa30cc608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:33 +0000

    Add package couleur/0.4.0/couleur-0.4.0.tar.gz

---
+++ packages/couleur/couleur-0.4.0.tar.gz/couleur-0.4.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# <Couleur - fancy shell output for python>
+# Copyright (C) <2010-2012>  Gabriel Falcão <gabriel@nacaolivre.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from setuptools import setup
+from couleur import __version__
+
+setup(name='couleur',
+    version=__version__,
+    description=(u'ANSI terminal tool for python, colored shell and other '
+                 'handy fancy features'),
+    author='Gabriel Falcao',
+    author_email='gabriel@nacaolivre.org',
+    url='http://github.com/gabrielfalcao/couleur',
+    py_modules=['couleur'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        "Operating System :: POSIX",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+    ],
+)
