3bbc702ce630a81d62f3d3704696580bd40b653e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:22 +0000

    Add package sc.galleria.support/1.0rc1/sc.galleria.support-1.0rc1.zip

---
+++ packages/sc.galleria.support/sc.galleria.support-1.0rc1.zip/sc.galleria.support-1.0rc1/src/sc/galleria/support/tests/test_setup.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+import unittest2 as unittest
+
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+
+from plone.browserlayer.utils import registered_layers
+
+from sc.galleria.support.config import PROJECTNAME
+from sc.galleria.support.testing import INTEGRATION_TESTING
+
+JS = [
+    '++resource++galleria-scripts/galleria-1.2.8.min.js',
+    ]
+
+
+class InstallTestCase(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.qi = self.portal['portal_quickinstaller']
+
+    def test_installed(self):
+        self.assertTrue(self.qi.isProductInstalled(PROJECTNAME))
+
+    def test_browserlayer(self):
+        layers = [l.getName() for l in registered_layers()]
+        self.assertTrue('IGalleriaLayer' in layers,
+                        'browser layer not installed')
+
+    def test_jsregistry(self):
+        resource_ids = self.portal.portal_javascripts.getResourceIds()
+        for id in JS:
+            self.assertTrue(id in resource_ids, '%s not installed' % id)
+
+
+class UninstallTestCase(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.qi = self.portal['portal_quickinstaller']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.qi.uninstallProducts(products=[PROJECTNAME])
+
+    def test_uninstalled(self):
+        self.assertFalse(self.qi.isProductInstalled(PROJECTNAME))
+
+    def test_browserlayer_removed(self):
+        layers = [l.getName() for l in registered_layers()]
+        self.assertFalse('IGalleriaLayer' in layers,
+                         'browser layer not removed')
+
+    def test_jsregistry_removed(self):
+        resource_ids = self.portal.portal_javascripts.getResourceIds()
+        for id in JS:
+            self.assertTrue(id not in resource_ids, '%s not installed' % id)
