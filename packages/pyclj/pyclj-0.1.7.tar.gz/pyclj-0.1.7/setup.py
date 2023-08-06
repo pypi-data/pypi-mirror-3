2e9c5d1da8e654dd93fa9e1dbe4e4604c75b3bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:34 +0000

    Add package pyclj/0.1.7/pyclj-0.1.7.tar.gz

---
+++ packages/pyclj/pyclj-0.1.7.tar.gz/pyclj-0.1.7/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+long_desc=open('README.rst','r').read()
+
+setup(name="pyclj",
+      version="0.1.7",
+      author="Sun Ning",
+      author_email="sunng@about.me",
+      description="clojure literal reader and writer for python",
+      long_description=long_desc,
+      url="http://github.com/sunng87/pyclj",
+      license='mit',
+      classifiers=['Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Topic :: Software Development',
+            'Programming Language :: Python',
+            'Operating System :: OS Independent']
+    )
