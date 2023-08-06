79633d4f3c922cdacaef2402a7adc5dd15180dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:10 +0000

    Add package ewb_case.casclient/0.1.3/ewb_case.casclient-0.1.3.tar.gz

---
+++ packages/ewb_case.casclient/ewb_case.casclient-0.1.3.tar.gz/ewb_case.casclient-0.1.3/README.txt
@@ -0,0 +1,12 @@
+Introduction
+============
+
+:Author: Matt Bierner 
+
+
+About
+=====
+
+Provides CAS authentication by extending anz.casclient. Creates normal plone users
+similar to the AutoUserMakerPASPlugin.
+
---
+++ packages/ewb_case.casclient/ewb_case.casclient-0.1.3.tar.gz/ewb_case.casclient-0.1.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.3'
+
+setup(name='ewb_case.casclient',
+      version=version,
+      description="Provides CAS authentication for EWB Case website.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Matt Bierner',
+      author_email='mattbierner@gmail.com',
+      url='https://github.com/mattbierner/ewb_case.casclient',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ewb_case', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.i18nmessageid',
+          'anz.casclient',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
