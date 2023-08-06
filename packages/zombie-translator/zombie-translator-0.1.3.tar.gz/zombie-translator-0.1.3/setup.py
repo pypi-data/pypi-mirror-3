4f5f36d6fcc03a30ba4e91dd8ae9741db402e6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:50 +0000

    Add package zombie-translator/0.1.3/zombie-translator-0.1.3.tar.gz

---
+++ packages/zombie-translator/zombie-translator-0.1.3.tar.gz/zombie-translator-0.1.3/setup.py
@@ -0,0 +1,28 @@
+#/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+ROOT_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.join(ROOT_DIR)
+
+setup(
+    name="zombie-translator",
+    description="The best way to understand and be understood by your zombie friends",
+    author="Steven Skoczen",
+    author_email="steven.skoczen@wk.com",
+    url="https://github.com/wieden-kennedy/zombie-translator",
+    version="0.1.3",
+    install_requires=[],
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
