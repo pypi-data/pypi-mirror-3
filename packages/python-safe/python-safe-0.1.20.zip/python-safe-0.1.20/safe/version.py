e1b8fbd81383a4521a90f4241b2ab0e5ff815d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:53 +0000

    Add package python-safe/0.1.20/python-safe-0.1.20.zip

---
+++ packages/python-safe/python-safe-0.1.20.zip/python-safe-0.1.20/safe/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SAFESETUP.PY
+short_version = '0.1.20'
+version = '0.1.20'
+full_version = '0.1.20'
+git_revision = '18d8be886354c75a5dad97b6e81906d49f67dabf'
+release = True
+
+if not release:
+    version = full_version
