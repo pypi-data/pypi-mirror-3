2be793dcaf7de2c0c339a57a92aee9f3edeb84e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:09 +0000

    Add package trueaction.mongofile/0.2.0/trueaction.mongofile-0.2.0-py2.7.egg

---
+++ packages/trueaction.mongofile/trueaction.mongofile-0.2.0-py2.7.egg/trueaction/mongofile/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+__init__.py
+
+Created by Michael Smith on 2012-06-21.
+Copyright © 2012 True Action Network. All rights reserved.
+"""
+
+from mongo_file import MongoFile, remove, rename
+__all__ = ["MongoFile", "remove", "rename"]
