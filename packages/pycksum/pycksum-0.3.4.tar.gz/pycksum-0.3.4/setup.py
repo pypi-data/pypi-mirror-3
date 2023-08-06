0ad4a6305be68553b7e4c65d9a86aa078b3142cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:20 +0000

    Add package pycksum/0.3.4/pycksum-0.3.4.tar.gz

---
+++ packages/pycksum/pycksum-0.3.4.tar.gz/pycksum-0.3.4/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+from setuptools.extension import Extension
+from pycksum import version
+
+long_description = open('README.rst').read()
+
+setup(
+    name = "pycksum",
+    description = "Python implementation of cksum algorithm",
+    long_description = long_description,
+    packages=['pycksum'],
+    url = "https://github.com/sobotklp/pycksum",
+    version = version,
+    author = 'Lewis Sobotkiewicz',
+    author_email = 'lewis.sobot@gmail.com',
+    ext_modules = [
+        Extension("_pycksum", ["ext/_pycksum.c"])
+    ],
+    test_suite="tests",
+    entry_points = {
+        'console_scripts' : [
+            'pycksum = pycksum.main:main',
+        ]
+    },
+    use_2to3 = True,
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
