64d7b12a056443260d2212191d7e3a20f119c655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:16 +0000

    Add package NagParser/0.0.22/NagParser-0.0.22.tar.gz

---
+++ packages/NagParser/NagParser-0.0.22.tar.gz/NagParser-0.0.22/nagparser/Model/Host.py
@@ -0,0 +1,33 @@
+from Base import Base, servicesstatus
+from NagList import NagList
+
+from nagparser.Services.nicetime import getnicetimefromdatetime
+
+
+class Host(Base):
+    '''Host represents a host definition found in status.dat.'''
+
+    def __init__(self, nag):
+        super(Host, self).__init__(nag=nag)
+        self.host_name = ''
+
+    @property
+    def services(self):
+        return NagList([x for x in self.nag.services if x.host_name == self.host_name])
+
+    @property
+    def name(self):
+        return self.host_name
+
+    @property
+    def status(self):
+        return servicesstatus(self.services)
+
+    def laststatuschange(self, returntimesincenow=True):
+        lastchange = max(self.services, key=lambda x: x.laststatuschange(returntimesincenow=False)). \
+                        laststatuschange(returntimesincenow=False)
+
+        if returntimesincenow:
+            return getnicetimefromdatetime(lastchange)
+        else:
+            return lastchange
