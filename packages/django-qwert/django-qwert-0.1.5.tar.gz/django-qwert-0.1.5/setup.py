24ac86bd67fc366037ba188c0f7ae68c177fef5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:52 +0000

    Add package django-qwert/0.1.5/django-qwert-0.1.5.tar.gz

---
+++ packages/django-qwert/django-qwert-0.1.5.tar.gz/django-qwert-0.1.5/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author:       Alisue
+# Last Change:  18-Mar-2011.
+#
+from setuptools import setup, find_packages
+
+version = "0.1.5"
+
+def read(filename):
+    import os.path
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(
+        name="django-qwert",
+        version=version,
+        description = "Django's useful snippets collection",
+        long_description=read('README.rst'),
+        classifiers = [
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Topic :: Internet :: WWW/HTTP',
+        ],
+        keywords = "django snippet collection useful extra",
+        author = "Alisue",
+        author_email = "lambdalisue@hashnote.net",
+        url=r"https://github.com/lambdalisue/django-qwert",
+        download_url = r"https://github.com/lambdalisue/django-qwert/tarball/master",
+        license = 'BSD',
+        packages = find_packages(),
+        include_package_data = True,
+        zip_safe = True,
+        install_requires=[
+            'setuptools', 
+            'setuptools-git',
+            'markdown', 
+            'BeautifulSoup', 
+            'Akismet'
+        ],
+)
