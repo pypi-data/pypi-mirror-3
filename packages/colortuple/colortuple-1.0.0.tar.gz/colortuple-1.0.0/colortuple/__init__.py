f585354a7b70f434bd3183f6da146ee40bdfd019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:25 +0000

    Add package colortuple/1.0.0/colortuple-1.0.0.tar.gz

---
+++ packages/colortuple/colortuple-1.0.0.tar.gz/colortuple-1.0.0/colortuple/__init__.py
@@ -0,0 +1,11 @@
+'''
+A named tuple for storing RGBA colors.
+'''
+__name__ = 'colortuple'
+__version__ = '1.0.0'
+
+# __doc__, __name__ &__version__ are seen by pydoc, read by setup.py,
+# and generate output for --help.
+
+from colortuple.colortuple import Color
+
