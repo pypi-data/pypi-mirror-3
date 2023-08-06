350ffd96f2cade70858b302f24cb4a380674c6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:01 +0000

    Add package Monitis-SDK/1.0.2/Monitis-SDK-1.0.2.tar.gz

---
+++ packages/Monitis-SDK/Monitis-SDK-1.0.2.tar.gz/Monitis-SDK-1.0.2/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'Monitis-SDK',
+    version = '1.0.2',
+    description = 'Monitis API SDK',
+    author = 'Jeremiah Shirk',
+    author_email = 'jshirk@gmail.com',
+    url = 'https://github.com/monitisexchange/Python-SDK',
+    requires = [
+        'parsedatetime',
+        'boto'
+    ],
+    install_requires = [
+        'parsedatetime',
+        'boto'
+    ],
+    packages = [
+        'monitis',
+        'monitis.monitors',
+        'monitis.tools',
+        'monitis.tools.awsmon'
+    ],
+    scripts = [
+        'monitis/tools/monitis_cloudwatch.py'
+    ],
+    keywords = [
+        '',
+    ],
+    classifiers = [
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Developers'
+    ],
+    long_description = """\
+    Monitis API SDK
+    ---------------
+    
+    The Monitis API SDK provides a simple Python interface to the Monitis 
+    RESTful API.  More information on the API is available at 
+    http://monitis.com/api/api.html.
+    """
+)
