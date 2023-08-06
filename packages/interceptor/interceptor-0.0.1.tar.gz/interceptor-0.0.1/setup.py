872b41e3104c9b710778f06b489ce6d24a33756c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:45 +0000

    Add package interceptor/0.0.1/interceptor-0.0.1.tar.gz

---
+++ packages/interceptor/interceptor-0.0.1.tar.gz/interceptor-0.0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='interceptor',
+    version='0.0.1',
+    author='Zach Smith',
+    author_email='zachmsmith@gmail.com',
+    url='https://github.com/zmsmith/interceptor',
+    py_modules=['interceptor'],
+)
