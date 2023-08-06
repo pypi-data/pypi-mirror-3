7d413d282462c68fc0b133e8eea1fb981903050a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:59 +0000

    Add package trashman/0.2.3/trashman-0.2.3.tar.gz

---
+++ packages/trashman/trashman-0.2.3.tar.gz/trashman-0.2.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='trashman',
+      version='0.2.3',
+      description='A Python XDG trash manager.',
+      author='Kwpolska',
+      author_email='kwpolska@kwpolska.tk',
+      url='https://github.com/Kwpolska/trashman',
+      license='3-clause BSD',
+      long_description=open('README.rst').read(),
+      platforms='Arch Linux',
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Console',
+                   'Intended Audience :: End Users/Desktop',
+                   'License :: OSI Approved :: BSD License',
+                   'Natural Language :: English',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Utilities'],
+      py_modules = ['trashman'],
+      scripts=['bin/trash'],
+      data_files=[('share/man/man8', ['docs/trashman.8.gz'])])
