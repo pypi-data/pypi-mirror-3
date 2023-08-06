fd746f4bbc32dc211224e1fe9994a21a87bacd51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:13 +0000

    Add package dj-redis-url/0.1.2/dj-redis-url-0.1.2.tar.gz

---
+++ packages/dj-redis-url/dj-redis-url-0.1.2.tar.gz/dj-redis-url-0.1.2/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+DJ-Database-URL
+===============
+
+This simple Django utility allows you to utilize the
+`12factor <http://www.12factor.net/backing-services>`_ inspired
+``REDIS_URL`` environment variable to configure your Django application.
+
+
+Usage
+-----
+
+Configure your Redis database in ``settings.py`` from ``REDIS_URL``::
+
+    REDIS = {"default": dj_redis_url.config()}
+
+Parse an arbitrary Database URL::
+
+    REDIS = {"default": dj_redis_url.parse("redis://..."")}
+
+Installation
+------------
+
+Installation is simple too::
+
+    $ pip install dj-redis-url
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="dj-redis-url",
+    version="0.1.2",
+
+    description="Use Redis URLs in your Django Application.",
+    long_description=__doc__,
+    url="https://github.com/dstufft/dj-redis-url",
+
+    author="Donald Stufft",
+    author_email="donald.stufft@gmail.com",
+
+    extras_require={
+        "tests": ["pytest"],
+    },
+
+    packages=find_packages(exclude=["tests"]),
+    package_data={"": ["LICENSE"]},
+    include_package_data=True,
+
+    zip_safe=False,
+)
