288018880943a74ad691e20286b0fde7ceeb6e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:43 +0000

    Add package jmbo-competition/0.0.5/jmbo-competition-0.0.5.tar.gz

---
+++ packages/jmbo-competition/jmbo-competition-0.0.5.tar.gz/jmbo-competition-0.0.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmbo-competition',
+    version='0.0.5',
+    description='Jmbo competition app.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt International',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/jmbo-competition',
+    packages = find_packages(),
+    install_requires = [
+        'jmbo',
+        'django-ckeditor',
+    ],
+    tests_require=[
+        'django-setuptest',
+    ],
+    test_suite="setuptest.SetupTestSuite",
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
+
