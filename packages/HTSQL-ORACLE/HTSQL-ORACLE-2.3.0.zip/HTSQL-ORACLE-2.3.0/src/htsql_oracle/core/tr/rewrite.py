6c8f647d17a196c00ee3dafe8710636c6475bce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:13 +0000

    Add package HTSQL-ORACLE/2.3.0/HTSQL-ORACLE-2.3.0.zip

---
+++ packages/HTSQL-ORACLE/HTSQL-ORACLE-2.3.0.zip/HTSQL-ORACLE-2.3.0/src/htsql_oracle/core/tr/rewrite.py
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2006-2012, Prometheus Research, LLC
+#
+
+
+from htsql.core.adapter import adapts
+from htsql.core.domain import StringDomain
+from htsql.core.tr.flow import LiteralCode
+from htsql.core.tr.rewrite import Rewrite
+
+
+class OracleRewriteLiteral(Rewrite):
+
+    adapts(LiteralCode)
+
+    def __call__(self):
+        if isinstance(self.code.domain, StringDomain) and self.code.value == "":
+            return self.code.clone(value=None)
+        return self.code
+
+
