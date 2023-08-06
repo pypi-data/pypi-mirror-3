fc43dea07f493e2dbff1bd954c37fd551efcc005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:13 +0000

    Add package Shake/1.2.1/Shake-1.2.1.tar.gz

---
+++ packages/Shake/Shake-1.2.1.tar.gz/Shake-1.2.1/shake/skeletons/project/bundles/common/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+    Common models
+    -------------------------------
+    
+"""
+import shake
+
+from main import app, db
+
+
+class BaseMixin(object):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    
+    deleted = db.Column(db.Boolean, nullable=False,
+        default=False)
+    
+    def __repr__(self):
+        return '<%s %d>' % (self.__class__.__name__, self.id)
+
