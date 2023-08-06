97e929df591fc539ae31c4825f29fb414a418fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:43 +0000

    Add package pojson/0.4/pojson-0.4.tar.gz

---
+++ packages/pojson/pojson-0.4.tar.gz/pojson-0.4/pojson/convert.py
@@ -0,0 +1,44 @@
+import simplejson
+import polib
+import os
+import argparse
+
+def po2dict(po):
+    """Convert po object to dictionary data structure (ready for JSON).
+    """
+    result = {}
+    
+    result[''] = po.metadata.copy()
+
+    for entry in po:
+        if entry.obsolete:
+            continue
+        if entry.msgstr:
+            result[entry.msgid] = [None, entry.msgstr]
+        elif entry.msgstr_plural:
+            plural = [entry.msgid_plural]
+            result[entry.msgid] = plural
+            ordered_plural = sorted(entry.msgstr_plural.items())
+            for order, msgstr in ordered_plural:
+                plural.append(msgstr)
+    return result
+
+def convert(po_file, encoding=None, pretty_print=False):
+    if encoding is None:
+        po = polib.pofile(po_file,
+                          autodetect_encoding=True)
+    else:
+        po = polib.pofile(po_file,
+                          autodetect_encoding=False,
+                          encoding=encoding)
+    
+    data = po2dict(po)
+    
+    if not pretty_print:
+        result = simplejson.dumps(data, ensure_ascii=False, sort_keys=True)
+    else:
+        result = simplejson.dumps(data, sort_keys=True, indent=4 * ' ',
+                                  ensure_ascii=False)
+    return result
+
+        
