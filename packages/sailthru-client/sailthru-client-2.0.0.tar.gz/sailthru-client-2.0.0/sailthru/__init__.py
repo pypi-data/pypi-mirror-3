59bbae59ebb1d357ca6ec595508df76952a8219b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:25 +0000

    Add package sailthru-client/2.0.0/sailthru-client-2.0.0.tar.gz

---
+++ packages/sailthru-client/sailthru-client-2.0.0.tar.gz/sailthru-client-2.0.0/sailthru/__init__.py
@@ -0,0 +1,9 @@
+from sailthru_client import *
+
+__version__ = '2.0.0'
+from sailthru_error import *
+from sailthru_response import *
+__author__ = 'Prajwal Tuladhar'
+__doc__ = """
+Python binding for Sailthru API based on Requests
+"""
