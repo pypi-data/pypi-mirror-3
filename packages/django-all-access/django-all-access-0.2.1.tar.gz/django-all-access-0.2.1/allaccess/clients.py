64c25852c4e46b456b51cc892d15d159bfe6b1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:59 +0000

    Add package django-all-access/0.2.1/django-all-access-0.2.1.tar.gz

---
+++ packages/django-all-access/django-all-access-0.2.1.tar.gz/django-all-access-0.2.1/allaccess/clients.py
@@ -0,0 +1,187 @@
+from __future__ import unicode_literals
+
+import json
+from urllib import urlencode
+from urlparse import parse_qs
+
+from django.core.urlresolvers import reverse
+from django.utils.encoding import force_unicode
+
+from requests.api import request
+from requests.auth import OAuth1
+from requests.exceptions import RequestException
+
+
+class BaseOAuthClient(object):
+    
+    def __init__(self, provider):
+        self.provider = provider
+
+    def get_access_token(self, request, callback=None):
+        "Fetch access token from callback request."
+        raise NotImplementedError('Defined in a sub-class') # pragma: no cover
+
+    def get_profile_info(self, raw_token):
+        "Fetch user profile information."
+        try:
+            response = self.request('get', self.provider.profile_url, token=raw_token)
+        except RequestException:
+            # TODO: Logging
+            return None
+        else:
+            return response.json if response.json is not None else response.text
+
+    def get_redirect_args(self, request, callback):
+        "Get request parameters for redirect url."
+        raise NotImplementedError('Defined in a sub-class') # pragma: no cover
+
+    def get_redirect_url(self, request, callback, parameters=None):
+        "Build authentication redirect url."
+        args = self.get_redirect_args(request, callback=callback)
+        additional = parameters or {}
+        args.update(additional)
+        params = urlencode(args)
+        return '{0}?{1}'.format(self.provider.authorization_url, params)
+
+    def parse_raw_token(self, raw_token):
+        "Parse token and secret from raw token response."
+        raise NotImplementedError('Defined in a sub-class') # pragma: no cover
+
+    def request(self, method, url, **kwargs):
+        "Build remote url request."
+        return request(method, url, **kwargs)
+
+
+class OAuthClient(BaseOAuthClient):
+
+    def get_access_token(self, request, callback=None):
+        "Fetch access token from callback request."
+        raw_token = request.session.get(self.session_key, None)
+        verifier = request.GET.get('oauth_verifier', None)
+        if raw_token is not None and verifier is not None:
+            data = {'oauth_verifier': verifier}
+            callback = request.build_absolute_uri(callback or request.path)
+            callback = force_unicode(callback)
+            try:
+                response = self.request('post', self.provider.access_token_url, 
+                                        token=raw_token, data=data, oauth_callback=callback)
+            except RequestException:
+                # TODO: Logging
+                return None
+            else:
+                return response.text
+        return None
+
+    def get_request_token(self, request, callback):
+        "Fetch the OAuth request token. Only required for OAuth 1.0."
+        callback = force_unicode(request.build_absolute_uri(callback))
+        try:
+            response = self.request('post', self.provider.request_token_url, oauth_callback=callback)
+        except RequestException:
+            # TODO: Logging
+            return None
+        else:
+            return response.text
+
+    def get_redirect_args(self, request, callback):
+        "Get request parameters for redirect url."
+        callback = force_unicode(request.build_absolute_uri(callback))
+        raw_token = self.get_request_token(request, callback)
+        token, secret = self.parse_raw_token(raw_token)
+        if token is not None and secret is not None:
+            request.session[self.session_key] = raw_token
+        return {
+            'oauth_token': token,
+            'oauth_callback': callback,
+        }
+
+    def parse_raw_token(self, raw_token):
+        "Parse token and secret from raw token response."
+        qs = parse_qs(raw_token)
+        token = qs.get('oauth_token', [None])[0]
+        secret = qs.get('oauth_token_secret', [None])[0]
+        return (token, secret)
+
+    def request(self, method, url, **kwargs):
+        "Build remote url request. Constructs necessary auth."
+        user_token = kwargs.pop('token', '')
+        token, secret = self.parse_raw_token(user_token)
+        callback = kwargs.pop('oauth_callback', None)
+        verifier = kwargs.get('data', {}).get('oauth_verifier')
+        oauth = OAuth1(
+            resource_owner_key=token,
+            resource_owner_secret=secret,
+            client_key=self.provider.key,
+            client_secret=self.provider.secret,
+            verifier=verifier,
+            callback_uri=callback,
+        )
+        kwargs['auth'] = oauth
+        return super(OAuthClient, self).request(method, url, **kwargs)
+
+    @property
+    def session_key(self):
+        return 'allaccess-{0}-request-token'.format(self.provider.name)
+
+
+class OAuth2Client(BaseOAuthClient):
+
+    def get_access_token(self, request, callback=None):
+        "Fetch access token from callback request."
+        callback = request.build_absolute_uri(callback or request.path)
+        if 'code' in request.GET:
+            args = {
+                'client_id': self.provider.key,
+                'redirect_uri': callback,
+                'client_secret': self.provider.secret,
+                'code': request.GET['code'],
+                'grant_type': 'authorization_code',
+            }
+        else:
+            return None
+        try:
+            response = self.request('post', self.provider.access_token_url, data=args)
+        except RequestException:
+            # TODO: Logging
+            return None
+        else:
+            return response.text
+
+    def get_redirect_args(self, request, callback):
+        "Get request parameters for redirect url."
+        callback = request.build_absolute_uri(callback)
+        return {
+            'client_id': self.provider.key,
+            'redirect_uri': callback,
+            'response_type': 'code',
+        }
+
+    def parse_raw_token(self, raw_token):
+        "Parse token and secret from raw token response."
+        # Load as json first then parse as query string
+        try:
+            token_data = json.loads(raw_token)
+        except ValueError:
+            qs = parse_qs(raw_token)
+            token = qs.get('access_token', [None])[0]
+        else:
+            token = token_data.get('access_token', None)
+        return (token, None)
+
+    def request(self, method, url, **kwargs):
+        "Build remote url request. Constructs necessary auth."
+        user_token = kwargs.pop('token', '')
+        token, _ = self.parse_raw_token(user_token)
+        if token is not None:
+            params = kwargs.get('params', {})
+            params['access_token'] = token
+            kwargs['params'] = params
+        return super(OAuth2Client, self).request(method, url, **kwargs)
+
+
+def get_client(provider):
+    "Return the API client for the given provider."
+    cls = OAuth2Client
+    if provider.request_token_url:
+        cls = OAuthClient
+    return cls(provider)
