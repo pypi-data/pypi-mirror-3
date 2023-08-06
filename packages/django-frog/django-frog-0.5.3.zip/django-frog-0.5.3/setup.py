57ba3b8375a6542468beac84a540613b17eaea26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:36 +0000

    Add package django-frog/0.5.3/django-frog-0.5.3.zip

---
+++ packages/django-frog/django-frog-0.5.3.zip/django-frog-0.5.3/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='django-frog',
+    description='Media server built on django',
+    version='0.5.3',
+    author='Brett Dixon',
+    author_email='theiviaxx@gmail.com',
+    license='MIT',
+    url='https://github.com/theiviaxx/frog',
+    packages=[
+        'frog',
+        'frog.management',
+        'frog.management.commands',
+        'frog.templatetags',
+        ],
+    package_data={
+        'frog': [
+        	'templates/frog/*',
+        	'fixtures/*',
+        	'video.json',
+        	'README.md',
+        	'static/j/*.js',
+        	'static/j/libs/*.js',
+        	'static/i/*',
+        	'static/c/*.css'
+        ],
+    },
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    zip_safe=False,
+    include_package_data=True
+)
