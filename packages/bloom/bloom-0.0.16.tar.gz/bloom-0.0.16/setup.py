3880b654d30ef47fd8d8429f77e04ea2074b47d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:54 +0000

    Add package bloom/0.0.16/bloom-0.0.16.tar.gz

---
+++ packages/bloom/bloom-0.0.16.tar.gz/bloom-0.0.16/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='bloom',
+      version='0.0.16',
+      packages=['bloom'],
+      package_dir = {'': 'src'},
+      scripts = ['bin/bloom-generate-debian',
+                 'bin/bloom_util.sh',
+                 'bin/git-bloom-generate-debian',
+                 'bin/git-bloom-import-upstream',
+                 'bin/git-bloom-set-upstream'],
+      package_data = {'bloom': ['resources/em/*.em']},
+      install_requires = ['empy', 'pyyaml', 'argparse', 'rosdep', 'rospkg'],
+      author = 'Tully Foote', 
+      author_email = 'tfoote@willowgarage.com',
+      url = 'http://www.ros.org/wiki/bloom',
+      download_url = 'http://pr.willowgarage.com/downloads/bloom/',
+      keywords = ['ROS'],
+      classifiers = ['Programming Language :: Python',
+                     'Programming Language :: Unix Shell',
+                     'License :: OSI Approved :: BSD License'],
+      description = 'A tool for facilitating gitbuildpackage releases',
+      long_description = '''\
+A package to facilitate releasing into gitbuildpackage repositories.
+''',
+      license = 'BSD'
+      )
