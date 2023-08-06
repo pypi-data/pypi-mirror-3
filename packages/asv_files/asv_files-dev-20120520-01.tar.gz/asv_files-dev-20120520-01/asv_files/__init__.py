70bd32f4ae28858e8f804904f9871bbd9542e898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:02 +0000

    Add package asv_files/dev-20120520-01/asv_files-dev-20120520-01.tar.gz

---
+++ packages/asv_files/asv_files-dev-20120520-01.tar.gz/asv_files-dev-20120520-01/asv_files/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import os
+
+__name__ = 'asv_files'
+__version__ = 'dev-20120520-01'
+__keywords__ = 'django asv files ajax multiupload'
+__description__ = '''
+Module for connecting files to articles and other django models through ContentTypes.
+Ajax multi-upload FormField
+'''
