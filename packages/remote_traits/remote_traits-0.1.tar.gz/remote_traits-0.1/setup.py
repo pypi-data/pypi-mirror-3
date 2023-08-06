d02b20eff3fcea50b3e5a3c316bf8fdbdbd89783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:33 +0000

    Add package remote_traits/0.1/remote_traits-0.1.tar.gz

---
+++ packages/remote_traits/remote_traits-0.1.tar.gz/remote_traits-0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='remote_traits',
+      version='0.1',
+      description='use traits remotely',
+      license='MIT',
+      author='Andrew Straw',
+      author_email='strawman@astraw.com',
+      url='http://github.com/astraw/remote_traits',
+      py_modules=['remote_traits'],
+      )
