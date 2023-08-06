0c17876483bd90a24d5aae14ca986d0691577e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:11 +0000

    Add package agg2567/1.0.0/agg2567-1.0.0.zip

---
+++ packages/agg2567/agg2567-1.0.0.zip/agg2567-1.0.0/agg2567.py
@@ -0,0 +1,2 @@
+def print_test(x)
+    print(x)
---
+++ packages/agg2567/agg2567-1.0.0.zip/agg2567-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name ='agg2567',
+	version	='1.0.0',
+	py_modules =['agg2567'],
+	author ='agg2567',
+	author_email ='agg2567@hotmail.com',
+	url = 'http://www.google.com',
+	description = 'first packet of python',
+	)
