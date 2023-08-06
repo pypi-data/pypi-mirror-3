7810537777cc6afe57dc68910ec87a9bc7375ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:01 +0000

    Add package django-cherrypy/0.1/django-cherrypy-0.1.tar.gz

---
+++ packages/django-cherrypy/django-cherrypy-0.1.tar.gz/django-cherrypy-0.1/setup.py
@@ -0,0 +1,56 @@
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+extensions_dir = 'django_cherrypy'
+
+for dirpath, dirnames, filenames in os.walk(extensions_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+setup(
+    name='django-cherrypy',
+    version='0.1',
+    packages=packages,
+    license='LICENSE',
+    description='cherrypy, running under django',
+    long_description=open('README.md').read(),
+    author='Calvin Cheng',
+    author_email='calvin@calvinx.com',
+    install_requires=['cherrypy-wsgiserver'],
+    extras_require={
+        'newrelic': ['newrelic']
+    },
+    url='https://github.com/od-eon/django-cherrypy',
+)
