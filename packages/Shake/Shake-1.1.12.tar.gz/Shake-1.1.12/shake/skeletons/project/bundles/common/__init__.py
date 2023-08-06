68ca5b0cf65a4bbce170826d218d8e7728aafcac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:47 +0000

    Add package Shake/1.1.12/Shake-1.1.12.tar.gz

---
+++ packages/Shake/Shake-1.1.12.tar.gz/Shake-1.1.12/shake/skeletons/project/bundles/common/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+The priority is based upon order of creation:
+first created -> highest priority.
+"""
+from shake import Rule, EndpointPrefix, Submount
+
+from . import controllers
+
+
+urls = [
+    EndpointPrefix('bundles.common.controllers', [
+
+        Rule('/', 'index', name='index'), 
+        
+    ]),
+]
