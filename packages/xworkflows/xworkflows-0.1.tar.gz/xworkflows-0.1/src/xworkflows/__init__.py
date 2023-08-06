19b5f034203b4db12fe4b8e217573a1005f0f307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:29 +0000

    Add package xworkflows/0.1/xworkflows-0.1.tar.gz

---
+++ packages/xworkflows/xworkflows-0.1.tar.gz/xworkflows-0.1/src/xworkflows/__init__.py
@@ -0,0 +1,10 @@
+from . import base
+
+AbortTransition = base.AbortTransition
+
+InvalidTransitionError = base.InvalidTransitionError
+
+Workflow = base.Workflow
+WorkflowEnabled = base.WorkflowEnabled
+
+transition = base.transition
