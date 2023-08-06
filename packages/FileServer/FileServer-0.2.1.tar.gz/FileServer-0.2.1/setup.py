3c0ea5c7563edfce4f9f240113e54029e48bed2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:48 +0000

    Add package FileServer/0.2.1/FileServer-0.2.1.tar.gz

---
+++ packages/FileServer/FileServer-0.2.1.tar.gz/FileServer-0.2.1/setup.py
@@ -0,0 +1,43 @@
+"""
+setup packaging script for FileServer
+"""
+
+import os
+
+version = "0.2.1"
+dependencies = ['webob']
+
+# allow use of setuptools/distribute or distutils
+kw = {}
+try:
+    from setuptools import setup
+    kw['entry_points'] = """
+      [console_scripts]
+      serve = fileserver.web:main
+"""
+    kw['install_requires'] = dependencies
+except ImportError:
+    from distutils.core import setup
+    kw['requires'] = dependencies
+
+try:
+    here = os.path.dirname(os.path.abspath(__file__))
+    description = file(os.path.join(here, 'README.txt')).read()
+except IOError:
+    description = ''
+
+
+setup(name='FileServer',
+      version=version,
+      description="a simple static fileserver and directory index server in python (WSGI app)",
+      long_description=description,
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      author='Jeff Hammel',
+      author_email='jhammel@mozilla.com',
+      url='http://k0s.org/hg/FileServer',
+      license='MPL',
+      packages=['fileserver'],
+      include_package_data=True,
+      zip_safe=False,
+      **kw
+      )
