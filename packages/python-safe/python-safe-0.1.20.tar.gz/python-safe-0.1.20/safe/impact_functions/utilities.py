87b4797fba68cf0d0da6ec925037279e31b448d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:53 +0000

    Add package python-safe/0.1.20/python-safe-0.1.20.tar.gz

---
+++ packages/python-safe/python-safe-0.1.20.tar.gz/python-safe-0.1.20/safe/impact_functions/utilities.py
@@ -0,0 +1,80 @@
+"""Module to create damage curves from point data
+"""
+
+import numpy
+from safe.engine.interpolation1d import interpolate1d
+
+
+class Damage_curve:
+    """Class for implementation of damage curves based on point data
+    """
+
+    def __init__(self, data):
+
+        try:
+            data = numpy.array(data)
+        except:
+            msg = 'Could not convert data %s to damage curve' % str(data)
+            raise Exception(msg)
+
+        msg = 'Damage curve data must be a 2d array or a list of lists'
+        if len(data.shape) != 2:
+            raise RuntimeError(msg)
+
+        msg = 'Damage curve data must have two columns'
+        if data.shape[1] != 2:
+            raise RuntimeError(msg)
+
+        self.x = data[:, 0]
+        self.y = data[:, 1]
+
+    def __call__(self, zeta):
+
+        return interpolate1d(self.x, self.y, [zeta], mode='linear')[0]
+
+
+class ColorMapEntry:
+    """Representation of color map entry in SLD file
+
+    Input
+        color
+        quantity
+        opacity (default '0')
+    """
+
+    def __init__(self, color, quantity, opacity=None):
+        self.color = color
+        self.opacity = opacity
+        self.quantity = quantity
+
+
+class PointSymbol:
+    """
+    """
+
+    def __init__(self, value, icon):
+        self.value = value
+        self.icon = icon
+
+
+class PointClassColor:
+    """
+    """
+
+    def __init__(self, name, clmin, clmax, fill_color,
+                 stroke_color=None, opacity=1):
+        self.name = name
+        self.clmin = clmin
+        self.clmax = clmax
+        self.fill_color = fill_color
+        self.stroke_color = stroke_color
+        self.opacity = opacity
+
+
+class PointZoomSize:
+    """
+    """
+
+    def __init__(self, level, size):
+        self.level = level
+        self.size = size
---
+++ packages/python-safe/python-safe-0.1.20.tar.gz/python-safe-0.1.20/setup.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import subprocess
+
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+MAJOR = 0
+MINOR = 1
+MICRO = 20
+ISRELEASED = True
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+# Return the git revision as a string
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout = subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = "Unknown"
+
+    return GIT_REVISION
+
+def write_version_py(filename='safe/version.py'):
+    cnt = """
+# THIS FILE IS GENERATED FROM SAFESETUP.PY
+short_version = '%(version)s'
+version = '%(version)s'
+full_version = '%(full_version)s'
+git_revision = '%(git_revision)s'
+release = %(isrelease)s
+
+if not release:
+    version = full_version
+"""
+    # Adding the git rev number needs to be done inside write_version_py(),
+    # otherwise the import of safe.version messes up the build under Python 3.
+    FULLVERSION = VERSION
+    if os.path.exists('.git'):
+        GIT_REVISION = git_version()
+    elif os.path.exists('safe/version.py'):
+        # must be a source distribution, use existing version file
+        try:
+            from safe.version import git_revision as GIT_REVISION
+        except ImportError:
+            raise ImportError("Unable to import git_revision. Try removing " \
+                              "safe/version.py and the build directory " \
+                              "before building.")
+    else:
+        GIT_REVISION = "Unknown"
+
+    if not ISRELEASED:
+        FULLVERSION += '.dev-' + GIT_REVISION[:7]
+
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': VERSION,
+                       'full_version' : FULLVERSION,
+                       'git_revision' : GIT_REVISION,
+                       'isrelease': str(ISRELEASED)})
+    finally:
+        a.close()
+
+    return FULLVERSION
+
+
+# Creates safe/version.py and returns the full version
+full_version = write_version_py()
+
+setup(name          = 'python-safe',
+      version       = full_version,
+      description   = 'Spatial Analysis F* Engine',
+      license       = 'BSD',
+      keywords      = 'gis vector feature raster data',
+      author        = 'Ole Nielsen',
+      author_email  = 'ole.moller.nielsen@gmail.com',
+      maintainer        = 'Ariel Núñez',
+      maintainer_email  = 'ingenieroariel@gmail.com',
+      url   = 'http://github.com/AIFDR/python-safe',
+      long_description = read('README'),
+      packages = ['safe',
+                  'safe.storage',
+                  'safe.engine',
+                  'safe.engine.impact_functions_for_testing',
+                  'safe.impact_functions'],
+      package_dir = {'safe': 'safe'},
+      package_data = {'safe': ['test/data/*']},
+      classifiers   = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS',
+        ],
+)
