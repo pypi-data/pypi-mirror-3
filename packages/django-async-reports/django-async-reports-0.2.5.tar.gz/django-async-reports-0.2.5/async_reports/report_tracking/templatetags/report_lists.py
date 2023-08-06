dac6a27ff2f2eac5372919a9f270093125c18b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:38 +0000

    Add package django-async-reports/0.2.5/django-async-reports-0.2.5.tar.gz

---
+++ packages/django-async-reports/django-async-reports-0.2.5.tar.gz/django-async-reports-0.2.5/async_reports/report_tracking/templatetags/report_lists.py
@@ -0,0 +1,12 @@
+from django import template
+from .. import report_lists
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def available_reports(context):
+    reports = [(report.name, report.get_verbose_name(), report.get_form()) for report in report_lists]
+    reports = sorted(reports)
+    reports.insert(0, (None, "Select a report", None))
+    context['available_reports'] = reports
+    return ''
