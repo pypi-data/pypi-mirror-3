24f74d6cb174e23f679ab1af501fa4eb39a8723d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:49 +0000

    Add package clutchclient/0.1.7/clutchclient-0.1.7.tar.gz

---
+++ packages/clutchclient/clutchclient-0.1.7.tar.gz/clutchclient-0.1.7/clutchclient/__init__.py
@@ -0,0 +1,2 @@
+# http://packages.python.org/versiontools/#how-version-tuple-affects-version-string
+__version__ = (0, 1, 7, 'final', 0)
---
+++ packages/clutchclient/clutchclient-0.1.7.tar.gz/clutchclient-0.1.7/clutchclient/commands/startapp.py
@@ -0,0 +1,60 @@
+import argparse
+import contextlib
+import os
+import plistlib
+import re
+import sys
+import tempfile
+import urllib2
+import zipfile
+
+from StringIO import StringIO
+
+SKELETON_URL = 'http://clutchdownloads.s3.amazonaws.com/skeletons/app.zip'
+
+PARSER = argparse.ArgumentParser(
+    description='Command-line interface for clutch.io')
+PARSER.add_argument('command')
+PARSER.add_argument('dirname')
+
+SLUG_RE = re.compile(r'^[\w-]+$')
+
+def handle(namespace, extra):
+    namespace = PARSER.parse_args()
+
+    if SLUG_RE.search(namespace.dirname) is None:
+        print >> sys.stderr, 'Sorry, you specified an invalid directory name'
+        sys.exit(1)
+    
+    if namespace.dirname.lower() in ('global', 'clutch-core'):
+        print >> sys.stderr, 'Sorry, both "global" and "clutch-core" are reserved names'
+        sys.exit(1)
+    
+    dirname = os.path.abspath(os.path.expanduser(namespace.dirname))
+    if os.path.exists(dirname):
+        print >> sys.stderr, 'Sorry, a directory by that name already exists'
+        sys.exit(1)
+
+    d = urllib2.urlopen(SKELETON_URL)
+
+    # First we extract the zip file into a temporary directory
+    tmp = StringIO(d.read())
+    extracted = tempfile.mkdtemp()
+    zip_context = contextlib.closing(
+        zipfile.ZipFile(tmp, 'r', zipfile.ZIP_DEFLATED))
+    with zip_context as z:
+        z.extractall(extracted)
+    
+    # Delete any DELETEME.txt files
+    for root, dirs, files in os.walk(extracted):
+        for fn in files:
+            if fn.endswith('DELETEME.txt'):
+                os.unlink(os.path.join(root, fn))
+    
+    plist_path = os.path.join(extracted, 'clutch.plist')
+    plist = plistlib.readPlist(plist_path)
+    plist['ClutchAppShortName'] = namespace.dirname
+    plistlib.writePlist(plist, plist_path)
+    
+    # Now we rename it to the user's desired directory
+    os.rename(extracted, dirname)
