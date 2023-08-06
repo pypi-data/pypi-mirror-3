030c5b5a7e4e1a5c9de3b6f762fdcab93e50fca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:47 +0000

    Add package sixfoh/0.1.2.2/sixfoh-0.1.2.2.tar.gz

---
+++ packages/sixfoh/sixfoh-0.1.2.2.tar.gz/sixfoh-0.1.2.2/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="sixfoh",
+    version="0.1.2.2",
+    author="Tyler Ball",
+    author_email="tyler@tylerball.net",
+    description=(
+        "Generates base sixfoh-encoded images for yer websites."),
+    license="BSD",
+    keywords="base64 encoding images css less",
+    url="https://github.com/tylerball/sixfoh",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'sixfoh = sixfoh.cli:main',
+        ],
+    },
+    include_package_data=True,
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+    ],
+)
