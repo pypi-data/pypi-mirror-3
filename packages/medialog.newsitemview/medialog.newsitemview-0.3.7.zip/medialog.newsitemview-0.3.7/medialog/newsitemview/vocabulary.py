d32ec83e385afb75f37055b6827054f766dd7067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:07 +0000

    Add package medialog.newsitemview/0.3.7/medialog.newsitemview-0.3.7.zip

---
+++ packages/medialog.newsitemview/medialog.newsitemview-0.3.7.zip/medialog.newsitemview-0.3.7/medialog/newsitemview/vocabulary.py
@@ -0,0 +1,18 @@
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from Products.CMFCore.utils import getToolByName
+from zope.component import getUtility 
+from zope.schema.interfaces import IVocabularyFactory 
+from zope.app.component.hooks import getSite
+
+
+
+def ImageSizeVocabulary():
+    site = getSite()
+    #context = site.portal_url
+    #Cant get this to work, so stick with 5 sizes for now
+    #portal_properties = getToolByName(context, 'portal_properties', None)
+    #sizes = portal_properties.imaging_properties.getProperty('allowed_sizes')  
+    #terms = [title=pair for pair in sizes ]
+    #return ['thumb', 'mini', 'preview', 'large', 'none']  
+    return site
+        
