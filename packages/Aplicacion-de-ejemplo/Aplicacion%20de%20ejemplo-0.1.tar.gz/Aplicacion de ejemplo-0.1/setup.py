fa489e2557ba82facf96dacec13423012f441b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:59 +0000

    Add package Aplicacion-de-ejemplo/0.1/Aplicacion%20de%20ejemplo-0.1.tar.gz

---
+++ packages/Aplicacion-de-ejemplo/Aplicacion%20de%20ejemplo-0.1.tar.gz/Aplicacion de ejemplo-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup  
+
+setup(name="Aplicacion de ejemplo",  
+	version="0.1",  
+	description="Ejemplo del funcionamiento de distutils",  
+	author="Raul Gonzalez",  
+	author_email="zootropo en gmail",  
+	url="http://mundogeek.net/tutorial-python/",  
+	license="GPL",  
+	scripts=["ejemplo.py"],  
+	)
