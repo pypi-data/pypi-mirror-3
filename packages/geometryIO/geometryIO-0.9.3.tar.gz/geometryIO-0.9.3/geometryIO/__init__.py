064ccdfda6c907436079bc4cca0b3c3e9c30629e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:13 +0000

    Add package geometryIO/0.9.3/geometryIO-0.9.3.tar.gz

---
+++ packages/geometryIO/geometryIO-0.9.3.tar.gz/geometryIO-0.9.3/geometryIO/__init__.py
@@ -0,0 +1,207 @@
+"""
+GDAL wrapper for reading and writing geospatial data 
+to a variety of vector formats.
+
+For a list of supported vector formats and driver names,
+please see http://www.gdal.org/ogr/ogr_formats.html
+"""
+import archiveIO
+import datetime
+import os
+from itertools import izip
+from osgeo import gdal, ogr, osr
+from shapely import wkb, geometry
+
+
+proj4LL = '+proj=longlat +datum=WGS84 +no_defs'
+proj4SM = '+proj=merc +lon_0=0 +lat_ts=0 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +units=m +no_defs'
+
+
+@archiveIO.save
+def save(targetPath, sourceProj4, shapelyGeometries, fieldPacks=None, fieldDefinitions=None, driverName='ESRI Shapefile', targetProj4=''):
+    'Save shapelyGeometries using the given proj4 and fields'
+    # Validate arguments
+    if not fieldPacks:
+        fieldPacks = []
+    if not fieldDefinitions:
+        fieldDefinitions = []
+    if fieldPacks and set(len(x) for x in fieldPacks) != set([len(fieldDefinitions)]):
+        raise GeometryError('A field definition is required for each field')
+    # Make dataSource
+    if os.path.exists(targetPath): 
+        os.remove(targetPath)
+    dataDriver = ogr.GetDriverByName(driverName)
+    if not dataDriver:
+        raise GeometryError('Could not load driver: %s' % driverName)
+    dataSource = dataDriver.CreateDataSource(targetPath)
+    # Make layer
+    layerName = os.path.splitext(os.path.basename(targetPath))[0]
+    spatialReference = get_spatialReference(targetProj4 or sourceProj4)
+    geometryType = get_geometryType(shapelyGeometries)
+    layer = dataSource.CreateLayer(layerName, spatialReference, geometryType)
+    # Make fieldDefinitions in featureDefinition
+    for fieldName, fieldType in fieldDefinitions:
+        layer.CreateField(ogr.FieldDefn(fieldName, fieldType))
+    featureDefinition = layer.GetLayerDefn()
+    # Save features
+    transformGeometry = get_transformGeometry(sourceProj4, targetProj4)
+    for shapelyGeometry, fieldPack in izip(shapelyGeometries, fieldPacks) if fieldPacks else ((x, []) for x in shapelyGeometries):
+        # Prepare
+        feature = ogr.Feature(featureDefinition)
+        feature.SetGeometry(transformGeometry(ogr.CreateGeometryFromWkb(shapelyGeometry.wkb)))
+        for fieldIndex, fieldValue in enumerate(fieldPack):
+            feature.SetField2(fieldIndex, fieldValue)
+        # Save
+        layer.CreateFeature(feature)
+    # Return
+    return targetPath
+
+
+def save_points(targetPath, sourceProj4, coordinateTuples, fieldPacks=None, fieldDefinitions=None, driverName='ESRI Shapefile', targetProj4=''):
+    'Save points using the given proj4 and fields'
+    return save(targetPath, sourceProj4, [geometry.Point(x) for x in coordinateTuples], fieldPacks, fieldDefinitions, driverName, targetProj4)
+
+
+@archiveIO.load
+def load(sourcePath, sourceProj4='', targetProj4=''):
+    'Load proj4, shapelyGeometries, fields'
+    # Get layer
+    dataSource = ogr.Open(sourcePath)
+    if not dataSource:
+        raise GeometryError('Could not load %s' % os.path.basename(sourcePath))
+    layer = dataSource.GetLayer()
+    # Get fieldDefinitions from featureDefinition
+    featureDefinition = layer.GetLayerDefn()
+    fieldIndices = xrange(featureDefinition.GetFieldCount())
+    fieldDefinitions = []
+    fieldTypes = []
+    for fieldIndex in fieldIndices:
+        fieldDefinition = featureDefinition.GetFieldDefn(fieldIndex)
+        fieldType = fieldDefinition.GetType()
+        fieldTypes.append(fieldType)
+        fieldDefinitions.append((fieldDefinition.GetName(), fieldType))
+    # Get spatialReference
+    spatialReference = layer.GetSpatialRef()
+    sourceProj4 = spatialReference.ExportToProj4() if spatialReference else '' or sourceProj4
+    # Load shapelyGeometries and fieldPacks
+    shapelyGeometries, fieldPacks = [], []
+    methodNameByType = {
+        ogr.OFTDate: 'GetFieldAsDateTime',
+        ogr.OFTDateTime: 'GetFieldAsDateTime',
+        ogr.OFTInteger: 'GetFieldAsInteger',
+        ogr.OFTIntegerList: 'GetFieldAsIntegerList',
+        ogr.OFTReal: 'GetFieldAsDouble',
+        ogr.OFTRealList: 'GetFieldAsDoubleList',
+        ogr.OFTString: 'GetFieldAsString',
+        ogr.OFTStringList: 'GetFieldAsStringList',
+        ogr.OFTWideString: 'GetFieldAsString',
+        ogr.OFTWideStringList: 'GetFieldAsStringList',
+    }
+    def get_fieldPack(f):
+        fieldPack = []
+        for fieldIndex, fieldType in izip(fieldIndices, fieldTypes):
+            try:
+                methodName = methodNameByType[fieldType]
+            except KeyError: # pragma: no cover
+                methodName = 'GetField'
+            fieldValue = getattr(f, methodName)(fieldIndex)
+            if fieldType in (ogr.OFTDate, ogr.OFTDateTime):
+                fieldValue = datetime.datetime(*fieldValue)
+            fieldPack.append(fieldValue)
+        return tuple(fieldPack)
+    transformGeometry = get_transformGeometry(sourceProj4, targetProj4)
+    feature = layer.GetNextFeature()
+    while feature:
+        # Append
+        shapelyGeometries.append(wkb.loads(transformGeometry(feature.GetGeometryRef()).ExportToWkb()))
+        fieldPacks.append(get_fieldPack(feature))
+        # Get the next feature
+        feature = layer.GetNextFeature()
+    # Return
+    return targetProj4 or sourceProj4, shapelyGeometries, fieldPacks, fieldDefinitions
+
+
+def load_points(sourcePath, sourceProj4='', targetProj4=''):
+    'Load proj4, points, fields'
+    proj4, shapelyGeometries, fieldPacks, fieldDefinitions = load(sourcePath, sourceProj4, targetProj4)
+    return proj4, [(point.x, point.y) for point in shapelyGeometries], fieldPacks, fieldDefinitions
+
+
+def get_transformPoint(sourceProj4, targetProj4=proj4LL):
+    'Return a function that transforms point coordinates from one spatial reference to another'
+    if sourceProj4 == targetProj4:
+        return lambda x, y: (x, y)
+    coordinateTransformation = get_coordinateTransformation(sourceProj4, targetProj4)
+    return lambda x, y: coordinateTransformation.TransformPoint(x, y)[:2]
+
+
+def get_transformGeometry(sourceProj4, targetProj4=proj4LL):
+    'Return a function that transforms a geometry from one spatial reference to another'
+    if not targetProj4 or sourceProj4 == targetProj4:
+        return lambda x: x
+    coordinateTransformation = get_coordinateTransformation(sourceProj4, targetProj4)
+    def transformGeometry(g):
+        'Transform a shapelyGeometry or gdalGeometry using coordinateTransformation'
+        # Test for shapelyGeometry
+        isShapely = isinstance(g, geometry.base.BaseGeometry)
+        # If we have a shapelyGeometry, convert it to a gdalGeometry
+        if isShapely:
+            g = ogr.CreateGeometryFromWkb(g.wkb)
+        try:
+            g.Transform(coordinateTransformation)
+        except RuntimeError, error:
+            gdal.ErrorReset()
+            raise GeometryError('Could not transform %s: %s' % (g.ExportToWkt(), error))
+        # If we originally had a shapelyGeometry, convert it back
+        if isShapely:
+            g = wkb.loads(g.ExportToWkb())
+        # Return
+        return g
+    # Return
+    return transformGeometry
+
+
+def get_coordinateTransformation(sourceProj4, targetProj4=proj4LL):
+    'Return a CoordinateTransformation that can be used with gdalGeometry.transform()'
+    sourceSRS = get_spatialReference(sourceProj4)
+    targetSRS = get_spatialReference(targetProj4)
+    return osr.CoordinateTransformation(sourceSRS, targetSRS)
+
+
+def get_spatialReference(proj4):
+    'Return a SpatialReference from proj4'
+    spatialReference = osr.SpatialReference()
+    try:
+        spatialReference.ImportFromProj4(proj4)
+    except RuntimeError, error:
+        raise GeometryError("Could not import proj4='%s'" % proj4)
+    return spatialReference
+
+
+def get_geometryType(shapelyGeometries):
+    'Determine geometry type for layer'
+    geometryTypes = list(set(type(x) for x in shapelyGeometries))
+    return ogr.wkbUnknown if len(geometryTypes) > 1 else {
+        geometry.Point: ogr.wkbPoint,
+        geometry.point.PointAdapter: ogr.wkbPoint,
+        geometry.LineString: ogr.wkbLineString,
+        geometry.linestring.LineStringAdapter: ogr.wkbLineString,
+        geometry.Polygon: ogr.wkbPolygon,
+        geometry.polygon.PolygonAdapter: ogr.wkbPolygon,
+        geometry.MultiPoint: ogr.wkbMultiPoint,
+        geometry.multipoint.MultiPointAdapter: ogr.wkbMultiPoint,
+        geometry.MultiLineString: ogr.wkbMultiLineString,
+        geometry.multilinestring.MultiLineStringAdapter: ogr.wkbMultiLineString,
+        geometry.MultiPolygon: ogr.wkbMultiPolygon,
+        geometry.multipolygon.MultiPolygonAdapter: ogr.wkbMultiPolygon,
+    }[geometryTypes[0]]
+
+
+class GeometryError(Exception):
+    'Exception raised when there is an error loading or saving geometries'
+    pass
+
+
+gdal.UseExceptions()
+ogr.UseExceptions()
+osr.UseExceptions()
---
+++ packages/geometryIO/geometryIO-0.9.3.tar.gz/geometryIO-0.9.3/geometryIO/tests.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+from itertools import izip
+from geometryIO import GeometryError, save, save_points, load, load_points, get_coordinateTransformation, get_geometryType, get_spatialReference, get_transformPoint, get_transformGeometry, proj4LL, proj4SM
+from osgeo.ogr import CreateGeometryFromWkt, OFTString, OFTInteger, OFTReal, OFTDate, OFTDateTime, wkbPolygon
+from shapely.geometry import Polygon, Point
+import os
+import datetime
+import shutil
+import tempfile
+import unittest
+
+
+sourceGeometries = [
+    Polygon([(0, 0), (0, 10), (10, 10), (10, 0), (0, 0)]),
+]
+fieldPacks = [
+    ('xxx', 11111, 44444.44),
+]
+fieldDefinitions = [
+    ('Name', OFTString),
+    ('Population', OFTInteger),
+    ('GDP', OFTReal),
+]
+
+
+class TestGeometryIO(unittest.TestCase):
+
+    def test_save_and_load_work(self):
+        path = self.get_path('.shp.zip')
+        save(path, proj4LL, sourceGeometries)
+        targetProj4, targetGeometries = load(path)[:2]
+        self.assert_('+proj=longlat' in targetProj4)
+        for sourceGeometry, targetGeometry in izip(sourceGeometries, targetGeometries):
+            self.assert_(sourceGeometry.equals(targetGeometry))
+
+    def test_save_and_load_attributes_work(self):
+        fieldPacks = [(
+            # 'Спасибо'.decode('utf-8'), 
+            datetime.datetime(2000, 1, 1),
+        )]
+        fieldDefinitions = [
+            # ('String', OFTString),
+            ('Date', OFTDate),
+        ]
+        path = self.get_path()
+        save(path, proj4LL, sourceGeometries, fieldPacks, fieldDefinitions)
+        for sourceField, targetField in izip(fieldPacks[0], load(path)[2][0]):
+            self.assertEqual(sourceField, targetField)
+
+    def test_save_and_load_points_work(self):
+        path = self.get_path('.shp.tar.gz')
+        save_points(path, proj4LL, [(0, 0)], fieldPacks, fieldDefinitions)
+        self.assertEqual(load_points(path)[1], [(0, 0)])
+
+    def test_save_with_targetProj4_works(self):
+        path = self.get_path()
+        save(path, proj4LL, sourceGeometries, targetProj4=proj4SM)
+        self.assert_('+proj=longlat' not in load(path)[0])
+
+    def test_load_with_targetProj4_works(self):
+        path = self.get_path()
+        save(path, proj4LL, sourceGeometries)
+        self.assert_('+proj=longlat' not in load(path, targetProj4=proj4SM)[0])
+
+    def test_save_overwrites_existing_targetPath(self):
+        path = self.get_path()
+        for x in xrange(2):
+            save(path, proj4LL, sourceGeometries)
+
+    def test_save_raises_exceptions(self):
+        path = self.get_path()
+        # A geometry has fewer attributes than are actually defined
+        with self.assertRaises(GeometryError):
+            save(path, proj4LL, sourceGeometries, [x[1:] for x in fieldPacks], fieldDefinitions)
+        # A geometry has more attributes than are actually defined
+        with self.assertRaises(GeometryError):
+            save(path, proj4LL, sourceGeometries, [x * 2 for x in fieldPacks], fieldDefinitions)
+        # The driverName is unrecognized
+        with self.assertRaises(GeometryError):
+            save(path, proj4LL, sourceGeometries, driverName='')
+
+    def test_load_raises_exceptions(self):
+        # The format is unrecognized
+        path = self.get_path('')
+        with self.assertRaises(GeometryError):
+            load(path)
+
+    def test_get_coordinateTransformation_runs(self):
+        get_coordinateTransformation(proj4LL, proj4SM)
+
+    def test_get_geometryType(self):
+        self.assertEqual(get_geometryType(sourceGeometries), wkbPolygon)
+
+    def test_get_spatialReference_runs(self):
+        get_spatialReference(proj4LL)
+        with self.assertRaises(GeometryError):
+            get_spatialReference('')
+
+    def test_get_transformGeometry_runs(self):
+        transformGeometry = get_transformGeometry(proj4LL, proj4SM)
+        self.assertEqual(type(transformGeometry(Point(0, 0))), type(Point(0, 0)))
+        self.assertEqual(type(transformGeometry(CreateGeometryFromWkt('POINT (0 0)'))), type(CreateGeometryFromWkt('POINT (0 0)')))
+        with self.assertRaises(GeometryError):
+            transformGeometry(Point(1000, 1000))
+
+    def test_get_transformPoint_runs(self):
+        transformPoint0 = get_transformPoint(proj4LL, proj4LL)
+        transformPoint1 = get_transformPoint(proj4LL, proj4SM)
+        self.assertNotEqual(transformPoint0(0, 0), transformPoint1(0, 0))
+
+    def get_path(self, fileExtension='.shp'):
+        'Return a temporaryFolder path with the given fileExtension'
+        self.pathIndex += 1
+        return os.path.join(self.temporaryFolder, str(self.pathIndex) + fileExtension)
+
+    def setUp(self):
+        self.temporaryFolder = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.temporaryFolder)
+
+    pathIndex = 0
