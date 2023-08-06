afae396b5497c8bf66907780f36be3076f45e4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:37 +0000

    Add package righteous/0.2.2/righteous-0.2.2.tar.gz

---
+++ packages/righteous/righteous-0.2.2.tar.gz/righteous-0.2.2/righteous/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.2.2'
+__author__ = 'Michael Joseph'
+
+from .api import (
+    init, login,
+    list_servers, find_server, server_info, server_settings,
+    create_and_start_server, create_server, set_server_parameters,
+    start_server, stop_server, delete_server,
+)
+
+hush_pyflakes = (init, login, list_servers, find_server, server_info,
+    server_settings, create_and_start_server, create_server,
+    set_server_parameters, start_server, stop_server, delete_server
+)
