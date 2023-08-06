2085f5633be5a31a6a6660fe5c9424796fb6f376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:48 +0000

    Add package fedmsg/0.0.1a3/fedmsg-0.0.1a3.tar.gz

---
+++ packages/fedmsg/fedmsg-0.0.1a3.tar.gz/fedmsg-0.0.1a3/fedmsg/core.py
@@ -0,0 +1,43 @@
+import atexit
+import simplejson
+import time
+import warnings
+import zmq
+
+import fedmsg.schema
+
+TOPIC_PREFIX = "org.fedoraproject."
+
+
+class FedMsgContext(object):
+    def __init__(self, **kw):
+        super(FedMsgContext, self).__init__(**kw)
+
+        # Prepare our context and publisher
+        self.context = zmq.Context(1)
+        self.publisher = self.context.socket(zmq.PUB)
+        self.publisher.bind(kw.get("publish_endpoint", "tcp://*:6543"))
+
+        # Define and register a 'destructor'.
+        def destructor():
+            self.publisher.close()
+            self.context.term()
+
+        atexit.register(destructor)
+
+        # Sleep just to make sure that the socket gets set up before anyone
+        # tries anything.  This is a documented zmq 'feature'.
+        time.sleep(1)
+
+    def subscribe(self, topic, callback, **kw):
+        raise NotImplementedError
+
+    def send_message(self, topic, msg, **kw):
+        if topic[:len(TOPIC_PREFIX)] != TOPIC_PREFIX:
+            topic = TOPIC_PREFIX + topic
+
+        for key in msg.keys():
+            if key not in fedmsg.schema.keys:
+                warnings.warn("%r not one of %r" % (key, fedmsg.schema.keys))
+
+        self.publisher.send_multipart([topic, simplejson.dumps(msg)])
