f83c48b9a9b79ec12e44bd8b2e0a55e24591f410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:21 +0000

    Add package webexpy/2.2.4/webexpy-2.2.4.tar.gz

---
+++ packages/webexpy/webexpy-2.2.4.tar.gz/webexpy-2.2.4/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+VERSION = '2.2.4'
+
+setup(
+    name='webexpy',
+    version=VERSION,
+    description='Python WebEx Api Wrapper',
+    author='Michael Prior',
+    author_email='prior@cracklabs.com',
+    url='https://github.com/prior/webexpy',
+    download_url='https://github.com/prior/webexpy/tarball/v%s'%VERSION,
+    packages=['webex','webex.test'],
+    install_requires=[ 'lxml==2.3.1', 'nose==1.1.2']
+)
