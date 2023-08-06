21909bd9cbcb0e545d4e3a7c6bcc555bcdff6f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:31 +0000

    Add package PyGlobe/1.2/PyGlobe-1.2.tar.gz

---
+++ packages/PyGlobe/PyGlobe-1.2.tar.gz/PyGlobe-1.2/pyglobe/__init__.py
@@ -0,0 +1,12 @@
+"""
+***********************************************
+* @author: Ferdinand E. Silva
+* @email: ferdinandsilva@ferdinandsilva.com
+* @title: PyGlobe
+* @description: Python interface for Globe Labs API
+***********************************************
+"""
+__version__ = "1.2"
+__author__ = "Ferdinand E. Silva"
+
+from .core import *
