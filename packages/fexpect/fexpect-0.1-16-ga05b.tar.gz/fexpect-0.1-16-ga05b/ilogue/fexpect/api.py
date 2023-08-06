75f0ee9986205c4d31214ef53fb83409736097c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:50 +0000

    Add package fexpect/0.1-16-ga05b/fexpect-0.1-16-ga05b.tar.gz

---
+++ packages/fexpect/fexpect-0.1-16-ga05b.tar.gz/fexpect-0.1-16-ga05b/ilogue/fexpect/api.py
@@ -0,0 +1,27 @@
+import fabric.api
+from ilogue.fexpect.internals import wrapExpectations, ExpectationContext
+
+
+def expect(promptexpr, response, exitAfter=-1):
+    if not exitAfter == -1:
+        return [(promptexpr, response, exitAfter)]
+    return [(promptexpr, response)]
+
+def expecting(e):
+    return ExpectationContext(e)
+
+def run(cmd, **kwargs):
+    #run wrapper
+    wrappedCmd = wrapExpectations(cmd)
+    return fabric.api.run(wrappedCmd, **kwargs)
+
+def sudo(cmd, **kwargs):
+    #sudo wrapper
+    wrappedCmd = wrapExpectations(cmd)
+    return fabric.api.sudo(wrappedCmd, **kwargs)
+
+def local(cmd, **kwargs):
+    #run wrapper
+    wrappedCmd = wrapExpectations(cmd)
+    return fabric.api.local(wrappedCmd, **kwargs)
+
