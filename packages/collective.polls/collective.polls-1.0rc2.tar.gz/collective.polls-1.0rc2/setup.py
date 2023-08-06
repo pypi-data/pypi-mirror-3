2a2dc0c4650cda7a0627e98d1d5d3e6435a5846e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:44 +0000

    Add package collective.polls/1.0rc2/collective.polls-1.0rc2.tar.gz

---
+++ packages/collective.polls/collective.polls-1.0rc2.tar.gz/collective.polls-1.0rc2/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+version = '1.0rc2'
+
+setup(name='collective.polls',
+      version=version,
+      description="A content type, workflow, and portlet for conducting "
+                  "online polls, for anonymous and logged-in users",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
+                       open(os.path.join("docs", "CREDITS.txt")).read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone :: 4.1",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python",
+        "Topic :: Office/Business :: News/Diary",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone dexterity polls',
+      author='Franco Pellegrini',
+      author_email='frapell@gmail.com',
+      url='https://github.com/collective/collective.polls',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'plone.app.dexterity>=1.1',
+        'plone.app.referenceablebehavior',
+        'collective.z3cform.datagridfield'
+        ],
+      extras_require={
+        'test': ['plone.app.testing'],
+        },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
