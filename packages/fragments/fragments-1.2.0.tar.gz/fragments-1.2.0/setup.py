b2c34fff0412392f662176d742c8819bed526aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:41 +0000

    Add package fragments/1.2.0/fragments-1.2.0.tar.gz

---
+++ packages/fragments/fragments-1.2.0.tar.gz/fragments-1.2.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import fragments
+
+setup(
+    name='fragments',
+    version='.'.join(map(str, fragments.__version__)),
+    description='tool for diff-based templating / fragmentation control',
+    long_description=open('README.md').read(),
+    author='Matt Chisholm',
+    author_email='matt-fragments@theory.org',
+    url='https://github.com/glyphobet/fragments',
+    packages= ['fragments',],
+    entry_points = {
+        'console_scripts': [
+            'fragments = fragments.commands:_main',
+        ],
+    },
+    data_files=[('share/fragments', ['README.md', 'bash_completion', 'LICENSE.txt'])],
+    license='BSD License',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+    ),
+)
