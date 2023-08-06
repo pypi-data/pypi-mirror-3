1a5677821f066e6f6908981f806fe7ff959a67ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:13 +0000

    Add package sendgrid/0.1.2/sendgrid-0.1.2.tar.gz

---
+++ packages/sendgrid/sendgrid-0.1.2.tar.gz/sendgrid-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sendgrid',
+    version='0.1.2',
+    author='Kane Kim',
+    author_email='kane@sendgrid.com',
+    packages=find_packages(),
+    url='https://github.com/sendgrid/sendgrid-python/',
+    license='LICENSE.txt',
+    description='SendGrid client',
+    long_description='SendGrid client',
+)
