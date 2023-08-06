9118da7bd70f60b4d306622a3f45861d40a21449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:49 +0000

    Add package pylibftdi/0.10.1/pylibftdi-0.10.1.tar.gz

---
+++ packages/pylibftdi/pylibftdi-0.10.1.tar.gz/pylibftdi-0.10.1/pylibftdi/examples/led_flash.py
@@ -0,0 +1,22 @@
+"""
+Flash an LED connected via a FTDI UM232R/245R module using pylibftdi
+
+Copyright (c) 2010-2012 Ben Bass <benbass@codedstructure.net>
+All rights reserved.
+"""
+
+import time
+from pylibftdi import BitBangDevice
+
+
+def flash_forever(rate):
+    "toggle bit zero at rate Hz"
+    # put an LED with 1Kohm or similar series resistor
+    # on D0 pin
+    with BitBangDevice() as bb:
+        while True:
+            time.sleep(1.0 / (2 * rate))
+            bb.port ^= 1
+
+if __name__ == '__main__':
+    flash_forever(1)
