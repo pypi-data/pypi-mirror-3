9e8198e3cba84a6f03928fd99350caac8af476bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:06 +0000

    Add package lingpy/1.0/lingpy-1.0.1-py3.tar.gz

---
+++ packages/lingpy/lingpy-1.0.1-py3.tar.gz/lingpy-1.0.1/setup.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+# LingPy 
+#
+# Copyright (C) 2012 Johann-Mattis List 
+# Author: Johann-Mattis List
+# Author Email: <mattis.list@phil.uni-duesseldorf.de> URL: <http://lingulist.de/lingpy> For
+# license information, see <gpl-3.0.txt>
+
+#import distribute_setup
+#distribute_setup.use_setuptools()
+
+from setuptools import setup, find_packages,Extension
+import sys
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+    #extra['convert_2to3_doctests'] = ['src/your/module/README.txt']
+    #extra['use_2to3_fixers'] = ['your.fixers']
+
+setup(
+        name = "lingpy",
+        version = "1.0.1",
+        packages = find_packages(),
+        include_package_data = True,
+        install_requires = ['numpy','networkx'],
+        author = "Johann-Mattis List",
+        author_email = "mattis.list@gmail.com",
+        keywords = [
+            "historical linguistics", 
+            "sequence alignment",
+            "computational linguistics"
+            ],
+        url = "http://lingulist.de/lingpy/",
+        description = "Python library for automatic tasks in historical linguistics",
+        license = "gpl-3.0",
+        platforms = ["unix","linux"],
+        ext_modules=[
+            Extension(
+                'lingpy.algorithm.align',
+                ['lingpy/algorithm/align.pyx'],
+                )
+            ],
+        **extra
+        )
+
+
