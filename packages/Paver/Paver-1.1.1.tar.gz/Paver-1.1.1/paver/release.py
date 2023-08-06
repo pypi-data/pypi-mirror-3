a4fd4f04ed37359c7c2ab7816db1688160f4785c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:21 +0000

    Add package Paver/1.1.1/Paver-1.1.1.tar.gz

---
+++ packages/Paver/Paver-1.1.1.tar.gz/Paver-1.1.1/paver/release.py
@@ -0,0 +1,32 @@
+"""Release metadata for Paver."""
+
+from paver.options import Bunch
+from paver.tasks import VERSION
+
+setup_meta=Bunch(
+    name='Paver',
+    version=VERSION,
+    description='Easy build, distribution and deployment scripting',
+    long_description="""Paver is a Python-based build/distribution/deployment scripting tool along the
+lines of Make or Rake. What makes Paver unique is its integration with 
+commonly used Python libraries. Common tasks that were easy before remain 
+easy. More importantly, dealing with *your* applications specific needs and 
+requirements is also easy.""",
+    author='Kevin Dangoor',
+    author_email='dangoor+paver@gmail.com',
+    maintainer='Lukas Linhart',
+    maintainer_email='bugs@almad.net',
+    url='http://paver.github.com/',
+    packages=['paver', 'paver.cog'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2 :: Only",
+        "Environment :: Console",
+        "Topic :: Documentation",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Build Tools",
+    ])
