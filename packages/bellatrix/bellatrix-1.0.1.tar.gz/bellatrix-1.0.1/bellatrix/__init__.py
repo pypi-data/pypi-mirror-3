3e70d0ee8e20e13f3a22b28b2a2f71a01e0d0860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:20 +0000

    Add package bellatrix/1.0.1/bellatrix-1.0.1.tar.gz

---
+++ packages/bellatrix/bellatrix-1.0.1.tar.gz/bellatrix-1.0.1/bellatrix/__init__.py
@@ -0,0 +1,22 @@
+#Constants
+
+APP = "Bellatrix"
+__version__ = '1.0.1'
+description = "AWS EC2 magic utilities"
+
+SECRET_FILE = "secret"
+KEY_FILE = "key"
+#path to the private key to connect to agents
+PRIVATE_KEY_FILE = "ec2.pk"
+
+OUT_TMP = "exec.tmp"
+
+#running state for an ec2 instance
+RUNNING = 16   
+
+
+#AWS constants
+#shutdown behaviour
+TERMINATE = "terminate" 
+
+AMI_AVAILABLE = "available"
