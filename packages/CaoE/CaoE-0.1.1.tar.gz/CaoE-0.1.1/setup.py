b3714456b80a13c69b8062a809069260a0bb9f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:45 +0000

    Add package CaoE/0.1.1/CaoE-0.1.1.tar.gz

---
+++ packages/CaoE/CaoE-0.1.1.tar.gz/CaoE-0.1.1/setup.py
@@ -0,0 +1,29 @@
+import sys, os
+from setuptools import setup
+
+install_requires = []
+if sys.platform.startswith('linux'):
+    install_requires.append('prctl')
+
+setup(
+    name = "CaoE",
+    description = "Kill all children processes when the parent dies",
+    long_description = open(os.path.join(os.path.dirname(__file__),
+                                         'README.rst')).read(),
+    version = "0.1.1",
+    platforms = ['POSIX'],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
+    py_modules = ['caoe'],
+    install_requires = install_requires,
+    author = "Qiangning Hong",
+    author_email = "hongqn@douban.com",
+    keywords = "process management",
+    url = "https://github.com/douban/caoe",
+    test_suite = 'nose.collector',
+    tests_require = ['nose'],
+)
