c462dda376b7d15ab4fba749fa5e9ed4e000d6be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:23 +0000

    Add package WebStar/0.1.1/WebStar-0.1.1.tar.gz

---
+++ packages/WebStar/WebStar-0.1.1.tar.gz/WebStar-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="WebStar",
+    version="0.1.1",
+    url="http://github.com/mikeboers/webstar",
+    
+    author="Mike Boers",
+    author_email="webstar@mikeboers.com",
+    license="BSD-3"
+)
