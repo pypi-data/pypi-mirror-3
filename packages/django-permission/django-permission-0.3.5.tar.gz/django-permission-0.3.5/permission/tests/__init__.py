9ec51358e28743a3ae848a7dc4b742b0a869fdbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:19 +0000

    Add package django-permission/0.3.5/django-permission-0.3.5.tar.gz

---
+++ packages/django-permission/django-permission-0.3.5.tar.gz/django-permission-0.3.5/permission/tests/__init__.py
@@ -0,0 +1,6 @@
+from permission.patches.cmp_patch import *
+from test_models import *
+from test_handlers import *
+from test_backends import *
+from test_decorators import *
+from test_templatetags import *
