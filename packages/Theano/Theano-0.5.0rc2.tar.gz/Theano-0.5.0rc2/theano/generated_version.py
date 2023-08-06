4d9f62fdfea9ad7b5cadcda78d468a0b7adcfc31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:40 +0000

    Add package Theano/0.5.0rc2/Theano-0.5.0rc2.tar.gz

---
+++ packages/Theano/Theano-0.5.0rc2.tar.gz/Theano-0.5.0rc2/theano/generated_version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM THEANO SETUP.PY
+short_version = '0.5.0rc2'
+version = '0.5.0rc2'
+git_revision = '6c8b5b8330a15cbe50c2259a549d67aba0d3dfd9'
+full_version = '0.5.0rc2.dev-%(git_revision)s' % {'git_revision': git_revision}
+release = True
+
+if not release:
+    version = full_version
