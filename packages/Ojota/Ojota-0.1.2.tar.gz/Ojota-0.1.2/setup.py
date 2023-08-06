3efa334ca5b9590b28577d39b5b78ca34dc5db01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:35 +0000

    Add package Ojota/0.1.2/Ojota-0.1.2.tar.gz

---
+++ packages/Ojota/Ojota-0.1.2.tar.gz/Ojota-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='Ojota',
+    version='0.1.2',
+    author='Felipe Lerena, Juan Pedro Fisanoti, Ezequiel Chan, Nicolas Sarubbi',
+    author_email='flerena@msa.com.ar, fisadev@gmail.com, echan@msa.com.ar, nicosarubi@gmail.com',
+    packages=['ojota'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/Ojota/',
+    license='LICENSE.txt',
+    description='Flat File Database with ORM',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
