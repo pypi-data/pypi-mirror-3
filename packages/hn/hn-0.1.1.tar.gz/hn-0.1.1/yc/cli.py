cd8f09745ec1dc3c494b23aad1d9107e35086d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:52 +0000

    Add package hn/0.1.1/hn-0.1.1.tar.gz

---
+++ packages/hn/hn-0.1.1.tar.gz/hn-0.1.1/yc/cli.py
@@ -0,0 +1,93 @@
+"""
+Command line goodness.
+"""
+
+import sys
+from argparse import ArgumentParser
+import simplejson as json
+from .core import News
+
+try:
+    from pygments import highlight
+    from pygments.formatters import Terminal256Formatter as Term256
+    from pygments.lexers import JavascriptLexer
+    has_pygments = True
+except ImportError:
+    has_pygments = False
+
+
+def create_parser():
+    """Create the Hacker News command line parser."""
+    description = 'A simple and easy-to-use CLI for the HNSearch API.'
+    parser = ArgumentParser(prog='hn', description=description)
+    parser.add_argument('search', metavar='S', type=str, nargs='*',
+                        help="A search term to query for.")
+    parser.add_argument('-n', '--limit', metavar='N', type=int,
+                        help="number of results to return (max 100)")
+    parser.add_argument('-s', '--start', metavar='N', type=int,
+                        help="ordinal position of first result")
+    parser.add_argument('-d', '--day', metavar='YYYY-MM-DD',
+                        help="filter by a specific date")
+    parser.add_argument('-S', '--sort', metavar='S', nargs='*',
+                        help="list of pairs to sort results")
+    parser.add_argument('-T', '--type', metavar='TYPE',
+                        help="type of items returned (comment or submission)")
+    parser.add_argument('-u', '-U', '--username', metavar='NAME',
+                        help="filter for a specific username")
+    parser.add_argument('--hits', action='store_true',
+                        help="return only the number of results encountered")
+    return parser
+
+
+def search(options):
+    """Perform a search against the HNSearch API."""
+    params = {}
+    if options.search:
+        term = ' '.join(options.search)
+        params['q'] = term
+    if options.limit:
+        params['limit'] = options.limit
+    if options.start:
+        params['start'] = options.start
+    if options.day:
+        date = options.day
+        if len(date) == 8:
+            # Then it's in MM-DD-YY format.
+            month, day, year = date.split('-')
+            date = '20%s-%s-%s' % (year, month, day)
+        params['day'] = date
+    if options.sort:
+        sort = options.sort
+        if len(sort) == 1:
+            sort.append('desc')
+        params['sortby'] = ' '.join(sort)
+    if options.type:
+        params['type'] = options.type
+    if options.username:
+        params['username'] = options.username
+    data = News().get(**params)
+    return present(options, data)
+
+
+def present(options, data):
+    """Determine what to do with the data based on CLI options."""
+    if options.hits:
+        print data['hits']
+    else:
+        return decorate(data)
+
+
+def decorate(stream, indent=2):
+    """Turn JSON into something readable."""
+    print_formatted_json(sys.stdout, stream, indent)
+    sys.stdout.write('\n')
+
+
+def print_formatted_json(stream, json_data, indentation):
+    """Format and print JSON."""
+    formatted = json.dumps(json_data, indent=indentation)
+    if has_pygments and getattr(stream, 'isatty', lambda: False)():
+        formatter = Term256()
+        lexer = JavascriptLexer()
+        formatted = highlight(formatted, formatter=formatter, lexer=lexer)
+    print >> stream, formatted.rstrip()
---
+++ packages/hn/hn-0.1.1.tar.gz/hn-0.1.1/yc/date.py
@@ -0,0 +1,40 @@
+"""
+Abstractions for dealing with dates. I don't know if you knew this, but dealing
+with dates and times in Python kind of blows.
+"""
+
+import datetime
+from collections import Iterable
+
+
+class Date(object):
+
+    def __init__(self, date):
+        self._d = date
+
+    def _convert_date(self):
+        """Convert the given input to a date."""
+        date = self._d
+        if isinstance(date, str):
+            # Check it's length -- YYYY-MM-DD vs MM-DD-YY
+            print date
+        elif isinstance(date, Iterable):
+            # then it's an iterable
+            print date
+        else:
+            # it's a datetime
+            print date
+
+    def hour(self, hour):
+        """Add a time to the date."""
+        if isinstance(hour, str):
+            print hour
+        else:
+            print hour
+        return self
+
+    def __str__(self):
+        return 'YYYY-MM-DD'
+
+    def __repr__(self):
+        return self.__str__()
