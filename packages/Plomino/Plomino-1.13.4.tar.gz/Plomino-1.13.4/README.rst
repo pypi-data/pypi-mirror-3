c08dfe8ffdd92563bfc5f64b78b01850a7863344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:02 +0000

    Add package Plomino/1.13.4/Plomino-1.13.4.tar.gz

---
+++ packages/Plomino/Plomino-1.13.4.tar.gz/Plomino-1.13.4/README.rst
@@ -0,0 +1,26 @@
+OBSOLETE
+========
+
+The **Plomino** package has been renamed
+`Products.CMFPlomino <http://pypi.python.org/pypi/Products.CMFPlomino>`_
+Please update your buildout eggs section.
+
+Note: This egg is empty, it just declares a dependency to Products.CMFPlomino
+(so buildout searching for 'Plomino' gets the accurate egg).
+
+Introduction
+============
+
+Plomino is a powerful and flexible web-based application builder for Plone.
+
+Credits
+========
+Companies
+---------
+|makinacom|_
+
+  * `Planet Makina Corpus <http://www.makina-corpus.org>`_
+  * `Contact us <mailto:python@makina-corpus.org>`_
+
+.. |makinacom| image:: http://depot.makina-corpus.org/public/logo.gif
+.. _makinacom:  http://www.makina-corpus.com
---
+++ packages/Plomino/Plomino-1.13.4.tar.gz/Plomino-1.13.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.13.4'
+
+setup(name='Plomino',
+      version=version,
+      description="Create specific applications in Plone without developing. Created by Makina Corpus.",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='RAD, dynamic forms, web application builder',
+      author='Eric BREHAULT',
+      author_email='eric.brehault@makina-corpus.org',
+      url='http://www.plomino.net',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'Products.CMFPlomino',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """
+      )
