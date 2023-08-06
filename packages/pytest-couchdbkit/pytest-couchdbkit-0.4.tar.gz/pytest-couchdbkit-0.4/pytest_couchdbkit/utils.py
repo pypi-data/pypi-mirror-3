7b847c2675c9e77351b423d7e63fbafc0dd45270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:13 +0000

    Add package pytest-couchdbkit/0.4/pytest-couchdbkit-0.4.tar.gz

---
+++ packages/pytest-couchdbkit/pytest-couchdbkit-0.4.tar.gz/pytest-couchdbkit-0.4/pytest_couchdbkit/utils.py
@@ -0,0 +1,17 @@
+
+def server_from_config(config):
+    from couchdbkit import Server
+    backend = config.getini('couchdbkit_backend')
+    return Server(backend=backend)
+
+def dbname_from_config(config, fmt):
+    suffix = config.getini('couchdbkit_suffix')
+    if not suffix:
+        pytest.fail('no couchdbkit_suffix set in ini')
+    return fmt % (suffix,)
+
+
+def maybe_destroy_and_create(server, dbname):
+    if dbname in server.all_dbs():
+        server.delete_db(dbname)
+    return server.create_db(dbname)
