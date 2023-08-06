3b27528145a874c66f51265bcdb69e36f0ddfd7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:09 +0000

    Add package trytond_calendar_classification/2.4.0/trytond_calendar_classification-2.4.0.tar.gz

---
+++ packages/trytond_calendar_classification/trytond_calendar_classification-2.4.0.tar.gz/trytond_calendar_classification-2.4.0/__tryton__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+{
+    'name': 'Calendar Classification',
+    'name_bg_BG': 'Класификация за календар',
+    'name_ca_ES': 'classificació del calendari',
+    'name_de_DE': 'Kalender Klassifikation',
+    'name_es_AR': 'Clasificación del calendario',
+    'name_es_CO': 'Clasificación del calendario',
+    'name_es_ES': 'Clasificación del calendario',
+    'name_fr_FR': 'Classification calendrier',
+    'name_ru_RU': 'Классификация по календарю',
+    'version': '2.4.0',
+    'author': 'B2CK',
+    'email': 'info@b2ck.com',
+    'website': 'http://www.tryton.org/',
+    'description': 'Handle classification of event',
+    'description_bg_BG': 'Добавя класификация на събития в CalDAV',
+    'description_ca_ES': '''Gestiona la classificació dels esdeveniments al
+CalDAV.
+''',
+    'description_de_DE': '''
+    Fügt Unterstützung für die Klassifikation von Terminen in CalDAV hinzu.
+''',
+    'description_es_AR': 'Gestiona la clasificación de eventos en CalDAV',
+    'description_es_CO': 'Gestiona la clasificación de eventos en CalDAV',
+    'description_es_ES': 'Gestiona la clasificación de eventos en CalDAV',
+    'description_fr_FR': 'Gère la classification des évènements',
+    'description_ru_RU': 'Добавление классификации событий в CalDAV',
+    'depends': [
+        'ir',
+        'calendar',
+    ],
+    'xml': [
+    ],
+    'translation': [
+        'locale/bg_BG.po',
+        'locale/ca_ES.po',
+        'locale/cs_CZ.po',
+        'locale/de_DE.po',
+        'locale/es_AR.po',
+        'locale/es_CO.po',
+        'locale/es_ES.po',
+        'locale/fr_FR.po',
+        'locale/nl_NL.po',
+        'locale/ru_RU.po',
+    ],
+}
---
+++ packages/trytond_calendar_classification/trytond_calendar_classification-2.4.0.tar.gz/trytond_calendar_classification-2.4.0/calendar_.py
@@ -0,0 +1,146 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+import vobject
+from trytond.model import ModelSQL
+from trytond.tools import reduce_ids
+from trytond.transaction import Transaction
+from trytond.pool import Pool
+
+
+class Event(ModelSQL):
+    _name = 'calendar.event'
+
+    def __init__(self):
+        super(Event, self).__init__()
+        self._error_messages.update({
+            'transparent': 'Free',
+            'opaque': 'Busy',
+            })
+
+    def search(self, domain, offset=0, limit=None, order=None, count=False,
+            query_string=False):
+        if Transaction().user:
+            domain = domain[:]
+            domain = [domain,
+                ['OR',
+                    [
+                        ('classification', '=', 'confidential'),
+                        ['OR',
+                            ('calendar.owner', '=', Transaction().user),
+                            ('calendar.write_users', '=', Transaction().user),
+                            ],
+                        ],
+                    ('classification', '!=', 'confidential'),
+                    ],
+                ]
+        return super(Event, self).search(domain, offset=offset, limit=limit,
+                order=order, count=count, query_string=query_string)
+
+    def create(self, values):
+        new_id = super(Event, self).create(values)
+        if self.search([('id', '=', new_id)], count=True) != 1:
+            self.raise_user_error('access_error', self._description)
+        return new_id
+
+    def _clean_private(self, record, transp):
+        '''
+        Clean private record
+
+        :param record: a dictionary with record values
+        :param transp: the time transparency
+        '''
+        summary = self.raise_user_error(transp, raise_exception=False)
+        if 'summary' in record:
+            record['summary'] = summary
+
+        vevent = None
+        if 'vevent' in record:
+            vevent = record['vevent']
+            if vevent:
+                vevent = vobject.readOne(vevent)
+                if hasattr(vevent, 'summary'):
+                    vevent.summary.value = summary
+
+        for field, value in (
+                ('description', ''),
+                ('categories', []),
+                ('location', None),
+                ('status', ''),
+                ('organizer', ''),
+                ('attendees', []),
+                ('alarms', [])):
+            if field in record:
+                record[field] = value
+            if field + '.rec_name' in record:
+                record[field + '.rec_name'] = ''
+            if vevent:
+                if hasattr(vevent, field):
+                    delattr(vevent, field)
+        if vevent:
+            record['vevent'] = vevent.serialize()
+
+    def read(self, ids, fields_names=None):
+        rule_obj = Pool().get('ir.rule')
+        cursor = Transaction().cursor
+        int_id = False
+        if isinstance(ids, (int, long)):
+            int_id = True
+            ids = [ids]
+        if len({}.fromkeys(ids)) != self.search([('id', 'in', ids)],
+                count=True):
+            self.raise_user_error('access_error', self._description)
+
+        writable_ids = []
+        domain1, domain2 = rule_obj.domain_get(self._name, mode='write')
+        if domain1:
+            for i in range(0, len(ids), cursor.IN_MAX):
+                sub_ids = ids[i:i + cursor.IN_MAX]
+                red_sql, red_ids = reduce_ids('id', sub_ids)
+                cursor.execute('SELECT id FROM "' + self._table + '" ' \
+                        'WHERE ' + red_sql + ' AND (' + domain1 + ')',
+                        red_ids + domain2)
+                writable_ids.extend(x[0] for x in cursor.fetchall())
+        else:
+            writable_ids = ids
+        writable_ids = set(writable_ids)
+
+        if fields_names is None:
+            fields_names = []
+        fields_names = fields_names[:]
+        to_remove = set()
+        for field in ('classification', 'calendar', 'transp'):
+            if field not in fields_names:
+                fields_names.append(field)
+                to_remove.add(field)
+        res = super(Event, self).read(ids, fields_names=fields_names)
+        for record in res:
+            if record['classification'] == 'private' \
+                    and record['id'] not in writable_ids:
+                self._clean_private(record, record['transp'])
+            for field in to_remove:
+                del record[field]
+        if int_id:
+            return res[0]
+        return res
+
+    def write(self, ids, values):
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        if len({}.fromkeys(ids)) != self.search([('id', 'in', ids)],
+                count=True):
+            self.raise_user_error('access_error', self._description)
+        res = super(Event, self).write(ids, values)
+        if len({}.fromkeys(ids)) != self.search([('id', 'in', ids)],
+                count=True):
+            self.raise_user_error('access_error', self._description)
+        return res
+
+    def delete(self, ids):
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        if len({}.fromkeys(ids)) != self.search([('id', 'in', ids)],
+                count=True):
+            self.raise_user_error('access_error', self._description)
+        return super(Event, self).delete(ids)
+
+Event()
