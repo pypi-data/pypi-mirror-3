af32ad6455b554cb622c7dd6151151db8d57b4c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:30 +0000

    Add package networkx/1.7rc1/networkx-1.7rc1.tar.gz

---
+++ packages/networkx/networkx-1.7rc1.tar.gz/networkx-1.7rc1/networkx/version.py
@@ -0,0 +1,25 @@
+"""
+Version information for NetworkX, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+import datetime
+
+version = '1.7rc1'
+date = 'Tue Jun 12 16:05:51 2012'
+
+# Was NetworkX built from a development version? If so, remember that the major
+# and minor versions reference the "target" (rather than "current") release.
+dev = False
+
+# Format: (name, major, min, revision)
+version_info = ('networkx', 1, '7rc1', '24dc63984b6c+')
+
+# Format: a 'datetime.datetime' instance
+date_info = datetime.datetime(2012, 6, 12, 16, 5, 51, 505277)
+
+# Format: (vcs, vcs_tuple)
+vcs_info = ('mercurial', ('24dc63984b6c+', 'tip'))
+
