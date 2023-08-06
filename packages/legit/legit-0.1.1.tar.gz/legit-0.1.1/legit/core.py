6891f85a0f83951453b11954e945d5eefe771aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:11 +0000

    Add package legit/0.1.1/legit-0.1.1.tar.gz

---
+++ packages/legit/legit-0.1.1.tar.gz/legit-0.1.1/legit/core.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+legit.core
+~~~~~~~~~~
+
+This module provides the basic functionality of legit.
+"""
+
+import bootstrap
+del bootstrap
+
+__version__ = '0.1.0'
+__author__ = 'Kenneth Reitz'
+__license__ = 'BSD'
+
