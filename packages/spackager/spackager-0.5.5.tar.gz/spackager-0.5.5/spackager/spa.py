e2206cb30645a60e9d4fc67eedfccd825e02c701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:35 +0000

    Add package spackager/0.5.5/spackager-0.5.5.tar.gz

---
+++ packages/spackager/spackager-0.5.5.tar.gz/spackager-0.5.5/spackager/spa.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import os
+
+from urllib2 import urlopen
+from base64 import b64encode
+
+from pyquery import PyQuery as pq
+
+from spackager.mhtml import generate_mhtml
+
+
+def compile(html, legacy_mode=False):
+    doc = pq(html)
+
+    doc.find('script').each(convert_script)
+    doc.find('link[rel=stylesheet]').each(convert_stylesheet)
+    doc.find('img').each(convert_image)
+    html = doc.__html__() # __html__ method avoids escaping -- TODO: retain doctype
+
+    if legacy_mode:
+        mhtml = generate_mhtml(doc) # XXX: rename variable
+        html = '%s%s' % (mhtml, html) # TODO: MHTML data can also be appended after HTML (retaining validity)!?
+
+    return html
+
+
+def convert_script(node):
+    node = pq(node)
+    uri = node.attr('src')
+    if uri:
+        print 'converting', uri
+        src = '/*<![CDATA[*/\n%s\n/*]]>*/' % _get_uri(uri)
+        node.attr('src', None).text(src) # TODO: delete src attribute altogether?
+
+
+def convert_stylesheet(node):
+    node = pq(node)
+    uri = node.attr('href')
+    if uri:
+        print 'converting', uri
+        css = '<style>/*<![CDATA[*/\n%s\n/*]]>*/</style>' % _get_uri(uri) # TODO: XHTML/HTML4 compatibility
+        pq(css).insertBefore(node)
+        node.remove()
+
+
+def convert_image(node):
+    node = pq(node)
+    uri = node.attr('src')
+    if uri:
+        print 'converting', uri
+        img = _get_uri(uri, binary=True)
+        ext = uri.rsplit('.', 1)[1]
+        uri = 'data:image/%s;base64,%s' % (ext, b64encode(img))
+        node.attr('src', uri)
+
+
+def _get_uri(uri, binary=False):
+    if uri.startswith('http://') or uri.startswith('https://'): # XXX: ignore HTTPS?
+        return urlopen(uri).read() # TODO: should be optional
+    else:
+        filepath = os.sep.join(uri.split('/'))
+        return _readfile(filepath, binary)
+
+
+def _readfile(filepath, binary=False):
+    mode = 'rb' if binary else 'r'
+    f = open(filepath, mode)
+    content = f.read().decode('utf-8')
+
+    f.close()
+    return content
