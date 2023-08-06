bea0186c5b727173b22c23131bb6457cf855e151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:17 +0000

    Add package RandomSources/0.1.3/RandomSources-0.1.3.zip

---
+++ packages/RandomSources/RandomSources-0.1.3.zip/RandomSources-0.1.3/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='RandomSources',
+    version='0.1.3',
+    author='Eric Astor',
+    author_email='eric.astor@gmail.com',
+    packages=['randomSources'],
+    url='https://github.com/ericastor/RandomSources/',
+    license='LICENSE.txt',
+    description='Drop-in replacements for Random, providing remote random-number sources.',
+    long_description=open('README.rst').read()
+)
