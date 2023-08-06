ca3d22bacc65cd79804f3db963d1f6093cd3f898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:30 +0000

    Add package sanitizer/1.0.0/sanitizer-1.0.0.zip

---
+++ packages/sanitizer/sanitizer-1.0.0.zip/sanitizer-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name = 'sanitizer',
+        version = '1.0.0',
+        py_modules = ['sanitizer'],
+        author = 'spyBuy',
+        author_email = 'serkan.duman@gmail.com',
+        description = 'Cleans dash, colon and converts to period as seperator in time data'
+    )
