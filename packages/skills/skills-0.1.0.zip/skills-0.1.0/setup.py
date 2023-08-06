a34233df2bcde2358f30b87a02d61e9115a7ac54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:56 +0000

    Add package skills/0.1.0/skills-0.1.0.zip

---
+++ packages/skills/skills-0.1.0.zip/skills-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+    name='skills',
+    version='0.1.0',
+    author='Scott Hamilton',
+    author_email='mcleopold@gmail.com',
+    packages=['skills',
+              'skills.trueskill',
+              'skills.testsuite',
+              'skills.testsuite.trueskill'
+    ],
+    url='http://github.com/McLeopold/PythonSkills/',
+    license="BSD",
+    description='Implementation of the TrueSkill, Glicko'
+                ' and Elo Ranking Algorithms',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Games/Entertainment',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
