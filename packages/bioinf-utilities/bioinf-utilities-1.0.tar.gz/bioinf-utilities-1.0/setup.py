f4a0fa94e1cb402843c55743816260817e1dc0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:22 +0000

    Add package bioinf-utilities/1.0/bioinf-utilities-1.0.tar.gz

---
+++ packages/bioinf-utilities/bioinf-utilities-1.0.tar.gz/bioinf-utilities-1.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+#
+# Copyright John Reid 2012
+#
+
+"""
+distutils setup script
+"""
+
+from distutils.core import setup
+
+setup(
+    name='bioinf-utilities',
+    version='1.0',
+    description='Some utilities for sequence analysis',
+    long_description=open('README.txt').read(),
+    author='John Reid',
+    author_email='johnbaronreid@netscape.net',
+    scripts = [
+        'scripts/seq-filter-empty',
+        'scripts/seq-head',
+        'scripts/seq-info',
+        'scripts/seq-match-lengths',
+        'scripts/seq-shorten',
+        'scripts/seq-shuffle',
+        'scripts/seq-subtract',
+        'scripts/seq-tally',
+    ],
+    url='http://sysbio.mrc-bsu.cam.ac.uk/group/index.php/John_Reid',
+    #packages=['distutils', 'distutils.command'],
+)
