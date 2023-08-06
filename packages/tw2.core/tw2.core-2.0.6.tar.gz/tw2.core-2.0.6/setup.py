acd3934b1889ff4caab9e5095d936a9208c7474e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:25 +0000

    Add package tw2.core/2.0.6/tw2.core-2.0.6.tar.gz

---
+++ packages/tw2.core/tw2.core-2.0.6.tar.gz/tw2.core-2.0.6/setup.py
@@ -0,0 +1,98 @@
+"""Setuptools setup file"""
+
+import sys, os
+import logging
+
+from setuptools import setup
+
+# Ridiculous as it may seem, we need to import multiprocessing and logging here
+# in order to get tests to pass smoothly on python 2.7.
+try:
+    import multiprocessing, logging
+except:
+    pass
+
+def get_description(fname='README.rst'):
+    # Adapted from PEAK-Rules' setup.py
+    # Get our long description from the documentation
+    f = file(fname)
+    lines = []
+    for line in f:
+        if not line.strip():
+            break     # skip to first blank line
+    for line in f:
+        if line.startswith('Documentation contents'):
+            break     # read to "Documentation contents..."
+        lines.append(line)
+    f.close()
+    return ''.join(lines)
+
+# Requirements to install buffet plugins and engines
+_extra_cheetah = ["Cheetah>=1.0", "TurboCheetah>=0.9.5"]
+_extra_genshi = ["Genshi >= 0.3.5"]
+_extra_kid = ["kid>=0.9.5", "TurboKid>=0.9.9"]
+_extra_mako = ["Mako >= 0.1.1"]
+
+setup(
+    name='tw2.core',
+    version='2.0.6',
+    description="Web widget creation toolkit based on TurboGears widgets",
+    long_description = get_description(),
+    install_requires=[
+        'WebOb>=0.9.7',
+        'simplejson >= 2.0',
+        'PasteDeploy',
+        'speaklater',
+        'decorator',
+        ],
+    tests_require = [
+        'unittest2',
+        'nose',
+        'coverage',
+        'BeautifulSoup',
+        'FormEncode',
+        'WebTest',
+        'strainer',
+    ] + _extra_kid + _extra_cheetah + _extra_genshi + _extra_mako,
+    test_suite = 'nose.collector',
+    extras_require = {
+        'cheetah': _extra_cheetah,
+        'kid': _extra_kid,
+        'genshi': _extra_genshi,
+        'mako': _extra_mako,
+        },
+    url = "http://toscawidgets.org/",
+    download_url = "http://toscawidgets.org/download/",
+    author='Paul Johnston, Christopher Perkins, Alberto Valverde & contributors',
+    author_email='paj@pajhome.org.uk',
+    license='MIT',
+    packages = ['tw2', 'tw2.core'],
+    namespace_packages = ['tw2'],
+    include_package_data=True,
+    exclude_package_data={"thirdparty" : ["*"]},
+    entry_points="""
+    [tw2.widgets]
+    widgets = tw2.core
+
+    [paste.filter_app_factory]
+    middleware = tw2.core.middleware:make_middleware
+
+    [distutils.commands]
+    archive_tw2_resources = tw2.core.command:archive_tw2_resources
+
+    """,
+    zip_safe=False,
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Environment :: Web Environment :: ToscaWidgets',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Software Development :: Widget Sets',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
