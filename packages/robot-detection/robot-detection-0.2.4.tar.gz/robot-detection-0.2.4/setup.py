c15596055ea46b3bb9161d5d9d2b45f66ad6c593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:23 +0000

    Add package robot-detection/0.2.4/robot-detection-0.2.4.tar.gz

---
+++ packages/robot-detection/robot-detection-0.2.4.tar.gz/robot-detection-0.2.4/setup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="robot-detection",
+      version="0.2.4",
+      author="Rory McCann",
+      author_email="rory@technomancy.org",
+      py_modules=['robot_detection'],
+)
