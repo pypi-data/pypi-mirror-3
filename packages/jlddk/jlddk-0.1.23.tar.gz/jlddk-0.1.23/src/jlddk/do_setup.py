fe7faab7981ec4572c59a472d840af818f6e390f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:33 +0000

    Add package jlddk/0.1.23/jlddk-0.1.23.tar.gz

---
+++ packages/jlddk/jlddk-0.1.23.tar.gz/jlddk-0.1.23/src/jlddk/do_setup.py
@@ -0,0 +1,15 @@
+"""
+    @author: Jean-Lou Dupont
+"""
+
+import logging,sys,os
+
+## force stdout flushing
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+
+FORMAT='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+formatter = logging.Formatter(FORMAT)
+
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+
