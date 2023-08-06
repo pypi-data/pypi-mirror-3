a71fcdfa10cf929c19aa80a28537b21e76b10ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:24 +0000

    Add package cover_grabber/1.2.1/cover_grabber-1.2.1-py2.7.egg

---
+++ packages/cover_grabber/cover_grabber-1.2.1-py2.7.egg/cover_grabber/handler/handler.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2011 Jayson Vaughn <vaughn.jayson@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+class Handler(object):
+    def __init__(self, dirname, filenames):
+        """ Initialize Handler """
+        self.dirname = dirname #Name of directory
+        self.filenames = filenames #List of filenames found in directory
+        self.audio_files = [os.path.join(dirname, file) for file in filenames]
+
+
+    def get_album_and_artist(self):
+        """ Return tags for album and artist"""
+        pass 
