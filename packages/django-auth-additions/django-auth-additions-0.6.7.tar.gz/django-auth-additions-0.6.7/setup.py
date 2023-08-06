1336e3b9f04a6ae7d782fe892ac6f016d39c069c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:20 +0000

    Add package django-auth-additions/0.6.7/django-auth-additions-0.6.7.tar.gz

---
+++ packages/django-auth-additions/django-auth-additions-0.6.7.tar.gz/django-auth-additions-0.6.7/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+import auth_additions
+
+setup(
+    name = "django-auth-additions",
+    version = auth_additions.__version__,
+    description = "Additions (monkey-patches) to auth models.",
+    long_description = open("README.rst").read(),
+    url = "http://bitbucket.org/schinckel/django-auth-additions",
+    author = "Matthew Schinckel",
+    author_email = "matt@schinckel.net",
+    packages = [
+        "auth_additions",
+        "auth_additions.management",
+        "auth_additions.management.commands",
+        "auth_additions.migrations"
+    ],
+    package_data = {
+        '': ['templates/admin/auth/*.html', 'VERSION']
+    },
+    classifiers = [
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+    ],
+)
