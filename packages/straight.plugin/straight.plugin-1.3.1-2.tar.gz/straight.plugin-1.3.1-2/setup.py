4ca218388db29e930f0c6d8e086c4b9fb6a60da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:17 +0000

    Add package straight.plugin/1.3.1-2/straight.plugin-1.3.1-2.tar.gz

---
+++ packages/straight.plugin/straight.plugin-1.3.1-2.tar.gz/straight.plugin-1.3.1-2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+INSTALL_REQUIRES = []
+
+try:
+    import importlib
+except ImportError:
+    INSTALL_REQUIRES.append('importlib')
+
+setup(name='straight.plugin',
+    version='1.3.1-2',
+    description='A simple namespaced plugin facility',
+    author='Calvin Spealman',
+    author_email='ironfroggy@gmail.com',
+    url='https://github.com/ironfroggy/straight.plugin',
+    packages=['straight', 'straight.plugin'],
+    install_requires=INSTALL_REQUIRES,
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Environment :: Plugins',
+    ]
+)
