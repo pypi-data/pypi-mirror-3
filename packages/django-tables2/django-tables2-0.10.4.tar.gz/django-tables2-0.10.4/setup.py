a28102d31a5251142aeaee5e85aa8f0209b17389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:00 +0000

    Add package django-tables2/0.10.4/django-tables2-0.10.4.tar.gz

---
+++ packages/django-tables2/django-tables2-0.10.4.tar.gz/django-tables2-0.10.4/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-tables2',
+    version='0.10.4',
+    description='Table/data-grid framework for Django',
+
+    author='Bradley Ayers',
+    author_email='bradley.ayers@gmail.com',
+    license='Simplified BSD',
+    url='https://github.com/bradleyayers/django-tables2/',
+
+    packages=find_packages(exclude=['tests.*', 'tests', 'example.*', 'example']),
+    include_package_data=True,  # declarations in MANIFEST.in
+
+    install_requires=['Django >=1.3'],
+    tests_require=['Django >=1.3', 'Attest >=0.5.3', 'django-attest >=0.2.4',
+                   'fudge', 'django-haystack', 'unittest-xml-reporting',
+                   'pylint'],
+
+    test_loader='tests:loader',
+    test_suite='tests.everything',
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
