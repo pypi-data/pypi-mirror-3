ac8bcb2cbf71fa4c6d3d1ed707e0485dadde3f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:57 +0000

    Add package plumi.locales/4.4/plumi.locales-4.4.zip

---
+++ packages/plumi.locales/plumi.locales-4.4.zip/plumi.locales-4.4/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+import os
+
+version = '4.4'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    )
+
+
+setup(name='plumi.locales',
+      version=version,
+      description="Locales for Plumi 4.x",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone plumi translation locales internationalization i18n l10n',
+      author='Andy Nicholson',
+      author_email='andy@infiniterecursion.com.au',
+      url='https://github.com/plumi/plumi.locales',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plumi'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [distutils.setup_keywords]
+      paster_plugins = setuptools.dist:assert_string_list
+
+      [egg_info.writers]
+      paster_plugins.txt = setuptools.command.egg_info:write_arg
+      """,
+      paster_plugins = ["ZopeSkel"],
+      )
