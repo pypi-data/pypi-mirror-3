8048f50b56c7e73970d2da5aff6c0b99560133b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:28 +0000

    Add package collective.panels/1.1/collective.panels-1.1.tar.gz

---
+++ packages/collective.panels/collective.panels-1.1.tar.gz/collective.panels-1.1/CHANGES.rst
@@ -0,0 +1,66 @@
+Changes
+=======
+
+1.1 (2012-05-15)
+----------------
+
+Changes:
+
+- Layout titles are now simply letters: A, B, C, etc. It turned out to
+  be difficult to provide a short, descriptive title for each layout.
+
+Features:
+
+- Improved styling of adding interface.
+
+- Added two new horizontal layouts that assign a fixed width of 1/3 to
+  respectively the left-most and the right-most assignment.
+
+Translation:
+
+- Added Danish translation.
+
+Bugfixes:
+
+- Fixed layout "Left" in the case of a single assignment.
+
+- The addable portlets check is now robust to misconfigured adding
+  views and will log a warning instead of letting the exception
+  trickle through.
+
+- Assignments now get unique names. For example, if two calendar
+  assignments are added, the second assignment gets the name
+  "calendar-1".
+
+- The panel info hash now correctly encodes a valid portlet
+  manager. This fixes an issue where KSS-enabled portlets would
+  operate incorrectly.
+
+- The panel assignment class and traverser now inherit from
+  ``OFS.Traversable.Traversable`` and implement the ``getId``
+  method. This fixes an issue where a physical path would not be
+  correctly computed.
+
+1.0.2 (2012-04-19)
+------------------
+
+Bugfixes:
+
+- Fixed issue where you could not add portlets to a panel other than
+  the first (for a given manager). This fixes issue #3.
+
+1.0.1 (2012-04-17)
+------------------
+
+Bugfixes:
+
+- Fixed template formatting issue.
+
+- Added markup from Plone's column renderer, wrapping each portlet in
+  a structure that provides a unique portlet hash. Also, use a "safe"
+  rendering method such that rendering may fail gracefully.
+
+1.0 (2012-04-12)
+----------------
+
+- Initial public release.
---
+++ packages/collective.panels/collective.panels-1.1.tar.gz/collective.panels-1.1/src/collective/panels/content.py
@@ -0,0 +1,133 @@
+from zope.interface import implements
+from zope.interface import providedBy
+from zope.component import getUtilitiesFor
+from zope.container.contained import Contained
+
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletType
+from plone.portlets.interfaces import IPortletAssignment
+from plone.app.portlets.interfaces import IPortletAssignmentMapping
+
+from Acquisition import Implicit
+from OFS.Traversable import Traversable
+
+from persistent import Persistent
+
+from .interfaces import IPanel
+from .i18n import MessageFactory as _
+
+PANEL_ANNOTATION_KEY = "collective.panels"
+
+
+def getAddablePortletTypes(interface):
+    types = (p[1] for p in getUtilitiesFor(IPortletType))
+
+    return filter(
+        lambda p: any(
+            i for i in p.for_ if interface.isOrExtends(i)
+            ),
+        types)
+
+
+def getAssignmentMapping(panel, manager):
+    return panel
+
+
+class Panel(Implicit, Persistent, Contained, Traversable):
+    implements(IPanel, IPortletAssignment, IPortletAssignmentMapping)
+
+    __allow_access_to_unprotected_subobjects__ = 1
+
+    # Currently, panel assignments do not carry state.
+    data = None
+
+    def __init__(self, name, layout, *assignments):
+        self.__name__ = name
+        self._assignments = list(assignments)
+        self.layout = layout
+
+    def __delitem__(self, name):
+        for index, assignment in enumerate(self):
+            if assignment.__name__ == name:
+                break
+        else:
+            raise KeyError(name)
+
+        self._assignments.pop(index)
+        self._p_changed = True
+
+    def __getitem__(self, name):
+        for assignment in self:
+            if assignment.__name__ == name:
+                return assignment
+
+        raise KeyError(name)
+
+    def __setitem__(self, name, assignment):
+        # Make sure assignment name is unique, e.g. "calendar-1".
+        if name in self:
+            c = 1
+            while True:
+                suggestion = "%s-%d" % (name, c)
+                if suggestion not in self:
+                    break
+
+                c += 1
+
+            name = suggestion
+
+        assignment.__name__ = name
+        self._assignments.append(assignment)
+        self._p_changed = True
+
+    def __contains__(self, name):
+        return any(name == assignment.__name__ for assignment in self)
+
+    def __iter__(self):
+        return iter(self._assignments)
+
+    def __len__(self):
+        return len(self._assignments)
+
+    def __repr__(self):
+        return '<%s name="%s" items="%d">' % (
+            type(self.aq_base).__name__, self.__name__, len(self._assignments)
+            )
+
+    def getId(self):
+        return self.__name__
+
+    @property
+    def available(self):
+        return any(
+            assignment.available for assignment in self
+            )
+
+    @property
+    def title(self):
+        return _(u"Panel ${name}", mapping={'name': self.__name__})
+
+    def getAddablePortletTypes(self):
+        interface = providedBy(self)
+        return getAddablePortletTypes(interface)
+
+    def keys(self):
+        return [assignment.__name__ for assignment in self]
+
+    def updateOrder(self, keys):
+        self._assignments.sort(
+            key=lambda assignment: keys.index(assignment.__name__)
+            )
+        self._p_changed = True
+
+
+class PanelManager(object):
+    implements(IPortletManager)
+
+    def __call__(self, context, request, view):
+        raise NotImplementedError(
+            "This portlet manager does not provide a renderer."
+            )
+
+    def getAddablePortletTypes(self):
+        return getAddablePortletTypes(IPanel)
