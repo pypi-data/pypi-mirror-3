e79468d56d9fa8ab0a9bf7735e79362170945ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:15 +0000

    Add package plumi.skin/4.3.1/plumi.skin-4.3.1.zip

---
+++ packages/plumi.skin/plumi.skin-4.3.1.zip/plumi.skin-4.3.1/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+import os
+
+version = '4.3.1'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    )
+
+setup(name='plumi.skin',
+      version=version,
+      description="Plumi Skin",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone plumi skin',
+      author='Andy Nicholson',
+      author_email='andy@infiniterecursion.com.au',
+      url='http://plumi.org/',
+      download_url = 'https://github.com/plumi/plumi.skin',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plumi'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [distutils.setup_keywords]
+      paster_plugins = setuptools.dist:assert_string_list
+
+      [egg_info.writers]
+      paster_plugins.txt = setuptools.command.egg_info:write_arg
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      paster_plugins = ["ZopeSkel"],
+      )
