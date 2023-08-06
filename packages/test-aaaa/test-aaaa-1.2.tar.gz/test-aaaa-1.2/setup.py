51238b00db331ec316cba6728d763bb1dd803359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:19 +0000

    Add package test-aaaa/1.2/test-aaaa-1.2.tar.gz

---
+++ packages/test-aaaa/test-aaaa-1.2.tar.gz/test-aaaa-1.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='test-aaaa',
+      version='1.2',
+      description='test pypi',
+      author='Daegeun Kim',
+      author_email='dgkim84@gmail.com',
+      url='http://twitter.com/GeekDani',
+      packages=['test'],
+     )
