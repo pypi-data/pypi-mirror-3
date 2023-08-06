6092ca1631c16a3caf79442efc61ef61e62cb159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:35 +0000

    Add package nydus/0.8.0/nydus-0.8.0.tar.gz

---
+++ packages/nydus/nydus-0.8.0.tar.gz/nydus-0.8.0/nydus/db/routers/__init__.py
@@ -0,0 +1,10 @@
+"""
+nydus.db.routers
+~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2011 DISQUS.
+:license: Apache License 2.0, see LICENSE for more details.
+"""
+
+from .base import BaseRouter, RoundRobinRouter
+
---
+++ packages/nydus/nydus-0.8.0.tar.gz/nydus-0.8.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+tests_require = [
+    'Django>=1.2,<1.4',
+    'mock',
+    'nose',
+    'psycopg2',
+    'redis',
+    'riak',
+    'thoonk==1.0.1.0',
+    'unittest2',
+]
+
+dependency_links = [
+    'https://github.com/andyet/thoonk.py/tarball/master#egg=thoonk',
+]
+
+
+install_requires = [
+]
+
+setup(
+    name='nydus',
+    version='0.8.0',
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    url='http://github.com/disqus/nydus',
+    description='Connection utilities',
+    packages=find_packages(exclude=('tests',)),
+    zip_safe=False,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='nose.collector',
+    include_package_data=True,
+    license='Apache License 2.0',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
