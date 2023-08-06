21a9356a36a0b51988b171fcecd62dc1d3c92df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:58 +0000

    Add package apns/1.1.1/apns-1.1.1.tar.gz

---
+++ packages/apns/apns-1.1.1.tar.gz/apns-1.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    author = 'Simon Whitaker',
+    author_email = 'simon@goosoftware.co.uk',
+    description = 'A python library for interacting with the Apple Push Notification Service',
+    download_url = 'http://github.com/simonwhitaker/PyAPNs',
+    license = 'unlicense.org',
+    name = 'apns',
+    py_modules = ['apns'],
+    url = 'http://www.goosoftware.co.uk/',
+    version = '1.1.1',
+)
