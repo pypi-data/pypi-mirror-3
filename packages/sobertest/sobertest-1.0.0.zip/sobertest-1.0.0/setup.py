4ab690c053a77b6c9dd8e9654163929b428af2a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:33 +0000

    Add package sobertest/1.0.0/sobertest-1.0.0.zip

---
+++ packages/sobertest/sobertest-1.0.0.zip/sobertest-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'sobertest',
+    version = '1.0.0',
+    py_modules = ['sobertest'],
+    author = 'Sober90',
+    author_email = 'patrick.tunack@web.de',
+    url = '',
+    description = 'Einfache Ausgabe einer Liste',
+    )
---
+++ packages/sobertest/sobertest-1.0.0.zip/sobertest-1.0.0/sobertest.py
@@ -0,0 +1,3 @@
+def sobertest(liste):
+    print(liste)
+    
