050b05fd5cff271065c899ff313944b24d0fa2df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:50 +0000

    Add package django-inviter/0.1.7/django-inviter-0.1.7.tar.gz

---
+++ packages/django-inviter/django-inviter-0.1.7.tar.gz/django-inviter-0.1.7/README.rst
@@ -0,0 +1,8 @@
+django-inviter
+==============
+
+Let your users invite other users.
+
+See `code <https://github.com/caffeinehit/django-inviter>`_ and 
+`documentation <http://django-inviter.readthedocs.org>`_.
+
---
+++ packages/django-inviter/django-inviter-0.1.7.tar.gz/django-inviter-0.1.7/inviter/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import patterns, url
+from inviter.views import Register, Done
+
+urlpatterns = patterns('',
+    url(r'^done/$', Done.as_view(), name = 'done'),
+    url(r'^register/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]+)/$',
+        Register.as_view(), name = 'register'),
+)
