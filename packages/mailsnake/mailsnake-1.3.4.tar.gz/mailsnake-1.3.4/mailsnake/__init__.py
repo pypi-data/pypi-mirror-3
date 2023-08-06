cd04cf98bb3bd87d478d2c184d1d2458231b371e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:04 +0000

    Add package mailsnake/1.3.4/mailsnake-1.3.4.tar.gz

---
+++ packages/mailsnake/mailsnake-1.3.4.tar.gz/mailsnake-1.3.4/mailsnake/__init__.py
@@ -0,0 +1,11 @@
+from mailsnake import *
+
+__author__ = 'John-Kim Murphy'
+__copyright__ = 'Copyright 2012, John-Kim Murphy'
+__credits__ = [
+    'John-Kim Murphy',
+    'Michael Helmick',
+    'vlinhart',
+    'starenka'
+]
+__version__ = '1.3.4'
