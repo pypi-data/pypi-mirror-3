49c56b196e0ef97b809fe753c0737029db82ad07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:50 +0000

    Add package jldaws/0.2.7/jldaws-0.2.7.zip

---
+++ packages/jldaws/jldaws-0.2.7.zip/jldaws-0.2.7/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+"""
+    Jean-Lou Dupont's AWS scripts
+    
+    Created on 2012-01-19
+    @author: jldupont
+"""
+__author__  ="Jean-Lou Dupont"
+__version__ ="0.2.7"
+
+
+from distutils.core import setup
+from setuptools import find_packages
+
+DESC="""
+Overview
+--------
+
+This package contains a collection of Amazon Web Service related scripts e.g.
+
+* jlds3upload : automated file upload to S3
+* jlds3download : automated file download from S3
+* jldrxsqs : receive from an SQS queue to JSON/string stdout
+* jldtxsqs : transmit on SQS JSON/string from stdin
+* jldleader: distributed leader election protocol manager
+* jlds3up  : simple file upload to s3 with functionality to delete 'old' files
+
+The philosophy behind these scripts is:
+
+* simplicity  : each script only does 1 thing
+* reporting   : status & error reporting through stderr and (optionally) JSON stdout
+* data driven : object in from src ==> object out to dst
+* data flow   : piping through stdin / stdout    
+
+Scripts can easily be "composed" through standard Linux piping :
+
+    jldrxsqs @config1.txt | some_other_command | jldtxsqs @config2.txt
+
+The data format used is JSON.
+
+Configuration
+-------------
+
+Can be performed through options on the command line or using a file (use a leading `@`).
+"""
+
+
+setup(name=         'jldaws',
+      version=      __version__,
+      description=  'Collection of Amazon AWS related scripts',
+      author=       __author__,
+      author_email= 'jl@jldupont.com',
+      url=          'http://www.systemical.com/doc/opensource/jldaws',
+      package_dir=  {'': "src",},
+      packages=     find_packages("src"),
+      scripts=      ['src/scripts/jldexec',
+                     'src/scripts/jlds3notify',
+                     'src/scripts/jldtxsqs',
+                     'src/scripts/jldrxsqs',     
+                     'src/scripts/jlds3upload',
+                     'src/scripts/jlds3download',
+                     'src/scripts/jldleader',
+                     'src/scripts/jlds3up',
+                     ],
+      zip_safe=False
+      ,long_description=DESC
+      ,install_requires=["pyfnc >= 0.1.2"]
+      )
---
+++ packages/jldaws/jldaws-0.2.7.zip/jldaws-0.2.7/src/jldaws/tools_sys.py
@@ -0,0 +1,101 @@
+"""
+    Created on 2012-01-26
+    @author: jldupont
+"""
+import shutil, sys, json, logging
+from time import sleep
+import functools
+
+
+class SignalTerminate(Exception): pass
+
+
+def coroutine(func):
+    @functools.wraps(func)
+    def start(*args, **kwargs):
+        cr=func(*args, **kwargs)
+        cr.next()
+        return cr
+    return start
+
+def jstdout(jo):
+    sys.stdout.write(json.dumps(jo)+"\n")
+
+
+def stdout(s):
+    sys.stdout.write(s+"\n")
+
+def dnorm(d):
+    """
+    Normalize dictionary
+    
+    >>> dnorm({"SoMeKeY":"  spaces  "})
+    {'somekey': 'spaces'}
+    """
+    r={}
+    for e in d:
+        try:    r[e.lower()]=d[e].strip()
+        except: r[e.lower()]=d[e]
+    return r
+
+
+def dstrip(d):
+    """
+    Strip each element in a dict
+    
+    >>> dstrip({"e1":" v1", "e2":" v2 "})
+    {'e1': 'v1', 'e2': 'v2'}
+    """
+    for e in d:
+        try:    d[e]=d[e].strip()
+        except: pass
+    return d
+
+
+def move(src_path, dst_path):
+    try:
+        shutil.move(src_path, dst_path)
+        return ("ok", None)
+    except Exception, e:
+        return ("error", e)
+
+
+def retry(f, always=True, min_wait=1, max_wait=30, max_retries=10, logmsg=None):
+    """
+    Retries function 'f' : the function should throw an exception to indicate failure 
+    
+    :param always: boolean, should always retry true/false
+    :param min_wait: int, minimum seconds between tries
+    :param max_wait: int, maximum seconds before restarting cycle
+    :param max_retries: int, maximum number of retries when 'always==False'
+    
+    @raise KeyboardInterrupt
+    @return f()
+    """
+    showed_msg=False
+    wait=min_wait
+    retry_count=max_retries
+    while True:
+        try:
+            return f()
+        except KeyboardInterrupt:
+            raise
+        except Exception, exp:   
+            if not showed_msg:
+                showed_msg=True
+                if logmsg is not None:
+                    logging.warning(logmsg)     
+            retry_count=max(retry_count-1, 0)
+            if not always and retry_count==0:
+                raise exp
+            
+        sleep(wait)
+        
+        wait=wait*2
+        wait=wait if wait<max_wait else min_wait
+
+
+
+if __name__=="__main__":
+    import doctest
+    doctest.testmod()
