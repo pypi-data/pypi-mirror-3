85bef4823b564ab2c4e928361cf87b72e8757a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:17 +0000

    Add package webbpsf/0.2.8/webbpsf-0.2.8.tar.gz

---
+++ packages/webbpsf/webbpsf-0.2.8.tar.gz/webbpsf-0.2.8/webbpsf/__init__.py
@@ -0,0 +1,9 @@
+﻿from .webbpsf import Instrument, NIRCam, NIRISS, NIRSpec,MIRI,FGS
+from .webbpsfgui import gui  # quick function to start the GUI and run it instantly
+from .webbpsfgui import WebbPSF_GUI  # actual GUI class 
+
+from poppy import (display_PSF, display_PSF_difference, display_EE, display_profiles, radial_profile,
+        measure_EE, measure_radial, measure_fwhm, measure_sharpness, measure_centroid, measure_strehl,
+        measure_anisotropy, specFromSpectralType, fwcentroid)
+
+from poppy._version import __version__
---
+++ packages/webbpsf/webbpsf-0.2.8.tar.gz/webbpsf-0.2.8/webbpsf/mockdms.py
@@ -0,0 +1,59 @@
+﻿#!/usr/bin/env python
+"""
+=====================
+Mock DMS for WebbPSF
+=====================
+
+
+This module provides a very simple mock version of the 
+JWST Data Management System (DMS) for use with WebbPSF. 
+Its purpose is to take the simple FITS files as produced by WebbPSF and
+format them into the somewhat more complicated data products expected from
+actual pipeline-processed JWST data, with realistic FITS header keywords 
+and so on. 
+
+In particular, it takes the small PSF arrays computed by WebbPSF (which will typically
+be at most some tens of arcsec across) and embeds them into a full-sized detector array.
+
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as pl
+import pyfits
+from IPython.core.debugger import Tracer; stop = Tracer()
+import logging
+_log = logging.getLogger('webbpsf')
+
+
+class MockDMS(object):
+    def __init__(self):
+        pass
+
+    def convert(self, fitsfile):
+        """
+        Convert a given file
+
+        Parameters
+        ----------
+        HDUlist_or_filename : pyfits.HDUlist or string
+        FITS file containing image to display.
+ 
+        """
+        if isinstance(fitsfile, str):
+            HDUlist = pyfits.open(fitsfile)
+        elif isinstance(fitsfile, pyfits.HDUList):
+            HDUlist = fitsfile
+        else: raise ValueError("input must be a filename or HDUlist")
+
+
+
+
+
+if __name__ == "__main__":
+        
+    logging.basicConfig(level=logging.INFO, format='%(name)-12s: %(levelname)-8s %(message)s',)
+
+    pass
+
+
