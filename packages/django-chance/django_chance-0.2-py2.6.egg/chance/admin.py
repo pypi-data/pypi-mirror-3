04f17c9faa6597e431a76ebc8f48906476704bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:20 +0000

    Add package django-chance/0.2/django_chance-0.2-py2.6.egg

---
+++ packages/django-chance/django_chance-0.2-py2.6.egg/chance/admin.py
@@ -0,0 +1,51 @@
+try:
+    from reversion import VersionAdmin as ModelAdmin
+except ImportError:
+    from django.contrib.admin import ModelAdmin
+
+from django.contrib import admin
+
+from chance.models import (Event, EventFee, EventChoice, EventChoiceOption,
+        Registration, EventChoiceSelection, Talk)
+
+
+
+class EventFeeInlineAdmin(admin.TabularInline):
+    model = EventFee
+    extra = 1
+
+class EventChoiceInlineAdmin(admin.StackedInline):
+    model = EventChoice
+    extra = 1
+
+class EventAdmin(ModelAdmin):
+    date_hierarchy = 'starts'
+    inlines = [
+        EventFeeInlineAdmin,
+        EventChoiceInlineAdmin
+    ]
+
+class EventChoiceOptionInlineAdmin(admin.TabularInline):
+    model = EventChoiceOption
+    extra = 1
+
+
+class EventChoiceAdmin(ModelAdmin):
+    inlines = [
+        EventChoiceOptionInlineAdmin
+    ]
+    exclude = ('event',)
+
+class RegistrationAdmin(ModelAdmin):
+    model = Registration
+    exclude = ('event',)
+    list_filter = ('event', 'paid',)
+
+class TalkAdmin(ModelAdmin):
+    list_filter = ('event', 'accepted',)
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(EventChoice, EventChoiceAdmin)
+admin.site.register(Registration, RegistrationAdmin)
+admin.site.register(Talk, TalkAdmin)
+
