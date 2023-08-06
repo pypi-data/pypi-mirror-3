f50ee60316dc2009f24c1d2cd3c78fe1d7783f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:17 +0000

    Add package config-resolver/1.1/config_resolver-1.1.tar.gz

---
+++ packages/config-resolver/config_resolver-1.1.tar.gz/config_resolver-1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+PACKAGE = "config_resolver"
+NAME = "config_resolver"
+DESCRIPTION = "A small package to automatically find a configuration file."
+AUTHOR = "Michel Albert"
+AUTHOR_EMAIL = "michel@albert.lu"
+VERSION = __import__(PACKAGE).__version__
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    url='https://github.com/exhuma/config_resolver',
+    long_description=open("README.txt").read(),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="LGPL",
+    packages=find_packages(exclude=["tests.*", "tests"]),
+)
