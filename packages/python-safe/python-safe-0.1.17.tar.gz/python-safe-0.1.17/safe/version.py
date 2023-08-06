e06e00134c1ea47d02f7bc27049464646d8996a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:34 +0000

    Add package python-safe/0.1.17/python-safe-0.1.17.tar.gz

---
+++ packages/python-safe/python-safe-0.1.17.tar.gz/python-safe-0.1.17/safe/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SAFESETUP.PY
+short_version = '0.1.17'
+version = '0.1.17'
+full_version = '0.1.17'
+git_revision = 'd05255ac744abf9662d26861e3086bc8aec90fcb'
+release = True
+
+if not release:
+    version = full_version
