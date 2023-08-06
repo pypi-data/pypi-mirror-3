52bd58f91e8d3049ff286058aa68d4b75e9820bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:47 +0000

    Add package worldcat/0.3.6/worldcat-0.3.6.tar.gz

---
+++ packages/worldcat/worldcat-0.3.6.tar.gz/worldcat-0.3.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+install_requires = ['pymarc']
+
+classifiers = """
+Intended Audience :: Education
+Intended Audience :: Developers
+Intended Audience :: Information Technology
+License :: OSI Approved :: BSD License
+License :: OSI Approved :: GNU General Public License (GPL)
+Programming Language :: Python
+Development Status :: 4 - Beta
+"""
+
+setup(
+    name = 'worldcat',
+    version = '0.3.6',  # remember to update worldcat/__init__.py on release!
+    url = 'http://github.com/anarchivist/worldcat',
+    author = 'Mark A. Matienzo',
+    author_email = 'mark@matienzo.org',
+    license = 'GPL/BSD',
+    packages = find_packages(),
+    install_requires = install_requires,
+    description = 'Interact with OCLC\'s WorldCat Affiliate APIs',
+    classifiers = filter(None, classifiers.split('\n')),
+)
