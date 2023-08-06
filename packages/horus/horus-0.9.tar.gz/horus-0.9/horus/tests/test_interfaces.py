4a06b93f9973203b1ce1c4065a161d1e78122b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:27 +0000

    Add package horus/0.9/horus-0.9.tar.gz

---
+++ packages/horus/horus-0.9.tar.gz/horus-0.9/horus/tests/test_interfaces.py
@@ -0,0 +1,29 @@
+from horus.tests import UnitTestBase
+
+class TestInterfaces(UnitTestBase):
+    def test_susession(self):
+        """ Shouldn't be able to instantiate the interface """
+        from horus.interfaces import IHorusSession
+
+        def make_session():
+            IHorusSession('1')
+
+        self.assertRaises(TypeError, make_session)
+
+    def test_suloginschema(self):
+        """ Shouldn't be able to instantiate the interface """
+        from horus.interfaces import IHorusLoginSchema
+
+        def make_session():
+            IHorusLoginSchema('1')
+
+        self.assertRaises(TypeError, make_session)
+
+    def test_suloginform(self):
+        """ Shouldn't be able to instantiate the interface """
+        from horus.interfaces import IHorusLoginForm
+
+        def make_session():
+            IHorusLoginForm('1')
+
+        self.assertRaises(TypeError, make_session)
