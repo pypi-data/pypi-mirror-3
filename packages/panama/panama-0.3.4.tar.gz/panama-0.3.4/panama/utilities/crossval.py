9403413004cb3429fabda9fe73606d2fab979c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:46 +0000

    Add package panama/0.3.4/panama-0.3.4.tar.gz

---
+++ packages/panama/panama-0.3.4.tar.gz/panama-0.3.4/panama/utilities/crossval.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def k_fold_cross_validation(X, K, randomise = False):
+	"""
+	Generates K (training, validation) pairs from the items in X.
+
+	Each pair is a partition of X, where validation is an iterable
+	of length len(X)/K. So each training iterable is of length (K-1)*len(X)/K.
+
+	If randomise is true, a copy of X is shuffled before partitioning,
+	otherwise its order is preserved in training and validation.
+	"""
+	if randomise: from random import shuffle; X=list(X); shuffle(X)
+	for k in xrange(K):
+		training = [x for i, x in enumerate(X) if i % K != k]
+		validation = [x for i, x in enumerate(X) if i % K == k]
+		yield training, validation
+
+def RMSE(Y, Y_pred):
+    return np.sqrt(np.power(Y - Y_pred, 2).mean())
