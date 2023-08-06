a575217aadcba991bc35d80602f59080abb6e32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:33 +0000

    Add package longcat/1.0/longcat-1.0.tar.gz

---
+++ packages/longcat/longcat-1.0.tar.gz/longcat-1.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="longcat",
+    version="1.0",
+    description="Adding some longcat to your command line.",
+    author="Zach Williams",
+    author_email="hey@zachwill.com",
+    url="http://github.com/zachwill/longcat",
+    license="MIT",
+    classifiers=[
+    ],
+    scripts=[
+        "longcat"
+    ],
+)
