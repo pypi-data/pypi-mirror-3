2f1d8499179d165bb7addfbf5c07c2a1de11f557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:06 +0000

    Add package appspace/0.5.3/appspace-0.5.3.tar.bz2

---
+++ packages/appspace/appspace-0.5.3.tar.bz2/appspace-0.5.3/appspace/utils.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+'''appspace utilities'''
+
+from keyword import iskeyword
+
+from importlib import import_module
+
+from stuf.six import strings
+
+__all__ = ('checkname', 'lazyimport')
+
+
+def lazyimport(path, attribute=None):
+    '''
+    deferred module loader
+
+    @param path: something to load
+    @param attribute: attribute on loaded module to return
+    '''
+    if isinstance(path, strings):
+        try:
+            dot = path.rindex('.')
+            # import module
+            path = getattr(import_module(path[:dot]), path[dot + 1:])
+        # If nothing but module name, import the module
+        except (AttributeError, ValueError):
+            path = import_module(path)
+        if attribute:
+            path = getattr(path, attribute)
+    return path
+
+
+class CheckName(object):
+
+    '''ensures string is a legal Python name'''
+
+    # Illegal characters for Python names
+    ic = '()[]{}@,:`=;+*/%&|^><\'"#\\$?!~'
+
+    def __call__(self, name):
+        '''
+        ensures a string is a legal python name
+
+        @param name: name to check
+        '''
+        # Remove characters that are illegal in a Python name
+        name = name.strip().lower().replace('-', '_').replace(
+            '.', '_'
+        ).replace(' ', '_')
+        name = ''.join(i for i in name if i not in self.ic)
+        # Add _ if value is a Python keyword
+        return name + '_' if iskeyword(name) else name
+
+
+checkname = CheckName()
