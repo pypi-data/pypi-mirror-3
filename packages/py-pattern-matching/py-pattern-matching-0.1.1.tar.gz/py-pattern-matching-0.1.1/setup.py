53eed9565e12ab0cebc4b9ae854b2c8777cb2a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:34 +0000

    Add package py-pattern-matching/0.1.1/py-pattern-matching-0.1.1.tar.gz

---
+++ packages/py-pattern-matching/py-pattern-matching-0.1.1.tar.gz/py-pattern-matching-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-pattern-matching",
+    version="0.1.1",
+    packages=find_packages(),
+
+    author="Erik Allik",
+    author_email="eallik@gmail.com",
+    url='https://github.com/eallik/py-pattern-matching',
+
+    description="Data-structure pattern matching and unpacking for Python",
+    long_description=open('README').read(),
+)
