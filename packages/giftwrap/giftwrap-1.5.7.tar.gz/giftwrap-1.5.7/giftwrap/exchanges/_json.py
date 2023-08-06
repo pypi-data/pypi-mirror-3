0ba49867ceb1259045b3c65575c9ad8bab56e615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:48 +0000

    Add package giftwrap/1.5.7/giftwrap-1.5.7.tar.gz

---
+++ packages/giftwrap/giftwrap-1.5.7.tar.gz/giftwrap-1.5.7/giftwrap/exchanges/_json.py
@@ -0,0 +1,29 @@
+try:
+    import ujson as json
+except ImportError:
+    try:
+        import simplejson as json
+    except ImportError:
+        import json
+from ..exchange import Exchange
+
+
+class JsonExchange(Exchange):
+    headers = { 'Content-Type':'application/json', 'Accept':'application/json' }
+
+    def process_response(self, response): 
+        data = None
+        if response.text and response.text.strip():
+            data = json.loads(response.text)
+        return self.process_data(data, response)
+
+    def process_data(self, data, response): return NotImplementedError()
+
+    def data(self): 
+        _data = self.python_data()
+        if _data is None: return None
+        return json.dumps(self.python_data())
+        
+    def python_data(self): return None
+
+
