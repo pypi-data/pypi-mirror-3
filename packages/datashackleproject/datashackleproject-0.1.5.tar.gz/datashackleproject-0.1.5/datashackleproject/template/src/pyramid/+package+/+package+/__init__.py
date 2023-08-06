874e92c724ddd70106ee30343027c2d2e56f6cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:15 +0000

    Add package datashackleproject/0.1.5/datashackleproject-0.1.5.tar.gz

---
+++ packages/datashackleproject/datashackleproject-0.1.5.tar.gz/datashackleproject-0.1.5/datashackleproject/template/src/pyramid/+package+/+package+/__init__.py
@@ -0,0 +1,19 @@
+from pyramid.config import Configurator
+from p2.datashackle.core import init_datashackle_core
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+
+    config = Configurator(settings=settings)
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.add_route('media', '/media/{id}')
+    config.scan()
+   
+    # This MUST happen after configuration scan 
+    init_datashackle_core(settings)
+    
+    return config.make_wsgi_app()
+
+
---
+++ packages/datashackleproject/datashackleproject-0.1.5.tar.gz/datashackleproject-0.1.5/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.5'
+
+_description = (
+    "Script that creates a Datashackle skeleton for "
+    "a new Datashackle web application."
+    )
+
+long_description = (
+    "===========\n"
+    "Datashackle\n"
+    "===========\n"
+    "\n"
+    "%s\n"
+    "\n"
+    ".. contents::\n"
+    "\n"
+    "Description\n"
+    "===========\n"
+    "\n" +
+    open('README.txt').read() +
+    '\n' +
+    open('CHANGES.txt').read()
+    ) % _description
+
+setup(
+    name='datashackleproject',
+    version=VERSION,
+    author='Michael Jenny',
+    author_email='michaelj@datashackle.net',
+    url='http://datashackle.net',
+    download_url='',
+    description=_description,
+    long_description=long_description,
+    license='GPL',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['PasteScript>=1.6'],
+    test_suite='tests.test_suite',
+    entry_points={
+        # Later maybe a dedicated script in /usr/local/bin ..
+        #'console_scripts': ['datashackleproject = datashackleproject:main'],
+        'paste.paster_create_template': ['datashackle = datashackleproject:DatashackleProject']},
+    )
