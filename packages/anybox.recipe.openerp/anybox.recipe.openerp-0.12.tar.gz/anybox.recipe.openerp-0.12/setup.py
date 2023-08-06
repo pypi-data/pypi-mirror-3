17c17220fb98dc4c854aff8fefebf60617a23d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:07 +0000

    Add package anybox.recipe.openerp/0.12/anybox.recipe.openerp-0.12.tar.gz

---
+++ packages/anybox.recipe.openerp/anybox.recipe.openerp-0.12.tar.gz/anybox.recipe.openerp-0.12/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '0.12'
+
+requires = ['setuptools', 'zc.recipe.egg', 'zc.buildout']
+
+setup(
+    name = "anybox.recipe.openerp",
+    version = version,
+    author="Anybox",
+    author_email="contact@anybox.fr",
+    description="A buildout recipe to install and configure OpenERP",
+    license="ZPL",
+    long_description=open('README.txt').read() + open('CHANGES.txt').read(),
+    url="https://code.launchpad.net/~anybox/+junk/anybox.recipe.openerp",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    namespace_packages=['anybox', 'anybox.recipe'],
+    install_requires=requires,
+    tests_require=requires + ['nose'],
+    classifiers=[
+      'Framework :: Buildout',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+    entry_points = {'zc.buildout': [
+        'server = anybox.recipe.openerp:ServerRecipe',
+        'webclient = anybox.recipe.openerp:WebClientRecipe',
+        'gtkclient = anybox.recipe.openerp:GtkClientRecipe',
+        ]},
+    )
+
+
