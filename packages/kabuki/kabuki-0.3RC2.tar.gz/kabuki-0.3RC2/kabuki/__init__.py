daa4384e22b63a500dc1de13682ddec87c471785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:24 +0000

    Add package kabuki/0.3RC2/kabuki-0.3RC2.tar.gz

---
+++ packages/kabuki/kabuki-0.3RC2.tar.gz/kabuki-0.3RC2/kabuki/__init__.py
@@ -0,0 +1,17 @@
+from hierarchical import *
+
+import utils
+import analyze
+import step_methods as steps
+import generate
+import distributions
+
+__version__ = '0.3RC1'
+
+try:
+    from IPython.core.debugger import Tracer; debug_here = Tracer()
+except:
+    try:
+        from IPython.Debugger import Tracer; debug_here = Tracer()
+    except:
+        def debug_here(): pass
