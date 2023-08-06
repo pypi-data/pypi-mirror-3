645a7e1c8c105bd23a48147bd6f5c52738f5fff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:41 +0000

    Add package Pytoad/1.0.4/Pytoad-1.0.4.tar.gz

---
+++ packages/Pytoad/Pytoad-1.0.4.tar.gz/Pytoad-1.0.4/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name="Pytoad",
+      version="1.0.4",
+      description="Pytoad is a lightweight Hoptoad notifier for Python.",
+      author="Benjamin Coe",
+      author_email="ben@attachments.me",
+      entry_points = {},
+      url="https://github.com/attachmentsme/Pytoad",
+      packages = find_packages(),
+      install_requires = ['xmlbuilder', 'simplejson'],
+      tests_require=['nose', 'coverage', 'mock']
+)
