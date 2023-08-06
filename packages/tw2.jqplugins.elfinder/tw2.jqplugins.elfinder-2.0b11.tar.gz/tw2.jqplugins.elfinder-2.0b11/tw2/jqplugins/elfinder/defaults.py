4c5882dc634751387c66567c9125afacae79e223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:43 +0000

    Add package tw2.jqplugins.elfinder/2.0b11/tw2.jqplugins.elfinder-2.0b11.tar.gz

---
+++ packages/tw2.jqplugins.elfinder/tw2.jqplugins.elfinder-2.0b11.tar.gz/tw2.jqplugins.elfinder-2.0b11/tw2/jqplugins/elfinder/defaults.py
@@ -0,0 +1,6 @@
+#jQuery.ui
+_plugin_css_dirname_ = 'static/jquery/plugins/%(name)s/%(version)s/%(subdir)s'
+_elfinder_name_       = 'elfinder'
+_elfinder_basename_       = 'elfinder'
+_elfinder_version_        = 'trunk'
+_elfinder_debug_          = 'full'
