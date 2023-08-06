c8d063a6fc5d8a6df9ed2e71f2cc182edbfcf8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:09 +0000

    Add package coop-cms/0.4.6/coop-cms-0.4.6.zip

---
+++ packages/coop-cms/coop-cms-0.4.6.zip/coop-cms-0.4.6/coop_cms/apps/demo_cms/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+def get_article_templates(article, user):
+    return (
+        ('standard.html', 'Standard'),
+        ('homepage.html', 'Homepage'),
+        ('blog.html', 'Blog'),
+    )
