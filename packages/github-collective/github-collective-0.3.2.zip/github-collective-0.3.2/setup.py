30307172dca3822c6dd3988ab7410c2a6bf9cdc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:41 +0000

    Add package github-collective/0.3.2/github-collective-0.3.2.zip

---
+++ packages/github-collective/github-collective-0.3.2.zip/github-collective-0.3.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+version = '0.3.2'
+
+setup(
+    name='github-collective',
+    version=version,
+    description="Script to manage GitHub organizations in a collective manner",
+    long_description=open("README.rst").read() + "\n" + \
+                     open("CHANGES.rst").read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        #"Programming Language :: Python :: 2.7",
+        #"Programming Language :: Python :: 3.2",
+        ],
+    keywords='github git permission collaboration collective organization',
+    author='Rok Garbas',
+    author_email='rok@garbas.si',
+    url='https://github.com/collective/github-collective',
+    license='BSD',
+    packages=['githubcollective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'argparse',
+        'requests==0.13.1',
+        ],
+    extras_require={
+        'test': ['nose']
+    },
+    entry_points="""
+        [console_scripts]
+        github-collective = githubcollective:run
+        """,
+    )
