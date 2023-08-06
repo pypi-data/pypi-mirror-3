44e4a07314407ace2b68dd1feaf8e0ecf5ee8d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:54 +0000

    Add package bigsignal/0.0.1/bigsignal-0.0.1.tar.gz

---
+++ packages/bigsignal/bigsignal-0.0.1.tar.gz/bigsignal-0.0.1/eventable.py
@@ -0,0 +1,40 @@
+import logging
+
+class Eventable(object):
+    """
+    base class for object you want to handle events
+    uses a simple event tracking system
+    """
+    def __init__(self):
+        self.__handlers = {}
+
+    def on(self,event,handler):
+        """
+        add a listener
+        """
+        logging.debug('adding handler: %s' % event)
+        self.__handlers.setdefault(event,[]).append(handler)
+        return self
+
+    def un(self,event,handler):
+        """
+        remove a listener
+        """
+        if event in self.__handlers:
+            try:
+                self.__handlers[event].remove(handler)
+            except ValueError:
+                pass # nothing to remove
+        return self
+
+    def fire(self,event,*args,**kwargs):
+        """
+        fire the event. aka call the handlers
+        and pass them the args, kwargs
+        """
+        logging.debug('firing %s' % event)
+        for handler in self.__handlers.get(event,[]):
+            logging.debug('firing handler')
+            handler(*args,**kwargs)
+        return self
+
