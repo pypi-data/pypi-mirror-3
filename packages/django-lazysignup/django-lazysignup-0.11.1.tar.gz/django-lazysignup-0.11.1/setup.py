ceeb3362eea1ba58c5b1a2b85ec0f512471d7de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:28 +0000

    Add package django-lazysignup/0.11.1/django-lazysignup-0.11.1.tar.gz

---
+++ packages/django-lazysignup/django-lazysignup-0.11.1.tar.gz/django-lazysignup-0.11.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.11.1'
+
+setup(name='django-lazysignup',
+      version=version,
+      description="Lazy signup for Django",
+      long_description=open("README").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License"],
+      keywords='django lazy signup app user',
+      author='Dan Fairs',
+      author_email='dan@fezconsulting.com',
+      url='http://github.com/danfairs/django-lazysignup',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      package_data={
+        'lazysignup': ['templates/lazysignup/*html'],
+        '': ['*.txt', '*.rst'],
+      },
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools', 'Django'],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
