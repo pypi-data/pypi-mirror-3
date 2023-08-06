bf45640f1ec26cef278739f7ed3a5dcef941aa21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:17 +0000

    Add package three/0.2/three-0.2.tar.gz

---
+++ packages/three/three-0.2.tar.gz/three-0.2/setup.py
@@ -0,0 +1,36 @@
+"""
+Setup and installation for the package.
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name="three",
+    version="0.2",
+    author="Zach Williams",
+    author_email="hey@zachwill.com",
+    description="An easy-to-use wrapper for the Open311 API",
+    long_description=open('README.md').read(),
+    packages=[
+        'three'
+    ],
+    install_requires=[
+        'mock',
+        'simplejson',
+        'requests',
+    ],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
