9dd14a938ba4251594a4c81e3e692cc0f575b971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:10 +0000

    Add package monet.calendar.extensions/0.9.7/monet.calendar.extensions-0.9.7.tar.gz

---
+++ packages/monet.calendar.extensions/monet.calendar.extensions-0.9.7.tar.gz/monet.calendar.extensions-0.9.7/monet/calendar/extensions/interfaces.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from zope.interface import Interface
+from monet.calendar.event.interfaces import IMonetCalendar
+
+from zope.app.publisher.interfaces.browser import IBrowserMenu
+from zope.app.publisher.interfaces.browser import IBrowserSubMenuItem
+
+class IMonetCalendarSection(IMonetCalendar):
+    """Identifies folders on which you can use the view search events"""
+    
+class IMonetCalendarSearchRoot(Interface):
+    """Identifies sub-folders, called sub-sites"""
+
+class ICalendarMenu(IBrowserMenu):
+    """The calendar menu.
+    """
+
+class ICalendarSubMenuItem(IBrowserSubMenuItem):
+    """The menu item linking to the calendar menu.
+    """
+
+class IMonetCalendarExtensionsLayer(Interface):
+    """Marker interface for the monet.calendar.extensions layer"""
