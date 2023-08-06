4330daba168aa11f68ddd7c88d861643830026d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:20 +0000

    Add package collective.noindexing/1.2/collective.noindexing-1.2.zip

---
+++ packages/collective.noindexing/collective.noindexing-1.2.zip/collective.noindexing-1.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+version = '1.2'
+
+setup(name='collective.noindexing',
+      version=version,
+      description="Ignore all indexing operations",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 3.3",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Maurits van Rees',
+      author_email='m.van.rees@zestsoftware.nl',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+          'test': ['plone.app.testing'],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
