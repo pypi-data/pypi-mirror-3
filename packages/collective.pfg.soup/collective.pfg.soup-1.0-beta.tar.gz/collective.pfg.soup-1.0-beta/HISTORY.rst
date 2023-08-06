7d51a32c357b4da08923e0aacf5226c92dc3d366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:36 +0000

    Add package collective.pfg.soup/1.0-beta/collective.pfg.soup-1.0-beta.tar.gz

---
+++ packages/collective.pfg.soup/collective.pfg.soup-1.0-beta.tar.gz/collective.pfg.soup-1.0-beta/HISTORY.rst
@@ -0,0 +1,8 @@
+
+History
+=======
+
+1.0-beta
+--------
+
+- make it work [jensens, 2012-07-03]
---
+++ packages/collective.pfg.soup/collective.pfg.soup-1.0-beta.tar.gz/collective.pfg.soup-1.0-beta/src/collective/pfg/soup/browser/edit.py
@@ -0,0 +1,20 @@
+import json
+from Acquisition import aq_parent
+from Products.Five import BrowserView
+
+
+class EditData(BrowserView):
+
+    def __call__(self):
+        soup = self.context.get_soup()
+        iid = int(self.request.get('iid'))
+        record = soup.get(iid)
+        data = {}
+        for name in record.attrs:
+            if name.startswith('_auto_'):
+                continue
+            data[name] = record.attrs[name]
+            # XXX binary data not handled
+        result = dict(data=data)
+        result['url'] = aq_parent(self.context).absolute_url()
+        return json.dumps(data)
