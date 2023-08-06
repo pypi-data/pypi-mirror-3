693ffa40d65a58e82bae008fa1438c937beefef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:50 +0000

    Add package django-weekday-field/0.1.2/django-weekday-field-0.1.2.tar.gz

---
+++ packages/django-weekday-field/django-weekday-field-0.1.2.tar.gz/django-weekday-field-0.1.2/weekday_field/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+import utils
+import operator
+
+class WeekdayFormField(forms.TypedMultipleChoiceField):
+    def __init__(self, *args, **kwargs):
+        if 'choices' not in kwargs:
+          kwargs['choices'] = utils.DAY_CHOICES
+        kwargs.pop('max_length', None)
+        if 'widget' not in kwargs:
+          kwargs['widget'] = forms.widgets.SelectMultiple
+        super(WeekdayFormField, self).__init__(*args, **kwargs)
+        
+    def clean(self, value):
+      value = super(WeekdayFormField, self).clean(value)
+      return value
+
+class BitwiseWeekdayFormField(WeekdayFormField):
+  def __init__(self, *args, **kwargs):
+    if 'short' in kwargs:
+      if kwargs['short']:
+        kwargs['choices'] = [(x[0],x[1]) for x in utils.BITWISE_DAY_CHOICES]
+      del kwargs['short']
+    else:
+      kwargs['choices'] = [(x[0],x[2]) for x in utils.BITWISE_DAY_CHOICES]
+    super(BitwiseWeekdayFormField, self).__init__(*args, **kwargs)
+
+  def clean(self,value):
+    value = [int(x) for x in value]
+    value = reduce(operator.or_, value)
+    return value
---
+++ packages/django-weekday-field/django-weekday-field-0.1.2.tar.gz/django-weekday-field-0.1.2/weekday_field/utils.py
@@ -0,0 +1,19 @@
+DAY_CHOICES = (
+    (0, "Monday"),
+    (1, "Tuesday"),
+    (2, "Wednesday"),
+    (3, "Thursday"),
+    (4, "Friday"),
+    (5, "Saturday"),
+    (6, "Sunday")
+)
+
+BITWISE_DAY_CHOICES = (
+    (1, "Sun","Sunday"),
+    (2, "Mon","Monday"),
+    (4, "Tue","Tuesday"),
+    (8, "Wed","Wednesday"),
+    (16, "Thu","Thursday"),
+    (32, "Fri","Friday"),
+    (64, "Sat","Saturday"),
+)
