45459a96005b6f4dd860f223fb4325a32039714e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:09 +0000

    Add package django-cities-light/1.7rc2/django-cities-light-1.7rc2.tar.gz

---
+++ packages/django-cities-light/django-cities-light-1.7rc2.tar.gz/django-cities-light-1.7rc2/cities_light/admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+
+from .models import *
+from .settings import *
+
+
+class CountryAdmin(admin.ModelAdmin):
+    """
+    ModelAdmin for Country.
+    """
+
+    list_display = (
+        'name',
+        'code2',
+        'code3',
+        'continent',
+        'tld',
+    )
+    search_fields = (
+        'name',
+        'name_ascii',
+        'code2',
+        'code3',
+        'tld'
+    )
+    list_filter = (
+        'continent',
+    )
+admin.site.register(Country, CountryAdmin)
+
+
+class CityAdmin(admin.ModelAdmin):
+    """
+    ModelAdmin for City.
+    """
+    list_display = (
+        'name',
+        'country',
+    )
+    search_fields = (
+        'name',
+        'search_names',
+    )
+    list_filter = (
+        'country__continent',
+        'country',
+    )
+
+admin.site.register(City, CityAdmin)
