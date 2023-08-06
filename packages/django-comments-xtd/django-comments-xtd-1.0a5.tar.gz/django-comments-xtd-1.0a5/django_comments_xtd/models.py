4be8bafa0c401e103f5fa6c895bbe52703b83160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:03 +0000

    Add package django-comments-xtd/1.0a5/django-comments-xtd-1.0a5.tar.gz

---
+++ packages/django-comments-xtd/django-comments-xtd-1.0a5.tar.gz/django-comments-xtd-1.0a5/django_comments_xtd/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from django.contrib.comments.models import Comment
+from django.contrib.contenttypes.models import ContentType
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
+
+
+class XtdCommentManager(models.Manager):
+    def for_app_models(self, *args):
+        """Return XtdComments for pairs "app.model" given in args"""
+        content_types = []
+        for app_model in args:
+            app, model = app_model.split(".")
+            content_types.append(ContentType.objects.get(app_label=app, 
+                                                         model=model))
+        return self.for_content_types(content_types)
+
+    def for_content_types(self, content_types):
+        qs = self.get_query_set().filter(content_type__in=content_types).reverse()
+        return qs
+
+
+class XtdComment(Comment):
+    followup = models.BooleanField(help_text=_("Receive by email further comments in this conversation"), blank=True)
+    objects = XtdCommentManager()
+
+
+class DummyDefaultManager:
+    """
+    Dummy Manager to mock django's CommentForm.check_for_duplicate method.
+    """
+    def __getattr__(self, name):
+        return lambda *args, **kwargs: []
+    
+    def using(self, *args, **kwargs):
+        return self
+
+    # def __repr__(self):
+    #     return ""
+
+
+class TmpXtdComment(dict):
+    """
+    Temporary XtdComment to be pickled, ziped and appended to a URL.
+    """
+    _default_manager = DummyDefaultManager()
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            return None
+
+    def __setattr__(self, key, value):
+        self[key] = value
+            
+    def save(self, *args, **kwargs):
+        pass
+
+    def _get_pk_val(self):
+        if self.xtd_comment:
+            return self.xtd_comment._get_pk_val()
+        else:
+            return ""
+
+    def __reduce__(self):
+        return (TmpXtdComment, (), None, None, self.iteritems())
---
+++ packages/django-comments-xtd/django-comments-xtd-1.0a5.tar.gz/django-comments-xtd-1.0a5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test
+
+def run_tests(*args):
+    import subprocess
+    subprocess.Popen(["python", "tests/runtests.py"]).wait()
+
+test.run_tests = run_tests
+
+setup(
+    name = "django-comments-xtd",
+    version = "1.0a5",
+    packages = find_packages(),
+    license = "MIT",
+    description = "Django Comments Framework extension app with follow up notifications and email confirmations",
+    long_description = "A reusable Django app that extends the built-in Django's Comments Framework with following up notifications and comments that only hits the database after users confirm them by email.",
+    author = "Daniel Rus Morales",
+    author_email = "inbox@danir.us",
+    maintainer = "Daniel Rus Morales",
+    maintainer_email = "inbox@danir.us",
+    url = "http://pypi.python.org/pypi/django-comments-xtd",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    test_suite = "dummy",
+)
