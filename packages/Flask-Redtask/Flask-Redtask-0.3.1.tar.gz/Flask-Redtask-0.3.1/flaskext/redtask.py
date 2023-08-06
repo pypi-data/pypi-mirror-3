2ab2d6dada3dc0622bd3b416c53cf6b72af54f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:40 +0000

    Add package Flask-Redtask/0.3.1/Flask-Redtask-0.3.1.tar.gz

---
+++ packages/Flask-Redtask/Flask-Redtask-0.3.1.tar.gz/Flask-Redtask-0.3.1/flaskext/redtask.py
@@ -0,0 +1,68 @@
+import json
+from functools import wraps
+
+import memcache
+from flask import Blueprint, request, current_app
+from itsdangerous import Signer
+
+app = Blueprint('task', __name__)
+
+@app.route('/_ah/task', methods=['POST'])
+def task_run():
+    signer = Signer(current_app.secret_key)
+
+    _data = request.form['_']
+    data = json.loads(signer.unsign(_data))
+
+    callname = data.get('call')
+    a, kw = data['args'], data['kwargs']
+
+    modname, symname = callname.rsplit('.', 1)
+    mod = __import__(modname, fromlist=[symname])
+    call = getattr(mod, symname)
+
+    call.direct(*a, **kw)
+    return 'ok'
+
+def get_mc():
+    mc = memcache.Client(['127.0.0.1:33211'])
+    return mc
+
+mc = get_mc()
+
+def _defer(f, *a, **kw):
+
+    signer = Signer(current_app.secret_key)
+
+    env = [
+    ]
+
+    data = json.dumps({
+        "args": a,
+        "kwargs": kw,
+        "env": env,
+        "call": "%s.%s" % (f.__module__,f.__name__),
+    })
+
+    task = {
+        "url": request.url_root+'_ah/task',
+            "method": "POST",
+            "body": { "_": signer.sign(data) },
+    }
+    mc.set('task:url', json.dumps(task))
+
+def defer(f):
+    @wraps(f)
+    def defered(*a, **kw):
+        return _defer(f, *a, **kw)
+
+    defered.direct = f
+    defered.defer = defered
+
+    return defered
+
+def push(push_id, data):
+    mc.set('task:ws', json.dumps({
+        "_id": push_id,
+        "payload": data,
+    }))
