70637b28e9ab5e6ed4c31a1b41ad4aee5b3064e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:30 +0000

    Add package py-sample/0.1/py-sample-0.1.tar.gz

---
+++ packages/py-sample/py-sample-0.1.tar.gz/py-sample-0.1/py_sample/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# Filename: hello.py
+
+def world():
+  print 'Hello World!'
+  return "Hello from Py-Sample"
+
+version = '0.1'
+
+# End of hello.py
---
+++ packages/py-sample/py-sample-0.1.tar.gz/py-sample-0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='py-sample',
+    version='0.1',
+    description='Sample package for Gemfury',
+    author='Michael Rykov',
+    author_email='michael@gemfury.com',
+    url='http://www.gemfury.com',
+    packages=['py_sample'])
