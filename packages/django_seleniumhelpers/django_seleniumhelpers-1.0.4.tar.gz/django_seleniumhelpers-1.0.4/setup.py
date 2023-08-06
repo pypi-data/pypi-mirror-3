e645e59a9c89bd94df3657cee74b8a5f95bd418b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:45 +0000

    Add package django_seleniumhelpers/1.0.4/django_seleniumhelpers-1.0.4.tar.gz

---
+++ packages/django_seleniumhelpers/django_seleniumhelpers-1.0.4.tar.gz/django_seleniumhelpers-1.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from seleniumhelpers_version import version
+
+setup(name = 'django_seleniumhelpers',
+      description = 'Extends django.test.LiveServerTestCase to make selenium testing even easier.',
+      version = version,
+      license='BSD',
+      url = 'https://github.com/espenak/django_seleniumhelpers',
+      author = 'Espen Angell Kristiansen',
+      packages=find_packages(exclude=['ez_setup']),
+      install_requires = ['distribute', 'Django', 'selenium'],
+      include_package_data=True,
+      zip_safe=False,
+      long_description='See http://github.com/espenak/django_seleniumhelpers',
+      classifiers=[
+                   'Development Status :: 5 - Production/Stable',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python'
+                  ]
+)
