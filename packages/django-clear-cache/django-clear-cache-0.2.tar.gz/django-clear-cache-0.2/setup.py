597e51341e4cc78b3410a5a0f90ded2f972bd7a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:14 +0000

    Add package django-clear-cache/0.2/django-clear-cache-0.2.tar.gz

---
+++ packages/django-clear-cache/django-clear-cache-0.2.tar.gz/django-clear-cache-0.2/setup.py
@@ -0,0 +1,30 @@
+from os.path import abspath, dirname, join, normpath
+
+from setuptools import setup
+
+
+setup(
+
+    # Basic package information:
+    name = 'django-clear-cache',
+    version = '0.2',
+    packages = ('clear_cache',),
+
+    # Packaging options:
+    zip_safe = False,
+    include_package_data = True,
+
+    # Package dependencies:
+    install_requires = ['Django>=1.0'],
+
+    # Metadata for PyPI:
+    author = 'Randall Degges',
+    author_email = 'rdegges@gmail.com',
+    license = 'UNLICENSE',
+    url = 'https://github.com/rdegges/django-clear-cache',
+    keywords = 'django cache management memcached clear',
+    description = 'A simple Django management command which clears your cache.',
+    long_description = open(normpath(join(dirname(abspath(__file__)),
+        'README.md'))).read()
+
+)
