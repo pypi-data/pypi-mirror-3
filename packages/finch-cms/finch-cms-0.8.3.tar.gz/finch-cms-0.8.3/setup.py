99a2fb38429b1d70d260f56db03ac64c858161d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:21 +0000

    Add package finch-cms/0.8.3/finch-cms-0.8.3.tar.gz

---
+++ packages/finch-cms/finch-cms-0.8.3.tar.gz/finch-cms-0.8.3/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+from finch import __version__
+
+setup(
+    name='finch-cms',
+    version=__version__,
+    description="Simple but flexible CMS for django",
+    long_description=open('README.rst').read(),
+    keywords='django cms',
+    author='Peter van Kampen',
+    author_email='pterk@datatailors.com',
+    url='https://github.com/pterk/finch-cms',
+    license='BSD',
+    packages=find_packages(),
+    package_data={'finch': ['templates/*/*',
+                            'static/*/*/*/*/*/*/*/*/*',
+                            ]},
+    include_package_data=True,
+    install_requires=[
+        'Django==1.3',
+        'PIL==1.1.7',
+        'South==0.7.3',
+        'Whoosh==2.3.0',
+        'django-bop==0.3',
+        'django-filebrowser==3.3.0',
+        'django-grappelli==2.3.5',
+        'django-haystack==1.2.5',
+        'django-treebeard==1.61',
+        'django-contentmanager==2012.5.9',
+        'django-tinymce==trunk'
+        ],
+    dependency_links = [
+        'https://github.com/aljosa/django-tinymce/tarball/master#egg=django-tinymce-trunk'
+        ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        "License :: OSI Approved :: BSD License",
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
