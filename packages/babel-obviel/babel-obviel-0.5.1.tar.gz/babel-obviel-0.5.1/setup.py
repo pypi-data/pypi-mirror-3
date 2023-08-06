0a92642540b18bbc6882ca29bbb1e42082ae94f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:15 +0000

    Add package babel-obviel/0.5.1/babel-obviel-0.5.1.tar.gz

---
+++ packages/babel-obviel/babel-obviel-0.5.1.tar.gz/babel-obviel-0.5.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read())
+
+
+setup(
+    name='babel-obviel',
+    version='0.5.1',
+    description='Obviel Template message extractor for Babel',
+    long_description=long_description,
+    author='Obviel Developers',
+    author_email='obviel@googlegroups.com',
+    packages=['babelobviel'],
+    include_package_data = True,
+    zip_safe=False,
+    license='BSD',
+    install_requires=[
+        'lxml',
+    ],
+    extras_require=dict(
+        test=['pytest >= 2.0'],
+        ),
+    entry_points="""
+    [babel.extractors]
+    obvt = babelobviel.obvt:extractor
+    """,
+    )
