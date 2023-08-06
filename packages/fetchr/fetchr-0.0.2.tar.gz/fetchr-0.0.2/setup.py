d832b75e26f53cfaf10ae2f93169311820eaa011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:36 +0000

    Add package fetchr/0.0.2/fetchr-0.0.2.tar.gz

---
+++ packages/fetchr/fetchr-0.0.2.tar.gz/fetchr-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import fetchr
+from setuptools import setup, find_packages
+
+#dynamically read dependencies from file
+with open('requirements.txt') as requirements:
+    requires = map(lambda r: r.strip(), requirements.readlines())
+
+version = '0.0.2'
+
+dist = setup(
+    name = 'fetchr',
+    version = version,
+    packages = find_packages(),
+    package_data = {'fetchr': ['data/config.yaml']},
+    include_package_data = True,
+    description = 'A tool to load files from the internet',
+    author = 'George Erickson',
+    author_email = 'george55@mit.edu',
+    download_url = 'https://github.com/GeorgeErickson/fetchr/zipball/v%s' % version,
+    install_requires = requires,
+    entry_points = {
+        'console_scripts': [
+            'fetchr = fetchr.fetchr:main'
+        ]
+    }
+)
