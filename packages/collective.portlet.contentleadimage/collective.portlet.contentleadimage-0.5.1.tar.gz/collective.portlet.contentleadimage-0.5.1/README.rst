af0af3f7d522ad873ad0fba13bf1971e991148ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:37 +0000

    Add package collective.portlet.contentleadimage/0.5.1/collective.portlet.contentleadimage-0.5.1.tar.gz

---
+++ packages/collective.portlet.contentleadimage/collective.portlet.contentleadimage-0.5.1.tar.gz/collective.portlet.contentleadimage-0.5.1/README.rst
@@ -0,0 +1,15 @@
+Introduction
+============
+
+This portlet behaves like collection portlet, but shows the
+contentleadimages and the title of each item. If the item has no lead
+image but has a image field (like News Items, for example) then this field
+is displayed.
+
+This portlet has an extra option to display start dates instead of
+publication dates.
+
+Another extra option can be used to choose the scale of the images.
+
+To install just add collective.portlet.contentleadimage to your buildout's
+eggs and go to the products section of Plone's control panel.
---
+++ packages/collective.portlet.contentleadimage/collective.portlet.contentleadimage-0.5.1.tar.gz/collective.portlet.contentleadimage-0.5.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.5.1'
+
+setup(name='collective.portlet.contentleadimage',
+      version=version,
+      description="Collection portlet that shows contenteleadimages",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        ],
+      keywords='',
+      author='Ales Zabala Alava (Shagi)',
+      author_email='shagi@gisa-elkartea.org',
+      url='http://github.com/collective/collective.portlet.contentleadimage',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.portlet.collection',
+          'collective.contentleadimage',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
