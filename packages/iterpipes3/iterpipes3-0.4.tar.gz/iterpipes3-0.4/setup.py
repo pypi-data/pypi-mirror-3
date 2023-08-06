369dc28bc7321bf64ccec1c1f0d8ec21a1dfa19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:22 +0000

    Add package iterpipes3/0.4/iterpipes3-0.4.tar.gz

---
+++ packages/iterpipes3/iterpipes3-0.4.tar.gz/iterpipes3-0.4/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='iterpipes3',
+    version='0.4',
+    packages=['iterpipes3'],
+    requires=['six'],
+    author='Kevin Kelley',
+    author_email='kelleyk@kelleyk.net',
+    description='A library for running shell pipelines using shell-like syntax',
+    license='MIT',
+    url='http://github.com/kelleyk/iterpipes3',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        ])
