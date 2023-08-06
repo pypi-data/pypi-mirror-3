7ffe5f3c480321e9b32ececb34a6e9c2d4b5fecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:45 +0000

    Add package ftw.book/2.2.7/ftw.book-2.2.7.zip

---
+++ packages/ftw.book/ftw.book-2.2.7.zip/ftw.book-2.2.7/ftw/book/content/chapter.py
@@ -0,0 +1,22 @@
+from AccessControl import ClassSecurityInfo
+try:
+    from Products.LinguaPlone.public import registerType
+except ImportError:
+    # No multilingual support
+    from Products.Archetypes.atapi import registerType
+from ftw.book.config import PROJECTNAME
+from ftw.book.interfaces import IChapter
+from simplelayout.base.interfaces import ISimpleLayoutBlock
+from simplelayout.base.interfaces import ISimpleLayoutCapable
+from simplelayout.types.common.content.page import Page
+from zope.interface import implements
+
+
+class Chapter(Page):
+    implements(IChapter, ISimpleLayoutCapable, ISimpleLayoutBlock)
+    security = ClassSecurityInfo()
+
+    schema = Page.schema.copy()
+
+
+registerType(Chapter, PROJECTNAME)
