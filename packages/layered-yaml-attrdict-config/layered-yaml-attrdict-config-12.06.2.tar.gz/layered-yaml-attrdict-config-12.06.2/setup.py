8eaa2ebfe81ca5615d2662fa16d2fa11931477e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:50 +0000

    Add package layered-yaml-attrdict-config/12.06.2/layered-yaml-attrdict-config-12.06.2.tar.gz

---
+++ packages/layered-yaml-attrdict-config/layered-yaml-attrdict-config-12.06.2.tar.gz/layered-yaml-attrdict-config-12.06.2/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+pkg_root = os.path.dirname(__file__)
+
+# Error-handling here is to allow package to be built w/o README included
+try: readme = open(os.path.join(pkg_root, 'README.txt')).read()
+except IOError: readme = ''
+
+setup(
+
+	name = 'layered-yaml-attrdict-config',
+	version = '12.06.2',
+	author = 'Mike Kazantsev',
+	author_email = 'mk.fraggod@gmail.com',
+	license = 'WTFPL',
+	keywords = 'yaml configuration conf serialization inheritance merge update',
+	url = 'https://github.com/mk-fg/layered-yaml-attrdict-config',
+
+	description = 'YAML-based configuration module',
+	long_description = readme,
+
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Framework :: Twisted',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Information Technology',
+		'Intended Audience :: System Administrators',
+		'License :: OSI Approved',
+		'Operating System :: POSIX',
+		'Operating System :: Unix',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 2 :: Only',
+		'Topic :: Software Development',
+		'Topic :: Software Development :: Libraries :: Python Modules' ],
+
+	install_requires = ['PyYAML'],
+	packages = find_packages(),
+	package_data = {'': ['README.txt']},
+	exclude_package_data = {'': ['README.*']} )
