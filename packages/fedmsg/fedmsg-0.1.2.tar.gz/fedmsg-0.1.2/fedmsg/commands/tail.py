0a17643d0f25b2dd857699527e6c3e5c28155dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:13 +0000

    Add package fedmsg/0.1.2/fedmsg-0.1.2.tar.gz

---
+++ packages/fedmsg/fedmsg-0.1.2.tar.gz/fedmsg-0.1.2/fedmsg/commands/tail.py
@@ -0,0 +1,70 @@
+import pprint
+import time
+
+import pygments
+import pygments.lexers
+import pygments.formatters
+
+import fedmsg
+import fedmsg.json
+from fedmsg.commands import command
+
+
+extra_args = [
+    (['--topic'], {
+        'dest': 'topic',
+        'help': 'The topic pattern to listen for.  Everything by default.',
+        'default': '',
+    }),
+    (['--pretty'], {
+        'dest': 'pretty',
+        'help': 'Pretty print the JSON messages.',
+        'default': False,
+        'action': 'store_true',
+    }),
+    (['--really-pretty'], {
+        'dest': 'really_pretty',
+        'help': 'Extra-pretty print the JSON messages.',
+        'default': False,
+        'action': 'store_true',
+    }),
+]
+
+
+@command(extra_args=extra_args)
+def tail(**kw):
+    """ Watch the bus. """
+
+    # Disable sending
+    kw['publish_endpoint'] = None
+    # Disable timeouts.  We want to tail forever!
+    kw['timeout'] = 0
+    fedmsg.init(**kw)
+
+    # Build a message formatter
+    formatter = lambda d: d
+    if kw['pretty']:
+        def formatter(d):
+            d['timestamp'] = time.ctime(d['timestamp'])
+            return "\n" + pprint.pformat(d)
+
+    if kw['really_pretty']:
+        def formatter(d):
+            fancy = pygments.highlight(
+                fedmsg.json.pretty_dumps(d),
+                pygments.lexers.JavascriptLexer(),
+                pygments.formatters.TerminalFormatter()
+            ).strip()
+            return "\n" + fancy
+
+    # The "proper" fedmsg way to do this would be to spin up or connect to an
+    # existing Moksha Hub and register a consumer on the "*" topic that simply
+    # prints out each message it consumes.  That seems like overkill, so we're
+    # just going to directly access the endpoints ourself.
+
+    # TODO -- colors?
+    # TODO -- tabular layout?
+    for name, ep, topic, message in fedmsg.__context._tail_messages(**kw):
+        if '_heartbeat' in topic:
+            continue
+        print name, ep, topic, formatter(message)
---
+++ packages/fedmsg/fedmsg-0.1.2.tar.gz/fedmsg-0.1.2/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+f = open('README.rst')
+long_description = f.read().strip()
+long_description = long_description.split('split here', 1)[1]
+f.close()
+
+# Ridiculous as it may seem, we need to import multiprocessing and
+# logging here in order to get tests to pass smoothly on python 2.7.
+try:
+    import multiprocessing
+    import logging
+except Exception:
+    pass
+
+setup(
+    name='fedmsg',
+    version='0.1.2',
+    description="Fedora Messaging Client API",
+    long_description=long_description,
+    author='Ralph Bean',
+    author_email='rbean@redhat.com',
+    url='http://github.com/ralphbean/fedmsg/',
+    license='LGPLv2+',
+    install_requires=[
+        'pyzmq',
+        'simplejson',
+        'fabulous',
+        'moksha>=0.7.1',
+    ],
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    packages=[
+        'fedmsg',
+        'fedmsg.commands',
+        'fedmsg.consumers',
+        'fedmsg.producers',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': [
+            "fedmsg-logger=fedmsg.commands.logger:logger",
+            "fedmsg-status=fedmsg.commands.status:status",
+            "fedmsg-tail=fedmsg.commands.tail:tail",
+            "fedmsg-hub=fedmsg.commands.hub:hub",
+            "fedmsg-relay=fedmsg.commands.relay:relay",
+            "fedmsg-config=fedmsg.commands.config:config",
+            "fedmsg-irc=fedmsg.commands.ircbot:ircbot",
+        ],
+        'moksha.consumer': [
+            "fedmsg-relay=fedmsg.consumers.relay:RelayConsumer",
+            "fedmsg-ircbot=fedmsg.consumers.ircbot:IRCBotConsumer",
+        ],
+        'moksha.producer': [
+            "heartbeat=fedmsg.producers.heartbeat:HeartbeatProducer",
+        ],
+    }
+)
