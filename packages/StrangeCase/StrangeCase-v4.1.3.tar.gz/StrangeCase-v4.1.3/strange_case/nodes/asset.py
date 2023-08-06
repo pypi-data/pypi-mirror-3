73e13138029f38a8c676de01cb5d36de00c374ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:22 +0000

    Add package StrangeCase/v4.1.3/StrangeCase-v4.1.3.tar.gz

---
+++ packages/StrangeCase/StrangeCase-v4.1.3.tar.gz/StrangeCase-v4.1.3/strange_case/nodes/asset.py
@@ -0,0 +1,16 @@
+import os
+from shutil import copy2
+from strange_case.nodes import FileNode
+
+
+class AssetNode(FileNode):
+    """
+    Copies a file to a destination
+    """
+    def generate_file(self, site, source_path, target_path):
+        if not os.path.exists(target_path) or not self['skip']:
+            copy2(source_path, target_path)
+        elif self['verbose']:
+            print "Skipping %s" % target_path
+        self.files_tracked.append(source_path)
+        self.files_written.append(target_path)
