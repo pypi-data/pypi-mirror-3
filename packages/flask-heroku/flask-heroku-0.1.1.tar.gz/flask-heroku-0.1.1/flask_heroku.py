50ea8e9153161bd8825dc9e6b1c9fdf8c9b18247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:25 +0000

    Add package flask-heroku/0.1.1/flask-heroku-0.1.1.tar.gz

---
+++ packages/flask-heroku/flask-heroku-0.1.1.tar.gz/flask-heroku-0.1.1/flask_heroku.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from os import environ
+
+class Heroku(object):
+    """Heroku configurations for flask."""
+
+    def __init__(self, app):
+        self.app = app
+
+        # SQL-Alchemy
+        self.app.config.setdefault('SQLALCHEMY_DATABASE_URI', environ.get('DATABASE_URL'))
+
+        # Sentry
+        self.app.config.setdefault('SENTRY_DSN', environ.get('SENTRY_DSN'))
+
+        # Exceptional
+        self.app.config.setdefault('EXCEPTIONAL_API_KEY', environ.get('EXCEPTIONAL_API_KEY'))
+
+        # Flask-GoogleFed
+        self.app.config.setdefault('GOOGLE_DOMAIN', environ.get('GOOGLE_DOMAIN'))
+
