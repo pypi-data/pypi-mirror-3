226bc69a6641a0470b0f821ef9a2db9245a21c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:25 +0000

    Add package django-threatmetrix/0.0.1/django-threatmetrix-0.0.1.tar.gz

---
+++ packages/django-threatmetrix/django-threatmetrix-0.0.1.tar.gz/django-threatmetrix-0.0.1/threatmetrix/tests/settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'django_threatmetrix_db',
+    },
+}
+
+INSTALLED_APPS = (
+    'threatmetrix'
+)
+
+THREATMETRIX_ORG_ID = 'test_org_id'
