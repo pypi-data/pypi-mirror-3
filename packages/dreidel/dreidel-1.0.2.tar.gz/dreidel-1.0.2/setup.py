e0ab4bd9e9527ea6aa0a24b1984ca5db2ff0a978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:33 +0000

    Add package dreidel/1.0.2/dreidel-1.0.2.tar.gz

---
+++ packages/dreidel/dreidel-1.0.2.tar.gz/dreidel-1.0.2/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+packages = [
+    'dreidel'
+]
+
+with open('readme.md') as file:
+    long_description = file.read()
+
+setup(
+    name='dreidel',
+    version="1.0.2",
+    author='Bernhard Mäser',
+    author_email='bernhard.maeser@gmail.com',
+    url='https://github.com/bmaeser/dreidel',
+    scripts=['dreidel/dreidel.py'],
+    license="MIT",
+    description="A python replacement for logrotate",
+    long_description=long_description,
+    data_files=['readme.md'],
+    packages = packages,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Archiving',
+        'Topic :: System :: Archiving :: Backup',
+        'Operating System :: POSIX',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
