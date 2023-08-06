93e4d7689505f314f0372463d2e5b43177c1598b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:06 +0000

    Add package eea.exhibit/4.2/eea.exhibit-4.2.zip

---
+++ packages/eea.exhibit/eea.exhibit-4.2.zip/eea.exhibit-4.2/setup.py
@@ -0,0 +1,40 @@
+""" EEA Exhibit Installer
+"""
+from setuptools import setup, find_packages
+import os
+from os.path import join
+
+NAME = 'eea.exhibit'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(join(*PATH)).read().strip()
+
+setup(name=NAME,
+      version=VERSION,
+      description=(
+          "EEA Exhibit provides Simile Widgets Exhibit JS libraries as Zope 3 "
+          "resources."),
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='exhibit eea plone zope python',
+      author='European Environment Agency',
+      author_email="webadmin@eea europa eu",
+      url='http://svn.eionet.europa.eu/projects/Zope/wiki/DaViz',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['eea'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'rdflib',
+          'eea.app.visualization',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
