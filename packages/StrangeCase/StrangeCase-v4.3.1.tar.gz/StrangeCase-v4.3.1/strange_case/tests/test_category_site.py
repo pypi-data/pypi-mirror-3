d41e67264b0cdce6f0955a37800bb9512ec6ac8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:40 +0000

    Add package StrangeCase/v4.3.1/StrangeCase-v4.3.1.tar.gz

---
+++ packages/StrangeCase/StrangeCase-v4.3.1.tar.gz/StrangeCase-v4.3.1/strange_case/tests/test_category_site.py
@@ -0,0 +1,31 @@
+from strange_case import strange_case
+from strange_case.tests import will_generate, check_path_contents, tree
+
+
+@will_generate('category_site')
+def test_category_site(config):
+    try:
+        strange_case(config)
+
+        path_contents = {
+            'index.html': True,
+            'blogs': {
+                'index.html': True,
+                '2012_01_01_post1.html': True,
+                '2012_01_02_post2.html': True,
+            },
+            'zee_categories': {
+                'index.html': [
+                    'Categories (2)',
+                    '<a href="/zee_categories/good.html">good</a>',
+                    '<a href="/zee_categories/bad.html">bad</a>'
+                ],
+                'good.html': ['Category: good'],
+                'bad.html': ['Category: bad'],
+            },
+        }
+        check_path_contents(config['deploy_path'], path_contents)
+    except Exception:
+        tree(config['site_path'], config['project_path'])
+        tree(config['deploy_path'], config['project_path'])
+        raise
