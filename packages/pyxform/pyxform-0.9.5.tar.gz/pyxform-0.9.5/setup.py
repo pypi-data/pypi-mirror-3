238e91a9af536a5bd8e5ceec76915d55fb4e3f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:06 +0000

    Add package pyxform/0.9.5/pyxform-0.9.5.tar.gz

---
+++ packages/pyxform/pyxform-0.9.5.tar.gz/pyxform-0.9.5/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+     name='pyxform',
+     version='0.9.5',
+     author='modilabs',
+     author_email='info@modilabs.org',
+     packages=['pyxform'],
+     url='http://pypi.python.org/pypi/pyxform/',
+     description='A Python package to create XForms for ODK Collect.',
+     long_description=open('README.rst').read(),
+)
