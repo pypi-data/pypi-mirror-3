be06df32399b75cd3320e6555e87a802fe33089e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:55 +0000

    Add package leotest/1.0.0/leotest-1.0.0.tar.gz

---
+++ packages/leotest/leotest-1.0.0.tar.gz/leotest-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='leotest',
+    version='1.0.0',
+    py_modules=['leotest'],
+    author='leopython',
+    author_email='llvh@163.com',
+    url='llvh.blog.163.com',
+    description='Some simples of leotet python'
+    )
