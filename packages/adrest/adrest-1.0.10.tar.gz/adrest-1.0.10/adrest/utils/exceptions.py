980b1848b09aa024111aea9b11892ba457647a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:30 +0000

    Add package adrest/1.0.10/adrest-1.0.10.tar.gz

---
+++ packages/adrest/adrest-1.0.10.tar.gz/adrest-1.0.10/adrest/utils/exceptions.py
@@ -0,0 +1,14 @@
+from . import status as http_status
+
+
+class HttpError(Exception):
+    """ Represent Http Error.
+    """
+    def __init__(self, message, status=http_status.HTTP_400_BAD_REQUEST):
+        self.message, self.status = message, status
+        super(HttpError, self).__init__(message)
+
+    def __str__(self):
+        return self.message
+
+    __repr__ = __str__
