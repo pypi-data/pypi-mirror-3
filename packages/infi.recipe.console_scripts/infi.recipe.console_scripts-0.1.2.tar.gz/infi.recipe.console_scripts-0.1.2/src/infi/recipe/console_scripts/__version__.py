e7f75fbd25a95cbb44acc611e5e615c839b9a815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:40 +0000

    Add package infi.recipe.console_scripts/0.1.2/infi.recipe.console_scripts-0.1.2.tar.gz

---
+++ packages/infi.recipe.console_scripts/infi.recipe.console_scripts-0.1.2.tar.gz/infi.recipe.console_scripts-0.1.2/src/infi/recipe/console_scripts/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.2"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '21913ef19f1adda8a856cb361a884742f22daa9c'
+__git_head_subject__ = 'empty commit after version v0.1.1'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
