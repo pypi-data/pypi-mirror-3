8b77c9aac4de32a069b39077cdc89b6ed595ca93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:35 +0000

    Add package python-varnish/0.2.0/python-varnish-0.2.0.tar.gz

---
+++ packages/python-varnish/python-varnish-0.2.0.tar.gz/python-varnish-0.2.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='python-varnish',
+    version='0.2.0',
+    long_description=open('README.rst').read(),
+    description='Simple Python interface for the Varnish management port',
+    author='Justin Quick',
+    author_email='justquick@gmail.com',
+    url='http://github.com/justquick/python-varnish',
+    scripts=['bin/varnish_manager'],
+    py_modules=['varnish'],
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Environment :: Console',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Utilities'],
+)
