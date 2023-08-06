c2672dda33cce9936a26320283fba8d6fd6669e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:29 +0000

    Add package bottle-pystache/0.0.2/bottle-pystache-0.0.2.tar.gz

---
+++ packages/bottle-pystache/bottle-pystache-0.0.2.tar.gz/bottle-pystache-0.0.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from distutils.core import setup
+
+if sys.version_info < (2,5):
+    raise NotImplementedError("Sorry, you need at least Python 2.5 or Python 3.x to use bottle.")
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
+setup(name='bottle-pystache',
+      version='0.0.2',
+      description='Bottle Pystache template wrappers',
+      long_description='This project is an easy to use Bottle wrapper to Pystache.',
+      author='AleiPhoenix',
+      author_email='aleiphoenix@gmail.com',
+      url='https://github.com/aleiphoenix/bottle-pystache',
+      py_modules=['bottle_pystache'],
+      scripts=['bottle_pystache.py'],
+      license='MIT',
+      platforms = 'any',
+      cmdclass = {'build_py': build_py}
+     )
+
