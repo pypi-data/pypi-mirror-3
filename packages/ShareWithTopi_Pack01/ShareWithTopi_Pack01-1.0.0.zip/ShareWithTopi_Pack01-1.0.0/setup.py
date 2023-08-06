a862c8e134bf11c3e7786fdb0ffdf0f1f1541996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:41 +0000

    Add package ShareWithTopi_Pack01/1.0.0/ShareWithTopi_Pack01-1.0.0.zip

---
+++ packages/ShareWithTopi_Pack01/ShareWithTopi_Pack01-1.0.0.zip/ShareWithTopi_Pack01-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name		='ShareWithTopi_Pack01',
+	version		='1.0.0',
+	py_modules	=['module_test'],
+	author		='topizz',
+	author_email	='dmsahabandu@gmail.com',
+	url		='dammina.blogspot.com',
+	description	='My first damn python code',
+      )
