4c0aaa2a8c1c061815e37b3277b3fc43fb7b80e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:31 +0000

    Add package mikado.oss.doctest_additions/0.0.1/mikado.oss.doctest_additions-0.0.1.tar.gz

---
+++ packages/mikado.oss.doctest_additions/mikado.oss.doctest_additions-0.0.1.tar.gz/mikado.oss.doctest_additions-0.0.1/setup.py
@@ -0,0 +1,50 @@
+
+'''
+setup.py for mikado packages
+
+'''
+
+from distutils.core import setup
+import os, glob
+
+def get_version():
+
+    '''return version from fixed always must exist file
+
+       Making very broad assumptions about the 
+       existence of files '''
+    
+    v = open('mikado/oss/doctest_additions/version.txt').read().strip()
+    return v
+
+
+
+
+def main():
+
+    setup(name='mikado.oss.doctest_additions',
+          version=get_version(),
+          packages=['mikado.oss.doctest_additions'
+                   ],
+          namespace_packages = ['mikado', 'mikado.oss'],
+          author='See AUTHORS.txt',
+          author_email='info@mikadosoftware.com',
+          url='https://github.com/lifeisstillgood/mikado.oss.doctest_additions',
+          license='LICENSE.txt',
+          description='trivial "patch" to allow doctests to ignore certain lines',
+          long_description='see description',
+          install_requires=[
+              "unittest-xml-reporting",
+                           ],
+          package_data={'mikado.oss.doctest_additions': ['version.txt', 
+                                                         'tests/*.*'],
+                        },
+
+          
+          )
+
+
+
+if __name__ == '__main__':
+    main()
+
