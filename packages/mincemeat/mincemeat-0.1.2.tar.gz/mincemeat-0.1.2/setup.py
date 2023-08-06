5c7d2360687c190ccffb235c534ba4cc5dec5bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:44 +0000

    Add package mincemeat/0.1.2/mincemeat-0.1.2.tar.gz

---
+++ packages/mincemeat/mincemeat-0.1.2.tar.gz/mincemeat-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='mincemeat',
+    version='0.1.2',
+    author='Michael Fairley',
+    author_email='michaelfairley@gmail.com',
+    url='http://mincemeatpy.com',
+    py_modules=['mincemeat'],
+    scripts=['mincemeat.py'],
+)
