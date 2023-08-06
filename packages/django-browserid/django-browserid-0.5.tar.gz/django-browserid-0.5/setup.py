43f6acebaaa57c57c59cd977da222fa4551bc87e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:20 +0000

    Add package django-browserid/0.5/django-browserid-0.5.tar.gz

---
+++ packages/django-browserid/django-browserid-0.5.tar.gz/django-browserid-0.5/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-browserid',
+    version='0.5',
+    packages=['django_browserid'],
+    author='Paul Osman, Michael Kelly',
+    author_email='mkelly@mozilla.com',
+    url='https://github.com/mozilla/django-browserid',
+    install_requires='requests>=0.9.1',
+    package_data={'django_browserid': ['static/browserid/*.js']},
+)
