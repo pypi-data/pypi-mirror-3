65bc9a0430ba4026a61d4f9f6503cb5d58cb0a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:40 +0000

    Add package thunderpush/0.9.4/thunderpush-0.9.4.tar.gz

---
+++ packages/thunderpush/thunderpush-0.9.4.tar.gz/thunderpush-0.9.4/thunderpush/handler.py
@@ -0,0 +1,92 @@
+import logging
+
+from sockjs.tornado import SockJSConnection
+from thunderpush.messenger import Messenger
+from thunderpush.sortingstation import SortingStation
+
+try:
+    import json
+except ImportError:
+    import simplejson as json 
+
+logger = logging.getLogger()
+
+class ThunderSocketHandler(SockJSConnection):
+    def on_open(self, info):
+        logger.debug("New connection opened.")
+
+        # no messenger object yet, client needs issue CONNECT command first
+        self.messenger = None
+
+    def on_message(self, msg):
+        logger.debug("Got message: %s" % msg)
+
+        self.process_message(msg)
+
+    def on_close(self):
+        if self.connected:
+            self.messenger.unregister_user(self)
+            self.messenger = None
+
+        logger.debug("User %s has disconnected." 
+            % getattr(self, "userid", None))
+
+    def process_message(self, msg):
+        """
+        We assume that every client message comes in following format:
+        COMMAND argument1[:argument2[:argumentX]]
+        """
+
+        tokens = msg.split(" ")
+
+        messages = {
+            'CONNECT': self.handle_connect,
+            'SUBSCRIBE': self.handle_subscribe
+        }
+
+        try:
+            messages[tokens[0]](tokens[1])
+        except (KeyError, IndexError):
+            logger.warning("Received invalid message: %s." % msg)
+
+    def handle_connect(self, args):
+        if self.connected:
+            logger.warning("User already connected.")
+            return
+
+        try:
+            self.userid, self.apikey = args.split(":")
+        except ValueError:
+            logger.warning("Invalid message syntax.")
+            return
+
+        # get singleton instance of SortingStation
+        ss = SortingStation.instance()
+
+        # get and store the messenger object for given apikey
+        self.messenger = ss.get_messenger_by_apikey(self.apikey)
+
+        if self.messenger:
+            self.messenger.register_user(self)
+        else:
+            logger.warning("Invalid API key.")
+
+            # inform client that the key was not good
+            self.send("WRONGKEY")
+            self.close()
+
+    def handle_subscribe(self, args):
+        if not self.connected:
+            logger.warning("User not connected.")
+            return
+
+        channels = args.split(":")
+
+        if len(channels):
+            for channel in channels:
+                self.messenger.subsribe_user_to_channel(self, channel)
+
+    @property
+    def connected(self):
+        return bool(self.messenger)
+    
---
+++ packages/thunderpush/thunderpush-0.9.4.tar.gz/thunderpush-0.9.4/thunderpush/messenger.py
@@ -0,0 +1,81 @@
+import logging
+import re
+
+logger = logging.getLogger()
+
+class Messenger(object):
+    """ 
+    Handles dispatching messages to Channels and Users
+    for given client.
+    """
+    
+    def __init__(self, apikey, apisecret, *args, **kwargs):
+        self.apikey = apikey
+        self.apisecret = apisecret
+        self.users = {}
+        self.channels = {}
+        self.user_count = 0
+
+    @staticmethod
+    def is_valid_channel_name(name):
+        return re.match("^[a-zA-Z0-9_\-\=\@\,\.\;]{1,64}$", name) != None
+
+    def send_to_channel(self, channel, message):
+        """ 
+        Sends a message to given channel.
+        Returns a count of messages sent.
+        """
+
+        users = self.get_users_in_channel(channel)
+        return self._send_to_users(users, message)
+
+    def send_to_user(self, userid, message):
+        """ 
+        Sends a message to given user.
+        Returns a count of messages sent.
+        """
+
+        users = self.users.get(userid, [])
+        return self._send_to_users(users, message)
+
+    def _send_to_users(self, users, message):
+        for user in users:
+            user.send(message)
+
+        return len(users)
+
+    def register_user(self, user):
+        self.user_count += 1
+        self.users.setdefault(user.userid, []).append(user)
+
+    def subsribe_user_to_channel(self, user, channel):
+        if self.is_valid_channel_name(channel):
+            self.channels.setdefault(channel, []).append(user)
+
+            logger.debug("%s subscribed to %s." % (user.userid, channel,))
+            logger.debug("User count in %s: %d." % 
+                (channel, self.get_channel_user_count(channel)))
+        else:
+            logger.debug("Invalid channel name %s." % channel)
+
+    def unregister_user(self, user):
+        for name in self.channels.iterkeys():
+            try:
+                self.channels[name].remove(user)
+            except ValueError:
+                pass
+
+        self.user_count -= 1
+        self.users[user.userid].remove(user)
+
+    def get_user_count(self):
+        return self.user_count
+
+    def is_user_online(self, userid):
+        return bool(self.users.get(userid, 0))
+
+    def get_channel_user_count(self, channel):
+        return len(self.get_users_in_channel(channel))
+
+    def get_users_in_channel(self, channel):
+        return self.channels.get(channel, [])
