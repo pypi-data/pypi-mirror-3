0599960964dd337c5319a6500ae021b0875149aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:52 +0000

    Add package django-async-messages/0.1.2/django-async-messages-0.1.2.tar.gz

---
+++ packages/django-async-messages/django-async-messages-0.1.2.tar.gz/django-async-messages-0.1.2/async_messages/middleware.py
@@ -0,0 +1,16 @@
+from django.contrib import messages
+
+from async_messages import get_message
+
+
+class AsyncMiddleware(object):
+
+    def process_response(self, request, response):
+        # Check for message for this user and, if it exists,
+        # call the messages API with it
+        if not request.user.is_authenticated():
+            return
+        msg, level = get_message(request.user)
+        if msg:
+            messages.add_message(request, level, msg)
+        return response
