9eaf98fb6f0e68e02a6bb60c0679daaefb29ced9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:42 +0000

    Add package Flask-Mako/0.1/Flask-Mako-0.1.tar.gz

---
+++ packages/Flask-Mako/Flask-Mako-0.1.tar.gz/Flask-Mako-0.1/flask_mako.py
@@ -0,0 +1,80 @@
+# Find the stack on which we want to store the database connection.
+# Starting with Flask 0.9, the _app_ctx_stack is the correct one,
+# before that we need to use the _request_ctx_stack.
+try:
+    from flask import _app_ctx_stack as stack
+except ImportError:
+    from flask import _request_ctx_stack as stack
+
+from flask import request, session, get_flashed_messages, url_for, g
+from flask.signals import template_rendered
+from mako.lookup import TemplateLookup
+from mako.exceptions import RichTraceback, text_error_template
+
+class TemplateError(RuntimeError):
+    """
+    A template has thrown an error during rendering. The following properties
+    are provided by this exception:
+
+    * ``self.tb``: a ``RichTraceback`` object generated from the exception.
+    * ``self.text``: the exception information, generated with
+      ``mako.text_error_template``.
+
+    """
+    def __init__(self, template):
+        self.tb = RichTraceback()
+        self.text = text_error_template().render()
+        msg = "Error occurred while rendering template '{0}'".format(template)
+        msg += "\n" + self.text
+        super(TemplateError, self).__init__(msg)
+
+class MakoTemplates(object):
+    """
+    Extension object that allows Flask applications to render Mako templates.
+
+    """
+    def __init__(self, app=None):
+        self.app = app
+        if self.app is not None:
+            self.init_app(self.app)
+
+    def init_app(self, app):
+        app.config.setdefault('MAKO_TEMPLATE_DIR', 'templates')
+
+    def create_lookup(self):
+        templates = self.app.config.get('MAKO_TEMPLATE_DIR')
+        cache = self.app.config.get('MAKO_CACHE_DIR', None)
+        cache_size = self.app.config.get('MAKO_CACHE_SIZE', -1)
+
+        templates = templates if isinstance(templates, list) else [templates]
+
+        return TemplateLookup(directories=templates,
+                              module_directory=cache,
+                              collection_size=cache_size)
+
+    @property
+    def lookup(self):
+        ctx = stack.top
+        if ctx is not None:
+            if not hasattr(ctx, 'mako_lookup'):
+                ctx.mako_lookup = self.create_lookup()
+            return ctx.mako_lookup
+
+    def render(self, template_name, **kwargs):
+        """
+        Render the given template with standard Flask parameters in addition to
+        the specified template `kwargs`.
+
+        """
+        template = self.lookup.get_template(template_name)
+        try:
+            args = {'url_for': url_for,
+                    'get_flashed_messages': get_flashed_messages}
+            args.update(kwargs)
+            self.app.update_template_context(args)
+            rv = template.render(**args)
+            template_rendered.send(self.app, template=template, context=args)
+            return rv
+        except:
+            raise TemplateError(template_name)
+
---
+++ packages/Flask-Mako/Flask-Mako-0.1.tar.gz/Flask-Mako-0.1/test_flask_mako.py
@@ -0,0 +1,126 @@
+import os, tempfile, unittest
+
+import flask
+from flask import Flask
+from flask.ext.mako import MakoTemplates
+
+basic = """
+% for arg in arguments:
+    ${arg}
+% endfor
+"""
+
+error = """
+% for arg in arguments:
+    ${error}
+% endfor
+"""
+
+class MakoTemplateTest(unittest.TestCase):
+    """ Tests the `flask_mako` templating extension. """
+
+    def setUp(self):
+        self.root = root = tempfile.mkdtemp()
+
+        self.template_dir = template_dir = os.path.join(root, "templates")
+        os.mkdir(template_dir)
+
+        self.app = app = Flask(__name__)
+        app.config.update(
+            MAKO_TEMPLATE_DIR=template_dir,
+            MAKO_CACHE_DIR=os.path.join(root, "cache"),
+            MAKO_CACHE_SIZE=10
+        )
+        self.mako = mako = MakoTemplates(app)
+
+        for name, template in (('basic', basic), ('error_template', error)):
+            with open(os.path.join(template_dir, name), 'w') as f:
+                f.write(template)
+
+    def _add_template(self, name, text):
+        with open(os.path.join(self.template_dir, name), 'w') as f:
+            f.write(text)
+
+    def test_basic_template(self):
+        """ Tests that the application can render a template. """
+        with self.app.test_request_context():
+            result = self.mako.render('basic', arguments=['testing', '123'])
+            self.assertTrue('testing' in result)
+            self.assertTrue('123' in result)
+
+    def test_standard_variables(self):
+        """
+        Tests that the variables generally available to Flask Jinja
+        templates are also available to Mako templates.
+
+        """
+        self._add_template("vars", """
+        ${config['MAKO_TEMPLATE_DIR']}
+        ${request.args}
+        ${session.new}
+        ${url_for('test')}
+        ${get_flashed_messages()}
+
+        ${injected()}
+        """)
+
+        @self.app.route('/test')
+        def test(): return "test"
+
+        @self.app.context_processor
+        def inject(): return {"injected": lambda: "injected"}
+
+        with self.app.test_request_context():
+            result = self.mako.render("vars")
+
+    def test_signals(self):
+        """ Tests that template rendering fires the right signal. """
+        from flask.signals import template_rendered
+        log = []
+
+        def record(*args, **extra):
+            log.append(args)
+        template_rendered.connect(record, self.app)
+
+        with self.app.test_request_context():
+            result = self.mako.render('basic', arguments=[])
+
+        self.assertEqual(len(log), 1)
+
+    if not flask.signals_available:
+        test_signals = unittest.skip("To test signals, install the blinker "
+                                     "library")(test_signals)
+
+    def test_multiple_dirs(self):
+        """ Tests template loading from multiple directories. """
+        alt_d = os.path.join(self.root, 'alt_templates')
+        os.mkdir(alt_d)
+        with open(os.path.join(alt_d, 'alt'), 'w') as f:
+            f.write("${alt}")
+
+        td = self.app.config['MAKO_TEMPLATE_DIR']
+        self.app.config.update(MAKO_TEMPLATE_DIR=[alt_d, td])
+        self.mako = MakoTemplates(self.app)
+
+        with self.app.test_request_context():
+            result = self.mako.render('alt', alt='testing')
+            self.assertTrue('testing' in result)
+
+    def test_error(self):
+        """ Tests that template errors are properly handled. """
+        from flask.ext.mako import TemplateError
+
+        with self.app.test_request_context():
+            with self.assertRaises(TemplateError) as error:
+                self.mako.render('error_template', arguments=['x'])
+
+            e = error.exception
+            self.assertTrue('error_template' in e.message)
+            self.assertTrue('error_template' in e.text)
+            self.assertTrue('line 3' in e.text)
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(MakoTemplateTest))
+    return suite
