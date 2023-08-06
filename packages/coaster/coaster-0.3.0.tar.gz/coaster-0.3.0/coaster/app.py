6754e9b1efda4a161d03f577b9ecf168d7c593b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:45 +0000

    Add package coaster/0.3.0/coaster-0.3.0.tar.gz

---
+++ packages/coaster/coaster-0.3.0.tar.gz/coaster-0.3.0/coaster/app.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from os import environ
+import sys
+import coaster.logging
+
+
+_additional_config = {
+    'dev': 'development.py',
+    'development': 'development.py',
+    'test': 'testing.py',
+    'testing': 'testing.py',
+    'prod': 'production.py',
+    'production': 'production.py',
+    }
+
+
+def configure(app, env):
+    """
+    Configure an app depending on the environment.
+    """
+    load_config_from_file(app, 'settings.py')
+
+    additional = additional_settings_file(env)
+    if additional:
+        load_config_from_file(app, additional)
+
+    coaster.logging.configure(app)
+
+
+def additional_settings_file(env):
+    """
+    Checks to see if the environment has an additional settings file that we
+    need to look for and load as well.
+    """
+    return _additional_config.get(environ.get(env))
+
+
+def load_config_from_file(app, filepath):
+    try:
+        app.config.from_pyfile(filepath)
+    except IOError:
+        # TODO: Can we print to sys.stderr in production? Should this go to
+        # logs instead?
+        print >> sys.stderr, "Did not find settings file %s" % filepath
