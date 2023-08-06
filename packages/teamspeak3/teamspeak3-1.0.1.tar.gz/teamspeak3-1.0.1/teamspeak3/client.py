9e0647dd68bdcb56fe4874480f6a9532db4c882c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:36 +0000

    Add package teamspeak3/1.0.1/teamspeak3-1.0.1.tar.gz

---
+++ packages/teamspeak3/teamspeak3-1.0.1.tar.gz/teamspeak3-1.0.1/teamspeak3/client.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2012 Adam Coddington
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from multiprocessing import Process, Queue
+
+from connection import TeamspeakConnection
+from command import Command
+
+__all__ = ['Client']
+
+class Client(object):
+    def __init__(self, hostname='127.0.0.1', port=25639, timeout=0.25):
+        self.pipe_out = Queue()
+        self.pipe_in = Queue()
+
+        self.proc = Process(
+                target=self.__class__.start_connection,
+                args = (hostname, port, timeout, self.pipe_in, self.pipe_out)
+            )
+        self.proc.start()
+
+    def __enter__(self, *args, **kwargs):
+        return Client(*args, **kwargs)
+
+    def __exit__(self, type, value, traceback):
+        self.proc.terminate()
+        return True
+
+    def close(self):
+        self.proc.terminate()
+
+    @classmethod
+    def start_connection(cls, hostname, port, timeout, pipe_in, pipe_out):
+        # Connect the out to the in, and the in to the out.
+        conn = TeamspeakConnection(hostname, port, timeout, pipe_out, pipe_in)
+        conn.main_loop()
+
+    def get_messages(self):
+        messages = []
+        while True:
+            message = self.get_message()
+            if not message:
+                return messages
+            messages.append(message)
+
+    def get_message(self):
+        if not self.pipe_in.empty():
+            msg = self.pipe_in.get_nowait()
+            if isinstance(msg, Exception):
+                raise msg
+            else:
+                return msg
+        return None
+
+    def subscribe(self, type='any'):
+        """
+        Shortcut method to subscribe to all messages received from the client.
+        """
+        return self.send_command(
+                    Command(
+                            'clientnotifyregister',
+                            schandlerid=0,
+                            event=type
+                        )
+                )
+
+    def send_command(self, command):
+        self.pipe_out.put(command)
---
+++ packages/teamspeak3/teamspeak3-1.0.1.tar.gz/teamspeak3-1.0.1/teamspeak3/message.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2012 Adam Coddington
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+class Message(object):
+    MAPPINGS = {
+            '\\s': ' ',
+            '\\n': '\n',
+            '\\/': '/',
+            }
+
+    def __init__(self, command):
+        command = command.strip()
+        if not command:
+            raise ValueError("No command")
+
+        self.raw_command = command
+
+        self.args = self._get_arguments_from_string(self.raw_command)
+        self.command = self._get_command_from_string(self.raw_command)
+
+    def _get_command_from_string(self, cmd):
+        return cmd.split(' ')[0]
+
+    def _get_arguments_from_string(self, cmd):
+        args = {}
+        raw_args = cmd.split(' ')[1:]
+        for raw_arg in raw_args:
+            attribute, value = raw_arg.split('=', 1)
+            args[attribute] = self._clean_incoming_value(value)
+        return args
+
+    def _clean_incoming_value(self, value):
+        for fr, to in self.MAPPINGS.items():
+            value = value.replace(fr, to)
+        return value
+
+    def __getitem__(self, key):
+        return self.args[key]
+
+    def __str__(self):
+        return self.command
+
+    def __unicode__(self):
+        return unicode(self.__str__())
+
+    def __repr__(self):
+        return "<%s %s>" % (
+                    self.__str__(),
+                    self.args
+                )
