3fdb67a540200dcaad02197c5e4adcb4f2e37a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:17 +0000

    Add package Cytoplasm/0.06.3/Cytoplasm-0.06.3.tar.gz

---
+++ packages/Cytoplasm/Cytoplasm-0.06.3.tar.gz/Cytoplasm-0.06.3/cytoplasm/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+''' Cytoplasm is a static site compiler meant to be used mostly as a blogging
+engine.
+
+See http://cytoplasm.somethingsido.com.
+'''
+
+from cytoplasm.site import Site
---
+++ packages/Cytoplasm/Cytoplasm-0.06.3.tar.gz/Cytoplasm-0.06.3/cytoplasm/errors.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''Some errors that can be raised...'''
+
+
+class CytoplasmError(Exception):
+    pass
+
+
+class ControllerError(CytoplasmError):
+    pass
+
+
+class InterpreterError(CytoplasmError):
+    pass
