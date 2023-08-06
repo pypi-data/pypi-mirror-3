fabc8ac6453c01940e1bbfd4ac0b46ac0e829a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:44 +0000

    Add package twoq/0.2.1/twoq-0.2.1.tar.gz

---
+++ packages/twoq/twoq-0.2.1.tar.gz/twoq-0.2.1/twoq/active/reducing.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+'''twoq active reducing queues'''
+
+from inspect import ismodule
+
+from twoq.support import port
+from twoq.mixins.reducing import MathMixin, TruthMixin, ReducingMixin
+
+from twoq.active.mixins import AutoResultMixin, ManResultMixin, SyncResultMixin
+
+###############################################################################
+## active math queues #########################################################
+###############################################################################
+
+
+class amathq(AutoResultMixin, MathMixin):
+
+    '''auto-balancing math queue'''
+
+mathq = amathq
+
+
+class mmathq(ManResultMixin, MathMixin):
+
+    '''manually balanced math queue'''
+
+
+class smathq(SyncResultMixin, MathMixin):
+
+    '''autosynchronized math queue'''
+
+###############################################################################
+## active truth queues ####E###################################################
+###############################################################################
+
+
+class atruthq(AutoResultMixin, TruthMixin):
+
+    '''auto-balancing truth queue'''
+
+truthq = atruthq
+
+
+class mtruthq(ManResultMixin, TruthMixin):
+
+    '''manually balanced truth queue'''
+
+
+class struthq(SyncResultMixin, TruthMixin):
+
+    '''autosynchronized truth queue'''
+
+###############################################################################
+## reduce queues ##############################################################
+###############################################################################
+
+
+class areduceq(AutoResultMixin, ReducingMixin):
+
+    '''auto-balancing reduce queue'''
+
+reduceq = areduceq
+
+
+class mreduceq(ManResultMixin, ReducingMixin):
+
+    '''manually balanced reduce queue'''
+
+
+class sreduceq(SyncResultMixin, ReducingMixin):
+
+    '''autosynchronized reduce queue'''
+
+__all__ = sorted(name for name, obj in port.items(locals()) if not any([
+    name.startswith('_'), ismodule(obj),
+]))
+del ismodule
