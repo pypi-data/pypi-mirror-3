9b3a6b9f3ac84b6977ab0f8dfb35cfd23c9bf433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:37 +0000

    Add package producteev/0.1.1/producteev-0.1.1.tar.gz

---
+++ packages/producteev/producteev-0.1.1.tar.gz/producteev-0.1.1/producteev/__version.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2012 Martin Garcia <newluxfero@gmail.com>
+#
+# This file is part of python-producteev, and is made available under
+# MIT license. See LICENSE for the full details.
+
+dotted = "0.1.1"
+libtool = "0:11"
+hex = 0x000101
+date = "2012-06-08"
+tuple = (0, 1, 1)
+web = "producteev/0.1.1"
