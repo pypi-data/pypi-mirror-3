2f2d372768aed57e991bb33d064a4797a1e5c741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:44 +0000

    Add package docopt/0.1.1/docopt-0.1.1.tar.gz

---
+++ packages/docopt/docopt-0.1.1.tar.gz/docopt-0.1.1/setup.py
@@ -0,0 +1,26 @@
+"""
+
+``docopt`` lives on `GitHub <http://github.com/halst/docopt/>`_
+===============================================================
+
+"""
+from setuptools import setup
+
+
+setup(
+    name = "docopt",
+    version = "0.1.1",
+    author = "Vladimir Keleshev",
+    author_email = "vladimir@keleshev.com",
+    description = "Pythonic option parser, that will make you smile",
+    license = "MIT",
+    keywords = "option parsing optparse argparse getopt",
+    url = "http://github.com/halst/docopt",
+    py_modules=['docopt'],
+    long_description=__doc__,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
