6155b4c709939d36399d4d9568852abfd0ae28e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:33 +0000

    Add package PyNLPl/0.5/PyNLPl-0.5.tar.gz

---
+++ packages/PyNLPl/PyNLPl-0.5.tar.gz/PyNLPl-0.5/setup.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+# -*- coding: utf8 -*-
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+os.chdir(os.path.dirname(sys.argv[0]))
+if not os.path.exists('pynlpl') and not os.path.exists('.readysetup'):
+    print >>sys.stderr, "Running setup for first time, preparing source tree"
+    os.mkdir('pynlpl')
+    os.system('mv * pynlpl/ 2> /dev/null')
+    os.system('mv pynlpl/README pynlpl/docs pynlpl/setup.py pynlpl/AUTHORS pynlpl/INDEX . ')
+    open('.readysetup','w')
+elif not os.path.exists('.readysetup'):
+    print >>sys.stderr, "Not ready for setup. Please obtain sources anew."
+    sys.exit(2)
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "PyNLPl",
+    version = "0.5",
+    author = "Maarten van Gompel",
+    author_email = "proycon@anaproy.nl",
+    description = ("PyNLPl, pronounced as 'pineapple', is a Python library for Natural Language Processing. It contains various modules useful for common, and less common, NLP tasks. PyNLPl can be used for example the computation of n-grams, frequency lists and distributions, language models. There are also more complex data types, such as Priority Queues, and search algorithms, such as Beam Search."),
+    license = "GPL",
+    keywords = "nlp computational_linguistics search ngrams language_models linguistics toolkit",
+    url = "https://github.com/proycon/pynlpl",
+    packages=['pynlpl','pynlpl.clients','pynlpl.lm','pynlpl.formats','pynlpl.mt'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Text Processing :: Linguistic",
+        "Programming Language :: Python :: 2.6",
+        "Operating System :: POSIX",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    #include_package_data=True,
+    #package_data = {'': ['*.wsgi','*.js','*.xsl','*.gif','*.png','*.xml','*.html','*.jpg','*.svg','*.rng'] },
+    install_requires=['lxml >= 2.2']
+)
