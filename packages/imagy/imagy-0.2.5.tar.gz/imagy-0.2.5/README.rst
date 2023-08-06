b988dfb3faecc98886cf23e5e18ea485b6a7aed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:10 +0000

    Add package imagy/0.2.5/imagy-0.2.5.tar.gz

---
+++ packages/imagy/imagy-0.2.5.tar.gz/imagy-0.2.5/README.rst
@@ -0,0 +1,67 @@
+Imagy - make your website's images load upto 50% faster
+===============
+
+Imagy is a file daemon, that watches your website's media root (where your images are stored) and automatically optimizes image files that are created or modified within it. Imagy uses *lossless compression*, so your users never have to load unnecessary bytes while the algorithms used are completely lossless. Your images look the same, but load faster.
+
+A lot of work has gone into making it `set-and-forget`. Instead of having to code up deamons, file watches and handle different file formats on your own, Imagy does all the work for you. All you need to do is
+::
+
+    imagy /awesome/images/
+    
+
+The algorithms used are stable (don't further modify files after multiple invocations), however to make trying it out as easy as possible Imagy, by default, keeps the original file around for later reversal. 
+
+If you wish to stop using Imagy, run ``imagy -r`` which will copy all original images back to their initial location
+
+If you have (rightfully) come to the conclusion that you don't really need to keep originals around, set ``KEEP_ORIGINALS`` in ``config.py`` to  ``False``. If you want to delete all already stored originals run ``imagy --deloriginals``.
+
+
+Getting Started 
+-----------------
+
+Tested on Ubuntu 10.04 & 11.10:
+
+::
+
+    sudo apt-get install pngnq pngcrush imagemagick gifsicle libjpeg-progs
+
+    pip install imagy
+    
+
+That's it. You should now be able to run ``imagy``.
+
+
+.. _Information on how to install ``pip``: http://www.pip-installer.org/en/latest/installing.html#prerequisites
+
+
+Example
+-----------------
+
+As soon as the file
+
+::
+
+    images/img.jpg
+
+gets created, Imagy optimizes it while keeping the original at 
+
+::
+
+    images/img-original.jpg
+     
+
+Homage
+-----------------
+
+In the background Imagy uses the awesome library smush which exposes a general interface to handle the various file types.
+
+
+
+Dependencies
+-----------------
+
+imagemagick
+gifsicle
+jpegtran (on ubuntu in libjpeg-progs)
+pngcrush
+pngnq 
---
+++ packages/imagy/imagy-0.2.5.tar.gz/imagy-0.2.5/imagy/tests/imgtest.py
@@ -0,0 +1,78 @@
+from __future__ import division
+from path import path
+from context import imagy
+from subprocess import Popen, call
+import unittest
+from operator import eq
+from tempfile import mkdtemp
+import time
+
+QUIET = 1
+
+class ImagyTestCase(unittest.TestCase):
+    image_files = {
+        'png':'png.png',
+        'jpg':'jpg.jpg',
+        'gif':'gif.gif',
+        'gifgif':'gifgif.gif'
+        }
+    imagy = ['imagy']
+    imagy_mem = ['imagy', '-m']
+    if QUIET:
+        imagy += ['-q']
+        imagy_mem += ['-q']
+        
+    def __init__(self, *args, **kwargs):
+        super(ImagyTestCase, self).__init__(*args, **kwargs)
+        self.root = path(__file__)
+        self.image_loc = self.root.parent.joinpath('images')
+        self.images = dict((k, self.image_loc.joinpath(v)) for k,v in self.image_files.items())
+        
+    def setUp(self, *args, **kwargs):
+        self.__setup(*args, **kwargs)
+        if not self.__setup is self.setup:
+            self.setup(*args, **kwargs)
+    
+    def tearDown(self, *args, **kwargs):
+        self.__teardown(*args, **kwargs)
+        if not self.__teardown is self.teardown:
+            self.teardown(*args, **kwargs)
+    
+    def setup(self):
+        self.tmp = path(mkdtemp())
+        self.proc = None
+    __setup = setup
+        
+    def teardown(self):
+        if self.tmp.exists():
+            self.tmp.rmtree()
+        if self.proc:
+            self.proc.terminate()
+    __teardown = teardown
+
+    def img_path(self, img):
+        return self.tmp.joinpath(self.image_files[img])
+
+#    @property
+#    def images(self):
+#        return dict((k, self.img_path(k)) for k,v in self.image_files.items())
+
+    def wait_until_passes(self, valfun, genfun=eq, classfun='assertEqual', sleep=7, res=0.5):
+        '''
+        wait upto `sleep` seconds for the test to pass
+        incredibly hackish and very probably not >the< way to do it, but alas..
+        '''
+        classfun = getattr(self, classfun)
+        for _ in range(int(sleep/res)):
+            if genfun(*valfun()):
+                break
+            time.sleep(res)
+        classfun(*valfun())
+
+    def start(self, *args, **kwargs):
+        starter = kwargs.setdefault('starter', Popen)
+        args = self.imagy + list(args)
+        self.proc = starter(args + [self.tmp])
+
+    def copy_images_over(self):
+        call(['cp'] + self.images.values() + [self.tmp])
