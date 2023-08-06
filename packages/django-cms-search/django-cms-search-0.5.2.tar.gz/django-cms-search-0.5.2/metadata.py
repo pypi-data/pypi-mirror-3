0df79fce50c4027a0a55ee452b432b9752a3ee4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:53 +0000

    Add package django-cms-search/0.5.2/django-cms-search-0.5.2.tar.gz

---
+++ packages/django-cms-search/django-cms-search-0.5.2.tar.gz/django-cms-search-0.5.2/metadata.py
@@ -0,0 +1,8 @@
+package_name = 'cms_search'
+name = 'django-cms-search'
+author = 'Divio AG'
+author_email = 'developers@divio.ch'
+description = "An extension to django CMS to provide multilingual Haystack indexes"
+version = __import__(package_name).__version__
+project_url = 'http://github.com/piquadrat/%s' % name
+license = 'BSD'
