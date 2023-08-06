3f4da6d0fd8e0cdd48a047bb547c0b6e7e2256d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:24 +0000

    Add package adrest/1.0.8/adrest-1.0.8.tar.gz

---
+++ packages/adrest/adrest-1.0.8.tar.gz/adrest-1.0.8/adrest/utils/parser.py
@@ -0,0 +1,50 @@
+from django.utils import simplejson as json
+
+from . import status
+from .exceptions import HttpError
+
+
+class BaseParser(object):
+
+    media_type = None
+
+    def __init__(self, resource):
+        self.resource = resource
+
+    @staticmethod
+    def parse(request):
+        return request.raw_post_data
+
+
+class JSONParser(BaseParser):
+
+    media_type = 'application/json'
+
+    @staticmethod
+    def parse(request):
+        try:
+            return json.loads(request.raw_post_data)
+        except ValueError, e:
+            raise HttpError('JSON parse error - %s' % str(e), status=status.HTTP_400_BAD_REQUEST)
+
+
+class XMLParser(BaseParser):
+
+    media_type = 'application/xml'
+
+
+class FormParser(BaseParser):
+
+    media_type = 'application/x-www-form-urlencoded'
+
+    @staticmethod
+    def parse(request):
+        source = dict(request.REQUEST.iteritems())
+        if request.method == "PUT" and not source:
+            # Fix django bug: request.GET, .POST are empty on PUT request
+            request.method = "POST"
+            del request._files
+            request._load_post_and_files()
+            request.method = "PUT"
+            source = dict(request.POST.iteritems())
+        return source
---
+++ packages/adrest/adrest-1.0.8.tar.gz/adrest-1.0.8/tests/main/api.py
@@ -0,0 +1,17 @@
+from adrest.api import Api
+from adrest.utils.auth import AnonimousAuthenticator, AccessKeyAuthenticator, UserAuthenticator
+from adrest.utils.emitter import XMLTemplateEmitter, JSONEmitter
+from .resourses import AuthorResource, BookPrefixResource, ArticleResource, SomeOtherResource, CustomResource
+
+
+class CustomUserAuth(UserAuthenticator):
+    username_fieldname = 'nickname'
+
+
+API = Api(version=(1, 0, 0), emitters=(XMLTemplateEmitter, JSONEmitter), api_prefix='main')
+
+API.register(AuthorResource, authenticators=(AnonimousAuthenticator, CustomUserAuth))
+API.register(BookPrefixResource)
+API.register(ArticleResource, authenticators=AccessKeyAuthenticator)
+API.register(CustomResource)
+API.register(SomeOtherResource, urlname='test', urlregex='test/mem/$')
