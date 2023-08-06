44e2493f84820b09b09474514ba56dea62dd1299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:04 +0000

    Add package datahaven/rev6311/datahaven-rev6311.tar.gz

---
+++ packages/datahaven/datahaven-rev6311.tar.gz/datahaven-rev6311/datahaven/p2p/backup_monitor.py
@@ -0,0 +1,215 @@
+#!/usr/bin/python
+#backup_monitor.py
+#
+#    Copyright DataHaven.NET LTD. of Anguilla, 2006-2012
+#    All rights reserved.
+#
+#This does a bunch of things.  
+#1)  monitor the lists of file sent back from suppliers,
+#    if there is a gap we need to try to fix it
+#    * main class is _BackupMonitor,
+#      it saves the lists of files in _BackupListFiles,
+#      breaks down the lists of files into info on a single backup in _SupplierBackupInfo
+#    * _BlockRebuilder takes care of a single broken block,
+#      request what we have available, builds whatever we can
+#      and stops either when we have fixed everything
+#      or there is nothing more we can do
+#    * _BlockRebuilder requests files through io_throttle and sends out the fixed files
+#      also through io_throttle
+#
+#2)  if a backup is unfixable, not enough information, we delete it CleanupBackups in _BackupMonitor
+#
+#3)  every hour it requests a list of files from each supplier - _hourlyRequestListFiles
+#
+#4)  every hour it tests a file from each supplier,
+#    seeing if they have the data they claim,
+#    and that it is correct
+#    * data is stored in _SuppliersSet and _SupplierRemoteTestResults,
+#    was data good, bad, being rebuilt, or they weren't online 
+#    and we got no data on the result
+#    * if a supplier hasn't been seen in settings.FireInactiveSupplierIntervalHours()
+#    we replace them
+
+
+import os
+import sys
+import time
+import random
+
+
+try:
+    from twisted.internet import reactor
+except:
+    sys.exit('Error initializing twisted.internet.reactor in backup_monitor.py')
+
+from twisted.internet.defer import Deferred, maybeDeferred
+from twisted.internet import threads
+
+
+import lib.dhnio as dhnio
+import lib.misc as misc
+import lib.nameurl as nameurl
+import lib.transport_control as transport_control
+import lib.settings as settings
+import lib.contacts as contacts
+import lib.eccmap as eccmap
+import lib.tmpfile as tmpfile
+import lib.diskspace as diskspace
+from lib.automat import Automat
+
+
+import backup_rebuilder
+import fire_hire
+import list_files_orator 
+import automats
+
+import backups
+import customerservice
+import io_throttle
+import backup_db
+import contacts_status
+
+_BackupMonitor = None
+
+#------------------------------------------------------------------------------ 
+
+def A(event=None, arg=None):
+    global _BackupMonitor
+    if _BackupMonitor is None:
+        _BackupMonitor = BackupMonitor('backup_monitor', 'READY', 4)
+    if event is not None:
+        _BackupMonitor.automat(event, arg)
+    return _BackupMonitor
+
+
+class BackupMonitor(Automat):
+    timers = {'timer-1sec':   (1,       ['RESTART']), 
+              'timer-1hour':  (60*60,   ['READY']), }
+
+    def state_changed(self, oldstate, newstate):
+        automats.set_global_state('MONITOR ' + newstate)
+
+    def A(self, event, arg):
+        #---READY---
+        if self.state == 'READY':
+            if event == 'restart' or event == 'timer-1hour':
+                self.state = 'RESTART'
+        #---RESTART---
+        elif self.state == 'RESTART':
+            if event == 'timer-1sec' and backup_rebuilder.A().state in [ 'STOPPED' , 'DONE' ]:
+                list_files_orator.A('need-files')
+                self.state = 'LIST_FILES'
+        #---LIST_FILES---
+        elif self.state == 'LIST_FILES':
+            if event == 'list_files_orator.state' and arg is 'SAW_FILES':
+                self.doPrepareListBackups()
+                self.state = 'LIST_BACKUPS'
+            elif event == 'list_files_orator.state' and arg is 'NO_FILES':
+                self.state = 'READY'
+            elif event == 'restart':
+                self.state = 'RESTART'
+        #---LIST_BACKUPS---
+        elif self.state == 'LIST_BACKUPS':
+            if event == 'list-backups-done':
+                backup_rebuilder.A('start')
+                self.state = 'REBUILDING'
+            elif event == 'restart':
+                self.state = 'RESTART'
+        #---REBUILDING---
+        elif self.state == 'REBUILDING':
+            if event == 'backup_rebuilder.state' and arg is 'DONE':
+                fire_hire.A('start')
+                self.state = 'FIRE_HIRE'
+            elif event == 'backup_rebuilder.state' and arg is 'STOPPED':
+                self.state = 'READY'
+            elif event == 'restart':
+                backup_rebuilder.SetStoppedFlag()
+                self.state = 'RESTART'
+        #---FIRE_HIRE---
+        elif self.state == 'FIRE_HIRE':
+            if event == 'hire-new-supplier' or event == 'restart': # or ( event == 'fire-hire-finished' and self.isFailedSuppliers() ):
+                self.state = 'RESTART'
+            elif event == 'fire-hire-finished': # and not self.isFailedSuppliers():
+                self.doCleanUpBackups()
+                self.state = 'READY'
+
+    def doPrepareListBackups(self):
+        # take remote and local backups and get union from it 
+        allBackupIDs = set(backups.local_files().keys() + backups.remote_files().keys())
+        # take only backups from data base
+        allBackupIDs.intersection_update(backup_db.GetBackupIds())
+        # remove running backups
+        allBackupIDs.difference_update(backups.backups_in_process())
+        # sort it in reverse order - newer backups should be repaired first
+        allBackupIDs = misc.sorted_backup_ids(list(allBackupIDs), True)
+        # add backups to the queue
+        backup_rebuilder.AddBackupsToWork(allBackupIDs)
+        # update suppliers, clear stats for all 
+        backups.suppliers_set().UpdateSuppliers(contacts.getSupplierIDs()) 
+        dhnio.Dprint(6, 'backup_monitor.doPrepareListBackups %s' % str(allBackupIDs))
+        self.automat('list-backups-done')
+
+    def doCleanUpBackups(self):
+        backupsToKeep = settings.getGeneralBackupsToKeep()
+        if backupsToKeep < 1:
+            backupsToKeep = 1
+        for backupDir in backup_db.GetBackupDirectories():
+            backupIDsForDirectory = backup_db.GetDirBackupIds(backupDir)
+            while len(backupIDsForDirectory) > backupsToKeep:
+                backupID = backupIDsForDirectory.pop(0)
+                dhnio.Dprint(6, 'backup_monitor.doCleanUpBackups wish to remove %s of %s' % (backupID, backupDir))
+                backup_db.AbortRunningBackup(backupID)
+                backups.DeleteBackup(backupID)
+                
+
+def Restart():
+    dhnio.Dprint(4, 'backup_monitor.Restart')
+    A('restart')
+
+
+
+
+        
+
+#    def CleanupBackups(self):
+#        #TODO - track if we've heard from enough suppliers to know a backup doesn't exist
+#        backupsToKeep = int(settings.getGeneralBackupsToKeep())
+#        dhnio.Dprint(12+self.dprintAdjust, "backup_monitor.CleanupBackups, keep " + str(backupsToKeep) + " backups")
+#        foundBackups = 0 # how many backups for a directory we've found
+#        didDelete = False
+#        delay = 1
+#        #potentially called many times after a request list files all
+#        if time.time() - self.backupDBCheck > 60.0: 
+#            # if it has been longer than x seconds since the last time we ran this
+#            self.backupDBCheck = time.time()
+#            backupDirectories = backup_db.GetBackupDirectories()
+#            for backupDir in backupDirectories:
+#                foundBackups = 0
+#                backupIDsForDirectory = backup_db.GetDirBackupIds(backupDir)
+#                if len(backupIDsForDirectory) > 0:
+#                    backupIDsForDirectory.reverse()
+#                    for backupId in backupIDsForDirectory:
+#                        if backupId not in backups_in_process():
+#                            if self.backupListFiles.has_key(str(backupId)):
+#                                foundBackups = foundBackups + 1
+#                                if backupsToKeep > 0 and foundBackups > backupsToKeep:
+#                                    dhnio.Dprint(2+self.dprintAdjust, "backup_monitor.CleanupBackups delete backup " + str(backupId) + " for directory " + str(backupDir) + " beyond the " + str(backupsToKeep) + " backups to keep")
+#                                    self.DeleteBackup(backupId)
+#                                    #customerservice.DeleteWholeBackup(backupId)
+#                                    didDelete = True
+#                            else: 
+#                                # so we have something in our list of backups 
+#                                # that should have happened, but no suppliers have info on it
+#                                if len(self.rawListFiles) > self.eccMap.CorrectableErrors:
+#                                    dhnio.Dprint(2+self.dprintAdjust, "backup_monitor.CleanupBackups too many suppliers don't have info on backup " + backupId + ", missing " + str(len(self.rawListFiles)) + " correctable " + str(self.eccMap.CorrectableErrors))
+#                                    #   we temporary remove this feature
+#                                    #   because something going wrong
+#                                    #   backups are deleted after finishing
+#                                    #backup_db.DeleteDirBackup(backupId)
+#                                    #customerservice.DeleteWholeBackup(backupId) 
+#                                    # may not be necessary if all suppliers don't have the backup
+#                                    didDelete = True
+#            if didDelete:
+#                dhnio.Dprint(12+self.dprintAdjust, "backup_monitor.CleanupBackups scheduling another listfilesall at " + str(time.asctime(time.localtime(time.time()))))
+#                reactor.callLater(30,customerservice.RequestListFilesAll)
+
---
+++ packages/datahaven/datahaven-rev6311.tar.gz/datahaven-rev6311/datahaven/p2p/initializer.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+#initializer.py
+#
+#    Copyright DataHaven.NET LTD. of Anguilla, 2006-2012
+#    All rights reserved.
+#
+#
+
+import os
+import sys
+
+try:
+    from twisted.internet import reactor
+except:
+    sys.exit('Error initializing twisted.internet.reactor in automat.py')
+from twisted.internet.defer import Deferred, maybeDeferred
+from twisted.internet.task import LoopingCall
+
+import lib.dhnio as dhnio
+from lib.automat import Automat
+
+import network_connector
+import p2p_connector
+import installer
+import automats
+
+import dhninit
+import webcontrol
+
+_Initializer = None
+
+#------------------------------------------------------------------------------
+
+def A(event=None, arg=None, use_reactor=True):
+    global _Initializer
+    if _Initializer is None:
+        _Initializer = Initializer('initializer', 'AT_STARTUP', 2)
+    if event is not None:
+        if use_reactor:
+            _Initializer.automat(event, arg)
+        else:
+            _Initializer.event(event, arg)
+    return _Initializer
+
+
+class Initializer(Automat):
+    
+    def init(self):
+        self.flagCmdLine = False
+        self.is_installed = None
+    
+    def state_changed(self, oldstate, newstate):
+        automats.set_global_state('INIT ' + newstate)
+
+    def A(self, event, arg):
+        #---AT_STARTUP---
+        if self.state == 'AT_STARTUP':
+            if event == 'run':
+                self.doInitLocal(arg)
+                self.state = 'LOCAL'
+            elif event == 'run-cmd-line-register':
+                self.flagCmdLine = True
+                installer.A('register-cmd-line', arg)
+                self.state = 'INSTALL'
+            elif event == 'run-cmd-line-recover':
+                self.flagCmdLine = True
+                installer.A('recover-cmd-line', arg)
+                self.state = 'INSTALL'
+        #---LOCAL---
+        elif self.state == 'LOCAL':
+            if event == 'init-local-done' and self.isInstalled():
+                self.doInitContacts()
+                self.state = 'CONTACTS'
+            elif event == 'init-local-done' and not self.isInstalled() and self.isGUIPossible():
+                self.doShowGUI()
+                installer.A('init')
+                self.doUpdate()
+                self.state = 'INSTALL'
+            elif event == 'init-local-done' and not self.isInstalled() and not self.isGUIPossible():
+                self.doExit()
+                self.state = 'STOPPING'
+        #---CONTACTS---
+        elif self.state == 'CONTACTS':
+            if event == 'init-contacts-done':
+                self.doInitConnection()
+                network_connector.A('init')
+                p2p_connector.A('init')
+                self.doShowGUI()
+                self.doUpdate()
+                self.state = 'CONNECTION'
+            elif event == 'init-contacts-failed':
+                self.doExit()
+                self.doUpdate()
+                self.state = 'STOPPING'
+        #---CONNECTION---
+        elif self.state == 'CONNECTION':
+            if event == 'p2p_connector.state' and arg in ['CONNECTED', 'DISCONNECTED']:
+                self.doInitModules()
+                self.doUpdate()
+                self.state = 'MODULES'
+        #---MODULES---
+        elif self.state == 'MODULES':
+            if event == 'init-modules-done':
+                self.doUpdate()
+                self.state = 'READY'
+        #---INSTALL---
+        elif self.state == 'INSTALL':
+            if not self.flagCmdLine and event == 'installer.state' and arg == 'DONE':
+                self.doInitContacts()
+                self.doUpdate()
+                self.state = 'CONTACTS'
+            elif self.flagCmdLine and event == 'installer.state' and arg == 'DONE':
+                self.doExit()
+                self.state = 'STOPPING'
+        #---READY---
+        elif self.state == 'READY':
+            if event == 'reactor-stopped':
+                self.doDestroyMe()
+                self.state = 'EXIT'
+        #---STOPPING
+        elif self.state == 'STOPPING':
+            if event == 'reactor-stopped':
+                self.doDestroyMe()
+                self.state = 'EXIT'
+        #---EXIT---
+        elif self.state == 'EXIT':
+            pass
+
+    def isInstalled(self):
+        if self.is_installed is None:
+            self.is_installed = dhninit.check_install() 
+        return self.is_installed
+    
+    def isGUIPossible(self):
+        if dhnio.Windows():
+            return True
+        if dhnio.Linux():
+            return dhnio.X11_is_running()
+        return False
+
+    def doUpdate(self):
+        webcontrol.OnUpdateStartingPage()
+
+    def doInitLocal(self, arg):
+        maybeDeferred(dhninit.init_local, arg).addCallback(
+            lambda x: self.automat('init-local-done'))
+
+    def doInitContacts(self):
+        dhninit.init_contacts(
+            lambda x: self.automat('init-contacts-done'),
+            lambda x: self.automat('init-contacts-failed'), )
+
+    def doInitConnection(self):
+        dhninit.init_connection()
+
+    def doInitModules(self):
+        maybeDeferred(dhninit.init_modules).addCallback(
+            lambda x: self.automat('init-modules-done'))
+
+    def doShowGUI(self):
+        d = webcontrol.init()
+        if dhninit.UImode == 'show' or not self.is_installed: 
+            d.addCallback(webcontrol.show)
+        webcontrol.ready()
+
+    def doExit(self):
+        dhninit.shutdown_exit()
+
+    def doDestroyMe(self):
+        global _Initializer
+        del _Initializer
+        _Initializer = None
+        automats.get_automats_by_index().pop(self.index)
+
+
+
+
