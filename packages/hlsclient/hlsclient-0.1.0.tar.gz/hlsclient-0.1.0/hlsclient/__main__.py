ef707928174c1cc6a70893232de9c6a2f5032320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:32 +0000

    Add package hlsclient/0.1.0/hlsclient-0.1.0.tar.gz

---
+++ packages/hlsclient/hlsclient-0.1.0.tar.gz/hlsclient-0.1.0/hlsclient/__main__.py
@@ -0,0 +1,52 @@
+import ConfigParser
+import logging
+import time
+import os
+
+from balancer import Balancer
+from discover import discover
+from consumer import consume
+
+def load_config(path=None):
+    if path is None:
+        path = os.getenv('HLSCLIENT_CONFIG', 'config.ini')
+    config = ConfigParser.RawConfigParser()
+    with open(path) as f:
+        config.readfp(f)
+    return config
+
+def main():
+    logging.basicConfig()
+    logger = logging.getLogger('hls-client')
+    logger.setLevel(logging.DEBUG)
+    logger.debug('HLS CLIENT Started')
+
+    config = load_config()
+    destination = config.get('hlsclient', 'destination')
+    logger.debug('Config loaded')
+
+    balancer = Balancer()
+
+    while True:
+        paths = discover(config)
+        logger.info(u'Discovered the following paths: %s' % paths.items())
+        balancer.update(paths)
+
+        for resource in balancer.actives:
+            resource_path = str(resource)
+            logger.debug('Consuming %s' % resource_path)
+            try:
+                modified = consume(resource_path, destination)
+            except (IOError, OSError) as err:
+                logger.warning(u'Notifying error for resource %s: %s' % (resource_path, err))
+                balancer.notify_error(resource.server, resource.path)
+            else:
+                if modified:
+                    logger.info('Notifying content modified: %s' % resource)
+                    balancer.notify_modified(resource.server, resource.path)
+                else:
+                    logger.debug('Content not modified: %s' % resource)
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
---
+++ packages/hlsclient/hlsclient-0.1.0.tar.gz/hlsclient-0.1.0/hlsclient/balancer.py
@@ -0,0 +1,80 @@
+from collections import deque
+import datetime
+
+class Balancer(object):
+    '''
+    Controls which server is active for a playlist (m3u8)
+    '''
+    NOT_MODIFIED_TOLERANCE = 8 # in seconds
+
+    def update(self, paths):
+        '''
+        ``paths`` is a dict returned from ``discover.discover()``
+        '''
+        self.paths = {}
+        self.modified_at = {}
+        for path, servers in paths.items():
+            self.paths[path] = deque(servers)
+
+    def notify_modified(self, server, path):
+        '''
+        Remembers that a given server returned a new playlist
+        '''
+        self.modified_at[path] = self._now()
+
+    def notify_error(self, server, path):
+        '''
+        Remembers that a given server failed.
+        This immediately changes the active server for this path, is another one exists.
+        '''
+        if self._active_server_for_path(path) == server:
+            self._change_active_server(path)
+
+    @property
+    def actives(self):
+        '''
+        Returns a list of ``PlaylistResource``s
+        '''
+        for path in self.paths:
+            active_server = self._active_server_for_path(path)
+            if self._outdated(active_server, path):
+                self._change_active_server(path)
+                active_server = self._active_server_for_path(path)
+            yield PlaylistResource(active_server, path)
+
+    def _active_server_for_path(self, path):
+        return self.paths[path][0]
+
+    def _change_active_server(self, path):
+        self.paths[path].rotate(-1)
+        self.modified_at[path] = None
+
+    def _outdated(self, server, path):
+        last_change = self.modified_at.get(path, None)
+        if not last_change:
+            # This server is new, so it can't be obsolete
+            return False
+        delta_from_last_change = self._now() - last_change
+        delta_tolerance = datetime.timedelta(seconds=self.NOT_MODIFIED_TOLERANCE)
+        return delta_from_last_change > delta_tolerance
+
+    def _now(self):
+        # The only reason for this to be a method
+        # is that it's easier to monkey patch it
+        return datetime.datetime.now()
+
+
+class PlaylistResource(object):
+    
+    def __init__(self, server, path):
+        self.server = server
+        self.path = path
+
+    def __str__(self):
+        return 'http://{server}:{port}{path}'.format(
+            server=self.server.server,
+            port=self.server.port,
+            path=self.path)
+
+    def __unicode__(self):
+        return str(self)
