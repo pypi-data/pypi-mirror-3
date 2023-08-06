f25f3d880544d470e614a4455a29feb99dac595b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:11 +0000

    Add package flyingsphinx/0.0.5/flyingsphinx-0.0.5.tar.gz

---
+++ packages/flyingsphinx/flyingsphinx-0.0.5.tar.gz/flyingsphinx-0.0.5/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import flyingsphinx
+
+from setuptools import setup
+
+os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
+
+setup(
+  name                 = 'flyingsphinx',
+  version              = flyingsphinx.__version__,
+  description          = 'Flying Sphinx Python client',
+  long_description     = 'Flying Sphinx API client for Python applications',
+  author               = 'Pat Allan',
+  author_email         = 'pat@freelancing-gods.com',
+  url                  = 'https://github.com/flying-sphinx/flying-sphinx-py',
+  packages             = ['flyingsphinx'],
+  install_requires     = ['requests'],
+  license              = open('LICENCE').read(),
+  classifiers          = (
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7'
+  )
+)
