12678a8465cf733c6c2e1e6551ebbd68274872ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:04 +0000

    Add package locaweb_gateway/v0.1.1/locaweb_gateway-v0.1.1.tar.gz

---
+++ packages/locaweb_gateway/locaweb_gateway-v0.1.1.tar.gz/locaweb_gateway-v0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+LOCAWEB_GATEWAY_VERSION = 'v0.1.1'
+
+setup(name='locaweb_gateway',
+    version=LOCAWEB_GATEWAY_VERSION,
+    description='Locaweb gateway bindings',
+    author='locaweb',
+    packages=['locaweb_gateway'],
+    install_requires=['requests >= 0.8.8', 'simplejson'],
+    author_email = "saas-dev@locaweb.com.br",
+    license = "MIT license",
+    keywords = "locaweb gateway pagamento checkout",
+    url = "https://github.com/locaweb/locaweb-gateway-python",
+    include_package_data=True
+)
