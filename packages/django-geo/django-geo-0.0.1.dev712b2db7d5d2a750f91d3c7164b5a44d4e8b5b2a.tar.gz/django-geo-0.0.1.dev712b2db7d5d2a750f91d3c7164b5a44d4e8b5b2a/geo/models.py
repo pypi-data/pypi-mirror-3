d826135cb6ced0de4215b915ce5e7a062e944e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:46 +0000

    Add package django-geo/0.0.1.dev712b2db7d5d2a750f91d3c7164b5a44d4e8b5b2a/django-geo-0.0.1.dev712b2db7d5d2a750f91d3c7164b5a44d4e8b5b2a.tar.gz

---
+++ packages/django-geo/django-geo-0.0.1.dev712b2db7d5d2a750f91d3c7164b5a44d4e8b5b2a.tar.gz/django-geo-0.0.1.dev712b2db7d5d2a750f91d3c7164b5a44d4e8b5b2a/geo/models.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+'''
+Created on May 7, 2010
+
+@author: sax
+'''
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.db.models.manager import Manager
+from django.utils.translation import gettext_lazy as _
+import logging
+from mptt.exceptions import InvalidMove
+from mptt.managers import TreeManager
+from mptt.models import MPTTModel, TreeForeignKey
+
+logger = logging.getLogger("geo")
+
+
+class Currency(models.Model):
+    code = models.CharField(max_length=5)
+    name = models.CharField(max_length=30)
+    symbol = models.CharField(max_length=5)
+
+    class Meta:
+        app_label = 'geo'
+        ordering = ['code', ]
+
+CONTINENTS = (
+    ('AF', _('Africa')),
+    ('AS', _('Asia')),
+    ('EU', _('Europe')),
+    ('NA', _('North America')),
+    ('SA', _('South America')),
+    ('OC', _('Oceania')),
+    ('AN', _('Antartica'))
+    )
+
+Regions = zip(range(1, 5), ('Africa', 'Americas', 'Asia', 'Middle East'))
+
+class CountryManager(Manager):
+    def _by_continent(self, c):
+        return self.get_query_set().filter(continent=c)
+
+    def asia(self):
+        return self._by_continent('AS')
+
+    def europa(self):
+        return self._by_continent('EU')
+
+    def north_america(self):
+        return self._by_continent('NA')
+
+    def south_america(self):
+        return self._by_continent('SA')
+
+    def oceania(self):
+        return self._by_continent('OC')
+
+    def antartica(self):
+        return self._by_continent('AN')
+
+    def get_by_natural_key(self, iso_code):
+        return self.get(iso_code=iso_code)
+
+
+class Country(models.Model):
+    """ Model for the country of origin """
+    iso_code = models.CharField(max_length=2, unique=True, blank=False, null=False, db_index=True)
+    iso3_code = models.CharField(max_length=3, blank=True, null=True, db_index=True)
+    num_code = models.CharField(max_length=3, blank=True, null=True)
+    name = models.CharField(max_length=100, db_index=True)
+    fullname = models.CharField(max_length=100, db_index=True)
+
+    region = models.IntegerField(choices=Regions, blank=True, null=True)
+    continent = models.CharField(choices=CONTINENTS, max_length=2)
+    currency = models.ForeignKey(Currency, blank=True, null=True)
+
+    fullname.alphabetic_filter = True
+    objects = CountryManager()
+
+    class Meta:
+        app_label = 'geo'
+        verbose_name_plural = _('Countries')
+        ordering = ['name']
+
+    def __unicode__(self):
+        return self.fullname
+
+    def natural_key(self):
+        return (self.iso_code,)
+
+    def __contains__(self, item):
+        if hasattr(item, 'country'):
+            return item.country == self
+
+
+class AdministrativeAreaTypeManager(TreeManager):
+    use_for_related_fields = True
+
+    def get_by_natural_key(self, iso_code, name):
+        return self.get(country__iso_code=iso_code, name=name)
+
+
+class AdministrativeAreaType(MPTTModel):
+    name = models.CharField(_('Name'), max_length=100, db_index=True)
+    country = models.ForeignKey(Country)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
+    objects = AdministrativeAreaTypeManager()
+
+    class Meta:
+        verbose_name = _("Administrative Area Type")
+        verbose_name_plural = _("Administrative Area Types")
+        app_label = 'geo'
+        ordering = ['name']
+        order_with_respect_to = 'country'
+
+    def __unicode__(self):
+        return self.name
+
+    def natural_key(self):
+        return self.country.natural_key() + (self.name, )
+
+    natural_key.dependencies = ['geo.country']
+
+    def clean(self):
+        if self.parent == self:
+            raise ValidationError(_('`%s` cannot contains same type') % self.parent)
+        super(AdministrativeAreaType, self).clean()
+
+class AdministrativeAreaManager(TreeManager):
+    use_for_related_fields = True
+
+    def get_by_natural_key(self, iso_code, name):
+        return self.get(country__iso_code=iso_code, name=name)
+
+    def get_or_create(self, **kwargs):
+        return super(AdministrativeAreaManager, self).get_or_create(**kwargs)
+
+
+class AdministrativeArea(MPTTModel):
+    name = models.CharField(_('Name'), max_length=255, db_index=True)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='areas')
+    country = models.ForeignKey(Country, related_name='areas')
+    type = models.ForeignKey(AdministrativeAreaType)
+    objects = AdministrativeAreaManager()
+
+    class Meta:
+        verbose_name = _("Administrative Area")
+        verbose_name_plural = _("Administrative Areas")
+        app_label = 'geo'
+        ordering = ['name']
+        order_with_respect_to = 'country'
+
+    def __unicode__(self):
+        return self.name
+
+    def natural_key(self):
+        return (self.country.iso_code, self.name )
+    natural_key.dependencies = ['geo.country']
+
+    def clean(self):
+        if self.parent == self:
+            raise ValidationError(_('`%s` cannot contains self') % self)
+        if self.parent and self.parent.type == self.type:
+            raise ValidationError(_('`%s` cannot contains same type') % self.parent.type)
+        if (self.pk and self.parent) and self in self.parent:
+            raise ValidationError(_('`%s` cannot contains same type') % self.parent)
+
+        super(AdministrativeArea, self).clean()
+
+    def save(self, *args, **kwargs):
+        if not self.country_id:
+            self.country = self.parent.country
+        self.clean()
+        super(AdministrativeArea, self).save(*args, **kwargs)
+
+    def __contains__(self, item):
+        return True
+
+
+class Location(models.Model):
+    """ Geographical location ( city, place everything with a name and Lat/Lng"""
+    NONE = 0
+    COUNTRY = 10
+    EXACT = 20
+
+    ACCURACY = (
+        (NONE, _('None')),
+        (COUNTRY, _('Country')),
+        (EXACT, _('Exact')),
+        )
+
+    CITY = 0
+    OTHER = 1
+    TYPE = ((CITY, _("City")),
+            (OTHER, _("Other")),
+        )
+
+    country = models.ForeignKey(Country, db_index=True)
+    area = models.ForeignKey(AdministrativeArea, db_index=True, blank=True, null=True)
+    type = models.IntegerField(choices=TYPE, default=CITY)
+    is_capital = models.BooleanField(default=False,
+        help_text="True if is the capital of `country`")
+
+    is_administrative = models.BooleanField(default=False,
+        help_text="True if is administrative for `area`")
+
+    name = models.CharField(_('Name'), max_length=255, db_index=True)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    lat = models.DecimalField(max_digits=18, decimal_places=12, blank=True, null=True)
+    lng = models.DecimalField(max_digits=18, decimal_places=12, blank=True, null=True)
+    acc = models.IntegerField(choices=ACCURACY, default=NONE, blank=True, null=True,
+        help_text="Define the level of accuracy of lat/lng infos")
+
+    class Meta:
+        verbose_name_plural = _('Locations')
+        verbose_name = _('Location')
+        app_label = 'geo'
+        ordering = ("name",)
+        order_with_respect_to = 'country'
+
+    def __unicode__(self):
+        return self.name
+
+    def natural_key(self):
+        return self.country.natural_key() + (self.name, str(self.lat), str(self.lng))
+    natural_key.dependencies = ['geo.country']
+
+    def clean(self):
+        super(Location, self).clean()
