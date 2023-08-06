0d5a644afa472c27b3d72408cec1f3dc34524cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:46 +0000

    Add package python-statsd/1.4.2/python-statsd-1.4.2.tar.gz

---
+++ packages/python-statsd/python-statsd-1.4.2.tar.gz/python-statsd-1.4.2/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+if os.path.isfile('README.rst'):
+    long_description = open('README.rst').read()
+else:
+    long_description = 'See http://pypi.python.org/pypi/python-statsd/'
+
+setup(
+    name = 'python-statsd',
+    version = '1.4.2',
+    author = 'Rick van Hattem',
+    author_email = 'Rick.van.Hattem@Fawo.nl',
+    description = '''statsd is a client for Etsy's node-js statsd server. 
+        A proxy for the Graphite stats collection and graphing server.''',
+    url='https://github.com/WoLpH/python-statsd',
+    license = 'BSD',
+    packages=['statsd'],
+    long_description=long_description,
+    test_suite='nose.collector',
+    setup_requires=['nose'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+    ],
+)
