3d46f4521fa19abe9c89cc39a2938fb3409910f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:10 +0000

    Add package meteo/0.2.4/meteo-0.2.4.tar.gz

---
+++ packages/meteo/meteo-0.2.4.tar.gz/meteo-0.2.4/meteo/humidity.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# Filename: radiation.py
+
+from numpy import log10
+
+# Description:
+# conversions between different humidity quantities
+
+Mw=18.0160 # molecular weight of water
+Md=28.9660 # molecular weight of dry air
+R =  8.31432E3 # gas constant
+Rd = R/Md # specific gas constant for dry air
+Rv = R/Mw # specific gas constant for vapour
+eps = Mw/Md
+#saturation pressure
+def esat(T):
+    ''' get sateration pressure (units [Pa]) for a given air temperature (units [K])'''
+    from numpy import log10
+    TK = 273.15
+    e1 = 101325.0
+    logTTK = log10(T/TK)
+    esat =  e1*10**(10.79586*(1-TK/T)-5.02808*logTTK+ 1.50474*1e-4*(1.-10**(-8.29692*(T/TK-1)))+ 0.42873*1e-3*(10**(4.76955*(1-TK/T))-1)-2.2195983) 
+    return esat
+
+# conversion relative humidity to mixing ratio
+def rh2mixr(RH,p,T):
+    es = esat(T)
+    return Mw/Md*RH/100.*es/(p-RH/100.*es)*1000.
+
+# conversion mixing ratio to specific humidity (always kg/kg)
+def mixr2sh(W):
+    '''conversion from mixing ratio (units [kg/kg]) to specific humidity (units also [kg/kg])
+    '''
+    return W/(1.+W)
+def sh2mixr(qv):
+    '''conversion from specific humidity (units [kg/kg]) to mixing ratio (units also [kg/kg])
+    '''
+    return qv/(1.-qv)
+def ea2mixr(P,ea):
+    '''conversion from dry air and vapour pressure (units [Pa]) to mixing ratio (units [kg/kg]) 
+    '''
+    return 0.622*ea/(P-ea)
+def mixr2ea(P,mixr):
+    '''conversion from dry air pressure (units [Pa]) and mixing ratio (units [kg/kg] to vapour pressure (units [Pa])
+    '''
+    return P*mixr/(0.622+ea)
+
+# not tested
+def ah2mixr (rhov,p,T):
+    '''conversion from absolute humidity (units [kg/m**3]) to mixing ratio (units also [kg/kg])
+    '''
+    return (Rd * T)/(p/rhov-Rv*T)
+
+# not tested
+def wvap2sh(e,p):
+    '''conversion from water vapour pressure (units [Pa]) to specific humidity (units [kg/kg]) 
+    '''
+    return eps*e/(p-(1.-eps)*e)
+
