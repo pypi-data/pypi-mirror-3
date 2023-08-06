3c2755c192edab3ea434ef47016e9f698eaa4535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:42 +0000

    Add package WhooshAlchemy/0.1.4/WhooshAlchemy-0.1.4.tar.gz

---
+++ packages/WhooshAlchemy/WhooshAlchemy-0.1.4.tar.gz/WhooshAlchemy-0.1.4/setup.py
@@ -0,0 +1,36 @@
+"""
+WhooshAlchemy
+-------------
+
+Whoosh extension to SQLAlchemy.
+"""
+
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='WhooshAlchemy',
+    version='0.1.4',
+    url='https://github.com/sfermigier/WhooshAlchemy',
+    license='BSD',
+    author='Stefane Fermigier',
+    author_email='sf@fermigier.com',
+    description='Whoosh extension to SQLAlchemy',
+    long_description=long_description,
+    py_modules=['whooshalchemy'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'whoosh', 'sqlalchemy'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite = 'test',
+)
