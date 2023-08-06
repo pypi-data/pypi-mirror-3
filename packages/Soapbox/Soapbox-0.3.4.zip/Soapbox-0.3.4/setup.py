6d05bf4551eb31bf0395455223d55baea89e66b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:32 +0000

    Add package Soapbox/0.3.4/Soapbox-0.3.4.zip

---
+++ packages/Soapbox/Soapbox-0.3.4.zip/Soapbox-0.3.4/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) Damian Powązka and Flight Data Services, Ltd.
+# See the file "LICENSE" for the full license governing this code.
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import soapbox as pkg
+from requirements import RequirementsParser
+requirements = RequirementsParser()
+
+setup(
+    name=pkg.__packagename__,
+    version=pkg.__version__,
+    author=pkg.__author__,
+    author_email=pkg.__author_email__,
+    maintainer=pkg.__maintainer__,
+    maintainer_email=pkg.__maintainer_email__,
+    url=pkg.__url__,
+    description=pkg.__description__,
+    long_description=open('README.rst').read() + open('CHANGES').read() +
+    open('TODO').read() + open('AUTHORS').read(),
+    download_url=pkg.__download_url__,
+    classifiers=pkg.__classifiers__,
+    platforms=pkg.__platforms__,
+    license=pkg.__license__,
+    keywords=pkg.__keywords__,
+    packages=find_packages(exclude=('examples', 'tests',)),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requirements.install_requires,
+    setup_requires=requirements.setup_requires,
+    tests_require=requirements.tests_require,
+    extras_require=requirements.extras_require,
+    dependency_links=requirements.dependency_links,
+    test_suite='nose.collector',
+    entry_points={
+        'console_scripts': [
+            'py2wsdl=soapbox.py2wsdl:main',
+            'py2xsd=soapbox.py2xsd:main',
+            'wsdl2py=soapbox.wsdl2py:main',
+            'xsd2py=soapbox.xsd2py:main',
+        ],
+    },
+    package_data={
+        'soapbox.templates': ['wsdl', 'xsd'],
+    },
+)
+
+################################################################################
+# vim:et:ft=python:nowrap:sts=4:sw=4:ts=4
---
+++ packages/Soapbox/Soapbox-0.3.4.zip/Soapbox-0.3.4/soapbox/xsd2py.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+################################################################################
+
+'''
+'''
+
+################################################################################
+# Imports
+
+
+import argparse
+import hashlib
+import keyword
+import logging
+import textwrap
+
+from jinja2 import Environment, PackageLoader
+from lxml import etree
+
+from .xsdspec import Schema
+from .utils import (
+    capitalize,
+    find_xsd_namespaces,
+    get_get_type,
+    open_document,
+    remove_namespace,
+    url_template,
+    use,
+)
+
+
+################################################################################
+# Constants
+
+
+TEMPLATE_PACKAGE = 'soapbox.templates'
+
+
+################################################################################
+# Globals
+
+
+logger = logging.getLogger('soapbox')
+logger.addHandler(logging.NullHandler())
+
+
+################################################################################
+# Helpers
+
+
+def get_rendering_environment():
+    '''
+    '''
+    pkg = TEMPLATE_PACKAGE.split('.')
+    env = Environment(
+        extensions=['jinja2.ext.loopcontrols'],
+        loader=PackageLoader(*pkg),
+    )
+    env.filters['capitalize'] = capitalize
+    env.filters['remove_namespace'] = remove_namespace
+    env.filters['url_template'] = url_template
+    env.filters['use'] = use
+    env.globals['keywords'] = keyword.kwlist
+    env.globals['resolve_import'] = resolve_import
+    env.globals['schema_name'] = schema_name
+    return env
+
+
+def resolve_import(xsdimport, known_namespaces):
+    '''
+    '''
+    logger.info('Generating code for XSD import \'%s\'...' % xsdimport.schemaLocation)
+    xml = open_document(xsdimport.schemaLocation)
+    xmlelement = etree.fromstring(xml)
+    return generate_code_from_xsd(xmlelement, known_namespaces, xsdimport.schemaLocation)
+
+
+def schema_name(namespace):
+    '''
+    '''
+    return hashlib.sha512(namespace).hexdigest()[0:5]
+
+
+def generate_code_from_xsd(xmlelement, known_namespaces=None, location=None):
+    '''
+    '''
+    if known_namespaces is None:
+        known_namespaces = []
+    xsd_namespace = find_xsd_namespaces(xmlelement.nsmap)
+
+    schema = Schema.parse_xmlelement(xmlelement)
+
+    # Skip if this schema has already been included:
+    if schema.targetNamespace in known_namespaces:
+        return ''
+
+    return schema_to_py(schema, xsd_namespace, known_namespaces, location)
+
+
+def schema_to_py(schema, xsd_namespace, known_namespaces=None, location=None):
+    '''
+    '''
+    if known_namespaces is None:
+        known_namespaces = []
+    known_namespaces.append(schema.targetNamespace)
+
+    env = get_rendering_environment()
+    env.filters['type'] = get_get_type(xsd_namespace)
+    env.globals['known_namespaces'] = known_namespaces
+    env.globals['location'] = location
+
+    tpl = env.get_template('xsd')
+    return tpl.render(schema=schema)
+
+
+################################################################################
+# Program
+
+
+def parse_arguments():
+    '''
+    '''
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+            Generates Python code from an XSD document.
+        '''))
+    parser.add_argument('xsd', help='The path to an XSD document.')
+    return parser.parse_args()
+
+
+def main():
+    '''
+    '''
+    opt = parse_arguments()
+
+    logger.info('Generating code for XSD document \'%s\'...' % opt.xsd)
+    xml = open_document(opt.xsd)
+    xmlelement = etree.fromstring(xml)
+    print textwrap.dedent('''\
+    from soapbox import xsd
+    from soapbox.xsd import UNBOUNDED
+    ''')
+    print generate_code_from_xsd(xmlelement)
+
+
+if __name__ == '__main__':
+
+    main()
+
+
+################################################################################
+# vim:et:ft=python:nowrap:sts=4:sw=4:ts=4
