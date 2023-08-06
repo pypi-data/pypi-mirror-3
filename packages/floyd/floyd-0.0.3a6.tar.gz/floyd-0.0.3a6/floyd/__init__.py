2cc8a906f4ca5fc46632e7aeac6945ddd8d974d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:14 +0000

    Add package floyd/0.0.3a6/floyd-0.0.3a6.tar.gz

---
+++ packages/floyd/floyd-0.0.3a6.tar.gz/floyd-0.0.3a6/floyd/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""A command line static website generator for popular cloud hosting providers
+
+:copyright: Copyright (C) 2012 Nik Cubrilovic and others, see AUTHORS
+:license: new BSD, see LICENSE for more details.
+
+"""
+
+import sys
+from floyd.core.command_utils import run_cl
+
+VERSION = (0, 0, 3, 'alpha', 6)
+
+__clsname__ = 'floyd'
+__author__ = 'Nik Cubrilovic <nikcub@gmail.com>'
+__email__ = 'nikcub@gmail.com'
+__url__ = 'http://github.com/nikcub/floyd'
+__license__ = 'BSD'
+__copyright__ = 'Copyright (c) 2012, Nik Cubrilovic. All rights reserved.'
+
+
+def get_version(version=None):
+  if version is None:
+    version = VERSION
+  assert version[3] in ('alpha', 'beta', 'rc', 'final')
+  parts = 2 if version[2] == 0 else 3
+  main = '.'.join(str(x) for x in version[:parts])
+  sub = ''
+  if version[3] != 'final':
+    mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
+    sub = mapping[version[3]] + str(version[4])
+  return main + sub
+
+if __name__ == '__main__':
+  sys.exit(run_cl())
+
---
+++ packages/floyd/floyd-0.0.3a6.tar.gz/floyd-0.0.3a6/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import os
+import platform
+import floyd
+
+
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
+
+
+if getattr(sys, 'version_info', (0, 0, 0)) < (2, 5, 0, 'final'):
+    raise SystemExit("floyd requires Python 2.5 or later.")
+
+if sys.argv[-1] == 'publish':
+  os.system('python setup.py sdist upload')
+  sys.exit()
+
+scripts = ['flask/bin/floyd']
+packages = [
+  'jinja2',
+  'markdown',
+  'yaml'
+]
+
+if os.name == 'nt':
+  scripts.append('flask/bin/floyd.bat')
+
+def get_file_contents(file_path, curfile=__file__ or ''):
+  """Get the context of the file using full path name"""
+  try:
+    full_path = os.path.join(os.path.realpath(os.path.dirname(curfile)), 
+                             file_path)
+    return open(full_path, 'r').read()
+  except (IOError, NameError, TypeError):
+    return ""
+
+setup(
+  name = 'floyd',
+  description = floyd.__doc__.split('\n\n')[0],
+  long_description = get_file_contents('README.md'),
+  keywords = 'website, appengine, s3, cms, blog',
+  url = floyd.__url__,
+  platforms = ['linux', 'osx', 'win32'],
+  version = floyd.get_version(),
+  author = floyd.__author__,
+  author_email = floyd.__email__,
+  license = get_file_contents('LICENSE'),
+  install_requires = packages,
+  packages = ['floyd'],
+  scripts = scripts,
+)
