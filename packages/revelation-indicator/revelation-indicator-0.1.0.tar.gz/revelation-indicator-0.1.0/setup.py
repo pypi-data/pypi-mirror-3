113fba1395dab2c7e8407519cf05e8d4e5b05b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:33 +0000

    Add package revelation-indicator/0.1.0/revelation-indicator-0.1.0.tar.gz

---
+++ packages/revelation-indicator/revelation-indicator-0.1.0.tar.gz/revelation-indicator-0.1.0/setup.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# revelation-indicator
+# Copyright (C) 2012 Sebastian Vetter
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+
+import os
+import sys
+import pwd
+import glob
+import subprocess
+
+from distutils.core import setup
+from distutils.command.install_data import install_data
+
+from DistUtilsExtra.command import *
+
+
+class post_install(install_data):
+
+    def run(self):
+        # Call parent
+        install_data.run(self)
+
+        if pwd.getpwuid(os.getuid()).pw_name == 'root':
+            # Execute commands
+            config_source = subprocess.check_output([
+                'gconftool-2',
+                '--get-default-source'
+            ]).strip()
+            for schema_file in [os.path.basename(f) for f in schema_files]:
+                cmd = ' '.join([
+                    "GCONF_CONFIG_SOURCE=%s" % config_source,
+                    "gconftool-2",
+                    "--makefile-install-rule",
+                    sys.prefix+'/share/gconf/schemas/'+schema_file
+                ])
+                output = subprocess.check_output(cmd, shell=True)
+
+
+schema_files = glob.glob('data/gconf/*.schemas')
+
+setup(
+    name = 'revelation-indicator',
+    version = '0.1.0',
+    author = 'Sebastian Vetter',
+    author_email = 'sebastian@roadside-developer.com',
+    url = 'https://github.com/elbaschid/revelation-indicator',
+
+    description = 'Indicator for the revelation password manager on Unity desktop.',
+    long_description = open('README.rst').read(),
+    license = 'GNU General Public License (GPL)',
+
+    scripts = ['bin/revelation-indicator'],
+    packages = ['revelation_indicator'],
+    provides = [],
+
+    data_files = [
+        (sys.prefix+'/share/gconf/schemas', schema_files),
+        (sys.prefix+'/share/applications', glob.glob('data/applications/*.desktop')),
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Environment :: X11 Applications :: GTK',
+        'Environment :: X11 Applications :: Gnome',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Desktop Environment :: Gnome',
+    ],
+    cmdclass = {
+        "build" : build_extra.build_extra,
+        "install_data": post_install,
+        "build_i18n" :  build_i18n.build_i18n,
+        "build_help" :  build_help.build_help,
+        "build_icons" :  build_icons.build_icons
+    },
+)
