678044df110721419120b3825363edb7d3e256c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:06 +0000

    Add package cssselect/0.5/cssselect-0.5.tar.gz

---
+++ packages/cssselect/cssselect-0.5.tar.gz/cssselect-0.5/cssselect/__init__.py
@@ -0,0 +1,21 @@
+"""
+    CSS Selectors based on XPath
+    ============================
+
+    This module supports selecting XML/HTML elements based on CSS selectors.
+    See the `CSSSelector` class for details.
+
+
+    :copyright: (c) 2007-2012 Ian Bicking and contributors.
+                See AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+
+"""
+
+from cssselect.parser import (parse, Selector, SelectorError,
+                              SelectorSyntaxError)
+from cssselect.xpath import GenericTranslator, HTMLTranslator, ExpressionError
+
+
+VERSION = '0.5'
+__version__ = VERSION
