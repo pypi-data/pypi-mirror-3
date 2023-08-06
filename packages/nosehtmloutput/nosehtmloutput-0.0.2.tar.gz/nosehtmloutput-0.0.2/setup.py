fc3d9e5d2ebee0f507af666a1f5f2357ab7bc0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:49 +0000

    Add package nosehtmloutput/0.0.2/nosehtmloutput-0.0.2.tar.gz

---
+++ packages/nosehtmloutput/nosehtmloutput-0.0.2.tar.gz/nosehtmloutput-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import htmloutput.version
+import setuptools
+
+setuptools.setup(
+    name="nosehtmloutput",
+    version=htmloutput.version.__version__,
+    author='Hewlett-Packard Development Company, L.P.',
+    description="Nose plugin to produce test results in html.",
+    license="Apache License, Version 2.0",
+    url="https://github.com/cboylan/nose-html-output",
+    packages=["htmloutput"],
+    setup_requires=['nose'],
+    install_requires=['nose'],
+    classifiers=[
+        "Environment :: Console",
+        "Topic :: Software Development :: Testing",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python"
+    ],
+    entry_points = {
+        'nose.plugins.0.10': [
+            'html-output = htmloutput.htmloutput:HtmlOutput'
+        ]
+    }
+)
