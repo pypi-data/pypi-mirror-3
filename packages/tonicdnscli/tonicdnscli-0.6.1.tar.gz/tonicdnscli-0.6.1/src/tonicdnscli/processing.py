d0b61e5f709e7a3b8c2e10e0ef201899b5fa23c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:10 +0000

    Add package tonicdnscli/0.6.1/tonicdnscli-0.6.1.tar.gz

---
+++ packages/tonicdnscli/tonicdnscli-0.6.1.tar.gz/tonicdnscli-0.6.1/src/tonicdnscli/processing.py
@@ -0,0 +1,350 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2012 Kouhei Maeda <mkouhei@palmtb.net>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+def unprovide():
+    print("ERROR: This feature does not provide")
+    exit(10)
+
+
+def tonicDNSClient(uri, method, token, data, keyword='', domain=''):
+    import sys
+    import json
+    if sys.version_info > (2, 6) and sys.version_info < (2, 8):
+        import urllib2 as urllib
+    elif sys.version_info > (3, 0):
+        import urllib.request as urllib
+
+    encoded = json.JSONEncoder(object).encode(data)
+    obj = urllib.build_opener(urllib.HTTPHandler)
+    req = urllib.Request(uri, data=encoded.encode('utf-8'))
+    req.add_header('x-authentication-token', token)
+
+    # When encoded(=data) is False, retrieve data as GET method.
+    if encoded:
+        req.add_header('Content-Type', 'application/json')
+
+    req.get_method = lambda: method
+
+    try:
+        res = obj.open(req)
+
+    except urllib.HTTPError as e:
+        sys.stderr.write("ERROR: %s\n" % e)
+        exit(1)
+
+    # response body
+    if method == 'GET':
+
+        data = res.read()
+        datas = json.loads(data.decode('utf-8'))
+
+        # filtering with keyword
+        if keyword == 'serial':
+            from converter import JSONConvert
+            record = searchRecord(datas, 'SOA')[0]
+
+            # if SOA record, remove priority unnecessary
+            del record['priority']
+
+            # override ttl
+            record['ttl'] = int(record['ttl'])
+
+            c = JSONConvert(domain)
+            new_record = c.getSOA(record)
+            return record, new_record
+
+        # '--search' option of 'get' subcommand
+        elif keyword:
+            records = searchRecord(datas, keyword)
+            datas.update({"records": records})
+
+        # 'tmpl_get' subcommand
+        if uri.split('/')[3] == 'template':
+
+            # when specify template identfier
+            if len(uri.split('/')) == 5:
+                formattedPrint(datas)
+
+            # when get all templates
+            else:
+                for data in datas:
+                    formattedPrint(data)
+
+        # 'get' subcommand
+        else:
+            formattedPrint(datas)
+
+    # response non JSON data
+    else:
+        data = res.read()
+        print(data)
+
+
+def formattedPrint(datas):
+    import sys
+    if sys.version_info > (2, 6) and sys.version_info < (2, 8):
+        import utils2 as utils
+    elif sys.version_info > (3, 0):
+        import utils3 as utils
+
+    if not datas:
+        print("No data")
+        exit(1)
+
+    if datas.get('records'):
+        print("domain: %(name)s" % datas)
+
+        if datas.get('type') == 'MASTER':
+            print("serial: %(notified_serial)s" % datas)
+
+        print("DNS   : %(type)s" % datas)
+
+        # header
+        hr()
+        print('%-33s %-5s %-25s %-5s %-3s'
+              % ('name', 'type', 'content', 'ttl', 'prio'))
+        hr()
+
+        for record in datas.get('records'):
+
+            # print 'NAME'
+            utils.print_inline("%(name)-33s" % record)
+
+            # print 'TYPE' of SOA record
+            if record.get('type') == 'SOA':
+                print("%(type)-5s" % record)
+
+            # print 'TYPE' of non SOA record
+            else:
+                utils.print_inline("%(type)-5s" % record)
+
+            # print 'CONTENT' of non SOA
+            if record.get('type') == 'SOA':
+                utils.print_inline(">\t\t%(content)-25s " % record)
+
+            # print 'CONTENT' of SOA record
+            else:
+                utils.print_inline("%(content)-25s" % record)
+
+            # print TTL, and PRIORITY for MX, SRV record
+            if record.get('priority'):
+                utils.print_inline("%(ttl)5s" % record)
+                print("%(priority)2s" % record)
+
+            # print ttl for non SOA record
+            else:
+                print("%(ttl)5s " % record)
+
+        hr()
+
+    # for template
+    elif datas.get('identifier'):
+        print("identifier : %(identifier)s" % datas)
+        print("description: %(description)s" % datas)
+        hr()
+        print('%-33s %-5s %-25s %-5s %-3s'
+              % ('name', 'type', 'content', 'ttl', 'prio'))
+
+        for record in datas.get('entries'):
+
+            # print 'NAME'
+            utils.print_inline("%(name)-33s" % record)
+
+            # print 'TYPE' for SOA
+            if record.get('type') == 'SOA':
+                print("%(type)-5s" % record)
+
+            # print 'TYPE' for non SOA
+            else:
+                utils.print_inline("%(type)-5s" % record)
+
+            # print 'CONTENT' for SOA
+            if record.get('type') == 'SOA':
+                utils.print_inline("> %(content)-25s " % record)
+
+            # print 'CONTENT' for non SOA
+            else:
+                utils.print_inline("%(content)-24s" % record)
+
+            # print 'TTL', and 'PRIORITY'
+            if record.get('priority') != None:
+                utils.print_inline("%(ttl)5s" % record)
+                print("%(priority)2s" % record)
+
+            # print
+            else:
+                print("%(ttl)5s " % record)
+        hr()
+    else:
+        print("No match records")
+
+
+# print horizontal line
+def hr():
+    print('=' * 78)
+
+
+# `data' is list.
+# item of list is dictionary as
+# 1) {"records": records}
+# 2) {"name": domain, "records": records}
+#This is work around, see also commit 7571109.
+def createZoneRecords(server, token, domain, data, identifier):
+    # ContentType: application/json
+    # x-authentication-token: token
+    method = 'PUT'
+    uri = 'https://' + server + '/zone'
+
+    for i, v in enumerate(data):
+
+        if i == 0:
+            from converter import JSONConvert
+            obj = JSONConvert(domain)
+            zone = obj.generateZone(domain, identifier, v)
+            tonicDNSClient(uri, method, token, zone)
+
+        else:
+            # method: PUT
+            uri = 'https://' + server + '/zone/' + domain
+            tonicDNSClient(uri, method, token, v)
+
+
+def createRecords(server, token, domain, data):
+    # ContentType: application/json
+    # x-authentication-token: token
+    method = 'PUT'
+    uri = 'https://' + server + '/zone/' + domain
+    for i in data:
+        tonicDNSClient(uri, method, token, i)
+
+
+def deleteRecords(server, token, data):
+    # ContentType: application/json
+    # x-authentication-token: token
+    method = 'DELETE'
+    uri = 'https://' + server + '/zone'
+    for i in data:
+        tonicDNSClient(uri, method, token, i)
+
+
+def getZone(server, token, domain, keyword=''):
+    # x-authentication-token: token
+    method = 'GET'
+    uri = 'https://' + server + '/zone/' + domain
+    tonicDNSClient(uri, method, token, data=False, keyword=keyword)
+
+
+def getAllZone(server, token):
+    # x-authentication-token: token
+    method = 'GET'
+    uri = 'https://' + server + '/zone'
+    tonicDNSClient(uri, method, token, data=False)
+
+
+def deleteDomain():
+    # x-authentication-token: token
+    # method: DELETE
+    # uri: /zone/:domain
+    unprovide()
+
+
+def createTemplate(server, token, identifier, template):
+    # ContentType: application/json
+    # x-authentication-token: token
+    method = 'PUT'
+    uri = 'https://' + server + '/template/' + identifier
+    tonicDNSClient(uri, method, token, data=template)
+
+
+def updateTemplate(server, token, identifier, template):
+    # ContentType: application/json
+    # x-authentication-token: token
+    method = 'POST'
+    uri = 'https://' + server + '/template/' + identifier
+    tonicDNSClient(uri, method, token, data=template)
+
+
+def deleteTemplate(server, token, template):
+    # x-authentication-token: token
+    method = 'DELETE'
+    uri = 'https://' + server + '/template/' + template
+    tonicDNSClient(uri, method, token, data=False)
+
+
+def getTemplate(server, token, template):
+    # x-authentication-token: token
+    method = 'GET'
+    uri = 'https://' + server + '/template/' + template
+    tonicDNSClient(uri, method, token, data=False)
+
+
+def getAllTemplates(server, token):
+    # x-authentication-token: token
+    method = 'GET'
+    uri = 'https://' + server + '/template'
+    tonicDNSClient(uri, method, token, data=False)
+
+
+def updateSerial(server, token, domain):
+    # x-authentication-token: token
+
+    # Get SOA record
+    # `cur_soa` is current SOA record.
+    # `new_soa` is incremental serial SOA record.
+    method = 'GET'
+    uri = 'https://' + server + '/zone/' + domain
+    cur_soa, new_soa = tonicDNSClient(uri, method, token, data=False,
+                                      keyword='serial', domain=domain)
+    # set JSON
+    from converter import JSONConvert
+    cur_o = JSONConvert(domain)
+    new_o = JSONConvert(domain)
+    cur_o.records = [cur_soa]
+    new_o.records = [new_soa]
+    cur_o.genData(False)
+    new_o.genData(True)
+
+    # Create new SOA record
+    uri = 'https://' + server + '/zone/' + domain
+    method = 'PUT'
+    tonicDNSClient(uri, method, token, new_o.dict_records[0])
+
+    # Delete current SOA record why zone has only one SOA record.
+    method = 'DELETE'
+    uri = 'https://' + server + '/zone'
+    tonicDNSClient(uri, method, token, cur_o.dict_records[0])
+
+
+def searchRecord(datas, keyword):
+    # search target JSON -> dictionary
+    # key target is "name" or "content"
+    # type is "type", default null
+    # either key and type, or on the other hand
+    # data is dictionaly
+    result = []
+
+    for record in datas['records']:
+
+        if record['name'].find(keyword) >= 0 or \
+                record['content'].find(keyword) >= 0 or \
+                record['type'] == keyword:
+            result.append(record)
+
+    return result
