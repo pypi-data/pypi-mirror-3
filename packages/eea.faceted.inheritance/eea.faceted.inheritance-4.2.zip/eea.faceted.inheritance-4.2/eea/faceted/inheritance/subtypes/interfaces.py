7aaaa419b86523bfa2b7728eca71d1fa74954b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:34 +0000

    Add package eea.faceted.inheritance/4.2/eea.faceted.inheritance-4.2.zip

---
+++ packages/eea.faceted.inheritance/eea.faceted.inheritance-4.2.zip/eea.faceted.inheritance-4.2/eea/faceted/inheritance/subtypes/interfaces.py
@@ -0,0 +1,27 @@
+""" Subtypes interfaces
+"""
+from zope import schema
+from zope.interface import Interface, alsoProvides
+from zope.app.content.interfaces import IContentType
+from eea.facetednavigation.interfaces import IFacetedNavigable
+from eea.facetednavigation.subtypes.interfaces import IFacetedSubtyper
+
+class IPossibleFacetedHeritor(Interface):
+    """
+    A possible heritor that can inherit faceted configuration from a
+    faceted navigable object.
+    """
+
+class IFacetedHeritor(IFacetedNavigable):
+    """
+    A heritor that inherit faceted configuration from a
+    faceted navigable object.
+    """
+
+class IFacetedHeritorSubtyper(IFacetedSubtyper):
+    """ Extends IFacetedSubtyper functionallity
+    """
+    can_enable_heritor = schema.Bool(u'Can enable faceted inheritance',
+                                     readonly=True)
+
+alsoProvides(IFacetedHeritor, IContentType)
