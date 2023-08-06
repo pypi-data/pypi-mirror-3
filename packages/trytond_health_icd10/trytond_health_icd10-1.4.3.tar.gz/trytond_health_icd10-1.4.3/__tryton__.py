703becf27e49120c3ab7e1d2164bc148fde8a71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:50 +0000

    Add package trytond_health_icd10/1.4.3/trytond_health_icd10-1.4.3.tar.gz

---
+++ packages/trytond_health_icd10/trytond_health_icd10-1.4.3.tar.gz/trytond_health_icd10-1.4.3/__tryton__.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+{
+
+	'name' : 'GNU Health: International Classification of Diseases .- ICD-10',  
+    'version': '1.4.3',
+	'author' : 'GNU Solidario',
+	"website" : "http://health.gnu.org",
+	'email' : 'health@gnusolidario.org',
+	'depends' : ['health'],
+    'name_es_ES': 'GNU Health : Clasificación Internacional de Enfermedades .- CIE-10',
+	'description' : '''
+World Health Organization - International Classification of Diseases for GNU HEALTH - 10th revision
+''',
+
+	'description_es_ES' : '''
+Organización Mundial de la Salud - Clasificación Internacional de Enfermedades - Revisión 10' 
+''',
+	"xml" : ["data/disease_categories.xml","data/diseases.xml"],
+
+	"active": False 
+}
