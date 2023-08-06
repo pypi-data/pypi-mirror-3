93a2db9b3f0071eefda412eb8ae2e90aa159092b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:18 +0000

    Add package django-badbrowser/1.0.7/django-badbrowser-1.0.7.tar.gz

---
+++ packages/django-badbrowser/django-badbrowser-1.0.7.tar.gz/django-badbrowser-1.0.7/django_badbrowser/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import *
+
+from django_badbrowser.views import ignore, unsupported
+
+urlpatterns = patterns("",
+    url(r"^$", unsupported, name="django-badbrowser-unsupported"),
+    url(r"^ignore/$", ignore, name="django-badbrowser-ignore"),
+)
---
+++ packages/django-badbrowser/django-badbrowser-1.0.7.tar.gz/django-badbrowser-1.0.7/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import os
+
+from django_badbrowser import __version__
+
+long_description = open(os.path.join(os.path.dirname(__file__), "README.textile")).read()
+
+setup(
+	name="django-badbrowser",
+	version=__version__,
+	url="http://github.com/adamcharnock/django-badbrowser",
+	download_url="git@github.com:adamcharnock/django-badbrowser.git",
+	description="Browser detection (including browser upgrade notices) for Django",
+	long_description=long_description,
+	author="Adam Charnock",
+	author_email="adam@playnice.ly",
+	platforms=["any"],
+	license="MIT",
+	packages=[
+		"django_badbrowser",
+	],
+	package_dir={"django_badbrowser": "django_badbrowser"},
+	package_data={"django_badbrowser": ["templates/django_badbrowser/*", "uastrings/*", "static/django-badbrowser/*"]},
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"License :: OSI Approved :: MIT License",
+		"Environment :: Web Environment",
+		"Framework :: Django",
+		"Intended Audience :: Developers",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Topic :: Software Development :: Libraries :: Application Frameworks",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Internet :: WWW/HTTP :: Browsers",
+	],
+	install_requires=[
+		"httpagentparser",
+	],
+)
