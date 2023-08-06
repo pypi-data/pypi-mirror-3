55dd8a346608f331c080469a6d78aad5299436fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:11 +0000

    Add package pyelliptic/1.1/pyelliptic-1.1.tar.gz

---
+++ packages/pyelliptic/pyelliptic-1.1.tar.gz/pyelliptic-1.1/pyelliptic/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2010
+# Author: Yann GUIBET
+# Contact: <yannguibet@gmail.com>
+
+__version__ = '1.1'
+
+__all__ = [
+    'openssl',
+    'ecc',
+    'cipher',
+    'hmac',
+    ]
+
+from pyelliptic.openssl import openssl
+from pyelliptic.ecc import ecc
+from pyelliptic.cipher import cipher
+from pyelliptic.hmac import hmac
