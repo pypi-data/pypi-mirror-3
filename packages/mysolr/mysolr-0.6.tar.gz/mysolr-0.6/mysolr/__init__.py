f407477d192c3d9fef47eb719e0bf04e8b32e350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:29 +0000

    Add package mysolr/0.6/mysolr-0.6.tar.gz

---
+++ packages/mysolr/mysolr-0.6.tar.gz/mysolr-0.6/mysolr/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .mysolr_response import SolrResponse
+from .mysolr import Solr
+from .mysolr import Cursor
+
+__title__ = 'mysolr'
+__version__ = '0.6'
+__build__ = 0x000060
+__author__ = 'Rubén Abad, Miguel Olivares'
---
+++ packages/mysolr/mysolr-0.6.tar.gz/mysolr-0.6/mysolr/compat.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+    mysolr.compat
+    ~~~~~~~~~~~~~~
+
+    Resolve compatibility between python 2.X and 3.X
+
+"""
+
+import sys
+
+if sys.version_info.major == 3:
+    from urllib.parse import urljoin
+elif sys.version_info.major == 2:
+    from urlparse import urljoin
+
+def get_wt():
+    if sys.version_info.major == 3 and sys.version_info.minor == 2:
+        return 'json'
+    else:
+        return 'python'
+
+def parse_response(content):
+    if sys.version_info.major == 3 and sys.version_info.minor == 2:
+        import json
+        return json.loads(content.decode('utf-8'))
+    else:
+        return eval(content)
