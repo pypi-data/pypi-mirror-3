a48ddf1e11ed11e7624df205701d35b492524a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:18 +0000

    Add package django-pyroven/0.9/django-pyroven-0.9.tar.gz

---
+++ packages/django-pyroven/django-pyroven-0.9.tar.gz/django-pyroven-0.9/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Copyright 2011 Andrew Ryrie (amr66)
+
+from distutils.core import setup
+
+setup(name='django-pyroven',
+      description='A Django authentication backend for Ucam-WebAuth / Raven',
+#     No long_description due to http://bugs.python.org/issue13614
+#     long_description=open('README.md').read(),
+      url='https://github.com/pyroven/django-pyroven',
+      version='0.9',
+      license='MIT',
+      author='Andrew Ryrie',
+      author_email='smb314159@gmail.com',
+      maintainer='Kristian Glass',
+      maintainer_email='pyroven@doismellburning.co.uk',
+      packages=['pyroven'],
+      )
