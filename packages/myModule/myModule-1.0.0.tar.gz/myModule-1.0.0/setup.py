2644c022d221caede83797c239d80e93dc04a6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:30 +0000

    Add package myModule/1.0.0/myModule-1.0.0.tar.gz

---
+++ packages/myModule/myModule-1.0.0.tar.gz/myModule-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'myModule',
+    version     = '1.0.0',
+    py_modules  = ['myModule'],
+    author      = 'Rinchen',
+    author_email = 'rinchenn@gmail.com'
+#    url         = 'http://www.test.com',
+##    description  = 'A simple printer of nested lists'
+    )
