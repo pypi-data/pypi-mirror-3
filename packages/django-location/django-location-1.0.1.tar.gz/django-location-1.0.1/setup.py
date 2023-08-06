a68b1c8b51802304e5a14aefb6d64dea1f489601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:12 +0000

    Add package django-location/1.0.1/django-location-1.0.1.tar.gz

---
+++ packages/django-location/django-location-1.0.1.tar.gz/django-location-1.0.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+setup(
+    name='django-location',
+    version='1.0.1',
+    url='http://bitbucket.org/latestrevision/django-location/',
+    description='Gather, store, and display real-time location information from Foursquare, Google Latitude, and more.',
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    include_package_data=True,
+    extras_require={
+            'locationdetails': [
+                'django-neighborhoods',
+                'django-census-places',
+            ],
+            'runmeter': [
+                'django-mailbox',
+            ]
+        },
+    install_requires=[
+        'django>=1.4',
+        'django-social-auth',
+        'pytz',
+        ],
+    packages=[
+        'location',
+        'location.management',
+        'location.management.commands',
+        'location.migrations',
+        'location.templatetags',
+        ],
+)
