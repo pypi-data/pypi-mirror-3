653ec9bb2e39f833a0842b984c4d8bbb51ec4676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:48 +0000

    Add package mailsnake/1.3.3/mailsnake-1.3.3.tar.gz

---
+++ packages/mailsnake/mailsnake-1.3.3.tar.gz/mailsnake-1.3.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import mailsnake
+
+setup(
+    name='mailsnake',
+    version=mailsnake.__version__,
+    description='MailChimp API v1.3 wrapper for Python.',
+    author=mailsnake.__author__,
+    url='https://github.com/michaelhelmick/python-mailsnake',
+    packages=find_packages(),
+    download_url='http://pypi.python.org/pypi/mailsnake/',
+    keywords='mailsnake mailchimp api wrapper 1.3',
+    zip_safe=True,
+    install_requires=['simplejson'],
+    py_modules=['mailsnake'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
