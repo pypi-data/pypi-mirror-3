dbbc1cc7dea358e125f70f1bfab49b11de44c67c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:47 +0000

    Add package Zachs-nester/1.2.0/Zachs%20nester-1.2.0.tar.gz

---
+++ packages/Zachs-nester/Zachs%20nester-1.2.0.tar.gz/Zachs nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "Zachs nester",
+    version = '1.2.0',
+    py_modules = ['nester'],
+    author = 'zach',
+    author_email = 'mutable@gmail.com',
+    url = 'kingtutor.com',
+    description = 'nested list printer HFP ch2',
+    )
