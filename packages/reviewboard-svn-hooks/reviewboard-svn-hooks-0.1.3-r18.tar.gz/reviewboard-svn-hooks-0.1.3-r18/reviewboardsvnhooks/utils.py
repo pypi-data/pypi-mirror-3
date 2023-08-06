ad540c8d4daa9441a102940c43c80347fafebfe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:03 +0000

    Add package reviewboard-svn-hooks/0.1.3-r18/reviewboard-svn-hooks-0.1.3-r18.tar.gz

---
+++ packages/reviewboard-svn-hooks/reviewboard-svn-hooks-0.1.3-r18.tar.gz/reviewboard-svn-hooks-0.1.3-r18/reviewboardsvnhooks/utils.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*-
+import os
+import sys
+import subprocess
+import urllib2
+import cookielib
+import base64
+import re
+from urlparse import urljoin
+import shlex
+import shelve
+
+import ConfigParser
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+
+def get_cmd_output_old(cmd):
+    p = subprocess.Popen(cmd,
+#           stdin = subprocess.PIPE,
+            stdin = None,
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE)
+#   p.stdin.close()
+    return p.communicate()[0]
+
+def get_cmd_output(cmd):
+    return os.popen(' '.join(cmd)).read()
+
+
+def split(s):
+#    return set([i.strip() for i in shlex.split(s, posix = False) if i.strip()])
+    return set([i.strip() for i in s.split(',') if i.strip()])
+
+#print get_cmd_output(["python", "-c", "print 32"])
+
+#print split('a a,g')
+#print split('reviewboard, FindDupString')
+
+#print get_cmd_output('svn --help')
+
