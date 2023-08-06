04be56650c34ce153d234d7c31b3f3c692850ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:48 +0000

    Add package django-pipeline/1.2.8/django-pipeline-1.2.8.tar.gz

---
+++ packages/django-pipeline/django-pipeline-1.2.8.tar.gz/django-pipeline-1.2.8/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+readme = open('README.rst').read()
+license = open('LICENSE').read()
+
+setup(
+    name='django-pipeline',
+    version='1.2.8',
+    description='Pipeline is an asset packaging library for Django.',
+    long_description=readme,
+    author='Timothée Peignier',
+    author_email='timothee.peignier@tryphon.org',
+    url='https://github.com/cyberdelia/django-pipeline',
+    license=license,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ]
+)
