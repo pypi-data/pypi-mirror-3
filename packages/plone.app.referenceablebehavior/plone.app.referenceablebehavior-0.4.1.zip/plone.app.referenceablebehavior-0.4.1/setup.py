9cfbfcaa460d99e9d455b2fbd459f50130834137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:23 +0000

    Add package plone.app.referenceablebehavior/0.4.1/plone.app.referenceablebehavior-0.4.1.zip

---
+++ packages/plone.app.referenceablebehavior/plone.app.referenceablebehavior-0.4.1.zip/plone.app.referenceablebehavior-0.4.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.4.1'
+
+setup(name='plone.app.referenceablebehavior',
+      version=version,
+      description="Referenceable dexterity type behavior",
+      long_description=open("README.txt").read() + "\n\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='dexterity referenceable plone',
+      author='Plone Foundation',
+      author_email='mailto:dexterity-development@googlegroups.com',
+      url='http://plone.org/products/dexterity',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.behavior',
+          'plone.dexterity',
+          'plone.directives.form',
+          'plone.indexer',
+          'plone.uuid',
+          'Products.Archetypes',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
