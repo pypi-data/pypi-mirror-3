072ac4f14abe279b3a120bcd114aef53f2dc3d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:37 +0000

    Add package kiss.py/0.4.5/kiss.py-0.4.5.tar.gz

---
+++ packages/kiss.py/kiss.py-0.4.5.tar.gz/kiss.py-0.4.5/kiss/core/events.py
@@ -0,0 +1,19 @@
+from pev import Eventer
+
+ApplicationStarted = 0
+"""
+Event when application is started	
+"""
+
+ApplicationStopped = 1
+"""
+Event when application is stopped
+"""
+
+BeforeDatabaseEngineConfiguration = 2
+AfterDatabaseEngineConfiguration = 3
+BeforeInitStatic = 4
+AfterInitStatic = 5
+BeforeApplicationStarted = 6
+BeforeInitServer = 7
+AfterInitServer = 8
