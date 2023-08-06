fcae84433a0414d5ec05533404e266ba255d7e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:46 +0000

    Add package ecm/2.0.1/ecm-2.0.1.tar.gz

---
+++ packages/ecm/ecm-2.0.1.tar.gz/ecm-2.0.1/ecm/admin/cmd/upgrade.py
@@ -0,0 +1,152 @@
+# Copyright (c) 2010-2012 Robin Jarry
+#
+# This file is part of EVE Corporation Management.
+#
+# EVE Corporation Management is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# EVE Corporation Management is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# EVE Corporation Management. If not, see <http://www.gnu.org/licenses/>.
+
+
+from __future__ import with_statement
+
+
+__date__ = '2012 3 23'
+__author__ = 'diabeteman'
+
+import os
+import sys
+import shutil
+from os import path
+from optparse import OptionParser
+from ConfigParser import SafeConfigParser
+
+from django.core import management
+
+from ecm.lib.subcommand import Subcommand
+from ecm.admin import instance_template
+from ecm.admin.util import run_python_cmd, log
+from ecm.admin.cmd import collect_static_files, download_patched_eve_db, patch_ccp_dump,\
+    PATCHED_EVE_DB_URL, CCP_EVE_DB_URL
+
+#-------------------------------------------------------------------------------
+def sub_command():
+    # UPGRADE
+    upgrade_cmd = Subcommand('upgrade',
+                             parser=OptionParser(usage='%prog [OPTIONS] instance_dir'),
+                             help='Synchronize an instance\'s database and files.',
+                             callback=run)
+    upgrade_cmd.parser.add_option('--no-syncdb',
+                                  dest='no_syncdb', action='store_true', default=False,
+                                  help='Do not modify the instance\'s database.')
+    upgrade_cmd.parser.add_option('-u', '--upgrade-from-1.4.9',
+                                  dest='upgrade_from_149', action='store_true', default=False,
+                                  help='Upgrade from ECM-1.4.9.')
+    upgrade_cmd.parser.add_option('--eve-db-url',
+                                  dest='eve_db_url', default=PATCHED_EVE_DB_URL,
+                                  help='URL where to download EVE database archive.')
+    upgrade_cmd.parser.add_option('--eve-db-zip-archive',
+                                  dest='eve_zip_archive',
+                                  help='Local path to EVE database archive (skips download).')
+    upgrade_cmd.parser.add_option('--skip-eve-db-download',
+                                  dest='skip_eve_db_download', action='store_true',
+                                  help='Do NOT download EVE db (use with care).')
+    upgrade_cmd.parser.add_option('--from-ccp-dump',
+                                  dest='from_ccp_dump', action='store_true', default=False,
+                                  help='Update EVE database from CCP official dump (can take a long time).')
+    upgrade_cmd.parser.add_option('--ccp-dump-url',
+                                  dest='ccp_dump_url', default=CCP_EVE_DB_URL,
+                                  help='URL where to download CCP official dump.')
+    upgrade_cmd.parser.add_option('--ccp-dump-archive',
+                                  dest='ccp_dump_archive',
+                                  help='Local archive of CCP official dump (skips download).')
+    if not os.name == 'nt':
+        upgrade_cmd.parser.add_option('-s', '--symlink-files', dest='symlink_files',
+                                      help='Create symbolic links instead of copying static files.',
+                                      default=False, action='store_true')
+
+    return upgrade_cmd
+
+#-------------------------------------------------------------------------------
+def migrate_ecm_db(instance_dir, upgrade_from_149=False):
+    log("Migrating database...")
+    run_python_cmd('manage.py syncdb --noinput', instance_dir)
+
+    if upgrade_from_149:
+        instance_dir = path.abspath(instance_dir)
+        sys.path.insert(0, instance_dir)
+    
+        import settings #@UnresolvedImport
+        management.setup_environ(settings)
+        
+        from south.models import MigrationHistory
+
+        log('Migrating from ECM 1.4.9...')
+        # we are upgrading from ECM 1.X.Y, we must perform the init migration
+        # on the 'hr' app (rename tables from 'roles_xxxxx' to 'hr_xxxxx')
+        MigrationHistory.objects.all().delete() #@UndefinedVariable
+        run_python_cmd('manage.py migrate hr 0001 --noinput', instance_dir)
+        # we MUST "fake" the first migration for 1.4.9 apps
+        # otherwise the migrate command will fail because DB tables already exist...
+        for app in ('common', 'scheduler', 'corp', 'assets', 'accounting'):
+            run_python_cmd('manage.py migrate %s 0001 --fake --noinput' % app, instance_dir)
+
+        from ecm.apps.scheduler.models import ScheduledTask
+        ScheduledTask.objects.all().delete()
+        from ecm.apps.common.models import UrlPermission
+        UrlPermission.objects.all().delete()
+    
+        del settings
+        del sys.modules['settings']
+        sys.path.remove(instance_dir)
+
+    run_python_cmd('manage.py migrate --all --noinput', instance_dir)
+
+    log('Database Migration successful.')
+
+#-------------------------------------------------------------------------------
+def run(command, global_options, options, args):
+    if not args:
+        command.parser.error('Missing instance directory.')
+    instance_dir = args[0]
+    sqlite_db_dir = ''
+    config = SafeConfigParser()
+    if config.read([path.join(instance_dir, 'settings.ini')]):
+        sqlite_db_dir = config.get('database', 'sqlite_db_dir')
+    if not sqlite_db_dir:
+        sqlite_db_dir = path.join(instance_dir, 'db')
+    
+    # copy settings.py & manage.py from template
+    template_dir = path.abspath(path.dirname(instance_template.__file__))
+    shutil.copy(path.join(template_dir, 'settings.py'), instance_dir)
+    shutil.copy(path.join(template_dir, 'manage.py'), instance_dir)
+    if hasattr(os, 'chmod'):
+        os.chmod(path.join(instance_dir, 'manage.py'), 00755)
+    
+    # run collectstatic
+    collect_static_files(instance_dir, options)
+
+    # migrate ecm db
+    if not options.no_syncdb:
+        migrate_ecm_db(instance_dir, options.upgrade_from_149)
+
+    # download eve db
+    if not options.skip_eve_db_download:
+        if options.from_ccp_dump:
+            patch_ccp_dump(ccp_dump_url=options.ccp_dump_url,
+                           ccp_dump_archive=options.ccp_dump_archive,
+                           eve_db_dir=sqlite_db_dir)
+        else:
+            download_patched_eve_db(eve_db_url=options.eve_db_url,
+                                    eve_zip_archive=options.eve_zip_archive,
+                                    eve_db_dir=sqlite_db_dir)
+
+
---
+++ packages/ecm/ecm-2.0.1.tar.gz/ecm-2.0.1/ecm/plugins/accounting/tasks/contracts.py
@@ -0,0 +1,186 @@
+# Copyright (c) 2010-2012 Robin Jarry
+#
+# This file is part of EVE Corporation Management.
+#
+# EVE Corporation Management is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# EVE Corporation Management is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# EVE Corporation Management. If not, see <http://www.gnu.org/licenses/>.
+from ecm.apps.corp.models import Corp
+from ecm.lib.eveapi import Error
+
+__date__ = "2012 04 05"
+__author__ = "tash"
+
+
+import logging
+from datetime import datetime
+
+from django.db import transaction
+
+from ecm.apps.eve import api
+from ecm.apps.common.models import UpdateDate
+from ecm.utils import tools
+from ecm.plugins.accounting.models import Contract, ContractItem
+
+LOG = logging.getLogger(__name__)
+
+#------------------------------------------------------------------------------
+def update():
+    """
+    Update all contracts
+    """
+    LOG.info("fetching /corp/Contracts.xml.aspx...")
+    # Connect to EVE API
+    api_conn = api.connect()
+    LOG.debug("Fetching Contracts...")
+    contractsApi = api_conn.corp.Contracts()
+    # checkApiVersion(contractsApi._meta.version)
+
+    current_time = contractsApi._meta.currentTime
+    cached_until = contractsApi._meta.cachedUntil
+    LOG.debug("current time : %s", str(current_time))
+    LOG.debug("cached util : %s", str(cached_until))
+    LOG.debug("parsing api response...")
+    
+    process_contracts(contractsApi.contractList, api_conn)
+    UpdateDate.mark_updated(model=Contract, date=datetime.now())
+
+#------------------------------------------------------------------------------
+def process_contracts(contract_list, connection):
+    """
+    Process all contracts from the API
+    """
+    alliance_id = Corp.objects.get(pk=1).allianceID
+    LOG.debug("Fetching contracts from DB...")    
+    # Get old contracts
+    old_contracts = {}
+    for contract in Contract.objects.all():
+        old_contracts[contract] = contract
+    LOG.debug("%s contracts found in DB..." % len(old_contracts))
+    
+    # Get new contracts
+    LOG.debug("Fetching contracts from API...")   
+    new_contracts = {}
+    for entry in contract_list:
+        if entry.assigneeID != alliance_id:
+            contract = populate_contract(entry)
+            new_contracts[contract] = contract
+    LOG.debug("%s new contracts from API..." % len(new_contracts))
+    
+    # Get changes
+    removed_contracts, added_contracts = tools.diff(old_items=old_contracts, new_items=new_contracts)
+    LOG.debug("Contracts from API: %s" % len(contract_list))
+    LOG.debug("Removed contracts: %s" % len(removed_contracts))
+    LOG.debug("Added contracts: %s" % len(added_contracts))
+    
+    # Query the contract items
+    old_items = {}
+    for item in ContractItem.objects.all():
+        old_items[item] = item
+
+    # Note: We dont need to diff items, since they are contained in contracts
+    # Get new items by contractID from EVE API
+    new_items = {}
+    for contract in added_contracts:
+        if contract.type != 'Courier': # No items for courier contracts from API
+            try:
+                items_api = connection.corp.ContractItems(contractID=contract.contractID)
+            #    checkApiVersion(items_api._meta.version)
+                item_list = items_api.itemList
+                LOG.debug("%s items for contract id %s..." % (len(item_list), contract.contractID))
+                for item in item_list:
+                    new_item = populate_contract_item(item, contract)
+                    new_items[new_item] = new_item
+            except Error:
+                LOG.debug("Invalid or missing contractID: %s" % contract.contractID)
+                continue
+    # Get all contractitem ids for removed contracts
+    removed_items = []
+    for contract in removed_contracts:
+        removed_items.append(ContractItem.objects.filter(contract=contract))
+
+    LOG.debug("Writing contracts to DB...")
+    write_contracts(added_contracts, removed_contracts)
+    LOG.debug("Writing contract items to DB...")
+    write_contract_items(new_items, removed_items)
+
+#------------------------------------------------------------------------------
+@transaction.commit_on_success
+def write_contracts(new_contracts, old_contracts):
+    """
+    Write the API results
+    """
+    for contract in old_contracts:
+        old_contracts.delete()
+    LOG.info("%d old contracts removed." % len(old_contracts))
+    for contract in new_contracts:
+        contract.save()
+    LOG.info("%d new contracts added." % len(new_contracts))
+
+#------------------------------------------------------------------------------
+@transaction.commit_on_success
+def write_contract_items(new_items, old_items):
+    """
+    Write the API results for contract items to DB.
+    """
+    if len(old_items) > 0:
+        for item in old_items:
+            item.delete()
+        LOG.info("%d old contract items removed." % len(old_items))
+    for item in new_items:
+        item.save()
+    LOG.info("%d new contract items added." % len(new_items))
+
+#------------------------------------------------------------------------------
+def populate_contract(row):
+    """
+    Takes a contract row from the API resultset and creates a Contract object.
+    """
+    return Contract(contractID=row.contractID,
+                    issuerID=row.issuerID,
+                    issuerCorpID=row.issuerCorpID,
+                    assigneeID=row.assigneeID,
+                    acceptorID=row.acceptorID,
+                    startStationID=row.startStationID,
+                    endStationID=row.endStationID,
+                    type=row.type,
+                    status=row.status,
+                    title=row.title,
+                    forCorp=row.forCorp,
+                    availability=row.availability,
+                    dateIssued=row.dateIssued,
+                    dateExpired=row.dateExpired,
+                    dateAccepted=row.dateAccepted,
+                    numDays=row.numDays,
+                    dateCompleted=row.dateCompleted,
+                    price=row.price,
+                    reward=row.reward,
+                    collateral=row.collateral,
+                    buyout=row.buyout,
+                    volume=row.volume)
+
+#------------------------------------------------------------------------------
+def populate_contract_item(entry, contract):
+    """
+    Takes a contract item row from the API ResultSet and creates a ContractItem object.
+    """
+    if entry.rawQuantity:
+        raw_quantity = entry.rawQuantity
+    else:
+        raw_quantity = 0
+    return ContractItem(contract_id=contract.contractID,
+                        recordID=entry.recordID,
+                        typeID=entry.typeID,
+                        quantity=entry.quantity,
+                        rawQuantity=raw_quantity,
+                        singleton=entry.singleton,
+                        included=entry.included)
