f0d746fc25962b1734df18771d54e7c61f950fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:19 +0000

    Add package collective.externalimageeditor/1.0/collective.externalimageeditor-1.0.tar.gz

---
+++ packages/collective.externalimageeditor/collective.externalimageeditor-1.0.tar.gz/collective.externalimageeditor-1.0/README.rst
@@ -0,0 +1,56 @@
+==============================
+Introduction
+==============================
+
+.. contents::
+
+
+Description
+============
+
+This package will add a viewlet on Images with links to edit an image on external webservices.
+
+The activation of those services is controlled by settings in a specific control panel on the plone site setup.
+
+
+Currently, we support only pixlr but we plan to add fotoflexer and aviary.
+
+You can view a `Demo <http://www.youtube.com/watch?v=tCCJelVmYOE>`_.
+
+
+Credits
+========
+Companies
+---------
+|makinacom|_
+
+  * `Planet Makina Corpus <http://www.makina-corpus.org>`_
+  * `Contact us <mailto:python@makina-corpus.org>`_
+
+.. |makinacom| image:: http://depot.makina-corpus.org/public/logo.gif
+.. _makinacom:  http://www.makina-corpus.com
+
+Authors
+------------
+
+- kiorky  <kiorky@cryptelium.net>
+
+Contributors
+-----------------
+
+
+Repository
+------------
+
+- `GITHUB <https://github.com/collective/collective.externalimageeditor>`-
+
+
+TODO
+=====
+- integrate plone.protect in services url callbacks to protect image integration from CSRF attacks.
+- integrate some others services:
+
+      - fotoflexer
+      - aviary
+
+
---
+++ packages/collective.externalimageeditor/collective.externalimageeditor-1.0.tar.gz/collective.externalimageeditor-1.0/setup.py
@@ -0,0 +1,57 @@
+import os, sys
+
+from setuptools import setup, find_packages
+
+version = '1.0'
+
+def read(*rnames):
+    return open(
+        os.path.join('.', *rnames)
+    ).read()
+
+long_description = "\n\n".join(
+    [read('README.rst'),
+     read('docs', 'INSTALL.rst'),
+     read('docs', 'CHANGES.rst'),
+    ]
+)
+
+classifiers = [
+    "Programming Language :: Python",
+    "Topic :: Software Development",]
+
+name = 'collective.externalimageeditor'
+setup(
+    name=name,
+    namespace_packages=[         'collective',
+         'collective.externalimageeditor',],
+    version=version,
+    description='Addon product for plone to edit images on online webservices',
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords='plone image editor',
+    author='kiorky',
+    author_email='kiorky@cryptelium.net',
+    url='http://www.makina-corpus.com/',
+    license='GPL',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+        'z3c.autoinclude',
+        'Plone',
+        'plone.app.upgrade',
+        # with_ploneproduct_dexterity
+        'z3c.blobfile',
+        'plone.app.dexterity',
+        # -*- Extra requirements: -*-
+    ],
+    extras_require = {
+        'test': ['plone.app.testing',]
+    }, 
+    entry_points = {
+        'z3c.autoinclude.plugin': ['target = plone',],
+    },
+)
+# vim:set ft=python:
