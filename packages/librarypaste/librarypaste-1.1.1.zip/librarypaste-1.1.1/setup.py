f2542420575a1c7f5f522a94d21584a81b077e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:17 +0000

    Add package librarypaste/1.1.1/librarypaste-1.1.1.zip

---
+++ packages/librarypaste/librarypaste-1.1.1.zip/librarypaste-1.1.1/setup.py
@@ -0,0 +1,35 @@
+import sys
+
+import setuptools
+
+py26_reqs = ['argparse', 'importlib'] if sys.version_info < (2,7) else []
+
+setuptools.setup(
+    name='librarypaste',
+    use_hg_version=True,
+    author='Jamie Turner',
+    author_email='jamie@jamwt.com',
+    url='http://bitbucket.org/chmullig/librarypaste/',
+    description='Simple pastebin',
+    long_description='Simple pastebin',
+    license='MIT',
+    packages=['librarypaste'],
+    package_dir={'librarypaste': 'librarypaste'},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'librarypaste=librarypaste.librarypaste:main',
+        ],
+    },
+    install_requires=[
+        'pygments',
+        'Mako',
+        'simplejson',
+        'cherrypy',
+        'PyYAML',
+    ] + py26_reqs,
+    setup_requires=[
+        'hgtools',
+    ],
+    zip_safe=False,
+)
