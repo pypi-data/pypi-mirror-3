56a27fc6fc7b1bf1e423e8679a6f2aa909b68891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:28 +0000

    Add package BridgePython/0.2.0/BridgePython-0.2.0.tar.gz

---
+++ packages/BridgePython/BridgePython-0.2.0.tar.gz/BridgePython-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='BridgePython',
+    version='0.2.0',
+    author='',
+    author_email='team@getbridge.com',
+    packages=['BridgePython', 'BridgePython.data'],
+    url='http://pypi.python.org/pypi/BridgePython/',
+    license='LICENSE.txt',
+    description='A Python API for the Bridge service.',
+    long_description=open('README.md').read(),
+    requires=[
+        "tornado (>= 2.2)",
+    ]
+)
