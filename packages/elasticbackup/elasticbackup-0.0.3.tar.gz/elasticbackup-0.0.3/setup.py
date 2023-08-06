7ad0ae13b678c11134a3209a3b731a002f6bbc75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:40 +0000

    Add package elasticbackup/0.0.3/elasticbackup-0.0.3.tar.gz

---
+++ packages/elasticbackup/elasticbackup-0.0.3.tar.gz/elasticbackup-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'elasticbackup',
+    version = '0.0.3',
+    packages = find_packages(),
+
+    # metadata for upload to PyPI
+    author = 'Nick Stenning',
+    author_email = 'nick@whiteink.com',
+    url = 'https://github.com/nickstenning/elasticbackup',
+    description = 'elasticbackup: tools to backup and restore ElasticSearch indices',
+    license = 'MIT',
+    keywords = 'sysadmin elasticsearch backup restore',
+
+    install_requires = [
+        'requests==0.13.3',
+    ],
+
+    entry_points = {
+        'console_scripts': [
+            'elasticbackup = elasticbackup.backup:main',
+            'elasticrestore = elasticbackup.restore:main'
+        ]
+    }
+)
