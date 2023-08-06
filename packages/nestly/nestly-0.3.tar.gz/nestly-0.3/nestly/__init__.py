4e652d4592f9089b41d6735fdf897382ece48481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:12 +0000

    Add package nestly/0.3/nestly-0.3.tar.gz

---
+++ packages/nestly/nestly-0.3.tar.gz/nestly-0.3/nestly/__init__.py
@@ -0,0 +1,6 @@
+"""
+nestly is a collection of functions designed to make running software with
+combinatorial choices of parameters easier.
+"""
+
+from .core import Nest, nest_map, stripext
---
+++ packages/nestly/nestly-0.3.tar.gz/nestly-0.3/setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+
+setup(name='nestly',
+      version='0.3',
+      description="""Nestly is a collection of functions designed to make
+      running software with combinatorial choices of parameters easier.""",
+      author='Erick Matsen',
+      author_email='matsen@fhcrc.org',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'nestrun = nestly.scripts.nestrun:main',
+              'nestagg = nestly.scripts.nestagg:main',
+          ]
+      },
+      )
