02e1b506eee67ad410b11f3c5a1201de0ce72e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:04 +0000

    Add package pyspeckit/0.1.5/pyspeckit-0.1.5.tar.gz

---
+++ packages/pyspeckit/pyspeckit-0.1.5.tar.gz/pyspeckit-0.1.5/pyspeckit/wrappers/__init__.py
@@ -0,0 +1,8 @@
+"""
+A collection of wrappers
+"""
+from showspec_splat1d import splat_1d
+from fit_gaussians_to_simple_spectra import fit_gaussians_to_simple_spectra
+import fitnh3
+from cube_fit import cube_fit
+from load_IRAF_multispec import load_IRAF_multispec
