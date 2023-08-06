dd75ac3fba1088383d2dcf437cbae8b726c5323d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:51 +0000

    Add package pyteomics/1.2.4/pyteomics-1.2.4.tar.gz

---
+++ packages/pyteomics/pyteomics-1.2.4.tar.gz/pyteomics-1.2.4/tests/test_mzml.py
@@ -0,0 +1,17 @@
+import unittest
+from pyteomics.mzml import *
+
+import numpy as np
+
+class MzmlTest(unittest.TestCase):
+    def testReadSpectrum(self):
+        mz_array = np.load('test_mzml_mz.npy')
+        intensity_array = np.load('test_mzml_intensity.npy')
+        for spectrum in iter_spectrum('test.mzML'):
+            self.assertTrue(
+                all(spectrum['m/z array'] - mz_array < 1.0e-4))
+            self.assertTrue(all(np.equal(
+                spectrum['intensity array'], intensity_array)))
+
+if __name__ == '__main__':
+    unittest.main()
