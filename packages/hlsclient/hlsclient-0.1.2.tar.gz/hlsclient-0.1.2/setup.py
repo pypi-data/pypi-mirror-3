ef166be8c4a408a3f917cb2ea7b27729db7c3f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:33 +0000

    Add package hlsclient/0.1.2/hlsclient-0.1.2.tar.gz

---
+++ packages/hlsclient/hlsclient-0.1.2.tar.gz/hlsclient-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from os.path import dirname, abspath, join
+from setuptools import setup
+
+with open(abspath(join(dirname(__file__), 'README.md'))) as fileobj:
+    README = fileobj.read().strip()
+
+setup(
+    name='hlsclient',
+    description='Client to download all files from HLS streams',
+    long_description=README,
+    author='Globo.com',
+    url='https://github.com/globocom/hlsclient',
+    version='0.1.2',
+    zip_safe=False,
+    include_package_data=True,
+    packages=[
+        'hlsclient',
+        ],
+    install_requires=[
+        'requests==0.11.2',
+        'm3u8==0.1.0',
+        ],
+)
