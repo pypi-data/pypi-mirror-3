4b7c4847d04f53af22f1daefa83dbe44868ba17f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:46 +0000

    Add package rpstool/1.1/rpstool-1.1.tar.gz

---
+++ packages/rpstool/rpstool-1.1.tar.gz/rpstool-1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+with open('README.txt') as file:
+    long_description = file.read()
+
+setup(name='rpstool',
+      version='1.1',
+      description='A simple requests per second (rps) counter',
+      long_description=long_description,
+      author='OVK',
+      author_email='me@ovk.name',
+      url='https://github.com/ovkn/rpstool',
+      scripts=['rpstool'])
