aa46257a2de7b67e46e77bb66c03686125978f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:04 +0000

    Add package collective.mediaAlbum/0.3/collective.mediaAlbum-0.3-py2.6.egg

---
+++ packages/collective.mediaAlbum/collective.mediaAlbum-0.3-py2.6.egg/collective/mediaAlbum/interfaces.py
@@ -0,0 +1,12 @@
+"""Define interfaces for collective.mediaAlbum.
+"""
+
+from zope.interface import Interface
+
+class IMediaAlbumInstalled(Interface):
+    """A layer specific for this add-on product.
+
+    This interface is referred in browserlayers.xml.
+
+    All views and viewlets register against this layer will appear on your Plone site
+    only when the add-on installer has been run."""
