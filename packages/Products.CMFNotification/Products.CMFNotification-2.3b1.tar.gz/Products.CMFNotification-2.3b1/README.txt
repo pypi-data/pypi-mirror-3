80e4a294b4934fd872797a050a6e096fe199640f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:16 +0000

    Add package Products.CMFNotification/2.3b1/Products.CMFNotification-2.3b1.tar.gz

---
+++ packages/Products.CMFNotification/Products.CMFNotification-2.3b1.tar.gz/Products.CMFNotification-2.3b1/README.txt
@@ -0,0 +1,4 @@
+CMFNotification is a Plone product that allows users to be notified
+when various events occur in the portal: item creation or
+modification, workflow actions, etc.
+
---
+++ packages/Products.CMFNotification/Products.CMFNotification-2.3b1.tar.gz/Products.CMFNotification-2.3b1/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+version = '2.3b1'
+description = open('README.txt').read().strip()
+long_description = '\n\n'.join((
+        open(os.path.join(
+        'Products', 'CMFNotification', 'README.txt')).read().strip(),
+        open(os.path.join("docs", "HISTORY.txt")).read().strip()))
+
+setup(
+    name='Products.CMFNotification',
+    version=version,
+    description=description,
+    long_description=long_description,
+    classifiers=(
+        'Framework :: Plone',
+        'Programming Language :: Python',
+        'Development Status :: 5 - Production/Stable',
+        ),
+    keywords='CMFNotification plone notification e-mail',
+    author='Pilot Systems',
+    author_email='ploneorg@pilotsystems.net',
+    url='http://plone.org/products/cmfnotification',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=(
+        'setuptools',
+        'Plone >= 3.0',
+        ),
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
