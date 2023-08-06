19f8e4fd3bbfe33c12d32cd224574a47e5bc003b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:56 +0000

    Add package lingpy/1.0/lingpy-1.0-py3.2-linux-i686.egg

---
+++ packages/lingpy/lingpy-1.0-py3.2-linux-i686.egg/lingpy/output/color.py
@@ -0,0 +1,30 @@
+import colorsys
+
+def colorRange(
+        number,
+        brightness = 300,
+        ):
+    """
+    Function returns different colors for the given range.
+
+    Notes
+    -----
+
+    Idea taken from
+    http://stackoverflow.com/questions/876853/generating-color-ranges-in-python .
+    """
+
+    # get the hsv codes for the given range
+    hsv = [(x * 1.0 / number, 0.5, 0.5) for x in range(number)]
+    
+    # convert hsv to rgb
+    rgb = list([colorsys.hsv_to_rgb(*x) for x in hsv])
+    
+    # convert rgb to html
+    for i in range(number):
+        rgb[i] = tuple([int(rgb[i][k] * brightness) for k in range(3)])
+    html = []
+    for i in range(number):
+        html.append("#%02x%02x%02x" % rgb[i])
+
+    return html
