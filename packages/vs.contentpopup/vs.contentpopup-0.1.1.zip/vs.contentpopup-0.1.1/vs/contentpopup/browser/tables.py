03650c92144718163dd2aaf602646495ce9a2426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:08 +0000

    Add package vs.contentpopup/0.1.1/vs.contentpopup-0.1.1.zip

---
+++ packages/vs.contentpopup/vs.contentpopup-0.1.1.zip/vs.contentpopup-0.1.1/vs/contentpopup/browser/tables.py
@@ -0,0 +1,34 @@
+#####################################################################
+# vs.contentpopup
+# (C) 2011, Veit Schiele Communications GmbH
+#####################################################################
+
+import re
+from Products.Five.browser import BrowserView
+from Products.CMFCore.interfaces import ISiteRoot
+from Products.ATContentTypes.interfaces import IATFolder
+import lxml.html
+
+def extract_table(html, id):
+    root = lxml.html.fromstring(html)
+    nodes = root.xpath('//table[@id="%s"]' % id)
+    if len(nodes) == 1:
+        return lxml.html.tostring(nodes[0])
+    return ''
+
+class TableHandler(BrowserView):
+    """ Table folding - return a rendered HTML table by its ID """
+
+    def show_table(self, id):
+        """ Return the HTML code of a table given by its id """
+
+        if IATFolder.providedBy(self.context) or ISiteRoot.providedBy(self.context):
+            default_page = self.context.getDefaultPage()
+            context = self.context[default_page]
+            text_field = context.getField('text')
+        else:
+            text_field = self.context.getField('text')
+            context = self.context
+
+        if text_field:
+            return extract_table(text_field.get(context), id)
