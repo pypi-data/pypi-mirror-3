3907dff787cc3364833f61bd531941695c07cb6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:02 +0000

    Add package redissentry-core/0.2.0/redissentry-core-0.2.0.zip

---
+++ packages/redissentry-core/redissentry-core-0.2.0.zip/redissentry-core-0.2.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='redissentry-core',
+      version='0.2.0',
+      description='Module protecting a web framework against password bruteforce attacks',
+      author='Lev Maximov',
+      author_email='lev.maximov@gmail.com',
+      url='http://github.com/axil/redissentry-core',
+      packages=['redissentrycore'],
+      install_requires=['redis'],
+      license='MIT',
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Security',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Software Development :: User Interfaces',
+      ],
+)
