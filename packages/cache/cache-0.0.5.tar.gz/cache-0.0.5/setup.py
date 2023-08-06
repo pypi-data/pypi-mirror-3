bde4115d81fa5a1e01c55b00559440956fc1644d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:06 +0000

    Add package cache/0.0.5/cache-0.0.5.tar.gz

---
+++ packages/cache/cache-0.0.5.tar.gz/cache-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+from os.path import join, dirname
+import sys
+sys.path.insert(0, join(dirname(__file__), 'src'))
+from cache.version import __version__
+sys.path.pop(0)
+
+setup(
+    name="cache",
+    version=__version__,
+    description="caching for humans",
+    author="Jay Adkisson",
+    url="https://github.com/jayferd/python-cache",
+    author_email="j4yferd at gmail dot com (humans only, please)",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Topic :: Database :: Front-Ends",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="cache decorator humans",
+    packages=find_packages(),
+)
