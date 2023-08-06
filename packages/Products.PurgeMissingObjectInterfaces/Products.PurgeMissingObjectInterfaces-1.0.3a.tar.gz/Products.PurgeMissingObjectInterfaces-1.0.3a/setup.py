2236cfd795220045d6b3e0f5216eeb928a62d9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:38 +0000

    Add package Products.PurgeMissingObjectInterfaces/1.0.3a/Products.PurgeMissingObjectInterfaces-1.0.3a.tar.gz

---
+++ packages/Products.PurgeMissingObjectInterfaces/Products.PurgeMissingObjectInterfaces-1.0.3a.tar.gz/Products.PurgeMissingObjectInterfaces-1.0.3a/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.3a'
+
+setup(name='Products.PurgeMissingObjectInterfaces',
+      version=version,
+      description="A product for purging old and missing marker interfaces from objects",
+      long_description=open(os.path.join("Products", "PurgeMissingObjectInterfaces", "readme.txt")).read() + \
+          open(os.path.join("Products", "PurgeMissingObjectInterfaces", "changes.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Zope2",
+        "Framework :: ZODB",
+        "Development Status :: 3 - Alpha",
+        "Framework :: Plone",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Database",
+        "Topic :: Software Development :: Debuggers",
+        "Topic :: Utilities",
+        ],
+      keywords='python zope interfaces plone silva developers',
+      author='Morten W. Petersen',
+      author_email='info@nidelven-it.no',
+      url='http://www.nidelven-it.no/d',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'FakeZopeInterface',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
