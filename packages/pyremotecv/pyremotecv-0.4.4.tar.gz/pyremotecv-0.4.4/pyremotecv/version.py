5d2a62caca7fc0eb1d5218e9b519b3eb2410677a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:59 +0000

    Add package pyremotecv/0.4.4/pyremotecv-0.4.4.tar.gz

---
+++ packages/pyremotecv/pyremotecv-0.4.4.tar.gz/pyremotecv-0.4.4/pyremotecv/version.py
@@ -0,0 +1,2 @@
+__version__ = '0.4.4'
+Version = version = __version__
---
+++ packages/pyremotecv/pyremotecv-0.4.4.tar.gz/pyremotecv-0.4.4/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# pyremotecv - remotecv client for python
+# https://github.com/globocom/pyremotecv/wiki
+
+# Licensed under the MIT license: 
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2012 globo.com timehome@corp.globo.com
+
+from setuptools import setup, find_packages
+from pyremotecv.version import version
+
+description = "pyremotecv is a client library for remotecv (https://github.com/globocom/remotecv/wiki)."
+
+setup(
+    name = 'pyremotecv',
+    version = version,
+    description = description,
+    long_description = description,
+    keywords = 'facial feature detection opencv remote zeromq socket',
+    author = 'Time Home',
+    author_email = 'timehome@corp.globo.com',
+    url = 'https://github.com/globocom/pyremotecv',
+    license = 'MIT',
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Natural Language :: English',
+                   'Operating System :: MacOS',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 2.6',
+    ],
+
+    packages = find_packages(),
+
+    install_requires=[
+        "pyres>=1.1,<1.2"
+    ],
+
+)
+
+
