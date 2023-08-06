bc333ec7254a2689f49a66fa8fb37098c3730a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:51 +0000

    Add package fexpect/0.1-20120419/fexpect-0.1-20120419.tar.gz

---
+++ packages/fexpect/fexpect-0.1-20120419.tar.gz/fexpect-0.1-20120419/README.txt
@@ -0,0 +1,43 @@
+=======
+fexpect
+=======
+
+Fexpect is an extension to fabric for handling prompts with pexpect scripts.
+
+Provisional documentation in these blogposts:
+
+* http://ilogue.com/jasper/blog/fexpect--dealing-with-prompts-in-fabric-with-pexpect/
+* http://ilogue.com/jasper/blog/improved-fexpect-now-on-pypi/
+
+usage
+=====
+
+::
+
+    from ilogue.fexpect import expect, expecting, run
+
+    prompts = []
+    prompts += expect('What is your name?','Jasper')
+    prompts += expect('Where do you live?','Frankfurt')
+
+    with expecting(prompts):
+        run('command')
+
+You can use the included fabfile.py to run the fexpect tests:
+
+::
+
+    fab test -p 'yourlocalpassword'
+
+Note however that the tests may have different requirements (more recent fabric version). You can install these with the traditional:
+
+::
+
+    python setup.py test
+
+contributors
+============
+
+* Jasper van den Bosch - ilogue
+* Michael Ivanov
+
---
+++ packages/fexpect/fexpect-0.1-20120419.tar.gz/fexpect-0.1-20120419/ilogue/fexpect/tests.py
@@ -0,0 +1,70 @@
+import unittest
+from fabric.api import *
+
+def runtest(testclass):
+    suite = unittest.TestLoader().loadTestsFromTestCase(testclass)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+class FexpectTests(unittest.TestCase):
+
+    def test_one_expectation(self):
+        cmd = 'echo "Hello" && read NAME && echo "Hi $NAME."'
+        from ilogue.fexpect import expect, expecting, run
+        expectation =  expect('Hello','answer')
+        with expecting(expectation):
+            output = run(cmd)
+        self.assertIn('answer',output)
+
+    def test_two_expectations(self):
+        cmd = 'echo "Hello" && read ONE && echo "bladiebla" && read TWO && echo "First $ONE than $TWO."'
+        from ilogue.fexpect import expect, expecting, run
+        exp1 =  expect('Hello','111')
+        exp2 =  expect('bladiebla','222')
+        with expecting(exp1+exp2):
+            output = run(cmd)
+        self.assertIn('111',output)
+        self.assertIn('222',output)
+
+    def test_order_inconsequential(self):
+        #sequence shouldn't matter
+        cmd = 'echo "Hello" && read ONE && echo "bladiebla" && read TWO && echo "First $ONE than $TWO."'
+        from ilogue.fexpect import expect, expecting, run
+        exp1 =  expect('Hello','111')
+        exp2 =  expect('bladiebla','222')
+        with expecting(exp2+exp1):
+            output = run(cmd)
+        self.assertIn('111',output)
+        self.assertIn('222',output)
+
+    def test_exit_after_expectation(self):
+        import time
+        from StringIO import StringIO
+        #sequence shouldn't matter
+        script = "#!/usr/bin/python\nimport time\nfor i in range(1,8):\n\tprint(i)\n\ttime.sleep(1)"
+        cmd = 'python /tmp/test.py'
+        put(StringIO(script),'/tmp/test.py')
+        from ilogue.fexpect import expect, expecting, run
+        exp1 =  expect('Hello','111')
+        exp2 =  expect('3','expected',exitAfter=0)
+        t = time.time()
+        with expecting(exp1+exp2):
+            output = run(cmd)
+        elapsed = time.time() - t
+        self.assertGreater(elapsed,2)
+        self.assertLess(elapsed,4)
+
+    def test_one_expectation_local(self):
+        cmd = 'echo "Hello" && read NAME && echo "Hi $NAME."'
+        from ilogue.fexpect import expect, expecting, local
+        expectation =  expect('Hello','answer')
+        with expecting(expectation):
+            output = local(cmd,capture=True)
+        self.assertIn('answer',output)
+
+    def tryOrFailOnPrompt(self,method,args):
+        try:
+            with settings(abort_on_prompts=True):
+                result = method(*args)
+        except SystemExit as promptAbort:
+            self.fail("There was an unexpected (password) prompt.")
+        return result 
