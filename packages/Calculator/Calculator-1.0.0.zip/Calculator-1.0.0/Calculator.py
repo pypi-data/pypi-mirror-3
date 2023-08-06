da843ce8a6d4dd65de2b472c18cb843265f481a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:59 +0000

    Add package Calculator/1.0.0/Calculator-1.0.0.zip

---
+++ packages/Calculator/Calculator-1.0.0.zip/Calculator-1.0.0/Calculator.py
@@ -0,0 +1,16 @@
+def Add(a, b):
+        print(a + b)
+def AddList(a):
+        s = 0
+        for i in a:
+                s = s + i
+        print(s)
+def Substract(a, b):
+        print(a - b)
+def Multiply(a, b):
+        print(a * b)
+def divide(a, b):
+        if b == 0:
+                print("Error: Divide by zero")
+        else:
+                print(a / b)
---
+++ packages/Calculator/Calculator-1.0.0.zip/Calculator-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = 'Calculator',
+        version = '1.0.0',
+        py_modules = ['Calculator'],
+        author = 'Gopikrishna S',
+        author_email = 'sandalblack19@gmail.com',
+        url = 'http://gopikrishna.net78.net',
+        description = 'Performs basic calculation operations',
+        )
