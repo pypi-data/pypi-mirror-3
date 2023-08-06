cd7117c388baece5118c75c38f5f2f9c9597b174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:41 +0000

    Add package model-mommy/0.7/model_mommy-0.7.tar.gz

---
+++ packages/model-mommy/model_mommy-0.7.tar.gz/model_mommy-0.7/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="model_mommy",
+    version="0.7",
+    packages=["model_mommy"],
+    install_requires=["django<=1.3.1"],
+    author="vandersonmota",
+    author_email="vandersonmota@gmail.com",
+    url="http://github.com/vandersonmota/model_mommy",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    description="automatic object creation facility for django",
+    keywords="django testing factory python",
+)
