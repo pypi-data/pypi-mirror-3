e408b474c2a34914bb26c2aaf0ec2f3a0c277eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:44 +0000

    Add package capacity/0.0.8/capacity-0.0.8.tar.gz

---
+++ packages/capacity/capacity-0.0.8.tar.gz/capacity-0.0.8/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), "capacity", "__version__.py")) as version_file:
+    exec(version_file.read())
+
+setup(name="capacity",
+      classifiers = [
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.2",
+          ],
+      description="Data types to describe capacity",
+      license="BSD",
+      author="Rotem Yaari",
+      author_email="vmalloc@gmail.com",
+      version=__version__,
+      packages=find_packages(exclude=["tests"]),
+      scripts=[],
+      )
