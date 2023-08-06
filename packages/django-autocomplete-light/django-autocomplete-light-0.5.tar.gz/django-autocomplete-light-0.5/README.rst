e2b1b7d61ed99e1735b23ffde99a13e84cdb2eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:07 +0000

    Add package django-autocomplete-light/0.5/django-autocomplete-light-0.5.tar.gz

---
+++ packages/django-autocomplete-light/django-autocomplete-light-0.5.tar.gz/django-autocomplete-light-0.5/README.rst
@@ -0,0 +1,37 @@
+.. image:: https://secure.travis-ci.org/yourlabs/django-autocomplete-light.png?branch=master
+
+This is a simple alternative to django-ajax-selects.
+
+Requirements
+------------
+
+- Python 2.7
+- jQuery 1.7+
+- Django 1.4+ (at least for autocomplete_light.forms helpers)
+- django.contrib.staticfiles or you're on your own
+
+Resources
+---------
+
+Documentation graciously hosted by RTFD:
+http://django-autocomplete-light.rtfd.org
+
+Continuous integration graciously hosted by Travis:
+http://travis-ci.org/yourlabs/django-autocomplete-light
+
+Git graciously hosted by GitHub:
+https://github.com/yourlabs/django-autocomplete-light/
+
+Package graciously hosted by PyPi:
+http://pypi.python.org/pypi/django-autocomplete-light/
+
+Support
+-------
+
+Mailing list graciously hosted by librelist:
+yourlabs @ librelist.com
+
+Demo
+----
+
+See test_project/README
---
+++ packages/django-autocomplete-light/django-autocomplete-light-0.5.tar.gz/django-autocomplete-light-0.5/setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup, find_packages, Command
+import os
+import sys
+
+
+# Utility function to read the README file.
+# Used for the long_description. It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+class RunTests(Command):
+    description = "Run the django test suite from the testproj dir."
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        this_dir = os.getcwd()
+        testproj_dir = os.path.join(this_dir, "test_project")
+        os.chdir(testproj_dir)
+        sys.path.append(testproj_dir)
+        from django.core.management import execute_manager
+        os.environ["DJANGO_SETTINGS_MODULE"] = 'test_project.settings'
+        settings_file = os.environ["DJANGO_SETTINGS_MODULE"]
+        settings_mod = __import__(settings_file, {}, {}, [''])
+        execute_manager(settings_mod, argv=[
+            __file__, "test", "autocomplete_light"])
+        os.chdir(this_dir)
+
+setup(
+    name='django-autocomplete-light',
+    version='0.5',
+    description='Simple alternative to django-ajax-selects',
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='http://django-autocomplete-light.rtfd.org',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    long_description=read('README.rst'),
+    license='MIT',
+    keywords='django autocomplete',
+    cmdclass={"test": RunTests},
+    install_requires=[
+        'django',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
