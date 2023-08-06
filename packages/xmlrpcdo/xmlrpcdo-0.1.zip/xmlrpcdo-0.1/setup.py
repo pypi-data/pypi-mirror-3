5f11bb280e271436661c57c747c05bab0ed357b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:26 +0000

    Add package xmlrpcdo/0.1/xmlrpcdo-0.1.zip

---
+++ packages/xmlrpcdo/xmlrpcdo-0.1.zip/xmlrpcdo-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xmlrpcdo",
+    version="0.1",
+    # additional information
+    author="Michael Gruenewald",
+    author_email="mail@michaelgruenewald.eu",
+    description="XML-RPC request fiddler",
+    long_description="xmlrpcdo is a simple, self-explaining XML-RPC request fiddler",
+    license='License :: OSI Approved :: GPL License',
+    url="https://bitbucket.org/michaelgruenewald/xmlrpcdo",
+    # technical stuff
+    packages=find_packages(),
+    entry_points=dict(
+        console_scripts=['xmlrpcdo = xmlrpcdo:main']
+    )
+)
