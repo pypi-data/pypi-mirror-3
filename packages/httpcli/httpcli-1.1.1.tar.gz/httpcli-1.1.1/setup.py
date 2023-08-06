db39e1e43a8208f0ed30b1adb07a09d6a890afe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:25 +0000

    Add package httpcli/1.1.1/httpcli-1.1.1.tar.gz

---
+++ packages/httpcli/httpcli-1.1.1.tar.gz/httpcli-1.1.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+README = 'README.RST'
+with open(README) as file:
+    long_description = file.read()
+
+setup(
+        name='httpcli',
+        version='1.1.1',
+        description='RESTful command line HTTP client '
+            '(and simpler than cURL).',
+        long_description=long_description,
+        keywords='http client cli curl rest restful',
+        author='Dolph Mathews',
+        author_email='dolph.mathews@gmail.com',
+        url='http://github.com/dolph/httpcli',
+        packages=['httpcli'],
+        scripts=['bin/http'],
+        package_data = {'': [README]},
+        install_requires=[
+            'argparse',
+            'httplib2',
+            'setuptools',
+        ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: System Administrators',
+            'Topic :: Internet :: WWW/HTTP',
+            'Topic :: Utilities',
+        ],
+)
