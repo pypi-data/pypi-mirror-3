81215ec17bf5ca67c0af6b8cc15b15e2a9614ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:23 +0000

    Add package motmot.FastImage/0.5.5/motmot.FastImage-0.5.5.tar.gz

---
+++ packages/motmot.FastImage/motmot.FastImage-0.5.5.tar.gz/motmot.FastImage-0.5.5/setup.py
@@ -0,0 +1,20 @@
+import os, sys
+from setuptools import setup, find_packages, Extension
+
+setup(name="motmot.FastImage",
+      author="Andrew Straw",
+      author_email="strawman@astraw.com",
+      description="Pythonic API for the framewave SIMD library",
+      url='http://code.astraw.com/projects/motmot',
+      license="BSD",
+      version='0.5.5',
+      namespace_packages=['motmot'],
+      packages = find_packages(),
+      ext_modules=[Extension(name="motmot.FastImage.FastImage",
+                             sources=['src/FastImage.pyx',
+                                      'src/fic.c','src/fic_sobel.c'],
+                             libraries=['fwBase','fwImage'],
+                             ),
+                   ],
+      )
+
