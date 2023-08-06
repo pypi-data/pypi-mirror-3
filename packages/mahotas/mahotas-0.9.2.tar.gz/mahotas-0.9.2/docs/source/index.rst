32ac70e0608d5172c0f4bb9d39b7901d02f710a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:45 +0000

    Add package mahotas/0.9.2/mahotas-0.9.2.tar.gz

---
+++ packages/mahotas/mahotas-0.9.2.tar.gz/mahotas-0.9.2/docs/source/index.rst
@@ -0,0 +1,34 @@
+===================================
+Mahotas: Image Processing for numpy
+===================================
+
+Jump to full `API Documentation <api.html>`__
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   morphology
+   features
+   lbp
+   surf
+   thresholding
+   distance
+   labeled
+   polygon
+   wavelets
+   classification
+   faq
+   internals
+   principles
+   api
+
+
+.. include:: readme.rst
+
+Indices and tables
+------------------
+
+* :ref:`genindex`
+* :ref:`search`
---
+++ packages/mahotas/mahotas-0.9.2.tar.gz/mahotas-0.9.2/mahotas/tests/tools.py
@@ -0,0 +1,32 @@
+def hide_imports(hidden):
+    def capture(f):
+        def captured(*args, **kwargs):
+            original_import = __builtins__['__import__']
+            def masked_import(name, *rest):
+                print name
+                if name in hidden:
+                    raise ImportError, "Hidden package"
+                return original_import(name, *rest)
+            __builtins__['__import__'] = masked_import
+            try:
+                f()
+            finally:
+                __builtins__['__import__'] = original_import
+        return captured
+    return capture
+
+
+def test_hide_imports():
+    from nose.tools import raises
+    @raises(ImportError)
+    @hide_imports(['numpy'])
+    def import_imread():
+        import numpy
+    import_imread()
+    import numpy # should be restored
+
+def test_hide_imports_other():
+    @hide_imports(['numpy'])
+    def import_imread():
+        import os
+    import numpy
