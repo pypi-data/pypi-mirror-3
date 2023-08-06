ef5da65d5fa5eb0d1a6b78b7d1caecd0c81825e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:56 +0000

    Add package Flask-Security/1.2.2/Flask-Security-1.2.2.tar.gz

---
+++ packages/Flask-Security/Flask-Security-1.2.2.tar.gz/Flask-Security-1.2.2/setup.py
@@ -0,0 +1,61 @@
+"""
+Flask-Security
+--------------
+
+Flask-Security is a Flask extension that aims to add quick and simple security
+via Flask-Login, Flask-Principal, Flask-WTF, and passlib.
+
+Links
+`````
+
+* `development version
+  <https://github.com/mattupstate/flask-security/raw/develop#egg=Flask-Security-dev>`_
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='Flask-Security',
+    version='1.2.2',
+    url='https://github.com/mattupstate/flask-security',
+    license='MIT',
+    author='Matthew Wright',
+    author_email='matt@nobien.net',
+    description='Simple security for Flask apps',
+    long_description=__doc__,
+    packages=[
+        'flask_security',
+        'flask_security.datastore'
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'Flask-Login',
+        'Flask-Principal',
+        'Flask-WTF',
+        'passlib'
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'Flask-SQLAlchemy',
+        'Flask-MongoEngine',
+        'py-bcrypt'
+    ],
+    dependency_links=[
+        'http://github.com/sbook/flask-mongoengine/tarball/master#egg=Flask-MongoEngine-0.1.3-dev'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
