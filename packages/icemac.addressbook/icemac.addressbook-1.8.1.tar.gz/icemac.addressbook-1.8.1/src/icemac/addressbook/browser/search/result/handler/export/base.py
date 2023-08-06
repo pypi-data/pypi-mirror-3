9bd3df7a66eae30925dafca2595cb2e07363a4c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:13 +0000

    Add package icemac.addressbook/1.8.1/icemac.addressbook-1.8.1.tar.gz

---
+++ packages/icemac.addressbook/icemac.addressbook-1.8.1.tar.gz/icemac.addressbook-1.8.1/src/icemac/addressbook/browser/search/result/handler/export/base.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2008-2012 Michael Howitz
+# See also LICENSE.txt
+import icemac.addressbook.browser.base
+
+
+class BaseExport(object):
+    """Base class for exporters.
+
+    Subclasses only have to set the `exporter_class` which should implement
+    the interface `icemac.addressbook.export.interfaces.IExporter`.
+
+    """
+    exporter_class = NotImplemented
+
+    def __call__(self):
+        session = icemac.addressbook.browser.base.get_session(self.request)
+        persons = [self.context[id] for id in session['person_ids']]
+        exporter = self.exporter_class(persons, self.request)
+
+        self.request.response.setHeader('Content-Type', exporter.mime_type)
+        self.request.response.setHeader(
+            'Content-Disposition',
+            'attachment; filename=addressbook_export.%s' % (
+                exporter.file_extension))
+
+        return exporter.export()
