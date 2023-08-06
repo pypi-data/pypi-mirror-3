c9cd49ce638f2018a4465f78354d75555af80761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:56 +0000

    Add package pydstruct/0.0.1/pydstruct-0.0.1.tar.gz

---
+++ packages/pydstruct/pydstruct-0.0.1.tar.gz/pydstruct-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+    name = 'pydstruct',
+    packages = ['pydstruct'],
+    version = '0.0.1',
+    description = 'Reference Implementations of Data Structures',
+    long_description='Reference Implementations of Data Structures',
+    author='Craig Eales',
+    author_email='craig.eales@gmail.com',
+    url='http://sourceforge.net/projects/pydstruct/',
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries'
+        ]
+)
