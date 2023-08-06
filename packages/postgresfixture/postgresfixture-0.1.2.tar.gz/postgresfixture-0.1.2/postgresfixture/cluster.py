1e55e61f131422e88e626818df3ee571cecf17d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:09 +0000

    Add package postgresfixture/0.1.2/postgresfixture-0.1.2.tar.gz

---
+++ packages/postgresfixture/postgresfixture-0.1.2.tar.gz/postgresfixture-0.1.2/postgresfixture/cluster.py
@@ -0,0 +1,167 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Manage a PostgreSQL cluster."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = [
+    "Cluster",
+    ]
+
+from contextlib import closing
+from os import (
+    devnull,
+    environ,
+    makedirs,
+    path,
+    )
+import pipes
+from shutil import rmtree
+from subprocess import (
+    CalledProcessError,
+    check_call,
+    )
+
+import psycopg2
+
+
+PG_VERSION = "9.1"
+PG_BIN = "/usr/lib/postgresql/%s/bin" % PG_VERSION
+
+
+def path_with_pg_bin(exe_path):
+    """Return `exe_path` with `PG_BIN` added."""
+    exe_path = [
+        elem for elem in exe_path.split(path.pathsep)
+        if len(elem) != 0 and not elem.isspace()
+        ]
+    if PG_BIN not in exe_path:
+        exe_path.insert(0, PG_BIN)
+    return path.pathsep.join(exe_path)
+
+
+class Cluster:
+    """Represents a PostgreSQL cluster, running or not."""
+
+    def __init__(self, datadir):
+        self.datadir = path.abspath(datadir)
+
+    def execute(self, *command, **options):
+        """Execute a command with an environment suitable for this cluster."""
+        env = options.pop("env", environ).copy()
+        env["PATH"] = path_with_pg_bin(env.get("PATH", ""))
+        env["PGDATA"] = env["PGHOST"] = self.datadir
+        check_call(command, env=env, **options)
+
+    @property
+    def exists(self):
+        """Whether or not this cluster exists on disk."""
+        version_file = path.join(self.datadir, "PG_VERSION")
+        return path.exists(version_file)
+
+    @property
+    def pidfile(self):
+        """The (expected) pidfile for a running cluster.
+
+        Does *not* guarantee that the pidfile exists.
+        """
+        return path.join(self.datadir, "postmaster.pid")
+
+    @property
+    def logfile(self):
+        """The log file used (or will be used) by this cluster."""
+        return path.join(self.datadir, "backend.log")
+
+    @property
+    def running(self):
+        """Whether this cluster is running or not."""
+        with open(devnull, "rb") as null:
+            try:
+                self.execute("pg_ctl", "status", stdout=null)
+            except CalledProcessError, error:
+                if error.returncode == 1:
+                    return False
+                else:
+                    raise
+            else:
+                return True
+
+    def create(self):
+        """Create this cluster, if it does not exist."""
+        if not self.exists:
+            if not path.isdir(self.datadir):
+                makedirs(self.datadir)
+            self.execute("pg_ctl", "init", "-s", "-o", "-E utf8 -A trust")
+
+    def start(self):
+        """Start this cluster, if it's not already started."""
+        if not self.running:
+            self.create()
+            # pg_ctl options:
+            #  -l <file> -- log file.
+            #  -s -- no informational messages.
+            #  -w -- wait until startup is complete.
+            # postgres options:
+            #  -h <arg> -- host name; empty arg means Unix socket only.
+            #  -F -- don't bother fsync'ing.
+            #  -k -- socket directory.
+            self.execute(
+                "pg_ctl", "start", "-l", self.logfile, "-s", "-w",
+                "-o", "-h '' -F -k %s" % pipes.quote(self.datadir))
+
+    def connect(self, database="template1", autocommit=True):
+        """Connect to this cluster.
+
+        Starts the cluster if necessary.
+        """
+        self.start()
+        connection = psycopg2.connect(
+            database=database, host=self.datadir)
+        connection.autocommit = autocommit
+        return connection
+
+    def shell(self, database="template1"):
+        self.execute("psql", "--quiet", "--", database)
+
+    @property
+    def databases(self):
+        """The names of databases in this cluster."""
+        with closing(self.connect("postgres")) as conn:
+            with closing(conn.cursor()) as cur:
+                cur.execute("SELECT datname FROM pg_catalog.pg_database")
+                return {name for (name,) in cur.fetchall()}
+
+    def createdb(self, name):
+        """Create the named database."""
+        with closing(self.connect()) as conn:
+            with closing(conn.cursor()) as cur:
+                cur.execute("CREATE DATABASE %s" % name)
+
+    def dropdb(self, name):
+        """Drop the named database."""
+        with closing(self.connect()) as conn:
+            with closing(conn.cursor()) as cur:
+                cur.execute("DROP DATABASE %s" % name)
+
+    def stop(self):
+        """Stop this cluster, if started."""
+        if self.running:
+            # pg_ctl options:
+            #  -w -- wait for shutdown to complete.
+            #  -m <mode> -- shutdown mode.
+            self.execute("pg_ctl", "stop", "-s", "-w", "-m", "fast")
+
+    def destroy(self):
+        """Destroy this cluster, if it exists.
+
+        The cluster will be stopped if it's started.
+        """
+        if self.exists:
+            self.stop()
+            rmtree(self.datadir)
