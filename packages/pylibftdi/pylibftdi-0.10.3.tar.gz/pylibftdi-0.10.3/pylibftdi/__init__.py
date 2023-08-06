fdafa96d59ddf3c4d34de67524fccb4fb7d8a862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:22 +0000

    Add package pylibftdi/0.10.3/pylibftdi-0.10.3.tar.gz

---
+++ packages/pylibftdi/pylibftdi-0.10.3.tar.gz/pylibftdi-0.10.3/pylibftdi/__init__.py
@@ -0,0 +1,43 @@
+"""
+pylibftdi - python wrapper for libftdi
+
+Copyright (c) 2010-2012 Ben Bass <benbass@codedstructure.net>
+See LICENSE file for details and (absence of) warranty
+
+pylibftdi: http://bitbucket.org/codedstructure/pylibftdi
+
+
+libftdi can be found at:
+ http://www.intra2net.com/en/developer/libftdi/
+Neither libftdi nor Intra2net are associated with this project;
+if something goes wrong here, it's almost definitely my fault
+rather than a problem with the libftdi library.
+"""
+
+__VERSION__ = "0.10.3"
+__AUTHOR__ = "Ben Bass"
+
+
+__ALL__ = ['Driver', 'Device', 'BitBangDevice', 'Bus', 'FtdiError',
+           'ALL_OUTPUTS', 'ALL_INPUTS', 'BB_OUTPUT', 'BB_INPUT',
+           'USB_VID_LIST', 'USB_PID_LIST']
+
+from pylibftdi import _base, driver, util, bitbang
+
+# Bring them in to package scope so we can treat pylibftdi
+# as a module if we want.
+FtdiError = _base.FtdiError
+Bus = util.Bus
+Driver = driver.Driver
+Device = driver.Device
+BitBangDevice = bitbang.BitBangDevice
+USB_VID_LIST = driver.USB_VID_LIST
+USB_PID_LIST = driver.USB_PID_LIST
+
+ALL_OUTPUTS = bitbang.ALL_OUTPUTS
+ALL_INPUTS = bitbang.ALL_INPUTS
+BB_OUTPUT = bitbang.BB_OUTPUT
+BB_INPUT = bitbang.BB_INPUT
+FLUSH_BOTH = driver.FLUSH_BOTH
+FLUSH_INPUT = driver.FLUSH_INPUT
+FLUSH_OUTPUT = driver.FLUSH_OUTPUT
