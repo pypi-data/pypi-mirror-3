f5bce37bede11b20b58aa42b23cee4f2a08b9b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:33 +0000

    Add package pyres/1.4/pyres-1.4.tar.gz

---
+++ packages/pyres/pyres-1.4.tar.gz/pyres-1.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version='1.4'
+setup(
+    name='pyres',
+    version=version,
+    description='Python resque clone',
+    author='Matt George',
+    author_email='mgeorge@gmail.com',
+    maintainer='Matt George',
+    license='MIT',
+    url='http://github.com/binarydud/pyres',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    download_url='http://pypi.python.org/packages/source/p/pyres/pyres-%s.tar.gz' % version,
+    include_package_data=True,
+    entry_points = """\
+    [console_scripts]
+    pyres_manager=pyres.scripts:pyres_manager
+    pyres_scheduler=pyres.scripts:pyres_scheduler
+    pyres_worker=pyres.scripts:pyres_worker
+    """,
+    install_requires=[
+        'simplejson>=2.0.9',
+        'redis==2.4.12',
+        'setproctitle>=1.0'
+    ],
+    classifiers = [
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python'],
+)
