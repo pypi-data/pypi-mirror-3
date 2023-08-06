3c4a574e7132f7c7c164ac9fdb31a086e715363f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:35 +0000

    Add package pypcapfile/0.6/pypcapfile-0.6.tar.gz

---
+++ packages/pypcapfile/pypcapfile-0.6.tar.gz/pypcapfile-0.6/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='pypcapfile',
+      version='0.6',
+      description=('Pure Python package for reading and parsing libpcap '
+                       'savefiles.'),
+      long_description=read('README'),
+      author='Kyle Isom',
+      author_email='coder@kyleisom.net',
+      license='ISC',
+      url='http://kisom.github.com/pypcapfile',
+      scripts=['pcapfile_info',],
+      packages=['pcapfile', 
+                'pcapfile.protocols',
+                'pcapfile.protocols.linklayer',
+                'pcapfile.protocols.network',
+                ])
+
