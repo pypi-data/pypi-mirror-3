cf3b4c773b5fefbafbf15f4852909821883a4b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:49 +0000

    Add package dataflake.ldapconnection/1.3/dataflake.ldapconnection-1.3.tar.gz

---
+++ packages/dataflake.ldapconnection/dataflake.ldapconnection-1.3.tar.gz/dataflake.ldapconnection-1.3/dataflake/ldapconnection/utils.py
@@ -0,0 +1,27 @@
+##############################################################################
+#
+# Copyright (c) 2008-2012 Jens Vagelpohl and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" Utility functions and constants
+"""
+
+import ldap
+
+BINARY_ATTRIBUTES = ('objectguid', 'jpegphoto')
+
+def escape_dn(dn):
+    """ Escape all characters that need escaping for a DN, see RFC 2253 
+    """
+    if dn is None:
+        return None
+
+    return ldap.dn.dn2str(ldap.dn.str2dn(dn))
+
