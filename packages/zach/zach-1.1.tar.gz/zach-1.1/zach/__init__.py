767480c2a714f58e221da6bfb89170155e974c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:51 +0000

    Add package zach/1.1/zach-1.1.tar.gz

---
+++ packages/zach/zach-1.1.tar.gz/zach-1.1/zach/__init__.py
@@ -0,0 +1,9 @@
+"""
+Just because.
+"""
+
+import webbrowser
+
+webbrowser.open_new_tab('http://twitter.com/zachwill')
+webbrowser.open_new_tab('http://github.com/zachwill')
+webbrowser.open_new_tab('http://zachwill.com/')
