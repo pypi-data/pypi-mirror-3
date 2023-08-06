f965568b78143d419268f0d776e5005b84868b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:54 +0000

    Add package grokcore.startup/1.2/grokcore.startup-1.2.tar.gz

---
+++ packages/grokcore.startup/grokcore.startup-1.2.tar.gz/grokcore.startup-1.2/src/grokcore/startup/__init__.py
@@ -0,0 +1,19 @@
+##############################################################################
+#
+# Copyright (c) 2006-2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# Make this a package.
+from grokcore.startup.startup import (
+    application_factory,
+    debug_application_factory,
+    interactive_debug_prompt,
+    )
