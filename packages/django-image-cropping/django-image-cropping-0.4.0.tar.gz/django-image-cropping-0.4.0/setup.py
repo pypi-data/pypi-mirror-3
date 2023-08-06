c808a8f576671a618affd195b8888864912f2b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:12 +0000

    Add package django-image-cropping/0.4.0/django-image-cropping-0.4.0.tar.gz

---
+++ packages/django-image-cropping/django-image-cropping-0.4.0.tar.gz/django-image-cropping-0.4.0/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name = "django-image-cropping",
+    version = "0.4.0",
+    description = "A reusable app for cropping images easily and non-destructively in Django",
+    long_description=open('README.rst').read(),
+    author = "jonasvp",
+    author_email = "jvp@jonasundderwolf.de",
+    url = "http://github.com/jonasundderwolf/django-image-cropping",
+    #Name the folder where your packages live:
+    #(If you have other packages (dirs) or modules (py files) then
+    #put them into the package directory - they will be found
+    #recursively.)
+    packages = find_packages(),
+    include_package_data=True,
+    install_requires = [
+        'easy_thumbnails',
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
