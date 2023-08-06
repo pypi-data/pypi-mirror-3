304c0bf2927bc772e5396371a82aa29de62bb612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:49 +0000

    Add package pymultinest/0.1/pymultinest-0.1.tar.gz

---
+++ packages/pymultinest/pymultinest-0.1.tar.gz/pymultinest-0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name = "pymultinest",
+    version = "0.1",
+    description = "Access modules for MultiNest and APEMoST",
+    author = "Johannes Buchner",
+    author_email = "johannes.buchner.acad [ät] gmx.com",
+    url = "http://johannesbuchner.github.com/PyMultiNest/",
+    license = "GPLv3",
+    packages = ["pymultinest", "pyapemost", "pycuba"]
+)
