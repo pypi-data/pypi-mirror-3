4d23b639a952bb2da1f42c72325027638941a71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:33 +0000

    Add package pika_simple_daemon/0.1.4/pika_simple_daemon-0.1.4.tar.gz

---
+++ packages/pika_simple_daemon/pika_simple_daemon-0.1.4.tar.gz/pika_simple_daemon-0.1.4/setup.py
@@ -0,0 +1,29 @@
+__author__ = 'dtilleyshort'
+__version__ = '0.1.4'
+
+import os
+from setuptools import setup
+
+setup(name='pika_simple_daemon',
+    version=__version__,
+    url='https://github.com/Tilley/pika_simple_daemon',
+    author=__author__,
+    author_email="david.tilleyshort@gmail.com",
+    description="Simple Pika Daemon",
+    long_description = "",
+    license='BSD',
+    platforms=['linux'],
+    packages=['pika_simple_daemon', 'pika_simple_daemon'],
+    install_requires=[
+        "pika>=0.9.5",
+        "simplejson>=2.0.9",
+        "mock==0.8.0"
+
+    ],
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+    ],
+    zip_safe = False,
+)
