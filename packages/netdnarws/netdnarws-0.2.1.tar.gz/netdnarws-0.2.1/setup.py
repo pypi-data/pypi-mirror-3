faffb98e2487bf7707e0a8c7bf59f919a668abdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:29 +0000

    Add package netdnarws/0.2.1/netdnarws-0.2.1.tar.gz

---
+++ packages/netdnarws/netdnarws-0.2.1.tar.gz/netdnarws-0.2.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="netdnarws",
+    version="0.2.1",
+    description="A Python REST Client for NetDNA REST Web Services",
+    author="NetDNA Developer Team",
+    author_email = "devteam@netdna.com",
+    license = "GPL v3",
+    keywords = "NetDNA CDN API REST",
+    packages=['netdnarws'],
+    include_package_data=True,
+    install_requires=[
+      'requests_netdna',
+      'requests_oauth'
+    ],
+    url='http://developer.netdna.com',
+)
