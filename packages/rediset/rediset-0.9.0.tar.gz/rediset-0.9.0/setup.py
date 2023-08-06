ee3686e91e917092f3eda1a0d43a1387838e079e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:39 +0000

    Add package rediset/0.9.0/rediset-0.9.0.tar.gz

---
+++ packages/rediset/rediset-0.9.0.tar.gz/rediset-0.9.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='rediset',
+    version='0.9.0',
+    description='Composable, cacheable, lazy trees of Redis set operations',
+    author='Jamie Matthews',
+    author_email='jamie.matthews@gmail.com',
+    url='https://github.com/j4mie/rediset',
+    license = 'Public Domain',
+    py_modules=['rediset'],
+    install_requires=['redis'],
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: MacOS :: MacOS X',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Database",
+        "Topic :: Database :: Front-Ends",
+    ],
+)
