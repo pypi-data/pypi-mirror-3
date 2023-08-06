2b51a9069a07d912eb9f30a9997dbf96f4f225f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:07 +0000

    Add package tdm-loader/1.0.1/tdm_loader-1.0.1.tar.gz

---
+++ packages/tdm-loader/tdm_loader-1.0.1.tar.gz/tdm_loader-1.0.1/setup.py
@@ -0,0 +1,49 @@
+try:
+    from setuptools import setup
+    setuptools_available = True
+except ImportError:
+    from distutils.core import setup
+    setuptools_available = False
+import sys
+import os
+
+
+requires = ['numpy>=1.5']
+
+current_dir = os.getcwd()
+sys.path.insert(0, current_dir)
+
+package_name = 'tdm_loader'
+pkg = __import__(package_name)
+
+try:
+    long_description = open(os.path.join(current_dir, 'README.txt'),
+                            'rb').read()
+except:
+    long_description = ''
+
+
+kwargs = dict(
+    name = package_name,
+    version = pkg.__version__,
+    author = 'Josh Ayers',
+    author_email = 'josh.ayers (at) gmail.com',
+    url = 'https://bitbucket.org/joshayers/tdm_loader',
+    license = 'MIT',
+    description = ('Open National Instruments TDM/TDX files as '
+                   'NumPy structured arrays.'),
+    long_description = long_description,
+    packages = ['tdm_loader'],
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Programming Language :: Python :: 2.7',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Intended Audience :: End Users/Desktop',
+                   'Intended Audience :: Science/Research'])
+
+if setuptools_available:
+    kwargs.update(dict(
+        install_requires = requires))
+
+setup(**kwargs)
+
