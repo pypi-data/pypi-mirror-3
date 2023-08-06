2e42f50c895bb30c049f6d4c804262c3f327932e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:53 +0000

    Add package atbr/0.2/atbr-0.2.tar.gz

---
+++ packages/atbr/atbr-0.2.tar.gz/atbr-0.2/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for atbr
+"""
+
+# relevant doc for setuptools
+# http://docs.python.org/distutils/setupscript.html
+# http://docs.python.org/distutils/apiref.html
+
+from distutils.core import setup, Extension
+from distutils.sysconfig import get_config_vars
+import os
+
+os.environ['CC'] = 'g++'
+os.environ['CXX'] = 'g++'
+os.environ['CPP'] = 'g++'
+os.environ['LDSHARED'] = 'g++'
+
+# remove erranous default parameter Wstrict-prototypes
+(opt,) = get_config_vars('OPT')
+os.environ['OPT'] = " ".join(
+    flag for flag in opt.split() if flag != '-Wstrict-prototypes'
+)
+
+setup( 
+    name = "atbr",
+    author = 'Amund Tveit', 
+    author_email = 'amund@atbrox.com',
+    version = "0.2", 
+    ext_modules = [ 
+        Extension( 
+            "atbr._atbr",
+            sources = ["atbr/atbrpy.i"],
+            swig_opts=["-Wall","-c++"],
+            libraries=['rt','python2.6'],
+            include_dirs = ['/usr/include/python2.6'],
+            extra_compile_args = ['-std=c++0x'],
+            extra_link_args = ['-shared'],
+            language=["c++"]
+            ),
+        ],
+    packages=['atbr', 'atbrserver']
+    )
