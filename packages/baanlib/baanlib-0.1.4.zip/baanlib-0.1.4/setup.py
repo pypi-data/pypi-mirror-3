4111ad4fade698142130eecb4969acf1931e4d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:01 +0000

    Add package baanlib/0.1.4/baanlib-0.1.4.zip

---
+++ packages/baanlib/baanlib-0.1.4.zip/baanlib-0.1.4/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from baanlib import __version__
+
+
+setup(
+    name='baanlib',
+    version=__version__,
+    author='Mathias Fussenegger',
+    author_email='pip@zignar.net',
+    url='http://pypi.python.org/pypi/baanlib/',
+    license='LICENSE.txt',
+    description='ole automation library around win32com for use with Baan/Infor LN',
+    py_modules=['baanlib'],
+    install_requires=[
+        'mock',
+        'win32',
+    ],
+)
