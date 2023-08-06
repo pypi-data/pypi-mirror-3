371829f001f960d64e265f1c3d456ffd17d5250b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:59 +0000

    Add package listy/1.0.0/listy-1.0.0.tar.gz

---
+++ packages/listy/listy-1.0.0.tar.gz/listy-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name="listy",
+	version='1.0.0',
+	py_modules=['listy'],
+	author='anupam',
+	author_email='bhattacharyya.anupam@gmail.com',
+	url='http://www.google.com',
+	description='list displayer',
+	)
