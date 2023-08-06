a63391e1e6b97c7a79db252aea652ad340aef8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:48 +0000

    Add package dagny/0.3.0/dagny-0.3.0.tar.gz

---
+++ packages/dagny/dagny-0.3.0.tar.gz/dagny-0.3.0/src/dagny/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+from dagny.action import Action as action
+from dagny.resource import Resource
+import dagny.renderers
+
+__all__ = ['Resource', 'action']
+
+__version__ = '0.3.0'
---
+++ packages/dagny/dagny-0.3.0.tar.gz/dagny-0.3.0/src/dagny/conneg.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+"""
+Helpers and global mappings for content negotiation.
+
+If you want to define a custom mimetype shortcode, add it to the `MIMETYPES`
+dictionary in this module (without the leading '.' character). For example:
+
+    from dagny.conneg import MIMETYPES
+
+    MIMETYPES['png'] = 'image/png'
+    MIMETYPES['json'] = 'text/javascript'
+
+"""
+
+import mimetypes
+
+from webob.acceptparse import MIMEAccept
+
+__all__ = ['MIMETYPES', 'match_accept']
+
+
+# Maps renderer shortcodes => mimetypes.
+MIMETYPES = {
+    'rss': 'application/rss+xml',
+    'json': 'application/json',
+    'rdf_xml': 'application/rdf+xml',
+    'xhtml': 'application/xhtml+xml',
+    'xml': 'application/xml',
+}
+
+
+# Load all extension => mimetype mappings from `mimetypes` stdlib module.
+for ext, mimetype in mimetypes.types_map.iteritems():
+    shortcode = ext.lstrip(".").replace(".", "_")  # .tar.bz2 => tar_bz2
+    MIMETYPES.setdefault(shortcode, mimetype)
+del ext, shortcode, mimetype  # Clean up
+
+
+def match_accept(header, shortcodes):
+
+    """
+    Match an Accept header against a list of shortcodes, in order of preference.
+
+    A few examples:
+
+        >>> header = "application/xml,application/xhtml+xml,text/html"
+
+        >>> match_accept(header, ['html', 'json', 'xml'])
+        ['html', 'xml']
+
+        >>> header2 = "application/json,application/xml"
+
+        >>> match_accept(header2, ['html', 'json', 'xml'])
+        ['json', 'xml']
+
+        >>> match_accept(header2, ['html', 'xml', 'json'])
+        ['xml', 'json']
+
+    """
+
+    server_types = map(MIMETYPES.__getitem__, shortcodes)
+    client_types = list(MIMEAccept(header))
+    matches = []
+    for mimetype in server_types:
+        if mimetype in client_types:
+            matches.append(mimetype)
+
+    return map(shortcodes.__getitem__, map(server_types.index, matches))
