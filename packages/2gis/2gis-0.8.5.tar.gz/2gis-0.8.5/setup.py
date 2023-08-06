c1125bba57a2af822ffd9faa64fd78aa6dc627cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:35 +0000

    Add package 2gis/0.8.5/2gis-0.8.5.tar.gz

---
+++ packages/2gis/2gis-0.8.5.tar.gz/2gis-0.8.5/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+python-2gis
+============
+
+A Python library for accessing the 2gis API
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='2gis',
+    version='0.8.5',
+    author='IRK.ru',
+    author_email='support@irk.ru',
+    url='https://github.com/irkru/python-2gis',
+    description='2gis library for Python',
+    long_description=__doc__,
+    license='BSD',
+    packages=find_packages(),
+    install_requires=('requests'),
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries',
+    ),
+)
