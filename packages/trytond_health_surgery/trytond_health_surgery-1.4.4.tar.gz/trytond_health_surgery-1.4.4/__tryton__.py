a0db88d0cd47941c6e16cc10941f7caffcaf00ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:47 +0000

    Add package trytond_health_surgery/1.4.4/trytond_health_surgery-1.4.4.tar.gz

---
+++ packages/trytond_health_surgery/trytond_health_surgery-1.4.4.tar.gz/trytond_health_surgery-1.4.4/__tryton__.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'GNU Health: Surgery',
+    'version': '1.4.4',
+    'author': 'GNU Solidario',
+    'email': 'health@gnusolidario.org',
+    'website': 'http://health.gnu.org',
+    'depends': [
+        'health',
+    ],
+    'name_es_ES': 'GNU Health : Cirugía',
+    'translation': ['locale/es_ES.po', 'locale/fr_FR.po', 'locale/el.po',
+        'locale/fa.po','locale/it.po'],
+
+    'description': '''
+
+Basic Surgery Module for GNU Health
+
+If you want to include standard codings for procedures, please install 
+corresponding module, like ICD10-PCS
+
+''',
+
+    'description_es_ES': '''
+Módulo básico que cirugía para GNU Health
+
+Si quiere incluir códigos de procedimientos, por favor instale algún módulo
+con la funcionalidad específica, como gnuhealth_ICD10-PCS
+''',
+
+    'xml': [
+        'health_surgery_view.xml',
+        'security/access_rights.xml'
+    ],
+    'active': False,
+}
