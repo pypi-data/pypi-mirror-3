c4922b39f4eb983256fffa8120d77ea17804f233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:07 +0000

    Add package ftw.testing/1.2/ftw.testing-1.2.tar.gz

---
+++ packages/ftw.testing/ftw.testing-1.2.tar.gz/ftw.testing-1.2/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.2'
+maintainer = 'Jonas Baumann'
+
+tests_require = [
+    'Acquisition<4.0a1',
+    ]
+
+setup(name='ftw.testing',
+      version=version,
+      description='Provides some testing helpers and an advanced MockTestCase.',
+
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        ],
+
+      keywords='ftw testing mocking testcase mock stub',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
+      url='https://github.com/4teamwork/ftw.testing',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        'unittest2',
+        'plone.mocktestcase',
+        'plone.testing',
+        'zope.interface',
+        'zope.component',
+        'zope.configuration',
+        'zope.publisher',
+        ],
+      tests_require=tests_require,
+      extras_require={'tests': tests_require},
+
+      entry_points='''
+      # -*- Entry points: -*-
+      ''',
+      )
