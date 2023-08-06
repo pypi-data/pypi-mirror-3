7f8733884bdd3f4f6320f2ce5b745e755dc0e956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:16 +0000

    Add package django-group-access/1.0.0/django-group-access-1.0.0.tar.gz

---
+++ packages/django-group-access/django-group-access-1.0.0.tar.gz/django-group-access-1.0.0/django_group_access/admin.py
@@ -0,0 +1,10 @@
+# Copyright 2012 Canonical Ltd.
+from django.contrib import admin
+from django_group_access.models import *
+
+
+class GroupAdmin(admin.ModelAdmin):
+    filter_horizontal = ('members',)
+
+admin.site.register(AccessGroup, GroupAdmin)
+admin.site.register(Invitation)
