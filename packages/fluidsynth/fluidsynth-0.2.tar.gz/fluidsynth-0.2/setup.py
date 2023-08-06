f0450d0ba6cee1092cfbab2ada9510d81cc48604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:55 +0000

    Add package fluidsynth/0.2/fluidsynth-0.2.tar.gz

---
+++ packages/fluidsynth/fluidsynth-0.2.tar.gz/fluidsynth-0.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="fluidsynth",
+    version="0.2",
+    description="Fluidsynth bindings",
+    author="Corbin Simpson",
+    author_email="MostAwesomeDude@gmail.com",
+    url="http://corbinsimpson.com/",
+    packages=["fluidsynth"],
+)
