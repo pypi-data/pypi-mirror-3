64e40a5fff6e5bafa41b114b92d649c3af92ca20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:35 +0000

    Add package jldzeromq/0.3.3/jldzeromq-0.3.3.tar.gz

---
+++ packages/jldzeromq/jldzeromq-0.3.3.tar.gz/jldzeromq-0.3.3/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+    Jean-Lou Dupont's ZeroMQ scripts
+    
+    Created on 2012-01-19
+    @author: jldupont
+"""
+__author__  ="Jean-Lou Dupont"
+__version__ ="0.3.3"
+
+DESC="""
+Overview
+--------
+
+Collection of 0mq related scripts
+
+* jld0sub: subscribe to topics, output to stdout w/wo JSON
+* jld0pub: publish to a topic, input from stdin w/wo JSON
+* jld0fabric: fabric to publish/subscribe pattern
+* jld0send : send message to fabric for all the subscribers
+"""
+
+
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(name=         'jldzeromq',
+      version=      __version__,
+      description=  'Collection of ZeroMQ related tools',
+      author=       __author__,
+      author_email= 'jl@jldupont.com',
+      url=          'http://www.systemical.com/doc/opensource/jldzeromq',
+      package_dir=  {'': "src",},
+      packages=     find_packages("src"),
+      scripts=      ['src/scripts/jld0sub',
+                     'src/scripts/jld0pub',
+                     'src/scripts/jld0fabric',  
+                     'src/scripts/jld0send',
+                     ],
+      zip_safe=False
+      ,long_description=DESC
+      ,install_requires=[ "pyzmq",
+                         ]
+      )
+
+#############################################
+
+f=open("latest", "w")
+f.write(str(__version__)+"\n")
+f.close()
+
