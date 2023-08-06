bc88f4855c04c03d249451b357378192dd179228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:47 +0000

    Add package collective.googlenews/1.0rc2/collective.googlenews-1.0rc2.zip

---
+++ packages/collective.googlenews/collective.googlenews-1.0rc2.zip/collective.googlenews-1.0rc2/collective/googlenews/interfaces.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from zope import schema
+from zope.interface import Interface
+
+from collective.googlenews import _
+
+
+class IGoogleNewsLayer(Interface):
+    """Browser layer for this addon"""
+
+
+class GoogleNewsSettings(Interface):
+    """Settings site wide of this addon"""
+
+    # XXX: default value is declared at profiles/default/registry.xml
+    portal_types = schema.List(
+        title=_(u"Portal types"),
+        description=_(u"Select portal types you want to apply digit id."),
+        value_type=schema.Choice(vocabulary=u'plone.app.vocabularies.ReallyUserFriendlyTypes'),
+        )
