a71e9adb4751e3f2ed79dc93d86d8c844e6e80bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:20 +0000

    Add package metaTED/2.0.1/metaTED-2.0.1.zip

---
+++ packages/metaTED/metaTED-2.0.1.zip/metaTED-2.0.1/metaTED/crawler/get_supported_subtitle_languages.py
@@ -0,0 +1,27 @@
+import logging
+from lxml import html
+from lxml.cssselect import CSSSelector
+import re
+
+
+LANGUAGES_LIST_URL = 'http://www.ted.com/translate/languages'
+_LANGUAGES_SELECTOR = CSSSelector('div#maincontent div ul li a')
+_LANGUAGE_CODE_RE = re.compile('/translate/languages/([\w\-]+)')
+
+
+def get_supported_subtitle_languages():
+    logging.debug('Looking for supported subtitle languages...')
+    document = html.parse(LANGUAGES_LIST_URL)
+    
+    languages = {}
+    for a in _LANGUAGES_SELECTOR(document):
+        language_name = a.get('title')
+        match = _LANGUAGE_CODE_RE.search(a.get('href'))
+        if match:
+            languages[match.group(1)] = language_name
+        else:
+            logging.warning("'%s' doesn't seem to be a language", language_name)
+    
+    logging.info("Found %d supported subtitle language(s)", len(languages))
+    logging.debug("Supported subtitle languages are: %s", languages)
+    return languages
