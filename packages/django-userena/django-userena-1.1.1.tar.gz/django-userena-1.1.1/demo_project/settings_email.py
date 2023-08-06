72fb3a87546c6b88e818f90dc175c0b33bf36a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:10 +0000

    Add package django-userena/1.1.1/django-userena-1.1.1.tar.gz

---
+++ packages/django-userena/django-userena-1.1.1.tar.gz/django-userena-1.1.1/demo_project/settings_email.py
@@ -0,0 +1,6 @@
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ham@breadandpepper.com'
+EMAIL_HOST_PASSWORD = 'hamspam'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "Userena <hello@breadandpepper.com>"
