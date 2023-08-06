d6dd82afe845a461a515f669b45a3f4573421fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:01 +0000

    Add package xdvtheme.inventions/1.0/xdvtheme.inventions-1.0.zip

---
+++ packages/xdvtheme.inventions/xdvtheme.inventions-1.0.zip/xdvtheme.inventions-1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='xdvtheme.inventions',
+      version=version,
+      description="A xdvtheme for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='theme skin Plone xdv menttes',
+      author='Roberto Allende',
+      author_email='rover@menttes.com',
+      url='http://menttes.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['xdvtheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=[],
+      paster_plugins = ["ZopeSkel"],
+      )
