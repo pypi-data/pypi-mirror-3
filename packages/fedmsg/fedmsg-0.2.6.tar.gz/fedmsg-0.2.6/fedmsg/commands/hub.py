dc0cc93a5b96884d6052378ffca6adc28717a69a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:42 +0000

    Add package fedmsg/0.2.6/fedmsg-0.2.6.tar.gz

---
+++ packages/fedmsg/fedmsg-0.2.6.tar.gz/fedmsg-0.2.6/fedmsg/commands/hub.py
@@ -0,0 +1,33 @@
+import fedmsg
+from fedmsg.commands import command
+
+extra_args = [
+    (['--websocket-server-port'], {
+        'dest': 'moksha.livesocket.websocket.port',
+        'type': int,
+        'help': 'Port on which to host the websocket server.',
+        'default': None,
+    }),
+]
+
+
+@command(name="fedmsg-hub", extra_args=extra_args, daemonizable=True)
+def hub(**kw):
+    """ Run the fedmsg hub. """
+
+    # Check if the user wants the websocket server to run
+    if kw['moksha.livesocket.websocket.port']:
+        kw['moksha.livesocket.backend'] = 'websocket'
+
+    # Rephrase the fedmsg-config.py config as moksha *.ini format.
+    # Note that the hub we kick off here cannot send any message.  You should
+    # use fedmsg.publish(...) still for that.
+    moksha_options = dict(
+        zmq_subscribe_endpoints=','.join(
+            ','.join(bunch) for bunch in kw['endpoints'].values()
+        ),
+    )
+    kw.update(moksha_options)
+
+    from moksha.hub import main
+    main(options=kw)
