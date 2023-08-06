90a01bb42bdfe3f295222fa4579bcda50cb758c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:39 +0000

    Add package dlipower/0.1.1/dlipower-0.1.1.tar.gz

---
+++ packages/dlipower/dlipower-0.1.1.tar.gz/dlipower-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+  name="dlipower",
+  version="0.1.1",
+  packages=["dlipower",],
+  license="BSD",
+  scripts=["example.py"],
+  long_description=open('README.txt').read(),
+)
