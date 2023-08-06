31806e6f81b1029282e724dad9a5a8be22d1410a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:34 +0000

    Add package trytond_electronic_mail/2.4.0.1/trytond_electronic_mail-2.4.0.1.tar.gz

---
+++ packages/trytond_electronic_mail/trytond_electronic_mail-2.4.0.1.tar.gz/trytond_electronic_mail-2.4.0.1/__tryton__.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+{
+    'name': 'Electronic Mail',
+    'description': '''Electronic mail storage''',
+    'version': '2.4.0.1',
+    'author': 'Openlabs Technologies & Consulting (P) LTD',
+    'email': 'info@openlabs.co.in',
+    'website': 'http://openlabs.co.in/',
+    'depends': [
+        'ir',
+        'res',
+    ],
+    'xml': [
+        'electronic_mail.xml',
+    ],
+    'translation': [
+    ],
+}
