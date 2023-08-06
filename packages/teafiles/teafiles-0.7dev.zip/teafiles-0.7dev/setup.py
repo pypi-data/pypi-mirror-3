8622b6fd5690a0848e7d7fb60bb15c3609ff8b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:08 +0000

    Add package teafiles/0.7dev/teafiles-0.7dev.zip

---
+++ packages/teafiles/teafiles-0.7dev.zip/teafiles-0.7dev/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+setup(
+    name='teafiles',
+    version='0.7dev',
+    author='discretelogics',
+    author_email='pythonapi@discretelogics.com',
+    url='http://discretelogics.com/TeaFiles',
+    download_url='http://discretelogics.com/TeaFiles TBD',
+    description='Time Series storage in flat files.',
+    long_description=open('README.txt').read(),
+
+    packages=["teafiles"],
+    #package_dir={'': 'teafiles'},
+    #py_modules=['teafile', 'clockwise'],
+    
+    keywords='timeseries analysis event processing teatime simulation finance',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    classifiers= [
+                  #  'Development Status :: 3 - Alpha' // pypi chokes on that
+                    'Intended Audience :: Developers',
+                    'Intended Audience :: Science/Research',
+                    'License :: OSI Approved',
+                    'License :: OSI Approved :: GNU General Public License (GPL)',
+                    'Operating System :: MacOS',
+                    'Operating System :: Microsoft :: Windows',
+                    'Operating System :: POSIX',
+                    'Operating System :: Unix',
+                    'Programming Language :: Python',
+                    'Topic :: Scientific/Engineering',
+                    'Topic :: Software Development'
+                    
+                  ]
+    )
