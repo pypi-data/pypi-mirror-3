c55b0aa9108e3ef0977c162dd036a56fa827cce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:20 +0000

    Add package state/0.1.0dev/state-0.1.0dev.zip

---
+++ packages/state/state-0.1.0dev.zip/state-0.1.0dev/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.0'
+
+setup(name='state',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='LaiYonghao',
+      author_email='mail@laiyonghao.com',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
