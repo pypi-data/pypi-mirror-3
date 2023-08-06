29f95cad5ad0b27af1236f13c2bb73e4f64f6d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:55 +0000

    Add package leotest/1.2.0/leotest-1.2.0.tar.gz

---
+++ packages/leotest/leotest-1.2.0.tar.gz/leotest-1.2.0/leotest.py
@@ -0,0 +1,10 @@
+"""This is the "nester.py" module, has a "print_lol" function"""
+def print_lol(the_list, level=0):
+    """This function has a parameter, is "the_list", it's any list."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for num in range(level):
+                print("\t", end="")
+            print(each_item)
