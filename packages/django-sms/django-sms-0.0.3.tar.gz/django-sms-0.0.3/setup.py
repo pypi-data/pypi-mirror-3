7e6dad60048ad785e4b6945dbd6f797b383abb69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:48 +0000

    Add package django-sms/0.0.3/django-sms-0.0.3.tar.gz

---
+++ packages/django-sms/django-sms-0.0.3.tar.gz/django-sms-0.0.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='django-sms',
+    version='0.0.3',
+    description='A Django app for sending SMS with interchangable backends.',
+    author='Niels Sandholt Busch',
+    author_email='niels.busch@gmail.com',
+    url='https://bitbucket.org/resmio/django-sms/',
+    long_description=open('README', 'r').read(),
+    packages=find_packages(),
+    install_requires=['django>=1.3',],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
