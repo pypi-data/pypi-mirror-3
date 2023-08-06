b5e15f7d181330e3c21978df2ace579c536f1676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:25 +0000

    Add package datrie/0.4.1/datrie-0.4.1.tar.gz

---
+++ packages/datrie/datrie-0.4.1.tar.gz/datrie-0.4.1/tests/test_state.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import datrie
+
+def _trie():
+    trie = datrie.Trie(ranges=[(chr(0), chr(127))])
+    trie['f'] = 1
+    trie['fo'] = 2
+    trie['fa'] = 3
+    trie['faur'] = 4
+    trie['fauxiiiip'] = 5
+    trie['fauzox'] = 10
+    trie['fauzoy'] = 20
+    return trie
+
+def test_trie_state():
+    trie = _trie()
+    state = datrie.State(trie)
+    state.walk('f')
+    assert state.data() == 1
+    state.walk('o')
+    assert state.data() == 2
+
+def test_state_next():
+    trie = _trie()
+    state = datrie.State(trie)
+
+    counter = 0
+    while state._next():
+        counter += 1
+
+    assert counter == 15
+
+def test_state_next_values():
+    trie = _trie()
+    state = datrie.State(trie)
+
+    res = []
+    while state._next():
+        if state.is_terminal():
+            res.append(state.data())
+
+    assert res == [1, 3, 4, 5, 10, 20, 2]
