18c92b2168818970c3ac5b846739a0e77819d8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:04 +0000

    Add package subdown2/0.6.1/subdown2-0.6.1.tar.gz

---
+++ packages/subdown2/subdown2-0.6.1.tar.gz/subdown2-0.6.1/subdown2/slider test.py
@@ -0,0 +1,31 @@
+import math,time
+from Tkinter import *
+
+class Application(Frame):
+
+	def __init__(self, master=None):
+		Frame.__init__(self, master)
+		self.grid()
+		self.createWidgets()
+		
+		
+	def createWidgets(self):
+	
+		self.Slider = Scale(self, from_=0, to_=100, resolution=.1, orient=HORIZONTAL)
+		self.Slider.grid(row=0,column=0)
+		
+		self.sliderbutton = Button(self, text='Set to 50', command=self.sliderevent)
+		self.sliderbutton.grid(row=2,column=1)
+		
+		
+	def sliderevent(self):
+	
+		#self.value = float(self.Slider.get())
+		self.Slider.set(25.0)
+		self.Slider.set(50.0)
+		
+		#print 'Slider Value = %f' %self.value
+		
+		
+App = Application()
+App.mainloop()
