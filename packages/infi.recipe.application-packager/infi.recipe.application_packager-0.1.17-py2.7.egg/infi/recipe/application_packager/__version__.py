022b0ffbfeeaa0eaf4559c6b7f5076e1e6330eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:28 +0000

    Add package infi.recipe.application-packager/0.1.17/infi.recipe.application_packager-0.1.17-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.1.17-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.17"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'cca3b9dc50f86ea2ae088fe94f677511e07825c8'
+__git_head_subject__ = 'Finished release v0.1.17.'
+__git_head_message__ = '* release/v0.1.17:\n  TRIVIAL fixing production scripts\n  empty commit after version v0.1.16'
+__git_dirty_diff__ = ''
