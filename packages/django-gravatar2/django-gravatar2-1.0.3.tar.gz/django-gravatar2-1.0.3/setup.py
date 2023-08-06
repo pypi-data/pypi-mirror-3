1109aa73fc514b0e5e1c3bc8c87d9819abcf2408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:35 +0000

    Add package django-gravatar2/1.0.3/django-gravatar2-1.0.3.tar.gz

---
+++ packages/django-gravatar2/django-gravatar2-1.0.3.tar.gz/django-gravatar2-1.0.3/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+readme = open('README.rst')
+
+setup(
+    name='django-gravatar2',
+    version='1.0.3',
+    description='Essential Gravatar support for Django. Features helper methods, templatetags and a full test suite!',
+    long_description=readme.read(),
+    author='Tristan Waddington',
+    author_email='tristan.waddington@gmail.com',
+    url='https://github.com/twaddington/django-gravatar',
+    packages=['django_gravatar', 'django_gravatar.templatetags'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable', # 4 Beta, 5 Production/Stable
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
