38694f13627de117d54c829a27c634b36fe3f468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:34 +0000

    Add package gsconfig/0.5.1/gsconfig-0.5.1.tar.gz

---
+++ packages/gsconfig/gsconfig-0.5.1.tar.gz/gsconfig-0.5.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+readme_text = file('README.rst', 'rb').read()
+
+setup(name = "gsconfig",
+    version = "0.5.1",
+    description = readme_text,
+    keywords = "GeoServer REST Configuration",
+    license = "MIT",
+    url = "http://github.com/opengeo/gsconfig.py",
+    author = "David Winslow, Sebastian Benthall",
+    author_email = "dwinslow@opengeo.org",
+    install_requires = ['httplib2'],
+    package_dir = {'':'src'},
+    packages = find_packages('src'),
+    test_suite = "test.catalogtests",
+    classifiers = [
+                 'Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Scientific/Engineering :: GIS',
+                ]
+) 
+
