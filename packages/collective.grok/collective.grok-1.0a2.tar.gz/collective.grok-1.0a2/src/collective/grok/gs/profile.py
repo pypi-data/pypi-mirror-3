83201649df5e1149809e21519f0abdeb55db8848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:40 +0000

    Add package collective.grok/1.0a2/collective.grok-1.0a2.tar.gz

---
+++ packages/collective.grok/collective.grok-1.0a2.tar.gz/collective.grok-1.0a2/src/collective/grok/gs/profile.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+import sys
+
+from Products.GenericSetup.interfaces import EXTENSION
+
+
+def profile(name=u'default', title=None, description=None,
+            directory='', provides=EXTENSION, for_=None):
+    ''' Register a profile for GenericSetup '''
+    frame = sys._getframe(1)
+    try:
+        product = frame.f_locals['__package__']
+    except KeyError:
+        # Called from a function?
+        product = frame.f_globals['__package__']
+    if directory is None:
+        directory = 'profiles/%s' % name
+
+    if title is None:
+        title = u"Profile '%s' from '%s'" % (name, product)
+
+    if description is None:
+        description = u''
+
+    # XXX we do not have function grokkers (yet) so we put the annotation
+    # on the module and defer its registration.
+    gs_profiles = frame.f_locals.get('__grok_profiles__', None)
+    if gs_profiles is None:
+        frame.f_locals['__grok_profiles__'] = gs_profiles = []
+
+    profile = (name, title, description, directory, product, provides, for_)
+    gs_profiles.append(profile)
