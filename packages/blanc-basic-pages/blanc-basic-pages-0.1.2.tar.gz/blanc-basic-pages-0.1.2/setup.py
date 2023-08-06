c0497d3d188098cf94b6b080c2d39b69d0c2571f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:32 +0000

    Add package blanc-basic-pages/0.1.2/blanc-basic-pages-0.1.2.tar.gz

---
+++ packages/blanc-basic-pages/blanc-basic-pages-0.1.2.tar.gz/blanc-basic-pages-0.1.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='blanc-basic-pages',
+    version='0.1.2',
+    description='Blanc Basic Pages for Django',
+    long_description=open('README.rst').read(),
+    url='http://www.blanctools.com/',
+    maintainer='Alex Tomkins',
+    maintainer_email='alex@hawkz.com',
+    platforms=['any'],
+    install_requires=[
+        'django-mptt>=0.5.0',
+        'django-mptt-treechangelist>=0.1',
+    ],
+    packages=[
+        'blanc_basic_pages',
+        'blanc_basic_pages.pages',
+        'blanc_basic_pages.pages.templatetags',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    license='BSD-2',
+)
