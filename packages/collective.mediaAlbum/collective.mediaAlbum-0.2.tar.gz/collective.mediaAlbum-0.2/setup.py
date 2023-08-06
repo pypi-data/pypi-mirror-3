eff1bbfdd833f11ecfe5bcb7454d0dcf34ca83db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:44 +0000

    Add package collective.mediaAlbum/0.2/collective.mediaAlbum-0.2.tar.gz

---
+++ packages/collective.mediaAlbum/collective.mediaAlbum-0.2.tar.gz/collective.mediaAlbum-0.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2'
+
+setup(name='collective.mediaAlbum',
+      version=version,
+      description="Turn folders into albums that show your media in a user friendly interface (basically overrides Plone thumbnail view)",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='album plone images image link youtube vimeo mediashow quickupload folder',
+      author='David Jonas',
+      author_email='davidjonasdesign@gmail.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.mediaShow',
+          'collective.quickupload'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
