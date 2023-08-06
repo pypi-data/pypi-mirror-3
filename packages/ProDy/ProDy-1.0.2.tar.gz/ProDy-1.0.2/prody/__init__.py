a7642bb21838409ed6229da3545214d7330f639e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:38 +0000

    Add package ProDy/1.0.2/ProDy-1.0.2.tar.gz

---
+++ packages/ProDy/ProDy-1.0.2.tar.gz/ProDy-1.0.2/prody/__init__.py
@@ -0,0 +1,298 @@
+# ProDy: A Python Package for Protein Dynamics Analysis
+# 
+# Copyright (C) 2010-2012 Ahmet Bakan
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#  
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+__author__ = 'Ahmet Bakan'
+__copyright__ = 'Copyright (C) 2010-2012 Ahmet Bakan'
+__version__ = '1.0.2'
+
+release = [int(x) for x in __version__.split('.')]
+
+import os
+import sys
+import os.path
+import platform
+
+if not (2,6) <= sys.version_info[:2] <= (2,7):
+    raise Exception("prody is compatible with Python 2.6 and 2.7, you are "
+                    "using Python " + platform.python_version())
+
+try:
+    import numpy as np
+except ImportError:
+    raise ImportError('numpy not found, it is a required package')
+
+from tools import *
+
+import warnings
+
+DEPRECATION_WARNINGS = False
+
+CONFIGURATION = {
+    'backup': False,
+    'backup_ext': '.BAK',
+    'ligand_xml_save': False,
+    'typo_warnings': True,
+    'check_updates': 0,
+    'auto_secondary': False,
+}
+
+from datetime import date
+today = date.today()
+
+if today.year == 2012:
+    warnings.filterwarnings('default', category=DeprecationWarning)
+
+def deprecate(dep, alt, ver='1.1', sl=3):
+    """Issue a deprecation warning for *dep* and recommend using *alt*, if 
+    current year is 2012."""
+
+    warnings.warn('`{0:s}` is deprecated for removal in v{1:s}, use `{2:s}`.'
+                  .format(dep, ver, alt), DeprecationWarning, stacklevel=sl)
+
+def turnonDepracationWarnings(action='always'):
+    """Turn on deprecation warnings for the current session.  By default
+     (``action='always'``), deprecation warnings will be printed every time
+     a function is called to help identification of multiple occurrence 
+     of deprecated function and method names.  When ``action='default'``
+     is passed, warning will be issued at the first call of a function.
+     The latter behavior will automatically kick in when v0.9 is released.
+     Until v0.9 is released, restarting the session will turn of warnings.
+     This function must be called as ``prody.turnonDepracationWarnings``."""
+    
+    global DEPRECATION_WARNINGS
+    DEPRECATION_WARNINGS = True
+    warnings.filterwarnings(action, category=DeprecationWarning)
+
+    
+_PY3K = sys.version_info[0] > 2
+
+PACKAGECONF =  os.path.join(USERHOME, '.' + __package__ + 'rc')
+if not os.path.isfile(PACKAGECONF) and os.path.isfile(PACKAGECONF[:-2]):
+    os.rename(PACKAGECONF[:-2], PACKAGECONF)
+
+LOGGER = PackageLogger('.prody')
+
+SETTINGS = PackageSettings(logger=LOGGER) 
+SETTINGS.load()
+
+
+_max_key_len = max([len(_key) for _key in CONFIGURATION.iterkeys()])
+_max_def_len = 68 - _max_key_len
+_table_lines = '=' * _max_key_len +  '  ' + '=' * _max_def_len
+
+docstring = """
+
+    {0:s}
+    {1:s}  {2:s}         
+    {0:s}""".format(_table_lines, 'Option'.ljust(_max_key_len), 
+                    'Default setting (type)')
+
+_ = {}
+
+from textwrap import wrap
+_keys = CONFIGURATION.keys()
+_keys.sort()
+for _key in _keys:
+    _value = CONFIGURATION[_key]
+    if isinstance(_value, str):
+        _val = '"' + _value + '"'
+    else: 
+        _val = str(_value)
+    _lines = wrap(_val + ' (' + type(_value).__name__  + ')', _max_def_len)
+    docstring += """
+    {0:s}  {1:s}""".format(_key.ljust(_max_key_len), _lines[0])
+    for _line in _lines[1:]:
+        docstring += """
+        {0:s}  {1:52s}""".format(''.ljust(_max_key_len), _line)
+    if SETTINGS.get(_key) is None:
+        _[_key] = _value
+docstring += """
+    {0:s}""".format(_table_lines)
+
+if _:
+    SETTINGS.update(_)
+
+def confProDy(*args, **kwargs):
+    """Configure ProDy."""
+
+    if args:
+        if len(args) == 1:
+            return SETTINGS.get(args[0])
+        else:
+            return [SETTINGS.get(option) for option in args]
+
+    for option, value in kwargs.iteritems():    
+        if isinstance(option, str):
+            if option in CONFIGURATION:
+                type_ = type(CONFIGURATION[option])
+                if type(value) == type_:
+                    SETTINGS[option] = value
+                    SETTINGS.save()
+                    LOGGER.debug('ProDy configuration is set: {0:s}={1:s}'
+                                 .format(option, str(value)))
+                else:
+                    raise TypeError('value must be a ' + type_.__name__)
+                    
+            else:
+                raise KeyError("'{0:s}' is not a valid option".format(option))
+        else:
+            raise TypeError('option must be a string')
+
+confProDy.__doc__ += docstring
+
+confProDy.__doc__ += """
+
+    Usage example::
+            
+      confProDy('backup')
+      confProDy('backup', 'backup_ext')
+      confProDy(backup=True, backup_ext='.bak')
+      confProDy(backup_ext='.BAK')"""
+
+class ProDyException(Exception):
+    pass
+
+def plog(*text):
+    """Log *text* using ProDy logger at log level info.  Multiple arguments 
+    are accepted.  Each argument will be converted to string and joined using 
+    a white space as delimiter."""
+    
+    LOGGER.info(' '.join([str(s) for s in text]))
+
+def startLogfile(filename, **kwargs):
+    
+    LOGGER.startLogfile(filename, **kwargs)
+    
+startLogfile.__doc__ = LOGGER.startLogfile.__doc__
+    
+def closeLogfile(filename):
+    """Close logfile with *filename*."""
+
+    LOGGER.closeLogfile(filename)
+
+def setVerbosity(level):
+    """    
+
+    >>> from prody import *
+    >>> setVerbosity('none')
+    >>> plog('test')
+    >>> setVerbosity('debug')
+    >>> plog('test')"""
+
+    LOGGER.setVerbosity(level)
+
+setVerbosity.__doc__ = LOGGER.setVerbosity.__doc__.replace('\n    ', '\n') + \
+                       setVerbosity.__doc__ 
+
+def getVerbosity():
+    """Return ProDy console verbosity level."""
+    
+    return LOGGER.getVerbosity()
+
+getVerbosity.__doc__ = LOGGER.getVerbosity.__doc__
+
+def checkUpdates():
+    """Check PyPI to see if there is a newer ProDy version available.  Setting
+    ProDy configuration parameter *check_updates* to a positive integer will 
+    make ProDy automatically check updates, e.g.::
+       
+      confProDy(check_updates=7) # check at most once a week
+      confProDy(check_updates=0) # do not auto check updates
+      confProDy(check_updates=-1) # check at the start of every session"""
+    
+    import xmlrpclib
+    pypi = xmlrpclib.Server('http://pypi.python.org/pypi')
+    releases = pypi.package_releases('ProDy')
+    if releases[0] == __version__:
+        LOGGER.info('You are using the latest ProDy release (v{0:s}).'
+                    .format(__version__))
+    else:
+        LOGGER.info('ProDy v{0:s} is available, you are using {1:s}.'
+                    .format(releases[0], __version__))
+    if SETTINGS['check_updates']:
+        import time
+        SETTINGS['last_check'] = time.time()
+        SETTINGS.save()
+
+if SETTINGS['check_updates']: 
+    
+    if SETTINGS.get('last_check') is None:
+        SETTINGS['last_check'] = 0
+    import time
+    if ((time.time() - SETTINGS.get('last_check')) / 3600 / 24 > 
+        SETTINGS['check_updates']):
+        LOGGER.info('Checking PyPI for ProDy updates:')
+        checkUpdates()
+
+def test(**kwargs):
+    """Run ProDy tests, ``prody.test()``. See :mod:`prody.tests` 
+    documentation for more details."""
+    
+    if sys.version_info[:2] > (2,6):
+        try:
+            import prody.tests
+        except ImportError:
+            LOGGER.warning('Could not import ProDy unit tests, '
+                           'please check your installation.')
+        else:
+            prody.tests.test(**kwargs)
+    else:
+        LOGGER.warning('ProDy tests are available for Python 2.7') 
+
+__all__ = ['checkUpdates', 'confProDy', 'getVerbosity', 'setVerbosity',
+           'startLogfile', 'closeLogfile', 
+           'plog']
+
+import kdtree
+from kdtree import *
+__all__.extend(kdtree.__all__)
+__all__.append('kdtree')
+
+import atomic 
+from atomic import *
+__all__.extend(atomic.__all__)
+__all__.append('atomic')
+
+from atomic import SELECT
+
+import proteins
+from proteins import *  
+__all__.extend(proteins.__all__)
+__all__.append('proteins')
+
+import measure
+from measure import *
+__all__.extend(measure.__all__)
+__all__.append('measure')
+
+import dynamics
+from dynamics import *
+__all__.extend(dynamics.__all__)
+__all__.append('dynamics')
+
+import ensemble
+from ensemble import *
+__all__.extend(ensemble.__all__)
+__all__.append('ensemble')
+
+import trajectory
+from trajectory import *
+__all__.extend(trajectory.__all__)
+__all__.append('trajectory')
+
+import prody
+__all__.append('prody')
---
+++ packages/ProDy/ProDy-1.0.2.tar.gz/ProDy-1.0.2/prody/dynamics/editing.py
@@ -0,0 +1,385 @@
+# -*- coding: utf-8 -*-
+# ProDy: A Python Package for Protein Dynamics Analysis
+# 
+# Copyright (C) 2010-2012 Ahmet Bakan
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#  
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+"""This module defines functions for editing normal mode data."""
+
+__author__ = 'Ahmet Bakan'
+__copyright__ = 'Copyright (C) 2010-2012 Ahmet Bakan'
+
+import numpy as np
+
+from prody.atomic import Atomic, AtomGroup, AtomMap, AtomSubset
+from prody.atomic import Selection, SELECT
+from prody.tools import importLA
+from prody import LOGGER
+
+from nma import NMA
+from modeset import ModeSet
+from mode import VectorBase, Mode, Vector
+from gnm import GNM
+from anm import ANM
+from pca import PCA
+
+__all__ = ['extendModel', 
+           'sliceMode', 'sliceModel', 'sliceVector',
+           'reduceModel',]
+
+
+
+def extendModel(model, nodes, atoms):
+    """Extend a coarse grained *model* built for *nodes* to *atoms*.  *model*
+    may be :class:`~.ANM`, :class:`~.GNM`, :class:`~.PCA`, or :class:`~.NMA` 
+    instance.  This will take part of the normal modes for each node (i.e. Cα 
+    atoms) and extend it to all other atoms in the same residue.  For each atom
+    in *nodes* argument *atoms* argument must contain a corresponding residue.  
+    Note that modes in the extended model will not be normalized.  For a usage
+    example see :ref:`extendmodel`."""
+    
+    if not isinstance(model, NMA):
+        raise TypeError('model must be an NMA instance')
+    if not isinstance(nodes, Atomic):
+        raise TypeError('nodes must be an Atomic instance')
+    if model.numAtoms() != nodes.numAtoms():
+        raise ValueError('model and nodes must have same number of atoms')
+    
+    if isinstance(atoms, Atomic):
+        is3d = model.is3d()            
+        atom_indices = []
+        indices = []
+        hierview = atoms.getHierView()
+        for i, node in enumerate(nodes):
+            res = hierview[node.getChid(), node.getResnum(), node.getIcode()]
+            if res is None:
+                raise ValueError('hierview must contain a residue for all atoms')
+            atom_indices.append(res.getIndices())
+            if is3d:
+                indices.append(range(i*3, (i+1)*3) * len(res))
+            else:
+                indices.append([i] * len(res))
+        atom_indices = np.concatenate(atom_indices)
+        indices = np.concatenate(indices)
+        
+        array = model.getArray()[indices,:]
+        extended = NMA('Extended ' + str(model))
+        extended.setEigens(array, model.getEigvals())
+        if isinstance(atoms, AtomGroup):
+            ag = atoms
+        else: 
+            ag = atoms.getAtomGroup()
+        atommap = AtomMap(ag, atom_indices, np.arange(len(atom_indices)), 
+                          np.array([]), str(atoms), atoms.getACSIndex())
+        return extended, atommap
+    else:
+        raise TypeError('atoms must be an Atomic instance')
+    
+
+def sliceVector(vector, atoms, select):
+    """Return part of the *vector* for *atoms* matching *select*.  Note that 
+    returned :class:`.Vector` instance is not normalized.
+    
+    :arg vector: vector instance to be sliced
+    :type vector: :class:`.VectorBase`
+    
+    :arg atoms: atoms for which *vector* describes a deformation, motion, etc.
+    :type atoms: :class:`.Atomic`
+    
+    :arg select: an atom selection or a selection string 
+    :type select: :class:`.Selection`, str 
+    
+    :returns: (:class:`.Vector`, :class:`.Selection`)"""
+    
+    if not isinstance(vector, VectorBase):
+        raise TypeError('vector must be a VectorBase instance, not {0:s}'
+                        .format(type(vector)))
+    if not isinstance(atoms, Atomic):
+        raise TypeError('atoms must be an Atomic instance, not {0:s}'
+                        .format(type(atoms)))
+    if atoms.numAtoms() != vector.numAtoms(): 
+        raise ValueError('number of atoms in model and atoms must be equal')
+    
+    if isinstance(select, str):
+        selstr = select
+        if isinstance(atoms, AtomGroup):
+            sel = atoms.select(selstr)
+            which = sel._getIndices()
+        else:
+            which = SELECT.getIndices(atoms, selstr)
+            sel = Selection(atoms.getAtomGroup(), atoms.getIndices()[which],
+                            selstr, atoms.getACSIndex())
+            
+    elif isinstance(select, AtomSubset):
+        sel = select
+        if isinstance(atoms, AtomGroup):
+            if sel.getAtomGroup() != atoms:
+                raise ValueError('select and atoms do not match')
+            which = sel._getIndices()
+        else:
+            if atoms.getAtomGroup() != sel.getAtomGroup():
+                raise ValueError('select and atoms do not match')
+            elif not sel in atoms:
+                raise ValueError('select is not a subset of atoms')
+            idxset = set(atoms._getIndices())
+            which = np.array([idx in idxset for idx in sel._getIndices()])
+            which = which.nonzero()[0]
+        selstr = sel.getSelstr()
+    
+    else:
+        raise TypeError('select must be a string or a Selection instance')
+        
+    vec = Vector(vector.getArrayNx3()[
+                 which, :].flatten(),
+                 '{0:s} slice {1:s}'.format(str(vector), repr(selstr)), 
+                 vector.is3d())
+    return (vec, sel)
+
+
+def sliceMode(mode, atoms, select):
+    """Return part of the *mode* for *atoms* matching *select*.  This works 
+    slightly different from :func:`.sliceVector`. Mode array (eigenvector) is 
+    multiplied by square-root of the variance along the mode.  If mode is from
+    an elastic network model, variance is defined as the inverse of the 
+    eigenvalue.  Note that returned :class:`~.Vector` instance is not 
+    normalized.
+    
+    :arg mode: mode instance to be sliced
+    :type mode: :class:`.Mode`
+    
+    :arg atoms: atoms for which *mode* describes a deformation, motion, etc.
+    :type atoms: :class:`.Atomic`
+    
+    :arg select: an atom selection or a selection string 
+    :type select: :class:`.Selection`, str 
+    
+    :returns: (:class:`~.Vector`, :class:`~.Selection`)"""
+    
+    if not isinstance(mode, Mode):
+        raise TypeError('mode must be a Mode instance, not {0:s}'
+                        .format(type(mode)))
+    if not isinstance(atoms, Atomic):
+        raise TypeError('atoms must be an Atomic instance, not {0:s}'
+                        .format(type(atoms)))
+    if atoms.numAtoms() != mode.numAtoms(): 
+        raise ValueError('number of atoms in model and atoms must be equal')
+    
+    if isinstance(select, str):
+        selstr = select
+        if isinstance(atoms, AtomGroup):
+            sel = atoms.select(selstr)
+            which = sel._getIndices()
+        else:
+            which = SELECT.getIndices(atoms, selstr)
+            sel = Selection(atoms.getAtomGroup(), atoms.getIndices()[which],
+                            selstr, atoms.getACSIndex())
+        
+    elif isinstance(select, AtomSubset):
+        sel = select
+        if isinstance(atoms, AtomGroup):
+            if sel.getAtomGroup() != atoms:
+                raise ValueError('select and atoms do not match')
+            which = sel._getIndices()
+        else:
+            if atoms.getAtomGroup() != sel.getAtomGroup():
+                raise ValueError('select and atoms do not match')
+            elif not sel in atoms:
+                raise ValueError('select is not a subset of atoms')
+            idxset = set(atoms._getIndices())
+            which = np.array([idx in idxset for idx in sel._getIndices()])
+            which = which.nonzero()[0]
+        selstr = sel.getSelstr()
+    
+    else:
+        raise TypeError('select must be a string or a Selection instance')
+    
+    vec = Vector(mode.getArrayNx3()[
+                 which,:].flatten() * mode.getVariance()**0.5,
+                 '{0:s} slice {1:s}'.format(str(mode), repr(selstr)), 
+                 mode.is3d()) 
+    return (vec, sel)
+
+
+def sliceModel(model, atoms, select):
+    """Return a part of the *model* for *atoms* matching *select*.  Note that 
+    normal modes (eigenvectors) are not normalized.
+    
+    :arg mode: NMA model instance to be sliced
+    :type mode: :class:`.NMA`
+    
+    :arg atoms: atoms for which the *model* was built
+    :type atoms: :class:`.Atomic`
+    
+    :arg select: an atom selection or a selection string 
+    :type select: :class:`.Selection`, str 
+    
+    :returns: (:class:`.NMA`, :class:`.Selection`)"""
+    
+    if not isinstance(model, NMA):
+        raise TypeError('mode must be a NMA instance, not {0:s}'
+                        .format(type(model)))
+    if not isinstance(atoms, Atomic):
+        raise TypeError('atoms must be an Atomic instance, not {0:s}'
+                        .format(type(atoms)))
+    if atoms.numAtoms() != model.numAtoms(): 
+        raise ValueError('number of atoms in model and atoms must be equal')
+    
+    array = model._getArray()
+    
+    if isinstance(select, str):
+        selstr = select
+        if isinstance(atoms, AtomGroup):
+            sel = atoms.select(selstr)
+            which = sel.getIndices()
+        else:
+            which = SELECT.getIndices(atoms, selstr)
+            sel = Selection(atoms.getAtomGroup(), atoms.getIndices()[which],
+                            selstr, atoms.getACSIndex())
+        
+    elif isinstance(select, AtomSubset):
+        sel = select
+        if isinstance(atoms, AtomGroup):
+            if sel.getAtomGroup() != atoms:
+                raise ValueError('select and atoms do not match')
+            which = sel._getIndices()
+        else:
+            if atoms.getAtomGroup() != sel.getAtomGroup():
+                raise ValueError('select and atoms do not match')
+            elif not sel in atoms:
+                raise ValueError('select is not a subset of atoms')
+            idxset = set(atoms._getIndices())
+            which = np.array([idx in idxset for idx in sel._getIndices()])
+            which = which.nonzero()[0]
+        selstr = sel.getSelstr()
+    
+    else:
+        raise TypeError('select must be a string or a Selection instance')
+        
+    nma = type(model)('{0:s} slice {1:s}'
+                      .format(model.getTitle(), repr(selstr)))
+    if model.is3d():
+        which = [which.reshape((len(which),1))*3]
+        which.append(which[0]+1)
+        which.append(which[0]+2)
+        which = np.concatenate(which, 1).flatten()
+    nma.setEigens( array[which, :], model.getEigvals() )
+    return (nma, sel)
+    
+    
+def reduceModel(model, atoms, select):
+    """Return reduced NMA model.  Reduces a :class:`~.NMA` model to a subset of 
+    *atoms* matching *select*.  This function behaves differently depending on 
+    the type of the *model* argument.  For :class:`.ANM` and :class:`.GNM` or 
+    other :class:`.NMA` models, force constant matrix for system of interest 
+    (specified by the *select*) is derived from the force constant matrix for 
+    the *model* by assuming that for any given displacement of the system of 
+    interest, other atoms move along in such a way as to minimize the potential
+    energy.  This is based on the formulation in [KH00]_.  For :class:`.PCA` 
+    models, this function simply takes the sub-covariance matrix for selection.
+
+    :arg model: dynamics model
+    :type model: :class:`.ANM`, :class:`.GNM`, or :class:`.PCA`
+    
+    :arg atoms: atoms that were used to build the model
+    :type atoms: :class:`.Atomic`
+    
+    :arg select: an atom selection or a selection string 
+    :type select: :class:`.Selection`, str 
+    
+    :returns: (:class:`.NMA`, :class:`.Selection`)"""
+    
+    linalg = importLA()
+
+    if not isinstance(model, NMA):
+        raise TypeError('model must be an NMA instance, not {0:s}'
+                        .format(type(model)))
+    if not isinstance(atoms, (AtomGroup, AtomSubset, AtomMap)):
+        raise TypeError('atoms type is not valid')
+    if len(atoms) <= 1:
+        raise TypeError('atoms must contain more than 1 atoms')
+
+    if isinstance(model, GNM):
+        matrix = model._kirchhoff
+    elif isinstance(model, ANM):
+        matrix = model._hessian
+    elif isinstance(model, PCA):
+        matrix = model._cov
+    else:
+        raise TypeError('model does not have a valid type derived from NMA')
+    if matrix is None:
+        raise ValueError('model matrix (Hessian/Kirchhoff/Covariance) is not '
+                         'built')
+
+    if isinstance(select, str):
+        system = SELECT.getBoolArray(atoms, select)
+        n_sel = sum(system)
+        if n_sel == 0:
+            raise ValueError('select matches 0 atoms')
+        if len(atoms) == n_sel:
+            raise ValueError('select matches all atoms')
+
+        if isinstance(atoms, AtomGroup):
+            ag = atoms
+            which = np.arange(len(atoms))[system]
+        else:
+            ag = atoms.getAtomGroup()
+            which = atoms._getIndices()[system]
+        sel = Selection(ag, which, select, atoms.getACSIndex())
+        
+    elif isinstance(select, AtomSubset):
+        sel = select
+        if isinstance(atoms, AtomGroup):
+            if sel.getAtomGroup() != atoms:
+                raise ValueError('select and atoms do not match')
+            system = np.zeros(len(atoms), bool)
+            system[sel._getIndices()] = True 
+        else:
+            if atoms.getAtomGroup() != sel.getAtomGroup():
+                raise ValueError('select and atoms do not match')
+            elif not sel in atoms:
+                raise ValueError('select is not a subset of atoms')
+            idxset = set(atoms._getIndices())
+            system = np.array([idx in idxset for idx in sel._getIndices()])
+    
+    else:
+        raise TypeError('select must be a string or a Selection instance')
+    
+    other = np.invert(system)
+
+    if model.is3d():
+        system = np.tile(system, (3,1)).transpose().flatten()
+        other = np.tile(other, (3,1)).transpose().flatten()
+    ss = matrix[system,:][:,system]
+    if isinstance(model, PCA):
+        eda = PCA(model.getTitle() + ' reduced')
+        eda.setCovariance(ss)
+        return eda, system
+    so = matrix[system,:][:,other]
+    os = matrix[other,:][:,system]
+    oo = matrix[other,:][:,other]
+    matrix = ss - np.dot(so, np.dot(linalg.inv(oo), os))
+    
+    if isinstance(model, GNM):
+        gnm = GNM(model.getTitle() + ' reduced')
+        gnm.setKirchhoff(matrix)
+        return gnm, sel
+    elif isinstance(model, ANM):
+        anm = ANM(model.getTitle() + ' reduced')
+        anm.setHessian(matrix)
+        return anm, sel
+    elif isinstance(model, PCA):
+        eda = PCA(model.getTitle() + ' reduced')
+        eda.setCovariance(matrix)
+        return eda, sel
