cea5cb3f1b6b62ef00405620955406ba5f285cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:34 +0000

    Add package maplot/1.0.0/maplot-1.0.0.tar.gz

---
+++ packages/maplot/maplot-1.0.0.tar.gz/maplot-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        name             =   'maplot',
+        version          =   '1.0.0',
+        description      =   'Python package for creating interactive MA-plots',
+        long_description =   open('README').read(),
+        license          =   'GNU General Public License 3.0',
+        url              =   'http://delafont.github.com/maplot',
+        author           =   'Julien Delafontaine',
+        author_email     =   'julien.delafontaine@yandex.com',
+        classifiers      =   ['Topic :: Scientific/Engineering :: Bio-Informatics'],
+        packages         =   ['maplot',],
+    )
