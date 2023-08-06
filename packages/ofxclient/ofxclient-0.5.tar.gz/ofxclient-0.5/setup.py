bb327d3d6e76422758451ef06538f010753df509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:35 +0000

    Add package ofxclient/0.5/ofxclient-0.5.tar.gz

---
+++ packages/ofxclient/ofxclient-0.5.tar.gz/ofxclient-0.5/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(name='ofxclient',
+      version=0.5,
+      description="OFX client for dowloading transactions from banks",
+      long_description=open("./README.md", "r").read(),
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          ],
+      keywords='ofx, Open Financial Exchange, download transactions',
+      author='David Bartle',
+      author_email='captindave@gmail.com',
+      url='https://github.com/captin411/ofxclient',
+      license='MIT License',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      scripts=['scripts/ofxclientd'],
+      install_requires=[
+          "ofxhome",
+          "ofxparse>0.8",
+          "simplejson",
+          "BeautifulSoup>=3.0",
+          "cherrypy",
+          "mako"
+      ],
+      test_suite='tests',
+      entry_points="""
+      """,
+      )
