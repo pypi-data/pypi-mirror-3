8493f649983758e503365c4bd75e48b37e84699b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:06 +0000

    Add package TowelStuffPoskok/0.1.0/TowelStuffPoskok-0.1.0.zip

---
+++ packages/TowelStuffPoskok/TowelStuffPoskok-0.1.0.zip/TowelStuffPoskok-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='TowelStuffPoskok',
+    version='0.1.0',
+    author='Hrvoje Jednakovic',
+    author_email='hrvoje@jednakovic.com',
+    packages=['towelstuff', 'towelstuff.test'],
+    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description='Useful towel-related stuff.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.1.1",
+        "caldav == 0.1.4",
+    ],
+)
