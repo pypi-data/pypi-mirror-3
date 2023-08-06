85147de586c69c7c9bbc332124ce7c0d6bc35831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:07 +0000

    Add package rosinstall/0.5.30/rosinstall-0.5.30.tar.gz

---
+++ packages/rosinstall/rosinstall-0.5.30.tar.gz/rosinstall-0.5.30/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+try:
+    import yaml
+except:
+    raise SystemExit("rosinstall requires python-yaml. Please install python-yaml. On debian systems sudo apt-get install python-yaml.")
+
+
+setup(name='rosinstall',
+      version= '0.5.30',
+      packages=['rosinstall'],
+      package_dir = {'':'src'},
+      install_requires = ['vcstools'],
+      scripts = ["scripts/rosinstall", "scripts/roslocate", "scripts/rosws", "scripts/rosco"],
+      author = "Tully Foote", 
+      author_email = "tfoote@willowgarage.com",
+      url = "http://www.ros.org/wiki/rosinstall",
+      download_url = "http://pr.willowgarage.com/downloads/rosinstall/", 
+      keywords = ["ROS"],
+      classifiers = [
+        "Programming Language :: Python", 
+        "License :: OSI Approved :: BSD License" ],
+      description = "The installer for ROS", 
+      long_description = """\
+The installer for ROS
+""",
+      license = "BSD"
+      )
