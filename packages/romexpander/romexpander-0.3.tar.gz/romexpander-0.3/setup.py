d919276bf29ce4e082da1288fe1e0db7ab3489bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:52 +0000

    Add package romexpander/0.3/romexpander-0.3.tar.gz

---
+++ packages/romexpander/romexpander-0.3.tar.gz/romexpander-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="romexpander",
+      version="0.3",
+      author="Devon Meunier",
+      author_email="devon.meunier@utoronto.ca",
+      license="MIT",
+      scripts=["romexpander"],
+      description="NES ROM Expansion script compatible with DvD's" \
+                  "ROM Expander Pro.",
+)
