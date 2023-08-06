5a17f0f33e25782ba3daa5513459175da551d318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:33 +0000

    Add package carrier/0.1/carrier-0.1.tar.gz

---
+++ packages/carrier/carrier-0.1.tar.gz/carrier-0.1/carrier/core.py
@@ -0,0 +1,66 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+import logging
+import logging.config
+import os
+import time
+
+import forklift
+import redis
+import requests
+import xmlrpc2.client
+
+from apscheduler.scheduler import Scheduler
+
+from .config import Config, defaults
+from .processor import Processor
+
+
+logger = logging.getLogger(__name__)
+
+
+class Conveyor(object):
+
+    def __init__(self, *args, **kwargs):
+        super(Conveyor, self).__init__(*args, **kwargs)
+
+        # Get configuration
+        self.config = Config(os.getcwd())
+        self.config.from_object(defaults)
+
+        if "CONVEYOR_CONF" in os.environ:
+            self.config.from_envvar("CONVEYOR_CONF")
+
+        # Initalize app
+        logging.config.dictConfig(self.config["LOGGING"])
+
+        store = redis.StrictRedis(**dict([(k.lower(), v) for k, v in self.config["REDIS"].items()]))
+
+        wsession = requests.session(auth=(
+                        self.config["WAREHOUSE_AUTH"]["USERNAME"],
+                        self.config["WAREHOUSE_AUTH"]["PASSWORD"],
+                    ))
+        warehouse = forklift.Forklift(session=wsession)
+
+        psession = requests.session(verify=self.config["PYPI_SSL_VERIFY"])
+        ptransports = [xmlrpc2.client.HTTPTransport(session=psession), xmlrpc2.client.HTTPSTransport(session=psession)]
+        pypi = xmlrpc2.client.Client(self.config["PYPI_URI"], transports=ptransports)
+
+        self.processor = Processor(warehouse, pypi, store)
+
+    def run(self):
+        scheduler = Scheduler()
+
+        if self.config["SCHEDULE"].get("packages") is not None:
+            scheduler.add_interval_job(self.processor.process, **self.config["SCHEDULE"]["packages"])
+
+        scheduler.start()
+
+        try:
+            while True:
+                time.sleep(999)
+        except KeyboardInterrupt:
+            logger.info("Shutting down Conveyor...")
+            scheduler.shutdown(wait=False)
---
+++ packages/carrier/carrier-0.1.tar.gz/carrier-0.1/carrier/tasks/bulk.py
@@ -0,0 +1,52 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+import datetime
+import logging
+import time
+
+from requests.exceptions import ConnectionError, HTTPError
+
+from ..core import Conveyor
+
+
+logger = logging.getLogger(__name__)
+
+
+# We ignore the last component as we cannot properly handle it
+def get_jobs(last=0):
+    current = time.mktime(datetime.datetime.utcnow().timetuple())
+
+    print "Current time is", current
+
+    app = Conveyor()
+
+    for package in set(app.processor.pypi.list_packages()):
+        yield package
+
+
+def handle_job(name):
+    try:
+        tried = 0
+        delay = 1
+
+        while True:
+            try:
+                tried += 1
+
+                app = Conveyor()
+                app.processor.update(name)
+
+                break
+            except (ConnectionError, HTTPError):
+                # Attempt to process again if we have a connection error
+                if tried >= 10:  # Try a max of 10 times
+                    raise
+                else:
+                    # Wait a moment
+                    time.sleep(delay)
+                    delay = delay * 2
+    except Exception as e:
+        logger.exception(str(e))
+        raise
