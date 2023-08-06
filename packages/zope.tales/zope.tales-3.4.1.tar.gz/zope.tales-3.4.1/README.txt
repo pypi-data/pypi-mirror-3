26adbb50dce8f724a5a3865422cfbaacfaffadbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:18 +0000

    Add package zope.tales/3.4.1/zope.tales-3.4.1.tar.gz

---
+++ packages/zope.tales/zope.tales-3.4.1.tar.gz/zope.tales-3.4.1/README.txt
@@ -0,0 +1,3 @@
+Template Attribute Language - Expression Syntax
+
+See http://www.zope.org/Wikis/DevSite/Projects/ZPT/TALES%20Specification%201.3
---
+++ packages/zope.tales/zope.tales-3.4.1.tar.gz/zope.tales-3.4.1/setup.py
@@ -0,0 +1,60 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.tales package
+
+$Id: setup.py 126469 2012-05-23 15:22:12Z tseaver $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.tales',
+      version = '3.4.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      description='Zope 3 Template Application Language Expression Syntax '
+                  '(TALES)',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 template xml tales",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.tales',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      extras_require = dict(
+          test=['zope.testing',
+                ]),
+      install_requires=[
+          'setuptools',
+          'zope.interface',
+          'zope.tal'],
+      include_package_data = True,
+      zip_safe = False,
+      )
