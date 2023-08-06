4445a0a4ccb1dd42be6873311ccfd96244e81f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:19 +0000

    Add package azure/0.1/azure-0.1.zip

---
+++ packages/azure/azure-0.1.zip/azure-0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+#------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. 
+#
+# This source code is subject to terms and conditions of the Apache License, 
+# Version 2.0. A copy of the license can be found in the License.html file at 
+# the root of this distribution. If you cannot locate the Apache License, 
+# Version 2.0, please send an email to vspython@microsoft.com. By using this 
+# source code in any fashion, you are agreeing to be bound by the terms of the 
+# Apache License, Version 2.0.
+#
+# You must not remove this notice, or any other, from this software.
+#------------------------------------------------------------------------------
+
+from distutils.core import setup
+
+setup(name='azure',
+      version='0.1',
+      description='Windows Azure client APIs',
+      url='https://github.com/WindowsAzure/azure-sdk-for-python',
+      author='Microsoft',
+      author_email='dinov@microsoft.com',
+      packages=['azure',
+                #'azure.http',
+                #'azure.servicebus',
+                #'azure.storage',
+                ]
+     )
