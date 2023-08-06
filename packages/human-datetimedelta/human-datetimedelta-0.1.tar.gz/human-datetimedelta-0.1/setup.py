1ebbf126a042025539e00f4c7b858b7e3615a41d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:04 +0000

    Add package human-datetimedelta/0.1/human-datetimedelta-0.1.tar.gz

---
+++ packages/human-datetimedelta/human-datetimedelta-0.1.tar.gz/human-datetimedelta-0.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+'''
+  from datetime import datetime, timedelta
+
+  now = datetime.now()
+  an_hour_ago = now - timedelta(hours=1)
+  yesterday = now - timedelta(days=1)
+  tomorrow = now + timedelta(days=1)
+
+  import human-datetimedelta as human
+
+  print human.date(now)                      # 'now'
+  print human.date(an_hour_ago)              # 'an hour ago'
+  print human.date(an_hour_ago, short=True)  # '1h ago'
+  print human.date(an_hour_ago, asdays=True) # 'today'
+  print human.date(yesterday, short=True)    # 'yest'
+  print human.date(tomorrow)                 # 'tomorrow'
+
+'''
+from distutils.core import setup
+
+setup(
+    name        = 'human-datetimedelta',
+    version     = '0.1',
+    description = 'Human readable datetime deltas',
+    long_description=__doc__,
+    author      = 'Col Wilson',
+    author_email= 'tersecol@gmail.com',
+    license     = 'MIT License',
+    platforms   = 'any',
+    url         = 'https://github.com/colwilson/human-datetimedelta/',
+    packages    = ['human-datetimedelta'],
+    requires    = ['datetime'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
