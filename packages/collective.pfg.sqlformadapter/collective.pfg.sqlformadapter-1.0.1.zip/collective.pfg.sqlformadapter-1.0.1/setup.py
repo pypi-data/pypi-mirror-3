0406ccad4802abd6e7fbb5ddb475c50d48aad6f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:31 +0000

    Add package collective.pfg.sqlformadapter/1.0.1/collective.pfg.sqlformadapter-1.0.1.zip

---
+++ packages/collective.pfg.sqlformadapter/collective.pfg.sqlformadapter-1.0.1.zip/collective.pfg.sqlformadapter-1.0.1/setup.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of collective.pfg.sqlformadapter
+"""
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.0.1'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('collective', 'pfg', 'sqlformadapter', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' +
+    'Download\n'
+    '********\n')
+
+setup(name='collective.pfg.sqlformadapter',
+      version=version,
+      description="Product to insert values from the forms into msSQL",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='https://github.com/ianderso/collective.pfg.sqlformadapter',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.pfg'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        # -*- Extra requirements: -*-
+                        'Products.PloneFormGen',
+                        'pymssql'
+                        ],
+
+      extras_require={'tests':['plone.app.testing','Products.PloneFormGen','pymssql']},
+      test_suite='collective.pfg.sqlformadapter.tests.test_docs.test_suite',
+      entry_points="""
+      # -*- entry_points -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
