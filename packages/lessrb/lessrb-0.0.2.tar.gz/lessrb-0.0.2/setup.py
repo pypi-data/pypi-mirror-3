efe27ca7ad302fad1ad56936f4cc5f6ad6b8633d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:33 +0000

    Add package lessrb/0.0.2/lessrb-0.0.2.tar.gz

---
+++ packages/lessrb/lessrb-0.0.2.tar.gz/lessrb-0.0.2/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -
+#
+
+
+import os
+from setuptools import setup, find_packages
+import sys
+
+
+setup(
+    name="lessrb",
+    version="0.0.2",
+    description="Wrapper for ruby less so that it's in a virtualenv.",
+    author="Michael Axiak",
+    author_email="mike@axiak.net",
+    license="Apache 2",
+    include_package_data=True,
+    zip_safe=False,
+    packages=['lessrb'],
+    entry_points={
+        'console_scripts':
+            ['lessc=lessrb:run'],
+        }
+    )
