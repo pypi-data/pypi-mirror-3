72250fdc2246c9ac1091783a826da6b6cd1d0ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:23 +0000

    Add package Pykaboo/0.1.6/Pykaboo-0.1.6.tar.gz

---
+++ packages/Pykaboo/Pykaboo-0.1.6.tar.gz/Pykaboo-0.1.6/setup.py
@@ -0,0 +1,30 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='Pykaboo',
+    version='0.1.6',
+    author='Robrecht De Rouck',
+    author_email='Robrecht.De.Rouck@gmail.com',
+    packages=['pykaboo', ],
+    #include_package_data=True,
+    #scripts= None, #'pykaboo/__init__.py',
+    url='http://pykaboo.herokuapp.com',
+    license='Modified BSD license',
+    package_data={'': ['pykaboo_style.css']},
+    #data_files=[('pykaboo',['pykaboo/pykaboo_style.css'])],
+    entry_points={
+    'console_scripts': ['pykaboo = pykaboo:main', ],},
+    description='A python source viewer.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "pygments >= 1.5", "argparse >= 1.2.1", "distribute"
+    ],
+    classifiers=[
+        "Environment :: Console",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+    ],
+)
+
+#entry_points="""[console_scripts]pykaboo = pykaboo:main"""
