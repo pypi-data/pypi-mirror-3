e177f16f0d77ff74388e27d840a8e03272822976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:08 +0000

    Add package json_utils/0.2/json_utils-0.2.tar.gz

---
+++ packages/json_utils/json_utils-0.2.tar.gz/json_utils-0.2/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='json_utils', version='0.2',
+      description='Some basic JSON utils.',
+      author='Gustaf Sjöberg', author_email='gs@distrop.com',
+      maintainer='Tom Najdek', maintainer_email='tom@doppnet.com',
+      url='https://github.com/tnajdek/django-json-utils',
+      packages=['json_utils'])
