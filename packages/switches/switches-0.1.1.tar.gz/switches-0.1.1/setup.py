375d9c3032a07a99419a8e934b6cfd1a2def6554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:32 +0000

    Add package switches/0.1.1/switches-0.1.1.tar.gz

---
+++ packages/switches/switches-0.1.1.tar.gz/switches-0.1.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+settings = dict()
+
+# Publish
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+settings.update(
+    name='switches',
+    version='0.1.1',
+    description='Friendly Command Line Scripts',
+    long_description=open('README.rst').read(),
+    author='Andy Dirnberger',
+    author_email='dirn@dirnonline.com',
+    url='https://github.com/dirn/switches',
+    py_modules=['switches'],
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    license=open('LICENSE').read(),
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+)
+
+setup(**settings)
