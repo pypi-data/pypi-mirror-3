7d058ebe3746b14a53ac9cce499bb54264b910f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:33 +0000

    Add package accountsSSO/0.0.5/accountsSSO-0.0.5.tar.gz

---
+++ packages/accountsSSO/accountsSSO-0.0.5.tar.gz/accountsSSO-0.0.5/signon/global.h
@@ -0,0 +1,16 @@
+#undef QT_NO_STL
+#undef QT_NO_STL_WCHAR
+
+#ifndef NULL
+#define NULL    0
+#endif
+
+#include "pyside_global.h"
+
+#include <authservice.h>
+#include <authsession.h>
+#include <indentity.h>
+#include <sessiondata.h>
+#include <signon.h>
+#include <identityinfo.h>
+#include <error.h>
