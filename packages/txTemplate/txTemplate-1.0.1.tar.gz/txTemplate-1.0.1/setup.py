bd9d27e792d9370dd55a0ccb12e784b71c13472e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:47 +0000

    Add package txTemplate/1.0.1/txTemplate-1.0.1.tar.gz

---
+++ packages/txTemplate/txTemplate-1.0.1.tar.gz/txTemplate-1.0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(name='txTemplate',
+      version='1.0.1',
+      description='Twisted Adapters for Templating Engines',
+      long_description=open("README.rst", "r").read(),
+      author='Mike Steder',
+      author_email='steder@gmail.com',
+      url='http://github.com/steder/txtemplate',
+      packages=['txtemplate',
+                'txtemplate.test'],
+      test_suite="txtemplate.test",
+      install_requires=["genshi",
+                        "jinja2",
+                        "twisted"],
+      license="MIT",
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          ]
+)
