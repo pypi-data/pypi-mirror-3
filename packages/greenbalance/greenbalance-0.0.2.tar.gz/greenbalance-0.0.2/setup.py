534fff9ab2126a66d142da7668ae4d5101b0d90b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:46 +0000

    Add package greenbalance/0.0.2/greenbalance-0.0.2.tar.gz

---
+++ packages/greenbalance/greenbalance-0.0.2.tar.gz/greenbalance-0.0.2/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import os
+from distutils.core import setup
+
+homedir = os.path.expanduser('~')
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    data_files = [('', ['README.rst']),
+                  ('/etc', ['greenbalance.conf']),],
+    name = 'greenbalance',
+    version = '0.0.2',
+    url = 'https://github.com/waawal/green-balance',
+    description = 'Weighted Random Loadbalancer for TCP.',
+    long_description = long_description,
+    author = 'Daniel Waardal',
+    author_email = 'waawal@boom.ws',
+    license = 'gpl',
+    platforms = 'any',
+    scripts=['bin/greenbalance'],
+    py_modules = [
+        'greenbalance'
+    ],
+    install_requires = [
+        'wr',
+        'gevent'
+    ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Communications',
+    ],
+)
