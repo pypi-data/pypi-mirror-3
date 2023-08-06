161582a697877579cfaf87f7c875d0d1a4af891d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:22 +0000

    Add package tkbd/0.2.1-3-g4362/tkbd-0.2.1-3-g4362.tar.gz

---
+++ packages/tkbd/tkbd-0.2.1-3-g4362.tar.gz/tkbd-0.2.1-3-g4362/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# vim: et:sta:bs=2:sw=4:
+
+from setuptools import setup, find_packages, findall
+from get_git_version import get_git_version
+import os.path
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+setup(name='tkbd',
+    version=get_git_version(),
+    description='Terminal Kamer Bezetting Daemon voor de faculteit NWI '+
+                    'van de Radboud Universiteit',
+    author='Bas Westerbaan',
+    author_email='bas@westerbaan.name',
+    url='http://github.com/bwesterb/tkbd',
+    packages=['tkbd'],
+    zip_safe=False,
+    package_dir={'tkbd': 'src'},
+    package_data={
+        'tkbd': [f for f in findall('src') if f.endswith('.mirte')]},
+    install_requires = [
+            'docutils>=0.3',
+            'mirte>=0.1.0a3',
+            'sarah>=0.1.1',
+            'msgpack-python>=0.1.10',
+            'joyce>=0.1.1'],
+    )
