4c826c0a7771be1af838d2466122fffea9d78489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:33 +0000

    Add package chain/1.0/chain-1.0.tar.gz

---
+++ packages/chain/chain-1.0.tar.gz/chain-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='chain',
+      version='1.0',
+      description='A jQuery like list wrapper.',
+      author='Hao Deng',
+      author_email='denghao8888@gmail.com',
+      py_modules=['chain'],
+      )
