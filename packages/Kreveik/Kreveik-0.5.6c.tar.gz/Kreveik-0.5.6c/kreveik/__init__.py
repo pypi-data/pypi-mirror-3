3eaa79da6c540b199db73827627fb43f3ebedf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:18 +0000

    Add package Kreveik/0.5.6c/Kreveik-0.5.6c.tar.gz

---
+++ packages/Kreveik/Kreveik-0.5.6c.tar.gz/Kreveik-0.5.6c/kreveik/__init__.py
@@ -0,0 +1,19 @@
+"""
+Kreveik is a package that is meant to provide access to a set of tools 
+about 
+"""
+
+import classes
+import numpy as num
+import matplotlib.pyplot as plt
+import network
+import probes
+import copy
+import genetic
+import family
+import parallel
+import logging
+logging.basicConfig(level=logging.INFO)
+
+__all__ = ['classes','family','genetic','network','probes']
+
