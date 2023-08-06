97186e24a56b633f8ca86ab36d1ae505b12a63f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:21 +0000

    Add package django-ccpages/0.1.1/django-ccpages-0.1.1.tar.gz

---
+++ packages/django-ccpages/django-ccpages-0.1.1.tar.gz/django-ccpages-0.1.1/ccpages/forms.py
@@ -0,0 +1,34 @@
+import hashlib
+from django import forms
+from mptt.forms import TreeNodeChoiceField
+from mptt.admin import MPTTAdminForm
+from ccpages.models import Page
+
+
+class PageAdminForm(MPTTAdminForm):
+
+    class Meta:
+        model = Page
+
+    class Media:
+        css = {
+                'screen': ('ccpages/css/admin.css',)
+            }
+
+class PagePasswordForm(forms.Form):
+
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    def __init__(self, *args, **kwargs):
+        self.page = kwargs.pop('page', None)
+        super(PagePasswordForm, self).__init__(*args, **kwargs)
+
+
+    def clean_password(self):
+        page_hash = self.page.hash
+        password = self.cleaned_data['password']
+        user_hash = hashlib.sha1(password).hexdigest()
+        if user_hash == page_hash:
+            self.hash = user_hash
+            return password
+        raise forms.ValidationError('Incorrect password')
