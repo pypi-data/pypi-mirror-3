dd107613049184ed0ceb622bf1585093bdb98f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:17 +0000

    Add package datetime2/0.3.1/datetime2-0.3.1.zip

---
+++ packages/datetime2/datetime2-0.3.1.zip/datetime2-0.3.1/datetime2/__init__.py
@@ -0,0 +1,208 @@
+# datetime2 package main file
+
+# Copyright (c) 2011 Francesco Ricciardi
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, 
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name(s) of the copyright holders nor the names of its
+#   contributors may be used to endorse or promote products derived from this
+#   software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AS IS AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+# EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import re
+
+__author__ = 'Francesco Ricciardi <francescor2010 at yahoo.it>'
+
+
+import re
+
+from math import floor
+from functools import total_ordering
+
+import datetime2.osinterface
+import datetime2.gregorian
+
+
+__all__ = ['Date', 'TimeDelta']
+
+
+##############################################################################
+# support classes and functions
+#
+def constructor_wrapper(meth, wrapping_class, attr_to_set, method_to_call):
+    def new_method(*args):
+        ret_obj = meth(*args)
+        if hasattr(ret_obj, method_to_call):
+            wrapping_obj = wrapping_class(getattr(ret_obj, method_to_call)())
+            setattr(wrapping_obj, attr_to_set, ret_obj)
+            return wrapping_obj
+        else:
+            return ret_obj
+    return new_method
+
+class DynamicAttributeProxy:
+    def __init__(self, proxying_class, attr_to_set, proxied_class, meth_to_call):
+        self.proxying_class = proxying_class
+        self.attr_to_set = attr_to_set
+        self.proxied_class = proxied_class
+        self.meth_to_call = meth_to_call
+
+    def __call__(self, *args, **kwargs):
+        retobj = self.proxied_class(*args, **kwargs)
+        proxying_obj = self.proxying_class(getattr(retobj, self.meth_to_call)())
+        setattr(proxying_obj, self.attr_to_set, retobj)
+        return proxying_obj
+
+    def __getattr__(self, name):
+        attr_obj = getattr(self.proxied_class, name)
+        if callable(attr_obj):
+            return constructor_wrapper(attr_obj, self.proxying_class, self.attr_to_set, self.meth_to_call)
+        else:
+            return attr_obj
+
+class DynamicAtributeMeta(type):
+    """ We need a metaclass because we want to redefine how core classes will
+    find the calendars or time representations they need using the attribute
+    notation.
+    """
+    def __new__(mcs, name, bases, classdict):
+        """Check at creation time that class defines the following
+        attributes:
+          - dynamic_attrs : the dictionary that contains or will contain the
+            dynamic class constructors and methods
+          - method_to_call : name of the method that has to be called to
+            retrieve the value to be set in the proxing cls
+        """
+        #TODO add check that values are identifiers
+        if ("dynamic_attrs" in classdict and
+            "method_to_call" in classdict):
+            return type.__new__(mcs, name, bases, classdict)
+        else:
+            raise NotImplementedError("Classes of DynamicDatetimeMeta type should have two attributes.")
+
+    def __getattr__(cls, name):
+        if name in cls.dynamic_attrs:
+            return DynamicAttributeProxy(cls, name, cls.dynamic_attrs[name], cls.method_to_call)
+        else:
+            raise AttributeError("type object '{}' has no attribute '{}'".format(cls.__name__, name))
+
+class DynamicAttrsCls:
+    def __getattr__(self, name):
+        if name in self.dynamic_attrs:
+            attr_obj = self.dynamic_attrs[name].from_rata_die(self.day_count)
+            setattr(self, name, attr_obj)
+            return attr_obj
+        else:
+            raise AttributeError("'{}' object has no attribute '{}'".format(self.__class__.__name__, name))
+
+
+##############################################################################
+# interface with calendars and time representations
+#
+_identifier_restricted_re = re.compile("[_a-zA-Z]\\w*")
+_datetime2_calendars = {}
+
+def register_calendar(name, calendar):
+    if not _identifier_restricted_re.match(name):
+        raise RuntimeError("Calendar must have a valid name.")
+    if not hasattr(calendar, 'to_rata_die') or not callable(getattr(calendar, 'to_rata_die')):
+        raise RuntimeError("Calendar must have a callable 'to_rata_die' function.")
+    if not hasattr(calendar, 'from_rata_die') or not callable(getattr(calendar, 'from_rata_die')):
+        raise RuntimeError("Calendar must have a callable 'to_rata_die' function.")
+    _datetime2_calendars[name] = calendar
+
+#TODO: add code to manage the calendars (list, check if present, etc)
+
+register_calendar('gregorian', datetime2.gregorian.GregorianCalendar)
+
+
+##############################################################################
+# Core classes
+#
+
+class TimeDelta:
+    # This is a stub in version 0.2
+    def __init__(self, days):
+        self._days = days
+        
+    def __repr__(self):
+        return "TimeDelta({})".format(self.days)
+    
+    @property
+    def days(self):
+        return self._days
+    
+    def __add__(self, other):
+        raise TypeError   # required to let Date tests pass
+
+@total_ordering
+class Date(DynamicAttrsCls, metaclass=DynamicAtributeMeta):
+    dynamic_attrs = _datetime2_calendars
+    method_to_call = "to_rata_die"
+
+    def __init__(self, day_count):
+        if isinstance(day_count, int):
+            self._day_count = day_count
+        else:
+            raise TypeError("an integer is required")
+
+    @classmethod
+    def today(cls):
+        return cls(floor(datetime2.osinterface.get_moment()))
+
+    @property
+    def day_count(self):
+        return self._day_count
+    
+    def __repr__(self):
+        return "Date({})".format(self.day_count)
+    
+    def __str__(self):
+        return "R.D. {}".format(self.day_count)
+    
+    def __add__(self, other):
+        if isinstance(other, TimeDelta):
+            return Date(self.day_count + other.days)
+        else:
+            return NotImplemented
+        
+    def __radd__(self, other):
+        return self + other
+    
+    def __sub__(self, other):
+        if isinstance(other, Date):
+            return TimeDelta(self.day_count - other.day_count)
+        elif isinstance(other, TimeDelta):
+            return Date(self.day_count - other.days)
+        else:
+            return NotImplemented
+        
+    # TODO: undestand if we want to use total ordering
+    def __eq__(self, other):
+        return isinstance(other, Date) and self.day_count == other.day_count
+
+    def __lt__(self, other):
+        if isinstance(other, Date):
+            return self.day_count < other.day_count
+        else:
+            return NotImplemented
+        
+    def __hash__(self):
+        return hash(self._day_count)
---
+++ packages/datetime2/datetime2-0.3.1.zip/datetime2-0.3.1/test/test_datetime2.py
@@ -0,0 +1,236 @@
+# datetime2 package test
+
+# Copyright (c) 2011 Francesco Ricciardi
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, 
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name(s) of the copyright holders nor the names of its
+#   contributors may be used to endorse or promote products derived from this
+#   software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AS IS AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+# EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+__author__ = 'Francesco Ricciardi <francescor2010 at yahoo.it>'
+
+
+import decimal
+import pickle
+import unittest
+from datetime2 import Date, TimeDelta
+from fractions import Fraction
+
+INF = float("inf")
+NAN = float("nan")
+
+# contains all pickle protocols
+pickle_choices = [(pickle, pickle, proto)
+                  for proto in range(pickle.HIGHEST_PROTOCOL + 1)]
+assert len(pickle_choices) == pickle.HIGHEST_PROTOCOL + 1
+
+
+#############################################################################
+# datetime2 tests
+#
+#TODO: add test for register_calendar
+
+#############################################################################
+# Date tests
+#
+class TestDate(unittest.TestCase):
+    def test_000_valid_parameter_types(self):
+        for par in (-2, -1, 0, 1, 2, -1000, 1000, -123456789, 123456789, -999999999, 999999999, -1000000000, 1000000000):
+            self.assertEqual(Date(par).day_count, par, msg = 'par = {}'.format(par))
+
+    def test_010_invalid_parameter_types(self):
+        # exception with no or two parameters
+        self.assertRaises(TypeError, Date)
+        self.assertRaises(TypeError, Date, 1, 2)
+        # exception with non-numeric types
+        for par in ("1", (1,), [1], {1:1}, (), [], {}, None):
+            self.assertRaises(TypeError, Date, par)
+        # exception with invalid numeric types
+        for par in (1.0, Fraction(1, 1), decimal.Decimal(1), 1j):
+            self.assertRaises(TypeError, Date, par)
+
+    def test_020_today(self):
+        # for the time being, let's use the good old datetime module :-)
+        import datetime
+        
+        # we need to ensure that we are not testing across date change
+        for dummy in range(3):
+            today = Date.today()
+            # let's use the good old date module
+            day_count = datetime.date.today().toordinal()
+            if day_count != today.day_count:
+                break
+        self.assertEqual(today.day_count, day_count)
+
+    def test_030_write_attribute(self):
+        d = Date(1)
+        self.assertRaises(AttributeError, setattr, d, 'day_count', 3)
+
+    def test_040_get_unknown_attribute(self):
+        self.assertRaises(AttributeError, getattr, Date, 'unknown')
+        d = Date(1)
+        self.assertRaises(AttributeError, getattr, d, 'unknown')
+
+    def test_100_computations(self):
+        eq = self.assertEqual
+
+        a = Date(0)
+        b = Date(-3)
+        c = Date(5)
+        zero = TimeDelta(0)
+        one = TimeDelta(1)
+        minusone = TimeDelta(-1)
+        
+        # Addition between Date and TimeDelta, reverse is not defined
+        eq(a + zero, Date(0), msg = 'a + zero')
+        eq(a + one, Date(1), msg = 'a + one')
+        eq(a + minusone, Date(-1), msg = 'a + minusone')
+        eq(b + zero, Date(-3), msg = 'b + zero')
+        eq(b + one, Date(-2), msg = 'b + one')
+        eq(b + minusone, Date(-4), msg = 'b + minusone')
+        eq(c + zero, Date(5), msg = 'c + zero')
+        eq(c + one, Date(6), msg = 'c + one')
+        eq(c + minusone, Date(4), msg = 'c + minusone')
+
+        # subtraction between Date and TimeDelta, reverse is not defined        
+        eq(a - zero, Date(0), msg = 'a - zero')
+        eq(a - one, Date(-1), msg = 'a - one')
+        eq(a - minusone, Date(1), msg = 'a - minusone')
+        eq(b - zero, Date(-3), msg = 'b - zero')
+        eq(b - one, Date(-4), msg = 'b - one')
+        eq(b - minusone, Date(-2), msg = 'b - minusone')
+        eq(c - zero, Date(5), msg = 'c - zero')
+        eq(c - one, Date(4), msg = 'c - one')
+        eq(c - minusone, Date(6), msg = 'c - minusone')
+        
+        
+    def test_110_disallowed_computations(self):
+        a = Date(42)
+        b = Date(24)
+
+        # Add/sub int, float, string, complex, specials and containers should be illegal
+        for obj in (10, 34.5, "abc", 1 + 2j, INF, NAN, {}, [], ()):
+            self.assertRaises(TypeError, lambda: a + obj)
+            self.assertRaises(TypeError, lambda: a - obj)
+            self.assertRaises(TypeError, lambda: obj + a)
+            self.assertRaises(TypeError, lambda: obj - a)
+
+        # These opertations make no sense for Date objects
+        self.assertRaises(TypeError, lambda: TimeDelta(2) + a)  # reverse is valid
+        self.assertRaises(TypeError, lambda: TimeDelta(2) - a)  # reverse is valid
+        
+        self.assertRaises(TypeError, lambda: a * 1)
+        self.assertRaises(TypeError, lambda: 1 * a)
+        self.assertRaises(TypeError, lambda: 1.1 * a)
+        self.assertRaises(TypeError, lambda: a * 1.1)
+        self.assertRaises(TypeError, lambda: a / 1.1)
+        self.assertRaises(TypeError, lambda: a / 1)
+        self.assertRaises(TypeError, lambda: 1 / a)
+        self.assertRaises(TypeError, lambda: 1.1 / a)
+        self.assertRaises(TypeError, lambda: a // 1.1)
+        self.assertRaises(TypeError, lambda: a // 1)
+        self.assertRaises(TypeError, lambda: 1 // a)
+        self.assertRaises(TypeError, lambda: 1.1 // a)
+        self.assertRaises(TypeError, pow, a, 1)
+        self.assertRaises(TypeError, pow, 1, a)
+        self.assertRaises(TypeError, pow, a, 1.1)
+        self.assertRaises(TypeError, pow, 1.1, a)
+        self.assertRaises(TypeError, lambda: a ** 1)
+        self.assertRaises(TypeError, lambda: 1 ** a)
+        self.assertRaises(TypeError, lambda: 1.1 ** a)
+        self.assertRaises(TypeError, lambda: a ** 1.1)
+        self.assertRaises(TypeError, lambda: a ** b)
+        self.assertRaises(TypeError, lambda: a << 1)
+        self.assertRaises(TypeError, lambda: 1 << a)
+        self.assertRaises(TypeError, lambda: a << 1.1)
+        self.assertRaises(TypeError, lambda: 1.1 << a)
+        self.assertRaises(TypeError, lambda: a << b)
+        self.assertRaises(TypeError, lambda: a >> 1)
+        self.assertRaises(TypeError, lambda: 1 >> a)
+        self.assertRaises(TypeError, lambda: a >> 1.1)
+        self.assertRaises(TypeError, lambda: 1.1 >> a)
+        self.assertRaises(TypeError, lambda: a >> b)
+        
+
+    def test_120_compare(self):
+        d1 = Date(42)
+        d2 = Date(24)
+        d3 = Date(42)
+        self.assertEqual(d1, d3)
+        self.assertNotEqual(d2, d3)
+        self.assertGreaterEqual(d1, d3)
+        self.assertLessEqual(d1, d3)
+        self.assertGreater(d1, d2)
+        self.assertLess(d2, d1)
+
+        for badarg in (10, 34.5, "abc", 1 + 2j, INF, NAN, {}, [], ()):
+            self.assertFalse(d1 == badarg)
+            self.assertTrue(d1 != badarg)
+            self.assertFalse(badarg == d1)
+            self.assertTrue(badarg != d1)
+
+            self.assertRaises(TypeError, lambda: d1 <= badarg)
+            self.assertRaises(TypeError, lambda: d1 < badarg)
+            self.assertRaises(TypeError, lambda: d1 > badarg)
+            self.assertRaises(TypeError, lambda: d1 >= badarg)
+            self.assertRaises(TypeError, lambda: badarg <= d1)
+            self.assertRaises(TypeError, lambda: badarg < d1)
+            self.assertRaises(TypeError, lambda: badarg > d1)
+            self.assertRaises(TypeError, lambda: badarg >= d1)
+
+    def test_130_bool(self):
+        self.assertTrue(Date(3))
+        self.assertTrue(Date(0))
+        self.assertTrue(Date(-3))
+
+    def test_140_str(self):
+        for count in (0, -1, 1, -1000, 1000, 99999999, 99999999):
+            self.assertEqual(str(Date(count)), 'R.D. {}'.format(count))
+
+    def test_150_roundtrip(self):
+        for count in (0, -1, 1, -1000, 1000, 99999999, 99999999):
+
+            # Verify date -> string -> date identity.
+            d1 = Date(count)
+            s = repr(d1)
+            d2 = eval(s)
+            self.assertEqual(d1, d2)
+
+    # TODO: add tests for subclassing Date
+    
+    def test_210_hash_equality(self):
+        d1 = Date(42)
+        d2 = Date(32) + TimeDelta(10)
+        self.assertEqual(hash(d1), hash(d2))
+
+        d = {d1: 1}
+        d[d2] = 2
+        self.assertEqual(len(d), 1)
+        self.assertEqual(d[d1], 2)
+
+    def test_220_pickling(self):
+        orig = Date(3141592)
+        for pickler, unpickler, proto in pickle_choices:
+            green = pickler.dumps(orig, proto)
+            derived = unpickler.loads(green)
+            self.assertEqual(orig, derived)
