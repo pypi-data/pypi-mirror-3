6f74e8a31891838d014552c597db42c746e6f4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:19 +0000

    Add package telapi/0.0.3/telapi-0.0.3.tar.gz

---
+++ packages/telapi/telapi-0.0.3.tar.gz/telapi-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+from telapi import VERSION
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name                = "telapi",
+    version             = VERSION,
+    description         = "TelAPI REST API client and InboundXML generator",
+    author              = "TelAPI",
+    author_email        = "help@telapi.com",
+    url                 = "https://github.com/teltechsystems/telapi-python/",
+    keywords            = ["telapi", "inboundxml"],
+    install_requires    = ["requests"],
+    data_files          = [("telapi/schema", ["telapi/schema/telapi.json"])],
+    packages            = find_packages(),
+    classifiers         = [
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Communications :: Telephony"
+    ],
+    long_description = "TelAPI Python Helper Library",
+)
