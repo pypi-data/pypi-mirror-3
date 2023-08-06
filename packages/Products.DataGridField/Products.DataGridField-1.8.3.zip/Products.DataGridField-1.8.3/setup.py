f305154f6427d13f0d055a6a3c56a5ecd06c1c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:00 +0000

    Add package Products.DataGridField/1.8.3/Products.DataGridField-1.8.3.zip

---
+++ packages/Products.DataGridField/Products.DataGridField-1.8.3.zip/Products.DataGridField-1.8.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+version = '1.8.3'
+readme = open("README.rst").read()
+history = open("CHANGES.rst").read()
+long_description = readme + "\n" + history
+
+
+setup(name='Products.DataGridField',
+      version=version,
+      description="A table input component for Plone.",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Development Status :: 5 - Production/Stable",
+        ],
+      keywords='Plone DataGridField Archetypes',
+      author='Jarn',
+      author_email='info@jarn.com',
+      maintainer='Mikko Ohtamaa',
+      maintainer_email='info@redinnovation.com',
+      url='http://plone.org/products/datagridfield',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
