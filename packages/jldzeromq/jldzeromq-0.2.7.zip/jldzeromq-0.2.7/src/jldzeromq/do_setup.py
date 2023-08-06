9838eb5888f9900a72c80bd6874b07da7fd3f407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:21 +0000

    Add package jldzeromq/0.2.7/jldzeromq-0.2.7.zip

---
+++ packages/jldzeromq/jldzeromq-0.2.7.zip/jldzeromq-0.2.7/src/jldzeromq/do_setup.py
@@ -0,0 +1,18 @@
+"""
+    @author: Jean-Lou Dupont
+"""
+
+import logging, sys, os
+
+### alignment for program name
+ALIGN=15
+
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+name=os.path.basename(sys.argv[0])
+fname="%-"+str(ALIGN)+"s : %s" % name
+
+FORMAT='%(asctime)s - '+name+' - %(levelname)s - %(message)s'
+formatter = logging.Formatter(FORMAT)
+
+logging.basicConfig(level=logging.INFO, format=FORMAT)
