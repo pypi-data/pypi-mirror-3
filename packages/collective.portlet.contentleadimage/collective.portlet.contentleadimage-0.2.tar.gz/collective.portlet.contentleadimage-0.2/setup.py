424cfe1645b53dc56ab80b4ed2cb272df34db51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:50 +0000

    Add package collective.portlet.contentleadimage/0.2/collective.portlet.contentleadimage-0.2.tar.gz

---
+++ packages/collective.portlet.contentleadimage/collective.portlet.contentleadimage-0.2.tar.gz/collective.portlet.contentleadimage-0.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2'
+
+setup(name='collective.portlet.contentleadimage',
+      version=version,
+      description="Collection portlet that shows contenteleadimages",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        ],
+      keywords='',
+      author='Ales Zabala Alava (Shagi)',
+      author_email='shagi@gisa-elkartea.org',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.portlet.collection',
+          'collective.contentleadimage',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
