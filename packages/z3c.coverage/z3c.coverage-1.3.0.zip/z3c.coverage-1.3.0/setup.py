ed5ed97fc47f324530a61ae7fa555123407e65e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:24 +0000

    Add package z3c.coverage/1.3.0/z3c.coverage-1.3.0.zip

---
+++ packages/z3c.coverage/z3c.coverage-1.3.0.zip/z3c.coverage-1.3.0/setup.py
@@ -0,0 +1,78 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+
+$Id: setup.py 127754 2012-09-06 13:42:46Z mgedmin $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name='z3c.coverage',
+    version='1.3.0',
+    author="Zope Community",
+    author_email="zope3-dev@zope.org",
+    description="A script to visualize coverage reports via HTML",
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        'Detailed Documentation\n'
+        '**********************'
+        + '\n\n' +
+        read('src', 'z3c', 'coverage', 'README.txt')
+        + '\n\n' +
+        read('src', 'z3c', 'coverage', 'coveragediff.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license="ZPL 2.1",
+    keywords="zope3 test coverage html",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3',
+    ],
+    url='http://pypi.python.org/pypi/z3c.coverage',
+    packages=find_packages('src'),
+    include_package_data=True,
+    package_dir={'': 'src'},
+    namespace_packages=['z3c'],
+    extras_require=dict(
+        test=['zope.testing'],
+        ),
+    install_requires=[
+        'setuptools',
+        'coverage',
+        ],
+    entry_points="""
+        [console_scripts]
+        coveragereport = z3c.coverage.coveragereport:main
+        coveragediff = z3c.coverage.coveragediff:main
+        """,
+    zip_safe=False,
+)
