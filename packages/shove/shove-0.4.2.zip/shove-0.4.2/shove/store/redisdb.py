a1074a0eeb1c50bbab827b30eb658f455303fe74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:23 +0000

    Add package shove/0.4.2/shove-0.4.2.zip

---
+++ packages/shove/shove-0.4.2.zip/shove-0.4.2/shove/store/redisdb.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+'''
+Redis-based object store
+
+The shove psuedo-URL for a redis-based store is:
+
+redis://<host>:<port>/<db>
+'''
+
+import urlparse
+
+try:
+    import redis
+except ImportError:
+    raise ImportError('This store requires the redis library')
+
+from shove.store import ClientStore
+
+
+class RedisStore(ClientStore):
+
+    '''Redis based store'''
+
+    init = 'redis://'
+
+    def __init__(self, engine, **kw):
+        super(RedisStore, self).__init__(engine, **kw)
+        spliturl = urlparse.urlsplit(engine)
+        host, port = spliturl[1].split(':')
+        db = spliturl[2].replace('/', '')
+        self._store = redis.Redis(host, int(port), db)
+
+    def __getitem__(self, key):
+        item = super(RedisStore, self).__getitem__(key)
+        if item is not None:
+            return item
+        raise KeyError(key)
+
+
+__all__ = ['RedisStore']
