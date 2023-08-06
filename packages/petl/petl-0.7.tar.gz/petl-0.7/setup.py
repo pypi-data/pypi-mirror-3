d5a1b015cf31266241e2cb1fc4589f9fea48fa3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:38 +0000

    Add package petl/0.7/petl-0.7.tar.gz

---
+++ packages/petl/petl-0.7.tar.gz/petl-0.7/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='petl',
+    version='0.7',
+    author='Alistair Miles',
+    author_email='alimanfoo@googlemail.com',
+    package_dir={'': 'src'},
+    packages=['petl'],
+    url='https://github.com/alimanfoo/petl',
+    license='MIT License',
+    description='A tentative Python module for extracting, transforming and loading tables of data.',
+    long_description=open('README.txt').read(),
+    classifiers=['Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Libraries :: Python Modules'
+                 ]
+)
