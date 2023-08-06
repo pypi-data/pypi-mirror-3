6f5027cfbd568f6bd31b41ad7cd573aced0c91d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:11 +0000

    Add package rdflib-sqlite/0.1/rdflib-sqlite-0.1.tar.gz

---
+++ packages/rdflib-sqlite/rdflib-sqlite-0.1.tar.gz/rdflib-sqlite-0.1/test/test_sqlite.py
@@ -0,0 +1,78 @@
+import logging
+
+_logger = logging.getLogger(__name__)
+
+import context_case
+import graph_case
+from nose import SkipTest
+from rdflib import URIRef
+from tempfile import mkstemp
+
+"""
+Note: identifier must be a URIRef, choosing to use a Literal causes
+a "KeyError" exception when term2Letter() is called:
+
+======================================================================
+ERROR: testAdd (test.test_sqlite.SQLiteGraphTestCase)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File ".../rdflib-sqlite/test/graph_case.py", line 77, in testAdd
+    self.addStuff()
+  File ".../rdflib-sqlite/test/graph_case.py", line 50, in addStuff
+    self.graph.add((tarek, likes, pizza))
+  File ".../rdflib/graph.py", line 349, in add
+    self.__store.add((s, p, o), self, quoted=False)
+  File ".../rdflib-sqlite/rdflib_sqlite/AbstractSQLStore.py", line 522, in add
+    subject, predicate, obj, context, self._internedId, quoted)
+  File "../rdfextras/store/AbstractSQLStore.py", line 299, in buildTripleSQLCommand
+    triplePattern = statement2TermCombination(subject, predicate, obj, context)
+  File ".../rdfextras/utils/termutils.py", line 204, in statement2TermCombination
+    term2Letter(obj), normalizeGraph(context)[-1])]
+KeyError: 'UUUL'
+"""
+
+
+class SQLiteGraphTestCase(graph_case.GraphTestCase):
+    storetest = True
+    fp, tmppath = mkstemp(prefix='test', dir='/tmp')
+    create = True
+    store_name = "SQLite"
+    identifier = URIRef("http://rdflib.net")
+
+    def setUp(self):
+        graph_case.GraphTestCase.setUp(self)
+
+    def tearDown(self):
+        graph_case.GraphTestCase.tearDown(self)
+
+    def testStatementNode(self):
+        raise SkipTest("Known issue.")
+
+
+class SQLiteContextTestCase(context_case.ContextTestCase):
+    storetest = True
+    fp, tmppath = mkstemp(prefix='test', dir='/tmp')
+    create = True
+    store_name = "SQLite"
+    identifier = URIRef("http://rdflib.net")
+
+    def setUp(self):
+        context_case.ContextTestCase.setUp(self)
+
+    def tearDown(self):
+        self.create = False
+        context_case.ContextTestCase.tearDown(self)
+
+    def testConjunction(self):
+        raise SkipTest("Known issue.")
+
+    def testContexts(self):
+        raise SkipTest("Known issue.")
+
+    def testLenInMultipleContexts(self):
+        raise SkipTest("Known issue.")
+
+
+SQLiteGraphTestCase.storetest = True
+SQLiteContextTestCase.storetest = True
+
---
+++ packages/rdflib-sqlite/rdflib-sqlite-0.1.tar.gz/rdflib-sqlite-0.1/test/test_store_performance.py
@@ -0,0 +1,104 @@
+import unittest
+import gc
+import os
+import itertools
+import tempfile
+from time import time
+from random import random
+from rdflib import Graph
+from rdflib import URIRef
+
+
+def random_uri():
+    return URIRef("%s" % random())
+
+
+class SQLiteStoreTestCase(unittest.TestCase):
+    """
+    Test case for testing store performance... probably should be
+    something other than a unit test... but for now we'll add it as a
+    unit test.
+    """
+    store = "SQLite"
+    path = None
+    storetest = True
+    performancetest = True
+
+    def setUp(self):
+        self.gcold = gc.isenabled()
+        gc.collect()
+        gc.disable()
+
+        self.graph = Graph(store=self.store)
+        fp, self.path = tempfile.mkstemp(suffix='.sqlite')
+        self.graph.open(self.path, create=True)
+        self.input = Graph()
+
+    def tearDown(self):
+        self.graph.close()
+        if self.gcold:
+            gc.enable()
+        # TODO: delete a_tmp_dir
+        self.graph.close()
+        del self.graph
+        if hasattr(self, 'path') and self.path is not None:
+            if os.path.exists(self.path):
+                if os.path.isdir(self.path):
+                    for f in os.listdir(self.path):
+                        os.unlink(self.path + '/' + f)
+                    os.rmdir(self.path)
+                elif len(self.path.split(':')) == 1:
+                    os.unlink(self.path)
+                else:
+                    os.remove(self.path)
+
+    def testTime(self):
+        report = '"%s": [' % self.store
+        for i in ['500triples', '1ktriples', '2ktriples',
+                  '3ktriples', '5ktriples', '10ktriples',
+                  '25ktriples']:
+            inputloc = os.getcwd() + '/test/sp2b/%s.n3' % i
+            res = self._testInput(inputloc)
+            report += "%s," % res.strip()
+        print(report + "],")
+
+    def _testInput(self, inputloc):
+        number = 1
+        store = self.graph
+        self.input.parse(location=inputloc, format="n3")
+
+        def add_from_input():
+            for t in self.input:
+                store.add(t)
+        it = itertools.repeat(None, number)
+        t0 = time()
+        for _i in it:
+            add_from_input()
+        t1 = time()
+        return "%.3g " % (t1 - t0)
+
+    def testQuery(self):
+        query = """\
+            PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX dc:      <http://purl.org/dc/elements/1.1/>
+            PREFIX dcterms: <http://purl.org/dc/terms/>
+            PREFIX bench:   <http://localhost/vocabulary/bench/>
+            PREFIX xsd:     <http://www.w3.org/2001/XMLSchema#>
+
+            SELECT ?yr
+            WHERE {
+              ?journal rdf:type bench:Journal .
+              ?journal dc:title "Journal 1 (1940)"^^xsd:string .
+              ?journal dcterms:issued ?yr
+            }"""
+        self.input.parse(location=os.getcwd() + '/test/sp2b/25ktriples.n3',
+                        format="n3")
+        t0 = time()
+        res = self.input.query(query)
+        t1 = time()
+        print("Query time %s" % (t1 - t0))
+        assert '1940' in str(list(res)[0])
+
+
+if __name__ == '__main__':
+    unittest.main()
