beb42a2f00f3217b322b9ae764c05ba9a9be404c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:16 +0000

    Add package pyworksnaps/0.1/pyworksnaps-0.1.tar.gz

---
+++ packages/pyworksnaps/pyworksnaps-0.1.tar.gz/pyworksnaps-0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='pyworksnaps',
+    version='0.1',
+    description='Python module for Worksnaps API',
+    author='LetoLab Ltd',
+    author_email='team@letolab.com',
+    url='https://github.com/letolab/pyworksnaps',
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 3 - Alpha",
+    ],
+    py_modules=['pyworksnaps'],
+)
