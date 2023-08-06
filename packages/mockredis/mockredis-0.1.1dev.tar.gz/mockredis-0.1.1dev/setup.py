8496edc4b63b68962af6cca4755d76d254396574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:56 +0000

    Add package mockredis/0.1.1dev/mockredis-0.1.1dev.tar.gz

---
+++ packages/mockredis/mockredis-0.1.1dev.tar.gz/mockredis-0.1.1dev/setup.py
@@ -0,0 +1,26 @@
+#mockredis/setup.py
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.md')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.md')).read()
+except IOError:
+    CHANGES = README = ''
+
+requires = [
+]
+
+tests_require = requires + [
+]
+
+setup(name='mockredis',
+      version='0.1.1dev',
+      description='A mock redis object.',
+      long_description=README + '\n\n' + CHANGES,
+      install_requires=requires,
+      tests_require=tests_require,
+      packages=['mockredis'],
+      test_suite='mockredis.tests'
+)
