f07c5cbaf8322d3958f3e78fbd36374099888b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:25 +0000

    Add package PyGithub/0.5/PyGithub-0.5.tar.gz

---
+++ packages/PyGithub/PyGithub-0.5.tar.gz/PyGithub-0.5/github/Github.py
@@ -0,0 +1,31 @@
+from Requester import Requester
+from GithubObjects import *
+
+class Github:
+    def __init__( self, login, password ):
+        self.__requester = Requester( login, password )
+
+    def _dataRequest( self, verb, url, parameters, data ):
+        return self.__requester.dataRequest( verb, url, parameters, data )
+
+    def _statusRequest( self, verb, url, parameters, data ):
+        return self.__requester.statusRequest( verb, url, parameters, data )
+
+    def get_user( self, login = None ):
+        if login is None:
+            return AuthenticatedUser( self, {}, lazy = True )
+        else:
+            return NamedUser( self, { "login": login }, lazy = False )
+
+    def get_organization( self, login ):
+        return Organization( self, { "login": login }, lazy = False )
+
+    def get_gist( self, id ):
+        return Gist( self, { "id": id }, lazy = False )
+
+    def get_gists( self ):
+        return [
+            Gist( self, attributes, lazy = True )
+            for attributes
+            in self._dataRequest( "GET", "/gists/public", None, None )
+        ]
