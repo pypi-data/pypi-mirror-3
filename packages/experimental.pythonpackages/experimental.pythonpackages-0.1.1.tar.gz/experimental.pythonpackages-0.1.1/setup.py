9fbc99305f714d414c619a86f024c28980ba2f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:56 +0000

    Add package experimental.pythonpackages/0.1.1/experimental.pythonpackages-0.1.1.tar.gz

---
+++ packages/experimental.pythonpackages/experimental.pythonpackages-0.1.1.tar.gz/experimental.pythonpackages-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+from setuptools import setup
+import os
+
+VERSION = '0.1.1'
+
+setup(
+    description='Test',
+    include_package_data=True,
+    long_description=open('README.rst').read(),
+    name='experimental.pythonpackages',
+    packages=find_packages(),
+    version=VERSION,
+)
