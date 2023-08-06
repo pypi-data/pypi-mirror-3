5dcd72af3f4212f4ff415395b493da3e6c0e2a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:30 +0000

    Add package mobileapart.django.newsletter/0.6/mobileapart.django.newsletter-0.6.tar.gz

---
+++ packages/mobileapart.django.newsletter/mobileapart.django.newsletter-0.6.tar.gz/mobileapart.django.newsletter-0.6/mobileapart/django/newsletter/__init__.py
@@ -0,0 +1,7 @@
+"""mobileapart.django.newsletter"""
+__license__ = 'BSD License'
+
+__author__ = 'Leonardo Di Lella'
+__email__ = 'leonardo.dilella@mobileapart.com'
+
+__url__ = 'http://www.mobileapart.com/'
