0b666245d9ccc28d8931ff694745f75e12fc5dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:49 +0000

    Add package django-cache-toolbox-modified/0.1.0/django-cache-toolbox-modified-0.1.0.tar.gz

---
+++ packages/django-cache-toolbox-modified/django-cache-toolbox-modified-0.1.0.tar.gz/django-cache-toolbox-modified-0.1.0/README.md
@@ -0,0 +1,12 @@
+# django-cache-toolbox
+
+Forked from <https://github.com/playfire/django-cache-toolbox>.
+
+## Why did I fork?
+
+* I have several modifications regarding their cache-invalidation rules I want to experiment with.
+* I contacted the author asking that they publish the project to PyPI for ease of installation and never heard back. As such, I am managing this forked version at <TODO>.
+
+## Changes from playfire's
+
+* TODO
---
+++ packages/django-cache-toolbox-modified/django-cache-toolbox-modified-0.1.0.tar.gz/django-cache-toolbox-modified-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-cache-toolbox-modified",
+    packages=find_packages(),
+    version="0.1.0",
+    author="Matt Long",
+    license="BSD",
+    author_email="matt@mattlong.org",
+    url="http://pypi.python.org/pypi/django-cache-toolbox-modified/",
+    description="Non-magical object caching for Django. Based on django-cache-toolbox by Playfire.com",
+    long_description="cache_toolbox is intended to be a lightweight series of independent tools to leverage caching within Django projects. Based on django-cache-toolbox by Playfire.com available at http://code.playfire.com/django-cache-toolbox",
+    install_requires=['django>=1.3.0'],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Operating System :: Unix',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
