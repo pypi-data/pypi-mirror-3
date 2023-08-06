1877b83bda6f51a6cedb816dfc4f4ab816d56cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:18 +0000

    Add package django-tags-input/1.0/django-tags-input-1.0.tar.gz

---
+++ packages/django-tags-input/django-tags-input-1.0.tar.gz/django-tags-input-1.0/setup.py
@@ -0,0 +1,27 @@
+import tags_input
+import os
+from setuptools import setup
+
+if os.path.isfile('README.rst'):
+    long_description = open('README.rst').read()
+else:
+    long_description = ('See http://pypi.python.org/pypi/'
+        + tags_input.__name__)
+
+setup(
+    name=tags_input.__name__,
+    version=tags_input.__version__,
+    author=tags_input.__author__,
+    author_email=tags_input.__author_email__,
+    description=tags_input.__description__,
+    url=tags_input.__url__,
+    license='BSD',
+    packages=['tags_input'],
+    long_description=long_description,
+    test_suite='nose.collector',
+    setup_requires=['nose'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+    ],
+)
+
