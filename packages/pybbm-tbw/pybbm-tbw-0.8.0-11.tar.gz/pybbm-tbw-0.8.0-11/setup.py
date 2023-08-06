1302740a7beab30eb205f653feba84f60de24d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:26 +0000

    Add package pybbm-tbw/0.8.0-11/pybbm-tbw-0.8.0-11.tar.gz

---
+++ packages/pybbm-tbw/pybbm-tbw-0.8.0-11.tar.gz/pybbm-tbw-0.8.0-11/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    version = '0.8.0-11',
+    description = 'PyBB Modified, Modified. Django forum application modified to work with other languages than Russian and English, as well as working with easy_thumbnails',
+    long_description = open('README.rst').read(),
+    author = 'Trygve Bertelsen Wiig',
+    author_email = 'trygvebw@gmail.com',
+    name = 'pybbm-tbw',
+    packages = find_packages(),
+    include_package_data = True,
+    package_data = {'': ['pybb/templates', 'pybb/static']},
+    install_requires = [
+            'django',
+            'markdown',
+            'postmarkup',
+            'south',
+            'pytils',
+            'django-annoying',
+            'easy_thumbnails',
+            'django-pure-pagination'
+            ],
+
+    license = "BSD",
+    keywords = "django application forum board",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
