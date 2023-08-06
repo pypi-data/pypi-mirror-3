c86aa3d1d565b6db4cfa9fec9af213785b3b532c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:13 +0000

    Add package dj-redis-url/0.1.1/dj-redis-url-0.1.1.tar.gz

---
+++ packages/dj-redis-url/dj-redis-url-0.1.1.tar.gz/dj-redis-url-0.1.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="dj-redis-url",
+    version="0.1.1",
+
+    description="Use Redis URLs in your Django Application.",
+    long_description=open("README.rst").read(),
+    url="https://github.com/dstufft/dj-redis-url",
+    license=open("LICENSE").read(),
+
+    author="Donald Stufft",
+    author_email="donald.stufft@gmail.com",
+
+    extras_require={
+        "tests": ["pytest"],
+    },
+
+    packages=find_packages(exclude=["tests"]),
+    package_data={"": ["LICENSE"]},
+    include_package_data=True,
+
+    zip_safe=False,
+)
