7a312593951a8f9a736e944853cfa3e155ff27b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:54 +0000

    Add package omdox/0.5.1.alpha/omdox-0.5.1.alpha.tar.gz

---
+++ packages/omdox/omdox-0.5.1.alpha.tar.gz/omdox-0.5.1.alpha/omdox/__init__.py
@@ -0,0 +1,5 @@
+VERSION = (0, 5, 1, 'alpha')
+
+
+def get_version():
+    return '.'.join(str(i) for i in VERSION)
---
+++ packages/omdox/omdox-0.5.1.alpha.tar.gz/omdox-0.5.1.alpha/setup.py
@@ -0,0 +1,36 @@
+import os
+from distutils.core import setup
+from omdox import get_version
+
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+
+setup(
+    name='omdox',
+    version=get_version(),
+    license = 'BSD 3 Clause',
+    description='A lightweight documentation builder.',
+    long_description=open('README.md').read(),
+    author='Obscure Metaphor',
+    url='https://github.com/obscuremetaphor/omdox',
+    author_email='hello@obscuremetaphor.co.uk',
+    packages=[
+        'omdox',
+    ],
+    classifiers=[
+        'Topic :: Documentation',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Documentation',
+    ],
+    scripts=[
+        'bin/omdox'
+    ],
+    install_requires = [
+        'jinja2',
+        'pygments',
+        'pyyaml',
+        'walkdir',
+        'beautifulsoup4',
+        'markdown2']
+)
