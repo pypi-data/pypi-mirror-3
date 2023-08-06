bdad7ff80e1f86181c141bc08eb6d6d6b0ececf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:57 +0000

    Add package metrology/0.3.1/metrology-0.3.1.tar.gz

---
+++ packages/metrology/metrology-0.3.1.tar.gz/metrology-0.3.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='metrology',
+    version='0.3.1',
+    description='A library to easily measure what\'s going on in your python.',
+    author='Timothée Peignier',
+    author_email='timothee.peignier@tryphon.org',
+    url='https://github.com/cyberdelia/metrology',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'atomic>=0.3.2',
+        'bintrees>=1.0.0'
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ]
+)
