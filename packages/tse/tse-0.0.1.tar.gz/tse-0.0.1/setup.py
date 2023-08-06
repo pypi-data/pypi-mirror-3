c9acb76cff820760c65f282284e790e81ae15436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:46 +0000

    Add package tse/0.0.1/tse-0.0.1.tar.gz

---
+++ packages/tse/tse-0.0.1.tar.gz/tse-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "tse",
+    version = "0.0.1",
+    author = "Atsuo Ishimoto",
+    author_email = "ishimoto@gembook.org",
+    description = "tse is an input stream editor in Python.",
+    license = "MIT",
+    keywords = "text filter",
+    url = "https://github.com/atsuoishimoto/tse",
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Text Processing :: Filters",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages = find_packages("src"),
+    package_dir = {'':'src'},
+    entry_points = {
+        'console_scripts': ['tse = tse.main:main']
+    },
+    install_requires=["argparse"],
+)
