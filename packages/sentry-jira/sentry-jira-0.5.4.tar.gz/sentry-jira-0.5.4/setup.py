2cb38e0f7174a44b43a84950593766a74d2e53f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:41 +0000

    Add package sentry-jira/0.5.4/sentry-jira-0.5.4.tar.gz

---
+++ packages/sentry-jira/sentry-jira-0.5.4.tar.gz/sentry-jira-0.5.4/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+install_requires = [
+    'sentry>=4.10.0',
+]
+
+f = open('README.rst')
+readme = f.read()
+f.close()
+
+setup(
+    name='sentry-jira',
+    version='0.5.4',
+    author='Adam Thurlow',
+    author_email='thurloat@gmail.com',
+    url='http://github.com/thurloat/sentry-jira',
+    description='A Sentry extension which creates JIRA issues from sentry events.',
+    long_description=readme,
+    license='BSD',
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points={
+        'sentry.apps': [
+            'jira = sentry_jira',
+            ],
+        'sentry.plugins': [
+            'jira = sentry_jira.plugin:JIRAPlugin'
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Software Development'
+    ],
+)
