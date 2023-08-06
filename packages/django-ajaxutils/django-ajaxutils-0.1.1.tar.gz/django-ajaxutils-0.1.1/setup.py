c85c8dd8ea1185eacf8d274b43fe8580a361b792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:49 +0000

    Add package django-ajaxutils/0.1.1/django-ajaxutils-0.1.1.tar.gz

---
+++ packages/django-ajaxutils/django-ajaxutils-0.1.1.tar.gz/django-ajaxutils-0.1.1/setup.py
@@ -0,0 +1,41 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+install_requires = [
+    'Django',
+    'decorator>=3.3.2',
+]
+
+tests_require = [
+    'django-jenkins>=0.11.0',
+    'pep8>=0.6.1',
+]
+
+long_description = open('README.rst').read()
+
+setup(
+    name='django-ajaxutils',
+    version="0.1.1",
+    description='Ajax requests for Ponies',
+    url='http://github.com/ahref/django-ajaxutils',
+    packages=['ajaxutils'],
+    zip_safe=True,
+    license='BSD',
+    classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+    long_description=long_description,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='runtests.runtests',
+)
