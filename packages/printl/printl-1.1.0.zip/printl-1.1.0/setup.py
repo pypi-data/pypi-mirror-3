17fd2109ce45eb1d82c1623205dfd6088c43dfdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:09 +0000

    Add package printl/1.1.0/printl-1.1.0.zip

---
+++ packages/printl/printl-1.1.0.zip/printl-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'printl',
+    version = '1.1.0',
+	py_modules = ['printl'],
+    author = 'richard',
+    author_email = "rzeng@gmail.com",
+    url = "htt://www.cnblogs.com",
+    description = " a simple printer of nested list",
+    )
