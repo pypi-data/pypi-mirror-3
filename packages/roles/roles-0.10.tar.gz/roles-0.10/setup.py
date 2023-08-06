c177c868f1678ef65d4b60df9cef603d5b72aafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:16 +0000

    Add package roles/0.10/roles-0.10.tar.gz

---
+++ packages/roles/roles-0.10.tar.gz/roles-0.10/setup.py
@@ -0,0 +1,40 @@
+"""
+Setup script for roles module.
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from contextlib import closing
+import glob
+
+VERSION = '0.10'
+
+with closing(open('README.txt')) as f:
+    doc = f.read()
+
+
+setup(
+    name='roles',
+    version=VERSION,
+    description='Role based development',
+    long_description=doc,
+    author='Arjan Molenaar',
+    author_email='gaphor@gmail.com',
+    url='http://github.com/amolenaar/roles',
+    license="BSD License",
+    packages = [ 'roles' ],
+    keywords="role DCI data context interaction",
+    platforms=["All"],
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Libraries']
+    )
+
+#vim:sw=4:et:ai
