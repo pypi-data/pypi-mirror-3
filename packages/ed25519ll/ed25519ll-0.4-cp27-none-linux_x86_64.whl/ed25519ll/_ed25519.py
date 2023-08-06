dbe2ba782ac07e6a10ffd6532a940043221bf6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:30 +0000

    Add package ed25519ll/0.4/ed25519ll-0.4-cp27-none-linux_x86_64.whl

---
+++ packages/ed25519ll/ed25519ll-0.4-cp27-none-linux_x86_64.whl/ed25519ll/_ed25519.py
@@ -0,0 +1,48 @@
+import pkg_resources
+import sysconfig
+import os.path
+
+from distutils.util import get_platform
+from cffi import FFI
+ffi = FFI()
+
+decl = """
+    extern int crypto_sign(unsigned char *, unsigned long long *,
+        const unsigned char *, unsigned long long, const unsigned char *);
+    
+    extern int crypto_sign_open(unsigned char *, unsigned long long *, 
+        const unsigned char *, unsigned long long, const unsigned char *);
+    
+    extern int crypto_sign_keypair(unsigned char *pk, unsigned char *sk, 
+        unsigned char *seed);
+"""
+
+ffi.cdef(decl)
+
+verify = False
+
+plat_name = get_platform().replace('-', '_')
+so_suffix = sysconfig.get_config_var('SO')
+library_dir = os.path.abspath(pkg_resources.resource_filename('ed25519ll', ''))
+if not verify: # pragma no cover
+    lib_filename = pkg_resources.resource_filename('ed25519ll', 
+                                                   '_ed25519_%s%s' %
+                                                   (plat_name, so_suffix))
+    _ed25519 = ffi.dlopen(os.path.abspath(lib_filename))
+else: # pragma nocover
+    so_prefix = ""
+    if os.name == "posix":
+        so_prefix = ":" # to link with libraries without 'lib' prefix
+    _ed25519 = ffi.verify(decl,
+                          libraries=["%s_ed25519_%s%s" % (so_prefix,
+                                                          plat_name,
+                                                          so_suffix)],
+                          library_dirs=[library_dir],
+                          export_symbols=["crypto_sign",
+                                          "crypto_sign_open",
+                                          "crypto_sign_keypair"],
+                          )
+
+crypto_sign_keypair = _ed25519.crypto_sign_keypair
+crypto_sign = _ed25519.crypto_sign
+crypto_sign_open = _ed25519.crypto_sign_open
