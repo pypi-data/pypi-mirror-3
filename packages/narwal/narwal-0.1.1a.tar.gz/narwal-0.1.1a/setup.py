1a7a86ccf8cf4e9014542ca37f5c5f31beb67663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:34 +0000

    Add package narwal/0.1.1a/narwal-0.1.1a.tar.gz

---
+++ packages/narwal/narwal-0.1.1a.tar.gz/narwal-0.1.1a/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='narwal',
+    packages=['narwal'],
+    version='0.1.1a',
+    author='Larry Ng',
+    author_email='ng.larry@gmail.com',
+    url='https://github.com/larryng/narwal',
+    description='A thin wrapper for the Reddit API.',
+    long_description='See https://github.com/larryng/narwal for more info.',
+    keywords=['reddit', 'api', 'wrapper'],
+    install_requires=['requests>=0.11.1'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+        ],
+)
