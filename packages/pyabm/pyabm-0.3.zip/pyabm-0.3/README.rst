3f6d850f32c9b65a32eaf363bd6f7292c9f96534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:42 +0000

    Add package pyabm/0.3/pyabm-0.3.zip

---
+++ packages/pyabm/pyabm-0.3.zip/pyabm-0.3/README.rst
@@ -0,0 +1,40 @@
+pyabm README
+===============================================================================
+
+pyabm - an agent-based modeling toolkit for Python 2.7.x.
+ 
+Description
+_______________________________________________________________________________
+
+'pyabm' is an agent-based modeling toolkit written to simplify coding and 
+running agent-based models int he Python programming language. pyabm 
+includes tools to output model results (as text files, plots or ESRI 
+shapefiles), and has a parameter handling system to ease the process of 
+model testing and validation, and of running multiple scenarios with 
+varying model parameters.
+
+The model was constructed by Alex Zvoleff as part of his dissertation 
+research at San Diego State University (SDSU) in the Department of 
+Geography. Contact Alex Zvoleff or Prof. Li An at SDSU with any questions.
+
+GETTING THE CODE:
+_______________________________________________________________________________
+
+See the pyabm website at: http://rohan.sdsu.edu/~zvoleff/research/pyabm.php.
+
+The latest version of the code is available as a zipped snapshot from: 
+https://github.com/azvoleff/pyabm/zipball/master.
+
+The source can also be downloaded via git from: 
+git://github.com/azvoleff/pyabm.git.
+
+Author Contact Information
+_______________________________________________________________________________
+
+| Alex Zvoleff
+| SDSU/UCSB Joint Doctoral Candidate
+| Department of Geography
+| San Diego State University
+| 5500 Campanile Dr.
+| San Diego, CA 92182-4493
+| Email: azvoleff@mail.sdsu.edu
---
+++ packages/pyabm/pyabm-0.3.zip/pyabm-0.3/setup.py
@@ -0,0 +1,54 @@
+import distribute_setup
+distribute_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "pyabm",
+    version = "0.3",
+    packages = find_packages(),
+    include_package_data = True,
+    exclude_package_data = {'': ['.gitignore']},
+    zip_safe = True,
+    install_requires = ['docutils >= 0.3',
+                        'setuptools_git >= 0.3',
+                        'numpy >= 1.6.2',
+                        'GDAL > 1.6'],
+
+    author = "Alex Zvoleff",
+    author_email = "azvoleff@mail.sdsu.edu",
+    description = "Agent-based modeling toolkit",
+    license = "GPL v3 or later",
+    keywords = "agent-based modeling ABM simulation model",
+    url = "http://rohan.sdsu.edu/~zvoleff/pyabm.php",   # project home page, if any
+    long_description = """
+pyabm is an agent-based modeling toolkit written to simplify coding and 
+running agent-based models in the Python programming language. pyabm 
+includes basic classes for creation of agents, has tools to output model 
+results (as text files, plots or ESRI shapefiles), has a parameter handling 
+system to ease the process of model testing and validation, and has 
+utilities to assist in running multiple scenarios with varying model 
+parameters.
+
+Construction of pyabm is supported as part of an ongoing National Science 
+Foundation Partnerships for International Research and Education (NSF PIRE) 
+project `(grant OISE 0729709) <http://pire.psc.isr.umich.edu>`_
+Development of pyabm is ongoing, and the documentation is still a work in 
+progress.
+
+See the `pyabm website <http://rohan.sdsu.edu/~zvoleff/research/PyABM.php>`_ 
+for more information, past releases, publications, and recent presentations.
+""",
+	classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Topic :: Software Development",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Life",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules"]
+)
