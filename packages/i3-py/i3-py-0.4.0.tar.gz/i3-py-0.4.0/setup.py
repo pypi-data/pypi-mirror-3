a99d2233cc531cc2fb9b3fab606ef29d9a7062b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:43 +0000

    Add package i3-py/0.4.0/i3-py-0.4.0.tar.gz

---
+++ packages/i3-py/i3-py-0.4.0.tar.gz/i3-py-0.4.0/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+long_description = """
+i3-py contains tools for i3 users and developers
+
+The contents of i3-py project are:
+
+ - i3.py, a Python module for communicating with i3-wm
+ - i3-ipc, a command-line script that wraps some of i3.py's features
+ - i3wsbar, a Python implementation of i3-wm workspace bar
+
+"""
+
+setup(
+    name='i3-py',
+    description='tools for i3 users and developers',
+    long_description=long_description,
+    author='Jure Ziberna',
+    author_email='j.ziberna@gmail.com',
+    url='https://github.com/jzib/i3-py',
+    version='0.4.0',
+    license='GNU GPL 3',
+    py_modules=['i3', 'i3wsbar']
+)
