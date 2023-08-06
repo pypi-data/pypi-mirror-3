89c7ea954bdcd3dc19bf4c7b53a017643d40d24f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:27 +0000

    Add package mahotas/0.9/mahotas-0.9.tar.gz

---
+++ packages/mahotas/mahotas-0.9.tar.gz/mahotas-0.9/mahotas/bwperim.py
@@ -0,0 +1,9 @@
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
+#
+# Copyright (C) 2012  Luis Pedro Coelho
+# 
+# License: MIT (see COPYING file)
+
+from .labeled import bwperim
+__all__ = ['bwperim']
+
