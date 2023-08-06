e56b9ab718c1357fcbc062a7d6f8871c5816d133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:46 +0000

    Add package clay/1.0.13/Clay-1.0.13.tar.gz

---
+++ packages/clay/Clay-1.0.13.tar.gz/Clay-1.0.13/clay/config.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+# Clay.config
+
+"""
+import os
+
+SOURCE_DIR = 'source'
+BUILD_DIR = 'build'
+VIEWS_INDEX = '_index.html'
+
+DEFAULT_TEMPLATES = os.path.join(os.path.dirname(__file__), 'source')
+
+IGNORE = ('.', '_')
+
+default_settings = {
+    'host': '0.0.0.0',
+    'port': 8080,
+
+    'plain_text': ['.js', ],
+    'views_ignore': [],
+    'views_list_ignore': [],
+
+    'pre_processors': ['scss', 'less', 'coffee', 'markdown'],
+    'post_processors': ['pygments', 'typogrify'],
+
+    'theme_prefix': '',
+}
+
+app_settings = {
+    'RELOAD': False,
+}
+
