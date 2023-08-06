eeadf54d73c83335074ba191c15870b86ea8bc5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:41 +0000

    Add package modTest/0.0.7/modTest-0.0.7.zip

---
+++ packages/modTest/modTest-0.0.7.zip/modTest-0.0.7/modTest.py
@@ -0,0 +1,9 @@
+print("Hello,this is a distribution test")
+def m1():
+    print("Success2")
+	
+def m2():
+    print("Success update")
+	
+def m3():
+    print("Success upload")
---
+++ packages/modTest/modTest-0.0.7.zip/modTest-0.0.7/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup  
+  
+setup(  
+    name = "modTest",  
+    version = "0.0.7",  
+    py_modules = ["modTest"],  
+    author = "wklken",  
+    author_email = "wklken@yeah.net",  
+    url = "http://justtest",  
+    description = "just a sample of distribution",  
+    )     
