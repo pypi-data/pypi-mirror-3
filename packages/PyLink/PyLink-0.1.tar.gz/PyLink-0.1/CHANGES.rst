9b6ac7eef719ae41bad9f09eee0acd3b450e8a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:09 +0000

    Add package PyLink/0.1/PyLink-0.1.tar.gz

---
+++ packages/PyLink/PyLink-0.1.tar.gz/PyLink-0.1/CHANGES.rst
@@ -0,0 +1,9 @@
+Changelog
+---------
+
+Version 0.1
+~~~~~~~~~~~
+
+Released on 2012-06-05.
+
+* Support for TCP, UDP and Serial (with pyserial).
---
+++ packages/PyLink/PyLink-0.1.tar.gz/PyLink-0.1/pylink/logger.py
@@ -0,0 +1,46 @@
+# coding: utf8
+"""
+    pylink.logger
+    -------------
+
+    Logging setup.
+
+    :copyright: Copyright 2012 Salem Harrache and contributors, see AUTHORS.
+    :license: BSD.
+
+"""
+
+from __future__ import unicode_literals
+
+import logging
+
+
+def silent_logger():
+    '''Initialize a silent logger.'''
+    logger = logging.getLogger('pylink')
+    try:
+        from logging import NullHandler
+    except ImportError:
+        class NullHandler(logging.Handler):
+            def emit(self, record):
+                pass
+    logger.addHandler(NullHandler())
+    return logger
+
+
+def active_logger():
+    '''Initialize a speaking logger with stream handler (stderr).'''
+    logger = logging.getLogger('pylink')
+
+    logger.setLevel(logging.INFO)
+
+    # Default to logging to stderr.
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s ')
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
+
+LOGGER = silent_logger()
