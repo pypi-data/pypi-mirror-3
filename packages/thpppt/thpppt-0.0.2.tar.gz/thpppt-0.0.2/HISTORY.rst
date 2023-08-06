caaef3bdfbfbdc04390f9c1fe452c7d6e1a5a9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:21 +0000

    Add package thpppt/0.0.2/thpppt-0.0.2.tar.gz

---
+++ packages/thpppt/thpppt-0.0.2.tar.gz/thpppt-0.0.2/HISTORY.rst
@@ -0,0 +1,11 @@
+0.0.2
++++++
+Actually dsitribute file templates as part of the package.
+
+Add Elisp projects.
+Add Licensing and build system stubs
+
+0.0.1
++++++
+
+Minimum viable product
---
+++ packages/thpppt/thpppt-0.0.2.tar.gz/thpppt-0.0.2/thpppt/__main__.py
@@ -0,0 +1,9 @@
+
+import warnings
+
+with warnings.catch_warnings() as wr:
+    warnings.filterwarnings("ignore", category = SyntaxWarning)
+    from thpppt import main
+
+if __name__ == '__main__':
+    main()
