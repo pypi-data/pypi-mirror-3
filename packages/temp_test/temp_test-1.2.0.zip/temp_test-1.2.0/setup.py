f151960fa69fed18187349ec5e4fdc67b81eb94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:05 +0000

    Add package temp_test/1.2.0/temp_test-1.2.0.zip

---
+++ packages/temp_test/temp_test-1.2.0.zip/temp_test-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'temp_test',
+    version = '1.2.0',
+    py_modules = ['temp_test'],
+    author = 'hfpython',
+    author_email = 'temp',
+    url = 'temp',
+    description = 'temp',
+    )
