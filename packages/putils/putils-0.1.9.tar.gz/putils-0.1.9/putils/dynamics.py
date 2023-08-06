be70c7e3a8a5c4820638fc24ffb7d416fa3c1d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:04 +0000

    Add package putils/0.1.9/putils-0.1.9.tar.gz

---
+++ packages/putils/putils-0.1.9.tar.gz/putils-0.1.9/putils/dynamics.py
@@ -0,0 +1,70 @@
+import os
+import imp
+import sys
+import mimetypes
+
+
+class Importer(object):
+	"""
+	Use methods of this class to import class or module
+	"""
+	@staticmethod
+	def import_class(cl):
+		"""
+		Imports class by name
+		"""
+		d = cl.rfind(".")
+		classname = cl[d+1:len(cl)]
+		m = __import__(cl[0:d], globals(), locals(), [classname])
+		return getattr(m, classname)
+		
+	@staticmethod
+	def import_module(module):
+		"""
+		Imports module by name
+		"""
+		d = module.rfind(".")
+		modulename = module[d+1:len(module)]
+		mod = __import__(module, globals(), locals(), [modulename])
+		return mod
+		
+	@staticmethod
+	def module_path(module):
+		"""
+		Returns path of module
+		"""
+		result = os.path.dirname(Importer.import_module(module).__file__)
+		return result
+		
+	@staticmethod
+	def import_module_by_path(filepath):
+		"""
+		Imports module by path
+		"""
+		for base_path in sys.path:
+			try:
+				filepath = os.path.relpath(filepath, base_path)[:-3]
+				module_name = '.'.join(filter(None, filepath.replace('.', '').split('/')))
+				return Importer.import_module(module_name)
+			except:
+				pass
+		return None
+			
+	
+
+class Introspector(object):	
+	@staticmethod
+	def all_subclasses(cls, result=[]):
+		for sub in cls.__subclasses__():
+			result.append(sub)
+			Introspector.all_subclasses(sub, result)
+		return result
+		
+	@staticmethod
+	def class_that_defined_method(meth):
+		obj = meth.im_self
+		for cls in inspect.getmro(meth.im_class):
+			if meth.__name__ in cls.__dict__:
+				return cls
+		return None
+        
