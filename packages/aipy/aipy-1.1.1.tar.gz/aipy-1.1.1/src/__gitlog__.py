1ac1d8c4964ad3a4e4e5e3661080574118b0caaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:51 +0000

    Add package aipy/1.1.1/aipy-1.1.1.tar.gz

---
+++ packages/aipy/aipy-1.1.1.tar.gz/aipy-1.1.1/src/__gitlog__.py
@@ -0,0 +1,6 @@
+__gitlog__ = """fb63648
+Incremented to version 2.0.0 following merge with various dev branches & polarization stuff.
+--
+Aaron Parsons
+aparsons@astron.berkeley.edu
+2012-04-05 13:22:55 -0700"""
