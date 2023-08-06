41b6f364dd59dcfb1fdce673f7cd30b05dafaa4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:16 +0000

    Add package satchmo_payment_payworld/0.1/satchmo_payment_payworld-0.1.tar.gz

---
+++ packages/satchmo_payment_payworld/satchmo_payment_payworld-0.1.tar.gz/satchmo_payment_payworld-0.1/satchmo_payment_payworld/config.py
@@ -0,0 +1,85 @@
+from livesettings import *
+from django.utils.translation import ugettext_lazy as _
+
+PAYMENT_GROUP = ConfigurationGroup('PAYMENT_SATCHMO_PAYMENT_PAYWORLD',
+    _('Payworld Payment Module Settings'),
+    ordering = 101)
+
+config_register_list(
+
+StringValue(PAYMENT_GROUP,
+    'POST_URL',
+    description=_('Post URL'),
+    help_text=_('The Payworld URL for real transaction posting.'),
+    default="https://pay-world.ru/paymentsystem/enter/"),
+
+StringValue(PAYMENT_GROUP,
+    'POST_TEST_URL',
+    description=_('Post URL'),
+    help_text=_('The Payworld URL for test transaction posting.'),
+    default="https://sandbox.pay-world.ru/paymentsystem/enter/"),
+
+StringValue(PAYMENT_GROUP,
+    'BUSINESS',
+    description=_('Payworld account email'),
+    help_text=_('The email address for your payworld account'),
+    default=""),
+
+StringValue(PAYMENT_GROUP,
+    'BUSINESS_TEST',
+    description=_('Payworld test account email'),
+    help_text=_('The email address for testing your payworld account'),
+    default=""),
+
+StringValue(PAYMENT_GROUP,
+    'STORE_ID',
+    description=_('Store id'),
+    help_text=_('The store id from your payworld account settings'),
+    default=""),
+
+StringValue(PAYMENT_GROUP,
+    'SECRET_CODE',
+    description=_('Secret code'),
+    help_text=_('Store secret code from your payworld account settings'),
+    default=""),
+
+BooleanValue(PAYMENT_GROUP,
+    'LIVE',
+    description=_("Accept real payments"),
+    help_text=_("False if you want to be in test mode"),
+    default=False),
+
+ModuleValue(PAYMENT_GROUP,
+    'MODULE',
+    description=_('Implementation module'),
+    hidden=True,
+    default = 'satchmo_payment_payworld'),
+
+StringValue(PAYMENT_GROUP,
+    'KEY',
+    description=_("Module key"),
+    hidden=True,
+    default = 'SATCHMO_PAYMENT_PAYWORLD'),
+
+StringValue(PAYMENT_GROUP,
+    'LABEL',
+    description=_('English name for this group on the checkout screens'),
+    default = 'PayWorld',
+    dummy = _('PayWorld'), # Force this to appear on po-files
+    help_text = _('This will be passed to the translation utility')),
+
+StringValue(PAYMENT_GROUP,
+    'URL_BASE',
+    description=_('The url base used for constructing urlpatterns which will use this module'),
+    default = '^payworld/'),
+
+BooleanValue(PAYMENT_GROUP,
+    'EXTRA_LOGGING',
+    description=_("Verbose logs"),
+    help_text=_("Add extensive logs during post."),
+    default=False)
+)
+
+PAYMENT_GROUP['TEMPLATE_OVERRIDES'] = {
+    'shop/checkout/confirm.html' : 'shop/checkout/payworld/confirm.html',
+}
---
+++ packages/satchmo_payment_payworld/satchmo_payment_payworld-0.1.tar.gz/satchmo_payment_payworld-0.1/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "satchmo_payment_payworld",
+    version = "0.1",
+    author = "Ivan A. Anishchuk",
+    author_email = "anishchuk_ia@lavabit.com",
+    description = ("Intergration pay-world.ru for satchmo"),
+    license = "CC0",
+    keywords = "payment django satchmo integration",
+    url = "https://bitbucket.com/ib_soft/satchmo_payment_payworld",
+    packages=['satchmo_payment_payworld'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Programming Language :: Python :: 2 :: Only",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    ],
+    install_requires=[
+        'setuptools',
+        'django',
+        'Satchmo'
+    ],
+)
