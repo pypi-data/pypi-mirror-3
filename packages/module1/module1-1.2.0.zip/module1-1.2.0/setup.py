5fa8f5d3eb3cea815df64559477bce02acd622fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:15 +0000

    Add package module1/1.2.0/module1-1.2.0.zip

---
+++ packages/module1/module1-1.2.0.zip/module1-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+	   name = "module1",
+	   version = "1.2.0",
+	   py_mocules = ["nester"],
+	   author = "hfpython",
+           author_email = "hfpython@headfirstlabs.com",
+	   url = "http://www.headfirstlabs.com",
+	   descripton = "A simple printer of nested lists",
+         )
