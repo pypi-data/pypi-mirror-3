65faa240ef709a4821e9680867841859d53369b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:21 +0000

    Add package django-posts/0.0.8/django-posts-0.0.8.tar.gz

---
+++ packages/django-posts/django-posts-0.0.8.tar.gz/django-posts-0.0.8/posts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, editable=False)
+    headline = models.CharField(max_length=255)
+    text = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-creation_date']
+
+    def __unicode__(self):
+        return self.headline
+
+class PositionPostMixin(models.Model):
+    position = models.IntegerField()
+
+    class Meta:
+        ordering = ['position']
+        abstract = True
+
+class ImagePostMixin(models.Model):
+    image = models.CharField(max_length=40)
+
+    class Meta:
+        abstract = True
