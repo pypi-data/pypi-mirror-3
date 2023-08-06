858cccd492473723cd303d7c9473a03a44acbdac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:29 +0000

    Add package django-autoroutes/0.1.0/django-autoroutes-0.1.0.zip

---
+++ packages/django-autoroutes/django-autoroutes-0.1.0.zip/django-autoroutes-0.1.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name = 'django-autoroutes',
+    description = 'Module to automate routes for django projects',
+    version = '0.1.0',
+    author = 'Tony Kamillo Borges Reis',
+    author_email = 'tonysacanix@gmail.com',
+    maintainer = 'Tony Kamillo Borges Reis',
+    maintainer_email = 'tonysacanix@gmail.com',
+    url = 'https://github.com/sacanix/django-autoroutes',    
+    py_modules = ['django_autoroutes',]
+)
