ca4acfc90034699162235189c2ed6e1d83fa3726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:32 +0000

    Add package ofxclient/0.9.0/ofxclient-0.9.0.tar.gz

---
+++ packages/ofxclient/ofxclient-0.9.0.tar.gz/ofxclient-0.9.0/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+import ofxclient.version
+
+setup(name='ofxclient',
+      app=["osx/OFXClient.py"],
+      options={
+        'py2app': {
+            'iconfile': 'osx/image128.icns',
+            'resources': 'osx/image128.png, ofxclient/webapp/html',
+            'excludes': 'pygments',
+            'plist': {
+                'LSBackgroundOnly': True
+            }
+        }
+      },
+      version=ofxclient.version.__version__,
+      description="OFX client for dowloading transactions from banks",
+      long_description=open("./README.md", "r").read(),
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          ],
+      keywords='ofx, Open Financial Exchange, download transactions',
+      author='David Bartle',
+      author_email='captindave@gmail.com',
+      url='https://github.com/captin411/ofxclient',
+      license='MIT License',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={
+          'console_scripts': [
+              'ofxclientd = ofxclient.server:cmdline'
+          ]
+      },
+      install_requires=[
+          "ofxhome",
+          "ofxparse>0.8",
+          "simplejson",
+          "BeautifulSoup>=3.0",
+          "cherrypy",
+          "mako",
+          "keyring"
+      ],
+      test_suite='tests',
+      )
