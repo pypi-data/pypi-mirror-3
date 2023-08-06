ea7a1f02b2515e2b35c4901454ba13db669b26a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:44 +0000

    Add package marrow.templating/1.0.2/marrow.templating-1.0.2.tar.gz

---
+++ packages/marrow.templating/marrow.templating-1.0.2.tar.gz/marrow.templating-1.0.2/marrow/templating/template/mako_.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+from __future__ import unicode_literals
+
+from marrow.templating.core import Engine
+from marrow.templating.resolver import Resolver
+
+try:
+    from mako.template import Template
+
+except ImportError:  # pragma: no cover
+    raise ImportError('You must install the mako package.')
+
+
+__all__ = ['Mako']
+
+
+resolve = Resolver()
+
+
+class Mako(Engine):
+    def prepare(self, filename, **options):
+        return self.get_template(filename, **options)
+    
+    def render(self, template, data, **options):
+        return options.get('content_type', b'text/html'), template.render_unicode(**data)
+    
+    def get_template(self, uri, **options):
+        filename = resolve(uri)[1]
+        options.pop('content_type', None)
+        return Template(filename=filename, lookup=self, **options)
+    
+    def adjust_uri(self, uri, relativeto):
+        return uri
