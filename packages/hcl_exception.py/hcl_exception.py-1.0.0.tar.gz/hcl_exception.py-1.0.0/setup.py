0a2177ab61dee2d5853094d2907ad6b45178f691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:49 +0000

    Add package hcl_exception.py/1.0.0/hcl_exception.py-1.0.0.tar.gz

---
+++ packages/hcl_exception.py/hcl_exception.py-1.0.0.tar.gz/hcl_exception.py-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='hcl_exception.py',
+	version='1.0.0',
+	py_modules=['hcl_exception'],
+	author='wanliguhong',
+	email='huchaoling0823@126.com',
+	url='http://pypi.python.org/',
+	description='char data.',
+	)
