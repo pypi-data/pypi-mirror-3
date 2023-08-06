c307724869058e64818b0eeb3073b1ebd38d020d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:25 +0000

    Add package pygmyplot/0.2.11rc/pygmyplot-0.2.11rc.tar.gz

---
+++ packages/pygmyplot/pygmyplot-0.2.11rc.tar.gz/pygmyplot-0.2.11rc/info.py
@@ -0,0 +1,9 @@
+# This file is automatically created by make from PackageInfo.cfg
+package = "pygmyplot"
+version = "0.2"
+release = "0.2.11rc"
+author = "James C. Stroud"
+email = "jstroud@mbi.ucla.edu"
+url = "http://pygmyplot.bravais.net"
+copyright = "Copyright (c) 2012, James C. Stroud; All rights reserved."
+description = "Matplotlib wrapper plotting library"
