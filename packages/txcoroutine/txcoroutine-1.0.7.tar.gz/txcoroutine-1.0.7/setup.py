7fae80931926fb01d0586cbd93415e48a7dfbfc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:18 +0000

    Add package txcoroutine/1.0.7/txcoroutine-1.0.7.tar.gz

---
+++ packages/txcoroutine/txcoroutine-1.0.7.tar.gz/txcoroutine-1.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name="txcoroutine",
+    description="Coroutines for Twisted with tail call optimization support",
+    long_description=long_description,
+    version="1.0.7",
+    packages=find_packages(),
+
+    install_requires=[
+        'twisted',
+    ],
+
+    author="Erik Allik",
+    author_email="eallik@gmail.com",
+    license="BSD",
+    url="http://github.com/eallik/txcoroutine/"
+)
