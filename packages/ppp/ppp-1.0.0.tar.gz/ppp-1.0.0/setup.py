1e33755a434201f6867c7d0bed32fc93a08e54f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:01 +0000

    Add package ppp/1.0.0/ppp-1.0.0.tar.gz

---
+++ packages/ppp/ppp-1.0.0.tar.gz/ppp-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+      name='ppp',
+      version ='1.0.0',
+      py_modules=['ppp'],
+     author='sandesh',
+     email='sandeshsingh@hotmail.com',
+     desc='a simple program for nested list'
+         )
