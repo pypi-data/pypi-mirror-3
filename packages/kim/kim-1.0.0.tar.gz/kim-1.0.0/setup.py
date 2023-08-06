266da27c23c508e06eac76b5d5c0aae51f4ad081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:16 +0000

    Add package kim/1.0.0/kim-1.0.0.tar.gz

---
+++ packages/kim/kim-1.0.0.tar.gz/kim-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name ='kim',
+	version='1.0.0',
+	py_modul =['kim'],
+	author='KimJiHye',
+	author_email='icanw15@hanmail.net',
+	url = '10.10.111.43',
+	description = 'a simpel',
+     )
