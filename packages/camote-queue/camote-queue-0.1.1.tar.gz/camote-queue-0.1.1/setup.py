ae605aedb743a1380b91aa564b038d8a67c1a5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:38 +0000

    Add package camote-queue/0.1.1/camote-queue-0.1.1.tar.gz

---
+++ packages/camote-queue/camote-queue-0.1.1.tar.gz/camote-queue-0.1.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import camote
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist register upload')
+    sys.exit()
+
+packages = ['camote']
+requires = ['redis']
+
+setup(
+    name='camote-queue',
+    version=camote.__version__,
+    description='Redis based queue that supports fetching job position.',
+    author='Marconi Moreto',
+    author_email='caketoad@gmail.com',
+    url='https://github.com/marconi/camote-queue',
+    packages=packages,
+    zip_safe=False,
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=requires,
+    license=open("LICENSE").read(),
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
