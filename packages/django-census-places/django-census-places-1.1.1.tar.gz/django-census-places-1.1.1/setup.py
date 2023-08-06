ad2bbc551bafcf3aa58e519ac1d6eeba95081d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:58 +0000

    Add package django-census-places/1.1.1/django-census-places-1.1.1.tar.gz

---
+++ packages/django-census-places/django-census-places-1.1.1.tar.gz/django-census-places-1.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='django-census-places',
+    version='1.1.1',
+    url='http://bitbucket.org/latestrevision/django-census-places/',
+    description='Use city (and census designated place) boundaries provided by the United States Census',
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    packages=['census_places', 'census_places.management.commands'],
+)
