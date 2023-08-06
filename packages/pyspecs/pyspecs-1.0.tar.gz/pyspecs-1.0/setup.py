70594b49780851cb06a900362e2e39efce225b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:14 +0000

    Add package pyspecs/1.0/pyspecs-1.0.tar.gz

---
+++ packages/pyspecs/pyspecs-1.0.tar.gz/pyspecs-1.0/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+
+
+setup(
+    name='pyspecs',
+    version='1.0',
+    packages=['pyspecs'],
+    scripts=['bin/pyspecs', 'bin/pyspecs_idle'],
+    entry_points={
+        'console_scripts': [
+            'pyspecs = pyspecs._program:main',
+            'pyspecs_idle = pyspecs._idle:main',
+        ]
+    },
+    url='http://github.com/mdwhatcott/pyspecs',
+    license='GPL',
+    author='Michael Whatcott',
+    author_email='mdwhatcott+pyspecs@gmail.com',
+    description='Minimalistic BDD in Python',
+    long_description='pyspecs is a testing framework that strives to achieve '
+                     'more readable specifications (tests) by leveraging '
+                     'some fancy syntactic sugar and auto-discovery of ' \
+                     'tests/specs.',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+    ]
+)
