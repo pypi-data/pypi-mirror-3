222db61b846a1135b8d3c34496976242a65e3cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:55 +0000

    Add package mushroom/0.3.1/mushroom-0.3.1.tar.gz

---
+++ packages/mushroom/mushroom-0.3.1.tar.gz/mushroom-0.3.1/mushroom/server.py
@@ -0,0 +1,16 @@
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+
+from mushroom.application import Application
+
+
+class Server(WSGIServer):
+
+    def __init__(self, listener, rpc_handler=None, auth_handler=None):
+        super(Server, self).__init__(listener,
+                Application(rpc_handler, auth_handler),
+                handler_class=WebSocketHandler)
+
+    @property
+    def sessions(self):
+        return self.application.sessions
