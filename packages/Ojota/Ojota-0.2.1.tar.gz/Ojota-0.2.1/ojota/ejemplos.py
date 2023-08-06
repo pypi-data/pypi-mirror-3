0648511a0a333baa3a2e7c60f5e9e028eb59bab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:44 +0000

    Add package Ojota/0.2.1/Ojota-0.2.1.tar.gz

---
+++ packages/Ojota/Ojota-0.2.1.tar.gz/Ojota-0.2.1/ojota/ejemplos.py
@@ -0,0 +1,48 @@
+"""
+This file is part of Ojota.
+
+    Ojota is free software: you can redistribute it and/or modify
+    it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ojota is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU  Lesser General Public License
+    along with Ojota.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from ojota import Ojota, Relation
+
+
+class Espectaculo(Ojota):
+
+    """Lista que agrupa espectaculos."""
+    plural_name = 'Espectaculos'
+    pk_field = 'id_espectaculo'
+
+
+class Equipo(Ojota):
+    """Lista que agrupa personas."""
+    plural_name = 'Equipos'
+    pk_field = 'codigo'
+
+    required_fields = ('codigo',)
+
+
+class Persona(Ojota):
+    """Lista que agrupa personas."""
+    plural_name = 'Personas'
+    pk_field = 'codigo'
+
+    #required_fields = ('nombre', 'apellido', 'edad', 'estatura', 'cod_equipo')
+    equipo = Relation('cod_equipo', Equipo, related_name='personas')
+
+
+class Animal(Ojota):
+    """Lista que agrupa animales."""
+    plural_name = 'Animales'
+    pk_field = 'codigo'
