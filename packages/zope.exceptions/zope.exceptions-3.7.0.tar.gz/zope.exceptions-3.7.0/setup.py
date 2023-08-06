2db2b6ff4a995fad9711b2049670e389532e0546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:46 +0000

    Add package zope.exceptions/3.7.0/zope.exceptions-3.7.0.tar.gz

---
+++ packages/zope.exceptions/zope.exceptions-3.7.0.tar.gz/zope.exceptions-3.7.0/setup.py
@@ -0,0 +1,75 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.exceptions package
+"""
+import os
+from setuptools import setup, find_packages
+import sys
+if sys.version_info < (3, ):
+    extra = {}
+else:
+    # Python 3 support:
+    extra = dict(
+        use_2to3=True,
+        setup_requires=['zope.fixers'],
+        use_2to3_fixers = ['zope.fixers'],
+    )
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(name='zope.exceptions',
+      version='3.7.0',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Zope Exceptions',
+      long_description=(read('README.txt') + '\n\n' +
+                        read('CHANGES.txt')),
+      keywords = 'zope exceptions',
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.4',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.exceptions',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      install_requires=['setuptools',
+                        'zope.interface',
+                       ],
+      test_suite = 'zope.exceptions.tests',
+      include_package_data = True,
+      zip_safe = False,
+      **extra)
