e7cde053ebf7c4d55fffaa7609b7cda56491baed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:16 +0000

    Add package pyDBF/0.1/pyDBF-0.1.zip

---
+++ packages/pyDBF/pyDBF-0.1.zip/pyDBF-0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pyDBF',
+    version='0.1',
+    description='Search, read and write Dbase and Xbase files',
+    author='Tyrion MX',
+    author_email='tyrion.mx@gmail.com',
+    url='http://assembla.com/spaces/pyDBF',
+    packages=['dbf'],
+    keywords=['dbf', 'Dbase', 'Xbase'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)
