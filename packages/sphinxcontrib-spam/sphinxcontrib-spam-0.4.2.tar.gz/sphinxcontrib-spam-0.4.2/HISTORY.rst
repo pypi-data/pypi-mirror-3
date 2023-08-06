28379a4c79622d43c1bf10bae4bb5c85562d4deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:58 +0000

    Add package sphinxcontrib-spam/0.4.2/sphinxcontrib-spam-0.4.2.tar.gz

---
+++ packages/sphinxcontrib-spam/sphinxcontrib-spam-0.4.2.tar.gz/sphinxcontrib-spam-0.4.2/HISTORY.rst
@@ -0,0 +1,19 @@
+History
+=======
+
+0.4.2 (May 28, 2012)
+--------------------
+- modify LICENSE
+
+0.4.1 (May 27, 2012)
+--------------------
+- modify long_description
+
+0.4 (May 27, 2012)
+------------------
+- create spam role.
+
+
+\-0.3
+-----------
+- garden releases
---
+++ packages/sphinxcontrib-spam/sphinxcontrib-spam-0.4.2.tar.gz/sphinxcontrib-spam-0.4.2/test/test_spam.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from sphinxcontrib import spam
+from docutils.parsers.rst.states import Inliner
+
+
+def test_pass():
+    assert True
+
+
+def test_spam_role():
+    u'''ロールが指定された場合、正しいnodeが出力されること。
+    '''
+    ilr = Inliner()
+    node = spam.spam_role('spam', u':spam:`あばばばば`',
+            u'あばばばば', 123, ilr)[0][0]
+    assert unicode(node) == u'spam! あばばばば spam!'
