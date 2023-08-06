7063eb1ad017d8dcb19c3ed70c6ab3d58b7b0ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:15 +0000

    Add package warlock/0.2.0/warlock-0.2.0.tar.gz

---
+++ packages/warlock/warlock-0.2.0.tar.gz/warlock-0.2.0/warlock/core.py
@@ -0,0 +1,66 @@
+"""Core Warlock functionality"""
+
+import copy
+
+import jsonschema
+
+
+class InvalidOperation(RuntimeError):
+    pass
+
+
+class ValidationError(ValueError):
+    pass
+
+
+def model_factory(schema):
+    """Generate a model class based on the provided JSON Schema
+
+    :param schema: dict representing valid JSON schema
+    """
+    schema = copy.deepcopy(schema)
+
+    def validator(obj):
+        """Apply a JSON schema to an object"""
+        try:
+            jsonschema.validate(obj, schema)
+        except jsonschema.ValidationError:
+            raise ValidationError()
+
+    class Model(object):
+        """Self-validating model for arbitrary objects"""
+        def __init__(self, **kwargs):
+            self.__dict__['validator'] = validator
+            try:
+                self.__dict__['validator'](kwargs)
+            except ValidationError:
+                raise ValueError()
+            else:
+                self.__dict__['raw'] = kwargs
+
+        def __getattr__(self, key):
+            try:
+                return self.__dict__['raw'][key]
+            except KeyError:
+                raise AttributeError(key)
+
+        def __setattr__(self, key, value):
+            mutation = copy.deepcopy(self.__dict__['raw'])
+            mutation[key] = value
+            try:
+                self.__dict__['validator'](mutation)
+            except ValidationError:
+                raise InvalidOperation()
+            self.__dict__['raw'] = mutation
+
+        def __getitem__(self, key):
+            return self.__getattr__(key)
+
+        def __setitem__(self, key, value):
+            return self.__setattr__(key, value)
+
+        def iteritems(self):
+            return copy.deepcopy(self.__dict__['raw']).iteritems()
+
+    Model.__name__ = str(schema['name'])
+    return Model
