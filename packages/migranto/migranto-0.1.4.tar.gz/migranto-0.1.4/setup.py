0d72b6cbfddcf65d7f89b320ce3cea2d04492087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:50 +0000

    Add package migranto/0.1.4/migranto-0.1.4.tar.gz

---
+++ packages/migranto/migranto-0.1.4.tar.gz/migranto-0.1.4/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+setup(
+    name='migranto',
+    version='0.1.4',
+    author='Alexey Novikov',
+    author_email='velocityzen@gmail.com',
+    packages=find_packages(),
+    install_requires = ["psycopg2"],
+    url='https://github.com/velocityzen/migranto',
+    license='BSD',
+    description='Simple SQL migration tool for SQLite and PostgreSQL',
+    long_description = read('README.md'),
+    zip_safe=False,
+    entry_points="""
+    [console_scripts]
+    migranto = migranto:main
+    """
+)
