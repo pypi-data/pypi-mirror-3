a277bb2812f52f28e9eb7cbf850164f5abb28731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:26 +0000

    Add package imagy/0.0.1/imagy-0.0.1.tar.gz

---
+++ packages/imagy/imagy-0.0.1.tar.gz/imagy-0.0.1/imagy/tests/test_core.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from context import imagy
+from imagy.config import *
+from imagy.core import *
+from imagy.store import Store
+from path import path
+import imgtest
+
+import logging
+logging.disable(logging.CRITICAL)
+
+class ImagyTestSuite(unittest.TestCase):
+    def setUp(self):
+        self.images = imgtest.images
+        self.loc = imgtest.image_loc
+        self.tmp = self.loc.parent.joinpath('imagesTEMP')
+        self.loc.copytree(self.tmp)
+        self.s = Store()
+        self.img_locs = dict((k, self.get_imgp(v)) for k, v in self.images.items())
+
+    def get_imgp(self, img):
+        return self.tmp.joinpath(img)
+
+    def tearDown(self):
+        self.tmp.rmtree()
+
+    def check_image(self, img):
+        img = self.get_imgp(img)
+        sz = img.size
+        compress_image(img)
+        self.assertTrue(sz >= img.size)
+
+    def test_store_original(self):
+        img = self.img_locs['jpg']
+        orig = store_original(img)
+        self.assertTrue(same_file(img, orig))
+        orig.remove()
+        
+    def test_make_path(self):
+        self.assertFalse(make_path(__file__).exists())
+
+    def test_clear(self):
+        thing = 'thing'
+        self.s.originals[thing] = ''
+        self.s.clear()
+        self.s.save()
+        self.assertFalse(thing in self.s.originals)
+
+def main():
+    # dynamically add tests for various file formats, SO FN DRY
+    for typ, pth in imgtest.images.items():
+        fn = lambda self:self.check_image(pth)
+        setattr(ImagyTestSuite, 'test_%s' % typ, fn)
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
