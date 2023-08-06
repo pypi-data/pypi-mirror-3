748ef9cdc1779d894aca0ff4acc804cb515f8394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:48 +0000

    Add package guesstimator/0.0.1/guesstimator-0.0.1.tar.gz

---
+++ packages/guesstimator/guesstimator-0.0.1.tar.gz/guesstimator-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name="guesstimator",
+      version="0.0.1",
+      description="Estimates the performance of a distributed system based on a sample set of data.",
+      author="Benjamin Coe",
+      author_email="ben@attachments.me",
+      entry_points = {},
+      url="https://github.com/bcoe/guesstimator",
+      packages = find_packages(),
+      install_requires = ['redis'],
+      tests_require=['nose']
+)
