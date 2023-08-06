c7ea91e164aaf871c5e858943d7959e414e9727b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:05 +0000

    Add package mo2s3/0.0.1/mo2s3-0.0.1-py2.7.egg

---
+++ packages/mo2s3/mo2s3-0.0.1-py2.7.egg/mo2s3/__init__.py
@@ -0,0 +1,9 @@
+default_conf = """[aws]
+access_key = 
+secret_key = 
+s3_bucket = 
+
+[mongodb]
+host = 
+username = 
+password = """
---
+++ packages/mo2s3/mo2s3-0.0.1-py2.7.egg/mo2s3/conf.py
@@ -0,0 +1,9 @@
+[aws]
+access_key = 
+secret_key = 
+s3_bucket = 
+
+[mongodb]
+host = 
+username = 
+password = 
