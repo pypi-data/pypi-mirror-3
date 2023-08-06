6f833813bce4b121939491481a63d9ff36c9e778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:57 +0000

    Add package mdx_smartypants/1.0.4/mdx_smartypants-1.0.4.tar.gz

---
+++ packages/mdx_smartypants/mdx_smartypants-1.0.4.tar.gz/mdx_smartypants-1.0.4/setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+readme = open('README.rst', 'r')
+README_TEXT = readme.read()
+readme.close()
+
+setup(
+    name='mdx_smartypants',
+    version='1.0.4',
+    author='Jonathan Eunice',
+    author_email='jonathan.eunice@gmail.com',
+    description='Python-Markdown extension that uses smartypants to provide typographically nicer ("curly") quotes, proper ("em" and "en") dashes, etc.',
+    long_description=README_TEXT,
+    url='http://bitbucket.org/jeunice/mdx_smartypants',
+    py_modules=['mdx_smartypants'],
+    install_requires=['Markdown>=2.0','smartypants>=1.6'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Markup :: HTML'
+    ]
+)
