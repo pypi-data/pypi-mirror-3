d5bdd1a59c93104a46ca3125471ba9f595c77cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:41 +0000

    Add package pyramid_handlers/0.5/pyramid_handlers-0.5.tar.gz

---
+++ packages/pyramid_handlers/pyramid_handlers-0.5.tar.gz/pyramid_handlers-0.5/README.rst
@@ -0,0 +1,13 @@
+``pyramid_handlers``
+====================
+
+``pyramid_handlers`` is a package which allows Pyramid to largely emulate the
+functionality of Pylons 1 "controllers".  Handlers are a synthesis of
+Pyramid *url dispatch* and method introspection of a view class that makes it
+easier to create bundles of view logic which reacts to particular route
+patterns.  It works under Python 2.6, 2.7, and 3.2.
+
+See `http://docs.pylonsproject.org/projects/pyramid_handlers/en/latest/
+<http://docs.pylonsproject.org/projects/pyramid_handlers/en/latest/>`_ for
+detailed documentation.
+
---
+++ packages/pyramid_handlers/pyramid_handlers-0.5.tar.gz/pyramid_handlers-0.5/setup.py
@@ -0,0 +1,73 @@
+##############################################################################
+#
+# Copyright (c) 2008-2011 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+install_requires=[
+    'pyramid>=1.0a10',
+    ]
+
+PY3 = sys.version_info[0] == 3
+
+# pyramid_zcml 0.9.2 required for with_context function
+
+if PY3:
+    tests_require = []
+else:
+    tests_require = ['pyramid_zcml>=0.9.2']
+
+setup(name='pyramid_handlers',
+      version='0.5',
+      description='Pyramid handlers emulate Pylons 1 controllers',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "License :: Repoze Public License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Framework :: Pylons",
+        ],
+      keywords='web wsgi pylons pyramid',
+      author="Chris McDonough, Agendaless Consulting",
+      author_email="pylons-devel@googlegroups.com",
+      url="http://docs.pylonsproject.org",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires = install_requires,
+      tests_require = tests_require,
+      test_suite="pyramid_handlers",
+      entry_points = """
+      """
+      )
+
