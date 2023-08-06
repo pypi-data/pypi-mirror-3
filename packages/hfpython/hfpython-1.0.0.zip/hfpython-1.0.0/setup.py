84ed38e8b9fc5048f123ad0da3158c5dff727219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:34 +0000

    Add package hfpython/1.0.0/hfpython-1.0.0.zip

---
+++ packages/hfpython/hfpython-1.0.0.zip/hfpython-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'hfpython',
+	version = '1.0.0',
+	py_modules = ['hfpython'],
+	author = 'SUGA',
+	author_email = 'SUGA@SUGA.com',
+	url = 'www.suga.com',
+	description = 'A simple discription for test',
+	)
