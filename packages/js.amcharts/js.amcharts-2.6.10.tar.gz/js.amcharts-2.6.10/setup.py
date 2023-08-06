ded41f38c4ff654a8fedce5d25ec36576f30ab84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:44 +0000

    Add package js.amcharts/2.6.10/js.amcharts-2.6.10.tar.gz

---
+++ packages/js.amcharts/js.amcharts-2.6.10.tar.gz/js.amcharts-2.6.10/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+# The version of the wrapped library is the starting point for the
+# version number of the python package.
+# In bugfix releases of the python package, add a '-' suffix and an
+# incrementing integer.
+# For example, a packaging bugfix release version 1.4.4 of the
+# js.jquery package would be version 1.4.4-1 .
+
+version = '2.6.10'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+    read('js', 'amcharts', 'test_amcharts.txt')
+    + '\n' +
+    read('CHANGES.txt'))
+
+setup(
+    name='js.amcharts',
+    version=version,
+    description="Fanstatic packaging of amCharts",
+    long_description=long_description,
+    classifiers=[],
+    keywords='',
+    author='Fanstatic Developers',
+    author_email='fanstatic@googlegroups.com',
+    license='BSD',
+    packages=find_packages(),namespace_packages=['js'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'fanstatic',
+        'setuptools',
+        ],
+    entry_points={
+        'fanstatic.libraries': [
+            'amcharts = js.amcharts:library',
+            ],
+        },
+    )
