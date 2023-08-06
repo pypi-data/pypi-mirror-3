68c68bb2f9781b6d522db2c4276a7e24a762651b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:40 +0000

    Add package jobmine/1.0/jobmine-1.0.tar.gz

---
+++ packages/jobmine/jobmine-1.0.tar.gz/jobmine-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name = 'jobmine', 
+      version = '1.0',
+      description = 'Jobmine package for Waterloo students',
+      author = 'Michael Dong',
+      author_email = 'yhdong@uwaterloo.ca',
+      py_modules = ['jobmine'],
+     ) 
