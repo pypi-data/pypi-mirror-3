9fb56862aa520081b8a95419316466c503b54961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:42 +0000

    Add package jmbo-gallery/0.1/jmbo-gallery-0.1.tar.gz

---
+++ packages/jmbo-gallery/jmbo-gallery-0.1.tar.gz/jmbo-gallery-0.1/CHANGELOG.rst
@@ -0,0 +1,12 @@
+Changelog
+=========
+
+0.1
+---
+#. Add dependency on `jmbo-foundry`.
+#. Port templates from `jmbo-foundry`.
+
+0.0.3
+-----
+#. Clean up detail view.
+
---
+++ packages/jmbo-gallery/jmbo-gallery-0.1.tar.gz/jmbo-gallery-0.1/gallery/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from jmbo.admin import ModelBaseAdmin
+
+from gallery.models import Gallery, GalleryImage, VideoEmbed, VideoFile
+
+class GalleryImageAdmin(ModelBaseAdmin):
+    list_display = ModelBaseAdmin.list_display + ('gallery',)
+    list_filter = ModelBaseAdmin.list_filter + ('gallery',)
+
+admin.site.register(Gallery, ModelBaseAdmin)
+admin.site.register(GalleryImage, GalleryImageAdmin)
+admin.site.register(VideoEmbed, ModelBaseAdmin)
+admin.site.register(VideoFile, ModelBaseAdmin)
