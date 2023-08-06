bf9174cb19c3d94a6166074157b01a11100e91b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:58 +0000

    Add package urllib3/1.5/urllib3-1.5.tar.gz

---
+++ packages/urllib3/urllib3-1.5.tar.gz/urllib3-1.5/test/test_response.py
@@ -0,0 +1,68 @@
+import unittest
+
+from io import BytesIO
+
+from urllib3.response import HTTPResponse
+from urllib3.exceptions import DecodeError
+
+class TestLegacyResponse(unittest.TestCase):
+    def test_getheaders(self):
+        headers = {'host': 'example.com'}
+        r = HTTPResponse(headers=headers)
+        self.assertEqual(r.getheaders(), headers)
+
+    def test_getheader(self):
+        headers = {'host': 'example.com'}
+        r = HTTPResponse(headers=headers)
+        self.assertEqual(r.getheader('host'), 'example.com')
+
+
+class TestResponse(unittest.TestCase):
+    def test_cache_content(self):
+        r = HTTPResponse('foo')
+        self.assertEqual(r.data, 'foo')
+        self.assertEqual(r._body, 'foo')
+
+    def test_default(self):
+        r = HTTPResponse()
+        self.assertEqual(r.data, None)
+
+    def test_none(self):
+        r = HTTPResponse(None)
+        self.assertEqual(r.data, None)
+
+    def test_preload(self):
+        fp = BytesIO(b'foo')
+
+        r = HTTPResponse(fp, preload_content=True)
+
+        self.assertEqual(fp.tell(), len(b'foo'))
+        self.assertEqual(r.data, b'foo')
+
+    def test_no_preload(self):
+        fp = BytesIO(b'foo')
+
+        r = HTTPResponse(fp, preload_content=False)
+
+        self.assertEqual(fp.tell(), 0)
+        self.assertEqual(r.data, b'foo')
+        self.assertEqual(fp.tell(), len(b'foo'))
+
+    def test_decode_bad_data(self):
+        fp = BytesIO(b'\x00' * 10)
+        self.assertRaises(DecodeError, HTTPResponse, fp, headers={
+            'content-encoding': 'deflate'
+        })
+
+    def test_decode_deflate(self):
+        import zlib
+        data = zlib.compress(b'foo')
+
+        fp = BytesIO(data)
+        r = HTTPResponse(fp, headers={'content-encoding': 'deflate'})
+
+        self.assertEqual(r.data, b'foo')
+
+
+if __name__ == '__main__':
+    unittest.main()
