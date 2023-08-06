6a17c0a4d42ea089f33fb141b4abb7bd1bd78d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:13 +0000

    Add package namedentities/1.0.2/namedentities-1.0.2.tar.gz

---
+++ packages/namedentities/namedentities-1.0.2.tar.gz/namedentities-1.0.2/setup.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+readme = open('README.txt', 'r')
+README_TEXT = readme.read()
+readme.close()
+
+setup(
+    name='namedentities',
+    version='1.0.2',
+    author='Jonathan Eunice',
+    author_email='jonathan.eunice@gmail.com',
+    description='Simple way to convert numeric HTML entities to far more readable named entities.',
+    long_description=README_TEXT,
+    url='http://bitbucket.org/jeunice/namedentities',
+    py_modules=['namedentities' ],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Markup :: HTML'
+    ]
+)
