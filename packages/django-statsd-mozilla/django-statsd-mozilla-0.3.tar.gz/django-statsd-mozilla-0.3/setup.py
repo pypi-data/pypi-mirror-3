63264b8309e154bee15c726ca28cd3ebdecfa4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:00 +0000

    Add package django-statsd-mozilla/0.3/django-statsd-mozilla-0.3.tar.gz

---
+++ packages/django-statsd-mozilla/django-statsd-mozilla-0.3.tar.gz/django-statsd-mozilla-0.3/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+
+setup(
+    # Because django-statsd was taken, I called this django-statsd-mozilla.
+    name='django-statsd-mozilla',
+    version='0.3',
+    description='Django interface with statsd',
+    long_description=open('README.rst').read(),
+    author='Andy McKay',
+    author_email='andym@mozilla.com',
+    license='BSD',
+    install_requires=['statsd'],
+    packages=['django_statsd',
+              'django_statsd/patches',
+              'django_statsd/clients'],
+    url='https://github.com/andymckay/django-statsd',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Framework :: Django'
+        ],
+    )
