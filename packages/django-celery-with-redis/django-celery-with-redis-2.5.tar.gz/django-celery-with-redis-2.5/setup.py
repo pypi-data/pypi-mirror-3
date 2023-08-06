5d8f57c84816382a955b01887ba609ed64d5ac68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:41 +0000

    Add package django-celery-with-redis/2.5/django-celery-with-redis-2.5.tar.gz

---
+++ packages/django-celery-with-redis/django-celery-with-redis-2.5.tar.gz/django-celery-with-redis-2.5/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import os
+import codecs
+
+from setuptools import setup
+
+if os.path.exists("README"):
+    long_description = codecs.open("README", "r", "utf-8").read()
+else:
+    long_description = '''\
+This is a bundle of several packages that you can use as a shortcut in the
+requirements lists of your applications.  Bundles are used to follow a
+common group of packages, or a package with an optional extension feature.
+'''
+
+setup(name='django-celery-with-redis',
+      version='2.5',
+      description='''Bundle installing the dependencies for Django-Celery and Redis''',
+      author='''Celery Project''',
+      author_email='bundles@celeryproject.org',
+      url='''http://celeryproject.org''',
+      platforms=['all'],
+      license='''BSD''',
+      zip_safe=False,
+      install_requires=['django-celery>=2.5,<3.0', 'redis>=2.4.4'],
+      classifiers=[
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      long_description=long_description,
+)
