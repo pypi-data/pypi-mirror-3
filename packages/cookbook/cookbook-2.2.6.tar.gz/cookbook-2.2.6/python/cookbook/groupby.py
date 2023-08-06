3f93a5815b79fecd6e4269fe08bd54f6e33fb1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:16 +0000

    Add package cookbook/2.2.6/cookbook-2.2.6.tar.gz

---
+++ packages/cookbook/cookbook-2.2.6.tar.gz/cookbook-2.2.6/python/cookbook/groupby.py
@@ -0,0 +1,11 @@
+"""
+http://code.activestate.com/recipes/259173-groupby/
+"""
+
+
+class groupby(dict):
+    def __init__(self, seq, key=lambda x:x):
+        for value in seq:
+            k = key(value)
+            self.setdefault(k, []).append(value)
+    __iter__ = dict.iteritems
