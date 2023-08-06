555fc45d85ffb2fcd90419ec93eade3d542bb8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:04 +0000

    Add package testkit/0.1.7/testkit-0.1.7.tar.gz

---
+++ packages/testkit/testkit-0.1.7.tar.gz/testkit-0.1.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.7'
+
+LONG_DESCRIPTION = open('README.rst').read()
+
+setup(name='testkit',
+    version=VERSION,
+    description="testkit - A collection of tools for testing",
+    long_description=LONG_DESCRIPTION,
+    author='Reuven V. Gonzales',
+    author_email='reuven@tobetter.us',
+    url="https://github.com/ravenac95/testkit",
+    license='MIT',
+    platforms='Unix',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'fudge',
+    ],
+    entry_points={},
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Operating System :: POSIX',
+    ],
+)
