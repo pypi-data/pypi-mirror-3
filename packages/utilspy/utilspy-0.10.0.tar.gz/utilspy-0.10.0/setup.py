cfa11c920da55e4b392d45b41ff2a6f169b3dc71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:29 +0000

    Add package utilspy/0.10.0/utilspy-0.10.0.tar.gz

---
+++ packages/utilspy/utilspy-0.10.0.tar.gz/utilspy-0.10.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+VERSION = '0.10.0'
+
+setup(
+    name='utilspy',
+    version=VERSION,
+    author='prior',
+    author_email='mprior@hubspot.com',
+    packages=find_packages(),
+    url='https://github.com/HubSpot/utilspy',
+    download_url='https://github.com/HubSpot/utilspy/tarball/v%s'%VERSION,
+    license='LICENSE.txt',
+    description="A number of useful utilities that don't seem to exist elsewhere",
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'sanetime>=4,<5'
+        ],
+    platforms=['any']
+)
+
