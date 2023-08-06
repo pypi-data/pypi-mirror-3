4b9f09fd2248bb8f52543d25e5332e9b6e8aa79f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:45 +0000

    Add package emacs/24.1.50.1-20120610/emacs-24.1.50.1-20120610.tar.gz

---
+++ packages/emacs/emacs-24.1.50.1-20120610.tar.gz/emacs-24.1.50.1-20120610/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='emacs',
+    version='24.1.50.1-20120610',
+    author='Dave Love',
+    author_email='fx@gnu.org',
+    url='https://github.com/matthewlmcclure/python-emacs',
+    packages=[],
+    py_modules=['emacs', 'emacs2', 'emacs3'],
+    license='GPL 3',
+)
