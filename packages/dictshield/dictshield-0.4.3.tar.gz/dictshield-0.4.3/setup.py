7a92c85240626e0ceafb9cb58f14f9ccea9620f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:05 +0000

    Add package dictshield/0.4.3/dictshield-0.4.3.tar.gz

---
+++ packages/dictshield/dictshield-0.4.3.tar.gz/dictshield-0.4.3/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dictshield',
+      license='BSD',
+      version='0.4.3',
+      description='Python Library for typed dictionaries... sorta',
+      author='James Dennis',
+      author_email='jdennis@gmail.com',
+      url='http://github.com/j2labs/dictshield',
+      packages=['dictshield', 'dictshield.fields'])
