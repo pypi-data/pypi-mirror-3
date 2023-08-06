496421f3210dbfd262774a77adc2163c79ff9412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:28 +0000

    Add package cutools/0.1.1/cutools-0.1.1.tar.gz

---
+++ packages/cutools/cutools-0.1.1.tar.gz/cutools-0.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from cutools import VERSION
+
+def readfile(rfile):
+    try:
+        with open(rfile, 'w') as f:
+            return f.read()
+    except:
+        return ''
+
+setup(
+    name='cutools',
+    version='0.1.1',
+    description='Check local modifications in upstream branch',
+    long_description=readfile('README.rst'),
+    author='Eduard Carreras',
+    author_email='ecarreras@gmail.com',
+    url='https://github.com/ecarreras/cutools',
+    license=readfile('LICENSE'),
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    platforms=['Any'],
+    scripts=[],
+    entry_points={
+        'console_scripts': [
+            'cugit = cutools.cli.cugit:app.cmdline'
+        ]
+    },
+    provides=['cutools'],
+    install_requires=['clint', 'plumbum', 'subcmd'],
+)
