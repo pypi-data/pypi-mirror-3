4bbc19171d9fba8fd17f902138b6672df768e202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:20 +0000

    Add package gargoyle/0.8.2/gargoyle-0.8.2.tar.gz

---
+++ packages/gargoyle/gargoyle-0.8.2.tar.gz/gargoyle-0.8.2/tests/__init__.py
@@ -0,0 +1,6 @@
+"""
+:copyright: (c) 2010 DISQUS.
+:license: Apache License 2.0, see LICENSE for more details.
+"""
+
+from tests import *
---
+++ packages/gargoyle/gargoyle-0.8.2.tar.gz/gargoyle-0.8.2/tests/models.py
@@ -0,0 +1,4 @@
+"""
+:copyright: (c) 2010 DISQUS.
+:license: Apache License 2.0, see LICENSE for more details.
+"""
