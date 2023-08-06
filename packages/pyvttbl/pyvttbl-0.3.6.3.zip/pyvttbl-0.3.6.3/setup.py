195f39581cdac2643f6db48c4895966d8313d904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package pyvttbl/0.3.6.3/pyvttbl-0.3.6.3.zip

---
+++ packages/pyvttbl/pyvttbl-0.3.6.3.zip/pyvttbl-0.3.6.3/setup.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2011, Roger Lew [see LICENSE.txt]
+# This software is funded in part by NIH Grant P20 RR016454.
+
+##from distutils.core import setup
+from setuptools import setup
+
+setup(name='pyvttbl',
+    version='0.3.6.3',
+    description='Multidimensional pivot tables, data processing, statistical computation',
+    author='Roger Lew',
+    author_email='rogerlew@gmail.com',
+    license = "BSD",
+    classifiers=["Development Status :: 4 - Beta",
+                 "Intended Audience :: Developers",
+                 "Intended Audience :: Information Technology",
+                 "Intended Audience :: Science/Research",
+                 "License :: OSI Approved :: BSD License",
+                 "Natural Language :: English",
+                 "Programming Language :: Python :: 2.5",
+                 "Programming Language :: Python :: 2.6",
+                 "Programming Language :: Python :: 2.7",
+                 "Topic :: Database",
+                 "Topic :: Database :: Database Engines/Servers",
+                 "Topic :: Scientific/Engineering :: Information Analysis",
+                 "Topic :: Scientific/Engineering :: Mathematics",
+                 "Topic :: Software Development :: Libraries :: Python Modules"],
+    url='http://code.google.com/p/pyvttbl/',
+    packages=['pyvttbl',
+              'pyvttbl.test',
+              'pyvttbl.stats'])
+
+"""setup.py sdist upload --identity="Roger Lew" --sign"""
