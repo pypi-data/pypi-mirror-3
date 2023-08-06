b215b86b49d2f10b859c56c5bc57fda3e43bde60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:50 +0000

    Add package schachtler2.8/1.3.0/schachtler2.8-1.3.0.zip

---
+++ packages/schachtler2.8/schachtler2.8-1.3.0.zip/schachtler2.8-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name		= "schachtler2.8",
+	version		= "1.3.0",
+	py_modules	= ["schachtler"],
+	author		= "Araegis",
+	author_email	= "Lockon@web.de"
+	
+      )      	
