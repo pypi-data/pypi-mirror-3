1a69c0672c7a2ce06b25715dcc518cf1bd7d41e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:31 +0000

    Add package five.customerize/1.0.3/five.customerize-1.0.3.zip

---
+++ packages/five.customerize/five.customerize-1.0.3.zip/five.customerize-1.0.3/README.txt
@@ -0,0 +1,25 @@
+five.customerize
+================
+
+Overview
+--------
+
+``five.customerize`` provides the ability to locally customize Page
+Template-based browser views, much like it is possible to customize
+file-system based view templates in the CMF's portal_skin tools.
+
+Developer Resources
+-------------------
+
+- Subversion browser:
+
+  http://svn.zope.org/five.customerize
+
+- Read-only Subversion checkout:
+
+  $ svn co http://svn.zope.org/repos/main/five.customerize/trunk
+
+- Writable Subversion checkout:
+
+  $ svn co svn+ssh://svn.zope.org/repos/main/five.customerize/trunk
+
---
+++ packages/five.customerize/five.customerize-1.0.3.zip/five.customerize-1.0.3/setup.py
@@ -0,0 +1,65 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" Setup for five.customerize package """
+
+from setuptools import setup
+
+version = '1.0.3'
+
+setup(name = 'five.customerize',
+      version = version,
+      description = 'TTW customization of template-based Zope views',
+      long_description = (open('README.txt').read() + "\n" +
+                          open('CHANGES.txt').read()),
+      keywords = 'zope views templates customization ttw',
+      author = 'Zope Foundation and Contributors',
+      author_email = 'z3-five@codespeak.net',
+      url = 'http://pypi.python.org/pypi/five.customerize',
+      license = 'ZPL 2.1',
+      packages = ['five', 'five.customerize'],
+      package_dir = {'': 'src'},
+      namespace_packages = ['five',],
+      include_package_data = True,
+      platforms = 'Any',
+      zip_safe = False,
+      install_requires=[
+        'setuptools',
+        'plone.portlets',
+        'zope.component',
+        'zope.componentvocabulary',
+        'zope.dottedname',
+        'zope.interface',
+        'zope.lifecycleevent',
+        'zope.pagetemplate',
+        'zope.publisher',
+        'zope.schema',
+        'zope.site',
+        'zope.testing',
+        'zope.traversing',
+        'zope.viewlet',
+        'zope.app.pagetemplate',
+        'transaction',
+        'Acquisition',
+        'Zope2',
+      ],
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Zope2',
+        'License :: OSI Approved :: Zope Public License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+      ],
+)
