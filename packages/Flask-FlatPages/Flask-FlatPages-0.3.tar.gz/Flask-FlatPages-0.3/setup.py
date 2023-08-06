a044015c4a996ece4240fa689fb2623aed9ed8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:52 +0000

    Add package Flask-FlatPages/0.3/Flask-FlatPages-0.3.tar.gz

---
+++ packages/Flask-FlatPages/Flask-FlatPages-0.3.tar.gz/Flask-FlatPages-0.3/setup.py
@@ -0,0 +1,52 @@
+"""
+Flask-FlatPages
+---------------
+
+Provides flat static pages to a Flask_ application, based on text files
+as opposed to a relationnal database.
+
+* BSD licensed
+* Latest documentation `on python.org`_
+* Source, issues and pull requests `on Github`_
+* Releases `on PyPI`_
+* Install with ``pip install Flask-FlatPages``
+
+.. _Flask: http://flask.pocoo.org/
+.. _on python.org: http://packages.python.org/Flask-FlatPages/
+.. _on Github: https://github.com/SimonSapin/Flask-FlatPages/
+.. _on PyPI: http://pypi.python.org/pypi/Flask-FlatPages
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask-FlatPages',
+    version='0.3', # also change this in docs/conf.py
+    url='https://github.com/SimonSapin/Flask-FlatPages',
+    license='BSD',
+    author='Simon Sapin',
+    author_email='simon.sapin@exyr.org',
+    description='Provides flat static pages to a Flask application',
+    long_description=__doc__,
+    packages=find_packages(),
+    # test pages
+    package_data={'': ['pages*/*.*', 'pages/*/*.*', 'pages/*/*/*.*']},
+    test_suite='flask_flatpages.tests',
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'PyYAML',
+        'Markdown',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
