2fab05ea52da25096bb84672e002149af18853cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:44 +0000

    Add package transmogrify.extract/0.4.0/transmogrify.extract-0.4.0.tar.gz

---
+++ packages/transmogrify.extract/transmogrify.extract-0.4.0.tar.gz/transmogrify.extract-0.4.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+import os
+
+setup(
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
+    description='Extracts all content from within the specified CSS id',
+    entry_points={
+        'z3c.autoinclude.plugin': 'target = transmogrify',
+    },
+    license='GPL',
+    long_description=open('README.rst').read() +
+        open(os.path.join('docs','HISTORY.txt')).read(),
+    include_package_data=True,
+    install_requires=[
+        'lxml',
+        'mr.migrator',
+        'setuptools',
+    ],
+    name='transmogrify.extract',
+    namespace_packages=[
+        'transmogrify',
+    ],
+    packages=find_packages(),
+    url='https://github.com/aclark4life/transmogrify.extract',
+    version='0.4.0',
+)
