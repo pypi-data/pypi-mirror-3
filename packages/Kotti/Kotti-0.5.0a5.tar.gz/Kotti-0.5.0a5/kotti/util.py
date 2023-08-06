868b65355b49d665ea5d3449b06204ffd4675807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:12 +0000

    Add package Kotti/0.5.0a5/Kotti-0.5.0a5.tar.gz

---
+++ packages/Kotti/Kotti-0.5.0a5.tar.gz/Kotti-0.5.0a5/kotti/util.py
@@ -0,0 +1,230 @@
+from pyramid.compat import json
+import string
+import urllib
+
+from repoze.lru import LRUCache
+from pyramid.threadlocal import get_current_request
+from pyramid.url import resource_url
+from sqlalchemy.types import TypeDecorator, VARCHAR
+from sqlalchemy.ext.mutable import Mutable
+
+def dump_default(obj):
+    if isinstance(obj, MutationDict):
+        return obj._d
+    elif isinstance(obj, MutationList):
+        return obj._d
+
+class JsonType(TypeDecorator):
+    """http://www.sqlalchemy.org/docs/core/types.html#marshal-json-strings
+    """
+    impl = VARCHAR
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value, default=dump_default)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
+
+class MutationDict(Mutable):
+    """http://www.sqlalchemy.org/docs/orm/extensions/mutable.html
+    """
+    def __init__(self, data):
+        self._d = data
+        super(MutationDict, self).__init__()
+
+    @classmethod
+    def coerce(cls, key, value):
+        if not isinstance(value, MutationDict):
+            if isinstance(value, dict):
+                return cls(value)
+            return Mutable.coerce(key, value)
+        else:
+            return value
+
+class MutationList(Mutable):
+    def __init__(self, data):
+        self._d = data
+        super(MutationList, self).__init__()
+
+    @classmethod
+    def coerce(cls, key, value):
+        if not isinstance(value, MutationList):
+            if isinstance(value, list):
+                return cls(value)
+            return Mutable.coerce(key, value)
+        else:
+            return value
+
+    def __radd__(self, other):
+        return other + self._d
+
+def _make_mutable_method_wrapper(wrapper_class, methodname, mutates):
+    def replacer(self, *args, **kwargs):
+        method = getattr(self._d, methodname)
+        value = method(*args, **kwargs)
+        if mutates:
+            self.changed()
+        return value
+    replacer.__name__ = methodname
+    return replacer
+
+for wrapper_class in (MutationDict, MutationList):
+    for methodname, mutates in (
+        ('__iter__', False),
+        ('__len__', False),
+        ('__eq__', False),
+        ('__add__', False),
+        ('get', False),
+        ('keys', False),
+
+        ('__setitem__', True),
+        ('__delitem__', True),
+        ('append', True),
+        ('insert', True),
+        ('setdefault', True),
+        ):
+        setattr(
+            wrapper_class, methodname,
+            _make_mutable_method_wrapper(
+                wrapper_class, methodname, mutates),
+            )
+
+class NestedMixin(object):
+    __parent__ = None
+    
+    def __init__(self, *args, **kwargs):
+        self.__parent__ = kwargs.pop('__parent__', None)
+        super(NestedMixin, self).__init__(*args, **kwargs)
+
+    def __getitem__(self, key):
+        value = self._d.__getitem__(key)
+        return self.try_wrap(value)
+
+    def changed(self):
+        if self.__parent__ is not None:
+            self.__parent__.changed()
+        else:
+            super(NestedMixin, self).changed()
+
+    def try_wrap(self, value):
+        for typ, wrapper in MUTATION_WRAPPERS.items():
+            if isinstance(value, typ):
+                value = wrapper(value, __parent__=self)
+                break
+        return value
+
+class NestedMutationDict(NestedMixin, MutationDict):
+    pass
+
+class NestedMutationList(NestedMixin, MutationList):
+    pass
+
+MUTATION_WRAPPERS = {
+    dict: NestedMutationDict,
+    list: NestedMutationList,
+    }
+
+class ViewLink(object):
+    def __init__(self, path, title=None):
+        self.path = path
+        if title is None:
+            title = path.replace('-', ' ').replace('_', ' ').title()
+        self.title = title
+
+    def url(self, context, request):
+        return resource_url(context, request) + '@@' + self.path
+
+    def selected(self, context, request):
+        return urllib.unquote(request.url).startswith(
+            self.url(context, request))
+
+    def permitted(self, context, request):
+        from kotti.security import view_permitted
+        return view_permitted(context, request, self.path)
+
+    def __eq__(self, other):
+        return isinstance(other, ViewLink) and repr(self) == repr(other)
+
+    def __repr__(self):
+        return "ViewLink(%r, %r)" % (self.path, self.title)
+
+class DontCache(Exception):
+    pass
+
+_CACHE_ATTR = 'kotti_cache'
+
+def request_container():
+    request = get_current_request()
+    if request is None:
+        return None
+    cache = getattr(request, _CACHE_ATTR, None)
+    if cache is None:
+        cache = {}
+        setattr(request, _CACHE_ATTR, cache)
+    return cache
+
+def cache(compute_key, container_factory):
+    marker = object()
+    def decorator(func):
+        def replacement(*args, **kwargs):
+            cache = container_factory()
+            if cache is None:
+                return func(*args, **kwargs)
+            try:
+                key = compute_key(*args, **kwargs)
+            except DontCache:
+                return func(*args, **kwargs)
+            key = '%s.%s:%s' % (func.__module__, func.__name__, key)
+            cached_value = cache.get(key, marker)
+            if cached_value is marker:
+                #print "\n*** MISS %r ***" % key
+                cached_value = cache[key] = func(*args, **kwargs)
+            else:
+                #print "\n*** HIT %r ***" % key
+                pass
+            return cached_value
+        replacement.__doc__ = func.__doc__
+        return replacement
+    return decorator
+
+def request_cache(compute_key):
+    return cache(compute_key, request_container)
+
+class LRUCacheSetItem(LRUCache):
+    __setitem__ = LRUCache.put
+
+_lru_cache = LRUCacheSetItem(1000)
+
+def lru_cache(compute_key):
+    return cache(compute_key, lambda: _lru_cache)
+
+def clear_cache(): # only useful for tests really
+    request = get_current_request()
+    if request is not None:
+        setattr(request, _CACHE_ATTR, None)
+    _lru_cache.clear()
+
+def extract_from_settings(prefix, settings=None):
+    """
+      >>> settings = {
+      ...     'kotti_twitter.foo_bar': '1', 'kotti.spam_eggs': '2'}
+      >>> print extract_from_settings('kotti_twitter.', settings)
+      {'foo_bar': '1'}
+    """
+    from kotti import get_settings
+    settings = settings if settings is not None else get_settings()
+    extracted = {}
+    for key, value in settings.items():
+        if key.startswith(prefix):
+            extracted[key[len(prefix):]] = value
+    return extracted
+
+def title_to_name(title):
+    okay = string.letters + string.digits + '-'
+    name = u'-'.join(title.lower().split())
+    name = u''.join(ch for ch in name if ch in okay)
+    return name
---
+++ packages/Kotti/Kotti-0.5.0a5.tar.gz/Kotti-0.5.0a5/kotti/views/edit.py
@@ -0,0 +1,249 @@
+from pyramid.exceptions import Forbidden
+from pyramid.httpexceptions import HTTPFound
+from pyramid.security import has_permission
+from pyramid.url import resource_url
+import colander
+from deform.widget import RichTextWidget
+from deform.widget import TextAreaWidget
+
+from kotti import get_settings
+from kotti import DBSession
+from kotti.resources import Node
+from kotti.resources import Document
+from kotti.security import view_permitted
+from kotti.views.util import EditFormView
+from kotti.views.util import AddFormView
+from kotti.views.util import addable_types
+from kotti.views.util import disambiguate_name
+from kotti.views.util import ensure_view_selector
+from kotti.views.util import nodes_tree
+from kotti.views.util import template_api
+from kotti.views.util import title_to_name
+
+class ContentSchema(colander.MappingSchema):
+    title = colander.SchemaNode(colander.String())
+    description = colander.SchemaNode(
+        colander.String(),
+        widget=TextAreaWidget(cols=40, rows=5),
+        missing=u"",
+        )
+
+class DocumentSchema(ContentSchema):
+    body = colander.SchemaNode(
+        colander.String(),
+        widget=RichTextWidget(theme='advanced', width=790, height=500),
+        missing=u"",
+        )
+
+def add_node(context, request):
+    """This view's responsibility is to present the user with a form
+    where they can choose between locations to add to, and types of
+    nodes to add, and redirect to the actual add form based on this
+    information.
+    """
+    all_types = get_settings()['kotti.available_types']
+    
+    if request.POST:
+        what, where = request.POST['what'], request.POST['where']
+        session = DBSession()
+        what = [t for t in all_types if t.type_info.name == what][0]
+        where = session.query(Node).get(int(where))
+        location = resource_url(where, request) + '@@' + what.type_info.add_view
+        return HTTPFound(location=location)
+
+    possible_parents, possible_types = addable_types(context, request)
+    if len(possible_parents) == 1 and len(possible_parents[0]['factories']) == 1:
+        # Redirect to the add form straight away if there's only one
+        # choice of parents and addable types:
+        parent = possible_parents[0]
+        add_view = parent['factories'][0].type_info.add_view
+        location = resource_url(parent['node'], request) + '@@' + add_view
+        return HTTPFound(location=location)
+
+    # Swap first and second possible parents if there's no content in
+    # 'possible_parents[0]' yet.  This makes the parent then the
+    # default choice in the form:
+    api = template_api(context, request)
+    if not api.list_children() and len(possible_parents) > 1:
+        possible_parents[0], possible_parents[1] = (
+            possible_parents[1], possible_parents[0])
+
+    return {
+        'api': api,
+        'possible_parents': possible_parents,
+        'possible_types': possible_types,
+        }
+
+def move_node(context, request):
+    """This view allows copying, cutting, pasting, deleting of
+    'context' and reordering of children of 'context'.
+    """
+    P = request.POST
+    session = DBSession()
+
+    if 'copy' in P:
+        request.session['kotti.paste'] = (context.id, 'copy')
+        request.session.flash(u'%s copied.' % context.title, 'success')
+        if not request.is_xhr:
+            return HTTPFound(location=request.url)
+
+    if 'cut' in P:
+        request.session['kotti.paste'] = (context.id, 'cut')
+        request.session.flash(u'%s cut.' % context.title, 'success')
+        if not request.is_xhr:
+            return HTTPFound(location=request.url)
+
+    if 'paste' in P:
+        id, action = request.session['kotti.paste']
+        item = session.query(Node).get(id)
+        if action == 'cut':
+            if not has_permission('edit', item, request):
+                raise Forbidden()
+            item.__parent__.children.remove(item)
+            context.children.append(item)
+            del request.session['kotti.paste']
+        elif action == 'copy':
+            copy = item.copy()
+            name = copy.name
+            if not name: # for root
+                name = title_to_name(copy.title)
+            while name in context.keys():
+                name = disambiguate_name(name)
+            copy.name = name
+            context.children.append(copy)
+        request.session.flash(u'%s pasted.' % item.title, 'success')
+        if not request.is_xhr:
+            return HTTPFound(location=request.url)
+
+    if 'order-up' in P or 'order-down' in P:
+        up, down = P.get('order-up'), P.get('order-down')
+        id = int(down or up)
+        if up is not None:
+            mod = -1
+        else: # pragma: no cover
+            mod = +1
+
+        child = session.query(Node).get(id)
+        index = context.children.index(child)
+        context.children.pop(index)
+        context.children.insert(index+mod, child)
+        request.session.flash(u'%s moved.' % child.title, 'success')
+        if not request.is_xhr:
+            return HTTPFound(location=request.url)
+
+    if 'delete' in P and 'delete-confirm' in P:
+        parent = context.__parent__
+        request.session.flash(u'%s deleted.' % context.title, 'success')
+        parent.children.remove(context)
+        location = resource_url(parent, request)
+        if view_permitted(parent, request, 'edit'):
+            location += '@@edit'
+        return HTTPFound(location=location)
+
+    if 'rename' in P:
+        name = P['name']
+        title = P['title']
+        if not name or not title:
+            request.session.flash(u'Name and title are required.', 'error')
+        else:
+            context.name = name
+            context.title = title
+            request.session.flash(u'Item renamed', 'success')
+            location = resource_url(context, request) + '@@move'
+            return HTTPFound(location=location)
+
+    return {}
+
+def generic_edit(context, request, schema, **kwargs):
+    api = template_api(context, request)
+    api.first_heading=u'<h1>Edit <em>%s</em></h1>' % context.title
+    api.page_title = u'Edit %s - %s' % (context.title, api.site_title)
+
+    rendered = EditFormView(context, request, schema=schema, **kwargs)()
+    if request.is_response(rendered):
+        return rendered
+
+    return {
+        'api': api,
+        'form': rendered['form'],
+        }
+
+def generic_add(context, request, schema, add, title, **kwargs):
+    api = template_api(context, request)
+    api.first_heading = u'<h1>Add %s to <em>%s</em></h1>' % (
+        title, context.title)
+    api.page_title=u'Add %s to %s - %s' % (title, context.title, api.site_title)
+
+    rendered = AddFormView(context, request, schema=schema, add=add, **kwargs)()
+    if request.is_response(rendered):
+        return rendered
+
+    return {
+        'api': api,
+        'form': rendered['form'],
+        }
+
+def make_generic_edit(schema, **kwargs):
+    @ensure_view_selector
+    def view(context, request):
+        return generic_edit(context, request, schema, **kwargs)
+    return view
+
+def make_generic_add(schema, add, title, **kwargs):
+    def view(context, request):
+        return generic_add(context, request, schema, add, title, **kwargs)
+    return view
+
+def render_tree_navigation(context, request):
+    tree = nodes_tree(request)
+    return {
+        'tree': {
+            'children': [tree],
+            },
+        }
+
+def includeme(config):
+    nodes_includeme(config)
+
+    config.add_view(
+        make_generic_edit(DocumentSchema()),
+        context=Document,
+        name='edit',
+        permission='edit',
+        renderer='kotti:templates/edit/node.pt',
+        )
+
+    config.add_view(
+        make_generic_add(DocumentSchema(), Document, u'document'),
+        name=Document.type_info.add_view,
+        permission='add',
+        renderer='kotti:templates/edit/node.pt',
+        )
+
+    config.add_view(
+        render_tree_navigation,
+        name='render_tree_navigation',
+        permission='view',
+        renderer='kotti:templates/edit/nav-tree.pt',
+        )
+
+    config.add_view(
+        render_tree_navigation,
+        name='navigate',
+        permission='view',
+        renderer='kotti:templates/edit/nav-tree-view.pt',
+        )
+
+def nodes_includeme(config):
+    config.add_view(
+        'kotti.views.edit.add_node',
+        name='add',
+        permission='add',
+        renderer='kotti:templates/edit/add.pt',
+        )
+    config.add_view(
+        'kotti.views.edit.move_node',
+        name='move',
+        permission='edit',
+        renderer='kotti:templates/edit/move.pt',
+        )
