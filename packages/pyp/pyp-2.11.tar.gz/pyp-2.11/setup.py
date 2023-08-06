db1b6c1353cb854ef29c98288e73aca13e780e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:21 +0000

    Add package pyp/2.11/pyp-2.11.tar.gz

---
+++ packages/pyp/pyp-2.11.tar.gz/pyp-2.11/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+version = '2.11'
+
+setup(name='pyp',
+      version=version,
+      description="Python Power At the Prompt",
+      classifiers=['python','prompt','awk','sed'],
+      keywords='',
+      author='Toby Rosen',
+      author_email='tobyrosen@gmail.com',
+      url='http://code.google.com/p/pyp/',
+      license='New BSD License',
+      scripts=['pyp'],
+)
