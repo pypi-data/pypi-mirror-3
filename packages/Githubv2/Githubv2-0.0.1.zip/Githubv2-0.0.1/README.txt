1709b3eb4f3e67cc6331ad2308fdaa2192d1662b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:24 +0000

    Add package Githubv2/0.0.1/Githubv2-0.0.1.zip

---
+++ packages/Githubv2/Githubv2-0.0.1.zip/Githubv2-0.0.1/README.txt
@@ -0,0 +1,13 @@
+===========
+Githubv2
+===========
+
+Githubv2 provides simple methods to use the Github v2 API. You might find
+it most useful for searching, listing user info, etc. 
+
+Thank You
+
+Pre-requisites
+==============
+
+pip install github2
---
+++ packages/Githubv2/Githubv2-0.0.1.zip/Githubv2-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Githubv2',
+    version='0.0.1',
+    author='kSampath',
+    author_email='karbrv@gmail.com',
+    packages=['githubv2', 'githubv2.test'],
+    url='http://pypi.python.org/pypi/Githubv2/',
+    license='LICENSE.txt',
+    description='Github API for v2.',
+    long_description=open('README.txt').read(),
+)
