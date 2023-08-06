c688e3ef9911bde49364a860f5f946f748a8a871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:47 +0000

    Add package geonode-extract/0.1.9/geonode-extract-0.1.9.zip

---
+++ packages/geonode-extract/geonode-extract-0.1.9.zip/geonode-extract-0.1.9/extract/version.py
@@ -0,0 +1,9 @@
+# THIS FILE IS GENERATED FROM setup.py in geonode-extract
+short_version = '0.1.9'
+version = '0.1.9'
+full_version = '0.1.9'
+git_revision = 'c9485e312f51da775cae44df19182f378806a3b3'
+release = True
+
+if not release:
+    version = full_version
