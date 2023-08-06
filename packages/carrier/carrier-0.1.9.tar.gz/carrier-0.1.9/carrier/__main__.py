83feffb0744af23a65598d3234c97f15119e14bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:08 +0000

    Add package carrier/0.1.9/carrier-0.1.9.tar.gz

---
+++ packages/carrier/carrier-0.1.9.tar.gz/carrier-0.1.9/carrier/__main__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+from .core import Carrier
+
+
+def main():
+    Carrier().run()
+
+
+if __name__ == "__main__":
+    main()
