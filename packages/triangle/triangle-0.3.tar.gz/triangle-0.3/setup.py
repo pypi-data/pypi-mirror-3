4651907899d504ad1dcf048fa75bd1baf8dc9289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:01 +0000

    Add package triangle/0.3/triangle-0.3.tar.gz

---
+++ packages/triangle/triangle-0.3.tar.gz/triangle-0.3/setup.py
@@ -0,0 +1,17 @@
+import numpy
+from distutils.core import setup, Extension
+
+setup(name='triangle',
+      packages=['triangle'],
+      version='0.3',
+      description='Python binding to the triangle library',
+      author='Dzhelil Rufat',
+      author_email='drufat@caltech.edu',
+      url='https://github.com/drufat/triangle',
+      requires = ['numpy'],
+      ext_modules=[Extension('triangle.core', ['c/triangle.c', 'c/triangle_ext.c'],
+                             include_dirs = ['c', numpy.get_include()],
+                             define_macros = [('NO_TIMER', 1), ('TRILIBRARY', 1)]) 
+              ],
+      headers=['c/triangle.h']
+     )
