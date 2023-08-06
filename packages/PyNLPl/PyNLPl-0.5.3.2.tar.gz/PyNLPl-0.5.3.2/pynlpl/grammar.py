db2c60905ffa9f4aa24a6530aa4123915dcf504b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:10 +0000

    Add package PyNLPl/0.5.3.2/PyNLPl-0.5.3.2.tar.gz

---
+++ packages/PyNLPl/PyNLPl-0.5.3.2.tar.gz/PyNLPl-0.5.3.2/pynlpl/grammar.py
@@ -0,0 +1,17 @@
+#---------------------------------------------------------------
+# PyNLPl - Grammar
+#   by Maarten van Gompel, ILK, Universiteit van Tilburg
+#   http://ilk.uvt.nl/~mvgompel
+#   proycon AT anaproy DOT nl
+#
+#   Licensed under GPLv3
+#
+#----------------------------------------------------------------
+
+class Grammar:
+    
+
+class GrammarRule:
+    def __init__(self, symbol, expression):
+        
+    
