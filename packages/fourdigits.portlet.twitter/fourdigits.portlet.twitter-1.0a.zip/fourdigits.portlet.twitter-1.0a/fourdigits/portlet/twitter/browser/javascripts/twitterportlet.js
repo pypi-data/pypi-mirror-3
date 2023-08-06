d449eba76105fbba54c164995cd7bc0b3e1d1402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:55 +0000

    Add package fourdigits.portlet.twitter/1.0a/fourdigits.portlet.twitter-1.0a.zip

---
+++ packages/fourdigits.portlet.twitter/fourdigits.portlet.twitter-1.0a.zip/fourdigits.portlet.twitter-1.0a/fourdigits/portlet/twitter/browser/javascripts/twitterportlet.js
@@ -0,0 +1,30 @@
+        var our_locales = {
+            "future_format": "%s %t",
+            "past_format": "%t %s",
+            "second": "seconde",
+            "seconds": "secondes",
+            "minute": "minuut",
+            "minutes": "minuten",
+            "hour": "uur",
+            "hours": "uren",
+            "day": "dag",
+            "days": "dagen",
+            "week": "week",
+            "weeks": "weken",
+            "month": "maand",
+            "months": "maanden",
+            "year": "jaar",
+            "years": "jaren",
+            "yesterday": "gisteren",
+            "tomorrow": "morgen",
+            "now": "net",
+            "ago": "geleden",
+            "in": "over"
+        };
+
+
+(function($){
+    $(window).load(function(){
+
+     });
+})(jQuery);
