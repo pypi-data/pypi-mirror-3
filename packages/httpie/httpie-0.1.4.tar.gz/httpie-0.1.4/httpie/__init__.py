6dc1333fc4a3b5cd5e3b68ad436266d535d2d45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:04 +0000

    Add package httpie/0.1.4/httpie-0.1.4.tar.gz

---
+++ packages/httpie/httpie-0.1.4.tar.gz/httpie-0.1.4/httpie/__init__.py
@@ -0,0 +1,7 @@
+"""
+HTTPie - cURL for humans.
+
+"""
+__author__ = 'Jakub Roztocil'
+__version__ = '0.1.4'
+__licence__ = 'BSD'
---
+++ packages/httpie/httpie-0.1.4.tar.gz/httpie-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import httpie
+
+
+setup(name='httpie',version=httpie.__version__,
+    description=httpie.__doc__.strip(),
+    url='https://github.com/jkbr/httpie',
+    author=httpie.__author__,
+    license=httpie.__licence__,
+    packages=['httpie'],
+    entry_points={'console_scripts': ['http = httpie.httpie:main']},
+    install_requires=['requests>=0.10.4', 'Pygments>=1.4'])
