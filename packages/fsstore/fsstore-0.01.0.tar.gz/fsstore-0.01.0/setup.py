13eee1b93c9be0627cb2a212a23349c1a10384bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:53 +0000

    Add package fsstore/0.01.0/fsstore-0.01.0.tar.gz

---
+++ packages/fsstore/fsstore-0.01.0.tar.gz/fsstore-0.01.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+
+setup(
+    name = "fsstore",
+    version = "0.01.0",
+    author = "startling",
+    author_email = "tdixon51793@gmail.com",
+    packages = ["fsstore"],
+    url = "https://github.com/startling/fsstore",
+    keywords = ["dicts", "filesystem", "storage"],
+    description = "Dict-like objects for file I/O.",
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
