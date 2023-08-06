bbbe620e34e4409505bc4a271f8c2349c4a90c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:36 +0000

    Add package osfreak/1.0.0/osfreak-1.0.0.zip

---
+++ packages/osfreak/osfreak-1.0.0.zip/osfreak-1.0.0/osfreak.py
@@ -0,0 +1,2 @@
+def hello(item):
+	print(item)
---
+++ packages/osfreak/osfreak-1.0.0.zip/osfreak-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+	name = 'osfreak',
+	version = '1.0.0',
+	py_modules = ['osfreak'],
+	author = 'Yadav',
+	author_email = 'me@pyadav.org',
+	url = 'http://www.pyadav.org',
+	description = "A simple Program..",
+	license = "BSD",
+	platform = "Window|Linx|Mac|Unix"
+)
