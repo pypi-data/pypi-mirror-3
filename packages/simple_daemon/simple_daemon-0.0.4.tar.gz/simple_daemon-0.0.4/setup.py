6aad1d2d920b2c5cd52871f707ba600765a9b0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:48 +0000

    Add package simple_daemon/0.0.4/simple_daemon-0.0.4.tar.gz

---
+++ packages/simple_daemon/simple_daemon-0.0.4.tar.gz/simple_daemon-0.0.4/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='simple_daemon',
+    version='0.0.4',
+    description='Simple nix daemon',
+    long_description=open('README.rst').read(),
+    author='Imbolc',
+    author_email='imbolc@imbolc.name',
+    url='https://bitbucket.org/imbolc/simple_daemon',
+    packages= ['simple_daemon'],
+    install_requires=['single_access==0.0.2'],
+    license='ISC',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+    ),
+)
