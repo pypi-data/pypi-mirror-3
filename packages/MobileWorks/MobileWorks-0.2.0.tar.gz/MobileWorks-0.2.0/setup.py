21b0e8fa9ea17b91f5bd16ff99baf7650a120888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:52 +0000

    Add package MobileWorks/0.2.0/MobileWorks-0.2.0.tar.gz

---
+++ packages/MobileWorks/MobileWorks-0.2.0.tar.gz/MobileWorks-0.2.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup( 
+    name = 'MobileWorks',
+    version = '0.2.0',
+    py_modules = ['mobileworks'],
+    url = 'http://www.mobileworks.com/',
+    description = 'A wrapper for the MobileWorks API.',
+ )
