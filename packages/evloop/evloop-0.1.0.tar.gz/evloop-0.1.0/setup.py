0fea8b1a8457d2fce5dd781e96379c9b0596842b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:32 +0000

    Add package evloop/0.1.0/evloop-0.1.0.tar.gz

---
+++ packages/evloop/evloop-0.1.0.tar.gz/evloop-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='evloop',
+    version='0.1.0',
+    description='Event driven library for Python',
+    author='Gregory Haynes',
+    author_email='greg@greghaynes.net',
+    url='http://github.com/greghaynes/pyevloop',
+    license='MIT',
+    packages=['evloop'],
+    )
