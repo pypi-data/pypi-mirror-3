0fbc75e6dde7056c6ae717afa2f7ff7d0f819f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:38 +0000

    Add package django-weekday-field/0.1.1/django-weekday-field-0.1.1.tar.gz

---
+++ packages/django-weekday-field/django-weekday-field-0.1.1.tar.gz/django-weekday-field-0.1.1/weekday_field/utils.py
@@ -0,0 +1,19 @@
+DAY_CHOICES = (
+    (0, "Monday"),
+    (1, "Tuesday"),
+    (2, "Wednesday"),
+    (3, "Thursday"),
+    (4, "Friday"),
+    (5, "Saturday"),
+    (6, "Sunday")
+)
+
+BITWISE_DAY_CHOICES = (
+    (1, "Sun","Sunday"),
+    (2, "Mon","Monday"),
+    (4, "Tues","Tuesday"),
+    (8, "Weds","Wednesday"),
+    (16, "Thur","Thursday"),
+    (32, "Fri","Friday"),
+    (64, "Sat","Saturday"),
+)
