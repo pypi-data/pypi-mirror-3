bac6dd9e5fb446478fd89e7e35f2ef41e2b6a8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:17 +0000

    Add package experimental.pythonpackages/0.3.4/experimental.pythonpackages-0.3.4.zip

---
+++ packages/experimental.pythonpackages/experimental.pythonpackages-0.3.4.zip/experimental.pythonpackages-0.3.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+import os
+
+VERSION = '0.3.4'
+
+setup(
+    description='Test',
+    include_package_data=True,
+    long_description=(
+        open('README.rst').read() + '\n' +
+        open('HISTORY.txt').read()
+        ),
+    name='experimental.pythonpackages',
+    packages=find_packages(),
+    version=VERSION,
+)
