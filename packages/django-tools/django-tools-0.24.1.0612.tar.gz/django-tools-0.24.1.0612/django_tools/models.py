9c43fcde603b7a8caea342978d269cae0a4683b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:19 +0000

    Add package django-tools/0.24.1.0612/django-tools-0.24.1.0612.tar.gz

---
+++ packages/django-tools/django-tools-0.24.1.0612.tar.gz/django-tools-0.24.1.0612/django_tools/models.py
@@ -0,0 +1,64 @@
+# coding: utf-8
+
+"""
+    models stuff
+    ~~~~~~~~~~~~
+
+    :copyleft: 2011 by the django-tools team, see AUTHORS for more details.
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+
+from django.db import models
+from django.contrib.auth.models import User
+try:
+    from django.utils.timezone import now
+except ImportError:
+    from datetime import datetime
+    now = datetime.now
+
+from django_tools.middlewares import ThreadLocal
+
+
+
+
+class UpdateInfoBaseModel(models.Model):
+    """
+    Base model with update info attributes, used by many models.
+    The createby and lastupdateby ForeignKey would be automaticly updated. This needs the 
+    request object as the first argument in the save method.
+
+    We doesn't used auto_now_add and auto_now here, because they have the odd side effect
+    see also:
+    https://github.com/jezdez/django-dbtemplates/commit/2f27327bebe7f2e7b33e5cfb0db517f53a1b9701#commitcomment-1396126
+    """
+    objects = models.Manager()
+
+    createtime = models.DateTimeField(default=now, editable=False, help_text="Create time")
+    lastupdatetime = models.DateTimeField(default=now, editable=False, help_text="Time of the last change.")
+
+    createby = models.ForeignKey(User, editable=False, related_name="%(class)s_createby",
+        null=True, blank=True, # <- If the model used outsite a real request (e.g. unittest, db shell)
+        help_text="User how create this entry.")
+    lastupdateby = models.ForeignKey(User, editable=False, related_name="%(class)s_lastupdateby",
+        null=True, blank=True, # <- If the model used outsite a real request (e.g. unittest, db shell)
+        help_text="User as last edit this entry.")
+
+    def save(self, *args, **kwargs):
+        """
+        Automatic update createby and lastupdateby attributes with the request object witch must be
+        the first argument.
+        """
+        self.lastupdatetime = now()
+        current_user = ThreadLocal.get_current_user()
+
+        if current_user and isinstance(current_user, User):
+            if self.pk == None or kwargs.get("force_insert", False): # New model entry
+                self.createby = current_user
+            self.lastupdateby = current_user
+
+        return super(UpdateInfoBaseModel, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
