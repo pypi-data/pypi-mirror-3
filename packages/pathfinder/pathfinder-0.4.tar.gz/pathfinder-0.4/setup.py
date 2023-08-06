42927b64a94c91b4bf40c2f09e41ae76003c78b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:35 +0000

    Add package pathfinder/0.4/pathfinder-0.4.tar.gz

---
+++ packages/pathfinder/pathfinder-0.4.tar.gz/pathfinder-0.4/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='pathfinder',
+    description='Pathfinder os.walk for humans',
+    url='http://jkeyes.github.com/pathfinder/',
+    author='John Keyes',
+    author_email='pathfinder@keyes.ie',
+    version='0.4',
+    license='MIT License',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    py_modules=['pathfinder']
+)
