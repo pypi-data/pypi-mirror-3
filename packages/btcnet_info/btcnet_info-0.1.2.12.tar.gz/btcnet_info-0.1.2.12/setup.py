4ee41ece9771c525ef47ba7154d1e44f8d11e69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:34 +0000

    Add package btcnet_info/0.1.2.12/btcnet_info-0.1.2.12.tar.gz

---
+++ packages/btcnet_info/btcnet_info-0.1.2.12.tar.gz/btcnet_info-0.1.2.12/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+setup(name='btcnet_info',
+        version='0.1.2.12',
+        description='BitCoin Network Information Library',
+        author='Colin Rice',
+        author_email='dah4k0r@gmail.com',
+        packages=['btcnet_info'],
+        include_package_data = True,
+        package_data = { "":["*/*"]},
+        url='github.com/c00w/btcnet_info',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: System :: Networking',
+            'License :: OSI Approved :: MIT License',
+            ],
+        license = 'MIT Expat License',
+        install_requires = ['gevent']
+        )
