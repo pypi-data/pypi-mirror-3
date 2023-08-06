839bbeab267380393e89e3303d6e05c3c8ce99e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:05 +0000

    Add package pyramid_rewrite/0.2/pyramid_rewrite-0.2.tar.gz

---
+++ packages/pyramid_rewrite/pyramid_rewrite-0.2.tar.gz/pyramid_rewrite-0.2/CHANGES.rst
@@ -0,0 +1,15 @@
+=======
+Changes
+=======
+
+0.2
+---
+
+*  Added unit tests
+*  Changed logging level
+
+0.1
+---
+
+*  Initial version. Working.
+
---
+++ packages/pyramid_rewrite/pyramid_rewrite-0.2.tar.gz/pyramid_rewrite-0.2/setup.py
@@ -0,0 +1,43 @@
+import os
+
+# this is a hack to prevent an ugly bug when
+# setuptools performs sys.modules restoration
+import multiprocessing
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme = open(os.path.join(here, 'README.rst')).read()
+changes = open(os.path.join(here, 'CHANGES.rst')).read()
+
+
+requires=['pyramid>=1.3a6']
+
+setup(
+    name='pyramid_rewrite',
+    version='0.2',
+    description='Small pyramid extension for rewriting urls',
+    long_description=readme + '\n' + changes,
+    classifiers=[
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    #packages=find_packages(),
+    packages=['pyramid_rewrite'],
+    install_requires=requires,
+    author='Benjamin Hepp',
+    author_email='benjamin.hepp@gmail.com',
+    license='BSD',
+    url='https://github.com/bennihepp/pyramid_rewrite',
+    keywords='pyramid rewrite pylons web',
+    test_require=requires,
+    test_suite='pyramid_rewrite.tests',
+)
+
