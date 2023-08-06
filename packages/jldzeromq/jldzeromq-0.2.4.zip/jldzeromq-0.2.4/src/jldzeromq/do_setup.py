268ecee095358c5cf32200216000dc3b4de2ebaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:32 +0000

    Add package jldzeromq/0.2.4/jldzeromq-0.2.4.zip

---
+++ packages/jldzeromq/jldzeromq-0.2.4.zip/jldzeromq-0.2.4/src/jldzeromq/do_setup.py
@@ -0,0 +1,18 @@
+"""
+    @author: Jean-Lou Dupont
+"""
+
+import logging, sys, os
+
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+FORMAT='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+formatter = logging.Formatter(FORMAT)
+
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+
+#logger=logging.getLogger()
+#console = logging.StreamHandler(sys.stdout)
+#console.setLevel(logging.INFO)
+#console.setFormatter(formatter)
+#logger.addHandler(console)
