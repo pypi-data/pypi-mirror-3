fdba5590c658c9acdad2b01b4d8db3b248bcc203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:26 +0000

    Add package pyp2rpm/0.5.0/pyp2rpm-0.5.0.tar.gz

---
+++ packages/pyp2rpm/pyp2rpm-0.5.0.tar.gz/pyp2rpm-0.5.0/pyp2rpmlib/filters.py
@@ -0,0 +1,25 @@
+from pyp2rpmlib import utils
+from pyp2rpmlib import settings
+
+def name_for_python_version(name, version):
+    return utils.rpm_name(name, version)
+
+def sitedir_for_python_version(name, version):
+    if version == settings.DEFAULT_PYTHON_VERSION:
+        return name
+    else:
+        return name.replace('python', 'python%s' % version)
+
+def python_bin_for_python_version(name, version):
+    if version == settings.DEFAULT_PYTHON_VERSION:
+        return name
+    else:
+        return name.replace('__python', '__python3')
+
+def macroed_pkg_name(name):
+    if name.startswith('python-'):
+        return 'python-%{pypi_name}'
+    else:
+        return '%{pypi_name}'
+
+__all__ = [name_for_python_version, sitedir_for_python_version, python_bin_for_python_version, macroed_pkg_name]
