8f5291c6f9a38ddae12af541d4d9de3b1fc00a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:16 +0000

    Add package vaporize/0.1.6/vaporize-0.1.6.tar.gz

---
+++ packages/vaporize/vaporize-0.1.6.tar.gz/vaporize-0.1.6/vaporize/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'vaporize'
+__author__ = 'Michael Lavers'
+__email__ = 'kolanos@gmail.com'
+__version__ = '0.1.5'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2012 Michael Lavers'
+
+
+from . import domains, flavors, images, ipgroups, loadbalancers, servers
+from .core import connect
