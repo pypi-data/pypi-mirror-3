14fe30baa6fecef6ccf414b724cad34a4dc0768c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:13 +0000

    Add package haste/0.5/haste-0.5.tar.gz

---
+++ packages/haste/haste-0.5.tar.gz/haste-0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name="haste",
+      version="0.5",
+      author="Devon Meunier",
+      author_email="devon.meunier@utoronto.ca",
+      url="http://www.cdf.utoronto.ca/~g8m/",
+      license="MIT",
+      scripts=["haste.py"],
+      description="Pure Python replacement of the hastebin.com client.",
+      install_requires=[
+          "requests",
+          "docopt>=0.3.0",
+      ],
+)
