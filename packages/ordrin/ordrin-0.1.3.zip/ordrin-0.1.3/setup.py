0598dce3de11502c221f5c708934be87e6f57f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:06 +0000

    Add package ordrin/0.1.3/ordrin-0.1.3.zip

---
+++ packages/ordrin/ordrin-0.1.3.zip/ordrin-0.1.3/setup.py
@@ -0,0 +1,22 @@
+import distribute_setup
+distribute_setup.use_setuptools()
+from setuptools import setup
+setup(
+  name="ordrin",
+  version='0.1.3',
+  packages=['ordrin', 'ordrindemo'],
+  py_modules=['distribute_setup'],
+  description="Ordr.in API Client",
+  author="Ordr.in",
+  author_email="tech@ordr.in",
+  url="https://github.com/ordrin/api-py",
+  install_requires=['requests>=0.13.1'],
+  classifiers=[
+    "Programming Language :: Python",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Development Status :: 4 - Beta",
+    "Topic :: Software Development",
+    "Topic :: Internet"],
+  scripts=('ordrindemo/demo.py',))
