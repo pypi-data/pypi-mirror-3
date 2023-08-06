8eb137fc0aed67d032505b8a6d1d9be8f93a6d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:02 +0000

    Add package django-merchant/0.0.4a0/django-merchant-0.0.4a0.tar.gz

---
+++ packages/django-merchant/django-merchant-0.0.4a0.tar.gz/django-merchant-0.0.4a0/billing/tests/__init__.py
@@ -0,0 +1,15 @@
+import unittest
+from base_tests import *
+from authorize_net_tests import *
+from pay_pal_tests import *
+from eway_tests import *
+from world_pay_tests import *
+from google_checkout_tests import *
+from amazon_fps_tests import *
+from braintree_payments_tests import *
+from braintree_payments_tr_tests import *
+from stripe_tests import *
+from samurai_tests import *
+
+if __name__ == "__main__":
+    unittest.main()
