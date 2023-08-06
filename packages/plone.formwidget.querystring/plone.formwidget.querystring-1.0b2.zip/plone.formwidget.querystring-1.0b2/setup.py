8ddb4e5ffae36774ff25e6fee48127a96431ec36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:17 +0000

    Add package plone.formwidget.querystring/1.0b2/plone.formwidget.querystring-1.0b2.zip

---
+++ packages/plone.formwidget.querystring/plone.formwidget.querystring-1.0b2.zip/plone.formwidget.querystring-1.0b2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+version = '1.0b2'
+
+setup(name='plone.formwidget.querystring',
+      version=version,
+      description="A widget for composing a Query string/search.",
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='https://github.com/plone/plone.formwidget.querystring',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.formwidget'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.querystring',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+          [z3c.autoinclude.plugin]
+          target = plone
+      """,
+      )
