ea1401d0d23500641ffa3d96093d8a8c8726f72a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:30 +0000

    Add package toutpt.zopeskel/1.3.2/toutpt.zopeskel-1.3.2.tar.gz

---
+++ packages/toutpt.zopeskel/toutpt.zopeskel-1.3.2.tar.gz/toutpt.zopeskel-1.3.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.3.2'
+
+setup(name='toutpt.zopeskel',
+      version=version,
+      description="",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='oaster zopeskel',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='toutpt@gmail.com',
+      url='https://github.com/toutpt/toutpt.zopeskel',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['toutpt'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'PasteScript',
+          'ZopeSkel<3.0',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [paste.paster_create_template]
+      toutpt_diazotheme = toutpt.zopeskel.theme:Theme
+      toutpt_collective = toutpt.zopeskel.collective:Collective
+      toutpt_collectivejs = toutpt.zopeskel.collectivejs:CollectiveJS
+      """,
+      )
