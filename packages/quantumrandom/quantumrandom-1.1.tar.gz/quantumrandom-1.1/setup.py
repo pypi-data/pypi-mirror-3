2d294a23ff2fad658429af8a6c037b2521327fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:17 +0000

    Add package quantumrandom/1.1/quantumrandom-1.1.tar.gz

---
+++ packages/quantumrandom/quantumrandom-1.1.tar.gz/quantumrandom-1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '1.1'
+
+f = open('README.rst')
+long_description = f.read()
+f.close()
+
+setup(name='quantumrandom',
+      version=version,
+      description="A Python interface to the ANU Quantum Random Numbers Server",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Scientific/Engineering :: Mathematics',
+          ],
+      keywords='quantum random',
+      author='Luke Macken',
+      author_email='lmacken@redhat.com',
+      url='http://github.com/lmacken/python-quantumrandom',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[],
+      entry_points="",
+      )
