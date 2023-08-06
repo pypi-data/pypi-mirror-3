2bcd021bd38a8140baf2c74619a265a733a39f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:46 +0000

    Add package nipype/0.5.1/nipype-0.5.1.tar.gz

---
+++ packages/nipype/nipype-0.5.1.tar.gz/nipype-0.5.1/nipype/pipeline/plugins/__init__.py
@@ -0,0 +1,11 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+from .debug import DebugPlugin
+from .linear import LinearPlugin
+from .ipythonx import IPythonXPlugin
+from .pbs import PBSPlugin
+from .sge import SGEPlugin
+from .condor import CondorPlugin
+from .multiproc import MultiProcPlugin
+from .ipython import IPythonPlugin
