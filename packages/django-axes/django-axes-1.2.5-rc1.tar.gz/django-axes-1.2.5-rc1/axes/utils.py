9de71b42ffa38086988afb24cca9b76702218327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:00 +0000

    Add package django-axes/1.2.5-rc1/django-axes-1.2.5-rc1.tar.gz

---
+++ packages/django-axes/django-axes-1.2.5-rc1.tar.gz/django-axes-1.2.5-rc1/axes/utils.py
@@ -0,0 +1,19 @@
+from axes.models import AccessAttempt
+
+
+def reset(ip=None, silent=False):
+    if not ip:
+        attempts = AccessAttempt.objects.all()
+        if attempts:
+            attempts.delete()
+        else:
+            if not silent:
+                print 'No attempts found.'
+    else:
+        try:
+            attempt = AccessAttempt.objects.get(ip_address=ip)
+        except:
+            if not silent:
+                print 'No matching attempt found.'
+        else:
+            attempt.delete()
