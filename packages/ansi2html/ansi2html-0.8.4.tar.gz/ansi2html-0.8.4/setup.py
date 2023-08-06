4b31700913d68468d7d1261123cd4f997cdc2231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:53 +0000

    Add package ansi2html/0.8.4/ansi2html-0.8.4.tar.gz

---
+++ packages/ansi2html/ansi2html-0.8.4.tar.gz/ansi2html-0.8.4/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+f = open('README.rst')
+long_description = f.read().strip()
+long_description = long_description.split('split here', 1)[1]
+f.close()
+
+# Ridiculous as it may seem, we need to import multiprocessing and
+# logging here in order to get tests to pass smoothly on python 2.7.
+import multiprocessing
+import logging
+
+setup(
+    name='ansi2html',
+    version='0.8.4',
+    description="Convert text with ANSI color codes to HTML",
+    long_description=long_description,
+    author='Ralph Bean',
+    author_email='rbean@redhat.com',
+    url='http://github.com/ralphbean/ansi2html/',
+    license='GPLv3+',
+    install_requires=[],
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    packages=['ansi2html'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points="""
+    [console_scripts]
+    ansi2html = ansi2html.ansi2html:main
+    """
+)
