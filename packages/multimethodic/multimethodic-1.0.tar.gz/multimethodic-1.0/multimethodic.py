01bba9b2ad3ba05c4f0043325619320e841a0c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:59 +0000

    Add package multimethodic/1.0/multimethodic-1.0.tar.gz

---
+++ packages/multimethodic/multimethodic-1.0.tar.gz/multimethodic-1.0/multimethodic.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+''' Multimethodic
+
+An implementation of multimethods for Python, heavily influenced by
+the Clojure programming language.
+
+Copyright (C) 2010-2012 by Daniel Werner.
+
+See the README file for information on usage and redistribution.
+'''
+
+
+class DefaultMethod(object):
+    def __repr__(self):
+        return '<DefaultMethod>'
+
+Default = DefaultMethod()
+
+
+class MultiMethod(object):
+    def __init__(self, name, dispatchfn):
+        if not callable(dispatchfn):
+            raise TypeError('dispatchfn must be callable')
+
+        self.dispatchfn = dispatchfn
+        self.methods = {}
+        self.name = name
+
+    def __call__(self, *args, **kwds):
+        dv = self.dispatchfn(*args, **kwds)
+
+        if dv in self.methods:
+            return self.methods[dv](*args, **kwds)
+
+        if Default in self.methods:
+            return self.methods[Default](*args, **kwds)
+
+        raise Exception("No matching method on multimethod '%s' and "
+                        "no default method defined" % self.name)
+
+    def addmethod(self, func, dispatchval):
+        self.methods[dispatchval] = func
+
+    def removemethod(self, dispatchval):
+        del self.methods[dispatchval]
+
+    def method(self, dispatchval):
+        ''' Decorates a function as a new method of this multimethod, to be
+            invoked when the dispatch function returns `dispatchval`.
+
+            The return value is the MultiMethod itself.
+        '''
+
+        def method_decorator(func):
+            self.addmethod(func, dispatchval)
+            # Return the multimethod itself
+            return self
+
+        return method_decorator
+
+    def __repr__(self):
+        return "<%s '%s'>" % (type(self).__name__, self.name)
+
+
+__all__ = ['MultiMethod', 'Default']
---
+++ packages/multimethodic/multimethodic-1.0.tar.gz/multimethodic-1.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+""" setup.py for multimethodic
+"""
+
+from distutils.core import setup
+
+setup(
+    name         = 'multimethodic',
+    version      = '1.0',
+    description  = 'Clojure-style multimethods for Python',
+    author       = 'Daniel Werner',
+    author_email = 'daniel.d.werner@googlemail.com',
+    license      = 'BSD 2-clause',
+    keywords     = 'multimethods dispatch',
+    classifiers  = [
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    url          = 'http://github.com/danwerner/multimethodic',
+    py_modules   = ['multimethodic'],
+
+    long_description = "This module adds multimethod support to the Python programming language. In \
+contrast to other multiple dispatch implementations, this one doesn't strictly \
+dispatch on argument types, but on a user-provided dispatch function that can \
+differ for each multimethod. This design is inspired the Clojure programming \
+language's multimethods.",
+)
