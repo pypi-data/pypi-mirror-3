54a98b82cb38f77917de68f85699f21cefd379fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:01 +0000

    Add package python-safe/0.1/python-safe-0.1.tar.gz

---
+++ packages/python-safe/python-safe-0.1.tar.gz/python-safe-0.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Get text from README.txt
+readme_text = file('README.rst', 'rb').read()
+
+setup(name          = 'python-safe',
+      version       = '0.1',
+      description   = 'Spatial Analysis F* Engine',
+      license       = 'BSD',
+      keywords      = 'gis vector feature raster data',
+      author        = 'Ole Nielsen',
+      author_email  = 'ole.moller.nielsen@gmail.com',
+      maintainer        = 'Ariel Núñez',
+      maintainer_email  = 'ingenieroariel@gmail.com',
+      url   = 'http://github.com/AIFDR/safe',
+      long_description = readme_text,
+      packages = ['safe'],
+      install_requires  = [],
+      tests_require = ['nose'],
+      test_suite = 'nose.collector',
+      classifiers   = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS',
+        ],
+)
