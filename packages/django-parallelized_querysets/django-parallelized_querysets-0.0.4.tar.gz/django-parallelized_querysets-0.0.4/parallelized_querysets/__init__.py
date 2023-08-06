f5396ed7325b239820690fa2b302226e382621f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:42 +0000

    Add package django-parallelized_querysets/0.0.4/django-parallelized_querysets-0.0.4.tar.gz

---
+++ packages/django-parallelized_querysets/django-parallelized_querysets-0.0.4.tar.gz/django-parallelized_querysets-0.0.4/parallelized_querysets/__init__.py
@@ -0,0 +1,12 @@
+__version_tuple__ = (0, 0, 4)
+__version__ = ".".join(map(str, __version_tuple__))
+
+
+
+def parallelized_multiple_querysets(querysets, *args, **kwargs):
+    from parallelized_querysets.core import parallelized_multiple_querysets
+    return parallelized_multiple_querysets(querysets, *args, **kwargs)
+
+
+def parallelized_queryset(queryset, *args, **kwargs):
+    return parallelized_multiple_querysets([queryset], *args, **kwargs)
