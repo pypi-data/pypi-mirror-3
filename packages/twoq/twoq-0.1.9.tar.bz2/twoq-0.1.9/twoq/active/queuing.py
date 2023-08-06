d5dc080de13b6eb66dee92a421a770023ccff7be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:06 +0000

    Add package twoq/0.1.9/twoq-0.1.9.tar.bz2

---
+++ packages/twoq/twoq-0.1.9.tar.bz2/twoq-0.1.9/twoq/active/queuing.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+'''twoq active queues'''
+
+from twoq.mixins.mapping import MapMixin
+from twoq.mixins.reducing import ReduceMixin
+from twoq.mixins.ordering import OrderMixin
+from twoq.mixins.filtering import FilterMixin
+
+from twoq.active.mixins import AutoQMixin, ManQMixin, SyncQMixin
+
+__all__ = ('autoq', 'manq', 'syncq', 'twoq')
+
+
+class autoq(AutoQMixin, FilterMixin, MapMixin, ReduceMixin, OrderMixin):
+
+    '''auto-balancing manipulation queue'''
+
+
+class manq(ManQMixin, FilterMixin, MapMixin, ReduceMixin, OrderMixin):
+
+    '''manually balanced manipulation queue'''
+
+
+class syncq(SyncQMixin, FilterMixin, MapMixin, ReduceMixin, OrderMixin):
+
+    '''autosyncing manipulation queue'''
+
+
+twoq = autoq
