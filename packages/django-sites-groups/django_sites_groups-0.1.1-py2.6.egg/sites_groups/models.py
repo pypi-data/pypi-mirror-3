2bf66b2b19e68e3d73fd17b0ccd4664ec2aef6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:42 +0000

    Add package django-sites-groups/0.1.1/django_sites_groups-0.1.1-py2.6.egg

---
+++ packages/django-sites-groups/django_sites_groups-0.1.1-py2.6.egg/sites_groups/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class SitesGroup(models.Model):
+    title = models.CharField(
+        max_length=256,
+        help_text='A short descriptive title.',
+    )
+    sites = models.ManyToManyField(
+        'sites.Site',
+        help_text='Sites that belong to this group.',
+    )
+
+    class Meta:
+        app_label = 'sites'
+        ordering = ('title',)
+
+    def __unicode__(self):
+        return self.title
+
+    @property
+    def site_ids(self):        
+        return self.sites.all().values_list('id', flat=True)
