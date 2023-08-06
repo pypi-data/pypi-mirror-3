4e09dbaa4cf20670c4be96b10f475320b2add2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:49 +0000

    Add package collective.wtforms/1.0a4/collective.wtforms-1.0a4.zip

---
+++ packages/collective.wtforms/collective.wtforms-1.0a4.zip/collective.wtforms-1.0a4/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a4'
+
+setup(name='collective.wtforms',
+      version=version,
+      description="plone wtforms integration",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='forms plone wtforms',
+      author='Nathan Van Gheem',
+      author_email='vangheem@gmail.com',
+      url='http://github.com/collective/collective.wtforms',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'WTForms'
+      ],
+      extras_require=dict(
+          tests=['plone.app.testing']
+      ),
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
