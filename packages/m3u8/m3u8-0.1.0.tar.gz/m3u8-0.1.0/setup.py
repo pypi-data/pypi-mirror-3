5945442b83728e6c0244f7b2fa2f7678b4c774cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:32 +0000

    Add package m3u8/0.1.0/m3u8-0.1.0.tar.gz

---
+++ packages/m3u8/m3u8-0.1.0.tar.gz/m3u8-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="m3u8",
+    author='Globo.com',
+    version="0.1.0",
+    zip_safe=False,
+    packages=["m3u8"],
+    url="https://github.com/globocom/m3u8"
+    )
