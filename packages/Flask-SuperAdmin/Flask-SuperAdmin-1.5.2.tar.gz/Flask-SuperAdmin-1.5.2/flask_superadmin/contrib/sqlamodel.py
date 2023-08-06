dc8e0d0badc689fea473fabccb0110e42ea6b458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:52 +0000

    Add package Flask-SuperAdmin/1.5.2/Flask-SuperAdmin-1.5.2.tar.gz

---
+++ packages/Flask-SuperAdmin/Flask-SuperAdmin-1.5.2.tar.gz/Flask-SuperAdmin-1.5.2/flask_superadmin/contrib/sqlamodel.py
@@ -0,0 +1,5 @@
+from flask_superadmin.contrib import DeprecatedModelView
+
+from flask_superadmin.model.backends.sqlalchemy import ModelAdmin
+
+class ModelView(DeprecatedModelView, ModelAdmin): pass
