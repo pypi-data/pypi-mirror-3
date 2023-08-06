62d2f002ae534c4c1ad0bf688eae34b9f3bda915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:08 +0000

    Add package Py-Infinote/0.91/Py-Infinote-0.91.tar.gz

---
+++ packages/Py-Infinote/Py-Infinote-0.91.tar.gz/Py-Infinote-0.91/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(
+    name='Py-Infinote',
+    version="0.91",
+    description="Py-Infinote is a Python implementation of the infinote \
+    operation transformation protocol. It's a direct port of JInfinote",
+    long_description=(read('README.rst')),
+    author="Jeroen van Veen",
+    author_email="j.veenvan@gmail.com",
+    url="https://github.com/phrearch/py-infinote",
+    license="BSD",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Information Technology",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Operating System :: OS Independent"
+    ],
+    packages=('infinote', ),
+    keywords="infinote,text,editing,collaborative,ot,adopted",
+    include_package_data=True,
+    zip_safe=False,
+)
