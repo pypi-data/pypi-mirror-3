30bf2ad940f215cb1e701f7c9cc80825f8cf4858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:46 +0000

    Add package ztfy.i18n/0.2.6/ztfy.i18n-0.2.6.tar.gz

---
+++ packages/ztfy.i18n/ztfy.i18n-0.2.6.tar.gz/ztfy.i18n-0.2.6/ztfy/i18n/browser/namespace.py
@@ -0,0 +1,43 @@
+### -*- coding: utf-8 -*- ####################################################
+##############################################################################
+#
+# Copyright (c) 2008 Thierry Florac <tflorac AT ulthar.net>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+
+# import standard packages
+
+# import Zope3 interfaces
+from z3c.language.switch.interfaces import II18n
+from zope.traversing.interfaces import TraversalError
+from zope.app.file.interfaces import IFile
+
+# import local interfaces
+
+# import Zope3 packages
+from zope.traversing import namespace
+
+# import local packages
+
+
+class I18nFilePropertyTraverser(namespace.view):
+    """Simple file property traverser"""
+
+    def traverse(self, name, ignored):
+        if name.find(':') > 0:
+            name, lang = name.split(':')
+            result = II18n(self.context).getAttribute(name, language=lang)
+        else:
+            result = II18n(self.context).queryAttribute(name, request=self.request)
+        if not IFile.providedBy(result):
+            raise TraversalError("++i18n++%s" % name)
+        return result
