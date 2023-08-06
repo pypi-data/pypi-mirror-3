e08bd1268e233aa6a00dad686a2b3591a472e9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:21 +0000

    Add package Products.Object/0.8/Products.Object-0.8.tar.gz

---
+++ packages/Products.Object/Products.Object-0.8.tar.gz/Products.Object-0.8/Products/Object/config.py
@@ -0,0 +1,9 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'Products.Object'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+    'Object': 'Products.Object: Add Object',
+}
