ce43b912b80e5b1437bfeae66c4535272ff573c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:05 +0000

    Add package npyscreen/2.0pre49/npyscreen-2.0pre49.tar.gz

---
+++ packages/npyscreen/npyscreen-2.0pre49.tar.gz/npyscreen-2.0pre49/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+	name="npyscreen",
+	version="2.0pre49",
+	description="Writing user interfaces without all that ugly mucking about in hyperspace",
+	author="Nicholas Cole",
+	author_email="n@npcole.com",
+	url="http://www.npcole.com/npyscreen/",
+	packages=['npyscreen'],
+	license='New BSD License',
+	classifiers= [
+	    'Environment :: Console',
+	    'Operating System :: POSIX',
+	    'Environment :: Console :: Curses',
+	    'Intended Audience :: Developers',
+	    'License :: OSI Approved :: BSD License',
+	    'Topic :: Terminals'
+	    ],
+	long_description = """This library provides a framework for developing console applications using Python and curses.
+
+This framework should be powerful enough to create everything from quick, simple programs to complex, multi-screen applications. It is designed to make doing the simple tasks very quick and to take much of the pain out of writing larger applications.
+
+There is a very wide variety of default widgets - everything from simple text fields to more complex tree and grid views.
+
+I have used versions of this library for private scripts and small applications for around ten years. As a result, it is fairly mature.	
+	"""
+)
